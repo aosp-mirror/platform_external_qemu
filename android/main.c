bb57db60f4bef73d6da701a68f0bce562ef2803f
Marcus Comstedt <marcus@mc.pp.se>
2010-10-01 22:43:44 +0200

    Propagate envp parameter to the real main()
    The envp parameter is used for the call to qemu_cache_utils_init(),
    so make sure the main function actually gets it.
    
    If SDL wraps main, there is no envp parameter, so pass NULL instead.
    This behavior is copied from upstreams.
    
    Change-Id: I0dcf401a3c1820dd18c282673380d3bcfc438e6e

--- android/main.c
+++ android/main.c
@@ -473,7 +473,7 @@ found_a_skin:
     }
 }
 
-int qemu_main(int argc, char **argv);
+int qemu_main(int argc, char **argv, char **envp);
 
 /* this function dumps the QEMU help */
 extern void  help( void );
@@ -732,8 +732,14 @@ _adjustPartitionSize( const char*  description,
     return convertMBToBytes(imageMB);
 }
 
+#if defined(__APPLE__) || defined(main)
 int main(int argc, char **argv)
 {
+    char** envp = NULL;
+#else
+int main(int argc, char **argv, char **envp)
+{
+#endif
     char   tmp[MAX_PATH];
     char*  tmpend = tmp + sizeof(tmp);
     char*  args[128];
@@ -1640,5 +1646,5 @@ int main(int argc, char **argv)
             fprintf(stdout, "emulator: argv[%02d] = \"%s\"\n", i, args[i]);
         }
     }
-    return qemu_main(n, args);
+    return qemu_main(n, args, envp);
 }
