5cd2c6950255300c2b30982c959bc2731a4202b7
Bruce Beare <bruce.j.beare@intel.com>
2011-02-21 12:36:24 -0800

    Build both IA and ARM versions
    Change-Id: I572d2b1488b74bcaca9386382f75e6009b60d652
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,9 @@
-# the following test is made to detect that we were called
-# through the 'm' or 'mm' build commands. if not, we use the
-# standard QEMU Makefile
-#
-ifeq ($(DEFAULT_GOAL),droid)
-    LOCAL_PATH:= $(call my-dir)
-    include $(LOCAL_PATH)/Makefile.android
-else
-    include Makefile.qemu
-endif
+LOCAL_PATH:= $(call my-dir)
+
+EMULATOR_TARGET_CPU_DIR := arm
+EMULATOR_TARGET_CPU_ABI := armeabi
+include $(LOCAL_PATH)/Makefile.android
+
+EMULATOR_TARGET_CPU_DIR := i386
+EMULATOR_TARGET_CPU_ABI := x86
+include $(LOCAL_PATH)/Makefile.android
