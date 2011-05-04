9fb360ec4c47d750711c8f1776c180e3802b0aab
David 'Digit' Turner <digit@google.com>
2011-05-04 22:01:28 +0200

    Correct fix for snapshot saving speed
    QEMU is a weird animal. The "snapshot" property speeds-up snapshot
    saving but also redirect all stores to a temporary file which is
    deleted when the emulator exit.
    
    Use a different property to get the desired speed-up. Note that
    using "writeback" instead of "unsafe" forces the use of fdatasync()
    which will still be horribly slow.
    
    + Argument checking in avd snapshot commands in the console.
      (trying to load a name-less snapshot actually crashes the emulator).
    
    Change-Id: Ie61f110f037bbb3539c7f9892cb03bee8bfec6bd

--- vl-android.c
+++ vl-android.c
@@ -5095,9 +5095,9 @@ int main(int argc, char **argv, char **envp)
             hdb_opts = drive_add(spath, HD_ALIAS, 1);
             /* VERY IMPORTANT:
              *    Set this property or the file will be mounted with O_DIRECT,
-             *    which will slow down snapshot saving.x100 !
+             *    which will slow down snapshot saving x100 !
              */
-            qemu_opt_set(hdb_opts, "snapshot", "on");
+            qemu_opt_set(hdb_opts, "cache", "unsafe");
         }
     }
 
