2eb91591a630ecc8b2770c8dc3ec006e8264e742
Vladimir Chtchetkine <vchtchetkine@google.com>
2012-03-08 14:20:20 -0800

    Increase default partition size, and fix data partition size calculation. do not merge.
    There is a (reasonable) demand from android development comunity to increase default
    data partition size. It seems that 66MB doesn't match anymore user needs. So, with this
    CL default data partition size will be increased to 12MB.
    
    This CL also fixes a bug where data partition size set by the AVD manager has been wrongly
    treated as an MB value, while it was a byte value.
    
    (cherry picked from commit d4f5a3ae87a7246613188940c1667bf2880da402)
    
    Change-Id: I282b1f031e48ebb726d5a0b6b391138961dac66f

--- android/main.c
+++ android/main.c
@@ -235,7 +235,7 @@ int main(int argc, char **argv)
     AConfig*          skinConfig;
     char*             skinPath;
     int               inAndroidBuild;
-    uint64_t          defaultPartitionSize = convertMBToBytes(66);
+    uint64_t          defaultPartitionSize = convertMBToBytes(128);
 
     AndroidOptions  opts[1];
     /* net.shared_net_ip boot property value. */
@@ -729,7 +729,7 @@ int main(int argc, char **argv)
         uint64_t     defaultBytes =
                 hw->disk_dataPartition_size == 0 ?
                 defaultPartitionSize :
-                convertMBToBytes(hw->disk_dataPartition_size);
+                hw->disk_dataPartition_size;
         uint64_t     dataBytes;
         const char*  dataPath = hw->disk_dataPartition_initPath;
 
