88078b8875dab47abf9fe80488503f60d9835eaf
Vladimir Chtchetkine <vchtchetkine@google.com>
2012-04-19 12:24:38 -0700

    Increate default partition size to 200MB
    Change-Id: I2996197e7ec4c36c7e46b35edab5b10d7f103c10

--- android/main.c
+++ android/main.c
@@ -176,7 +176,7 @@ int main(int argc, char **argv)
     AConfig*          skinConfig;
     char*             skinPath;
     int               inAndroidBuild;
-    uint64_t          defaultPartitionSize = convertMBToBytes(128);
+    uint64_t          defaultPartitionSize = convertMBToBytes(200);
 
     AndroidOptions  opts[1];
     /* net.shared_net_ip boot property value. */
