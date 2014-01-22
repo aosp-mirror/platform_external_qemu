60d0a1536e4e75cee039c09da65a2e994be2e106
David 'Digit' Turner <digit@google.com>
2014-01-22 01:09:57 +0100

    Fix Windows command-line quoting.
    When the 'emulator.exe' program invokes the real emulation program
    (e.g. 'emulator-arm.exe' or 'emulator-x86.exe'), it passes its own
    command-line arguments through execv().
    
    Unfortunately, on Windows this doesn't work, as the command-line
    arguments must be quoted in a special way. This is describ
    is described in detail in the MSDN blog post named
    "Everyone quotes command line arguments the wrong way" [1]
    
    The code under android/main-emulator.c had several problems:
    
    - It only quoted the first parameter (the program name), but
      not any other one.
    
    - The quoting was too naive to work in the general case.
    
    This patch adds a utility function named win32_cmdline_quote()
    to perform correct quoting, and changes android/main-emulator.c
    to use it.
    
    Fix for http://b.android.com/18317
    
    [1] http://blogs.msdn.com/b/twistylittlepassagesallalike/archive/2011/04/23/everyone-quotes-arguments-the-wrong-way.aspx
    
    Change-Id: I7cfd6ddd10706c7aeb6c7df56e20398277d78409

---
+++ android/utils/compiler.h
@@ -0,0 +1,23 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_UTILS_COMPILER_H
+#define ANDROID_UTILS_COMPILER_H
+
+#ifdef __cplusplus
+#define ANDROID_BEGIN_HEADER extern "C" {
+#define ANDROID_END_HEADER   }
+#else
+#define ANDROID_BEGIN_HEADER /* nothing */
+#define ANDROID_END_HEADER  /* nothing */
+#endif
+
+#endif  // ANDROID_UTILS_COMPILER_H
