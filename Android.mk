1af8215c58e5c94432f04ec87d34e429fe7cd7a6
David 'Digit' Turner <digit@google.com>
2014-03-03 20:41:37 +0100

    Support GPU emulation standalone build.
    This patch modifies the emulator's standalone build system
    to rebuild all GPU emulation libraries directly from sources,
    assuming their are located under ../../sdk/emulator/opengl/
    or ../opengl/
    
    Change-Id: I2ac8fc4bc2d06561dc52298689840f74171397aa

--- Android.mk
+++ Android.mk
@@ -2,9 +2,5 @@
 # through the 'm' or 'mm' build commands. if not, we use the
 # standard QEMU Makefile
 #
-ifeq ($(DEFAULT_GOAL),droid)
-    LOCAL_PATH:= $(call my-dir)
-    include $(LOCAL_PATH)/Makefile.android
-else
-    include Makefile.qemu
-endif
+LOCAL_PATH:= $(call my-dir)
+include $(LOCAL_PATH)/Makefile.android
