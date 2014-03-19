b974f3f88682fc47219fe08c308ddc1a07722f4e
David 'Digit' Turner <digit@google.com>
2014-03-19 14:13:35 +0100

    exec.c: Use ram_addr_t in cpu_physical_memory_rw(...).
    Upstream 8ca5692df4a635b2c9211d6dc3dea15bd2b674c8
    
    Change-Id: If80e841279631740034c1d2fbc1ed47a0448d75c

--- exec.c
+++ exec.c
@@ -1923,7 +1923,7 @@ void cpu_physical_memory_rw(hwaddr addr, void *buf,
     uint8_t *ptr;
     uint32_t val;
     hwaddr page;
-    unsigned long pd;
+    ram_addr_t pd;
     uint8_t* buf8 = (uint8_t*)buf;
     PhysPageDesc *p;
 
@@ -1964,7 +1964,7 @@ void cpu_physical_memory_rw(hwaddr addr, void *buf,
                     l = 1;
                 }
             } else {
-                unsigned long addr1;
+                ram_addr_t addr1;
                 addr1 = (pd & TARGET_PAGE_MASK) + (addr & ~TARGET_PAGE_MASK);
                 /* RAM case */
                 ptr = qemu_get_ram_ptr(addr1);
