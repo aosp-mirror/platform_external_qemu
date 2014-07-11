57b2a766f7af808f7670aecbe57a18373a903a0d
David 'Digit' Turner <digit@google.com>
2014-07-11 13:55:21 +0200

    android/: Add proper C++ inclusion guards.
    Change-Id: Ie3f03d967323448eda87f7b4454345323d5bfec0

--- android/main-common.h
+++ android/main-common.h
@@ -13,10 +13,13 @@
 #define ANDROID_MAIN_COMMON_H
 
 #include <stdint.h>
-#include "android/cmdline-option.h"
-#include "android/skin/keyset.h"
-#include "android/config-file.h"
 #include "android/avd/hw-config.h"
+#include "android/cmdline-option.h"
+#include "android/config-file.h"
+#include "android/skin/keyset.h"
+#include "android/utils/compiler.h"
+
+ANDROID_BEGIN_HEADER
 
 /* Common routines used by both android/main.c and android/main-ui.c */
 
@@ -102,5 +105,7 @@ void handle_ui_options( AndroidOptions* opts );
  * standalone UI program. This is a no-op otherwise.
  */
 int attach_ui_to_core( AndroidOptions* opts );
+
+ANDROID_END_HEADER
 
 #endif /* ANDROID_MAIN_COMMON_H */
