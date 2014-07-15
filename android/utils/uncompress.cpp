7a21e6e3534284d250d6f791b186db248dbe4c17
Vince Harron <vharron@google.com>
2014-07-15 09:25:18 -0700

    Add kernel version detection to emulator
    Different Linux kernels require different handling by the emulator.
    
    Modified emulator to scan compressed/uncompressed kernel images for
    a 'Linux version ' string.  Manually tested on all prebuilt kernel
    images and added basic unit tests.
    
    Change-Id: Icecfb048e9184fd983799d0975215448feba8e08

---
+++ android/utils/uncompress.cpp
@@ -0,0 +1,48 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/utils/uncompress.h"
+#include "zlib.h"
+
+int uncompress_gzipStream(uint8_t* dst, size_t* dstLen, const uint8_t* src,
+                   size_t srcLen) {
+    z_stream stream;
+    stream.next_in = (Bytef*)src;
+    stream.avail_in = srcLen;
+    stream.next_out = dst;
+    stream.avail_out = *dstLen;
+    stream.zalloc = (alloc_func)0;
+    stream.zfree = (free_func)0;
+
+    // magic number from gz_read
+    const int GZIP_WINDOW_BITS = 15 + 16;
+
+    int result = inflateInit2(&stream, GZIP_WINDOW_BITS);
+    if (result != Z_OK) {
+        return result;
+    }
+
+    result = inflate(&stream, Z_FINISH);
+    if (result == Z_BUF_ERROR || result == Z_OK) {
+        *dstLen = stream.total_out;
+    }
+
+    if (result != Z_STREAM_END) {
+        inflateEnd(&stream);
+
+        // ignore inflateEnd result and return whatever we got from inflate
+        if (result == Z_NEED_DICT || (result == Z_BUF_ERROR && stream.avail_in == 0)) {
+            return result;
+        }
+        return result;
+    }
+    return inflateEnd(&stream);
+}
