e06f6c05c7f9f3a3b63ce06b8902e51fd77d87c1
bohu <bohu@google.com>
2014-07-15 18:16:45 -0700

    shutdown socket gracefully with SocketDrainer
    Previous implementation does not actually drain socket before
    closing it. The new SocketDrainer class will read all the data
    from the socket and then close it.
    
    Change-Id: Ia9d47051bdb02ce5b24ebd836df7fdcf51d647fe

---
+++ android/base/SocketDrainer.h
@@ -0,0 +1,64 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_SOCKET_DRAINER_H
+#define ANDROID_SOCKET_DRAINER_H
+
+struct Looper;
+
+namespace android {
+namespace base {
+/*
+
+This class is used to manage graceful closure of sockets, as described by
+"The ultimate SO_LINGER page", which requires several steps to do properly.
+
+To use this class from C code:
+// After creating a looper, call following
+   start_socket_drainer(looper);
+
+// When a socket needs to be gracefully closed, call following
+   socket_drainer_add (socket_fd);
+// Note: the above function can be called before calling start_socket_drainer(looper),
+   or after calling stop_socket_drainer(). In those cases, the socket is simply shut down
+   and closed without receiving all the pending data.
+
+// After looper_run completes, call following
+   stop_socket_drainer();
+
+// For C++ code, simply create an object as follows:
+
+   SocketDrainer drainManager(looper);
+// When a socket needs to be gracefully closed, call following
+   drainManager.add(socket_fd);
+
+*/
+
+class SocketDrainerImpl;
+
+class SocketDrainer {
+
+public:
+    SocketDrainer(Looper* looper);
+    ~SocketDrainer();
+
+    // Add a socket to be properly closed
+    void add(int socket_fd);
+private:
+    SocketDrainerImpl*  mSocketDrainerImpl;
+};
+
+} // namespace base
+} // namespace android
+
+#endif
+
+
