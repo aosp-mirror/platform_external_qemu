2d458683a48a0d0766ea7bdd1140813fe5d12315
David 'Digit' Turner <digit@google.com>
2014-08-14 12:13:43 +0200

    android/iolooper.h: Refactor on top of SocketWaiter
    Refactor the implementation of android/iolooper.h to use the
    SocketWaiter class from android/base/.
    
    Change-Id: I072325a99d697b84e2b6802662290091e9e92ed8

--- android/iolooper.cpp
+++ android/iolooper.cpp
@@ -1,238 +1,95 @@
 #include "android/iolooper.h"
-#include "qemu-common.h"
 
-/* An implementation of iolooper.h based on Unix select() */
-#ifdef _WIN32
-#  include <winsock2.h>
-#else
-#  include <sys/types.h>
-#  include <sys/select.h>
-#endif
-#include "android/sockets.h"
+#include "android/base/sockets/SocketWaiter.h"
 
-struct IoLooper {
-    fd_set   reads[1];
-    fd_set   writes[1];
-    fd_set   reads_result[1];
-    fd_set   writes_result[1];
-    int      max_fd;
-    int      max_fd_valid;
-};
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/time.h>
 
-IoLooper*
-iolooper_new(void)
-{
-    IoLooper*  iol = malloc(sizeof(*iol));
-    iolooper_reset(iol);
-    return iol;
+using namespace android::base;
+
+namespace {
+
+SocketWaiter* asWaiter(IoLooper* iol) {
+    return reinterpret_cast<SocketWaiter*>(iol);
 }
 
-void
-iolooper_free( IoLooper*  iol )
-{
-    free(iol);
-}
-
-void
-iolooper_reset( IoLooper*  iol )
-{
-    FD_ZERO(iol->reads);
-    FD_ZERO(iol->writes);
-    iol->max_fd = -1;
-    iol->max_fd_valid = 1;
-}
-
-static void
-iolooper_add_fd( IoLooper*  iol, int fd )
-{
-    if (iol->max_fd_valid && fd > iol->max_fd) {
-        iol->max_fd = fd;
+unsigned toWaiterEvents(int flags) {
+    unsigned events = 0;
+    if (flags & IOLOOPER_READ) {
+        events |= SocketWaiter::kEventRead;
     }
-}
-
-static void
-iolooper_del_fd( IoLooper*  iol, int fd )
-{
-    if (iol->max_fd_valid && fd == iol->max_fd)
-        iol->max_fd_valid = 0;
-}
-
-void
-iolooper_modify( IoLooper* iol, int fd, int oldflags, int newflags )
-{
-    if (fd < 0)
-        return;
-
-    int changed = oldflags ^ newflags;
-
-    if ((changed & IOLOOPER_READ) != 0) {
-        if ((newflags & IOLOOPER_READ) != 0)
-            iolooper_add_read(iol, fd);
-        else
-            iolooper_del_read(iol, fd);
+    if (flags & IOLOOPER_WRITE) {
+        events |= SocketWaiter::kEventWrite;
     }
-    if ((changed & IOLOOPER_WRITE) != 0) {
-        if ((newflags & IOLOOPER_WRITE) != 0)
-            iolooper_add_write(iol, fd);
-        else
-            iolooper_del_write(iol, fd);
-    }
+    return events;
 }
 
+}  // namespace
 
-static int
-iolooper_fd_count( IoLooper*  iol )
-{
-    int  max_fd = iol->max_fd;
-    int  fd;
-
-    if (iol->max_fd_valid)
-        return max_fd + 1;
-
-    max_fd = -1;
-
-    /* recompute max fd */
-    for (fd = 0; fd < FD_SETSIZE; fd++) {
-        if (!FD_ISSET(fd, iol->reads) && !FD_ISSET(fd, iol->writes))
-            continue;
-
-        max_fd = fd;
-    }
-    iol->max_fd       = max_fd;
-    iol->max_fd_valid = 1;
-
-    return max_fd + 1;
+IoLooper* iolooper_new(void) {
+    return reinterpret_cast<IoLooper*>(SocketWaiter::create());
 }
 
-void
-iolooper_add_read( IoLooper*  iol, int  fd )
-{
-    if (fd >= 0) {
-        iolooper_add_fd(iol, fd);
-        FD_SET(fd, iol->reads);
-    }
+void iolooper_free(IoLooper*  iol) {
+    delete asWaiter(iol);
 }
 
-void
-iolooper_add_write( IoLooper*  iol, int  fd )
-{
-    if (fd >= 0) {
-        iolooper_add_fd(iol, fd);
-        FD_SET(fd, iol->writes);
-    }
+void iolooper_reset(IoLooper*  iol) {
+    asWaiter(iol)->reset();
 }
 
-void
-iolooper_del_read( IoLooper*  iol, int  fd )
-{
-    if (fd >= 0) {
-        iolooper_del_fd(iol, fd);
-        FD_CLR(fd, iol->reads);
-    }
+void iolooper_modify(IoLooper* iol, int fd, int oldflags, int newflags) {
+    asWaiter(iol)->update(fd, toWaiterEvents(newflags));
 }
 
-void
-iolooper_del_write( IoLooper*  iol, int  fd )
-{
-    if (fd >= 0) {
-        iolooper_del_fd(iol, fd);
-        FD_CLR(fd, iol->writes);
-    }
+void iolooper_add_read(IoLooper* iol, int fd) {
+    SocketWaiter* waiter = asWaiter(iol);
+    unsigned events = waiter->wantedEventsFor(fd);
+    waiter->update(fd, events | SocketWaiter::kEventRead);
 }
 
-int
-iolooper_poll( IoLooper*  iol )
-{
-    int     count = iolooper_fd_count(iol);
-    int     ret;
-    fd_set  errs;
-
-    if (count == 0)
-        return 0;
-
-    FD_ZERO(&errs);
-
-    do {
-        struct timeval  tv;
-
-        tv.tv_sec = tv.tv_usec = 0;
-
-        iol->reads_result[0]  = iol->reads[0];
-        iol->writes_result[0] = iol->writes[0];
-
-        ret = select( count, iol->reads_result, iol->writes_result, &errs, &tv);
-    } while (ret < 0 && errno == EINTR);
-
-    return ret;
+void iolooper_add_write(IoLooper* iol, int fd) {
+    SocketWaiter* waiter = asWaiter(iol);
+    unsigned events = waiter->wantedEventsFor(fd);
+    waiter->update(fd, events | SocketWaiter::kEventWrite);
 }
 
-int
-iolooper_wait( IoLooper*  iol, int64_t  duration )
-{
-    int     count = iolooper_fd_count(iol);
-    int     ret;
-    fd_set  errs;
-    struct timeval tm0, *tm = NULL;
-
-    if (count == 0)
-        return 0;
-
-    CLAMP_MAC_TIMEOUT(duration);
-
-    if (duration < 0)
-        tm = NULL;
-    else {
-        tm = &tm0;
-        tm->tv_sec  = duration / 1000;
-        tm->tv_usec = (duration - 1000*tm->tv_sec) * 1000;
-    }
-
-    FD_ZERO(&errs);
-
-    do {
-        iol->reads_result[0]  = iol->reads[0];
-        iol->writes_result[0] = iol->writes[0];
-
-        ret = select( count, iol->reads_result, iol->writes_result, &errs, tm);
-        if (ret == 0) {
-            // Indicates timeout
-            errno = ETIMEDOUT;
-        }
-    } while (ret < 0 && errno == EINTR);
-
-    return ret;
+void iolooper_del_read(IoLooper*  iol, int  fd) {
+    SocketWaiter* waiter = asWaiter(iol);
+    unsigned events = waiter->wantedEventsFor(fd);
+    waiter->update(fd, events & ~SocketWaiter::kEventRead);
 }
 
-
-int
-iolooper_is_read( IoLooper*  iol, int  fd )
-{
-    return FD_ISSET(fd, iol->reads_result);
+void iolooper_del_write(IoLooper*  iol, int  fd) {
+    SocketWaiter* waiter = asWaiter(iol);
+    unsigned events = waiter->wantedEventsFor(fd);
+    waiter->update(fd, events & ~SocketWaiter::kEventWrite);
 }
 
-int
-iolooper_is_write( IoLooper*  iol, int  fd )
-{
-    return FD_ISSET(fd, iol->writes_result);
+int iolooper_wait(IoLooper* iol, int64_t  duration) {
+    return asWaiter(iol)->wait(duration);
 }
 
-int
-iolooper_has_operations( IoLooper* iol )
-{
-    return iolooper_fd_count(iol) > 0;
+int iolooper_is_read(IoLooper* iol, int fd) {
+    return asWaiter(iol)->pendingEventsFor(fd) & SocketWaiter::kEventRead;
 }
 
-int64_t
-iolooper_now(void)
-{
+int iolooper_is_write(IoLooper* iol, int  fd) {
+    return asWaiter(iol)->pendingEventsFor(fd) & SocketWaiter::kEventWrite;
+}
+
+int iolooper_has_operations(IoLooper* iol) {
+    return asWaiter(iol)->hasFds();
+}
+
+int64_t iolooper_now(void) {
     struct timeval time_now;
     return gettimeofday(&time_now, NULL) ? -1 : (int64_t)time_now.tv_sec * 1000LL +
                                                 time_now.tv_usec / 1000;
 }
 
-int
-iolooper_wait_absolute(IoLooper* iol, int64_t deadline)
-{
+int iolooper_wait_absolute(IoLooper* iol, int64_t deadline) {
     int64_t timeout = deadline - iolooper_now();
 
     /* If the deadline has passed, set the timeout to 0, this allows us
