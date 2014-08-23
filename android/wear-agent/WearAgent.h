513b2043f29d0777024930dca015d2316add8bb9
bohu <bohu@google.com>
2014-08-23 00:52:41 -0700

    Connects wear with compatible phone
    Implements the wear agent that connects wear to compatible phone.
    The phone could be emulator or real device. Previously, manual procedure
    is used to setup the connection and the setup has to be performed each
    time the connection is lost or adb server is killed. With this wear
    agent, all the connection and reconnection is done automatically.
    
    Limitation: only one wear and one compatible phone is connected.
    Multiple connection is not handled in this implementatation.
    
    Change-Id: Ia4632e80f5504f532a4f2ace5b89fda49643ddb9

---
+++ android/wear-agent/WearAgent.h
@@ -0,0 +1,69 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_WEAR_AGENT_WEARAGENT_H
+#define ANDROID_WEAR_AGENT_WEARAGENT_H
+
+#include "android/base/Compiler.h"
+#include "android/base/async/Looper.h"
+
+// This class establishes a connection between one Android Wear and one
+// compatible phone, so that the wear can get notifications automatically
+// from the phone. The phone could be emulated phone or a real phone.
+//
+// Usage is simply:
+//
+//     WearAgent agent(looper);
+//
+// Then simply run the |looper| as usual.
+//
+// When |agent| falls out of scope before the looper stops, any on-going
+// pairing activity will be aborted. In addition, the looper should not be
+// destroyed when the agent is destroyed.
+
+namespace android {
+
+namespace base {
+class Looper;
+}  // namespace base
+
+namespace wear {
+
+class WearAgentImpl;
+
+class WearAgent {
+public:
+    // Create a new Android Wear agent instance.
+    //
+    // |looper| is a Looper instance that is used to by the agent to process
+    // pairing requests asynchronously.
+    //
+    // |adbHostPort| is the localhost TCP port number used to talk to
+    // the ADB server on the host.
+    //
+    // Creating the instance doesn't do anything, one has to run the looper
+    // to ensure that the agent works.
+    WearAgent(::android::base::Looper* looper, int adbHostPort = 5037);
+
+    // Destroy a given Android Wear agent instance. This removes it from
+    // its looper. NOTE: This must happen _before_ the looper is destroyed.
+    ~WearAgent();
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(WearAgent);
+
+    WearAgentImpl*  mWearAgentImpl;
+};
+
+}  // namespace wear
+}  // namespace android
+
+#endif  // ANDROID_WEAR_AGENT_WEARAGENT_H
