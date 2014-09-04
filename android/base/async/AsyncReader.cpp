be01ba1720633afbc94ad9464d4aaab7515e88d3
David 'Digit' Turner <digit@google.com>
2014-09-04 15:56:27 +0200

    android/base/async/: Add AsyncReader and AsyncWriter
    Change-Id: I1814b24a78e0b3eb663867ea822439c514e2e889

---
+++ android/base/async/AsyncReader.cpp
@@ -0,0 +1,63 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/base/async/AsyncReader.h"
+
+#include "android/base/sockets/SocketUtils.h"
+
+#include <errno.h>
+
+namespace android {
+namespace base {
+
+void AsyncReader::reset(void* buffer,
+                        size_t bufferSize,
+                        Looper::FdWatch* watch) {
+    mBuffer = reinterpret_cast<uint8_t*>(buffer);
+    mBufferSize = bufferSize;
+    mPos = 0U;
+    mFdWatch = watch;
+    if (bufferSize > 0) {
+        watch->wantRead();
+    }
+}
+
+AsyncStatus AsyncReader::run() {
+    if (mPos >= mBufferSize) {
+        return kAsyncCompleted;
+    }
+
+    do {
+        ssize_t ret = socketRecv(mFdWatch->fd(),
+                                 mBuffer + mPos,
+                                 mBufferSize - mPos);
+        if (ret == 0) {
+            // Disconnection!
+            errno = ECONNRESET;
+            return kAsyncError;
+        }
+
+        if (ret < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                mFdWatch->wantRead();
+                return kAsyncAgain;
+            }
+            return kAsyncError;
+        }
+        mPos += static_cast<size_t>(ret);
+    } while (mPos < mBufferSize);
+
+    mFdWatch->dontWantRead();
+    return kAsyncCompleted;
+}
+
+}  // namespace base
+}  // namespace android
---
+++ android/base/async/AsyncWriter.h
@@ -0,0 +1,47 @@
+// Copyright 2014 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_BASE_ASYNC_ASYNC_WRITER_H
+#define ANDROID_BASE_ASYNC_ASYNC_WRITER_H
+
+#include "android/base/async/AsyncStatus.h"
+#include "android/base/async/Looper.h"
+
+#include <stdint.h>
+
+namespace android {
+namespace base {
+
+class AsyncWriter {
+public:
+    AsyncWriter() :
+            mBuffer(NULL),
+            mBufferSize(0U),
+            mPos(0U),
+            mFdWatch(NULL) {}
+
+    void reset(const void* buffer,
+               size_t bufferSize,
+               Looper::FdWatch* watch);
+
+    AsyncStatus run();
+
+private:
+    const uint8_t* mBuffer;
+    size_t mBufferSize;
+    size_t mPos;
+    Looper::FdWatch* mFdWatch;
+};
+
+}  // namespace base
+}  // namespace android
+
+#endif  // ANDROID_BASE_ASYNC_ASYNC_WRITER_H
