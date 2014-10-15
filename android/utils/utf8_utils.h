ffaa9aaf1399a893a81992e0b45144c4a449f326
David 'Digit' Turner <digit@google.com>
2014-10-15 18:37:00 +0200

    Add UTF-8 encode/decode functions.
    These will be used by later patches.
    
    Change-Id: I26e8b6f72fc2c89c957df94f0c2eccdd8119c252

--- android/utils/utf8_utils.h
+++ android/utils/utf8_utils.h
@@ -18,10 +18,26 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 ANDROID_BEGIN_HEADER
 
 bool android_utf8_is_valid(const char* text, size_t text_len);
+
+// Try to decode an UTF-8 encoded Unicode codepoint from the |text_len| bytes
+// at |text|. On success, return the encoded length, and sets |*codepoint| to
+// the corresponding Unicode value. On failure (invalid input), return -1.
+int android_utf8_decode(const uint8_t* text,
+                        size_t text_len,
+                        uint32_t* codepoint);
+
+// Try to encode a Unicode |codepoint| into its UTF-8 representation into
+// |buffer|, which must hold at least |buffer_len| bytes.
+// Return the size of the encoding in bytes, or -1 in case of error
+// (e.g. invalid value).
+int android_utf8_encode(uint32_t codepoint,
+                        uint8_t* buffer,
+                        size_t buffer_len);
 
 ANDROID_END_HEADER
 
