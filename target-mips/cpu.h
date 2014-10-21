a95b293eea13c29dae7a1334d4b2e72f405867eb
Miodrag Dinic <miodrag.dinic@imgtec.com>
2014-10-21 17:28:25 +0200

    [MIPS64] fasttlb : Add check for high segbits and pool c0_context register for each VA-PA request
    When requesting translation of virtual addresses from
    mips_cpu_get_phys_page_debug() path, the page global directory (pgd)
    address must be pooled from the c0_context register, because it holds
    the current pgd of the target process address space this VA belongs to.
    
    This commit also includes updates to the fasttlb optimisation,
    for covering scenarios with addresses which use high segbits.
    
    The kernel currently implicitely assumes that the
    MIPS SEGBITS parameter for the processor is
    (PGDIR_SHIFT+PGDIR_BITS) or less, and will never
    allocate virtual addresses outside the maximum
    range for SEGBITS = (PGDIR_SHIFT+PGDIR_BITS). But
    that doesn't prevent user code from accessing the
    higher xuseg addresses.
    
    Change-Id: I791f79386047d75392779adcd9b53fff54777903

--- target-mips/cpu.h
+++ target-mips/cpu.h
@@ -235,11 +235,11 @@ struct CPUMIPSState {
 #define CP0VPEOpt_DWX0	0
     target_ulong CP0_EntryLo0;
     target_ulong CP0_EntryLo1;
-#define CP0ENTRYLO_PFN  6
-#define CP0ENTRYLO_C    3
-#define CP0ENTRYLO_D    2
-#define CP0ENTRYLO_V    1
-#define CP0ENTRYLO_G    0
+#define CP0EnLo_PFN  6
+#define CP0EnLo_C    3
+#define CP0EnLo_D    2
+#define CP0EnLo_V    1
+#define CP0EnLo_G    0
     target_ulong CP0_Context;
     int32_t CP0_PageMask;
     int32_t CP0_PageGrain;
