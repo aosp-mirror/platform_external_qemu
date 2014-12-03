aosp-mirror/platform_external_qemu

commit 5a4a159898e79ce88d1a2f85843e973036d6e057

android/skin/event.h: Add kEventTextInput event.
To better match the SDL2 input event model, create a new event
type used to text input, as opposed to raw key presses.

Now, whenever a key is pressed, two events will be generated
with SDL1.x: One kEventKeyDown event + one kEventTextInput one
containing the corresponding Unicode value.

Change-Id: I98157c4c4075aa09a83c721ba7b4a5bbe94ef1f6

android/skin/event-sdl.c
@@ -12,8 +12,11 @@
 #include "android/skin/event.h"
 
 #include "android/skin/keycode.h"
+#include "android/utils/utf8_utils.h"
 
 #include <SDL.h>
+
+#include <stdbool.h>
 
 /** LAST PRESSED KEYS
  ** a small buffer of last pressed keys, this is used to properly
@@ -245,8 +248,34 @@ static bool is_winsys_exit(SDL_Event* ev) {
 #endif
 }
 
+static bool prepare_text_input_event(SkinEvent* event,
+                                     uint32_t unicode, 
+                                     bool down) {
+    event->type = kEventTextInput;
+    // Encode the Unicode value as a 0-terminated UTF-8 value.
+    int len = android_utf8_encode(unicode,
+                                  event->u.text.text,
+                                  sizeof(event->u.text.text));
+    if (len < 0) {
+        return false;
+    }
+    event->u.text.down = down;
+    event->u.text.text[len] = '\0';
+    return true;
+}
+
 bool skin_event_poll(SkinEvent* event) {
     SDL_Event ev;
+
+    static SkinEvent pending_event;
+    static bool pending = false;
+
+    // We need to buffer at most one event to support text input with SDL 1.x
+    if (pending) {
+        pending = false;
+        event[0] = pending_event;
+        return true;
+    }
 
     for (;;) {
         if (!SDL_PollEvent(&ev)) {
@@ -265,10 +294,9 @@ bool skin_event_poll(SkinEvent* event) {
                 }
                 // fall-through.
 
-            case SDL_KEYUP:
-                event->type = (ev.type == SDL_KEYDOWN) ?
-                        kEventKeyDown :
-                        kEventKeyUp;
+            case SDL_KEYUP: {
+                bool down = (ev.type == SDL_KEYDOWN);
+                event->type = down ? kEventKeyDown : kEventKeyUp;
 
                 int sdl_sym = ev.key.keysym.sym;
                 int sdl_mod = ev.key.keysym.mod;
@@ -277,17 +305,18 @@ bool skin_event_poll(SkinEvent* event) {
                 event->u.key.keycode = sdl_sym_to_key_code(sdl_sym);
                 event->u.key.mod = sdl_mod_to_key_mod(sdl_mod);
 
+                // Prepare a pending text input event for the next call.
+
                 // ev.key.keysym.unicode is only valid on keydown events.
                 // and will be 0 on the corresponding keyup ones, so
                 // remember the set of last pressed key syms to "undo" the
                 // job.
                 LastKeys* last_keys = &sLastKeys;
-                event->u.key.unicode = unicode;
                 if (ev.type == SDL_KEYUP) {
                     LastKey* k = last_keys_find(last_keys, sdl_sym);
                     if (k != NULL) {
                         if (!unicode) {
-                            event->u.key.unicode = k->unicode;
+                            unicode = k->unicode;
                         }
                         if (!sdl_mod) {
                             event->u.key.mod = sdl_mod_to_key_mod(sdl_mod);
@@ -298,7 +327,19 @@ bool skin_event_poll(SkinEvent* event) {
                     last_keys_add(last_keys, sdl_sym, sdl_mod, unicode);
                 }
 
+                if (unicode) {
+                    // The unicode value is added to a pending event
+                    // unless it is invalid.
+                    if (prepare_text_input_event(
+                            &pending_event, 
+                            unicode, 
+                            down)) {
+                        pending = true;
+                    }
+                }
+
                 return true;
+                }
 
             case SDL_MOUSEMOTION:
                 event->type = kEventMouseMotion;

android/skin/keyboard.c
@@ -17,6 +17,7 @@
 #include "android/utils/debug.h"
 #include "android/utils/bufprint.h"
 #include "android/utils/system.h"
+#include "android/utils/utf8_utils.h"
 
 #include <stdio.h>
 
@@ -177,59 +178,70 @@ skin_keyboard_do_key_event( SkinKeyboard*   kb,
 void
 skin_keyboard_process_event(SkinKeyboard*  kb, SkinEvent* ev, int  down)
 {
-    int unicode = ev->u.key.unicode;
-    int keycode = ev->u.key.keycode;
-    int mod = ev->u.key.mod;
-
     /* ignore key events if we're not enabled */
     if (!kb->enabled) {
-        printf( "ignoring key event keycode=%d mod=0x%x unicode=%d\n",
-                keycode, mod, unicode );
+        //printf( "ignoring key event\n");
         return;
     }
 
-    /* first, try the keyboard-mode-independent keys */
-    int code = skin_keyboard_key_to_code(kb, keycode, mod, down);
-    if (code == 0) {
-        return;
-    }
-    if ((int)code > 0) {
-        skin_keyboard_do_key_event(kb, code, down);
-        skin_keyboard_flush(kb);
-        return;
-    }
-
-    /* Ctrl-K is used to switch between 'unicode' and 'raw' modes */
-    if (keycode == kKeyCodeK) {
-        if (mod == kKeyModLCtrl || mod == kKeyModRCtrl) {
-            if (down) {
-                kb->raw_keys = !kb->raw_keys;
-                skin_event_enable_unicode(!kb->raw_keys);
-                D( "switching keyboard to %s mode", kb->raw_keys ? "raw" : "unicode" );
+    if (ev->type == kEventTextInput) {
+        if (!kb->raw_keys) {
+            // TODO(digit): For each Unicode value in the input text.
+            const uint8_t* text = ev->u.text.text;
+            const uint8_t* end = text + sizeof(ev->u.text.text);
+            while (text < end && *text) {
+                uint32_t codepoint = 0;
+                int len = android_utf8_decode(text, end - text, &codepoint);
+                if (len < 0) {
+                    break;
+                }
+                skin_keyboard_process_unicode_event(kb, codepoint, down);
+                text += len;
             }
+            skin_keyboard_flush(kb);
+        }
+    } else if (ev->type == kEventKeyDown || ev->type == kEventKeyUp) {
+        int keycode = ev->u.key.keycode;
+        int mod = ev->u.key.mod;
+
+        /* first, try the keyboard-mode-independent keys */
+        int code = skin_keyboard_key_to_code(kb, keycode, mod, down);
+        if (code == 0) {
             return;
         }
-    }
+        if ((int)code > 0) {
+            skin_keyboard_do_key_event(kb, code, down);
+            skin_keyboard_flush(kb);
+            return;
+        }
 
-    if (!kb->raw_keys &&
-        skin_keyboard_process_unicode_event(kb, unicode, down) > 0)
-    {
-        skin_keyboard_flush(kb);
-        return;
-    }
+        /* Ctrl-K is used to switch between 'unicode' and 'raw' modes */
+        if (keycode == kKeyCodeK) {
+            if (mod == kKeyModLCtrl || mod == kKeyModRCtrl) {
+                if (down) {
+                    kb->raw_keys = !kb->raw_keys;
+                    skin_event_enable_unicode(!kb->raw_keys);
+                    D( "switching keyboard to %s mode", kb->raw_keys ? "raw" : "unicode" );
+                }
+                return;
+            }
+        }
 
-    if ( !kb->raw_keys &&
-         (code == kKeyCodeAltLeft  || code == kKeyCodeAltRight ||
-          code == kKeyCodeCapLeft  || code == kKeyCodeCapRight ||
-          code == kKeyCodeSym) ) {
-        return;
-    }
+        code = keycode;
 
-    if (code == -1) {
-        D("ignoring keycode %d", keycode);
-    } else if (code > 0) {
-        skin_keyboard_do_key_event(kb, code, down);
-        skin_keyboard_flush(kb);
+        if ( !kb->raw_keys &&
+            (code == kKeyCodeAltLeft  || code == kKeyCodeAltRight ||
+             code == kKeyCodeCapLeft  || code == kKeyCodeCapRight ||
+             code == kKeyCodeSym) ) {
+            return;
+        }
+
+        if (code == -1) {
+            D("ignoring keycode %d", keycode);
+        } else if (code > 0) {
+            skin_keyboard_do_key_event(kb, code, down);
+            skin_keyboard_flush(kb);
+        }
     }
 }
 

android/skin/event.h
@@ -18,6 +18,7 @@
 typedef enum {
     kEventKeyDown,
     kEventKeyUp,
+    kEventTextInput,
     kEventMouseButtonDown,
     kEventMouseButtonUp,
     kEventMouseMotion,
@@ -36,8 +37,13 @@ typedef enum {
 typedef struct {
     uint32_t keycode;
     uint32_t mod;
-    uint32_t unicode;
+    //uint32_t unicode;
 } SkinEventKeyData;
+
+typedef struct {
+    bool down;
+    uint8_t text[32];
+} SkinEventTextInputData;
 
 typedef struct {
     int x;
@@ -52,6 +58,7 @@ typedef struct {
     union {
         SkinEventKeyData key;
         SkinEventMouseData mouse;
+        SkinEventTextInputData text;
     } u;
 } SkinEvent;
 

android/skin/ui.c
@@ -369,6 +369,10 @@ bool skin_ui_process_events(SkinUI* ui) {
             skin_keyboard_process_event(ui->keyboard, &ev, 0);
             break;
 
+        case kEventTextInput:
+            skin_keyboard_process_event(ui->keyboard, &ev, ev.u.text.down);
+            break;
+
         case kEventMouseMotion:
             skin_window_process_event(ui->window, &ev);
             break;

commit 2d389578c30dd7a8066ffe9fec57cf8d565071a8

Fix keyboard emulation.
Previous patch 5a4a159898e79ce88d1a2f85843e973036d6e057 broke
keyboard emulation. For example, typing '@' would enter '2@'
in the guest, due to duplicate but slightly incompatible Linux
keycodes being sent to the kernel.

More precisely:

  - Fix a bug in the computation of the 'modifier' bitmask
    from SDL 1.x

  - Do not send keydown events that were not handled as UI keyboard
    shortcuts to the guest. Rely on kEventTextInput events to send
    Unicode-based key events instead.

  - Add docs/ANDROID-KEYBOARD-EMULATION.TXT to explain how all of
    this works at the moment.

Change-Id: I011cc6da328f6a00d43e02c62733f22dbaa88d1c

android/skin/event-sdl.c
@@ -215,7 +215,7 @@ static SkinKeyMod sdl_mod_to_key_mod(int sdl_mod) {
     SkinKeyMod mod = 0;
     size_t nn;
     for (nn = 0; nn < kConvertSize; ++nn) {
-        if (sdl_mod && kConvert[nn].sdl_flag) {
+        if (sdl_mod & kConvert[nn].sdl_flag) {
             mod |= kConvert[nn].mod_flag;
         }
     }

android/skin/keyboard.c
@@ -236,12 +236,7 @@ skin_keyboard_process_event(SkinKeyboard*  kb, SkinEvent* ev, int  down)
             return;
         }
 
-        if (code == -1) {
-            D("ignoring keycode %d", keycode);
-        } else if (code > 0) {
-            skin_keyboard_do_key_event(kb, code, down);
-            skin_keyboard_flush(kb);
-        }
+        D("ignoring keycode %d", keycode);
     }
 }
 