7384c9dc7101a3c0f15d49a04e6c26d7a4c15883
David 'Digit' Turner <digit@google.com>
2014-12-15 18:21:48 +0100

    android/skin/rect.h: Add unit test + fix skin_rect_equals
    Add unit tests for all functions declared in android/skin/rect.h
    and fix the implementation of skin_rect_equals() which failed
    to properly check the height of both rectangles!
    
    Change-Id: If224639fbe0810ead08d3cc90fcf0ab16c3ff937
    (cherry picked from commit 8ba5fa751199da507cf214c93245b6bedcca6a4d)

--- android/skin/rect.c
+++ android/skin/rect.c
@@ -14,9 +14,7 @@
 
 #define  SKIN_POS_INITIALIZER   { 0, 0 }
 
-void
-skin_pos_rotate( SkinPos*  dst, SkinPos*  src, SkinRotation  rotation )
-{
+void skin_pos_rotate(SkinPos* dst, const SkinPos* src, SkinRotation rotation) {
     int  x = src->x;
     int  y = src->y;
 
@@ -45,16 +43,12 @@ skin_pos_rotate( SkinPos*  dst, SkinPos*  src, SkinRotation  rotation )
 
 #define  SKIN_SIZE_INITIALIZER  { 0, 0 }
 
-int
-skin_size_contains( SkinSize*  size, int  x, int  y )
-{
+bool skin_size_contains(const SkinSize* size, int x, int y) {
     return ( (unsigned) x < (unsigned) size->w &&
              (unsigned) y < (unsigned) size->h );
 }
 
-void
-skin_size_rotate( SkinSize*  dst, SkinSize*  src, SkinRotation  rot )
-{
+void skin_size_rotate(SkinSize* dst, const SkinSize* src, SkinRotation rot) {
     int  w = src->w;
     int  h = src->h;
 
@@ -71,12 +65,10 @@ skin_size_rotate( SkinSize*  dst, SkinSize*  src, SkinRotation  rot )
  **/
 #define  SKIN_RECT_INITIALIZER  { SKIN_POS_INITIALIZER, SKIN_SIZE_INITIALIZER }
 
-void
-skin_rect_init( SkinRect*  r, int x, int  y, int  w, int  h )
-{
-    if (w < 0 || h < 0)
+void skin_rect_init(SkinRect* r, int x, int y, int w, int h) {
+    if (w < 0 || h < 0) {
         x = y = w = h = 0;
-
+    }
     r->pos.x  = x;
     r->pos.y  = y;
     r->size.w = w;
@@ -84,19 +76,14 @@ skin_rect_init( SkinRect*  r, int x, int  y, int  w, int  h )
 }
 
 
-void
-skin_rect_translate( SkinRect*  r, int  dx, int  dy )
-{
+void skin_rect_translate(SkinRect* r, int dx, int dy) {
     r->pos.x += dx;
     r->pos.y += dy;
 }
 
 
-void
-skin_rect_rotate( SkinRect*  dst, SkinRect*  src, SkinRotation  rot )
-{
+void skin_rect_rotate(SkinRect* dst, const SkinRect* src, SkinRotation rot) {
     int  x, y, w, h;
-
     switch (rot & 3) {
         case SKIN_ROTATION_90:
             x = src->pos.x;
@@ -132,17 +119,13 @@ skin_rect_rotate( SkinRect*  dst, SkinRect*  src, SkinRotation  rot )
 }
 
 
-int
-skin_rect_contains( SkinRect*  r, int  x, int  y )
-{
+bool skin_rect_contains(const SkinRect* r, int x, int y) {
     return ( (unsigned)(x - r->pos.x) < (unsigned)r->size.w &&
              (unsigned)(y - r->pos.y) < (unsigned)r->size.h );
 }
 
-SkinOverlap
-skin_rect_contains_rect( SkinRect  *r1, SkinRect  *r2 )
-{
-    SkinBox  a, b;
+SkinOverlap skin_rect_contains_rect(const SkinRect* r1, const SkinRect* r2) {
+    SkinBox a, b;
 
     skin_box_from_rect( &a, r1 );
     skin_box_from_rect( &b, r2 );
@@ -159,9 +142,9 @@ skin_rect_contains_rect( SkinRect  *r1, SkinRect  *r2 )
 }
 
 
-int
-skin_rect_intersect( SkinRect*  result, SkinRect*  r1, SkinRect*  r2 )
-{
+bool skin_rect_intersect(SkinRect* result,
+                         const SkinRect* r1,
+                         const SkinRect* r2) {
     SkinBox  a, b, r;
 
     skin_box_from_rect( &a, r1 );
@@ -181,25 +164,26 @@ skin_rect_intersect( SkinRect*  result, SkinRect*  r1, SkinRect*  r2 )
     return 1;
 }
 
-int
-skin_rect_equals( SkinRect*  r1, SkinRect*  r2 )
-{
+bool skin_rect_equals(const SkinRect* r1, const SkinRect* r2) {
     return (r1->pos.x  == r2->pos.x  && r1->pos.y  == r2->pos.y &&
-            r1->size.w == r2->size.w && r2->size.h == r2->size.h);
+            r1->size.w == r2->size.w && r1->size.h == r2->size.h);
 }
 
 /** SKIN BOXES
  **/
-void
-skin_box_minmax_init( SkinBox*  box )
-{
+void skin_box_init(SkinBox* box, int x1, int y1, int x2, int y2) {
+    box->x1 = x1;
+    box->y1 = y1;
+    box->x2 = x2;
+    box->y2 = y2;
+}
+
+void skin_box_minmax_init(SkinBox* box) {
     box->x1 = box->y1 = INT_MAX;
     box->x2 = box->y2 = INT_MIN;
 }
 
-void
-skin_box_minmax_update( SkinBox*  a, SkinRect*  r )
-{
+void skin_box_minmax_update(SkinBox* a, const SkinRect* r) {
     SkinBox  b[1];
 
     skin_box_from_rect(b, r);
@@ -210,29 +194,23 @@ skin_box_minmax_update( SkinBox*  a, SkinRect*  r )
     if (b->y2 > a->y2) a->y2 = b->y2;
 }
 
-int
-skin_box_minmax_to_rect( SkinBox*  box, SkinRect*  r )
-{
+int skin_box_minmax_to_rect(const SkinBox* box, SkinRect* r) {
     if (box->x1 > box->x2) {
         r->pos.x = r->pos.y = r->size.w = r->size.h = 0;
         return 0;
     }
-    skin_box_to_rect( box, r );
+    skin_box_to_rect(box, r);
     return 1;
 }
 
-void
-skin_box_from_rect( SkinBox*  box, SkinRect*  r )
-{
+void skin_box_from_rect(SkinBox*  box, const SkinRect* r) {
     box->x1 = r->pos.x;
     box->y1 = r->pos.y;
     box->x2 = r->size.w + box->x1;
     box->y2 = r->size.h + box->y1;
 }
 
-void
-skin_box_to_rect( SkinBox*  box, SkinRect*  r )
-{
+void skin_box_to_rect(const SkinBox* box, SkinRect* r) {
     r->pos.x  = box->x1;
     r->pos.y  = box->y1;
     r->size.w = box->x2 - box->x1;
