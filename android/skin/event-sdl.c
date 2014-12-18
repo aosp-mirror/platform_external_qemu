9c3cf29f04473454968f36ba31733febfadc56f8
David 'Digit' Turner <digit@google.com>
2014-12-18 12:56:17 +0100

    android/skin/keyboard: Fix SDL2 keyboard handling.
    This modifies the way kEventTextInput events are handled by
    the SkinKeyboar class:
    
    - Before:
    
        * Each kEventTextInput event generated a corresponding
          a single up/down sequence of keycodes.
    
        * SDL 1.x generated two kEventTextInput events per
          keypress, one for down, then one for up.
    
        * SDL 2.x only generates a single kEventTextInput
          event (down), by design.
    
    - After:
    
        * Each kEventTextInput event generates two sequences
          of keycodes, one for down, followed by one for up.
    
        * The SDL 1.x backend is modified to only generated
          a single kEventTextInput on SDL_KEYDOWN events only.
    
        * The SDL 2.x backend is unmodified.
    
    Change-Id: I7a4deca549720eb5898e7c576917913198cd817d
    (cherry picked from commit 14829cf91b11d24b291cc94c031023f417630020)

--- android/skin/event-sdl.c
+++ android/skin/event-sdl.c
@@ -327,12 +327,12 @@ bool skin_event_poll(SkinEvent* event) {
                     last_keys_add(last_keys, sdl_sym, sdl_mod, unicode);
                 }
 
-                if (unicode) {
+                if (unicode && down) {
                     // The unicode value is added to a pending event
                     // unless it is invalid.
                     if (prepare_text_input_event(
-                            &pending_event, 
-                            unicode, 
+                            &pending_event,
+                            unicode,
                             down)) {
                         pending = true;
                     }
