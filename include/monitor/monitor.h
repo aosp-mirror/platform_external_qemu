philmd/qemu-staging

monitor.c: Add support for Android console
Implement the Android console by hijacking the stock QEMU
monitor and implementing a different command set.

This commit provides the initial minimal framework and
changes to the core monitor code; it implements only the
two commands 'help' and 'kill'.

Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

include/monitor/monitor.h
@@ -15,6 +15,7 @@ extern Monitor *default_mon;
 #define MONITOR_USE_READLINE  0x02
 #define MONITOR_USE_CONTROL   0x04
 #define MONITOR_USE_PRETTY    0x08
+#define MONITOR_ANDROID_CONSOLE 0x10
 
 /* flags for monitor commands */
 #define MONITOR_CMD_ASYNC       0x0001

aosp-mirror/platform_external_qemu

include/monitor/monitor.h
@@ -48,6 +48,7 @@ typedef struct mon_cmd_t {
 #define MONITOR_USE_CONTROL   0x04
 #define MONITOR_USE_PRETTY    0x08
 #define MONITOR_DYNAMIC_CMDS  0x10
+#define MONITOR_ANDROID_CONSOLE 0x20
 
 /* flags for monitor commands */
 #define MONITOR_CMD_ASYNC       0x0001