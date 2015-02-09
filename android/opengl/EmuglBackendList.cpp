3bf0685413df5eae56f8a671aac74d19c373e91f
David 'Digit' Turner <digit@google.com>
2015-02-09 10:51:25 +0100

    android::opengl::EmuglBackendList: new helper class.
    Change-Id: Ib042afbc50ca8947d9bf2f98f2648de1c5adfe69

---
+++ android/opengl/EmuglBackendList.cpp
@@ -0,0 +1,101 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/opengl/EmuglBackendList.h"
+
+#include "android/base/Log.h"
+#include "android/base/StringFormat.h"
+#include "android/base/system/System.h"
+#include "android/opengl/EmuglBackendScanner.h"
+
+#define DEBUG 0
+
+#if DEBUG
+#  include <stdio.h>
+#  define D(...)  printf(__VA_ARGS__)
+#else
+#  define D(...)  ((void)0)
+#endif
+
+namespace android {
+namespace opengl {
+
+using android::base::String;
+using android::base::StringVector;
+using android::base::System;
+
+EmuglBackendList::EmuglBackendList(const char* execDir,
+                                   int hostBitness) :
+        mDefaultName("host"), mNames(), mHostBitness(0), mExecDir(execDir) {
+    // Fix host bitness if needed.
+    if (!hostBitness) {
+        hostBitness = System::kProgramBitness;
+    }
+    mHostBitness = hostBitness;
+
+    mNames = EmuglBackendScanner::scanDir(execDir, hostBitness);
+}
+
+String EmuglBackendList::getLibDirPath(const char* name) {
+    return android::base::StringFormat(
+            "%s/%s/gles_%s",
+            mExecDir.c_str(),
+            mHostBitness == 64 ? "lib64" : "lib",
+            name);
+}
+
+bool EmuglBackendList::getBackendLibPath(const char* name,
+                                         Library library,
+                                         String* libPath) {
+#ifdef _WIN32
+    static const char kLibPrefix[] = "";
+#else
+    static const char kLibPrefix[] = "lib";
+#endif
+#ifdef _WIN32
+    static const char kLibSuffix[] = ".dll";
+#elif defined(__APPLE__)
+    static const char kLibSuffix[] = ".dylib";
+#else
+    static const char kLibSuffix[] = ".so";
+#endif
+
+    const char* libraryName = NULL;
+    if (library == LIBRARY_EGL) {
+        libraryName = "EGL";
+    } else if (library == LIBRARY_GLESv1) {
+        libraryName = "GLES_CM";
+    } else if (library == LIBRARY_GLESv2) {
+        libraryName = "GLESv2";
+    } else {
+        // Should not happen.
+        D("%s: Invalid library type: %d\n", __FUNCTION__, library);
+        return false;
+    }
+
+    String path = android::base::StringFormat(
+            "%s/%s%s%s",
+            getLibDirPath(name).c_str(),
+            kLibPrefix,
+            libraryName,
+            kLibSuffix);
+
+    if (!System::get()->pathIsFile(path.c_str())) {
+        D("%s(name=%s lib=%s): File does not exist: %s\n",
+          __FUNCTION__, name, libraryName, path.c_str());
+        return false;
+    }
+    *libPath = path;
+    return true;
+}
+
+}  // namespace opengl
+}  // namespace android
---
+++ android/opengl/EmuglBackendList.h
@@ -0,0 +1,67 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_OPENGL_EMUGL_BACKEND_LIST_H
+#define ANDROID_OPENGL_EMUGL_BACKEND_LIST_H
+
+#include "android/base/containers/StringVector.h"
+#include "android/base/String.h"
+
+namespace android {
+namespace opengl {
+
+class EmuglBackendList {
+public:
+    typedef android::base::String String;
+    typedef android::base::StringVector StringVector;
+
+    // Parse the content of |execDir|/<lib>/ for Emugl backends.
+    // |hostBitness| can be 0 (autodetect), 32 or 64, and determines
+    // the value of <lib> which will be 'lib' for 32-bit systems,
+    // and 'lib64' for 64-bit ones.
+    EmuglBackendList(const char* execDir, int hostBitness);
+
+    // Return the name of the default Emugl backend.
+    const String& defaultName() const { return mDefaultName; }
+
+    // Return the list of installed Emugl backends.
+    const StringVector& names() const { return mNames; }
+
+    // Convert the name of an Emugl backend into the path of the
+    // corresponding sub-directory, if it exits, or NULL otherwise.
+    String getLibDirPath(const char* name);
+
+    // List of supported Emugl shared libraries.
+    enum Library {
+        LIBRARY_NONE,
+        LIBRARY_EGL,
+        LIBRARY_GLESv1,
+        LIBRARY_GLESv2
+    };
+
+    // Probe the library directory for Emugl backend |name| and return
+    // the path of one of the EmuGL shared libraries for it. The result
+    // will be empty if there is no such library.
+    // |library| is a library type. On success, return true and sets
+    // |*libPath| to the library's path value. On failure, return false.
+    bool getBackendLibPath(const char* name, Library library, String* libPath);
+
+private:
+    String mDefaultName;
+    StringVector mNames;
+    int mHostBitness;
+    String mExecDir;
+};
+
+}  // namespace opengl
+}  // namespace android
+
+#endif  // ANDROID_OPENGL_EMUGL_BACKEND_LIST_H
