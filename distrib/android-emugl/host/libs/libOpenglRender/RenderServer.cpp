c7e7d8a53fe53a14492cbb7ce55789d164a0517e
bohu <bohu@google.com>
2015-02-11 08:25:39 -0800

    Disable waiting for RenderThread termination on windows
    For some unknown reason, emulator freezes on windows when it is
    shut down. Temporarily disable waiting for render threads on
    windows during shutting down and will fix in next release.
    
    related CL: 303a3771cee4311bb080515282603c8dfc5ecbde
    
    Change-Id: I6dcdae0b083965a37dcec92c66cc46240a5d06de

--- distrib/android-emugl/host/libs/libOpenglRender/RenderServer.cpp
+++ distrib/android-emugl/host/libs/libOpenglRender/RenderServer.cpp
@@ -149,8 +149,12 @@ intptr_t RenderServer::main()
     for (RenderThreadsSet::iterator t = threads.begin();
          t != threads.end();
          t++) {
+#ifndef _WIN32
+        // Note: temporarily disable following steps so emulator does not
+        // freeze on windows when it is shut down, will fix in 1.2
         (*t)->forceStop();
         (*t)->wait(NULL);
+#endif
         delete (*t);
     }
     threads.clear();
