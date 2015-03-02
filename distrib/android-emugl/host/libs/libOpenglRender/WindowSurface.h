0f328c50cfa169e11b534753e4b2a387892b0025
David 'Digit' Turner <digit@google.com>
2015-03-02 17:34:18 +0100

    emugl: Refactor WindowSurface class.
    This refactors the implementation of the WindowSurface class as follows:
    
    - Remove coupling from FrameBuffer and FbConfig classes.
    - Add documentation comments.
    - Remove un-used members, removing Clang compiler warnings.
    
    Change-Id: Ic620accfb3e94fd7b0a561dc3e3037f4f44c1f8e

--- distrib/android-emugl/host/libs/libOpenglRender/WindowSurface.h
+++ distrib/android-emugl/host/libs/libOpenglRender/WindowSurface.h
@@ -13,59 +13,93 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#ifndef _LIBRENDER_WINDOWSURFACE_H
-#define _LIBRENDER_WINDOWSURFACE_H
+#ifndef _LIBRENDER_WINDOW_SURFACE_H
+#define _LIBRENDER_WINDOW_SURFACE_H
 
 #include "ColorBuffer.h"
 #include "RenderContext.h"
-#include "FbConfig.h"
+
 #include "emugl/common/smart_ptr.h"
-#include "FixedBuffer.h"
+
 #include <EGL/egl.h>
 #include <GLES/gl.h>
 
-enum SurfaceBindType {
-    SURFACE_BIND_READ,
-    SURFACE_BIND_DRAW,
-    SURFACE_BIND_READDRAW
-};
-
-class WindowSurface
-{
+// A class used to model a guest-side window surface. The implementation
+// uses a host Pbuffer to act as the EGL rendering surface instead.
+class WindowSurface {
 public:
-    static WindowSurface *create(int p_config, int p_width, int p_height);
-    ~WindowSurface();
-    EGLSurface getEGLSurface() const { return m_eglSurface; }
+    // Create a new WindowSurface instance.
+    // |display| is the host EGLDisplay value.
+    // |config| is the host EGLConfig value.
+    // |width| and |height| are the initial size of the Pbuffer.
+    // Return a new WindowSurface instance on success, or NULL on failure.
+    static WindowSurface* create(EGLDisplay display,
+                                 EGLConfig config,
+                                 int width,
+                                 int height);
 
+    // Destructor.
+    ~WindowSurface();
+
+    // Retrieve the host EGLSurface of the WindowSurface's Pbuffer.
+    EGLSurface getEGLSurface() const { return mSurface; }
+
+    // Attach a ColorBuffer to this WindowSurface.
+    // Once attached, calling flushColorBuffer() will copy the Pbuffer's
+    // pixels to the color buffer.
+    //
+    // IMPORTANT: This automatically resizes the Pbuffer's to the ColorBuffer's
+    // dimensions. Potentially losing pixel values in the process.
     void setColorBuffer(ColorBufferPtr p_colorBuffer);
+
+    // Copy the Pbuffer's pixels to the attached color buffer.
+    // Returns true on success, or false on error (e.g. if there is no
+    // attached color buffer).
     bool flushColorBuffer();
-    void bind(RenderContextPtr p_ctx, SurfaceBindType p_bindType);
+
+    // Used by bind() below.
+    enum BindType {
+        BIND_READ,
+        BIND_DRAW,
+        BIND_READDRAW
+    };
+
+    // TODO(digit): What is this used for exactly? For example, the
+    // mReadContext is never used by this class. The mDrawContext is only
+    // used temporarily during flushColorBuffer() operation, and could be
+    // passed as a parameter to the function instead. Maybe this is only used
+    // to increment reference counts on the smart pointers.
+    //
+    // Bind a context to the WindowSurface (huh? Normally you would bind a
+    // surface to the context, not the other way around)
+    //
+    // |p_ctx| is a RenderContext pointer.
+    // |p_bindType| is the type of bind. For BIND_READ, this assigns |p_ctx|
+    // to mReadContext, for BIND_DRAW, it assigns it to mDrawContext, and for
+    // for BIND_READDRAW, it assigns it to both.
+    void bind(RenderContextPtr p_ctx, BindType p_bindType);
 
 private:
     WindowSurface();
+    WindowSurface(const WindowSurface& other);
+
+    explicit WindowSurface(EGLDisplay display, EGLConfig config);
 
     bool blitToColorBuffer();  // copy pbuffer content with texload and blit
-    bool resizePbuffer(unsigned int p_width, unsigned int p_height);
+
+    bool resize(unsigned int p_width, unsigned int p_height);
 
 private:
-    GLuint m_fbObj;   // GLES Framebuffer object (when EGLimage is used)
-    GLuint m_depthRB;
-    GLuint m_stencilRB;
-    EGLSurface m_eglSurface;
-    ColorBufferPtr m_attachedColorBuffer;
-    RenderContextPtr m_readContext;
-    RenderContextPtr m_drawContext;
-    GLuint m_width;
-    GLuint m_height;
-    GLuint m_pbufWidth;
-    GLuint m_pbufHeight;
-    bool m_useEGLImage;
-    bool m_useBindToTexture;
-    FixedBuffer m_xferBuffer;
-    FixedBuffer m_xUpdateBuf;
-    const FbConfig *m_fbconf;
+    EGLSurface mSurface;
+    ColorBufferPtr mAttachedColorBuffer;
+    RenderContextPtr mReadContext;
+    RenderContextPtr mDrawContext;
+    GLuint mWidth;
+    GLuint mHeight;
+    EGLConfig mConfig;
+    EGLDisplay mDisplay;
 };
 
 typedef emugl::SmartPtr<WindowSurface> WindowSurfacePtr;
 
-#endif
+#endif  // _LIBRENDER_WINDOW_SURFACE_H
