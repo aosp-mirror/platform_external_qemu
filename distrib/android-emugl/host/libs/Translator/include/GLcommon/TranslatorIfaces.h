7119986b674e1f26bb0b053f28c825d651fddf90
Konstantinos Menychtas <kmenychtas@google.com>
2015-03-06 17:21:25 -0800

    Initialization routines to support alternative GLES implementations.
    Added interface extensions to GLES_CM and GLES_V2 (in particular
    one function, GLES_Vx) to make it possible to load alternative
    implementations that require special initialization. This method
    is being used in the ongoing effort to use libes1x
    (GLESv1 --to--> GLESv2 translation).
    
    Change-Id: Ib41156d8f8ec7425b79829b74184bca884070b99

--- distrib/android-emugl/host/libs/Translator/include/GLcommon/TranslatorIfaces.h
+++ distrib/android-emugl/host/libs/Translator/include/GLcommon/TranslatorIfaces.h
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2011 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef TRANSLATOR_IFACES_H
 #define TRANSLATOR_IFACES_H
 #include <GLES/gl.h>
@@ -33,45 +33,45 @@ typedef struct {
 
 class TextureData : public ObjectData
 {
-public:
-    ~TextureData() {
-        if (sourceEGLImage && eglImageDetach) (*eglImageDetach)(sourceEGLImage);
-    }
-    TextureData():  ObjectData(TEXTURE_DATA),
-                    width(0),
-                    height(0),
-                    border(0),
-                    internalFormat(GL_RGBA),
-                    sourceEGLImage(0),
-                    wasBound(false),
-                    requiresAutoMipmap(false),
-                    target(0),
-                    oldGlobal(0) {
-        memset(crop_rect,0,4*sizeof(int));
-    };
+ public:
+  ~TextureData() {
+    if (sourceEGLImage && eglImageDetach) (*eglImageDetach)(sourceEGLImage);
+  }
+  TextureData():  ObjectData(TEXTURE_DATA),
+                  width(0),
+                  height(0),
+                  border(0),
+                  internalFormat(GL_RGBA),
+                  sourceEGLImage(0),
+                  wasBound(false),
+                  requiresAutoMipmap(false),
+                  target(0),
+                  oldGlobal(0) {
+    memset(crop_rect,0,4*sizeof(int));
+  };
 
-    unsigned int width;
-    unsigned int height;
-    unsigned int border;
-    unsigned int internalFormat;
-    unsigned int sourceEGLImage;
-    bool wasBound;
-    bool requiresAutoMipmap;
-    int          crop_rect[4];
-    void (*eglImageDetach)(unsigned int imageId);
-    GLenum target;
-    GLuint oldGlobal;
+  unsigned int width;
+  unsigned int height;
+  unsigned int border;
+  unsigned int internalFormat;
+  unsigned int sourceEGLImage;
+  bool wasBound;
+  bool requiresAutoMipmap;
+  int          crop_rect[4];
+  void (*eglImageDetach)(unsigned int imageId);
+  GLenum target;
+  GLuint oldGlobal;
 };
 
 struct EglImage
 {
-    ~EglImage(){};
-    unsigned int imageId;
-    unsigned int globalTexName;
-    unsigned int width;
-    unsigned int height;
-    unsigned int internalFormat;
-    unsigned int border;
+  ~EglImage(){};
+  unsigned int imageId;
+  unsigned int globalTexName;
+  unsigned int width;
+  unsigned int height;
+  unsigned int internalFormat;
+  unsigned int border;
 };
 
 typedef emugl::SmartPtr<EglImage> ImagePtr;
@@ -80,20 +80,21 @@ typedef std::map< unsigned int, ImagePtr>       ImagesHndlMap;
 class GLEScontext;
 
 typedef struct {
-    GLEScontext*                                    (*createGLESContext)();
-    void                                            (*initContext)(GLEScontext*,ShareGroupPtr);
-    void                                            (*deleteGLESContext)(GLEScontext*);
-    void                                            (*flush)();
-    void                                            (*finish)();
-    void                                            (*setShareGroup)(GLEScontext*,ShareGroupPtr);
-    __translatorMustCastToProperFunctionPointerType (*getProcAddress)(const char*);
+  void                                            (*initGLESx)();
+  GLEScontext*                                    (*createGLESContext)();
+  void                                            (*initContext)(GLEScontext*,ShareGroupPtr);
+  void                                            (*deleteGLESContext)(GLEScontext*);
+  void                                            (*flush)();
+  void                                            (*finish)();
+  void                                            (*setShareGroup)(GLEScontext*,ShareGroupPtr);
+  __translatorMustCastToProperFunctionPointerType (*getProcAddress)(const char*);
 }GLESiface;
 
 
 typedef struct {
-    GLEScontext* (*getGLESContext)();
-    EglImage*    (*eglAttachEGLImage)(unsigned int imageId);
-    void         (*eglDetachEGLImage)(unsigned int imageId);
+  GLEScontext* (*getGLESContext)();
+  EglImage*    (*eglAttachEGLImage)(unsigned int imageId);
+  void         (*eglDetachEGLImage)(unsigned int imageId);
 }EGLiface;
 
 typedef GLESiface* (*__translator_getGLESIfaceFunc)(const EGLiface*);
