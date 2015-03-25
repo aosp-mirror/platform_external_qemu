8ada71a39c1d880f38c4ce8e3773d1ca0c4c5537
David 'Digit' Turner <digit@google.com>
2015-03-25 11:56:12 +0100

    emugl: Document FrameBuffer class.
    Change-Id: I8016f2d74015167f979712f93ec103e71716b7ab

--- distrib/android-emugl/host/libs/libOpenglRender/FrameBuffer.h
+++ distrib/android-emugl/host/libs/libOpenglRender/FrameBuffer.h
@@ -30,6 +30,8 @@
 
 #include <stdint.h>
 
+// Type of handles, a.k.a. "object names" in the GL specification.
+// These are integers used to uniquely identify a resource of a given type.
 typedef uint32_t HandleType;
 
 struct ColorBufferRef {
@@ -40,91 +42,239 @@ typedef std::map<HandleType, RenderContextPtr> RenderContextMap;
 typedef std::map<HandleType, WindowSurfacePtr> WindowSurfaceMap;
 typedef std::map<HandleType, ColorBufferRef> ColorBufferMap;
 
-struct FrameBufferCaps
-{
+// A structure used to list the capabilities of the underlying EGL
+// implementation that the FrameBuffer instance depends on.
+// |has_eglimage_texture_2d| is true iff the EGL_KHR_gl_texture_2D_image
+// extension is supported.
+// |has_eglimage_renderbuffer| is true iff the EGL_KHR_gl_renderbuffer_image
+// extension is supported.
+// |eglMajor| and |eglMinor| are the major and minor version numbers of
+// the underlying EGL implementation.
+struct FrameBufferCaps {
     bool has_eglimage_texture_2d;
     bool has_eglimage_renderbuffer;
     EGLint eglMajor;
     EGLint eglMinor;
 };
 
-class FrameBuffer
-{
+// The FrameBuffer class holds the global state of the emulation library on
+// top of the underlying EGL/GLES implementation. It should probably be
+// named "Display" instead of "FrameBuffer".
+//
+// There is only one global instance, that can be retrieved with getFB(),
+// and which must be previously setup by calling initialize().
+//
+class FrameBuffer {
 public:
+    // Initialize the global instance.
+    // |width| and |height| are the dimensions of the emulator GPU display
+    // in pixels. Returns true on success, false otherwise.
     static bool initialize(int width, int height);
 
+    // Setup a new sub-window to display the content of the emulated GPU
+    // on-top of an existing UI window. |p_window| is the platform-specific
+    // parent window handle. |x|, |y|, |width| and |height| are the dimensions
+    // in pixels of the sub-window, relative to the parent window's coordinate.
+    // Note that |width| and |height| can be different from the dimensions
+    // used to initialize the framebuffer (in which case scaling will be
+    // applied automatically). |zRot| is a rotation angle in degrees,
+    // (clockwise in the Y-upwards GL coordinate space).
+    // Return true on success, false otherwise.
+    //
+    // NOTE: This can return NULL for software-only EGL engines like OSMesa.
     static bool setupSubWindow(FBNativeWindowType p_window,
                                 int x, int y,
                                 int width, int height, float zRot);
 
+    // Remove the sub-window created by setupSubWindow(), if any.
+    // Return true on success, false otherwise.
     static bool removeSubWindow();
 
+    // Finalize the global instance.
     static void finalize();
 
+    // Return a pointer to the global instance. initialize() must be called
+    // previously, or this will return NULL.
     static FrameBuffer *getFB() { return s_theFrameBuffer; }
 
+    // Return the capabilities of the underlying display.
     const FrameBufferCaps &getCaps() const { return m_caps; }
 
+    // Return the emulated GPU display width in pixels.
     int getWidth() const { return m_width; }
 
+    // Return the emulated GPU display height in pixels.
     int getHeight() const { return m_height; }
 
+    // Return the list of configs available from this display.
     const FbConfigList* getConfigs() const { return m_configs; }
 
+    // Set a callback that will be called each time the emulated GPU content
+    // is updated. This can be relatively slow with host-based GPU emulation,
+    // so only do this when you need to.
     void setPostCallback(OnPostFn onPost, void* onPostContext);
 
-    void getGLStrings(const char** vendor, const char** renderer, const char** version) const {
+    // Retrieve the GL strings of the underlying EGL/GLES implementation.
+    // On return, |*vendor|, |*renderer| and |*version| will point to strings
+    // that are owned by the instance (and must not be freed by the caller).
+    void getGLStrings(const char** vendor,
+                      const char** renderer,
+                      const char** version) const {
         *vendor = m_glVendor;
         *renderer = m_glRenderer;
         *version = m_glVersion;
     }
 
-    HandleType createRenderContext(int p_config, HandleType p_share, bool p_isGL2 = false);
-    HandleType createWindowSurface(int p_config, int p_width, int p_height);
-    HandleType createColorBuffer(int p_width, int p_height, GLenum p_internalFormat);
+    // Create a new RenderContext instance for this display instance.
+    // |p_config| is the index of one of the configs returned by getConfigs().
+    // |p_share| is either EGL_NO_CONTEXT or the handle of a shared context.
+    // |p_isGL2| is true to create a GLES 2.x context, or false for a GLES 1.x
+    // one.
+    // Return a new handle value, which will be 0 in case of error.
+    HandleType createRenderContext(
+            int p_config, HandleType p_share, bool p_isGL2 = false);
 
-    // Destroy all the remaining contexts that are created by current render thread
+    // Create a new WindowSurface instance from this display instance.
+    // |p_config| is the index of one of the configs returned by getConfigs().
+    // |p_width| and |p_height| are the window dimensions in pixels.
+    // Return a new handle value, or 0 in case of error.
+    HandleType createWindowSurface(int p_config, int p_width, int p_height);
+
+    // Create a new ColorBuffer instance from this display instance.
+    // |p_width| and |p_height| are its dimensions in pixels.
+    // |p_internalFormat| is the pixel format. See ColorBuffer::create() for
+    // list of valid values. Note that ColorBuffer instances are reference-
+    // counted. Use openColorBuffer / closeColorBuffer to operate on the
+    // internal count.
+    HandleType createColorBuffer(
+        int p_width, int p_height, GLenum p_internalFormat);
+
+    // Call this function when a render thread terminates to destroy all
+    // the remaining contexts it created. Necessary to avoid leaking host
+    // contexts when a guest application crashes, for example.
     void drainRenderContext();
 
-    // Destroy all the remaining window surfaces that are created by current render thread
+    // Call this function when a render thread terminates to destroy all
+    // remaining window surfqce it created. Necessary to avoid leaking
+    // host buffers when a guest application crashes, for example.
     void drainWindowSurface();
 
+    // Destroy a given RenderContext instance. |p_context| is its handle
+    // value as returned by createRenderContext().
     void DestroyRenderContext(HandleType p_context);
+
+    // Destroy a given WindowSurface instance. |p_surcace| is its handle
+    // value as returned by createWindowSurface().
     void DestroyWindowSurface(HandleType p_surface);
+
+    // Increment the reference count associated with a given ColorBuffer
+    // instance. |p_colorbuffer| is its handle value as returned by
+    // createColorBuffer().
     int openColorBuffer(HandleType p_colorbuffer);
+
+    // Decrement the reference count associated with a given ColorBuffer
+    // instance. |p_colorbuffer| is its handle value as returned by
+    // createColorBuffer(). Note that if the reference count reaches 0,
+    // the instance is destroyed automatically.
     void closeColorBuffer(HandleType p_colorbuffer);
 
-    bool  bindContext(HandleType p_context, HandleType p_drawSurface, HandleType p_readSurface);
-    bool  setWindowSurfaceColorBuffer(HandleType p_surface, HandleType p_colorbuffer);
+    // Equivalent for eglMakeCurrent() for the current display.
+    // |p_context|, |p_drawSurface| and |p_readSurface| are the handle values
+    // of the context, the draw surface and the read surface, respectively.
+    // Returns true on success, false on failure.
+    // Note: if all handle values are 0, this is an unbind operation.
+    bool  bindContext(HandleType p_context,
+                      HandleType p_drawSurface,
+                      HandleType p_readSurface);
+
+    // Attach a ColorBuffer to a WindowSurface instance.
+    // See the documentation for WindowSurface::setColorBuffer().
+    // |p_surface| is the target WindowSurface's handle value.
+    // |p_colorbuffer| is the ColorBuffer handle value.
+    // Returns true on success, false otherwise.
+    bool  setWindowSurfaceColorBuffer(
+            HandleType p_surface, HandleType p_colorbuffer);
+
+    // Copy the content of a WindowSurface's Pbuffer to its attached
+    // ColorBuffer. See the documentation for WindowSurface::flushColorBuffer()
+    // |p_surface| is the target WindowSurface's handle value.
+    // Returns true on success, false on failure.
     bool  flushWindowSurfaceColorBuffer(HandleType p_surface);
+
+    // Bind the current context's EGL_TEXTURE_2D texture to a ColorBuffer
+    // instance's EGLImage. This is intended to implement
+    // glEGLImageTargetTexture2DOES() for all GLES versions.
+    // |p_colorbuffer| is the ColorBuffer's handle value.
+    // Returns true on success, false on failure.
     bool  bindColorBufferToTexture(HandleType p_colorbuffer);
+
+    // Bind the current context's EGL_RENDERBUFFER_OES render buffer to this
+    // ColorBuffer's EGLImage. This is intended to implement
+    // glEGLImageTargetRenderbufferStorageOES() for all GLES versions.
+    // |p_colorbuffer| is the ColorBuffer's handle value.
+    // Returns true on success, false on failure.
     bool  bindColorBufferToRenderbuffer(HandleType p_colorbuffer);
+
+    // Read the content of a given ColorBuffer into client memory.
+    // |p_colorbuffer| is the ColorBuffer's handle value. Similar
+    // to glReadPixels(), this can be a slow operation.
+    // |x|, |y|, |width| and |height| are the position and dimensions of
+    // a rectangle whose pixel values will be transfered to the host.
+    // |format| indicates the format of the pixel data, e.g. GL_RGB or GL_RGBA.
+    // |type| is the type of pixel data, e.g. GL_UNSIGNED_BYTE.
+    // |pixels| is the address of a caller-provided buffer that will be filled
+    // with the pixel data.
     void  readColorBuffer(HandleType p_colorbuffer,
                            int x, int y, int width, int height,
                            GLenum format, GLenum type, void *pixels);
+
+    // Update the content of a given ColorBuffer from client data.
+    // |p_colorbuffer| is the ColorBuffer's handle value. Similar
+    // to glReadPixels(), this can be a slow operation.
+    // |x|, |y|, |width| and |height| are the position and dimensions of
+    // a rectangle whose pixel values will be transfered to the GPU
+    // |format| indicates the format of the pixel data, e.g. GL_RGB or GL_RGBA.
+    // |type| is the type of pixel data, e.g. GL_UNSIGNED_BYTE.
+    // |pixels| is the address of a buffer containing the new pixel data.
+    // Returns true on success, false otherwise.
     bool updateColorBuffer(HandleType p_colorbuffer,
                            int x, int y, int width, int height,
                            GLenum format, GLenum type, void *pixels);
 
+    // Display the content of a given ColorBuffer into the framebuffer's
+    // sub-window. |p_colorbuffer| is a handle value.
+    // |needLock| is used to indicate whether the operation requires
+    // acquiring/releasing the FrameBuffer instance's lock. It should be
+    // false only when called internally.
     bool post(HandleType p_colorbuffer, bool needLock = true);
+
+    // Re-post the last ColorBuffer that was displayed through post().
+    // This is useful if you detect that the sub-window content needs to
+    // be re-displayed for any reason.
     bool repost();
 
+    // Return the host EGLDisplay used by this instance.
     EGLDisplay getDisplay() const { return m_eglDisplay; }
-    EGLNativeWindowType getSubWindow() const { return m_subWin; }
-    bool bind_locked();
-    bool unbind_locked();
 
+    // Change the rotation of the displayed GPU sub-window.
     void setDisplayRotation(float zRot) {
         m_zRot = zRot;
         repost();
     }
 
+    // Return a TextureDraw instance that can be used with this surfaces
+    // and windows created by this instance.
     TextureDraw* getTextureDraw() const { return m_textureDraw; }
+
+    // Used internally.
+    bool bind_locked();
+    bool unbind_locked();
 
 private:
     FrameBuffer(int p_width, int p_height);
     ~FrameBuffer();
     HandleType genHandle();
+
     bool bindSubwin_locked();
 
 private:
