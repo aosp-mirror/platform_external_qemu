dc9285c2e160f95b48d435c6ea2b22930095c2cc
Jason Hu <jia-cheng.hu@intel.com>
2015-04-27 20:51:15 +0800

    android: target-i386/cpu.c: Add android32 and android64 CPU models.
    These are used when emulating Android/x86 and Android/x86_64 system
    images *without* CPU acceleration. They enforce the CPU feature set
    expected by the corresponding Android ABI requirements.

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -775,6 +775,38 @@ static X86CPUDefinition builtin_x86_defs[] = {
         .xlevel = 0x8000000A,
         .model_id = "QEMU Virtual CPU version " QEMU_HW_VERSION,
     },
+// ANDROID_BEGIN
+    {
+        /* A variant of qemu64 for Android emulation.
+         * According to Table 1 of NDK Programmer's Guide, section "ABI
+         * Management" (https://developer.android.com/ndk/guides/abis.html),
+         * Android requires the following additional instruction sets for the
+         * x86_64 ABI that are not supported by qemu64:
+         *  SSSE3, SSE4.1, SSE4.2
+         */
+        .name = "android64",
+        .level = 4,
+        .vendor = CPUID_VENDOR_INTEL,
+        .family = 6,
+        .model = 6,
+        .stepping = 3,
+        .features[FEAT_1_EDX] =
+            PPRO_FEATURES |
+            CPUID_MTRR | CPUID_CLFLUSH | CPUID_MCA |
+            CPUID_PSE36,
+        .features[FEAT_1_ECX] =
+            CPUID_EXT_POPCNT | CPUID_EXT_SSE42 | CPUID_EXT_SSE41 |
+            CPUID_EXT_CX16 | CPUID_EXT_SSSE3 | CPUID_EXT_SSE3,
+        .features[FEAT_8000_0001_EDX] =
+            (PPRO_FEATURES & CPUID_EXT2_AMD_ALIASES) |
+            CPUID_EXT2_LM | CPUID_EXT2_SYSCALL | CPUID_EXT2_NX,
+        .features[FEAT_8000_0001_ECX] =
+            CPUID_EXT3_LAHF_LM | CPUID_EXT3_SVM |
+            CPUID_EXT3_ABM | CPUID_EXT3_SSE4A,
+        .xlevel = 0x8000000A,
+        .model_id = "Android virtual processor"
+    },
+// ANDROID_END
     {
         .name = "phenom",
         .level = 5,
@@ -872,6 +904,31 @@ static X86CPUDefinition builtin_x86_defs[] = {
         .xlevel = 0x80000004,
         .model_id = "QEMU Virtual CPU version " QEMU_HW_VERSION,
     },
+// ANDROID_BEGIN
+    {
+        /* A variant of qemu32 for Android emulation.
+         * According to Table 1 of NDK Programmer's Guide, section "ABI
+         * Management" (https://developer.android.com/ndk/guides/abis.html),
+         * Android requires the following additional instruction sets for the
+         * x86 ABI that are not supported by qemu32:
+         *  SSSE3
+         * Another change from qemu32 is the removal of POPCNT, which is not
+         * required by Android and usually comes alongside SSE4.2.
+         */
+        .name = "android32",
+        .level = 4,
+        .vendor = CPUID_VENDOR_INTEL,
+        .family = 6,
+        .model = 6,
+        .stepping = 3,
+        .features[FEAT_1_EDX] =
+            PPRO_FEATURES,
+        .features[FEAT_1_ECX] =
+            CPUID_EXT_SSE3 | CPUID_EXT_SSSE3,
+        .xlevel = 0x80000004,
+        .model_id = "Android 32-bit virtual processor"
+    },
+// ANDROID_END
     {
         .name = "kvm32",
         .level = 5,
