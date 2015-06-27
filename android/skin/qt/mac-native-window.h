cdc79d8ca34925d2b2462a549c951b87ce0abb8d
David 'Digit' Turner <digit@google.com>
2015-06-27 06:44:10 +0200

    Add Qt-based UI backend - DO NOT SUBMIT
    This patch finally adds a new UI backend based on Qt.
    
    Note that for the moment, SDL2 is still the default, to use the
    new backend, rebuild with:
    
      ./android-rebuild --ui=qt
    
    Note that package-release.sh also supports --ui=qt.
    
    A few caveats that will be fixed in a future patch, or a future
    revision of this patch:
    
      - Tool window doesn't display icons properly.
    
      - Under Chrome Remote Desktop, the main window is empty, i.e.
        only the window title bar is displayed.
    
      - window is always forcibly recentered!
    
    Last two issues seem to indicate an issue in winsys-qt.cpp related
    to window management. Investigating.
    
    + Move the build declarations related to the UI to:
    
         android/skin/sources.mk
         android/skin/qt/sources.mk
    
      to make development a little easier.
    
    Change-Id: Ifa48b631680941a991b53a638b76af35fe8a2a0e

---
+++ android/skin/qt/mac-native-window.h
@@ -0,0 +1,20 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+#ifndef __MAC_NATIVE_WINDOW_C_INTF_H__
+#define __MAC_NATIVE_WINDOW_C_INTF_H__
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void* getNSWindow(void* ns_view);
+
+#endif
