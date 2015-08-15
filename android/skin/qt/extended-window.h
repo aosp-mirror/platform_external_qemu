4f90467c42e1d35b63ff93557f148e5c3eba0f5e
Jim Kaye <jameskaye@google.com>
2015-08-15 12:24:14 -0700

    Add "cellular" functionality
    The developer can set the cellular system (GSM, etc),
    whether they're roaming, etc.
    
    Change-Id: I135c35289fc40942a89d2c7b00240f60c3efc320

--- android/skin/qt/extended-window.h
+++ android/skin/qt/extended-window.h
@@ -24,6 +24,7 @@
 #include <QValidator>
 
 #include "android/battery-agent.h"
+#include "android/cellular-agent.h"
 #include "android/telephony-agent.h"
 
 class EmulatorQtWindow;
@@ -80,12 +81,14 @@ private:
     };
 
     void initBattery();
+    void initCellular();
     void initTelephony();
 
     BatteryState    batteryState;
     TelephonyState  telephonyState;
 
     const BatteryAgent    *batteryAgent;
+    const CellularAgent   *cellularAgent;
     const TelephonyAgent  *telephonyAgent;
 
     bool     themeIsDark;
@@ -113,6 +116,12 @@ private slots:
     void on_bat_levelSlider_valueChanged(int value);
     void on_bat_healthBox_currentIndexChanged(int index);
     void on_bat_statusBox_currentIndexChanged(int index);
+
+    // Cellular
+    void on_cell_signalStrengthSlider_valueChanged(int value);
+    void on_cell_standardBox_currentIndexChanged(int index);
+    void on_cell_voiceStatusBox_currentIndexChanged(int index);
+    void on_cell_dataStatusBox_currentIndexChanged(int index);
 
     // Telephony
     void on_tel_startCallButton_clicked();
