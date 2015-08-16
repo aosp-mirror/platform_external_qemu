6b6774766acd6dd519716d43ab3b95591dc74df5
Jim Kaye <jameskaye@google.com>
2015-08-15 18:00:24 -0700

    Add "fingerprint" function
    Change-Id: I699dacac99ce02bfd7c0a2902deea78784810d77

---
+++ android/skin/qt/ext-finger.cpp
@@ -0,0 +1,42 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/finger-agent.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+#include <QtWidgets>
+
+void ExtendedWindow::initFinger()
+{ }
+
+void ExtendedWindow::on_finger_touchCkBox_toggled(bool checked)
+{
+    bool OK = false;
+    int  id = extendedUi->finger_IdBox->toPlainText().toInt(&OK);
+
+    if (checked && !OK) {
+        QErrorMessage *eM = new QErrorMessage;
+        eM->showMessage(QString("The \"Fingerprint ID\" number is invalid"));
+        extendedUi->finger_IdBox->setEnabled(true);
+        extendedUi->finger_touchCkBox->setChecked(false);
+        return;
+    }
+
+    if (fingerAgent && fingerAgent->setTouch) {
+        fingerAgent->setTouch(checked, id);
+    }
+
+    // Don't allow changing the ID while a touch is active
+    extendedUi->finger_IdBox->setEnabled( !checked );
+}
