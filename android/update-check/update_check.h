e942c579fc8ab8e5e75798478870a9785e3f3d98
Yurii Zubrytskyi <zyy@google.com>
2015-09-03 16:57:20 -0700

    Update checker in emulator
    Created a class which performs update check for the
    emulator version:
     - checks if check has been done today
     - if not, runs a thread to load xml with the latest version
     -   checks version and compares it with the current one
     -   if it's newer, just prints a message to stdout
     -   saves current time as a last check time
    
    No UTs for now
    
    Change-Id: Ifdffd82532e00117e4a8f0ff8f46d05aef2a53e4

---
+++ android/update-check/update_check.h
@@ -0,0 +1,12 @@
+#ifndef QEMU_ANDROID_UPDATE_CHECK_H
+#define QEMU_ANDROID_UPDATE_CHECK_H
+
+#include "android/utils/compiler.h"
+
+ANDROID_BEGIN_HEADER
+
+extern void checkForUpdates(const char* homePath);
+
+ANDROID_END_HEADER
+
+#endif //QEMU_ANDROID_UPDATE_CHECK_H
