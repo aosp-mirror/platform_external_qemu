917a78cae8aacede880ab3048db5fe16438e35d5
Prathmesh Prabhu <pprabhu@google.com>
2015-09-09 15:58:40 -0700

    curl: Bundle ca-certs file with qemu.
    qemu now uses libcurl to ping metrics home. curl needs a ca-certs file
    to verify the identity of the remote server. This fails if the user
    doesn't have a certs file in one of system defined default locations.
    
    Instead, this CL bundles a ca-bundle.pem file that is installed along
    with the emulator and used to verify peer.
    
    BUG=23749292
    
    Change-Id: Ibc2443e8d0b1594bd156213217dc216a20fe750d

--- android/utils/curl.c
+++ android/utils/curl.c
@@ -12,12 +12,17 @@
 #include "android/utils/curl.h"
 
 #include "android/utils/debug.h"
+#include "android/utils/system.h"
 
 #include <curl/curl.h>
 
-static int initCount = 0;
+#define mwarning(fmt, ...) \
+    dwarning("%s:%d: " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
 
-bool curl_init() {
+static int initCount = 0;
+static char* cached_ca_info = NULL;
+
+bool curl_init(const char* ca_info) {
     if (initCount == 0) {
         // first time - try to initialize the library
         const CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
@@ -28,6 +33,8 @@ bool curl_init() {
         }
     }
 
+    AFREE(cached_ca_info);
+    cached_ca_info = ASTRDUP(ca_info);
     ++initCount;
     return true;
 }
@@ -36,6 +43,26 @@ void curl_cleanup() {
     if (initCount == 0) {
         return;
     } else if (--initCount == 0) {
+        AFREE(cached_ca_info);
+        cached_ca_info = NULL;
         curl_global_cleanup();
     }
+}
+
+CURL* curl_easy_default_init() {
+    CURL* const curl = curl_easy_init();
+    CURLcode curlRes;
+
+    if (!curl) {
+        mwarning("Failed to initialize libcurl");
+        return NULL;
+    }
+    if (cached_ca_info != NULL) {
+        curlRes = curl_easy_setopt(curl, CURLOPT_CAINFO, cached_ca_info);
+        if (curlRes != CURLE_OK) {
+            mwarning("Could not set CURLOPT_CAINFO: %s",
+                     curl_easy_strerror(curlRes));
+        }
+    }
+    return curl;
 }
