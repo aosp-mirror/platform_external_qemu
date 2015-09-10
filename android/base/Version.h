6080de6f3319902042ddd55b6d000b0adddddc37
Yurii Zubrytskyi <zyy@google.com>
2015-09-10 13:30:45 -0700

    Added a class for version manipulation
    new class Verssion which supports parsing
    and comparing versions
    
    Change-Id: Ic25b8dea615a128746c284aa0a7fa16c6d909521

---
+++ android/base/Version.h
@@ -0,0 +1,44 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#ifndef ANDROID_BASE_VERSION_H
+#define ANDROID_BASE_VERSION_H
+
+#include <string>
+
+namespace android {
+namespace base {
+
+class Version {
+public:
+    explicit Version(const char* ver);
+    Version(unsigned int major, unsigned int minor, unsigned int micro);
+
+    bool isValid() const;
+
+    bool operator<(const Version& other) const;
+    bool operator==(const Version& other) const;
+    bool operator!=(const Version& other) const;
+
+    std::string toString() const;
+
+    static const Version kInvalid;
+
+private:
+    unsigned int mMajor;
+    unsigned int mMinor;
+    unsigned int mMicro;
+};
+
+}  // namespace android
+}  // namespace base
+
+#endif  // ANDROID_BASE_VERSION_H
