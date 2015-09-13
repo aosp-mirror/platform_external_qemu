32955450e9174a5dc52facb570c5d358734e96c5
Konstantinos Menychtas <kmenychtas@google.com>
2015-09-13 16:30:08 -0700

    Get .AndroidStudio opt-ins and installation-id
    This patch enables the emulator to scan for the appropriate
    .AndroidStudio preferences directory and pick up user preferences
    on crash-reports (opted-in or not), as well as the Android Studio
    installation id. 0/NULL values will be retrieved if no such
    installation exists.
    
    Change-Id: Id2d9000b77f125d462e95de18c3835e779444c71

---
+++ android/metrics/StudioHelper.cpp
@@ -0,0 +1,376 @@
+// Copyright 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/metrics/StudioHelper.h"
+
+#include "android/base/files/PathUtils.h"
+#include "android/base/system/System.h"
+#include "android/base/String.h"
+#include "android/base/Version.h"
+#include "android/metrics/studio-helper.h"
+#include "android/utils/debug.h"
+#include "android/utils/dirscanner.h"
+#include "android/utils/path.h"
+
+#include <libxml/tree.h>
+
+#include <stdlib.h>
+#include <fstream>
+
+#define E(...) derror(__VA_ARGS__)
+#define W(...) dwarning(__VA_ARGS__)
+#define D(...) VERBOSE_PRINT(init, __VA_ARGS__)
+
+/***************************************************************************/
+// These consts are replicated as macros in StudioHelper_unittest.cpp
+// changes to these will require equivalent changes to the unittests
+//
+static const char kAndroidStudioUuidDir[] = "JetBrains";
+static const char kAndroidStudioUuid[] = "PermanentUserID";
+
+#ifdef __APPLE__
+static const char kAndroidStudioDir[] = "AndroidStudio";
+#else   // ! ___APPLE__
+static const char kAndroidStudioDir[] = ".AndroidStudio";
+static const char kAndroidStudioDirInfix[] = "config";
+#endif  // __APPLE__
+
+static const char kAndroidStudioDirSuffix[] = "options";
+static const char kAndroidStudioDirPreview[] = "Preview";
+static const char kAndroidStudioUuidHexPattern[] =
+        "00000000-0000-0000-0000-000000000000";
+
+// StudioXML describes the XML parameters we are seeking for in a
+// Studio preferences file. StudioXml structs are statically
+// defined and used in  android_studio_get_installation_id()
+// and android_studio_get_optins().
+typedef struct {
+    const char* filename;
+    const char* nodename;
+    const char* propname;
+    const char* propvalue;
+    const char* keyname;
+} StudioXml;
+
+/***************************************************************************/
+
+using namespace android;
+using namespace android::base;
+
+// static
+const Version StudioHelper::extractAndroidStudioVersion(
+        const char* const dirName) {
+    if (dirName == NULL) {
+        return Version::Invalid();
+    }
+
+    // get rid of kAndroidStudioDir prefix to get to version
+    const char* cVersion = strstr(dirName, kAndroidStudioDir);
+    if (cVersion == NULL || cVersion != dirName) {
+        return Version::Invalid();
+    }
+    cVersion += strlen(kAndroidStudioDir);
+
+    // if this is a preview, get rid of kAndroidStudioDirPreview
+    // prefix too and mark preview as micro version 0
+    // (assume micro version 1 for releases)
+    const char* micro = "2";
+    const char* previewVersion = strstr(cVersion, kAndroidStudioDirPreview);
+    if (previewVersion == cVersion) {
+        cVersion += strlen(kAndroidStudioDirPreview);
+        micro = "1";
+    }
+
+    // cVersion should now contain at least a number; if not,
+    // this is a very early AndroidStudio installation, let's
+    // call it version 0
+    if (cVersion[0] == '\0') {
+        cVersion = "0";
+    }
+
+    // Create a major.micro.minor version string and append
+    // a "2" for release and a "1" for previews; this will
+    // allow proper sorting
+    Version rawVersion(cVersion);
+    String adjustedVersion(rawVersion.toString());
+    adjustedVersion.append(micro);
+
+    return Version(adjustedVersion.c_str());
+}
+
+// static
+String StudioHelper::latestAndroidStudioDir(const String& scanPath) {
+    String latest_path;
+
+    if (scanPath.empty()) {
+        return latest_path;
+    }
+
+    DirScanner* scanner = dirScanner_new(scanPath.c_str());
+    if (scanner == NULL) {
+        return latest_path;
+    }
+
+    System* system = System::get();
+    Version latest_version(0, 0, 0);
+
+    for (;;) {
+        const char* full_path = dirScanner_nextFull(scanner);
+        if (full_path == NULL) {
+            // End of enumeration.
+            break;
+        }
+        // ignore files, only interested in subDirs
+        if (!system->pathIsDir(full_path)) {
+            continue;
+        }
+
+        char* name = path_basename(full_path);
+        if (name == NULL) {
+            continue;
+        }
+        Version v = extractAndroidStudioVersion(name);
+        free(name);
+        if (v.isValid() && latest_version < v) {
+            latest_version = v;
+            latest_path = String(full_path);
+        }
+    }
+
+    return latest_path;
+}
+
+// static
+String StudioHelper::pathToStudioXML(const String& studioPath,
+                                     const String& filename) {
+    if (studioPath.empty() || filename.empty())
+        return String();
+
+    // build /path/to/.AndroidStudio/subpath/to/file.xml
+    StringVector vpath;
+    vpath.append(studioPath);
+#ifndef __APPLE__
+    vpath.append(String(kAndroidStudioDirInfix));
+#endif  // !__APPLE__
+    vpath.append(String(kAndroidStudioDirSuffix));
+    vpath.append(filename);
+    return PathUtils::recompose(vpath);
+}
+
+#ifdef _WIN32
+// static
+String StudioHelper::pathToStudioUUIDWindows() {
+    System* sys = System::get();
+    String appDataPath = sys->getAppDataDirectory();
+
+    String retval;
+    if (!appDataPath.empty()) {
+        // build /path/to/APPDATA/subpath/to/StudioUUID file
+        StringVector vpath;
+        vpath.append(appDataPath);
+        vpath.append(String(kAndroidStudioUuidDir));
+        vpath.append(String(kAndroidStudioUuid));
+
+        retval = PathUtils::recompose(vpath);
+    }
+
+    return retval;
+}
+#endif
+
+/*****************************************************************************/
+
+// Recurse through studio xml doc and return the value described in match
+// as a String, if one is set. Otherwise, return an empty string
+//
+static String eval_studio_config_xml(xmlDocPtr doc,
+                                     xmlNodePtr root,
+                                     const StudioXml* const match) {
+    xmlNodePtr current = NULL;
+    xmlChar* xmlval = NULL;
+    String retVal;
+
+    for (current = root; current; current = current->next) {
+        if (current->type == XML_ELEMENT_NODE) {
+            if ((!xmlStrcmp(current->name, BAD_CAST match->nodename))) {
+                xmlChar* propvalue =
+                        xmlGetProp(current, BAD_CAST match->propname);
+                int nomatch = xmlStrcmp(propvalue, BAD_CAST match->propvalue);
+                xmlFree(propvalue);
+                if (!nomatch) {
+                    xmlval = xmlGetProp(current, BAD_CAST match->keyname);
+                    if (xmlval != NULL) {
+                        // xmlChar* is defined as unsigned char
+                        // we are simply reading the result and don't
+                        // operate on it, soe simply reinterpret as
+                        // as char * array
+                        retVal = String(reinterpret_cast<char*>(xmlval));
+                        xmlFree(xmlval);
+                        break;
+                    }
+                }
+            }
+        }
+        retVal = eval_studio_config_xml(doc, current->children, match);
+        if (!retVal.empty()) {
+            break;
+        }
+    }
+
+    return retVal;
+}
+
+// Find latest studio preferences directory and return the
+// value of the xml entry described in |match|.
+//
+static String parseStudioXML(const StudioXml* const match) {
+    String retval;
+
+    System* sys = System::get();
+    // Get path to .AndroidStudio
+    String studio;
+    const char* env_studio = sys->envGet("ANDROID_STUDIO_PREFERENCES");
+    if (env_studio != NULL) {
+        studio = String(env_studio);
+    } else {
+        String appDataPath;
+#ifdef __APPLE__
+        appDataPath = sys->getAppDataDirectory();
+#else
+        appDataPath = sys->getHomeDirectory();
+#endif  // __APPLE__
+        if (appDataPath.empty()) {
+            return retval;
+        }
+        studio = StudioHelper::latestAndroidStudioDir(appDataPath);
+    }
+    if (studio.empty()) {
+        return retval;
+    }
+
+    // Find match->filename xml file under .AndroidStudio
+    String xml_path =
+            StudioHelper::pathToStudioXML(studio, String(match->filename));
+    if (xml_path.empty()) {
+        D("Failed to find %s in %s", match->filename, studio.c_str());
+        return retval;
+    }
+
+    xmlDocPtr doc = xmlReadFile(xml_path.c_str(), NULL, 0);
+    if (doc == NULL)
+        return retval;
+
+    xmlNodePtr root = xmlDocGetRootElement(doc);
+    retval = eval_studio_config_xml(doc, root, match);
+
+    xmlFreeDoc(doc);
+
+    return retval;
+}
+
+#ifdef _WIN32
+static String android_studio_get_Windows_uuid() {
+    // Appropriately sized container for UUID
+    String uuid_file_path = StudioHelper::pathToStudioUUIDWindows();
+    String retval;
+
+    // Read UUID from file
+    std::ifstream uuid_file(uuid_file_path.c_str());
+    if (uuid_file) {
+        std::string line;
+        std::getline(uuid_file, line);
+        retval = String(line.c_str());
+    }
+
+    return retval;
+}
+#endif  // _WIN32
+
+/*****************************************************************************/
+
+// Get the status of user opt-in to crash reporting in AndroidStudio
+// preferences. Returns 1 only if the user has opted-in, 0 otherwise.
+//
+int android_studio_get_optins() {
+    int retval = 0;
+
+    static const StudioXml optins = {
+            .filename = "usage.statistics.xml",
+            .nodename = "component",
+            .propname = "name",
+            .propvalue = "UsagesStatistic",
+            .keyname = "allowed",  // assuming "true"/"false" string values
+    };
+
+    String xmlVal = parseStudioXML(&optins);
+    if (xmlVal.empty()) {
+        D("Failed to parse %s preferences file %s", kAndroidStudioDir,
+          optins.filename);
+        D("Defaulting user crash-report opt-in to false");
+        return 0;
+    }
+
+    // return 0 if user has not opted in to crash reports
+    if (xmlVal == "true") {
+        retval = 1;
+    } else if (xmlVal == "false") {
+        retval = 0;
+    } else {
+        D("Invalid value set in %s preferences file %s", kAndroidStudioDir,
+          optins.filename);
+    }
+
+    return retval;
+}
+
+// Get the installation.id reported by Android Studio (string).
+// If there is not Android Studio installation or a value
+// cannot be retrieved, a random installation ID will
+// be generated. Caller is responsible for freeing returned
+// string
+//
+char* android_studio_get_installation_id() {
+    String retval;
+#ifndef __WIN32
+    static const StudioXml uuid = {
+            .filename = "options.xml",
+            .nodename = "property",
+            .propname = "name",
+            .propvalue = "installation.uid",
+            .keyname = "value",  // assuming kAndroidStudioUuidHexPattern
+    };
+    retval = parseStudioXML(&uuid);
+
+    if (retval.empty()) {
+        D("Failed to parse %s preferences file %s", kAndroidStudioDir,
+          uuid.filename);
+    }
+#else
+    // In Microsoft Windows, getting Android Studio installation
+    // ID requires searching in completely different path than the
+    // rest of Studio preferences ...
+    retval = android_studio_get_Windows_uuid();
+    if (retval.empty()) {
+        D("Failed to parse %s preferences file %s", kAndroidStudioDir,
+          kAndroidStudioUuid);
+    }
+#endif
+
+    if (retval.empty()) {
+        D("Defaulting to zero installation ID");
+        retval = String(kAndroidStudioUuidHexPattern);
+    }
+
+    return strdup(retval.c_str());
+}
---
+++ android/metrics/StudioHelper_unittest.cpp
@@ -0,0 +1,204 @@
+// Copyright 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/metrics/StudioHelper.h"
+
+#include "android/base/containers/StringVector.h"
+#include "android/base/testing/TestSystem.h"
+#include "android/base/testing/TestTempDir.h"
+#include "android/base/String.h"
+#include "android/base/Version.h"
+#include "android/utils/path.h"
+
+#include <gtest/gtest.h>
+
+/***************************************************************************/
+// These macros are replicated as consts in StudioHelper.cpp
+// changes to these will require equivalent changes to the unittests
+//
+
+#define ANDROID_STUDIO_UUID_DIR "JetBrains"
+#define ANDROID_STUDIO_UUID "PermanentUserID"
+
+#ifdef __APPLE__
+#define ANDROID_STUDIO_DIR "AndroidStudio"
+#else  // ! ___APPLE__
+#define ANDROID_STUDIO_DIR ".AndroidStudio"
+#define ANDROID_STUDIO_DIR_INFIX "config"
+#endif  // __APPLE__
+
+#define ANDROID_STUDIO_DIR_SUFFIX "options"
+#define ANDROID_STUDIO_DIR_PREVIEW "Preview"
+
+using namespace android::base;
+using namespace android;
+
+TEST(DotAndroidStudio, androidStudioVersioning) {
+    const char* str = NULL;
+
+    str = ANDROID_STUDIO_DIR;
+    Version v1studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v1 = Version(0, 0, 2);
+    EXPECT_EQ(v1studio, v1);
+
+    str = ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW;
+    Version v2studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v2 = Version(0, 0, 1);
+    EXPECT_EQ(v2studio, v2);
+
+    str = "prefix_" ANDROID_STUDIO_DIR;
+    Version v3studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v3 = Version::Invalid();
+    EXPECT_EQ(v3studio, v3);
+
+    str = ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "_suffix";
+    Version v4studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v4 = Version::Invalid();
+    EXPECT_EQ(v4studio, v4);
+
+    str = ANDROID_STUDIO_DIR "1";
+    Version v5studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v5 = Version(1, 0, 2);
+    EXPECT_EQ(v5studio, v5);
+
+    str = ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "1.3";
+    Version v6studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v6 = Version(1, 3, 1);
+    EXPECT_EQ(v6studio, v6);
+
+    str = ANDROID_STUDIO_DIR "1.20.3";
+    Version v7studio(StudioHelper::extractAndroidStudioVersion(str));
+    Version v7 = Version(1, 20, 32);
+    EXPECT_EQ(v7studio, v7);
+}
+
+#ifdef _WIN32
+#define FS "\\"
+#else
+#define FS "/"
+#endif
+
+TEST(DotAndroidStudio, androidStudioScanner) {
+    TestSystem testSys("/root", 32, "/root/home");
+    TestTempDir* testDir = testSys.getTempRoot();
+
+    String studioPath;
+    String foundStudioPath;
+
+    testDir->makeSubDir("root");
+    testDir->makeSubDir("root/home");
+    testDir->makeSubDir("root/adir");
+    testDir->makeSubFile("root/afile");
+
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+
+    testDir->makeSubFile("root/home/afile");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "-invalid");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR);
+
+    studioPath = "/root/home" FS ANDROID_STUDIO_DIR;
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "0");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "0.1");
+
+    studioPath = "/root/home" FS ANDROID_STUDIO_DIR "0.1";
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "1");
+    testDir->makeSubDir(
+            "root/home/" ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "1");
+
+    studioPath = "/root/home" FS ANDROID_STUDIO_DIR "1";
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "1.2");
+    testDir->makeSubDir(
+            "root/home/" ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "1.2");
+    testDir->makeSubFile("root/home/" ANDROID_STUDIO_DIR "1.3");
+    testDir->makeSubDir("root/home/prefix" ANDROID_STUDIO_DIR "1.4");
+
+    // 1.3 is a file, not valid
+    studioPath = "/root/home" FS ANDROID_STUDIO_DIR "1.2";
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "1.2.3");
+    testDir->makeSubDir(
+            "root/home/" ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "1.2.3");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "10.20.30");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "Preview10.20.30");
+
+    studioPath = "/root/home" FS ANDROID_STUDIO_DIR "10.20.30";
+    foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    EXPECT_STREQ(foundStudioPath.c_str(), studioPath.c_str());
+}
+
+TEST(DotAndroidStudio, androidStudioXMLPathBuilder) {
+    TestSystem testSys("/root", 32, "/root/home", "root/home");
+    TestTempDir* testDir = testSys.getTempRoot();
+
+    testDir->makeSubDir("root");
+    testDir->makeSubDir("root/home");
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR);
+    testDir->makeSubDir("root/home/" ANDROID_STUDIO_DIR "1.4");
+    testDir->makeSubDir(
+            "root/home/" ANDROID_STUDIO_DIR ANDROID_STUDIO_DIR_PREVIEW "1.4");
+    testDir->makeSubFile("root/home/" ANDROID_STUDIO_DIR "10.40");
+
+    String foundStudioPath = StudioHelper::latestAndroidStudioDir("/root/home");
+    String foundStudioXMLPath = StudioHelper::pathToStudioXML(
+            foundStudioPath, String("usage.statistics.xml"));
+
+#ifdef __APPLE__
+    // Normally, path to .AndroidStudio in MacOSX would include
+    // /path/to/home/Library/Preferences (equivalent of APPDATA).
+    // We simplify this test by setting APPDATA=HOME
+    const char* studioXMLPath =
+            "/root/home" FS ANDROID_STUDIO_DIR
+            "1.4" FS ANDROID_STUDIO_DIR_SUFFIX FS "usage.statistics.xml";
+#else
+    const char* studioXMLPath =
+            "/root/home" FS ANDROID_STUDIO_DIR
+            "1.4" FS ANDROID_STUDIO_DIR_INFIX FS ANDROID_STUDIO_DIR_SUFFIX FS
+            "usage.statistics.xml";
+#endif
+
+    EXPECT_STREQ(foundStudioXMLPath.c_str(), studioXMLPath);
+}
+
+#ifdef _WIN32
+// This is a Windows-only test
+TEST(DotAndroidStudio, androidStudioUUIDWindows) {
+    TestSystem testSys("/root", 32, "/root/home", "/root/appdata");
+    TestTempDir* testDir = testSys.getTempRoot();
+
+    testDir->makeSubDir("root");
+    testDir->makeSubDir("root/home");
+    testDir->makeSubDir("root/appdata");
+
+    testDir->makeSubDir("root/appdata/" ANDROID_STUDIO_UUID_DIR);
+    testDir->makeSubFile("root/appdata/" ANDROID_STUDIO_UUID_DIR
+                         "/" ANDROID_STUDIO_UUID);
+
+    String foundStudioUUIDPath = StudioHelper::pathToStudioUUIDWindows();
+    const char* UUIDPath =
+            "/root/appdata" FS ANDROID_STUDIO_UUID_DIR FS ANDROID_STUDIO_UUID;
+    EXPECT_STREQ(foundStudioUUIDPath.c_str(), UUIDPath);
+}
+#endif  // _WIN32
