0da6f50a74944652d7a089265423b16f8b78a520
Jim Kaye <jameskaye@google.com>
2015-09-14 11:48:01 -0700

    Use style sheets to define the light and dark themes
    Load a style sheet based on the theme that the user selects.
    The style sheet defines the colors to use. The style
    sheet relies on the "ColorGroup" property to adjust
    the color of some elements (e.g., a page title vs.
    normal text).
    
    Some other minor changes:
      Add digital read-out to slider controls.
      Add some tabs to the window (placeholders for now).
    
    Change-Id: I114f749aa4b43676d83b2f01a8d26ab7b6ee9ccc

--- android/skin/qt/extended-window-styles.h
+++ android/skin/qt/extended-window-styles.h
@@ -16,44 +16,102 @@
 #ifndef ANDROID_SKIN_QT_EXTENDED_WINDOW_STYLES_H
 #define ANDROID_SKIN_QT_EXTENDED_WINDOW_STYLES_H
 
-// Colors for the dark theme
-#define DARK_FOREGROUND  "#f0f0f0"
-#define DARK_BACKGROUND  "#3c4249"
-#define DARK_ACTIVE      "#4c5259"  // A little lighter than the background
-
-// Colors for the light theme
-#define LIGHT_FOREGROUND "#0f0f0f"
-#define LIGHT_BACKGROUND "#c3bdb6"
-#define LIGHT_ACTIVE     "#b3ada6"  // A little darker than the background
-
-// The sliders look ugly without some customization
-#define SLIDER_STYLE "QSlider::sub-page:horizontal {background: #888}" \
-                     "QSlider::handle:horizontal {background: #eee;" \
-                               "border: 2px solid #444; border-radius: 4px }"
-
-// The check boxes also look ugly without some customization
-#define DARK_CHECKBOX_STYLE \
-    "QCheckBox::indicator:checked   { image: url(:/dark/check_yes); " \
-                                     "icon-size: 24px; height: 24 px; width: 24 px }" \
-    "QCheckBox::indicator:unchecked { image: url(:/dark/check_no); " \
-                                     "icon-size: 24px; height: 24 px; width: 24 px }"
-
-#define LIGHT_CHECKBOX_STYLE \
-    "QCheckBox::indicator:checked   { image: url(:/light/check_yes); " \
-                                     "icon-size: 24px; height: 24 px; width: 24 px }" \
-    "QCheckBox::indicator:unchecked { image: url(:/light/check_no); " \
-                                     "icon-size: 24px; height: 24 px; width: 24 px }"
-
 // This gives the order of the tabbed panes on the extended window.
 // This must correspond to the ordering that is set from within
 // QtDesigner and written to extended.ui.
-enum { PANE_IDX_BATTERY = 0,
-       PANE_IDX_TELEPHONE,
-       PANE_IDX_MESSAGE,
-       PANE_IDX_LOCATION,
+enum { PANE_IDX_LOCATION = 0,
        PANE_IDX_CELLULAR,
+       PANE_IDX_BATTERY,
+       PANE_IDX_CAMERA,
+       PANE_IDX_TELEPHONE,
+       PANE_IDX_VIRT_SENSORS,
+       PANE_IDX_HW_SENSORS,
+       PANE_IDX_DPAD,
+       PANE_IDX_MESSAGE,
        PANE_IDX_FINGER,
        PANE_IDX_SD,
        PANE_IDX_SETTINGS };
+
+// Style sheets
+//
+// We have two styles: one for the light-colored theme and one for
+// the dark-colored theme.
+//
+// Each theme indicates the colors that are to be used for the foreground
+// (mostly text) and the background.
+//
+// Even within a theme, not all widgets use the same colors. The style
+// sheet accomodates this by associating colors based on "properties"
+// that can be assigned to a widget. (Properties are listed in the .ui
+// file.)
+//
+// So far, we have four sets of colors for each theme:
+//   "MajorTab":       The area of the tab buttons on the left of the window
+//   "MajorTabTitle":  Section titles separating the tab buttons
+//   "Title":          Section titles in the main part of the window
+//   <Normal>:         Text in the main part of the window
+//
+// The first three sets are identified by the value of their "ColorGroup"
+// property. The last set is identified by not having a "ColorGroup" property.
+
+// These are the colors used in the two themes
+
+#define LIGHT_BKG_COLOR         "#f9f9f9"  // Main page background
+#define LIGHT_MAJOR_TAB_COLOR   "#91a4ad"  // Text of major tabs
+#define LIGHT_MAJOR_TITLE_COLOR "#617d8a"  // Text of major tab separators
+#define LIGHT_TAB_BKG_COLOR     "#ffffff"  // Background of major tabs
+#define LIGHT_TEXT_COLOR        "#212121"  // Main page text
+#define LIGHT_TITLE_COLOR       "#757575"  // Main page titles
+#define LIGHT_TOOL_COLOR        "#00bea4"  // Checkboxes, sliders, etc.
+
+#define DARK_BKG_COLOR          "#273238"
+#define DARK_MAJOR_TAB_COLOR    "#bdc0c3"
+#define DARK_MAJOR_TITLE_COLOR  "#e5e6e7"
+#define DARK_TAB_BKG_COLOR      "#394249"
+#define DARK_TEXT_COLOR         "#eeeeee"
+#define DARK_TITLE_COLOR        "#ffffff"
+#define DARK_TOOL_COLOR         "#00bea4"
+
+// These are the styles. One for each theme.
+
+#define QT_STYLE_LIGHT_THEME \
+    "*[ColorGroup=\"MajorTab\"]      { color:" LIGHT_MAJOR_TAB_COLOR "}" \
+    "*[ColorGroup=\"MajorTabTitle\"] { color:" LIGHT_MAJOR_TITLE_COLOR ";" \
+                                      "background-color:" LIGHT_TAB_BKG_COLOR "}" \
+    "*[ColorGroup=\"Title\"]         { color:" LIGHT_TITLE_COLOR "}" \
+    "*                               { color:" LIGHT_TEXT_COLOR ";" \
+                                      "background-color: "LIGHT_BKG_COLOR "}" \
+    "QTextEdit, QPlainTextEdit, QTreeView {" \
+            "border: 1px solid " LIGHT_TEXT_COLOR "} "\
+    "QCheckBox::indicator:checked {" \
+            "image: url(:/dark/check_yes); " \
+            "icon-size: 24px; height: 24 px; width: 24 px }" \
+    "QCheckBox::indicator:unchecked {" \
+            "image: url(:/dark/check_no); " \
+            "icon-size: 24px; height: 24 px; width: 24 px }" \
+    "QSlider::sub-page:horizontal {background: " LIGHT_TOOL_COLOR "}" \
+    "QSlider::handle:horizontal {" \
+            "background:" LIGHT_TOOL_COLOR ";" \
+            "border: 1px solid " LIGHT_TOOL_COLOR "; border-radius: 7px }"
+
+#define QT_STYLE_DARK_THEME \
+    "*[ColorGroup=\"MajorTab\"]      { color:" DARK_MAJOR_TAB_COLOR "}" \
+    "*[ColorGroup=\"MajorTabTitle\"] { color:" DARK_MAJOR_TITLE_COLOR ";" \
+                                      "background-color:" DARK_TAB_BKG_COLOR "}" \
+    "*[ColorGroup=\"Title\"]         { color:" DARK_TITLE_COLOR "}" \
+    "*                               { color:" DARK_TEXT_COLOR ";" \
+                                      "background-color: "DARK_BKG_COLOR "}" \
+    "QTextEdit, QPlainTextEdit, QTreeView {" \
+            "border: 1px solid " DARK_TEXT_COLOR "} "\
+    "QCheckBox::indicator:checked {" \
+            "image: url(:/dark/check_yes); " \
+            "icon-size: 24px; height: 24 px; width: 24 px }" \
+    "QCheckBox::indicator:unchecked {" \
+            "image: url(:/dark/check_no); " \
+            "icon-size: 24px; height: 24 px; width: 24 px }" \
+    "QSlider::sub-page:horizontal {background: " DARK_TOOL_COLOR "}" \
+    "QSlider::handle:horizontal {" \
+            "background:" DARK_TOOL_COLOR ";" \
+            "border: 1px solid " DARK_TOOL_COLOR "; border-radius: 7px }"
 
 #endif // ANDROID_SKIN_QT_EXTENDED_WINDOW_STYLES_H
