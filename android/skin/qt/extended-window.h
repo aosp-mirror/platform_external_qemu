e3acf8a8d3f3299b2b1aa46765239f587ffae0f6
Jim Kaye <jameskaye@google.com>
2015-09-18 15:37:16 -0700

    Populate the Keyboard shortcuts pane
    Add a table showing the shortcut keys and their functions
    
    Change-Id: I96bf941374992b572db2cdbcfa74cb7513215d19

--- android/skin/qt/extended-window.h
+++ android/skin/qt/extended-window.h
@@ -94,6 +94,7 @@ private:
     void initBattery();
     void initCellular();
     void initFinger();
+    void initKbdShorts();
     void initLocation();
     void initSd();
     void initSettings();
