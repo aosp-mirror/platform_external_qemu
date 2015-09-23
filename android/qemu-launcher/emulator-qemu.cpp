adb7f72cac8e3586274ac914cf4bef21a7b983ac
Miroslav Tisma <miroslav.tisma@imgtec.com>
2015-09-23 14:48:25 +0200

    qemu-launcher: Add qemu=1 and set lcd-density
    Add qemu=1 parameter to kernel command line to identify itself as
    emulator to Android.
    
    Add -lcd-density as qemu switch for controling the default
    screen density. It's required when gpu emulation is on
    for choosing appropriate density read from hw properties.
    
    Change-Id: Ic148177ab9f8412022d725c5db2cdc08fd097b1d

--- android/qemu-launcher/emulator-qemu.cpp
+++ android/qemu-launcher/emulator-qemu.cpp
@@ -536,12 +536,23 @@ extern "C" int main(int argc, char **argv, char **envp) {
                 " androidboot.selinux=%s", opts->selinux);
     }
 
+    kernelCommandLine += " qemu=1";
+
     if (hw->hw_gpu_enabled) {
         kernelCommandLine += " qemu.gles=1";
     } else {
         kernelCommandLine += " qemu.gles=0";
     }
+
     args[n++] = kernelCommandLine.c_str();
+
+    // Support for changing default lcd-density
+    String lcd_density;
+    if (hw->hw_lcd_density) {
+        args[n++] = "-lcd-density";
+        lcd_density = StringFormat("%d", hw->hw_lcd_density);
+        args[n++] = lcd_density.c_str();
+    }
 
     args[n++] = "-serial";
     args[n++] = "mon:stdio";
