e94e18ab213f9d21a7fe8a7c11b7ec7591e6d75e
Prathmesh Prabhu <pprabhu@google.com>
2015-09-24 14:05:44 -0700

    metrics: url-escape the ping to toolbar.
    toolbar expects url escaped fields in the GET query. This CL updates the
    URL formatting function to escape the URL, and updates the unittests
    accordingly.
    
    Note that Google Analytics actually doesn't handle escaped POST fields
    properly, so that is left untouched.
    
    Change-Id: I61f37fbc59f1ddbe8e5a12b38bd4490d27032e0c

---
+++ android/utils/uri.h
@@ -0,0 +1,33 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Some free functions for manipulating Strings as URIs. Wherever possible,
+// these functions take const references to StringView to avoid unnecessary
+// copies.
+
+#pragma once
+
+#include "android/utils/compiler.h"
+
+ANDROID_BEGIN_HEADER
+
+// This is a simple wrapper around android/base/Uri.h to expose the same
+// functionality to C code.
+
+// Returns percent encoded URI. Caller must free the returned string.
+// Returns the empty string "" on encode error, NULL on memory allocation error.
+extern char* uri_encode(const char* uri);
+
+// Decodes a percent encoded URI. Caller must free the returned string.
+// Returns the empty string "" on decode error, NULL on memory allocation error.
+extern char* uri_decode(const char* uri);
+
+ANDROID_END_HEADER
