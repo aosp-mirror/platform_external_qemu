d246d7c8494df42940f5d3828205bdc0d710c43c
Yurii Zubrytskyi <zyy@google.com>
2015-09-27 19:29:40 -0700

    QEMUD refactoring: the refactoring
    1. Split the implemenation from hw-qemud into a bunch
    of files under android/emulation/qemud:
     client, service, serial, sink, multiplexer
    Public header is android/emulation/android_qemud.h,
    the rest is just implementaion details and is not supposed
    to be included.
    
    2. Replaced all uses of CharDriverState with CSerialLine
    in the library code. hw-qemud still uses CharDriverState
    and creates wrappers to pass them into library
    
    3. Also, caught two errors in the old code:
    - boot-properties.c - initialization function did check
    initialized flag, but it was only set from outside. As the
    function is called from two places, one of them didn't set
    the flag - moved flag changes inside the function
    - hw-qemud - _qemud_char_client_close was treating its
    |opaque| parameter as QemudCLient, but actually it was a
    communication object (CharDriverState before, CSerialLine now)
    
    Change-Id: I9bd6472d4b975869bab939a495959704aa703a1d

---
+++ android/emulation/qemud/android_qemud_common.h
@@ -0,0 +1,55 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/utils/debug.h"
+#include "android/utils/misc.h"
+
+/* length of the framed header */
+#define  FRAME_HEADER_SIZE  4
+
+/* Version number of snapshots code. Increment whenever the data saved
+ * or the layout in which it is saved is changed.
+ */
+#define QEMUD_SAVE_VERSION 2
+
+#define  D(...)    VERBOSE_PRINT(qemud,__VA_ARGS__)
+#define  D_ACTIVE  VERBOSE_CHECK(qemud)
+
+/* the T(...) macro is used to dump traffic */
+#define  T_ACTIVE   0
+
+#if T_ACTIVE
+#define  T(...)    VERBOSE_PRINT(qemud,__VA_ARGS__)
+#else
+#define  T(...)    ((void)0)
+#endif
+
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+/* define SUPPORT_LEGACY_QEMUD to 1 if you want to support
+ * talking to a legacy qemud daemon. See docs/ANDROID-QEMUD.TXT
+ * for details.
+ */
+#ifdef TARGET_ARM
+#define  SUPPORT_LEGACY_QEMUD  1
+#endif
+#ifdef TARGET_I386
+#define  SUPPORT_LEGACY_QEMUD  0 /* no legacy support */
+#endif
+
+#if SUPPORT_LEGACY_QEMUD
+#include "android/telephony/modem.h"
+#include "telephony/modem_driver.h"
+#endif
