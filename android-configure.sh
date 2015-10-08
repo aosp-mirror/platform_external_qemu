c7cf4d038b428274ed8014df20e217cfcec5c43c
Chaoren Lin <chaorenl@google.com>
2015-10-08 11:10:41 -0700

    Use the MinGW toolchain generated by gen-android-sdk-toolchain.sh.
    So that it's consistent with other toolchains with wrappers, and gets
    rid of the ugly hack in http://r.android.com/174668.
    
    Change-Id: Idb0c0c0f3055378f050a4f42b66e00a8341b44a3

--- android-configure.sh
+++ android-configure.sh
@@ -499,38 +499,14 @@ if [ "$OPTION_MINGW" = "yes" ] ; then
         echo "Sorry, but mingw compilation is only supported on Linux !"
         exit 1
     fi
-    # Do we have our prebuilt mingw64 toolchain?
-    log "Mingw      : Looking for prebuilt mingw64 toolchain."
-    MINGW_DIR=$PROGDIR/../../prebuilts/gcc/linux-x86/host/x86_64-w64-mingw32-4.8
-    MINGW_CC=
-    MINGW_CXX=
-    if [ -d "$MINGW_DIR" ]; then
-        MINGW_PREFIX=$MINGW_DIR/bin/x86_64-w64-mingw32
-        find_program MINGW_CC "$MINGW_PREFIX-gcc"
-        find_program MINGW_CXX "$MINGW_PREFIX-g++"
-    fi
-    if [ -z "$MINGW_CC" ]; then
-        log "Mingw      : Looking for mingw64 toolchain."
-        MINGW_PREFIX=x86_64-w64-mingw32
-        find_program MINGW_CC $MINGW_PREFIX-gcc
-        find_program MINGW_CXX $MINGW_PREFIX-g++
-    fi
-    if [ -z "$MINGW_CC" ]; then
-        echo "ERROR: It looks like no Mingw64 toolchain is available!"
-        echo "Please install x86_64-w64-mingw32 package !"
-        exit 1
-    fi
-    log2 "Mingw      : Found $MINGW_CC"
-    CC=$MINGW_CC
-    CXX=$MINGW_CXX
-    if [ "$CCACHE" ]; then
-        CC="$CCACHE $CC"
-        CXX="$CCACHE $CXX"
-    fi
-    CXX="$CXX -std=c++11 -Werror=c++11-compat"
-    LD=$MINGW_CC
+    GEN_SDK_FLAGS="$GEN_SDK_FLAGS --host=windows-x86_64"
+    $GEN_SDK $GEN_SDK_FLAGS $OUT_DIR/toolchain || panic "Cannot generate SDK toolchain!"
+    BINPREFIX=$($GEN_SDK $GEN_SDK_FLAGS --print=binprefix $OUT_DIR/toolchain)
+    CC="$OUT_DIR/toolchain/${BINPREFIX}gcc"
+    CXX="$OUT_DIR/toolchain/${BINPREFIX}g++"
+    LD=$CC
     WINDRES=$MINGW_PREFIX-windres
-    AR=$MINGW_PREFIX-ar
+    AR="$OUT_DIR/toolchain/${BINPREFIX}ar"
     HOST_OS=windows
     HOST_TAG=$HOST_OS-$HOST_ARCH
 fi
