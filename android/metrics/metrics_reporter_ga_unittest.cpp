e70e10ce957cf4fee3a8ca108e80f222cddcc499
Lingfeng Yang <lfy@google.com>
2015-10-19 12:40:08 -0700

    Add opengl_alive metric variable; track in emulator init; exit if openGL init fails
    Change-Id: I6ad791c033733414f58467f6c4bd713b4228b52c

--- android/metrics/metrics_reporter_ga_unittest.cpp
+++ android/metrics/metrics_reporter_ga_unittest.cpp
@@ -40,7 +40,7 @@ TEST_F(MetricsReporterGaTest, defaultMetrics) {
     static const char kExpected[] =
             "v=1&tid=UA-19996407-3&an=Android Studio&av=unknown&"
             "cid=00000000-0000-0000-0000-000000000000&cd6=unknown&t=event&"
-            "ec=emulator&ea=singleRunCrashInfo&el=crashDetected&cm2=0&cm3=0";
+            "ec=emulator&ea=singleRunCrashInfo&el=crashDetectedOpenglInitFailed&cm2=0&cm3=0";
     static const int kExpectedLen = (int)(sizeof(kExpected) - 1);
 
     androidMetrics_init(&metrics);
@@ -63,6 +63,7 @@ TEST_F(MetricsReporterGaTest, cleanRun) {
     ANDROID_METRICS_STRASSIGN(metrics.emulator_version, "standalone");
     ANDROID_METRICS_STRASSIGN(metrics.guest_arch, "x86_64");
     metrics.guest_gpu_enabled = 1;
+    metrics.opengl_alive = 1;
     metrics.tick = 1;
     metrics.system_time = 1170;
     metrics.user_time = 220;
@@ -89,10 +90,65 @@ TEST_F(MetricsReporterGaTest, dirtyRun) {
     ANDROID_METRICS_STRASSIGN(metrics.emulator_version, "standalone");
     ANDROID_METRICS_STRASSIGN(metrics.guest_arch, "x86_64");
     metrics.guest_gpu_enabled = 1;
+    metrics.opengl_alive = 1;
     metrics.tick = 1;
     metrics.system_time = 1080;
     metrics.user_time = 180;
     metrics.is_dirty = 1;
+    metrics.num_failed_reports = 9;
+
+    ASSERT_EQ(kExpectedLen, formatGAPostData(&post_data, &metrics));
+    ASSERT_STREQ(kExpected, post_data);
+    androidMetrics_fini(&metrics);
+    free(post_data);
+}
+
+TEST_F(MetricsReporterGaTest, openGLErrorRun) {
+    char* post_data;
+    AndroidMetrics metrics;
+    static const char kExpected[] =
+            "v=1&tid=UA-19996407-3&an=Android Studio&av=standalone&"
+            "cid=00000000-0000-0000-0000-000000000000&cd6=x86_64&t=event&"
+            "ec=emulator&ea=singleRunCrashInfo&el=crashDetectedOpenglInitFailed&cm2=180&"
+            "cm3=1080";
+    static const int kExpectedLen = (int)(sizeof(kExpected) - 1);
+
+    androidMetrics_init(&metrics);
+    ANDROID_METRICS_STRASSIGN(metrics.emulator_version, "standalone");
+    ANDROID_METRICS_STRASSIGN(metrics.guest_arch, "x86_64");
+    metrics.guest_gpu_enabled = 1;
+    metrics.opengl_alive = 0;
+    metrics.tick = 1;
+    metrics.system_time = 1080;
+    metrics.user_time = 180;
+    metrics.is_dirty = 1;
+    metrics.num_failed_reports = 9;
+
+    ASSERT_EQ(kExpectedLen, formatGAPostData(&post_data, &metrics));
+    ASSERT_STREQ(kExpected, post_data);
+    androidMetrics_fini(&metrics);
+    free(post_data);
+}
+
+TEST_F(MetricsReporterGaTest, openGLErrorInconsistentRun) {
+    char* post_data;
+    AndroidMetrics metrics;
+    static const char kExpected[] =
+            "v=1&tid=UA-19996407-3&an=Android Studio&av=standalone&"
+            "cid=00000000-0000-0000-0000-000000000000&cd6=x86_64&t=event&"
+            "ec=emulator&ea=singleRunCrashInfo&el=cleanExitOpenglInitFailedINCONSISTENT&cm2=180&"
+            "cm3=1080";
+    static const int kExpectedLen = (int)(sizeof(kExpected) - 1);
+
+    androidMetrics_init(&metrics);
+    ANDROID_METRICS_STRASSIGN(metrics.emulator_version, "standalone");
+    ANDROID_METRICS_STRASSIGN(metrics.guest_arch, "x86_64");
+    metrics.guest_gpu_enabled = 1;
+    metrics.opengl_alive = 0;
+    metrics.tick = 1;
+    metrics.system_time = 1080;
+    metrics.user_time = 180;
+    metrics.is_dirty = 0;
     metrics.num_failed_reports = 9;
 
     ASSERT_EQ(kExpectedLen, formatGAPostData(&post_data, &metrics));
