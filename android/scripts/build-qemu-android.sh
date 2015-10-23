0f25c3d5a279b902ca28af2ef8b273d9126b936c
David 'Digit' Turner <digit@google.com>
2015-10-23 17:46:40 +0200

    build-qemu-android.sh: Capture link output.
    This changes the QEMU2 build script to capture the link
    commands for each one of the final qemu-system-* executable
    into a LINK-qemu-system-* text file.
    
    This will be used by a future script to auto-generate the
    list of sources files, to be used when building QEMU2 from
    the emulator's build system.
    
    NOTE: This doesn't remove the generation of OBJECTS-LIST.TXT
    files, this may happen later.
    
    Change-Id: I9e426f97de65ebf3cc8ff12f568ca03c0554b58d

--- android/scripts/build-qemu-android.sh
+++ android/scripts/build-qemu-android.sh
@@ -276,6 +276,29 @@ EOF
                 ;;
         esac
 
+        # Create a linking program that will capture its command-line to a file.
+        cat > link-prog <<EOF
+#!/bin/sh
+
+# Extract target file.
+seen_o=
+target=
+for opt; do
+    if [ "\$opt" = "-o" ]; then
+      seen_o=true
+    elif [ "\$seen_o" ]; then
+      target=\$opt
+      seen_o=
+    fi
+done
+
+if [ "\$target" ]; then
+  printf "%s\n" "\$@" > $BUILD_DIR/LINK-\$target
+fi
+$(builder_gnu_config_host_prefix)g++ "\$@"
+EOF
+        chmod a+x link-prog
+
         SDL_CONFIG=$PREFIX/bin/sdl2-config
         export SDL_CONFIG
 
@@ -344,7 +367,7 @@ EOF
             esac
 
             # Now build everything else in parallel.
-            run make -j$NUM_JOBS $BUILD_FLAGS
+            run make -j$NUM_JOBS $BUILD_FLAGS LINKPROG=$BUILD_DIR/link-prog
 
             for QEMU_EXE in $QEMU_TARGET_BUILDS; do
                 if [ ! -f "$QEMU_EXE" ]; then
@@ -374,6 +397,14 @@ EOF
     # Generate object list file.
     find "$BUILD_DIR" -name "*.o" | sed -e 's|'$BUILD_DIR'/||g' | \
             sort -u | cat > $INSTALL_DIR/$1/OBJECTS-LIST.TXT
+
+    # Copy LINK-* files, adjusting hard-coded paths in them.
+    for LINK_FILE in "$BUILD_DIR"/LINK-qemu-system-*; do
+        sed \
+            -e 's|'${PREBUILTS_DIR}'|@PREBUILTS_DIR@|g' \
+            -e 's|'${QEMU_ANDROID}'|@SRC_DIR@|g' \
+            "$LINK_FILE" > $INSTALL_DIR/$1/$(basename "$LINK_FILE")
+    done
 
     unset PKG_CONFIG PKG_CONFIG_PATH PKG_CONFIG_LIBDIR SDL_CONFIG
     unset LIBFFI_CFLAGS LIBFFI_LIBS GLIB_CFLAGS GLIB_LIBS
