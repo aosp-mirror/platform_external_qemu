49fe8fb450429d589a6f6177e7d421e6b2bb340b
David 'Digit' Turner <digit@google.com>
2015-11-02 09:48:21 +0100

    Makefile.qemu-launcher.mk: Simplify build script.
    Remove un-needed variable declarations + use LOCAL_C_INCLUDES
    instead of adding -I<path> to LOCAL_CFLAGS.
    
    Change-Id: I7a65afdece93c95fff613bf18457737d319dd12d

--- Makefile.qemu-launcher.mk
+++ Makefile.qemu-launcher.mk
@@ -5,44 +5,44 @@
 ###
 ###
 
-# Check EMULATOR_TARGET_CPU value and determine
+# Check EMULATOR_TARGET_ARCH value and determine
 # corresponding TARGET_XXX macro at the same time
-
-EMULATOR_TARGET_CPU := $(EMULATOR_TARGET_ARCH)
 
 TARGET_VARNAME_arm64  := TARGET_ARM64
 TARGET_VARNAME_mips64 := TARGET_MIPS64
 TARGET_VARNAME_x86_64 := TARGET_X86_64
 TARGET_VARNAME_x86    := TARGET_X86
-ifeq (,$(strip $(TARGET_VARNAME_$(EMULATOR_TARGET_CPU))))
-  $(error Unrecognized EMULATOR_TARGET_CPU value: [$(EMULATOR_TARGET_CPU)])
+ifeq (,$(strip $(TARGET_VARNAME_$(EMULATOR_TARGET_ARCH))))
+  $(error Unrecognized EMULATOR_TARGET_ARCH value: [$(EMULATOR_TARGET_ARCH)])
 endif
 
-qemu_launcher_SOURCES := \
+$(call start-emulator-program,\
+    emulator$(HOST_SUFFIX)-ranchu-$(EMULATOR_TARGET_ARCH))
+
+LOCAL_SRC_FILES := \
     android/qemu-launcher/emulator-qemu.cpp \
     android/cmdline-option.c \
     android/cpu_accelerator.cpp \
     android/help.c \
     android/main-common.c \
 
-qemu_launcher_CFLAGS := \
+LOCAL_CFLAGS := \
     -DNO_SKIN=1 \
-    -D$(TARGET_VARNAME_$(EMULATOR_TARGET_CPU))=1 \
-    -I$(OBJS_DIR)/build
+    -D$(TARGET_VARNAME_$(EMULATOR_TARGET_ARCH))=1 \
 
-qemu_launcher_LDLIBS := $(CXX_STD_LIB)
+LOCAL_C_INCLUDES := $(OBJS_DIR)/build
 
-$(call start-emulator-program, emulator$(HOST_SUFFIX)-ranchu-$(EMULATOR_TARGET_CPU))
-LOCAL_SRC_FILES := $(qemu_launcher_SOURCES)
-LOCAL_CFLAGS := $(qemu_launcher_CFLAGS)
 LOCAL_STATIC_LIBRARIES := \
     emulator-common \
     emulator-libext4_utils \
     emulator-libsparse \
     emulator-libselinux \
     emulator-zlib
-LOCAL_LDLIBS := $(qemu_launcher_LDLIBS)
+
+LOCAL_LDLIBS += $(CXX_STD_LIB)
+
 LOCAL_GENERATE_SYMBOLS := true
+
 $(call gen-hw-config-defs)
 
 ifeq ($(HOST_OS),windows)
