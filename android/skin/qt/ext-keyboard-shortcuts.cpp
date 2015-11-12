1b1861e65b3953f360dd1c168566d7164fc59523
Grigory Dzhavadyan <grigoryj@google.com>
2015-11-11 18:16:53 -0800

    Altering keyboard shortcut behavior
    * Ctrl+G will let the guest system grab all input, effectively switching
      keyboard shortcuts off
    * Ctrl+Alt to "ungrab"
    * All shortcuts will be handled in Qt layer. Old version's "keyset" will
      not be used.
    
    Change-Id: I16ecb5b863b8b64ff3efd2dbb4705a41698185fb

--- android/skin/qt/ext-keyboard-shortcuts.cpp
+++ android/skin/qt/ext-keyboard-shortcuts.cpp
@@ -27,41 +27,7 @@ static void addShortcutsTableRow(QTableWidget* table_widget,
 }
 
 void ExtendedWindow::initKbdShorts() {
-    EmulatorWindow* emu_win = mEmulatorWindow->getEmulatorWindow();
-    SkinKeyset* kset = mEmulatorWindow->getCurrentKeyset(emu_win->ui);
-    SkinKeyBinding bindings[SKIN_KEY_COMMAND_MAX_BINDINGS];
     QTableWidget* table_widget = mExtendedUi->shortcutsTableWidget;
-
-    for (int cmd = SKIN_KEY_COMMAND_NONE + 1;
-         cmd < SKIN_KEY_COMMAND_MAX;
-         ++cmd) {
-        int bindings_count =
-            skin_keyset_get_bindings(
-                kset,
-                static_cast<SkinKeyCommand>(cmd),
-                bindings);
-        if (bindings_count > 0) {
-            QString binding_str;
-            for (int binding_idx = 0; binding_idx < bindings_count; ++binding_idx) {
-                SkinKeyBinding& binding = bindings[binding_idx];
-                binding_str += skin_key_pair_to_string(binding.sym, binding.mod);
-                if (binding_idx < bindings_count - 1) {
-                   binding_str += ",";
-                }
-            }
-            // Unfortunately, string representations of the non-Qt specific
-            // shortcuts use "-" rather than "+", which is used by QKeySequence.
-            // Replace with + so that it appears consistent in the UI. Once we
-            // start handling all keyboard shortcut related things in Qt UI,
-            // this will no longer be necessary.
-            binding_str.replace(QString("-"), QString("+"));
-
-            QString description(skin_key_command_description(static_cast<SkinKeyCommand>(cmd)));
-            addShortcutsTableRow(table_widget, binding_str, description);
-        }
-    }
-
-    // Now add the Qt UI specific keyboard shortcuts
     if (mQtUIShortcuts) {
         for (const auto& key_sequence_and_command : *mQtUIShortcuts) {
             addShortcutsTableRow(table_widget,
