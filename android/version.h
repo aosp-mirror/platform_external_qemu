d4f01c91a14e2fc083a4fa95ff0043c0871f4693
Joshua Lang <joshualang@google.com>
2015-11-23 14:39:44 -0800

    Adding android build number to version defines
    Android build system provides a build number used during crash
    reporting. Build.gradle changes to get buildnumber env var.
    Additional android-configure arg to accept buildnumber and changes to
    Makefiles to add buildnumber to compile flags.
    
    A corresponding change to BuildEmulator.groovy makes use of this change
    during the sdk tools build flow.
    
    Change-Id: I507abc1c90d4662609b9954ef6ad2223140d61f5

--- android/version.h
+++ android/version.h
@@ -21,3 +21,10 @@
 #  define  VERSION_STRING_SHORT  "standalone"
 #  define  VERSION_STRING        VERSION_STRING_SHORT
 #endif
+
+// Define the emulator build number string
+#ifdef ANDROID_SDK_TOOLS_BUILDNUMBER
+#  define EMULATOR_BUILD_STRING STRINGIFY(ANDROID_SDK_TOOLS_BUILDNUMBER)
+#else
+#  define EMULATOR_BUILD_STRING "0"
+#endif
