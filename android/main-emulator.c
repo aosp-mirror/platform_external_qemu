7556788bc706d8ad937b7ee603e497caa30fd5ff
Yurii Zubrytskyi <zyy@google.com>
2015-12-09 13:41:48 -0800

    [Launcher] Allow '-debug-init' to act as '-verbose' in launcher
    In the main emulator '-verbose' is an alias of '-debug-init'.
    This CL makes to to act the same in the laucnher as well
    (emulator[.exe])
    
    Change-Id: I73003d1080ce9107dc67fb3a4f34bdf6b053243f

--- android/main-emulator.c
+++ android/main-emulator.c
@@ -139,7 +139,8 @@ int main(int argc, char** argv)
     /* Parse command-line and look for
      * 1) an avd name either in the form or '-avd <name>' or '@<name>'
      * 2) '-force-32bit' which always use 32-bit emulator on 64-bit platforms
-     * 3) '-verbose', or '-debug-all' or '-debug all' to enable verbose mode.
+     * 3) '-verbose'/'-debug-all'/'-debug all'/'-debug-init'/'-debug init'
+     *    to enable verbose mode.
      */
     int  nn;
     for (nn = 1; nn < argc; nn++) {
@@ -164,12 +165,13 @@ int main(int argc, char** argv)
         if (!strcmp(opt,"-qemu"))
             break;
 
-        if (!strcmp(opt,"-verbose") || !strcmp(opt,"-debug-all")) {
+        if (!strcmp(opt,"-verbose") || !strcmp(opt,"-debug-all")
+                 || !strcmp(opt,"-debug-init")) {
             android_verbose = 1;
         }
 
         if (!strcmp(opt,"-debug") && nn + 1 < argc &&
-            !strcmp(argv[nn + 1], "all")) {
+            (!strcmp(argv[nn + 1], "all") || !strcmp(argv[nn + 1], "init"))) {
             android_verbose = 1;
         }
 
