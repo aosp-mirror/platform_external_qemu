eda4eb26d83736ab124faee34548800521850bcb
David 'Digit' Turner <digit@google.com>
2015-12-09 23:54:52 +0100

    build: Move AndroidEmu unit-tests.
    This moves the AndroidEmu unit tests to Makefile.android-emu.mk
    and also rename them from emulator[64]_unit_tests to
    android_emu[64]_unit_tests.
    
    Change-Id: I0237f413cc0e63d322c4ed63d8ec27c2550751a1

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -324,7 +324,125 @@ ANDROID_EMU_LDLIBS := \
     $(LIBCURL_LDLIBS) \
     $(BREAKPAD_CLIENT_LDLIBS) \
 
-# TODO(digit): Move AndroidEmu unit tests declarations here.
+###############################################################################
+#
+#  android-emu unit tests
+#
+#
+
+$(call start-emulator-program, android_emu$(HOST_SUFFIX)_unittests)
+
+LOCAL_C_INCLUDES += \
+    $(ANDROID_EMU_INCLUDES) \
+    $(EMULATOR_GTEST_INCLUDES) \
+
+LOCAL_LDLIBS += \
+    $(ANDROID_EMU_LDLIBS) \
+
+LOCAL_SRC_FILES := \
+  android/avd/util_unittest.cpp \
+  android/base/containers/HashUtils_unittest.cpp \
+  android/base/containers/PodVector_unittest.cpp \
+  android/base/containers/PointerSet_unittest.cpp \
+  android/base/containers/ScopedPointerSet_unittest.cpp \
+  android/base/containers/StringVector_unittest.cpp \
+  android/base/containers/TailQueueList_unittest.cpp \
+  android/base/EintrWrapper_unittest.cpp \
+  android/base/files/IniFile_unittest.cpp \
+  android/base/files/PathUtils_unittest.cpp \
+  android/base/files/ScopedFd_unittest.cpp \
+  android/base/files/ScopedStdioFile_unittest.cpp \
+  android/base/files/Stream_unittest.cpp \
+  android/base/Log_unittest.cpp \
+  android/base/memory/LazyInstance_unittest.cpp \
+  android/base/memory/MallocUsableSize_unittest.cpp \
+  android/base/memory/ScopedPtr_unittest.cpp \
+  android/base/memory/QSort_unittest.cpp \
+  android/base/misc/HttpUtils_unittest.cpp \
+  android/base/misc/StringUtils_unittest.cpp \
+  android/base/misc/Utf8Utils_unittest.cpp \
+  android/base/sockets/ScopedSocket_unittest.cpp \
+  android/base/sockets/SocketDrainer_unittest.cpp \
+  android/base/sockets/SocketWaiter_unittest.cpp \
+  android/base/String_unittest.cpp \
+  android/base/StringFormat_unittest.cpp \
+  android/base/StringView_unittest.cpp \
+  android/base/synchronization/ConditionVariable_unittest.cpp \
+  android/base/synchronization/Lock_unittest.cpp \
+  android/base/synchronization/MessageChannel_unittest.cpp \
+  android/base/system/System_unittest.cpp \
+  android/base/threads/FunctorThread_unittest.cpp \
+  android/base/threads/Thread_unittest.cpp \
+  android/base/threads/ThreadStore_unittest.cpp \
+  android/base/Uri_unittest.cpp \
+  android/base/Version_unittest.cpp \
+  android/emulation/bufprint_config_dirs_unittest.cpp \
+  android/emulation/ConfigDirs_unittest.cpp \
+  android/emulation/control/LineConsumer_unittest.cpp \
+  android/emulation/CpuAccelerator_unittest.cpp \
+  android/emulation/serial_line_unittest.cpp \
+  android/filesystems/ext4_utils_unittest.cpp \
+  android/filesystems/fstab_parser_unittest.cpp \
+  android/filesystems/partition_config_unittest.cpp \
+  android/filesystems/partition_types_unittest.cpp \
+  android/filesystems/ramdisk_extractor_unittest.cpp \
+  android/filesystems/testing/TestSupport.cpp \
+  android/kernel/kernel_utils_unittest.cpp \
+  android/metrics/metrics_reporter_unittest.cpp \
+  android/metrics/metrics_reporter_ga_unittest.cpp \
+  android/metrics/metrics_reporter_toolbar_unittest.cpp \
+  android/metrics/StudioHelper_unittest.cpp \
+  android/opengl/EmuglBackendList_unittest.cpp \
+  android/opengl/EmuglBackendScanner_unittest.cpp \
+  android/opengl/emugl_config_unittest.cpp \
+  android/opengl/GpuFrameBridge_unittest.cpp \
+  android/qt/qt_path_unittest.cpp \
+  android/qt/qt_setup_unittest.cpp \
+  android/telephony/gsm_unittest.cpp \
+  android/telephony/sms_unittest.cpp \
+  android/update-check/UpdateChecker_unittest.cpp \
+  android/update-check/VersionExtractor_unittest.cpp \
+  android/utils/aconfig-file_unittest.cpp \
+  android/utils/bufprint_unittest.cpp \
+  android/utils/dirscanner_unittest.cpp \
+  android/utils/eintr_wrapper_unittest.cpp \
+  android/utils/file_data_unittest.cpp \
+  android/utils/format_unittest.cpp \
+  android/utils/host_bitness_unittest.cpp \
+  android/utils/path_unittest.cpp \
+  android/utils/property_file_unittest.cpp \
+  android/utils/string_unittest.cpp \
+  android/utils/x86_cpuid_unittest.cpp \
+  android/wear-agent/PairUpWearPhone_unittest.cpp \
+  android/wear-agent/testing/WearAgentTestUtils.cpp \
+  android/wear-agent/WearAgent_unittest.cpp \
+
+ifeq (windows,$(HOST_OS))
+LOCAL_SRC_FILES += \
+  android/base/files/ScopedHandle_unittest.cpp \
+  android/base/files/ScopedRegKey_unittest.cpp \
+  android/base/system/Win32UnicodeString_unittest.cpp \
+  android/base/system/Win32Utils_unittest.cpp \
+  android/utils/win32_cmdline_quote_unittest.cpp \
+  android/windows_installer_unittest.cpp \
+
+else
+LOCAL_SRC_FILES += \
+  android/emulation/nand_limits_unittest.cpp \
+
+endif
+
+LOCAL_CFLAGS += -O0
+
+LOCAL_STATIC_LIBRARIES += \
+    $(ANDROID_EMU_STATIC_LIBRARIES) \
+    emulator-libgtest \
+
+# Link against static libstdc++ on Linux and Windows since the unit-tests
+# cannot pick up our custom versions of the library from $(OBJS_DIR)/lib[64]/
+$(call local-link-static-c++lib)
+
+$(call end-emulator-program)
 
 ###############################################################################
 #
