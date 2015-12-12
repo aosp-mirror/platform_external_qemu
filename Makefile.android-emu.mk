904b5837682428afdf5556603a0b3b1fa477291a
Lingfeng Yang <lfy@google.com>
2015-12-11 16:08:59 -0800

    Android Emulator Bridge (AEB)
    In order to increase the speed file transfers
    between the host and guest,
    we are working on a minimal I/O path.
    
    Previously, this was done through ADB, which
    has its own many levels of indirection (mainly, TCP) and
    concurrent chatter. It can be difficult to get
    ADB to run fast for the emulator.
    
    We propose adding an "Android Emulator Bridge" (AEB)
    which is expressely for handling expensive transactions
    such as "adb install."
    
    This CL must be applied jointly with the qemu-android CL
    
    https://android-review.googlesource.com/#/c/187130/
    
    and guest-side CLs introducing the aebd daemon
    must be applied.
    
    In qemu, this CL adds the main communication logic
    with AEB using qemud. android/aeb.c contains
    the implementation. Currently, there is only one
    function supported: transferring an APK
    and automatically installing it.
    
    This currently only works for QEMU2.
    
    Change-Id: I86061b2837b7e85c907bd87c7c9410252fb17bb8

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -187,6 +187,8 @@ LOCAL_C_INCLUDES := \
 LOCAL_SRC_FILES := \
     android/adb-qemud.c \
     android/adb-server.c \
+    android/android-constants.c \
+    android/android_emulator_bridge.c \
     android/avd/hw-config.c \
     android/avd/info.c \
     android/avd/scanner.c \
