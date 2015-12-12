540d2bbf9a0d63d91160104081ff63a6a0812e2c
Lingfeng Yang <lfy@google.com>
2015-12-11 16:08:59 -0800

    WIP HACK: Android Emulator Bridge (AEB)
    In order to increase the speed of adb install,
    we are working on a different I/O path from host
    to device.
    
    Previously, this was done through ADB, which
    has its own many levels of indirection and
    concurrent chatter. It can be difficult to get
    ADB to run fast for the emulator.
    
    We propose adding an "Android Emulator Bridge" (AEB)
    which is expressely for handling expensive transactions
    such as "adb install."
    
    This CL must be applied jointly with qemu-android CLs
    and guest-side CLs.
    
    In qemu, this CL adds the main communication logic
    with AEB using qemud. android/aeb.c contains
    the implementation. Currently, there is only one
    function supported: transferring an APK
    and automatically installing it.
    
    This currently only works for QEMU2
    as we still need to add the console command for QEMU1
    (and work on the interface in general).
    
    Latest transfer speed: ~250MB/s smp=1, ~450MB/s smp=8
    
    Currently, in order to use "apk install", -selinux permissive
    must be used to boot the emulator. We will fix this soon,
    but would also appreciate code review feedback in the meantime.
    
    RECENT:
    
    1. Removed all qemud stuff.
    2. Start on new protocol.
    
    TODO:
    
    1. Add selinux fixes
    2. Move guest stuff to device/generic/goldfish
    
    Change-Id: I86061b2837b7e85c907bd87c7c9410252fb17bb8

---
+++ android/emulation/control/aeb_agent.h
@@ -0,0 +1,36 @@
+
+// Copyright 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/utils/compiler.h"
+
+#include <stdbool.h>
+
+ANDROID_BEGIN_HEADER
+
+typedef struct QAndroidAEBAgent {
+    // AEBAgent: Communicates with Android Emulator Bridge
+    // to facilitate installation of large APKs.
+    
+    // install: args = filename of apk to install.
+    bool (*install)(const char* args);
+    // push: args = filename of apk to install.
+    // NYI
+    // pull: args = filename of apk to install.
+    // NYI
+} QAndroidAEBAgent;
+
+ANDROID_END_HEADER
