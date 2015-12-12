dd981f027fcc8f7387bbc77b82d699a3c5f13ffa
Lingfeng Yang <lfy@google.com>
2015-12-11 16:08:59 -0800

    WIP HACK: Android Emulator Bridge (AEB)
    In order to increase the speed of adb install,
    we are working on a different I/O path from host
    to device.
    
    Previously, this was done through ADB, which
    has its own many levels of indirection and
    concurrent chatter. It can be difficult to get
    ADB to run fast for the emulator.
    
    We propose adding an "Android Emulator Bridge" (AEB)
    which is expressely for handling expensive transactions
    such as "adb install."
    
    This CL must be applied jointly with qemu-android CLs
    and guest-side CLs.
    
    In qemu, this CL adds the main communication logic
    with AEB using qemud. android/aeb.c contains
    the implementation. Currently, there is only one
    function supported: transferring an APK
    and automatically installing it.
    
    This currently only works for QEMU2
    as we still need to add the console command for QEMU1
    (and work on the interface in general).
    
    Latest transfer speed: ~250MB/s smp=1, ~450MB/s smp=8
    
    Currently, in order to use "apk install", -selinux permissive
    must be used to boot the emulator. We will fix this soon,
    but would also appreciate code review feedback in the meantime.
    
    RECENT:
    
    1. Removed all qemud stuff.
    2. Start on new protocol.
    3. Added pull functionality.
    4. Added shutdown command.
    
    TODO:
    
    1. Add selinux fixes
    2. Move guest stuff to device/generic/goldfish
    
    Change-Id: I86061b2837b7e85c907bd87c7c9410252fb17bb8

---
+++ android-qemu1-glue/qemu-aeb-agent-impl.c
@@ -0,0 +1,47 @@
+// Copyright 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/android_emulator_bridge.h"
+#include "android/emulation/control/aeb_agent.h"
+
+#include "android/utils/debug.h"
+
+static bool shutdown(void) {
+    VERBOSE_PRINT(aeb, "aebAgent shutdown (qemu1)");
+    android_emulator_bridge_shutdown();
+    VERBOSE_PRINT(aeb, "aebAgent shutdown complete (qemu1)");
+    return true;
+}
+
+static bool pull(const char* filename) {
+    VERBOSE_PRINT(aeb, "aebAgent pull (qemu1)");
+    android_emulator_bridge_pull(filename);
+    VERBOSE_PRINT(aeb, "aebAgent install complete (qemu2)");
+    return true;
+}
+
+static bool install(const char* filename) {
+    VERBOSE_PRINT(aeb, "aebAgent install (qemu1)");
+    android_emulator_bridge_install(filename);
+    VERBOSE_PRINT(aeb, "aebAgent install complete (qemu1)");
+    return true;
+}
+
+static const QAndroidAEBAgent aebAgent = {
+  .shutdown = &shutdown,
+  .pull = &pull,
+  .install = &install
+};
+
+const QAndroidAEBAgent* const gQAndroidAEBAgent = &aebAgent;
