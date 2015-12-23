42af71ec805d4dbad250b98212f3b65fd38ba7c0
Yurii Zubrytskyi <zyy@google.com>
2015-12-22 16:46:06 -0800

    [Emulib] Add Uri::FormatEncodeArguments() to encode query string
    For the server to be able to parse the query string we need to
    encode it properly: the parameter delimiters (&) and name-value
    delimiters (=) have to stay unencoded.
    
    Change-Id: Ic9a9e1b66b621edf7e82d9fde5d6200f26ecbc3f

--- android/base/Uri_unittest.cpp
+++ android/base/Uri_unittest.cpp
@@ -52,5 +52,13 @@ TEST(Uri, Decode) {
                          .c_str());
 }
 
+TEST(Uri, FormatEncodeArguments) {
+    EXPECT_STREQ("", Uri::FormatEncodeArguments("").c_str());
+    EXPECT_STREQ("aa=b", Uri::FormatEncodeArguments("%s=%s", "aa", "b").c_str());
+    EXPECT_STREQ("1=2&%26=%3D", Uri::FormatEncodeArguments("%d=%u&%s=%s",
+                                                           1, 2u, "&", "=").c_str());
+}
+
+
 }  // namespace base
 }  // namespace android
