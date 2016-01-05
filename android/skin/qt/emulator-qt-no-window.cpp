9cf78d5dc28f00a58f63112f808bc690767b4833
Konstantinos Menychtas <kmenychtas@google.com>
2016-01-04 17:56:35 -0800

    Adding -no-window support in the Android Emulator
    This patch adds -no-window support in the Android Emulator.
    The no-window option requires that a GUI-less QT application
    (QCoreApplication) be built, so some QT features like font
    support are disabled and a QTWidget-less version of the
    emulator-qt-window is created (defined in emulator-qt-no-window).
    
    A separate patch in qemu-android enables support for this option
    in the qemu2 engine.
    
    Change-Id: I16014e45327f836d574e332e29f2852ada7a02d5

---
+++ android/skin/qt/emulator-qt-no-window.cpp
@@ -0,0 +1,94 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/skin/qt/emulator-qt-no-window.h"
+
+#include <QObject>
+#include <QThread>
+#include <QtCore>
+#include <QCoreApplication>
+
+#include "android/base/memory/LazyInstance.h"
+
+#define  DEBUG 0
+
+#if DEBUG
+#define  D(...)   qDebug(__VA_ARGS__)
+#else
+#define  D(...)   ((void)0)
+#endif
+
+static android::base::LazyInstance<EmulatorQtNoWindow::Ptr> sNoWindowInstance = LAZY_INSTANCE_INIT;
+
+Task::Task(std::function<void ()> f) : fptr(f)
+{
+}
+
+void Task::run()
+{
+    fptr();
+    D("Task finished, notify connected slots");
+    emit finished();
+}
+
+/******************************************************************************/
+
+void EmulatorQtNoWindow::create()
+{
+    sNoWindowInstance.get() = Ptr(new EmulatorQtNoWindow());
+}
+
+EmulatorQtNoWindow::EmulatorQtNoWindow(QObject *parent)
+{
+    QObject::connect(QCoreApplication::instance(),&QCoreApplication::aboutToQuit,this,&EmulatorQtNoWindow::slot_clearInstance);
+}
+
+EmulatorQtNoWindow::Ptr EmulatorQtNoWindow::getInstancePtr()
+{
+    return sNoWindowInstance.get();
+}
+
+EmulatorQtNoWindow* EmulatorQtNoWindow::getInstance()
+{
+    return getInstancePtr().get();
+}
+
+void EmulatorQtNoWindow::startThread(std::function<void ()> f)
+{
+    auto thread = new QThread();
+    auto task = new Task(f);
+
+    // pass task object to thread and start task when thread starts
+    task->moveToThread(thread);
+    connect(thread, SIGNAL(started()),  task,  SLOT(run()));
+    // when the task is finished, signal the thread to quit
+    connect(task, SIGNAL(finished()), thread, SLOT(quit()));
+    // queue up task object for deletion when the thread is finished
+    connect(thread, SIGNAL(finished()), task,  SLOT(deleteLater()));
+    // queue up thread object for deletion when the thread is finished
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+    // when the thread is finished, quit this GUI-less window too
+    connect(thread, SIGNAL(finished()), getInstance(), SLOT(slot_finished()));
+
+    thread->start();
+}
+
+void EmulatorQtNoWindow::slot_clearInstance()
+{
+    sNoWindowInstance.get().reset();
+}
+
+void EmulatorQtNoWindow::slot_finished()
+{
+    D("Closing GUI-less window, quiting application");
+    QCoreApplication::instance()->quit();
+}
