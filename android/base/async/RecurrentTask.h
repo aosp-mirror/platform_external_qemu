04baebe3fa6457c2cbbf0dc338fc20936a06368b
Prathmesh Prabhu <pprabhu@google.com>
2016-01-14 18:07:50 -0800

    [Emulib] Add RecurrentTask and simplify ParallelTask.
    RecurrentTask is a new generic class to run a given function repeatedly
    on the event loop until done. A class that wants to use it registers a
    method with the object. That function then gets called repeatedly (in
    the object's context) until it returns false.
    
    This CL also simplifies ParallelTask. ParallelTask now also accepts
    std::function parameters to call, and calls these methods in a context.
    A generic function is added in case you wish to use free functions in a
    parallel task.
    
    Updates unittests for ParallelTask, and adds unittests for
    RecurrentTask.
    
    Change-Id: I32d03b74fbfd43cc997ae7f308fb8a6a221a6ca7

---
+++ android/base/async/RecurrentTask.h
@@ -0,0 +1,91 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/base/async/Looper.h"
+#include "android/base/Compiler.h"
+
+#include <functional>
+#include <memory>
+
+namespace android {
+namespace base {
+
+// A RecurrentTask is an object that allows you to run a task repeatedly on the
+// event loop, until you're done.
+// Example:
+//
+//     class AreWeThereYet {
+//     public:
+//         AreWeThereYet(Looper* looper) :
+//                 mAskRepeatedly(looper,
+//                                std::bind(&AreWeThereYet::askAgain),
+//                                1 * 60 * 1000) {}
+//
+//         void askAgain() {
+//             std::cout << "Are we there yet?" << std::endl;
+//         }
+//
+//         void startHike() {
+//             mAskRepeatedly.start();
+//         }
+//
+//     private:
+//         RecurrentTask mAskRepeatedly;
+//     };
+//
+// Note: RecurrentTask is meant to execute a task __on the current thread__.
+// It is not thread safe.
+class RecurrentTask {
+public:
+    using TaskFunction = std::function<bool()>;
+
+    RecurrentTask(Looper* looper,
+                     TaskFunction function,
+                     Looper::Duration taskIntervalMs) :
+        mLooper(looper), mFunction(function), mTaskIntervalMs(taskIntervalMs),
+        mTimer(mLooper->createTimer(&RecurrentTask::taskCallback, this)) {}
+
+    ~RecurrentTask() {
+        stop();
+    }
+
+    void start() {
+        mTimer->startRelative(mTaskIntervalMs);
+    }
+
+    void stop() {
+        if(mTimer) {
+            mTimer->stop();
+        }
+    }
+
+protected:
+    static void taskCallback(void* opaqueThis, Looper::Timer* timer) {
+        auto thisPtr = static_cast<RecurrentTask*>(opaqueThis);
+        if (thisPtr->mFunction()) {
+            thisPtr->mTimer->startRelative(thisPtr->mTaskIntervalMs);
+        }
+    }
+
+private:
+    Looper* mLooper;
+    TaskFunction mFunction;
+    Looper::Duration mTaskIntervalMs;
+    std::unique_ptr<Looper::Timer> mTimer;
+};
+
+}  // namespace base
+}  // namespace android
