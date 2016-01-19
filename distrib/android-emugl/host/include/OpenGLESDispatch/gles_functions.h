839940db6bc1923c8ad110c36462b65f4985341e
David 'Digit' Turner <digit@google.com>
2016-01-19 13:00:14 +0100

    emugl: Make glGetStringi() a GLESv3 extension to avoid compiler warning.
    Change-Id: Idae2043d242f63c1e82fa5587847153c18e7adad

--- distrib/android-emugl/host/include/OpenGLESDispatch/gles_functions.h
+++ distrib/android-emugl/host/include/OpenGLESDispatch/gles_functions.h
@@ -9,6 +9,11 @@
 #include "OpenGLESDispatch/gles2_extensions_functions.h"
 #include "OpenGLESDispatch/gles3_only_functions.h"
 
+// As a special case, LIST_GLES3_ONLY_FUNCTIONS below uses the Y parameter
+// instead of the X one, meaning that the corresponding functions are
+// optional extensions. This is only because currently, the only GLESv3
+// API we support is glGetStringi(), which is not always provided by
+// host desktop GL drivers (though most do).
 #define LIST_GLES_FUNCTIONS(X,Y) \
     LIST_GLES_COMMON_FUNCTIONS(X) \
     LIST_GLES_EXTENSIONS_FUNCTIONS(Y) \
@@ -16,6 +21,6 @@
     LIST_GLES1_EXTENSIONS_FUNCTIONS(Y) \
     LIST_GLES2_ONLY_FUNCTIONS(X) \
     LIST_GLES2_EXTENSIONS_FUNCTIONS(Y) \
-    LIST_GLES3_ONLY_FUNCTIONS(X) \
+    LIST_GLES3_ONLY_FUNCTIONS(Y) \
 
 #endif  // GLES_FUNCTIONS_H
