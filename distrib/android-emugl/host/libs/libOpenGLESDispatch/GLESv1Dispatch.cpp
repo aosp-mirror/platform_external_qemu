f66c898d738b54407405584bda18e5d0f22e0858
Lingfeng Yang <lfy@google.com>
2016-01-26 09:40:13 -0800

    [DNR][ANGLE] Remove GLESv1, add fine-grained err logs
    new:
    
    - Fine-Grained Error Logging in libopenglrender
    - uses opengl logging class
    
    Change-Id: I6036f4a296659abe4d35cddf48d3d94c3cff22a6

--- distrib/android-emugl/host/libs/libOpenGLESDispatch/GLESv1Dispatch.cpp
+++ distrib/android-emugl/host/libs/libOpenGLESDispatch/GLESv1Dispatch.cpp
@@ -37,26 +37,54 @@ static void gles1_unimplemented() {
 
 #define DEFAULT_GLES_CM_LIB EMUGL_LIBNAME("GLES_CM_translator")
 
+#define RETURN_void return
+#define RETURN_GLboolean return GL_FALSE
+#define RETURN_GLint return 0
+#define RETURN_GLuint return 0U
+#define RETURN_GLenum return 0
+#define RETURN_int return 0
+#define RETURN_GLconstubyteptr return NULL
+
+#define RETURN_(x)  RETURN_ ## x
+
+#ifdef _WIN32
+
+#define DEFINE_DUMMY_FUNCTION(return_type, func_name, signature, args) \
+static return_type  __stdcall gles1_dummy_##func_name signature { \
+    fprintf(stderr, "dummygles1!%s\n", #func_name); \
+    RETURN_(return_type); \
+}
+
+#define DEFINE_DUMMY_EXTENSION_FUNCTION(return_type, func_name, signature, args) \
+static return_type __stdcall gles1_dummy_##func_name signature { \
+    fprintf(stderr, "dummygles1_ext!%s\n", #func_name); \
+    RETURN_(return_type); \
+}
+
+#else
+
+#define DEFINE_DUMMY_FUNCTION(return_type, func_name, signature, args) \
+static return_type gles1_dummy_##func_name signature { \
+    fprintf(stderr, "dummygles1!%s\n", #func_name); \
+    RETURN_(return_type); \
+}
+
+#define DEFINE_DUMMY_EXTENSION_FUNCTION(return_type, func_name, signature, args) \
+static return_type gles1_dummy_##func_name signature { \
+    fprintf(stderr, "dummygles1_ext!%s\n", #func_name); \
+    RETURN_(return_type); \
+}
+
+#endif
+
+LIST_GLES1_FUNCTIONS(DEFINE_DUMMY_FUNCTION, DEFINE_DUMMY_EXTENSION_FUNCTION)
+
 bool gles1_dispatch_init(GLESv1Dispatch* dispatch_table) {
-    const char* libName = getenv("ANDROID_GLESv1_LIB");
-    if (!libName) {
-        libName = DEFAULT_GLES_CM_LIB;
-    }
-
-    char error[256];
-    s_gles1_lib = emugl::SharedLibrary::open(libName, error, sizeof(error));
-    if (!s_gles1_lib) {
-        fprintf(stderr, "%s: Could not load %s [%s]\n", __FUNCTION__,
-                libName, error);
-        return false;
-    }
-
     //
     // init the GLES dispatch table
     //
 #define LOOKUP_SYMBOL(return_type,function_name,signature,callargs) \
-    dispatch_table-> function_name = reinterpret_cast< function_name ## _t >( \
-            s_gles1_lib->findSymbol(#function_name));
+    dispatch_table-> function_name = gles1_dummy_##function_name;
 
     LIST_GLES1_FUNCTIONS(LOOKUP_SYMBOL,LOOKUP_SYMBOL)
 
