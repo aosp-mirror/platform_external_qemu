b40cb774ec867c65951531b368123354d4bf4bdb
Lingfeng Yang <lfy@google.com>
2016-02-03 10:27:55 -0800

    Report crashes on exit differently.
    We'd like to
    
    a) explicitly distinguish crashes on exit from other crashes
    b) bother the user less, using a checkbox for sending further
    reports automatically, which is set to be checked by default.
    
    RELEASE NOTE: Added an option to send crash reports automatically after
    the first one.
    
    Change-Id: Ifced3d437cb101cdbc0bae7de4b94bddc2ba0d2e

--- android/crashreport/ui/ConfirmDialog.h
+++ android/crashreport/ui/ConfirmDialog.h
@@ -10,8 +10,10 @@
 ** GNU General Public License for more details.
 */
 
+#include "android/crashreport/CrashReporter.h"
 #include "android/crashreport/CrashService.h"
 
+#include <QCheckBox>
 #include <QDialogButtonBox>
 #include <QFontDatabase>
 #include <QGridLayout>
@@ -40,6 +42,7 @@ private:
     QPlainTextEdit* mDetailsText;
     QLabel* mProgressText;
     QProgressBar* mProgress;
+    QCheckBox* mExitCrashCheckBox;
 
     QLabel* mSuggestionText;
 
@@ -51,6 +54,8 @@ private:
     bool mDetailsHidden;
     bool mDidGetSysInfo;
     bool mDidUpdateDetails;
+    bool mIsExitCrash;
+    bool mQuietMode;
     void disableInput();
     void enableInput();
     void showProgressBar(const std::string& msg);
@@ -63,7 +68,9 @@ private:
 
 public:
     ConfirmDialog(QWidget* parent,
-                  android::crashreport::CrashService* crashservice);
+                  android::crashreport::CrashService* crashservice,
+                  bool isExitCrash,
+                  bool quietMode);
     bool didGetSysInfo() const;
 
     QString getUserComments();
