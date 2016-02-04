800ec203916e83e42e7bc7c965ba3a8eaf1a89d2
Evan Birenbaum <birenbaum@google.com>
2016-02-04 11:09:30 -0800

    [ui] Cleaning up EmulatorQtWindow.
    Change the following from inner classes to full classes:
    - EmulatorContainer - the QScrollArea that contains the emulator.
      This both acts as the top-level widget (and therefore the window
      of the application), and provides the viewport for zooming.
    - EmulatorOverlay - the invisible QFrame used for the flash of a
      screenshot, the multitouch UI, and the zoom selection box UI.
    
    Also migrates the appropriate slot/signal functionality to these
    two new classes, since QMetaObject functionality is not allowed
    in inner classes.
    
    Change-Id: Iceaa9c31d1c624e61844bfcd580492b6b5b6efe7

---
+++ android/skin/qt/emulator-container.h
@@ -0,0 +1,51 @@
+/* Copyright (C) 2016 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#pragma once
+
+#include <QtCore>
+#include <QObject>
+#include <QScrollArea>
+#include <QTimer>
+#include <QWidget>
+
+class EmulatorQtWindow;
+
+class EmulatorContainer : public QScrollArea
+{
+    Q_OBJECT
+
+public:
+    explicit EmulatorContainer(EmulatorQtWindow *window);
+    virtual ~EmulatorContainer();
+
+    void closeEvent(QCloseEvent *event);
+    bool event(QEvent *e);
+    void focusInEvent(QFocusEvent *event);
+    void hideEvent(QHideEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void moveEvent(QMoveEvent *event);
+    void resizeEvent(QResizeEvent *event);
+    void showEvent(QShowEvent *event);
+
+    void stopResizeTimer();
+    QSize viewportSize() const;
+
+private slots:
+    void slot_resizeDone();
+
+private:
+    EmulatorQtWindow *mEmulatorWindow;
+    QList<QEvent::Type> mEventBuffer;
+    QTimer mResizeTimer;
+};
