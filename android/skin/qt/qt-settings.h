03a9b98d1efbe64eebc088fb65ac6fd9d8eb9e80
Lingfeng Yang <lfy@google.com>
2016-02-04 13:57:48 -0800

    Save 'save report prefs' checkbox.
    Change-Id: Ie1a4a844d378bc3f3b32d6d4fad53844edaf1376

--- android/skin/qt/qt-settings.h
+++ android/skin/qt/qt-settings.h
@@ -17,35 +17,36 @@
 #include <QString>
 
 namespace Ui {
-    namespace Settings {
+namespace Settings {
 
-        const QString ORG_NAME("Android Open Source Project");
-        const QString ORG_DOMAIN("android.com");
-        const QString APP_NAME("Emulator");
+const QString ORG_NAME("Android Open Source Project");
+const QString ORG_DOMAIN("android.com");
+const QString APP_NAME("Emulator");
 
-        const QString SHOW_AVD_ARCH_WARNING("showAvdArchWarning");
+const QString SHOW_AVD_ARCH_WARNING("showAvdArchWarning");
 
-        // Note: The "set/" here is because these settings are
-        //       associated with "settings-page", not because
-        //       we're in "namespace Settings".
+// Note: The "set/" here is because these settings are
+//       associated with "settings-page", not because
+//       we're in "namespace Settings".
 
-        const QString ALLOW_KEYBOARD_GRAB("set/allow_keyboard_grab");
-        const QString ALWAYS_ON_TOP("set/alwaysOnTop");
-        const QString SAVE_PATH("set/savePath");
-        const QString SDK_PATH("set/sdkPath");
-        const QString UI_THEME("set/theme");
+const QString ALLOW_KEYBOARD_GRAB("set/allow_keyboard_grab");
+const QString ALWAYS_ON_TOP("set/alwaysOnTop");
+const QString SAVE_PATH("set/savePath");
+const QString SDK_PATH("set/sdkPath");
+const QString UI_THEME("set/theme");
 
-        const QString CRASHREPORT_PREFERENCE("set/crashReportPreference");
-        enum CRASHREPORT_PREFERENCE_VALUE {
-            CRASHREPORT_PREFERENCE_ASK = 0,
-            CRASHREPORT_PREFERENCE_ALWAYS = 1,
-            CRASHREPORT_PREFERENCE_NEVER = 2
-        };
+const QString CRASHREPORT_PREFERENCE("set/crashReportPreference");
+enum CRASHREPORT_PREFERENCE_VALUE {
+    CRASHREPORT_PREFERENCE_ASK = 0,
+    CRASHREPORT_PREFERENCE_ALWAYS = 1,
+    CRASHREPORT_PREFERENCE_NEVER = 2
+};
+const QString CRASHREPORT_SAVEPREFERENCE_CHECKED("set/crashReportSavePreferenceChecked");
 
-        const QString LOCATION_LATITUDE("loc/latitude");
-        const QString LOCATION_LONGITUDE("loc/longitude");
-        const QString LOCATION_ALTITUDE("loc/altitude");
-        const QString LOCATION_PLAYBACK_FILE("loc/playback_file_path");
-        const QString LOCATION_PLAYBACK_SPEED("loc/playback_speed");
-    }
+const QString LOCATION_LATITUDE("loc/latitude");
+const QString LOCATION_LONGITUDE("loc/longitude");
+const QString LOCATION_ALTITUDE("loc/altitude");
+const QString LOCATION_PLAYBACK_FILE("loc/playback_file_path");
+const QString LOCATION_PLAYBACK_SPEED("loc/playback_speed");
+}
 }
