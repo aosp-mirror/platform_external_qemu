0ca4ee409c5c36e34fe78fd983495b3ea12ff4a8
Grigory Dzhavadyan <grigoryj@google.com>
2016-02-08 10:16:04 -0800

    [UI] Refactor the way emulator handles Qt stylesheets
    This change separates the UI stylesheet into a standalone
    editable file, which is easier to work with than the giant macro.
    
    Change-Id: I6c2402bd12449adf3d0f095a7a98a205ac3f8734

---
+++ android/skin/qt/stylesheet.h
@@ -0,0 +1,36 @@
+//Copyright (C) 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/settings-agent.h"
+#include <QString>
+#include <QHash>
+
+namespace Ui {
+
+// Returns a string containing the style sheet for the
+// given UI theme.
+const QString& stylesheetForTheme(SettingsTheme theme);
+
+// Retuns a string containing the style sheet for fonts.
+// |hi_density| should be true for Retina displays.
+const QString& fontStylesheet(bool hi_density);
+
+// Returns a mapping of stylesheet template variable names to their
+// values in a given theme.
+const QHash<QString, QString>& stylesheetValues(SettingsTheme theme);
+
+extern const char* THEME_PATH_VAR;
+extern const char* MAJOR_TAB_COLOR_VAR;
+extern const char* TAB_BKG_COLOR_VAR;
+extern const char* TAB_SELECTED_COLOR_VAR;
+}
