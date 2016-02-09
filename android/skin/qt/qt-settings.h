2809e1bb525d6ecd7cdb1c5ef91514851722360b
Evan Birenbaum <birenbaum@google.com>
2016-02-08 16:44:43 -0800

    [ui] Change "SDK Location" to "Backup ADB".
    The SDK location setting is only used to find ADB, and setting it
    improperly can break some emulator functionality (screenshot, APK
    install, file copy).
    
    Split this setting into two:
    1. "Auto-detect ADB" - the emulator will find ADB on it's own.
    2. "Custom ADB Path" - if the above is off, let the user direct the
    emulator to an ADB executable (needed for those who use the emulator
    as a standalone and only download the tools/).
    
    Also, warn the user if we can't auto-find ADB.
    
    Change-Id: I5d8223df573cf696509b7bd8b87a9546f7f110e5

--- android/skin/qt/qt-settings.h
+++ android/skin/qt/qt-settings.h
@@ -29,10 +29,11 @@ const QString SHOW_AVD_ARCH_WARNING("showAvdArchWarning");
 //       associated with "settings-page", not because
 //       we're in "namespace Settings".
 
-const QString ALLOW_KEYBOARD_GRAB("set/allow_keyboard_grab");
+const QString ADB_PATH("set/adbPath");
+const QString ALLOW_KEYBOARD_GRAB("set/allowKeyboardGrab");
+const QString AUTO_FIND_ADB("set/autoFindAdb");
 const QString ALWAYS_ON_TOP("set/alwaysOnTop");
 const QString SAVE_PATH("set/savePath");
-const QString SDK_PATH("set/sdkPath");
 const QString UI_THEME("set/theme");
 
 const QString CRASHREPORT_PREFERENCE("set/crashReportPreference");
