17123648616fecc4f32986fa15be4093bf7d2044
Miodrag Dinic <miodrag.dinic@imgtec.com>
2016-02-10 16:13:32 +0100

    android_mips.c: Change default cpu to 74Kf
    Now that QEMU has Mips DSP support, we should default
    to a CPU that has DSP/DSPR2 support.
    
    Change-Id: Ie74a0eaf8be7ac446a5c69a442a400777db739cb
    Signed-off-by: Miodrag Dinic <miodrag.dinic@imgtec.com>

--- hw/android/android_mips.c
+++ hw/android/android_mips.c
@@ -169,7 +169,7 @@ static void android_mips_init_(ram_addr_t ram_size,
     ram_addr_t ram_offset;
 
     if (!cpu_model)
-        cpu_model = "24Kf";
+        cpu_model = "74Kf";
 
     env = cpu_init(cpu_model);
 
