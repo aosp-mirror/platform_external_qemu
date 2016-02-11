3f26697c7efa6c3a5147b0123dbc5636cd1c1baf
Yurii Zubrytskyi <zyy@google.com>
2016-02-10 18:20:25 -0800

    [Win32] Improve the Win32UnicodeString class
    - Fixed a bug in .data() which allowed it to return null
    - Fixed several places where null could've been passed into
      the memcpy()
    - Improved the resize() - it doesn't reallocate when unnecessary
    - Created a new set of functions for encoding conversion with
      buffers as parameters. convert[To/From]Utf8() now can be called
      with staticly allocated buffer - which allows one to use them
      in memory-critical context (e.g., during OOM exception handling)
    
    Change-Id: I0d624a738f7c425602d488360e5e5d05333dfe57

--- android/base/system/Win32UnicodeString.cpp
+++ android/base/system/Win32UnicodeString.cpp
@@ -27,12 +27,12 @@ Win32UnicodeString::Win32UnicodeString() : mStr(nullptr), mSize(0u) {}
 
 Win32UnicodeString::Win32UnicodeString(const char* str, size_t len)
     : mStr(nullptr), mSize(0u) {
-    reset(str, len);
+    reset(StringView(str, len));
 }
 
 Win32UnicodeString::Win32UnicodeString(StringView str)
     : mStr(nullptr), mSize(0u) {
-    reset(str.c_str(), str.size());
+    reset(str);
 }
 
 Win32UnicodeString::Win32UnicodeString(size_t size) : mStr(nullptr), mSize(0u) {
@@ -49,7 +49,7 @@ Win32UnicodeString::Win32UnicodeString(const wchar_t* str)
 Win32UnicodeString::Win32UnicodeString(const Win32UnicodeString& other)
     : mStr(nullptr), mSize(0u) {
     resize(other.mSize);
-    ::memcpy(mStr, other.mStr, other.mSize * sizeof(wchar_t));
+    ::memcpy(mStr, other.c_str(), other.mSize * sizeof(wchar_t));
 }
 
 Win32UnicodeString::~Win32UnicodeString() {
@@ -59,7 +59,7 @@ Win32UnicodeString::~Win32UnicodeString() {
 Win32UnicodeString& Win32UnicodeString::operator=(
         const Win32UnicodeString& other) {
     resize(other.mSize);
-    ::memcpy(mStr, other.mStr, other.mSize * sizeof(wchar_t));
+    ::memcpy(mStr, other.c_str(), other.mSize * sizeof(wchar_t));
     return *this;
 }
 
@@ -67,12 +67,15 @@ Win32UnicodeString& Win32UnicodeString::operator=(const wchar_t* str) {
     size_t len = str ? wcslen(str) : 0u;
     resize(len);
     ::memcpy(mStr, str ? str : L"", len * sizeof(wchar_t));
+    return *this;
 }
 
 wchar_t* Win32UnicodeString::data() {
     if (!mStr) {
         // Ensure the function never returns NULL.
-        resize(0u);
+        // it is safe to const_cast the pointer here - user isn't allowed to
+        // write into it anyway
+        return const_cast<wchar_t*>(L"");
     }
     return mStr;
 }
@@ -85,31 +88,35 @@ void Win32UnicodeString::reset(const char* str, size_t len) {
     if (mStr) {
         delete[] mStr;
     }
-    int utf16Len = MultiByteToWideChar(CP_UTF8,  // CodePage
-                                       0,        // dwFlags
-                                       str,      // lpMultiByteStr
-                                       len,      // cbMultiByte
-                                       NULL,     // lpWideCharStr
-                                       0);       // cchWideChar
-    mStr = new wchar_t[utf16Len + 1u];
+    const int utf16Len = calcUtf16BufferLength(str, len);
+    mStr = new wchar_t[utf16Len + 1];
     mSize = static_cast<size_t>(utf16Len);
-    MultiByteToWideChar(CP_UTF8, 0, str, len, mStr, utf16Len);
+    convertFromUtf8(mStr, utf16Len, str, len);
     mStr[mSize] = L'\0';
 }
 
-void Win32UnicodeString::reset(const String& str) {
+void Win32UnicodeString::reset(StringView str) {
     reset(str.c_str(), str.size());
 }
 
 void Win32UnicodeString::resize(size_t newSize) {
-    wchar_t* oldStr = mStr;
-    mStr = new wchar_t[newSize + 1u];
-    size_t copySize = std::min(newSize, mSize);
-    ::memcpy(mStr, oldStr, copySize * sizeof(wchar_t));
-    mStr[copySize] = L'\0';
-    mStr[newSize] = L'\0';
-    mSize = newSize;
-    delete[] oldStr;
+    if (newSize == 0) {
+        delete [] mStr;
+        mStr = nullptr;
+        mSize = 0;
+    } else if (newSize <= mSize) {
+        mStr[newSize] = 0;
+        mSize = newSize;
+    } else {
+        wchar_t* oldStr = mStr;
+        mStr = new wchar_t[newSize + 1u];
+        size_t copySize = std::min(newSize, mSize);
+        ::memcpy(mStr, oldStr ? oldStr : L"", copySize * sizeof(wchar_t));
+        mStr[copySize] = L'\0';
+        mStr[newSize] = L'\0';
+        mSize = newSize;
+        delete[] oldStr;
+    }
 }
 
 void Win32UnicodeString::append(const wchar_t* str) {
@@ -119,6 +126,9 @@ void Win32UnicodeString::append(const wchar_t* str) {
 void Win32UnicodeString::append(const wchar_t* str, size_t len) {
     // NOTE: This method should be rarely used, so don't try to optimize
     // storage with larger capacity values and exponential increments.
+    if (!str || !len) {
+        return;
+    }
     size_t oldSize = size();
     resize(oldSize + len);
     memmove(mStr + oldSize, str, len * sizeof(wchar_t));
@@ -130,33 +140,107 @@ void Win32UnicodeString::append(const Win32UnicodeString& other) {
 
 wchar_t* Win32UnicodeString::release() {
     wchar_t* result = mStr;
-    mStr = NULL;
+    mStr = nullptr;
     mSize = 0u;
     return result;
 }
 
 // static
-String Win32UnicodeString::convertToUtf8(const wchar_t* str) {
-    return convertToUtf8(str, wcslen(str));
+String Win32UnicodeString::convertToUtf8(const wchar_t* str, int len) {
+    String result;
+    const int utf8Len = calcUtf8BufferLength(str, len);
+    if (utf8Len > 0) {
+        result.resize(static_cast<size_t>(utf8Len));
+        convertToUtf8(&result[0], utf8Len, str, len);
+        if (len == -1) {
+            result.resize(utf8Len - 1);  // get rid of the null-terminator
+        }
+    }
+    return result;
+}
+
+// returns the return value of a Win32UnicodeString public conversion function
+// from a WinAPI conversion function returned code
+static int convertRetVal(int winapiResult) {
+    return winapiResult ? winapiResult : -1;
 }
 
 // static
-String Win32UnicodeString::convertToUtf8(const wchar_t* str, size_t len) {
-    String result;
-    int utf8Len = WideCharToMultiByte(CP_UTF8,  // CodePage
-                                      0,        // dwFlags
-                                      str,      // lpWideCharStr
-                                      len,      // cchWideChar
-                                      NULL,     // lpMultiByteStr
-                                      0,        // cbMultiByte
-                                      NULL,     // lpDefaultChar
-                                      NULL);    // lpUsedDefaultChar
-    if (utf8Len > 0) {
-        result.resize(static_cast<size_t>(utf8Len));
-        WideCharToMultiByte(CP_UTF8, 0, str, len, &result[0], utf8Len, NULL,
-                            NULL);
+int Win32UnicodeString::calcUtf8BufferLength(const wchar_t* str, int len) {
+    if (len < 0 && len != -1) {
+        return -1;
     }
-    return result;
+    if (len == 0) {
+        return 0;
+    }
+    const int utf8Len = WideCharToMultiByte(CP_UTF8,  // CodePage
+                                            0,        // dwFlags
+                                            str,      // lpWideCharStr
+                                            len,      // cchWideChar
+                                            nullptr,  // lpMultiByteStr
+                                            0,        // cbMultiByte
+                                            nullptr,  // lpDefaultChar
+                                            nullptr); // lpUsedDefaultChar
+
+    return convertRetVal(utf8Len);
+}
+
+// static
+int Win32UnicodeString::calcUtf16BufferLength(const char* str, int len) {
+    if (len < 0 && len != -1) {
+        return -1;
+    }
+    if (len == 0) {
+        return 0;
+    }
+    const int utf16Len = MultiByteToWideChar(CP_UTF8,  // CodePage
+                                             0,        // dwFlags
+                                             str,      // lpMultiByteStr
+                                             len,      // cbMultiByte
+                                             nullptr,  // lpWideCharStr
+                                             0);       // cchWideChar
+
+    return convertRetVal(utf16Len);
+}
+
+// static
+int Win32UnicodeString::convertToUtf8(char* outStr, int outLen,
+                                      const wchar_t* str, int len) {
+    if (!outStr || outLen < 0 || !str || (len < 0 && len != -1)) {
+        return -1;
+    }
+    if (len == 0) {
+        return 0;
+    }
+
+    const int utf8Len = WideCharToMultiByte(CP_UTF8,  // CodePage
+                                            0,        // dwFlags
+                                            str,      // lpWideCharStr
+                                            len,      // cchWideChar
+                                            outStr,   // lpMultiByteStr
+                                            outLen,   // cbMultiByte
+                                            nullptr,  // lpDefaultChar
+                                            nullptr); // lpUsedDefaultChar
+    return convertRetVal(utf8Len);
+}
+
+// static
+int Win32UnicodeString::convertFromUtf8(wchar_t* outStr, int outLen,
+                                        const char* str, int len) {
+    if (!outStr || outLen < 0 || !str || (len < 0 && len != -1)) {
+        return -1;
+    }
+    if (len == 0) {
+        return 0;
+    }
+
+    const int utf16Len = MultiByteToWideChar(CP_UTF8,  // CodePage
+                                             0,        // dwFlags
+                                             str,      // lpMultiByteStr
+                                             len,      // cbMultiByte
+                                             outStr,   // lpWideCharStr
+                                             outLen);  // cchWideChar
+    return convertRetVal(utf16Len);
 }
 
 }  // namespace base
