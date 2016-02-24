594f2723b7c8376661d819584973957b7c76d4ae
Konstantinos Menychtas <kmenychtas@google.com>
2016-02-23 17:47:42 -0800

    Emulator input event replay extension
    This patch should make it possible to replay captured
    input events to the android emulator widgets.
    Some care has been taken to make this feature
    use-able through a UI panel, but command-line
    options must be used instead for now:
    $ emulator -input-record </path/to/file>
    $ emulator -input-replay </path/to/file> -replay-delay <seconds>
    
    This is a follow-up to (now abandoned)
    c/202102/ (Main emulator window input event record/replay),
    written from scratch with utilities from
    c/202501 ([UI] Add a mechanism for recording UI events).
    Slight changes to the UI event recording mechanism
    are included in this change (e.g. format of event-recorder
    strings saved to file).
    
    TODO: Extend de-serializer beyond mouse events
    and make applicable to other widgets besides main
    window (currently only main window mouse events are
    captured).
    
    Change-Id: I20bf4e460a54e623729bc6fdc07cdeb3f65fd658

--- android/skin/qt/ui-event-recorder.h
+++ android/skin/qt/ui-event-recorder.h
@@ -18,11 +18,17 @@
 #include <QCloseEvent>
 #include <QHideEvent>
 #include <QFocusEvent>
+#include <QMetaEnum>
 #include <QMouseEvent>
 #include <QResizeEvent>
+#include <QString>
+#include <QTime>
 
 #include <cassert>
+#include <iostream>
 #include <memory>
+#include <string>
+#include <sstream>
 #include <unordered_map>
 
 using android::base::enable_if_convertible;
@@ -30,7 +36,21 @@ using android::base::enable_if_convertible;
 struct EventRecord {
     std::unique_ptr<QEvent> event;
     std::string target_name;
+    int dt;
+
+    inline std::string to_string() const {
+        static int event_enum_index =
+                QEvent::staticMetaObject.indexOfEnumerator("Type");
+        std::ostringstream str;
+        str << dt << " : " << target_name << " : "
+            << QEvent::staticMetaObject.enumerator(event_enum_index)
+                        .valueToKey(event->type());
+
+        return str.str();
+    }
 };
+
+/*****************************************************************************/
 
 // This class intercepts various UI events via EventCapturer
 // and stores their serialized representations in a container.
@@ -39,36 +59,43 @@ class UIEventRecorder : public EventSubscriber {
 public:
     using ContainerType = EventContainer<EventRecord>;
 
-    explicit UIEventRecorder(EventCapturer* ecap) : EventSubscriber(ecap) {}
+    explicit UIEventRecorder(EventCapturer* ecap)
+        : EventSubscriber(ecap), mTimeStamper() {
+        mTimeStamper.start();
+    }
 
     // Lets the client specialize their own container instance.
     template <typename U>
-    explicit UIEventRecorder(
-            EventCapturer* ecap,
-            U&& container,
-            enable_if_convertible<U, ContainerType> = nullptr) :
-        EventSubscriber(ecap),
-        mContainer(std::forward<U>(container)) {}
+    explicit UIEventRecorder(EventCapturer* ecap,
+                             U&& container,
+                             enable_if_convertible<U, ContainerType> = nullptr)
+        : EventSubscriber(ecap),
+          mContainer(std::forward<U>(container)),
+          mTimeStamper() {
+        mTimeStamper.start();
+    }
 
-    // Get a const reference to the underelying container with serialized
+    void print() {
+        for (int i = 0; i < mContainer.size(); ++i) {
+            fprintf(stderr, "*** %d : %s  ***\n", mContainer[i].dt,
+                    mContainer[i].to_string().c_str());
+        }
+    }
+
+    // Get a const reference to the underlying container with serialized
     // events.
     const ContainerType& container() const {
         return mContainer;
     }
+
+    // Get a reference to the underlying container for manipulation
+    ContainerType& wContainer() { return mContainer; }
 
 private:
     bool objectPredicate(const QObject*) const override { return true; }
 
     const EventCapturer::EventTypeSet& eventTypes() const override {
         return mEventTypes;
-    }
-
-    void processEvent(const QObject* target, const QEvent* event) override {
-        EventRecord record {
-            cloneEvent(event),
-            target->objectName().toStdString()
-        };
-        mContainer.push_back(std::move(record));
     }
 
     std::unique_ptr<QEvent> cloneEvent(const QEvent* e) {
@@ -79,8 +106,10 @@ private:
             return cloneEventHelper<QEnterEvent>(e);
         case QEvent::Leave:
             return cloneEventHelper<QEvent>(e);
+        case QEvent::MouseButtonDblClick:
         case QEvent::MouseButtonPress:
         case QEvent::MouseButtonRelease:
+        case QEvent::MouseMove:
             return cloneEventHelper<QMouseEvent>(e);
         case QEvent::FocusIn:
         case QEvent::FocusOut:
@@ -98,6 +127,14 @@ private:
         return nullptr;
     }
 
+    void processEvent(const QObject* target, const QEvent* event) override {
+        EventRecord record{cloneEvent(event),
+                           target->objectName().toStdString(),
+                           mTimeStamper.elapsed()};
+        mTimeStamper.restart();
+        mContainer.push_back(std::move(record));
+    }
+
     template <class T>
     std::unique_ptr<QEvent> cloneEventHelper(const QEvent* e, enable_if_convertible<T*, QEvent*> = nullptr) {
         return std::unique_ptr<QEvent>(new T(*static_cast<const T*>(e)));
@@ -106,19 +143,19 @@ private:
 private:
     ContainerType mContainer;
     static EventCapturer::EventTypeSet mEventTypes;
+    QTime mTimeStamper;
 };
 
-
-template <template<class T, class A = std::allocator<T>> class EventContainer>
+template <template <class T, class A = std::allocator<T>> class EventContainer>
 EventCapturer::EventTypeSet UIEventRecorder<EventContainer>::mEventTypes = {
-    QEvent::Close,
-    QEvent::Enter,
-    QEvent::Leave,
-    QEvent::FocusIn,
-    QEvent::FocusOut,
-    QEvent::Hide,
-    QEvent::MouseButtonPress,
-    QEvent::MouseButtonRelease,
-    QEvent::Resize
-};
-
+        QEvent::Close,
+        QEvent::Enter,
+        QEvent::Leave,
+        QEvent::FocusIn,
+        QEvent::FocusOut,
+        QEvent::Hide,
+        QEvent::MouseButtonDblClick,
+        QEvent::MouseButtonPress,
+        QEvent::MouseButtonRelease,
+        QEvent::MouseMove,
+        QEvent::Resize};
