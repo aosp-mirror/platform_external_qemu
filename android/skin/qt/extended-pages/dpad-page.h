d656566280af5ec0e02541ff20b6652dc397f8e0
Grigory Dzhavadyan <grigoryj@google.com>
2016-02-25 11:17:03 -0800

    [UI] Fix and refactor the DPad page
    * Fix the DPad buttons appearance on press
    * Refactor the code, removing the usage of preprocessor macros
    
    Change-Id: Ie3d375fe064d9300e553c580d3f01cd00740b581

--- android/skin/qt/extended-pages/dpad-page.h
+++ android/skin/qt/extended-pages/dpad-page.h
@@ -12,6 +12,8 @@
 #pragma once
 
 #include "ui_dpad-page.h"
+
+#include "android/skin/keycode.h"
 #include <QPushButton>
 #include <QWidget>
 #include <memory>
@@ -25,25 +27,10 @@ public:
     explicit DPadPage(QWidget *parent = 0);
     void setUserEventsAgent(const QAndroidUserEventAgent* agent);
 
-private slots:
-#define ON_PRESS_RELEASE(button) \
-    void on_dpad_ ## button ## Button_pressed(); \
-    void on_dpad_ ## button ## Button_released(); \
-
-    ON_PRESS_RELEASE(back);
-    ON_PRESS_RELEASE(down);
-    ON_PRESS_RELEASE(forward);
-    ON_PRESS_RELEASE(left);
-    ON_PRESS_RELEASE(play);
-    ON_PRESS_RELEASE(right);
-    ON_PRESS_RELEASE(select);
-    ON_PRESS_RELEASE(up);
-#undef ON_PRESS_RELEASE
-
 private:
-    void dpad_setPressed(QPushButton* button);
-    void dpad_setReleased(QPushButton* button);
-
+    void toggleButtonPressed(QPushButton* button,
+                             const SkinKeyCode key_code,
+                             const bool pressed);
 private:
     std::unique_ptr<Ui::DPadPage> mUi;
     const QAndroidUserEventAgent* mUserEventsAgent;
