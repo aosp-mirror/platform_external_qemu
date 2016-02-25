aabfc4d0e7e16bdcf2fa08451552db2a2bbf1694
Prathmesh Prabhu <pprabhu@google.com>
2016-02-24 16:14:47 -0800

    [AndroidEmu] Initialize openssl for multi-threaded environment.
    emulator is a multi-thread application. openssl (used by libcurl) needs
    to be setup correctly for this environement.
    
    BUG=b.android.com/201736
    
    Change-Id: I881e1ecbfe7922d3f7e31f3e399c366da2e5edba

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -262,6 +262,7 @@ LOCAL_SRC_FILES := \
     android/opengl/emugl_config.cpp \
     android/opengl/GpuFrameBridge.cpp \
     android/opengles.c \
+    android/openssl-support.cpp \
     android/process_setup.cpp \
     android/proxy/proxy_common.c \
     android/proxy/proxy_http.c \
