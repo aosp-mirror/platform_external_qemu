ef2e4f25db9d6c110649a027a17c641480a91519
Grigory Dzhavadyan <grigoryj@google.com>
2016-02-29 02:27:31 -0800

    Add anti-aliasing to GLWidget as a post-processing step
    * GLWidget renders its scene into a texture
    * The texture is used as input for shader that smoothes out
      jaggies
    * The output of the above-mentioned shader gets displayed on-screen
    
    Change-Id: I713571a759dc7b81d7a5cdf817a11dcfb8347103

--- android/skin/qt/gl-widget.cpp
+++ android/skin/qt/gl-widget.cpp
@@ -23,7 +23,7 @@
 // includes X11 headers which define stuff that conflicts
 // with Qt's own macros. It is a known issue.
 #include "OpenGLESDispatch/EGLDispatch.h"
-#include "OpenGLESDispatch/GLESv2Dispatch.h"
+#include "OpenGLESDispatch/GLESv2Dispatch.h"    
 
 namespace {
 
@@ -96,12 +96,96 @@ struct EGLState {
     EGLSurface surface;
 };
 
+
+// Helper macro for checking error status and cleaning up.
+#define CHECK_GL_ERROR(error_msg, cleanup_func) if (mGLES2->glGetError() != GL_NO_ERROR) { \
+    qWarning(error_msg); \
+    cleanup_func(); \
+    return; \
+}
+
+// Vertex shader for anti-aliasing - doesn't do anything special.
+static const char AAVertexShaderSource[] = R"(
+    attribute vec3 position;
+    void main(void) {
+        gl_Position = vec4(position, 1.0);
+    })";
+
+// Fragment shader - does fast approximate anti-aliasing (FXAA)
+// Based on:
+// http://www.geeks3d.com/20110405/fxaa-fast-approximate-anti-aliasing-demo-glsl-opengl-test-radeon-geforce/
+static const char AAFragmentShaderSource[] = R"(
+    uniform sampler2D texture;
+    uniform vec2 resolution;
+    float luma(vec3 color) {
+        return dot(color, vec3(0.299, 0.587, 0.114));
+    }
+    vec4 antialias(sampler2D tex,
+                   vec2 pixel_coord,
+                   vec2 inverse_res,
+                   vec2 nw, vec2 ne,
+                   vec2 sw, vec2 se,
+                   vec2 m) {
+        vec4 m_rgba = texture2D(tex, m);
+        vec3 nw_color = texture2D(tex, nw).xyz;
+        vec3 ne_color = texture2D(tex, ne).xyz;
+        vec3 sw_color = texture2D(tex, sw).xyz;
+        vec3 se_color = texture2D(tex, se).xyz;
+        vec3 m_color  = m_rgba.xyz;
+        float nw_luma = luma(nw_color);
+        float ne_luma = luma(ne_color);
+        float sw_luma = luma(sw_color);
+        float se_luma = luma(se_color);
+        float m_luma  = luma(m_color);
+        float min_luma = min(m_luma, min(min(nw_luma, ne_luma), min(sw_luma, se_luma)));
+        float max_luma = max(m_luma, max(max(nw_luma, ne_luma), max(sw_luma, se_luma)));;
+
+        mediump vec2 dir;
+        dir.x = -((nw_luma + ne_luma) - (sw_luma + se_luma));
+        dir.y =  ((nw_luma + sw_luma) - (ne_luma + se_luma));
+
+        float dir_reduce = max((nw_luma + ne_luma + sw_luma + se_luma) *
+                              (0.25 * (1.0 / 8.0)), (1.0/256.0));
+
+        float rcp_dir_min = 1.0 / (min(abs(dir.x), abs(dir.y)) + dir_reduce);
+        dir = min(vec2(16.0, 16.0),
+                  max(vec2(-16.0, -16.0),
+                  dir * rcp_dir_min)) * inverse_res;
+
+        vec2 tex_coord = pixel_coord * inverse_res;
+        vec3 rgb_a = 0.5 * (
+            texture2D(tex, tex_coord + dir * (1.0 / 3.0 - 0.5)).xyz +
+            texture2D(tex, tex_coord + dir * (2.0 / 3.0 - 0.5)).xyz);
+        vec3 rgb_b = rgb_a * 0.5 + 0.25 * (
+            texture2D(tex, tex_coord + dir * -0.5).xyz +
+            texture2D(tex, tex_coord+ dir * 0.5).xyz);
+
+        float luma_b = luma(rgb_b);
+        return vec4((luma_b < min_luma) || (luma_b > max_luma) ? rgb_a  : rgb_a, m_rgba.a);
+    }
+    void main(void) {
+        vec2 pixel_coord = gl_FragCoord.xy;
+        vec2 inverse_res = 1.0 / resolution;
+        vec2 nw = (pixel_coord + vec2(-1.0, -1.0)) * inverse_res;
+        vec2 ne = (pixel_coord + vec2( 1.0, -1.0)) * inverse_res;
+        vec2 sw = (pixel_coord + vec2(-1.0,  1.0)) * inverse_res;
+        vec2 se = (pixel_coord + vec2( 1.0,  1.0)) * inverse_res;
+        vec2 m = pixel_coord * inverse_res;
+        gl_FragColor = antialias(texture, pixel_coord, inverse_res, nw, ne, sw, se, m);
+    }
+)";
+
 GLWidget::GLWidget(QWidget* parent) :
         QWidget(parent),
         mEGL(MyEGLDispatch::get()),
         mGLES2(MyGLESv2Dispatch::get()),
         mEGLState(nullptr),
-        mValid(false) {
+        mValid(false),
+        mFramebuffer(0),
+        mTargetTexture(0),
+        mDepthTexture(0),
+        mAAProgram(0),
+        mAAVertexBuffer(0) {
     setAutoFillBackground(false);
     setAttribute(Qt::WA_OpaquePaintEvent, true);
     setAttribute(Qt::WA_PaintOnScreen, true);
@@ -124,7 +208,7 @@ bool GLWidget::ensureInit() {
 
     mEGLState->display = mEGL->eglGetDisplay(EGL_DEFAULT_DISPLAY);
     if (mEGLState->display == EGL_NO_DISPLAY) {
-        qWarning("Failed to get EGL display");
+        qWarning("Failed to get EGL display: EGL error %d", mEGL->eglGetError());
         return false;
     }
 
@@ -133,8 +217,10 @@ bool GLWidget::ensureInit() {
 
     // Try to initialize EGL display.
     // Initializing an already-initialized display is OK.
-    if (mEGL->eglInitialize(mEGLState->display, &egl_maj, &egl_min) == EGL_FALSE) {
-        qWarning("Failed to initialize EGL display");
+    if (mEGL->eglInitialize(mEGLState->display,
+                            &egl_maj,
+                            &egl_min) == EGL_FALSE) {
+        qWarning("Failed to initialize EGL display: EGL error %d", mEGL->eglGetError());
         return false;
     }
 
@@ -147,16 +233,19 @@ bool GLWidget::ensureInit() {
             EGL_GREEN_SIZE, 8,
             EGL_BLUE_SIZE, 8,
             EGL_DEPTH_SIZE, 24,
+            EGL_SAMPLES, 0, // No multisampling
             EGL_NONE
         };
     EGLint num_config;
-    if (mEGL->eglChooseConfig(
-                mEGLState->display,
-                config_attribs,
-                &egl_config,
-                1,
-                &num_config) == EGL_FALSE) {
-        qWarning("Failed to choose EGL config");
+    EGLBoolean choose_result =
+        mEGL->eglChooseConfig(
+            mEGLState->display,
+            config_attribs,
+            &egl_config,
+            1,
+            &num_config);
+    if (choose_result == EGL_FALSE || num_config < 1) {
+        qWarning("Failed to choose EGL config: EGL error %d", mEGL->eglGetError());
         return false;
     }
 
@@ -172,7 +261,7 @@ bool GLWidget::ensureInit() {
             EGL_NO_CONTEXT,
             context_attribs);
     if (mEGLState->context == EGL_NO_CONTEXT) {
-        qWarning("Failed to create EGL context");
+        qWarning("Failed to create EGL context %d", mEGL->eglGetError());
     }
 
     // Finally, create a window surface associated with this widget.
@@ -182,14 +271,18 @@ bool GLWidget::ensureInit() {
             (EGLNativeWindowType)(winId()),
             nullptr);
     if (mEGLState->surface == EGL_NO_SURFACE) {
-        qWarning("Failed to create an EGL surface");
+        qWarning("Failed to create an EGL surface %d", mEGL->eglGetError());
         return false;
     }
 
     mValid = true;
 
     makeContextCurrent();
+    initFramebuffer();
+    initAAProgram();
+    bindFramebuffer();
     initGL();
+    unbindFramebuffer();
 
     return true;
 }
@@ -207,7 +300,31 @@ void GLWidget::renderFrame() {
         return;
     }
     makeContextCurrent();
+
+    // Render 3D scene to texture.
+    bindFramebuffer();
     repaintGL();
+    unbindFramebuffer();
+
+    // Draw texture on-screen, applying the anti-aliasing shader.
+    mGLES2->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    mGLES2->glUseProgram(mAAProgram);
+    mGLES2->glBindBuffer(GL_ARRAY_BUFFER, mAAVertexBuffer);
+    mGLES2->glEnableVertexAttribArray(mAAPositionAttribLocation);
+    mGLES2->glVertexAttribPointer(mAAPositionAttribLocation,
+                                  3,
+                                  GL_FLOAT,
+                                  GL_FALSE,
+                                  0,
+                                  0);
+    mGLES2->glActiveTexture(GL_TEXTURE0);
+    mGLES2->glBindTexture(GL_TEXTURE_2D, mTargetTexture);
+    mGLES2->glUniform1i(mAAInputUniformLocation, 0);
+    mGLES2->glUniform2f(mAAResolutionUniformLocation,
+                        width() * devicePixelRatio(),
+                        height() * devicePixelRatio());
+    mGLES2->glDrawArrays(GL_TRIANGLES, 0, 3);
+
     mEGL->eglSwapBuffers(mEGLState->display, mEGLState->surface);
 }
 
@@ -228,18 +345,212 @@ void GLWidget::resizeEvent(QResizeEvent* e) {
         // EGL state at the time the resize event is generated, the default
         // framebuffer will not be created.
         makeContextCurrent();
+        // Re-create the framebuffer with new size.
+        destroyFramebuffer();
+        initFramebuffer();
+        bindFramebuffer();
         resizeGL(e->size().width(), e->size().height());
-        repaintGL();
-        mEGL->eglSwapBuffers(mEGLState->display, mEGLState->surface);
+        renderFrame();
     }
-
 }
 
+void GLWidget::initFramebuffer() {
+    // Create and bind framebuffer object.
+    mGLES2->glGenFramebuffers(1, &mFramebuffer);
+    CHECK_GL_ERROR("Failed to create framebuffer object", destroyFramebuffer);
+    mGLES2->glBindFramebuffer(GL_FRAMEBUFFER, mFramebuffer);
+    CHECK_GL_ERROR("Failed to bind framebuffer object", destroyFramebuffer);
+
+    // Create a texture to render to.
+    mGLES2->glGenTextures(1, &mTargetTexture);
+    CHECK_GL_ERROR("Failed to create target texture for FBO", destroyFramebuffer);
+    mGLES2->glBindTexture(GL_TEXTURE_2D, mTargetTexture);
+    mGLES2->glTexImage2D(GL_TEXTURE_2D,
+                         0,
+                         GL_RGB,
+                         width() * devicePixelRatio(),
+                         height() * devicePixelRatio(),
+                         0,
+                         GL_RGB,
+                         GL_UNSIGNED_BYTE,
+                         0);
+    CHECK_GL_ERROR("Failed to populate target texture", destroyFramebuffer);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    mGLES2->glFramebufferTexture2D(GL_FRAMEBUFFER,
+                                   GL_COLOR_ATTACHMENT0,
+                                   GL_TEXTURE_2D,
+                                   mTargetTexture,
+                                   0);
+    CHECK_GL_ERROR("Failed to set FBO color attachment", destroyFramebuffer);
+
+
+    // Create a texture for the depth buffer.
+    mGLES2->glGenTextures(1, &mDepthTexture);
+    CHECK_GL_ERROR("Failed to create depth texture", destroyFramebuffer);
+    mGLES2->glBindTexture(GL_TEXTURE_2D, mDepthTexture);
+    mGLES2->glTexImage2D(GL_TEXTURE_2D,
+                         0,
+                         GL_DEPTH_COMPONENT,
+                         width() * devicePixelRatio(),
+                         height() * devicePixelRatio(),
+                         0,
+                         GL_DEPTH_COMPONENT,
+                         GL_UNSIGNED_INT,
+                         0);
+    CHECK_GL_ERROR("Failed to populate depth texture", destroyFramebuffer);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    mGLES2->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    mGLES2->glFramebufferTexture2D(GL_FRAMEBUFFER,
+                                   GL_DEPTH_ATTACHMENT,
+                                   GL_TEXTURE_2D,
+                                   mDepthTexture,
+                                   0);
+    CHECK_GL_ERROR("Failed to set FBO depth attachment", destroyFramebuffer);
+
+    // Verify that FBO is valid.
+    if (mGLES2->glCheckFramebufferStatus(GL_FRAMEBUFFER) !=
+            GL_FRAMEBUFFER_COMPLETE) {
+        qWarning("FBO not complete");
+        destroyFramebuffer();
+    }
+
+    // Restore the default frame-buffer.
+    mGLES2->glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+// Helper for GLWidget::initAAProgram
+static GLuint createShader(const GLESv2Dispatch* gles2, GLint shaderType, const char* shaderText) {
+    GLuint shader = gles2->glCreateShader(shaderType);
+    if (!shader) {
+        return 0;
+    }
+    const GLchar* text = static_cast<const GLchar*>(shaderText);
+    const GLint textLen = ::strlen(shaderText);
+    gles2->glShaderSource(shader, 1, &text, &textLen);
+    gles2->glCompileShader(shader);
+    GLint success;
+    gles2->glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char i[256];
+        gles2->glGetShaderInfoLog(shader, 256, NULL, i);
+        qWarning("DIED %s", i);
+    }
+    return success == GL_TRUE ? shader : 0;
+}
+
+void GLWidget::initAAProgram() {
+    // Build the anti-aliasing shader.
+    GLuint vertex_shader =
+        createShader(mGLES2, GL_VERTEX_SHADER, AAVertexShaderSource);
+    CHECK_GL_ERROR("Failed to create vertex shader for anti-aliasing",
+                   destroyAAProgram);
+    GLuint fragment_shader =
+        createShader(mGLES2, GL_FRAGMENT_SHADER, AAFragmentShaderSource);
+    CHECK_GL_ERROR("Failed to create fragment shader for anti-aliasing",
+                   destroyAAProgram);
+    mAAProgram = mGLES2->glCreateProgram();
+    CHECK_GL_ERROR("Failed to create program object", destroyAAProgram);
+    mGLES2->glAttachShader(mAAProgram, vertex_shader);
+    mGLES2->glAttachShader(mAAProgram, fragment_shader);
+    mGLES2->glLinkProgram(mAAProgram);
+
+    // Shader objects no longer needed.
+    mGLES2->glDeleteShader(vertex_shader);
+    mGLES2->glDeleteShader(fragment_shader);
+
+    // Check for errors.
+    GLint success;
+    mGLES2->glGetProgramiv(mAAProgram, GL_LINK_STATUS, &success);
+    if (success == GL_FALSE) {
+        GLchar infolog[256];
+        mGLES2->glGetProgramInfoLog(
+                mAAProgram, sizeof(infolog), 0, infolog);
+        qWarning("Could not create/link program: %s\n", infolog);
+        destroyAAProgram();
+        return;
+    }
+
+    // Get all the attributes and uniforms.
+    mAAPositionAttribLocation = mGLES2->glGetAttribLocation(mAAProgram, "position");
+    mAAInputUniformLocation = mGLES2->glGetUniformLocation(mAAProgram, "texture");
+    mAAResolutionUniformLocation = mGLES2->glGetUniformLocation(mAAProgram, "resolution");
+    CHECK_GL_ERROR("Failed to get attributes & uniforms for anti-aliasing shader",
+                   destroyAAProgram);
+
+    // Create vertex and index buffers.
+    mGLES2->glGenBuffers(1, &mAAVertexBuffer);
+    CHECK_GL_ERROR("Failed to create vertex buffer for anti-aliasing shader",
+                   destroyAAProgram);
+    mGLES2->glBindBuffer(GL_ARRAY_BUFFER, mAAVertexBuffer);
+    static const float vertex_data[] = {
+        -1.0, -1.0, 0.0,
+         3.0, -1.0, 0.0,
+        -1.0,  3.0, 0.0,
+    };
+    mGLES2->glBufferData(
+            GL_ARRAY_BUFFER,
+            sizeof(vertex_data),
+            vertex_data,
+            GL_STATIC_DRAW);
+    CHECK_GL_ERROR("Failed to populate vertex buffer for anti-aliasing shader",
+                   destroyAAProgram);
+}
+
+void GLWidget::bindFramebuffer() {
+    mGLES2->glBindFramebuffer(GL_FRAMEBUFFER, mFramebuffer);
+    mGLES2->glViewport(0,
+                       0,
+                       width() * devicePixelRatio(),
+                       height() * devicePixelRatio());
+}
+
+void GLWidget::unbindFramebuffer() {
+    mGLES2->glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void GLWidget::destroyFramebuffer() {
+    if (mFramebuffer) {
+        mGLES2->glDeleteFramebuffers(1, &mFramebuffer);
+        mFramebuffer = 0;
+    }
+
+    if (mTargetTexture) {
+        mGLES2->glDeleteTextures(1, &mTargetTexture);
+        mTargetTexture = 0;
+    }
+
+    if (mDepthTexture) {
+        mGLES2->glDeleteTextures(1, &mDepthTexture);
+        mDepthTexture = 0;
+    }
+}
+
+void GLWidget::destroyAAProgram() {
+    if (mAAProgram) {
+        mGLES2->glUseProgram(0);
+        mGLES2->glDeleteProgram(mAAProgram);
+        mAAProgram = 0;
+    }
+
+    if (mAAVertexBuffer) {
+        mGLES2->glBindBuffer(GL_ARRAY_BUFFER, 0);
+        mGLES2->glDeleteBuffers(1, &mAAVertexBuffer);
+        mAAVertexBuffer = 0;
+    }
+}
 
 GLWidget::~GLWidget() {
     if (mEGL && mEGLState) {
         mEGL->eglDestroyContext(mEGLState->display, mEGLState->context);
         mEGL->eglDestroySurface(mEGLState->display, mEGLState->surface);
+        delete mEGLState;
     }
-    delete mEGLState;
+    unbindFramebuffer();
+    destroyFramebuffer();
+    destroyAAProgram();
 }
