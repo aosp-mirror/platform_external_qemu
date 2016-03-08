f04aa545eccd2ed27c2e16552b46356661a0f6e0
Yahan Zhou <yahan@google.com>
2016-03-08 11:14:10 -0800

    Checksum for GL pipe communication
    There is a possibility that the GL pipe got corrupted, which is
    potentially responsible for many random GL crashes on the crash server.
    This patch impelments a checksum for GL communications.
    
    The checksum mechanism is also designed to be backward compatible. That
    is, a guest without checksum support can work with a host with
    checksum support, and vise versa. Guest and host with different checksum
    version can also work together.
    
    The checksum implementation is in GLProtocol class.
    
    The guest is responsible for requesting a GL protocol.
    In this implementation, when the guest opens a pipe for GL
    communications, it firstly queries the supported maximum protocol
    version in the host. Then it tells the host about the protocol version
    it uses (which is max(guest_version, host_version)). After that all GL
    packets will have a checksum appended at the end.
    
    The current checksum (version 1) is 2 uint32. The first one is the
    bit-reversed packet length, the second one is the number of packets that
    has already been sent / received in the current thread. We might want to
    implement other checksum in the next patch.
    
    There are several things that are not yet supported and supposed to be
    implemented in a next CL:
    
    (1) CRC checksum,
    (2) when checksum fails, it should call crash service. Currently it is
    only calling abort;
    (3) currently it only adds checksum for messages from guest to host, we
    also need checksum from host to guest.
    
    Change-Id: Idfd6136c1ab271c888ef87a94a0980971ed46921

---
+++ distrib/android-emugl/shared/OpenglCodecCommon/GLProtocol.h
@@ -0,0 +1,85 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef GLPROTOCOL_H
+#define GLPROTOCOL_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// Set LOG_GLPROTOCOL to 1 to debug creation/destruction of GLprotocol
+// instances.
+#define TRACE_GLPROTOCOL 0
+
+#if TRACE_GLPROTOCOL
+#define LOG_GLPROTOCOL(x...) fprintf(stderr, x)
+#else
+#define LOG_GLPROTOCOL(x...)
+#endif
+
+// GLProtocol adds checksum to GL pipe communication.
+// It can:
+//	(1) take a buffer and compute its checksum string;
+//	(2) take a list of buffers one by one and compute their checksum string,
+//	    in this case the checksum should be as the data in those buffers are
+//	    concatenated;
+//	(3) take a buffer and a checksum string, tell if they match
+
+class GLProtocol {
+public:
+    GLProtocol();
+
+    // Get and set current protocol version
+    uint32_t getVersion() { return m_version; }
+    bool setVersion(uint32_t version);
+
+    // Maximum supported protocol version
+    static const uint32_t maxVersion = 1;
+    static const char* getMaxVersionString();
+
+    // Size of checksum string in the current version
+    size_t checksumStrSize() const;
+
+    // Compute checksum by taking buffers one by one
+    // Call addBuffer to add all the buffers then call checksumFromBuffers
+    // to get the checksum.
+    // Internally each call of addBuffer might change the state of
+    // m_partialChecksumData, while checksumFromBuffers will clear it.
+    void addBuffer(const void *buf, size_t packetLen);
+    // Write the checksum from the list of buffers to outputChecksum
+    // increase m_numWrite by 1
+    void writeChecksum(void *outputChecksum);
+
+    // Calculate the checksum of a packet (with size specified by packetLen),
+    // and compare it with the checksum encoded in
+    // buf + packetLen~packetLen+checksumStrSize
+    // increase m_numRead by 1
+    bool validate(const void *buf, size_t packetLen);
+protected:
+    uint32_t m_version;
+    // A temporary state used to compute the total length of a list of buffers,
+    // if addBuffer is called.
+    // Only used in protocol version 1.
+    uint32_t m_v1bufferTotalLength;
+    uint32_t m_numRead;
+    uint32_t m_numWrite;
+    // Compute a 32bit checksum
+    // Used in protocol v1
+    uint32_t checksum(const void *buf, size_t packetLen);
+};
+
+#endif /* GLPROTOCOL_H */
+
