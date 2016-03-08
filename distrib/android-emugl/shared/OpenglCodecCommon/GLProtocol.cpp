539c123d607d6b85533ef8b407dd3c1b32424878
Yahan Zhou <yahan@google.com>
2016-03-08 11:14:10 -0800

    Checksum for GL pipe communication
    There is a possibility that the GL pipe got corrupted, which is
    potentially responsible for many random GL crashes on the crash server.
    This patch impelments a checksum for GL communications.
    
    The checksum implementation is in GLProtocol class.
    
    The guest is responsible for requesting a GL protocol.
    In this implementation, when the guest opens a pipe for GL
    communications, it firstly queries the supported maximum protocol
    version in the host. Then it tells the host about the protocol version
    it uses (which is max(guest_version, host_version)). After that all GL
    packets will have a checksum appended at the end.
    
    Change-Id: Idfd6136c1ab271c888ef87a94a0980971ed46921

---
+++ distrib/android-emugl/shared/OpenglCodecCommon/GLProtocol.cpp
@@ -0,0 +1,117 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "GLProtocol.h"
+
+#include <string.h>
+#include <string>
+
+bool GLProtocol::setVersion(uint32_t version) {
+    if (version>maxVersion) return false; // unsupported version
+    m_version = version;
+    return true;
+}
+
+namespace {
+    std::string maxVersionStr;
+}
+
+GLProtocol::GLProtocol() {
+    m_version = 0;
+    m_v1bufferTotalLength = 0;
+    m_numRead = 0;
+    m_numWrite = 0;
+}
+
+const char* GLProtocol::getMaxVersionString() {
+    if (maxVersionStr.length() == 0) {
+        // A buffer to change 32bit integer to string.
+        // Size is 11 because the maximum 32bit integer has 10 digits (and +1 for the ending).
+        char buffer[11];
+        sprintf(buffer, "%d", maxVersion);
+        maxVersionStr = std::string("ANDROID_EMU_GL_PROTOCOL_v") + buffer;
+        LOG_GLPROTOCOL("%s: GLprotocol Version %s\n", __FUNCTION__,
+                maxVersionStr.c_str());
+    }
+    return maxVersionStr.c_str();
+}
+
+size_t GLProtocol::checksumStrSize() const {
+    switch (m_version) {
+        case 0:
+            return 0;
+        case 1:
+            return 8;
+        default:
+            return 0;
+    }
+}
+
+void GLProtocol::addBuffer(const void *buf, size_t packetLen) {
+    switch (m_version) {
+        case 1:
+	    m_v1bufferTotalLength += packetLen;
+	    break;
+    }
+}
+
+void GLProtocol::writeChecksum(void *outputChecksum) {
+    char *checksumPtr = (char *)outputChecksum;
+    switch (m_version) {
+        case 1: { // protocol v1 is to reverse the packetLen and write it at the end
+            uint32_t val = checksum(NULL, m_v1bufferTotalLength);
+            memcpy(checksumPtr, &val, 4);
+            memcpy(checksumPtr+4, &m_numWrite, 4);
+	    m_v1bufferTotalLength = 0;
+            break;
+        }
+    }
+    m_numWrite++;
+}
+
+bool GLProtocol::validate(const void *buf, size_t packetLen) {
+    size_t checksumSize = checksumStrSize();
+    unsigned char *checksumBuf = (unsigned char *)malloc(checksumSize);
+    switch (m_version) {
+        case 1: {
+            uint32_t val = checksum(buf, packetLen);
+            memcpy(checksumBuf, &val, 4);
+            memcpy(checksumBuf+4, &m_numRead, 4);
+            break;
+        }
+    }
+    const unsigned char *ptr = (unsigned char *)buf;
+    bool isValid = !memcmp(checksumBuf, ptr + packetLen, checksumSize);
+    free(checksumBuf);
+    m_numRead++;
+    return isValid;
+}
+
+uint32_t GLProtocol::checksum(const void *buf, size_t packetLen) {
+    uint32_t val = 0;
+    switch (m_version) {
+	case 1: // protocol v1 is to reverse the packetLen and write it at the end
+            uint32_t revLen = packetLen;
+            revLen = (revLen & 0xffff0000) >> 16 | (revLen & 0x0000ffff) << 16;
+            revLen = (revLen & 0xff00ff00) >> 8 | (revLen & 0x00ff00ff) << 8;
+            revLen = (revLen & 0xf0f0f0f0) >> 4 | (revLen & 0x0f0f0f0f) << 4;
+            revLen = (revLen & 0xcccccccc) >> 2 | (revLen & 0x33333333) << 2;
+            revLen = (revLen & 0xaaaaaaaa) >> 1 | (revLen & 0x55555555) << 1;
+	    val = revLen;
+	    break;
+    }
+    return val;
+}
