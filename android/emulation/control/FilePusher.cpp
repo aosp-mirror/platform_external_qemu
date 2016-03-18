fb98a7f56152d40b3fd6d14407baa8ffe5e21199
Prathmesh Prabhu <pprabhu@google.com>
2016-03-18 10:23:57 -0700

    [Emulib] Add FilePusher to 'adb push' files.
    This CL refactors the 'adb push' functionality to its own object.
    The behaviour is unchanged -- files may be queued (and re-queued while
    in progress) and they are pushed one by one.
    Progress is reported in terms of # of files completed.
    
    BUG=b.android.com/199725
    
    Change-Id: Iaf8d0d3af827d5c31151399f4fdfb307dc57c05d

---
+++ android/emulation/control/FilePusher.cpp
@@ -0,0 +1,161 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/emulation/control/FilePusher.h"
+
+#include "android/base/Compiler.h"
+#include "android/base/Log.h"
+#include "android/base/system/System.h"
+
+namespace android {
+namespace emulation {
+
+using android::base::Looper;
+using android::base::ParallelTask;
+using android::base::StringView;
+using android::base::System;
+using std::string;
+using std::vector;
+
+FilePusher::Ptr FilePusher::create(Looper* looper) {
+    auto inst = new FilePusher(looper);
+    return std::shared_ptr<FilePusher>(inst);
+}
+
+FilePusher::FilePusher(Looper* looper) : mLooper(looper) {}
+
+FilePusher::~FilePusher() {
+    cancel();
+}
+
+void FilePusher::setAdbCommandArgs(const vector<string>& adbCommandArgs) {
+    mAdbCommandArgs = adbCommandArgs;
+    mAdbCommandArgs.push_back("push");
+}
+
+bool FilePusher::enqueue(StringView localFilePath, StringView remoteFilePath) {
+    LOG(VERBOSE) << "Enqueue new file to push: " << localFilePath << " --> "
+                 << remoteFilePath;
+
+    bool triggerPush = mPushQueue.empty() && !mTask;
+    mPushQueue.emplace(localFilePath, remoteFilePath);
+
+    if (triggerPush) {
+        LOG(VERBOSE) << "Starting a new adb push";
+        resetProgress();
+    }
+
+    ++mNumQueued;
+    // Notify progress so that clients get immediate feedback.
+    for (auto& subscriber : mSubscribers) {
+        if (subscriber.progressCallback) {
+            subscriber.progressCallback(
+                    static_cast<double>(mNumPushed) / mNumQueued, false);
+        }
+    }
+
+    if (triggerPush) {
+        mSelfRef = shared_from_this();
+        pushNextItem();
+    }
+
+    return true;
+}
+
+void FilePusher::pushNextItem() {
+    if (mPushQueue.empty()) {
+        return;
+    }
+    LOG(VERBOSE) << "Pushing next item... [Queue length: " << mPushQueue.size()
+                 << "]";
+
+    mCurrentItem = mPushQueue.front();
+    mPushQueue.pop();
+
+    static auto sys = System::get();
+    if (!sys->pathCanRead(mCurrentItem.first)) {
+        pushDone(Result::FileReadError);
+        return;
+    }
+
+    auto commandLine = mAdbCommandArgs;
+    commandLine.push_back(mCurrentItem.first);
+    commandLine.push_back(mCurrentItem.second);
+
+    mTask.reset(new ParallelTask<Result>(
+            mLooper,
+            [this, commandLine](Result* outResult) {
+                this->pushOneFile(commandLine, outResult);
+            },
+            [this](const Result& result) { this->pushDone(result); }));
+    if (!mTask->start()) {
+        // May invalidate |this|.
+        pushDone(Result::ProcessStartFailure);
+    }
+}
+
+// Runs in a worker thread.
+// Since this doesn't access any member variables, we don't need any locking.
+void FilePusher::pushOneFile(vector<string> commandLine, Result* outResult) {
+    static auto sys = System::get();
+
+    System::ProcessExitCode exitCode;
+    if (!sys->runCommand(commandLine,
+                         System::RunOptions::WaitForCompletion |
+                                 System::RunOptions::TerminateOnTimeout,
+                         System::kInfinite, &exitCode) ||
+        exitCode != 0) {
+        *outResult = Result::UnknownError;
+        return;
+    }
+
+    *outResult = Result::Success;
+}
+
+void FilePusher::pushDone(const Result& result) {
+    bool allDone = mPushQueue.empty();
+    ++mNumPushed;
+    for (const auto& subscriber : mSubscribers) {
+        if (subscriber.resultCallback) {
+            subscriber.resultCallback(mCurrentItem.first, result);
+        }
+        if (subscriber.progressCallback) {
+            subscriber.progressCallback(
+                    static_cast<double>(mNumPushed) / mNumQueued, allDone);
+        }
+    }
+
+    if (!allDone) {
+        pushNextItem();
+    } else {
+        // Tell us that there's no outstanding push atm.
+        mTask.reset();
+        // May invalidate |this|.
+        mSelfRef.reset();
+    }
+}
+
+void FilePusher::cancel() {
+    while (!mPushQueue.empty()) {
+        mPushQueue.pop();
+    }
+}
+
+void FilePusher::resetProgress() {
+    mNumPushed = 0;
+    mNumQueued = 0;
+}
+
+}  // namespace emulation
+}  // namespace android
