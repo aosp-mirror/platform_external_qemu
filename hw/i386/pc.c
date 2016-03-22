e841e8db52c370c8560a08aa0df4790710815f9c
David 'Digit' Turner <digit@google.com>
2016-03-22 18:37:52 +0100

    DO NOT SUBMIT - Try to fix -logcat and -shell options for x86
    The implementation of -logcat <tags> and -shell require the setup
    of a third virtual tty, whose name is passed through android.bootconsole
    on the kernel's command-line.
    
    Unfortunately, on x86, the implementation in hw/i386/pc.c is limited
    to only the first two virtual ttys, which are always created for the
    following purpose:
    
      - first one receives kernel messages (through console=<name>)
    
      - second one receives qemud messages (only on legacy system images
        that still implement qemud through a serial port), named through
        android.qemud=<name>.
    
    The reason for this limitation is the limited number of available IRQs
    on the platform. Trying to increase the number of virtual tty created
    in pc.c will assert() at runtime with an error message of:
    
        "Goldfish device has exceeded available IRQ number."
    
    To fix this, the value of GFD_MAX_IRQS is incremented. This seems to
    be enough to boot a JB/x86 system image, but some IRQ conflicts might
    still exist (either when using older kernels, or different system
    images).
    
    HOWEVER, this breaks sound support in a Ginger/x86 AVD. So this is
    tagged as DO NOT SUBMIT.
    
    This patch is an experimental attempt to fix the issue and start
    discussing a proper solution. For reference see the following other
    (obsolete?) patches related to the IRQ problem:
    
      https://android-review.googlesource.com/#/c/31092/
      https://android-review.googlesource.com/#/c/31360/
    
    Change-Id: I236256e1012a4b214fd65606abb7f98bbfe5b5bd

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -1115,13 +1115,13 @@ static void pc_init1(ram_addr_t ram_size,
     goldfish_tty_add(serial_hds[0], 0, 0, 0);
     /* FIXME: This is just a work around before we have a permanent fix on
      * increasing number of IRQs available for x86 sysimages. In order to free up
-     * some IRQs for a better use, we limit number of TTY devices by 2. Normally
+     * some IRQs for a better use, we limit number of TTY devices by 3. Normally
      * we don't need more than that, so always having 4 of them would waste two
      * precious IRQs. */
 #if 0
     for(i = 1; i < MAX_SERIAL_PORTS; i++) {
 #else
-    for(i = 1; i < 2; i++) {
+    for(i = 1; i < 3; i++) {
 #endif
         if(serial_hds[i]) {
             goldfish_tty_add(serial_hds[i], i, 0, 0);
