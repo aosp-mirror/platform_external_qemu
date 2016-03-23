145cbd5e399de925629866de4850e00f790280d2
Evan Birenbaum <birenbaum@google.com>
2016-03-23 11:32:41 -0700

    [Emulib] Refactor drag-and-drop install implementation.
    This CL includes the following:
    - Pull together all install related functionality into a single
      object that is independent of UI code.
    - Replaces the use of QProcess with System::runCommand, which is
      broken on OS X.
    
    Change-Id: I625e2c7beb0a996231ed22e74dfdead1011fb5d9

--- android/skin/qt/tool-window.h
+++ android/skin/qt/tool-window.h
@@ -66,7 +66,6 @@ public:
 
     QString getAdbFullPath(QStringList *args);
 
-    void runAdbInstall(const QString &path);
     void runAdbPush(const QList<QUrl> &urls);
     void runAdbShellStopAndQuit();
 
@@ -109,11 +108,9 @@ private:
     QBoxLayout *top_layout;
     const UiEmuAgent *uiEmuAgent;
     Ui::ToolControls *toolsUi;
-    QProcess mInstallProcess;
     QProcess mPushProcess;
     bool mStartedAdbStopProcess = false;
     QProgressDialog mPushDialog;
-    QProgressDialog mInstallDialog;
     QQueue<QUrl> mFilesToPush;
     ShortcutKeyStore<QtUICommand> mShortcutKeyStore;
     bool mIsExtendedWindowActiveOnHide = false;
@@ -140,9 +137,6 @@ private slots:
     void on_volume_up_button_pressed();
     void on_volume_up_button_released();
     void on_zoom_button_clicked();
-
-    void slot_installCanceled();
-    void slot_installFinished(int exitStatus);
 
     void slot_pushCanceled();
     void slot_pushFinished(int exitStatus);
