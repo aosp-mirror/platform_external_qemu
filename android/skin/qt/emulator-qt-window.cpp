3a6a59b2059b2d0b1090c2a3860dd1ca8f63799a
Lingfeng Yang <lfy@google.com>
2016-04-01 15:55:31 -0700

    Fix typing '2' instead of '@' on 'shift' up first
    It's easy to type '2' instead of '@'
    because on most apps,
    we're used to text input happening on key down,
    not key up.
    
    This CL makes text input events happen on key down.
    The emulator's keyboard input also feels a bit more
    responsive as a result.
    
    Change-Id: Iae1f04efaf54b1604fb113d0868b236bd027b698

--- android/skin/qt/emulator-qt-window.cpp
+++ android/skin/qt/emulator-qt-window.cpp
@@ -391,11 +391,6 @@ void EmulatorQtWindow::dropEvent(QDropEvent *event)
 void EmulatorQtWindow::keyPressEvent(QKeyEvent *event)
 {
     handleKeyEvent(kEventKeyDown, event);
-}
-
-void EmulatorQtWindow::keyReleaseEvent(QKeyEvent *event)
-{
-    handleKeyEvent(kEventKeyUp, event);
 
     // If the key event generated any text, we need
     // to send an additional TextInput event to the emulator.
@@ -409,6 +404,11 @@ void EmulatorQtWindow::keyReleaseEvent(QKeyEvent *event)
         skin_event->u.text.text[sizeof(skin_event->u.text.text)-1] = 0;
         queueSkinEvent(skin_event);
     }
+}
+
+void EmulatorQtWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    handleKeyEvent(kEventKeyUp, event);
 
     // If we enabled trackball mode, tell Qt to always forward mouse movement
     // events. Otherwise, Qt will forward them only when a button is pressed.
