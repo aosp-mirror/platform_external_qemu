abaa868bfc2f4850019ee6fa3f2ebfd4d4c66d94
David 'Digit' Turner <digit@google.com>
2016-04-07 18:38:00 +0200

    Move QEMU1 GLES-related kernel parameter generation to AndroidEmu.
    Change-Id: I155f692715a112cbc0e443f4d47485b73205f03d

--- android/main-kernel-parameters.h
+++ android/main-kernel-parameters.h
@@ -16,6 +16,28 @@
 
 ANDROID_BEGIN_HEADER
 
+// List of values describing how EGL/GLES emulation should work in a given
+// Android virtual device.
+//
+// kAndroidGlesEmulationOff
+//    Means there is no GPU emulation, equivalent to "-gpu off" and instructs
+//    the guest system to use its old GLES 1.x software renderer.
+//
+// kAndroidGlesEmulationHost
+//    Means Host GPU emulation is being used. All EGL/GLES commands are
+//    sent to the host GPU or CPU through a simple wire protocol. This
+//    corresponds to "-gpu host" and "-gpu mesa".
+//
+// kAndroidGlesEmulationGuest
+//    Means a guest GLES 2.x library (e.g. SwiftShader) is being used in
+//    the guest. This should only be used with accelerated emulation, or
+//    results will be very very slow.
+typedef enum {
+    kAndroidGlesEmulationOff = 0,
+    kAndroidGlesEmulationHost,
+    kAndroidGlesEmulationGuest,
+} AndroidGlesEmulationMode;
+
 // Return a heap-allocated string containing the kernel parameter.
 // |opts| corresponds to the command-line options after they have been
 // processed by emulator_parseCommonCommandLineOptions().
@@ -24,6 +46,7 @@ ANDROID_BEGIN_HEADER
 char* emulator_getKernelParameters(const AndroidOptions* opts,
                                    const char* targetArch,
                                    const char* kernelSerialPrefix,
+                                   AndroidGlesEmulationMode glesMode,
                                    bool is_qemu2);
 
 ANDROID_END_HEADER
