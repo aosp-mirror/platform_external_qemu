3c6dcd1fad22fc9fbdbdc92e167d7f4f08806b18
Yurii Zubrytskyi <zyy@google.com>
2016-04-19 18:42:04 -0700

    [emugl] Replace all emugl copies of base library with base versions
    Use the android::base versions of the classes
    
    Change-Id: If6a6e983b8abf860a3d8231ef310ee49fe000c34

--- distrib/android-emugl/shared/emugl/common/thread.h
+++ distrib/android-emugl/shared/emugl/common/thread.h
@@ -12,93 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef EMUGL_COMMON_THREAD_H
-#define EMUGL_COMMON_THREAD_H
+#pragma once
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <pthread.h>
-#endif
-
-#include <stdint.h>
+#include "android/base/threads/Thread.h"
 
 namespace emugl {
 
-// Wrapper class for platform-specific threads.
-// To create your own thread, define a sub-class of emugl::Thread
-// and override its main() method.
-//
-// For example:
-//
-//    class MyThread : public emugl::Thread {
-//    public:
-//        MyThread() : Thread() {}
-//
-//        virtual intptr_t main() {
-//            ... main thread loop implementation
-//            return 0;
-//        }
-//    };
-//
-//    ...
-//
-//    // Create new instance, but does not start it.
-//    MyThread* thread = new MyThread();
-//
-//    // Start the thread.
-//    thread->start();
-//
-//    // Wait for thread completion, and gets result into |exitStatus|.
-//    int exitStatus;
-//    thread->wait(&exitStatus);
-//
-class Thread {
-public:
-    // Public constructor.
-    Thread();
-
-    // Virtual destructor.
-    virtual ~Thread();
-
-    // Override this method in your own thread sub-classes. This will
-    // be called when start() is invoked on the Thread instance.
-    virtual intptr_t main() = 0;
-
-    // Start a thread instance. Return true on success, false otherwise
-    // (e.g. if the thread was already started or terminated).
-    bool start();
-
-    // Wait for thread termination and retrieve exist status into
-    // |*exitStatus|. Return true on success, false otherwise.
-    // NOTE: |exitStatus| can be NULL.
-    bool  wait(intptr_t *exitStatus);
-
-    // Check whether a thread has terminated. On success, return true
-    // and sets |*exitStatus|. On failure, return false.
-    // NOTE: |exitStatus| can be NULL.
-    bool tryWait(intptr_t *exitStatus);
-
-private:
-#ifdef _WIN32
-    static DWORD WINAPI thread_main(void* arg);
-
-    CRITICAL_SECTION mLock;
-    HANDLE mThread;
-    DWORD mThreadId;
-#else // !WIN32
-    static void* thread_main(void* arg);
-
-    pthread_t mThread;
-    pthread_mutex_t mLock;
-    bool mJoined;
-    bool mStarted;
-#endif
-    bool mIsRunning;
-    intptr_t mExitStatus;
-};
+using android::base::Thread;
 
 }  // namespace emugl
-
-#endif  // EMUGL_COMMON_THREAD_H
-
