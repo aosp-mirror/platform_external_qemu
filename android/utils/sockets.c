39351ad17fb0d728bb1b0f178d7ee8c9f420c6d3
Tao Wu <lepton@google.com>
2016-04-29 12:45:35 -0700

    Fix buggy format_ip6 and add an unittest for it.
    The current code has 2 bugs and can't generate correct ipv6 string.
    Change to use getnameinfo since we've already used its peer getaddrinfo
    in our code.
    
    Change-Id: Ia771b1176dd07af842ed45739e6e4aed0633df0f

--- android/utils/sockets.c
+++ android/utils/sockets.c
@@ -25,6 +25,7 @@
 
 //#include "sysemu/char.h"
 
+#include <assert.h>
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -344,19 +345,6 @@ format_unsigned( char*  buf, char*  end, unsigned  val )
 }
 
 static char*
-format_hex( char*  buf, char*  end, unsigned  val, int  ndigits )
-{
-    int   shift = 4*ndigits;
-    static const char   hex[16] = "0123456789abcdef";
-
-    while (shift >= 0) {
-        buf = format_char(buf, end, hex[(val >> shift) & 15]);
-        shift -= 4;
-    }
-    return buf;
-}
-
-static char*
 format_ip4( char*  buf, char*  end, uint32_t  ip )
 {
     buf = format_unsigned( buf, end, (unsigned)(ip >> 24) );
@@ -372,17 +360,15 @@ format_ip4( char*  buf, char*  end, uint32_t  ip )
 static char*
 format_ip6( char*  buf, char*  end, const uint8_t*  ip6 )
 {
-    int  nn;
-    for (nn = 0; nn < 8; nn++) {
-        int  val = (ip6[0] << 16) | ip6[1];
-        ip6 += 2;
-        if (nn > 0)
-            buf = format_char(buf, end, ':');
-        if (val == 0)
-            continue;
-        buf  = format_hex(buf, end, val, 4);
-    }
-    return buf;
+    struct sockaddr_in6 sa[1];
+    memset(sa, 0, sizeof(sa));
+    sa->sin6_family = AF_INET6;
+    memcpy(sa->sin6_addr.s6_addr, ip6, 16);
+    if (getnameinfo(sa, sizeof(sa), buf, end - buf, NULL, 0, NI_NUMERICHOST))
+        return buf;
+    char* ptr = memchr(buf, 0, end - buf);
+    assert(ptr != NULL);
+    return ptr;
 }
 
 const char*
@@ -399,7 +385,9 @@ sock_address_to_string( const SockAddress*  a )
         break;
 
     case SOCKET_IN6:
+        buf = format_char( buf, end, '[');
         buf = format_ip6( buf, end, a->u.in6.address );
+        buf = format_char( buf, end, ']');
         buf = format_char( buf, end, ':' );
         buf = format_unsigned( buf, end, (unsigned) a->u.in6.port );
         break;
---
+++ android/utils/sockets_unittest.cpp
@@ -0,0 +1,25 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/utils/sockets.h"
+
+#include <gtest/gtest.h>
+
+
+TEST(SockAddress, ResolveAndToString) {
+    SockAddress s[1];
+
+    EXPECT_EQ(0 ,sock_address_init_resolve(s, "216.58.194.164", 53, 0));
+    EXPECT_STREQ("216.58.194.164:53", sock_address_to_string(s));
+
+    EXPECT_EQ(0 ,sock_address_init_resolve(s, "2001:4860:f802::9b", 53, 0));
+    EXPECT_STREQ("[2001:4860:f802::9b]:53", sock_address_to_string(s));
+}
