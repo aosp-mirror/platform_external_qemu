e447899ebd5681a685b9ea781084c70b643f3de7
Tao Wu <lepton@google.com>
2016-05-06 13:11:03 -0700

    Move sockets.c out of baselib, it depends on other
    It depends on sockets_drainer.cpp and unittest can't
    be compiled under windows.
    
    Change-Id: Ie9b8ebbf0e0a05a47c298ff1e90ced2e0eecf211
    Signed-off-by: Tao Wu <lepton@google.com>

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -126,7 +126,6 @@ LOCAL_SRC_FILES := \
     android/utils/property_file.c \
     android/utils/reflist.c \
     android/utils/refset.c \
-    android/utils/sockets.c \
     android/utils/stralloc.c \
     android/utils/stream.cpp \
     android/utils/string.cpp \
@@ -317,6 +316,7 @@ LOCAL_SRC_FILES := \
     android/update-check/VersionExtractor.cpp \
     android/user-config.cpp \
     android/utils/socket_drainer.cpp \
+    android/utils/sockets.c \
     android/utils/looper.cpp \
     android/wear-agent/android_wear_agent.cpp \
     android/wear-agent/WearAgent.cpp \
@@ -496,7 +496,6 @@ LOCAL_SRC_FILES := \
   android/utils/path_unittest.cpp \
   android/utils/property_file_unittest.cpp \
   android/utils/string_unittest.cpp \
-  android/utils/socket_drainer.cpp \
   android/utils/sockets_unittest.cpp \
   android/utils/x86_cpuid_unittest.cpp \
   android/wear-agent/PairUpWearPhone_unittest.cpp \
