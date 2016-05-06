287abd4d9ff040ad5f05f2f583e36bc1a4c890a8
Yurii Zubrytskyi <zyy@google.com>
2016-05-06 10:27:16 -0700

    [gl] Fix a race on mState in RenderChannelImpl
    mState is updated on two threads - emulated CPU and RenderThread
    This means we do have a race here and we may end up with an
    older state value in mState. This CL adds a lock there
    
    Change-Id: I7dd61f48b369cc56126a5045b6fa800d15404ebb

--- distrib/android-emugl/host/libs/libOpenglRender/RenderChannelImpl.cpp
+++ distrib/android-emugl/host/libs/libOpenglRender/RenderChannelImpl.cpp
@@ -22,14 +22,17 @@
 namespace emugl {
 
 RenderChannelImpl::RenderChannelImpl(std::shared_ptr<RendererImpl> renderer)
-    : mRenderer(renderer) {}
+    : mRenderer(std::move(renderer)), mState(State::Empty) {
+    assert(mState.is_lock_free()); // rethink it if this fails - we've already
+                                   // got a lock, use it instead.
+}
 
 void RenderChannelImpl::setEventCallback(
         RenderChannelImpl::EventCallback callback) {
     mOnEvent = std::move(callback);
 
     // Reset the current state to make sure the new subscriber gets it.
-    mState = State::Empty;
+    mState.store(State::Empty, std::memory_order_release);
     onEvent(true);
 }
 
@@ -111,9 +114,36 @@ void RenderChannelImpl::onEvent(bool byGuest) {
     if (!mOnEvent) {
         return;
     }
+
+    // We need to make sure that the state returned from calcState() remains
+    // valid when we write it into mState; this means we need to lock both
+    // calcState() and mState assignment with the same lock - otherwise it is
+    // possible (and it happened before the lock was added) that some other
+    // thread would overwrite a newer state with its older value, e.g.:
+    //  - thread 1 reads the last message from |mToGuest|
+    //  - thread 1 enters onEvent()
+    //  - thread 1 calculates state 1 = "can't read", switches out
+    //  -   thread 2 writes some data into |mToGuest|
+    //  -   thread 2 enters onEvent()
+    //  -   thread 2 calculates state 2 = "can read"
+    //  -   thread 2 gets the lock
+    //  -   thread 2 updates |mState| to "can read", unlocks, switches out
+    //  - thread 1 gets the lock
+    //  - thread 1 overwrites |mState| with state 1 = "can't read"
+    //  - thread 1 unlocks and calls the |mOnEvent| callback.
+    //
+    // The result is that state 2 = "can read" is completely lost - callback
+    // is never called when |mState| is "can read".
+    //
+    // But if the whole block of code is locked, threads can't overwrite newer
+    // |mState| with some older value, and the described situation would never
+    // happen.
+    android::base::AutoLock lock(mStateLock);
     const State newState = calcState();
-    if (newState != mState) {
+    if (mState != newState) {
         mState = newState;
+        lock.unlock();
+
         mOnEvent(newState,
                  byGuest ? EventSource::Client : EventSource::RenderChannel);
     }
