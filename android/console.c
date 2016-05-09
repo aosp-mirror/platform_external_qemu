847c743ac653e4195110af6a575d03e8e099d190
Vince Harron <vharron@google.com>
2016-05-09 12:44:57 -0700

    Fix console bannner line endings
    Android Studio is incredibly picky about exactly what line endings
    are required from the console.  Make sure we're adhering to that
    requirement.
    
    Change-Id: I8cc1e528e735d7a04851cec33c3a9ae4da58e115

--- android/console.c
+++ android/console.c
@@ -636,6 +636,8 @@ static void control_global_accept(void* opaque,
                 D(("control_global_accept: new client %p\n", client));
                 break;
             case CONSOLE_AUTH_STATUS_REQUIRED:
+                // Note that Studio looks for the first line of this message
+                // /exactly/
                 control_write(client,
                               "Android Console: Authentication required\r\n");
                 control_write(client,
@@ -2742,6 +2744,9 @@ const char* android_console_auth_banner_get() {
     static bool s_ready = false;
 
     if (!s_ready) {
+        // in QEMU2, banners get \n appended when they are output so no trailing
+        // \n here
+        // Note that Studio looks for the first line of this message /exactly/
         char* emulator_console_auth_token_path =
                 android_console_auth_token_path_dup();
         snprintf(s_android_monitor_auth_banner,
@@ -2749,7 +2754,7 @@ const char* android_console_auth_banner_get() {
                  "Android Console: Authentication required\n"
                  "Android Console: type 'auth <auth_token>' to authenticate\n"
                  "Android Console: you can find your <auth_token> in "
-                 "\n'%s'\nOK\n",
+                 "\n'%s'\nOK",
                  emulator_console_auth_token_path);
         free(emulator_console_auth_token_path);
 
@@ -2760,6 +2765,8 @@ const char* android_console_auth_banner_get() {
 }
 
 const char* android_console_help_banner_get() {
+    // in QEMU2, banners get \n appended when they are output so no trailing \n
+    // here
     return "Android Console: type 'help' for a list of commands"
-           "\nOK\n";
+           "\nOK";
 }
