ae0e4ae32503e3a66fbea23910c0511351a82c1d
Evan Birenbaum <birenbaum@google.com>
2016-05-12 10:43:57 -0700

    [Emugl] Use ANGLE to validate and translate shaders.
    dEQP-GLES2.functional.shaders.*: (CTS mustpass set)
    174 failures -> 0 failures!
    
    Replaces our current shader parsing code with a prebuilt ANGLE
    compiler that translates ESSL shaders to GLSL shaders. This is not
    enabled on Windows at the moment.
    
    Change-Id: Ifd2f32c176a3faa5e6da14946d408e357a8ae269

--- distrib/android-emugl/host/libs/Translator/GLES_V2/Android.mk
+++ distrib/android-emugl/host/libs/Translator/GLES_V2/Android.mk
@@ -8,11 +8,28 @@ host_common_SRC_FILES := \
      ShaderValidator.cpp \
      ProgramData.cpp
 
+# ANGLE shader translation is not supported on Windows yet.
+ifeq ($(BUILD_TARGET_OS),linux)
+    host_common_SRC_FILES += ANGLEShaderParser.cpp
+endif
+ifeq ($(BUILD_TARGET_OS),darwin)
+    host_common_SRC_FILES += ANGLEShaderParser.cpp
+endif
 
 ### GLES_V2 host implementation (On top of OpenGL) ########################
 $(call emugl-begin-shared-library,lib$(BUILD_TARGET_SUFFIX)GLES_V2_translator)
 $(call emugl-import, libGLcommon)
 
 LOCAL_SRC_FILES := $(host_common_SRC_FILES)
+
+# ANGLE shader translation is not supported on Windows yet.
+ifeq ($(BUILD_TARGET_OS),linux)
+    LOCAL_C_INCLUDES += $(ANGLE_TRANSLATION_INCLUDES)
+    LOCAL_STATIC_LIBRARIES += $(ANGLE_TRANSLATION_STATIC_LIBRARIES)
+endif
+ifeq ($(BUILD_TARGET_OS),darwin)
+    LOCAL_C_INCLUDES += $(ANGLE_TRANSLATION_INCLUDES)
+    LOCAL_STATIC_LIBRARIES += $(ANGLE_TRANSLATION_STATIC_LIBRARIES)
+endif
 
 $(call emugl-end-module)
