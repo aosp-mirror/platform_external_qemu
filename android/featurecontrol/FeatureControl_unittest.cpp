vicamo/libandroid-emugl

[warnings] Clean some warnings in the feature control test
Change-Id: I754b26726a700cf696de8497e47c07b939dacda9

android/featurecontrol/FeatureControl_unittest.cpp
@@ -85,9 +85,9 @@ TEST_F(FeatureControlTest, overrideSetting) {
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
         setEnabledOverride(feature, true);
-        EXPECT_EQ(true, isEnabled(feature));
+        EXPECT_TRUE(isEnabled(feature));
         setEnabledOverride(feature, false);
-        EXPECT_EQ(false, isEnabled(feature));
+        EXPECT_FALSE(isEnabled(feature));
     }
 }
 
@@ -112,14 +112,14 @@ TEST_F(FeatureControlTest, readDefaultSettings) {
     FeatureControlImpl::get().init(mDefaultIniFilePath, mUserIniFilePath);
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(true, isEnabled(feature));
+        EXPECT_TRUE(isEnabled(feature));
     }
 
     writeDefaultIni(mAllOffIni);
     FeatureControlImpl::get().init(mDefaultIniFilePath, mUserIniFilePath);
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(false, isEnabled(feature));
+        EXPECT_FALSE(isEnabled(feature));
     }
 }
 
@@ -128,14 +128,14 @@ TEST_F(FeatureControlTest, readDefaultSettingsWithNoUserSettings) {
     FeatureControlImpl::get().init(mDefaultIniFilePath, "");
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(true, isEnabled(feature));
+        EXPECT_TRUE(isEnabled(feature));
     }
 
     writeDefaultIni(mAllOffIni);
     FeatureControlImpl::get().init(mDefaultIniFilePath, "");
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(false, isEnabled(feature));
+        EXPECT_FALSE(isEnabled(feature));
     }
 }
 
@@ -146,14 +146,14 @@ TEST_F(FeatureControlTest, readUserSettings) {
     FeatureControlImpl::get().init(mDefaultIniFilePath, mUserIniFilePath);
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(true, isEnabled(feature));
+        EXPECT_TRUE(isEnabled(feature));
     }
 
     writeUserIni(mAllOffIni);
     FeatureControlImpl::get().init(mDefaultIniFilePath, mUserIniFilePath);
     for (int i = 0; i < Feature_n_items; i++) {
         Feature feature = static_cast<Feature>(i);
-        EXPECT_EQ(false, isEnabled(feature));
+        EXPECT_FALSE(isEnabled(feature));
     }
 }
 

aosp-mirror/platform_external_qemu

android/featurecontrol/FeatureControl_unittest.cpp
@@ -86,9 +86,9 @@ TEST_F(FeatureControlTest, overrideSetting) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
         setEnabledOverride(feature, true);
-        EXPECT_EQ(true, featureIsEnabled(feature));
+        EXPECT_TRUE(featureIsEnabled(feature));
         setEnabledOverride(feature, false);
-        EXPECT_EQ(false, featureIsEnabled(feature));
+        EXPECT_FALSE(featureIsEnabled(feature));
     }
 }
 
@@ -115,7 +115,7 @@ TEST_F(FeatureControlTest, readDefaultSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(true, featureIsEnabled(feature));
+        EXPECT_TRUE(featureIsEnabled(feature));
     }
 
     writeDefaultIni(mAllOffIni);
@@ -123,7 +123,7 @@ TEST_F(FeatureControlTest, readDefaultSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(false, featureIsEnabled(feature));
+        EXPECT_FALSE(featureIsEnabled(feature));
     }
 }
 
@@ -133,7 +133,7 @@ TEST_F(FeatureControlTest, readDefaultSettingsWithNoUserSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(true, featureIsEnabled(feature));
+        EXPECT_TRUE(featureIsEnabled(feature));
     }
 
     writeDefaultIni(mAllOffIni);
@@ -141,7 +141,7 @@ TEST_F(FeatureControlTest, readDefaultSettingsWithNoUserSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(false, featureIsEnabled(feature));
+        EXPECT_FALSE(featureIsEnabled(feature));
     }
 }
 
@@ -153,7 +153,7 @@ TEST_F(FeatureControlTest, readUserSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(true, featureIsEnabled(feature));
+        EXPECT_TRUE(featureIsEnabled(feature));
     }
 
     writeUserIni(mAllOffIni);
@@ -161,7 +161,7 @@ TEST_F(FeatureControlTest, readUserSettings) {
     for (int i = 0; i < Feature_n_items; i++) {
         AndroidFeatureControlFeature feature =
             static_cast<AndroidFeatureControlFeature>(i);
-        EXPECT_EQ(false, featureIsEnabled(feature));
+        EXPECT_FALSE(featureIsEnabled(feature));
     }
 }
 