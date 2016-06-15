e8b21c43a7172b443d3abb76e5bb4e25152f2da7
Lingfeng Yang <lfy@google.com>
2016-06-15 09:29:49 -0700

    [WIP][goldfish_sync] goldfish_sync qemu1 integration
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

---
+++ android/emulation/goldfish_sync.c
@@ -0,0 +1,40 @@
+/* Copyright (C) 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android/emulation/goldfish_sync.h"
+
+#include "android/utils/system.h"
+
+static const GoldfishSyncHwFuncs* sGoldfishSyncHwFuncs = NULL;
+
+const GoldfishSyncHwFuncs* goldfish_sync_set_hw_funcs(
+        const GoldfishSyncHwFuncs* hw_funcs) {
+        const GoldfishSyncHwFuncs* result = sGoldfishSyncHwFuncs;
+        sGoldfishSyncHwFuncs = hw_funcs;
+        return result;
+}
+
+uint64_t goldfish_sync_create_timeline() {
+    return sGoldfishSyncHwFuncs->createTimeline();
+}
+
+void goldfish_sync_create_fence(uint64_t timeline, uint32_t pt) {
+    sGoldfishSyncHwFuncs->createFence(timeline, pt);
+}
+
+void goldfish_sync_timeline_inc(uint64_t timeline, uint32_t howmuch) {
+    sGoldfishSyncHwFuncs->incTimeline(timeline, howmuch);
+}
+
+void goldfish_sync_destroy_timeline(uint64_t timeline) {
+    sGoldfishSyncHwFuncs->destroyTimeline(timeline);
+}
