55631760e086c90841d8945e964ef0238ef4c251
Lingfeng Yang <lfy@google.com>
2016-06-17 09:54:40 -0700

    goldfish_sync androidemu/emugl integration
    + tests for goldfish sync
    
    Goldfish Sync Device interacts with the emulator
    in androidemu along with emugl.
    
    The main entry points are in goldfish_sync.h,
    which provide the set of Android sync operators:
    
    - goldfish_sync_create_timeline
    - goldfish_sync_create_fence
    - goldfish_sync_timeline_inc
    - goldfish_sync_destroy_timeline
    
    These in turn make GoldfishSyncCommandQueue forward
    requests to the virtual sync device. GoldfishSyncCommandQueue
    makes sure that such requests occur only when the VM lock
    is held.
    
    An interface to RenderLib for use of sync in
    emugl is also included.
    
    This is part of a sequential, multi-CL change:
    
    external/qemu:
    
    https://android-review.googlesource.com/240119 <- needed for testing
    https://android-review.googlesource.com/239442 <- this CL
    https://android-review.googlesource.com/221593
    https://android-review.googlesource.com/248563
    https://android-review.googlesource.com/248564
    https://android-review.googlesource.com/223032
    
    external/qemu-android:
    
    https://android-review.googlesource.com/240155 <- needed for testing
    https://android-review.googlesource.com/238790 <- actual device
    implementation
    
    kernel/goldfish:
    
    https://android-review.googlesource.com/232631 <- needed to enable
    driver
    https://android-review.googlesource.com/238399 <- guest kernel driver
    for goldfish_sync
    
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -241,6 +241,9 @@ LOCAL_SRC_FILES := \
     android/emulation/control/TestRunnerSelfTests.cpp \
     android/emulation/control/ScreenCapturer.cpp \
     android/emulation/CpuAccelerator.cpp \
+    android/emulation/GoldfishSyncCommandQueue.cpp \
+    android/emulation/goldfish_sync.cpp \
+    android/emulation/goldfish_sync_tests.cpp \
     android/emulation/nand_limits.c \
     android/emulation/ParameterList.cpp \
     android/emulation/qemud/android_qemud_client.cpp \
