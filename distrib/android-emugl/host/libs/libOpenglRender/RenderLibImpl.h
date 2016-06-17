d321b6c8bc2dc26258145ca1918312555f501125
Lingfeng Yang <lfy@google.com>
2016-06-17 09:54:40 -0700

    [goldfish_sync] goldfish_sync qemu1 integration
    + tests for goldfish sync
    
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

--- distrib/android-emugl/host/libs/libOpenglRender/RenderLibImpl.h
+++ distrib/android-emugl/host/libs/libOpenglRender/RenderLibImpl.h
@@ -28,6 +28,10 @@ public:
     virtual void setLogger(emugl_logger_struct logger) override;
     virtual void setCrashReporter(emugl_crash_reporter_t reporter) override;
     virtual void setFeatureController(emugl_feature_is_enabled_t featureController) override;
+    virtual void setSyncDevice(emugl_sync_create_timeline_t,
+                               emugl_sync_create_fence_t,
+                               emugl_sync_timeline_inc_t,
+                               emugl_sync_destroy_timeline_t) override;
 
     virtual RendererPtr initRenderer(int width,
                                      int height,
