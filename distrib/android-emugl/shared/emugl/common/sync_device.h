76aa06381e954c48c37961e17a6f04df3db046ac
Lingfeng Yang <lfy@google.com>
2016-06-17 09:54:40 -0700

    goldfish_sync androidemu/emugl integration
    + tests for goldfish sync
    
    Goldfish Sync Device interacts with the emulator
    in androidemu along with emugl.
    
    The main entry points are in goldfish_sync.h,
    which provide the set of Android sync operators:
    
    - goldfish_sync_create_timeline
    - goldfish_sync_create_fence
    - goldfish_sync_timeline_inc
    - goldfish_sync_destroy_timeline
    
    These in turn make GoldfishSyncCommandQueue forward
    requests to the virtual sync device. GoldfishSyncCommandQueue
    makes sure that such requests occur only when the VM lock
    is held.
    
    An interface to RenderLib for use of sync in
    emugl is also included.
    
    This is part of a sequential, multi-CL change:
    
    external/qemu:
    
    https://android-review.googlesource.com/240119 <- needed for testing
    https://android-review.googlesource.com/239442 <- this CL
    https://android-review.googlesource.com/221593
    https://android-review.googlesource.com/248563
    https://android-review.googlesource.com/248564
    https://android-review.googlesource.com/223032
    
    external/qemu-android:
    
    https://android-review.googlesource.com/240155 <- needed for testing
    https://android-review.googlesource.com/238790 <- actual device
    implementation
    
    kernel/goldfish:
    
    https://android-review.googlesource.com/232631 <- needed to enable
    driver
    https://android-review.googlesource.com/238399 <- guest kernel driver
    for goldfish_sync
    
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

---
+++ distrib/android-emugl/shared/emugl/common/sync_device.h
@@ -0,0 +1,31 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#pragma once
+
+#include "OpenglRender/render_api_types.h"
+
+extern emugl_sync_create_timeline_t emugl_sync_create_timeline;
+extern emugl_sync_create_fence_t emugl_sync_create_fence;
+extern emugl_sync_timeline_inc_t emugl_sync_timeline_inc;
+extern emugl_sync_destroy_timeline_t emugl_sync_destroy_timeline;
+extern emugl_sync_register_trigger_wait_t emugl_sync_register_trigger_wait;
+
+void set_emugl_sync_create_timeline(emugl_sync_create_timeline_t);
+void set_emugl_sync_create_fence(emugl_sync_create_fence_t);
+void set_emugl_sync_timeline_inc(emugl_sync_timeline_inc_t);
+void set_emugl_sync_destroy_timeline(emugl_sync_destroy_timeline_t);
+void set_emugl_sync_register_trigger_wait(emugl_sync_register_trigger_wait_t trigger_fn);
