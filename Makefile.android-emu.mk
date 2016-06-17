df0c16d9eeddc9dbc64b8188702b4ff46a42fb78
Lingfeng Yang <lfy@google.com>
2016-06-17 09:54:40 -0700

    [WIP][goldfish_sync] goldfish_sync qemu1 integration
    + tests for goldfish sync (WIP)
    
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -235,6 +235,8 @@ LOCAL_SRC_FILES := \
     android/emulation/control/TestRunnerSelfTests.cpp \
     android/emulation/control/ScreenCapturer.cpp \
     android/emulation/CpuAccelerator.cpp \
+    android/emulation/goldfish_sync.c \
+    android/emulation/goldfish_sync_tests.cpp \
     android/emulation/nand_limits.c \
     android/emulation/ParameterList.cpp \
     android/emulation/qemud/android_qemud_client.cpp \
