ebd5bcb191f57ddec7b0a3bb61376eb191a0a582
Lingfeng Yang <lfy@google.com>
2016-06-17 09:55:13 -0700

    Console agent for running arbitrary tests
    Recently we have stuff like goldfish_sync device
    which requires an active guest to really be tested.
    
    There are a bunch of other things that require similar
    conditions such as goldfish_pipe, goldfish_audio etc.
    
    This CL puts an interface to trigger
    tests of such devices through the console---
    if bad behavior can be isolated, it's
    faster than running a particular app.
    
    Change-Id: I6ce6a01e322fa79e8f12a325202189a2f6911923

---
+++ android/emulation/control/TestRunner.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/emulation/control/TestRunner.h"
+#include "android/utils/debug.h"
+
+#include <stdio.h>
+#include <unordered_map>
+
+static bool initialized = false;
+static std::unordered_map<std::string, test_function_t>
+sTestFunctionMap;
+
+void test_runner_init() {
+    initialized = true;
+}
+
+void test_runner_add_test(const char* id, test_function_t func) {
+    if (!initialized) return;
+    sTestFunctionMap[id] = func;
+}
+
+bool test_runner_run_test(const char* _id) {
+    if (!initialized) return false;
+    std::string id(_id);
+    if (sTestFunctionMap.find(id) != sTestFunctionMap.end()) {
+        sTestFunctionMap[id]();
+        return true;
+    } else {
+        return false;
+    }
+}
