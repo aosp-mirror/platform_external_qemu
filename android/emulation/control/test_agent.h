c9e657e60513a71806974892ddaff5af8b8e54a7
Lingfeng Yang <lfy@google.com>
2016-06-17 09:55:13 -0700

    Console agent for running arbitrary tests
    Recently we have stuff like goldfish_sync device
    which requires an active guest to really be tested.
    
    There are a bunch of other things that require similar
    conditions such as goldfish_pipe, goldfish_audio etc.
    
    This CL puts an interface to trigger
    tests of such devices through the console---
    if bad behavior can be isolated, it's
    faster than running a particular app.
    
    Change-Id: I6ce6a01e322fa79e8f12a325202189a2f6911923

---
+++ android/emulation/control/test_agent.h
@@ -0,0 +1,79 @@
+// Copyright 20156The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Android Console Test Runner==================================================
+//
+// Introduction
+//
+// The emulator has a number of virtual devices, such as goldfish_pipe and
+// goldfish_audio, that are difficult to test thoroughly without a running guest.
+// On top of that, even if a guest is running, it can be hard to run apps
+// in just the right way to reproduce a problem at the virtual device level.
+//
+// The Android Console Test Runner allows one to run arbitrary test functions
+// from the emulator console. Once we find a minimal repro case of a
+// malfunctioning virtual device, we may write a test function for the
+// console test runner, start the emulator and emulator console,
+// and run it directly from there, instead of having to always take
+// the extra step to do it live in the app.
+//
+// Usage: Running existing tests and listing available tets
+//
+// To run a test, issue "test run <name-of-test>" in the emulator console.
+// The console will then output "OK" (test was able to run) or "KO"
+// (test failed to run). The test result will be appended to both stdout
+// and a file determined by the emulator (there will be a print to stdout
+// of the file's location when the test runner initializes).
+//
+// To get a list of all tests that are registered, issue "test list."
+//
+// Usage: Registering new tests
+//
+// New tests cannot yet be defined through the console. We expect them to be
+// compiled along with the rest of the emulator. See test_runner.h for
+// information on how to define and register new tests.
+// =============================================================================
+
+#pragma once
+
+#include "android/utils/compiler.h"
+
+#include <stdbool.h>
+
+ANDROID_BEGIN_HEADER
+
+typedef struct QAndroidTestAgent {
+    // Interface: Mirrors the API in test_runner.h.
+    // See test_runner.h for more details.
+    //
+    // |listTests|: For listing tests that have been registered.
+    // Uses double-call convention. See test_runner.h for usage details.
+    // Called with "test list" is issued in the console.
+    void (*listTests)(char* buf, unsigned int* sz);
+    // Gets location of test results
+    // Another double-call. This is used internally by the
+    // console to generate more helpful messages.
+    void (*getResultLocation)(char* buf, unsigned int* sz);
+    // |addTest|: This is for registering new tests.
+    // There is no way to call this from the console yet.
+    void (*addTest)(const char* test_id, bool (*func)(void));
+    // |runTest|: Run a registered test with ID |test_id|.
+    // Called when "test run <name>" is issued in the console.
+    // Returns whether or not the test was able to run.
+    // The result of the test is appended to a temp file,
+    // the name of which can be obtained using listTests.
+    bool (*runTest)(const char* test_id);
+} QAndroidTestAgent;
+
+ANDROID_END_HEADER
