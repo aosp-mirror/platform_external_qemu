efb963b5deaf0a36c7b4d0c1395e654d4a26e145
Lingfeng Yang <lfy@google.com>
2016-06-17 09:54:40 -0700

    goldfish_sync androidemu/emugl integration
    + tests for goldfish sync
    
    Goldfish Sync Device interacts with the emulator
    in androidemu along with emugl.
    
    The main entry points are in goldfish_sync.h,
    which provide the set of Android sync operators:
    
    - goldfish_sync_create_timeline
    - goldfish_sync_create_fence
    - goldfish_sync_timeline_inc
    - goldfish_sync_destroy_timeline
    
    These in turn make GoldfishSyncCommandQueue forward
    requests to the virtual sync device. GoldfishSyncCommandQueue
    makes sure that such requests occur only when the VM lock
    is held.
    
    An interface to RenderLib for use of sync in
    emugl is also included.
    
    Change-Id: I9378396396bb10cf78af7489c49e82df5ae61f18

---
+++ android/emulation/goldfish_sync.cpp
@@ -0,0 +1,172 @@
+/* Copyright (C) 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android/emulation/goldfish_sync.h"
+#include "android/emulation/GoldfishSyncCommandQueue.h"
+
+#include "android/base/containers/Lookup.h"
+#include "android/base/memory/LazyInstance.h"
+#include "android/base/synchronization/ConditionVariable.h"
+#include "android/base/synchronization/Lock.h"
+
+#include "android/utils/assert.h"
+#include "android/utils/debug.h"
+#include "android/utils/system.h"
+
+#include <unordered_map>
+
+using android::base::AutoLock;
+using android::base::ConditionVariable;
+using android::base::Lock;
+
+// Tag each command with unique id's,
+// so that for the commands that require a reply
+// from the guest, we signal them properly.
+static uint64_t sUniqueId = 0;
+static Lock sUniqueIdLock;
+
+uint64_t next_unique_id() {
+    AutoLock lock(sUniqueIdLock);
+    uint64_t res = sUniqueId;
+    sUniqueId += 1;
+    return res;
+}
+
+struct CommandWaitInfo {
+    CommandWaitInfo() : lock(), done(false), cvDone() { }
+    Lock lock;
+    bool done;
+    ConditionVariable cvDone;
+    uint64_t return_value;
+};
+
+// This keeps track of all the commands in flight
+// that require a reply from the guest.
+static std::unordered_map<uint64_t, CommandWaitInfo*> wait_map;
+
+static CommandWaitInfo* allocWait(uint64_t id) {
+    AutoLock lock(sUniqueIdLock);
+    CommandWaitInfo* res = new CommandWaitInfo;
+    wait_map[id] = res;
+    return res;
+}
+
+static void freeWait(uint64_t id) {
+    AutoLock lock(sUniqueIdLock);
+    if (auto elt = android::base::find(wait_map, id)) {
+        delete *elt;
+        wait_map.erase(id);
+    }
+}
+
+static GoldfishSyncDeviceInterface* sGoldfishSyncHwFuncs = NULL;
+
+// To send commands to the virtual device, there are two
+// alternatives:
+// - |sendCommand|, which just sends the command without waiting
+//   for a reply.
+// - |sendCoommandAndGetResult|, which sends the command and waits
+//   for that command to have completed in the guest.
+
+static void sendCommand(uint32_t cmd,
+                        uint64_t handle,
+                        uint32_t time_arg,
+                        uint64_t hostcmd_handle) {
+    GoldfishSyncCommandQueue::hostSignal(cmd, handle, time_arg, hostcmd_handle);
+}
+
+static void receiveCommandResult(void* data,
+                                 uint32_t cmd,
+                                 uint64_t handle,
+                                 uint32_t time_arg,
+                                 uint64_t hostcmd_handle) {
+    if (auto elt = android::base::find(wait_map, hostcmd_handle)) {
+        CommandWaitInfo* wait_info = *elt;
+        AutoLock lock(wait_info->lock);
+        wait_info->return_value = handle;
+        wait_info->done = true;
+        wait_info->cvDone.broadcast();
+    }
+}
+
+// We need to give the virtual device's hw funcs
+// a function pointer that it calls upon receiving
+// the result of an executed command from the guest.
+static bool wrapped = false;
+
+void goldfish_sync_set_cmd_receiver(
+        GoldfishSyncDeviceInterface* hw_funcs,
+        device_command_result_t next_recv) {
+
+    if (wrapped || !next_recv) return;
+
+    hw_funcs->receiveCommandResult = next_recv;
+
+    wrapped = true;
+
+    return;
+}
+
+static uint64_t sendCommandAndGetResult(uint64_t cmd,
+                                        uint64_t handle,
+                                        uint64_t time_arg,
+                                        uint64_t hostcmd_handle) {
+    // Set the "received command" callback if not set already
+    goldfish_sync_set_cmd_receiver(sGoldfishSyncHwFuncs, receiveCommandResult);
+
+    // Do the usual thing when sending commands
+    sendCommand(cmd, handle, time_arg, hostcmd_handle);
+
+    CommandWaitInfo* waitInfo = allocWait(hostcmd_handle);
+
+    AutoLock lock(waitInfo->lock);
+    while (!waitInfo->done) {
+        waitInfo->cvDone.wait(&waitInfo->lock);
+    }
+
+    uint64_t res = waitInfo->return_value;
+
+    lock.unlock();
+    freeWait(hostcmd_handle);
+
+    return res;
+}
+
+// Goldfish sync host-side interface////////////////////////////////////////////
+
+uint64_t goldfish_sync_create_timeline() {
+    return sendCommandAndGetResult(CMD_CREATE_SYNC_TIMELINE,
+                                   0, 0, next_unique_id());
+}
+
+int goldfish_sync_create_fence(uint64_t timeline, uint32_t pt) {
+    return (int)sendCommandAndGetResult(CMD_CREATE_SYNC_FENCE,
+                                        timeline, pt, next_unique_id());
+}
+
+void goldfish_sync_timeline_inc(uint64_t timeline, uint32_t howmuch) {
+    sendCommand(CMD_SYNC_TIMELINE_INC, timeline, howmuch, next_unique_id());
+}
+
+void goldfish_sync_destroy_timeline(uint64_t timeline) {
+    sendCommand(CMD_DESTROY_SYNC_TIMELINE, timeline, 0, next_unique_id());
+}
+
+void goldfish_sync_register_trigger_wait(trigger_wait_fn_t f) {
+    sGoldfishSyncHwFuncs->registerTriggerWait(f);
+}
+
+void goldfish_sync_set_hw_funcs(GoldfishSyncDeviceInterface* hw_funcs) {
+    sGoldfishSyncHwFuncs = hw_funcs;
+    GoldfishSyncCommandQueue::setQueueCommand(sGoldfishSyncHwFuncs->doHostCommand);
+}
+
