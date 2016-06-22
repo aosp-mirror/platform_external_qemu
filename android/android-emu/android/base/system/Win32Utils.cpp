781a3f5f5e766bc5f25cb2a4f05cf2bf763a3726
David 'Digit' Turner <digit@google.com>
2016-06-22 14:54:18 +0200

    Win32Utils.h: Add android::followLink() - DO NOT SUBMIT
    Add a new function to follow Windows symlinks and mount points
    correctly, this can be used to implement a proper stat() function.
    that returns information about the target, not the link itself.
    
    Change-Id: I6060112ae98ed450b333804dd2db40f52131173d

--- android/android-emu/android/base/system/Win32Utils.cpp
+++ android/android-emu/android/base/system/Win32Utils.cpp
@@ -12,12 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "android/base/files/ScopedFileHandle.h"
 #include "android/base/StringFormat.h"
 #include "android/base/system/Win32UnicodeString.h"
 #include "android/base/system/Win32Utils.h"
 
+#include <winioctl.h>
+#include <string.h>
+
 namespace android {
 namespace base {
+
+// This declaration if from <ntifs.h> which is not provided by Mingw.
+struct REPARSE_DATA_BUFFER {
+  ULONG  ReparseTag;
+  USHORT ReparseDataLength;
+  USHORT Reserved;
+  union {
+    struct {
+      USHORT SubstituteNameOffset;
+      USHORT SubstituteNameLength;
+      USHORT PrintNameOffset;
+      USHORT PrintNameLength;
+      ULONG  Flags;
+      WCHAR  PathBuffer[1];
+    } SymbolicLinkReparseBuffer;
+    struct {
+      USHORT SubstituteNameOffset;
+      USHORT SubstituteNameLength;
+      USHORT PrintNameOffset;
+      USHORT PrintNameLength;
+      WCHAR  PathBuffer[1];
+    } MountPointReparseBuffer;
+    struct {
+      UCHAR DataBuffer[1];
+    } GenericReparseBuffer;
+  };
+};
 
 // static
 std::string Win32Utils::quoteCommandLine(StringView commandLine) {
@@ -82,6 +113,88 @@ std::string Win32Utils::getErrorString(DWORD error_code) {
                            error_code, format_result);
     }
     return result;
+}
+
+// Recursive internal function that tries to follow the target of |path|.
+// If |path| is a regular file or directory, return true and sets |*result|
+// to a copy of |path|. If |path| is a symbolic link or mount point, extract
+// the target and call itself recursively. If |path| is invalid, return false.
+static bool followLinkUnicode(const Win32UnicodeString& path,
+                              Win32UnicodeString* result) {
+    WIN32_FILE_ATTRIBUTE_DATA fileData = {};
+    if (!GetFileAttributesExW(path.c_str(), GetFileExInfoStandard, &fileData)) {
+        // Probably an invalid path or access violation.
+        return false;
+    }
+    if (!(fileData.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT)) {
+        // Cannot be a symbolic link, since it doesn't have a reparse point.
+        *result = path;
+        return true;
+    }
+    // Extract target path now, the trick is to use FILE_READ_EA to avoid
+    // requiring admin privileges to open the file. See:
+    // http://blog.kalmbach-software.de/2008/02/28/howto-correctly-read-reparse-data-in-vista/
+    // NOTE: FILE_FLAG_BACKUP_SEMANTICS is needed to open a HANDLE to a
+    // directory.
+    ScopedFileHandle fileHandle(CreateFileW(path.c_str(),
+                                            FILE_READ_EA,
+                                            FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                            NULL,
+                                            OPEN_EXISTING,
+                                            FILE_FLAG_BACKUP_SEMANTICS |
+                                            FILE_FLAG_OPEN_REPARSE_POINT,
+                                            NULL));
+    if (!fileHandle.valid()) {
+        return false;
+    }
+
+    std::string reparseData(MAXIMUM_REPARSE_DATA_BUFFER_SIZE, '\0');
+    DWORD reparseDataLen = 0;
+    if (!DeviceIoControl(fileHandle.get(),
+                         FSCTL_GET_REPARSE_POINT,
+                         NULL,
+                         0,
+                         &reparseData[0],
+                         reparseData.size(),
+                         &reparseDataLen,
+                         NULL)) {
+        return false;
+    }
+    fileHandle.close();
+
+    auto data = reinterpret_cast<REPARSE_DATA_BUFFER*>(&reparseData[0]);
+    Win32UnicodeString target;
+    if (data->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
+        auto symlink = &data->SymbolicLinkReparseBuffer;
+        size_t wlen = symlink->SubstituteNameLength / sizeof(WCHAR);
+        target.resize(wlen);
+        ::memmove(target.data(),
+                  &symlink->PathBuffer[symlink->SubstituteNameOffset],
+                  wlen * sizeof(WCHAR));
+    } else if (data->ReparseTag == IO_REPARSE_TAG_MOUNT_POINT) {
+        auto mountpoint = &data->MountPointReparseBuffer;
+        size_t wlen = mountpoint->SubstituteNameLength / sizeof(WCHAR);
+        target.resize(wlen);
+        ::memmove(target.data(),
+                  &mountpoint->PathBuffer[mountpoint->SubstituteNameOffset],
+                  wlen * sizeof(WCHAR));
+    } else {
+        // Invalid tag. Should not happen.
+        return false;
+    }
+
+    // Recursive call.
+    return followLinkUnicode(target, result);
+}
+
+// static
+std::string followLink(StringView path) {
+    Win32UnicodeString result;
+    if (followLinkUnicode(Win32UnicodeString(path), &result)) {
+        return result.toString();
+    } else {
+        return std::string();
+    }
 }
 
 }  // namespace base
