f35194e4c5009ae5795bdc3814bef2b099b57340
bohu <bohu@google.com>
2016-06-24 15:55:10 -0700

    Cap the ram_size to 2G
    It seems system image cannot handle more than 2G
    memory, yet; We will relax this cap when system
    images are fixed.
    
    b.android.com/214093
    
    Change-Id: Ibf46aafabc7813fa71e05fdff13a22e9ead51837

--- android/main-common.c
+++ android/main-common.c
@@ -1021,7 +1021,10 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
     // all 64 bit archs we support include "64"
     bool guest_is_32_bit = strstr(hw->hw_cpu_arch, "64") == 0;
     bool host_is_32_bit = sizeof(void*) == 4;
-    bool limit_is_4gb = (guest_is_32_bit || host_is_32_bit);
+
+    // It turns out memory over 2G is not well-handled by systm images
+    // b.android.com/214093
+    const bool limit_is_2gb = true;
 
     // enforce CDD minimums
     int minRam = 32;
@@ -1055,9 +1058,9 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
         D("Increasing RAM size to %iMB", minRam);
         hw->hw_ramSize = minRam;
     }
-    else if (limit_is_4gb && hw->hw_ramSize > 4096) {
-        D("Decreasing RAM size to 4096MB");
-        hw->hw_ramSize = 4096;
+    else if (limit_is_2gb && hw->hw_ramSize > 2048) {
+        D("Decreasing RAM size to 2048MB");
+        hw->hw_ramSize = 2048;
     }
     else {
         D("Physical RAM size: %dMB\n", hw->hw_ramSize);
