4122ed1c4c67cef428ea00ce2bb582e46e10cbd0
Tao Wu <lepton@google.com>
2016-06-24 15:47:10 -0700

    Add a new flag to disable scale.
    Change-Id: I9596c94468202656e6bacf143bb95f84dd6cb813
    Signed-off-by: Tao Wu <lepton@google.com>

--- android/skin/window.h
+++ android/skin/window.h
@@ -45,6 +45,7 @@ typedef struct SkinWindowFuncs {
 extern SkinWindow* skin_window_create(SkinLayout* layout,
                                       int x,
                                       int y,
+                                      bool enable_scale,
                                       bool use_emugl_subwindow,
                                       const SkinWindowFuncs* win_funcs);
 
