271b970ec2a80dad450df417223b3dcdbfa774cf
Yurii Zubrytskyi <zyy@google.com>
2016-06-27 16:19:16 -0700

    [ram] Limit the VM heap size to [minCTSHeap, max(ram/4, minCTSvmHeap*4)]
    There are several reasons to limit the VM heap size:
    
    1. If the heap is too small, some CTS tests crash with OOM
     (libcore.java.util.zip.Zip[64]FileTest)
    2. If the heap is too big, device fails to allocate a single
     chunk at once (b.android.com/214093)
    
    Change-Id: I983c9c8222233577019aa175cbfa9fbd68d3a4b3

--- android/main-common.c
+++ android/main-common.c
@@ -1066,27 +1066,31 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
         D("Physical RAM size: %dMB\n", hw->hw_ramSize);
     }
 
-    int minVmHeapSize =
+    // Set the VM heap size to min vm heap per api level and make sure it falls
+    // into the range (min vm heap per api level, RAM size / 4); we want to
+    // neither constrain the apps just because of a dumb old hardcoded constant
+    // nor try to allocate a huge single chunk or memory for a heap which just
+    // could be impossible.
+    const int minApiLevelVmHeapSize =
             androidHwConfig_getMinVmHeapSize(hw, avdInfo_getApiLevel(avd));
-    if (hw->vm_heapSize < minVmHeapSize) {
-        D("VM heap size set below hardware specified minimum of %iMB",
-          minVmHeapSize);
+    const int minRamVmHeapSize = hw->hw_ramSize / 4;
+    const int maxVmHeapSize = 4 * minApiLevelVmHeapSize;
+    const int vmHeapSize =
+            minRamVmHeapSize > minApiLevelVmHeapSize
+                ? (minRamVmHeapSize > maxVmHeapSize
+                    ? maxVmHeapSize
+                    : minRamVmHeapSize)
+                : minApiLevelVmHeapSize;
+    if (hw->vm_heapSize < vmHeapSize) {
+        D("VM heap size %iMB is below hardware specified minimum of %iMB,"
+          "setting it to that value", hw->vm_heapSize, vmHeapSize);
 
-        int vmHeapSize = hw->hw_ramSize / 4;
-
-        if (vmHeapSize < minVmHeapSize) {
-            vmHeapSize = minVmHeapSize;
-        }
-
-        D("Setting VM heap size to %iMB", vmHeapSize);
         hw->vm_heapSize = vmHeapSize;
 
-        int minRamSize = vmHeapSize * 2;
-
+        const int minRamSize = vmHeapSize * 2;
         if (hw->hw_ramSize < minRamSize) {
             hw->hw_ramSize = minRamSize;
-            D("Increasing RAM to %iMB to accomodate min VM heap",
-                     minRamSize);
+            D("Increasing RAM to %iMB to accomodate min VM heap", minRamSize);
         }
     }
 
