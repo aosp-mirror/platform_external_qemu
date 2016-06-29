6bdb8ec6d79593ed7e27da897c1d96a60ea7a917
David 'Digit' Turner <digit@google.com>
2016-06-29 20:04:55 +0200

    AndroidPipe: refactor Android pipe implementation.
    This patch refactors the Android pipe implementation significantly:
    
    - Introduce a new C++ class AndroidPipe and AndroidPipe::Service
      to implement host-side pipe services with a better API that makes
      more sense.
    
    - Still support the now-legacy C API in android_pipe_host.h through
      an adaptor class in android_pipe_host.cpp. In the future, pipe
      implementations will be moved to the C++ version to get rid of
      this one too.
    
    - Remove android_pipe.c completely.
    
    Tested with both QEMU1 and QEMU2, including snapshot support on
    QEMU1 (QEMU2 doesn't support them yet due to limitations of its
    virtual devices).
    
    NOTE: This doesn't fix inherent thread-related issues and thus
          still enforces all pipe operations to happen in a thread
          that holds the global VM state mutex. This restriction
          will be lifted for a few pipe methods in a future patch.
    
    NOTE2: This only modifies the host-side support for Android pipes
           and this doesn't require qemu-android changes.
    
    Change-Id: I833bc3c33941f4dc79e2c87bc52ad37bb7e5d174

---
+++ android/emulation/android_pipe_host.cpp
@@ -0,0 +1,122 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/emulation/android_pipe_host.h"
+
+#include "android/emulation/AndroidPipe.h"
+
+namespace android {
+
+using CStream = ::Stream;
+using BaseStream = ::android::base::Stream;
+using Service = AndroidPipe::Service;
+
+namespace {
+
+static CStream* asCStream(BaseStream* stream) {
+    return reinterpret_cast<CStream*>(stream);
+}
+
+// An AndroidPipe derived class modeled around a legacy AndroidPipeFuncs
+// structure.
+class LegacyPipe : public AndroidPipe {
+public:
+    LegacyPipe(void* instance, void* hwPipe, const Service* service)
+        : AndroidPipe(hwPipe, service), mInstance(instance) {}
+
+    const AndroidPipeFuncs* getFuncs() const;
+
+    virtual void onGuestClose() override {
+        auto funcs = getFuncs();
+        if (funcs->close) {
+            funcs->close(mInstance);
+        }
+        // Delete the LegacyPipe instance, whether |mInstance| is deleted
+        // immediately or not depends on the close() callback above.
+        delete this;
+    }
+
+    virtual unsigned onGuestPoll() const { return getFuncs()->poll(mInstance); }
+
+    virtual int onGuestRecv(AndroidPipeBuffer* buffers,
+                            int numBuffers) override {
+        return getFuncs()->recvBuffers(mInstance, buffers, numBuffers);
+    }
+
+    virtual int onGuestSend(const AndroidPipeBuffer* buffers,
+                            int numBuffers) override {
+        return getFuncs()->sendBuffers(mInstance, buffers, numBuffers);
+    }
+
+    virtual void onGuestWantWakeOn(int flags) override {
+        getFuncs()->wakeOn(mInstance, flags);
+    }
+
+    virtual void onSave(BaseStream* stream) {
+        getFuncs()->save(mInstance, asCStream(stream));
+    }
+
+private:
+    void* mInstance;
+};
+
+// An un-named service for legacy pipes.
+class LegacyPipeService : public Service {
+public:
+    LegacyPipeService(const char* name,
+                      void* opaque,
+                      const AndroidPipeFuncs* funcs)
+        : Service(name), mOpaque(opaque), mFuncs(funcs) {}
+
+    virtual AndroidPipe* create(void* hwPipe, const char* args) const override {
+        void* hostPipe = mFuncs->init(hwPipe, mOpaque, args);
+        if (!hostPipe) {
+            return nullptr;
+        }
+        return new LegacyPipe(hostPipe, hwPipe, this);
+    }
+
+    virtual bool canLoad() const override { return (mFuncs->load != nullptr); }
+
+    virtual AndroidPipe* load(void* hwPipe,
+                              const char* args,
+                              BaseStream* stream) const override {
+        void* instance = mFuncs->load(hwPipe, mOpaque, args, asCStream(stream));
+        if (!instance) {
+            return nullptr;
+        }
+
+        return new LegacyPipe(instance, hwPipe, this);
+    }
+
+    void* mOpaque;
+    const AndroidPipeFuncs* mFuncs;
+};
+
+const AndroidPipeFuncs* LegacyPipe::getFuncs() const {
+    return reinterpret_cast<const LegacyPipeService*>(mService)->mFuncs;
+}
+
+}  // namespace
+
+}  // namespace android
+
+void android_pipe_add_type(const char* pipeName,
+                           void* pipeOpaque,
+                           const AndroidPipeFuncs* pipeFuncs) {
+    auto service =
+            new android::LegacyPipeService(pipeName, pipeOpaque, pipeFuncs);
+    android::Service::add(service);
+}
+
+void android_pipe_reset_service(void) {
+    android::Service::resetAll();
+}
