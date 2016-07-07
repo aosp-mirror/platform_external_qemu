2726a7511785e73bd8295cfdbba360e4fa0eb035
Lingfeng Yang <lfy@google.com>
2016-07-07 12:49:32 -0700

    [WIP again] rc(Create|ClientWait)Sync: +fps but keep frame order
    This is the second step of the better approach alluded to in
    
    https://android-review.googlesource.com/#/c/221311/
    
    When swapping buffers, instead of calling glFinish on the host to keep
    frames in order, a fence command is requested from the guest right after
    the rcFlushWindowColorBuffer.
    
    This generates a host-side fence command using glFenceSync.  The sync
    object is then passed to the guest in a way that allows the guest to
    check whether the object was signaled using egl(Client)WaitSyncKHR.
    
    --==WIP Part==--
    {
    + Sync threads on host side only, using goldfish sync device
    - Put in the new, more guest-centric (and correct) design where
      we increment timelines from the host but do not create
      fence objects from the host
    - (In separate CL) Move command queue stuff to AndroidEmu
    - First version with no egregious deadlocks or kernel panics while
      running Antutu/browser with all the sync traffic
    
    TODO
    
    - Make the guest wait on the fds
    - Ensure proper VM locking
    - Cleanup
    
    }
    
    This allows us to keep frame ordering, but increase FPS.
    
    If we issue glFinish every call to blitFromCurrentReadBuffer, we get ~30
    FPS on Linux Quadro K2200.
    
    If we use the implementation described above,
    we get ~36 FPS.
    
    Change-Id: I39564a4b14db1b1d22690ebc90ebb807f4bcd1e7

---
+++ android/emulation/GoldfishSyncCommandQueue.h
@@ -0,0 +1,109 @@
+/* Copyright (C) 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#pragma once
+
+#include "android/base/threads/Thread.h"
+#include "android/base/synchronization/Lock.h"
+#include "android/base/synchronization/ConditionVariable.h"
+#include "android/base/synchronization/MessageChannel.h"
+
+#include "android/emulation/goldfish_sync.h"
+#include "android/emulation/VmLock.h"
+
+#include "android/utils/assert.h"
+#include "android/utils/debug.h"
+
+#define DEBUG 0
+
+#if DEBUG
+#define DPRINT(...) do { \
+    if (!VERBOSE_CHECK(goldfishsync)) VERBOSE_ENABLE(goldfishsync); \
+    VERBOSE_TID_FUNCTION_DPRINT(goldfishsync, __VA_ARGS__); } while(0)
+#else
+#define DPRINT(...)
+#endif
+
+#define ERR(...) do { \
+    derror(__VA_ARGS__); \
+} while(0)
+
+using android::base::AutoLock;
+using android::base::ConditionVariable;
+using android::base::Lock;
+using android::base::MessageChannel;
+using android::base::Thread;
+using android::VmLock;
+
+class GoldfishSyncCommand {
+public:
+    GoldfishSyncCommand(uint32_t cmd,
+                        uint64_t handle,
+                        uint32_t time_arg) :
+        cmd(cmd),
+        handle(handle),
+        time_arg(time_arg) { }
+
+    uint32_t cmd;
+    uint64_t handle;
+    uint32_t time_arg;
+    Lock lock;
+    bool done;
+    ConditionVariable cvDone;
+};
+
+class GoldfishSyncCommandQueue : public Thread {
+public:
+    GoldfishSyncCommandQueue();
+    void initThreading();
+    void setQueueCommands(queue_device_command_t queueCmd,
+                          device_command_result_t getCmdResult);
+
+    // For users of goldfish sync command queue.
+    void sendCommandAndGetResult(GoldfishSyncCommand* cmd);
+
+    // Queue uses this to talk to waker.
+    // void wakeCommand(
+    //         uint32_t cmd,
+    //         uint64_t handle,
+    //         uint32_t time_arg,
+    //         uint32_t* cmd_out,
+    //         uint64_t* handle_out,
+    //         uint32_t* time_arg_out);
+    
+    // Waker uses this to actually communicate with device.
+    void sendCommandToDevice(
+            uint32_t cmd,
+            uint64_t handle,
+            uint32_t time_arg,
+            uint64_t hostcmd_handle);
+
+    void receiveCommandResult(uint32_t cmd,
+                              uint64_t handle,
+                              uint32_t time_arg,
+                              uint64_t hostcmd_handle);
+private:
+    virtual intptr_t main() override;
+
+    GoldfishSyncCommand* mCurrentCommand;
+    Lock mLock;
+
+    MessageChannel<GoldfishSyncCommand*, 512U> mInput;
+    queue_device_command_t mQueueCommand;
+    device_command_result_t mGetCommandResult;
+};
+
+void receiveCommandResult(void* queue,
+                          uint32_t cmd,
+                          uint64_t handle,
+                          uint32_t time_arg,
+                          uint64_t hostcmd_handle);
