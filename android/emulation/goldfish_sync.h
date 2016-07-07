7d987c0765f91d1746c10af0b498232816780fb1
Lingfeng Yang <lfy@google.com>
2016-07-07 12:49:32 -0700

    [WIP again] rc(Create|ClientWait)Sync: +fps but keep frame order
    This is the second step of the better approach alluded to in
    
    https://android-review.googlesource.com/#/c/221311/
    
    When swapping buffers, instead of calling glFinish on the host to keep
    frames in order, a fence command is requested from the guest right after
    the rcFlushWindowColorBuffer.
    
    This generates a host-side fence command using glFenceSync.  The sync
    object is then passed to the guest in a way that allows the guest to
    check whether the object was signaled using egl(Client)WaitSyncKHR.
    
    --==WIP Part==--
    {
    + Sync threads on host side only, using goldfish sync device
    - Put in the new, more guest-centric (and correct) design where
      we increment timelines from the host but do not create
      fence objects from the host
    - (In separate CL) Move command queue stuff to AndroidEmu
    - First version with no egregious deadlocks or kernel panics while
      running Antutu/browser with all the sync traffic
    - optimization: don't wait around for write back when incrementing
      timelines
    - optimization introduced another egregious threading problem :) (fixed)
    - Fixed another braino
    - Now also works with -gpu Swiftshader by utilizing existing
    eglCreateSyncKHR / eglClientWaitSyncKHR implementations
    - Now ensures proper VM locking using the same scheme as PipeWaker
    - Optimization: Demonstrated no need for the GoldfishSyncCommandQueue,
      we can use the GoldfishSyncWaker for all host->guest traffic
    - Removed old command queue class, uses DeviceContextRunner now.
    
    TODO
    
    - Cleanup
    
    }
    
    This allows us to keep frame ordering, but increase FPS.
    
    If we issue glFinish every call to blitFromCurrentReadBuffer, we get ~30
    FPS on Linux Quadro K2200.
    
    If we use the implementation described above,
    we get ~36 FPS.
    
    Change-Id: I39564a4b14db1b1d22690ebc90ebb807f4bcd1e7

--- android/emulation/goldfish_sync.h
+++ android/emulation/goldfish_sync.h
@@ -47,29 +47,84 @@ ANDROID_BEGIN_HEADER
 // Destroys a timeline. reads timeline handle
 #define CMD_DESTROY_SYNC_TIMELINE 4
 
+// Starts a wait on the host with
+// the given glsync object and sync thread handle.
+#define CMD_TRIGGER_HOST_WAIT     5
+
 // The register layout is:
 
-#define SYNC_REG_COMMAND      0x00 // which operation to run
-#define SYNC_REG_HANDLE       0x04 // Timeline or fence fd to operator on
-#define SYNC_REG_HANDLE_HIGH  0x08 // 64-bit part
-#define SYNC_REG_TIME_ARG     0x0c // how much to increment timeline
+// The register layout is:
 
-#define NO_WRITE_BACK(cmd) (cmd == CMD_SYNC_TIMELINE_INC || \
-                            cmd == CMD_DESTROY_SYNC_TIMELINE)
+#define SYNC_REG_COMMAND                     0x00 // which operation to run
+#define SYNC_REG_HANDLE                      0x04 // Stores pointer to goldfish_timeline_obj
+#define SYNC_REG_HANDLE_HIGH                 0x08 // 64-bit part
+#define SYNC_REG_TIME_ARG                    0x0c // how much to increment timeline
+#define SYNC_REG_HOSTCMD_HANDLE              0x10 // handle to host-side command to signal
+#define SYNC_REG_HOSTCMD_HANDLE_HIGH         0x14 // 64-bit part
+
+// Guest->host registers
+//
+#define SYNC_REG_COMMAND_OUT                     0x18 // which operation to run
+#define SYNC_REG_HANDLE_OUT                      0x1c // Stores pointer to goldfish_timeline_obj
+#define SYNC_REG_HANDLE_HIGH_OUT                 0x20 // 64-bit part
+#define SYNC_REG_TIME_ARG_OUT                    0x24 // how much to increment timeline
+#define SYNC_REG_HOSTCMD_HANDLE_OUT              0x28 // handle to host-side command to signal
+#define SYNC_REG_HOSTCMD_HANDLE_HIGH_OUT         0x2c // 64-bit part
+
+#define SYNC_REG_HOST_COMMAND                0x30
+#define SYNC_REG_GLSYNC_HANDLE               0x34 // Stores pointer to host sync object
+#define SYNC_REG_GLSYNC_HANDLE_HIGH          0x38 // 64-bit part
+#define SYNC_REG_THREAD_HANDLE               0x3c // Stores pointer to host sync thread object
+#define SYNC_REG_THREAD_HANDLE_HIGH          0x40 // 64-bit part
+#define SYNC_REG_GUEST_TIMELINE_HANDLE       0x44 // Stores pointer to guest timeline handle for host
+#define SYNC_REG_GUEST_TIMELINE_HANDLE_HIGH  0x48 // 64-bit part
+
+#define GUEST_TRIGGERED(cmd) (cmd == CMD_SYNC_READY || \
+                              cmd == CMD_TRIGGER_HOST_WAIT)
+
+typedef void (*trigger_wait_fn_t)(uint64_t, uint64_t, uint64_t);
 
 uint64_t goldfish_sync_create_timeline();
 int goldfish_sync_create_fence(uint64_t timeline, uint32_t pt);
 void goldfish_sync_timeline_inc(uint64_t timeline, uint32_t howmuch);
 void goldfish_sync_destroy_timeline(uint64_t timeline);
 
-typedef struct GoldfishSyncHwFuncs {
-    uint64_t (*createTimeline)();
-    int (*createFence)(uint64_t timeline, uint32_t pt);
-    void (*incTimeline)(uint64_t timeline, uint32_t howmuch);
-    void (*destroyTimeline)(uint64_t timeline);
-} GoldfishSyncHwFuncs;
+// Registering callbacks for goldfish sync ops
+// Currently, only trigger_wait is supported.
+void goldfish_sync_register_trigger_wait(
+        trigger_wait_fn_t trigger_fn);
 
-extern const GoldfishSyncHwFuncs*
-goldfish_sync_set_hw_funcs(const GoldfishSyncHwFuncs* hw_funcs);
+typedef struct GoldfishSyncHwFuncs2 {
+    void (*goldfishSyncSendCmd)(void* cmd);
+    void (*goldfishSyncRecvCmd)(void* cmd);
+} GoldfishSyncHwFuncs2;
+
+typedef void (*queue_device_command_t)
+    (uint32_t cmd, uint64_t handle, uint32_t time_arg,
+     uint64_t hostcmd_handle);
+
+typedef void (*device_command_result_t)
+    (void* data,
+     uint32_t cmd, uint64_t handle, uint32_t time_arg,
+     uint64_t hostcmd_handle);
+
+typedef struct GoldfishSyncDeviceInterface {
+    // Callback for all communications with virtual device
+    queue_device_command_t doHostCommand;
+    device_command_result_t receiveCommandResult;
+
+    // Callbacks to register other callbacks
+    void (*registerTriggerWait)(trigger_wait_fn_t);
+
+    // Pointer to command queue
+    void* context;
+} GoldfishSyncDeviceInterface;
+
+extern void
+goldfish_sync_set_hw_funcs(GoldfishSyncDeviceInterface* hw_funcs);
+
+extern void
+goldfish_sync_set_cmd_receiver(GoldfishSyncDeviceInterface* hw_funcs,
+                               device_command_result_t next_recv);
 
 ANDROID_END_HEADER
