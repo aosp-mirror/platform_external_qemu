463911980a2f3b11e40ca015773ba67d7dbd3320
Yahan Zhou <yahan@google.com>
2016-07-08 17:28:53 -0700

    [WIP] Add pre-process pipe for gralloc memory cleanup
    https://buganizer.corp.google.com/issues/29457657
    
    Change-Id: I8add9eb8f224cb32ffdfd309bf921c6921230a96

---
+++ android/opengl/GrallocPipe.cpp
@@ -0,0 +1,75 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/emulation/AndroidPipe.h"
+
+#include "android/opengles.h"
+#include <assert.h>
+#include <memory>
+
+namespace android {
+namespace opengl {
+
+namespace {
+
+// TODO: explain what it is doing
+
+class GrallocPipe : public AndroidPipe {
+public:
+    //////////////////////////////////////////////////////////////////////////
+    // The pipe service class for this implementation.
+    class Service : public AndroidPipe::Service {
+    public:
+        Service() : AndroidPipe::Service("gralloc") {}
+
+        virtual AndroidPipe* create(void* mHwPipe, const char* args) const
+                override {
+            return new GrallocPipe(mHwPipe, this);
+        }
+    };
+
+    GrallocPipe(void* hwPipe, const Service* service)
+            : AndroidPipe(hwPipe, service) {}
+
+    virtual void onGuestClose() override {
+        // process died on the guest, cleanup gralloc memory on the host
+        fprintf(stderr, "guest pid %d closed\n", m_guestPid);
+        android_cleanupProcColorbuffers(m_guestPid);
+    }
+    virtual unsigned onGuestPoll() const override {
+        return PIPE_POLL_IN | PIPE_POLL_OUT;
+    }
+    virtual int onGuestRecv(AndroidPipeBuffer* buffers, int numBuffers) {
+        assert(buffers[0].size >= 4);
+        memcpy(buffers[0].data, (const char*)&m_guestPid, 4);
+        return 4;
+    }
+    virtual int onGuestSend(const AndroidPipeBuffer* buffers,
+                            int numBuffers) override {
+        assert(buffers[0].size >= 4);
+        m_guestPid = *((const int *)(buffers[0].data));
+        fprintf(stderr, "recv guest pid %d\n", m_guestPid);
+        return 4;
+    }
+    virtual void onGuestWantWakeOn(int flags) override {}
+private:
+    // Process ID on the guest
+    int m_guestPid;
+};
+
+}
+
+void registerGrallocPipeService() {
+    android::AndroidPipe::Service::add(new GrallocPipe::Service());
+}
+
+}
+}
--- distrib/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ distrib/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -598,10 +598,9 @@ HandleType FrameBuffer::genHandle()
     return id;
 }
 
-HandleType FrameBuffer::createColorBuffer(int p_width, int p_height,
+HandleType FrameBuffer::createColorBufferLocked(int p_width, int p_height,
                                           GLenum p_internalFormat)
 {
-    emugl::Mutex::AutoLock mutex(m_lock);
     HandleType ret = 0;
 
     ColorBufferPtr cb(ColorBuffer::create(
@@ -615,7 +614,24 @@ HandleType FrameBuffer::createColorBuffer(int p_width, int p_height,
         ret = genHandle();
         m_colorbuffers[ret].cb = cb;
         m_colorbuffers[ret].refcount = 1;
+        fprintf(stderr, "cb created %d:%d\n", ret, 1);
     }
+    return ret;
+}
+
+HandleType FrameBuffer::createColorBuffer(int p_width, int p_height,
+                                          GLenum p_internalFormat) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    return createColorBufferLocked(p_width, p_height, p_internalFormat);
+}
+
+HandleType FrameBuffer::createColorBufferPid(int p_width, int p_height,
+                                          GLenum p_internalFormat, int pid) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    fprintf(stderr, "%d:", pid);
+    HandleType ret = createColorBufferLocked(
+                            p_width, p_height, p_internalFormat);
+    m_procOnwedColorBuffers[pid].insert(ret);
     return ret;
 }
 
@@ -730,9 +746,8 @@ void FrameBuffer::DestroyWindowSurface(HandleType p_surface)
     }
 }
 
-int FrameBuffer::openColorBuffer(HandleType p_colorbuffer)
+int FrameBuffer::openColorBufferLocked(HandleType p_colorbuffer)
 {
-    emugl::Mutex::AutoLock mutex(m_lock);
     ColorBufferMap::iterator c(m_colorbuffers.find(p_colorbuffer));
     if (c == m_colorbuffers.end()) {
         // bad colorbuffer handle
@@ -740,12 +755,25 @@ int FrameBuffer::openColorBuffer(HandleType p_colorbuffer)
         return -1;
     }
     (*c).second.refcount++;
+    fprintf(stderr, "cb opened %d:%d\n", p_colorbuffer, (*c).second.refcount);
     return 0;
 }
 
-void FrameBuffer::closeColorBuffer(HandleType p_colorbuffer)
-{
+int FrameBuffer::openColorBuffer(HandleType p_colorbuffer) {
     emugl::Mutex::AutoLock mutex(m_lock);
+    return openColorBufferLocked(p_colorbuffer);
+}
+
+int FrameBuffer::openColorBufferPid(HandleType p_colorbuffer, int pid) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    fprintf(stderr, "%d:", pid);
+    int ret = openColorBufferLocked(p_colorbuffer);
+    m_procOnwedColorBuffers[pid].insert(p_colorbuffer);
+    return ret;
+}
+
+void FrameBuffer::closeColorBufferLocked(HandleType p_colorbuffer)
+{
     ColorBufferMap::iterator c(m_colorbuffers.find(p_colorbuffer));
     if (c == m_colorbuffers.end()) {
         // This is harmless: it is normal for guest system to issue
@@ -754,9 +782,33 @@ void FrameBuffer::closeColorBuffer(HandleType p_colorbuffer)
         // to give guest a notice yet)
         return;
     }
+    fprintf(stderr, "cb closed %d:%d\n", p_colorbuffer, (*c).second.refcount-1);
     if (--(*c).second.refcount == 0) {
         m_colorbuffers.erase(c);
     }
+}
+
+void FrameBuffer::closeColorBuffer(HandleType p_colorbuffer) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    closeColorBufferLocked(p_colorbuffer);
+}
+
+void FrameBuffer::closeColorBufferPid(HandleType p_colorbuffer, int pid) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    fprintf(stderr, "%d:", pid);
+    closeColorBufferLocked(p_colorbuffer);
+    m_procOnwedColorBuffers[pid].erase(p_colorbuffer);
+}
+
+void FrameBuffer::onProcKilled(int pid) {
+    emugl::Mutex::AutoLock mutex(m_lock);
+    auto procIte = m_procOnwedColorBuffers.find(pid);
+    if (procIte == m_procOnwedColorBuffers.end()) return;
+    for (auto cb: procIte->second) {
+        //fprintf(stderr, "%d killed, draining cb %d\n", pid, cb);
+        closeColorBufferLocked(cb);
+    }
+    m_procOnwedColorBuffers.erase(procIte);
 }
 
 bool FrameBuffer::flushWindowSurfaceColorBuffer(HandleType p_surface)
