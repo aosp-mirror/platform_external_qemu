04097cf5dc1798ba50eb4e705a3289dbc0eade61
Yahan Zhou <yahan@google.com>
2016-07-08 17:28:53 -0700

    Add pre-process pipe for gralloc memory cleanup
    https://buganizer.corp.google.com/issues/29457657
    
    This patch clean up color buffers allocated by gralloc when a guest
    process is killed. It did two things:
    
    (1) It creates a grallocPipe the first time gralloc registers any
    buffers in a host process. The pipe will be released when the process
    dies, which triggers a callback function to clean up its color
    buffers.
    (2) For color buffer create/open/close, add a new render control
    function, which passes the guest process id as an additional parameter.
    Then we keep track of which process is holding which color buffers.
    
    The guest processes are identified by generating a 64bit ID for each
    process from the host.
    
    It will need the guest side patch to work:
    https://googleplex-android-review.git.corp.google.com/#/c/1207896/
    
    Change-Id: I8add9eb8f224cb32ffdfd309bf921c6921230a96

---
+++ android/opengl/GrallocPipe.cpp
@@ -0,0 +1,90 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/emulation/AndroidPipe.h"
+
+#include "android/opengles.h"
+#include <assert.h>
+#include <atomic>
+#include <memory>
+
+namespace android {
+namespace opengl {
+
+namespace {
+
+// GrallocPipe is a pipe service that is used for releasing gralloc resources
+// per guest process. Gralloc acquires host color buffer handles that needs to
+// be properly released when guest process crashes. This class is used to detect
+// if guest process exists, so that a proper cleanup function can be called.
+
+
+// It is done by setting up a pipe per guest process before acquiring color
+// buffer handles. When guest process exists, the pipe will be closed, and
+// onGuestClose() will trigger the cleanup path.
+
+class GrallocPipe : public AndroidPipe {
+public:
+    //////////////////////////////////////////////////////////////////////////
+    // The pipe service class for this implementation.
+    class Service : public AndroidPipe::Service {
+    public:
+        Service() : AndroidPipe::Service("grallocPipe") {}
+
+        virtual AndroidPipe* create(void* mHwPipe, const char* args) const
+                override {
+            return new GrallocPipe(mHwPipe, this);
+        }
+    };
+
+    GrallocPipe(void* hwPipe, const Service* service)
+            : AndroidPipe(hwPipe, service) {
+        m_uniqueId = ++s_headId;
+    }
+
+    virtual void onGuestClose() override {
+        // process died on the guest, cleanup gralloc memory on the host
+        android_cleanupProcColorbuffers(m_uniqueId);
+    }
+    virtual unsigned onGuestPoll() const override {
+        return PIPE_POLL_IN | PIPE_POLL_OUT;
+    }
+    virtual int onGuestRecv(AndroidPipeBuffer* buffers, int numBuffers) {
+        assert(buffers[0].size >= 8);
+        memcpy(buffers[0].data, (const char*)&m_uniqueId, sizeof(m_uniqueId));
+        return sizeof(m_uniqueId);
+    }
+    virtual int onGuestSend(const AndroidPipeBuffer* buffers,
+                            int numBuffers) override {
+        // The guest is supposed to send us the guest process id (4 bytes).
+        // But we don't use it.
+        // Not doing anything
+        assert(buffers[0].size >= 4);
+        return buffers[0].size;
+    }
+    virtual void onGuestWantWakeOn(int flags) override {}
+private:
+    // A (supposed to be) unique ID assigned from the host per guest process
+    // Please change it if you ever have a use case that exhausts them
+    uint64_t m_uniqueId;
+    static std::atomic_ullong s_headId;
+};
+
+std::atomic_ullong GrallocPipe::s_headId(0);
+
+}
+
+void registerGrallocPipeService() {
+    android::AndroidPipe::Service::add(new GrallocPipe::Service());
+}
+
+}
+}
