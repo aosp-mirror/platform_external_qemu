85315d51237b6b5636f3a9dc69a712f370057c67
Lingfeng Yang <lfy@google.com>
2016-07-15 14:35:41 -0700

    FenceSyncInfo class: track GLsync objects on host
    The purpose of FenceSyncInfo is to track the state
    of OpenGL sync objects for implementing
    EGL_KHR_fence_sync and EGL_ANDROID_native_fence_sync
    extensions.
    
    FenceSyncInfo:
    - Wraps host-side EGL sync objects, providing a
      handle that the guest can use
    - Lets us query a sync object for whether it has been
      signaled already, by querying existence in a global
      map of sync objects.
    - Can live and one place and safely interact with
      multiple threads at once, so we can transfer information
      about sync objects across threads.
    
    This is part of a sequential, multi-CL change:
    
    external/qemu:
    
    https://android-review.googlesource.com/240119
    https://android-review.googlesource.com/239442
    https://android-review.googlesource.com/221593 <- needed
    https://android-review.googlesource.com/248563 <- this CL
    https://android-review.googlesource.com/248564 <- needs this
    https://android-review.googlesource.com/223032 <- needs this
    
    external/qemu-android:
    
    https://android-review.googlesource.com/240155
    https://android-review.googlesource.com/238790
    
    kernel/goldfish:
    
    https://android-review.googlesource.com/232631
    https://android-review.googlesource.com/238399
    
    Change-Id: I76be0fbe8de1e23f1fdc49de9b926f9348ce5d8e

---
+++ distrib/android-emugl/host/libs/libOpenglRender/FenceSyncInfo.h
@@ -0,0 +1,83 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#pragma once
+
+#include "android/base/synchronization/Lock.h"
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include <memory>
+#include <unordered_map>
+
+// The purpose of FenceSyncInfo is to track the progress
+// of OpenGL fence commands issued from the guest,
+// and to have lower host driver overhead when
+// waiting on fence commands to be completed.
+//
+// Tracking is necessary because fence objects on the host
+// are just opaque pointers and don't correspond to anything
+// real on the guest. When the guest calls eglCreateSyncKHR,
+// the guest receives a handle that corresponds to the
+// actual EGLSyncKHR object on the host.
+//
+// When the EGLSyncKHR object is signaled / when the guest
+// calls eglClientWaitSyncKHR, the host processes
+// the EGLSyncKHR object directly, while communicating only
+// the result of the wait command to the guest.
+
+// The FenceSync class wraps actual EGLSyncKHR objects.
+// The handle is included with the EGLSyncKHR object.
+// The OpenGL context that created the EGLSyncKHR object is also
+// added as a class member, so that it is easier to
+// create sync contexts when waiting on fence commands
+// from other threads (contexts).
+class FenceSync {
+public:
+    explicit FenceSync(EGLSyncKHR eglsync,
+                       void* cxt = nullptr) :
+        sync(eglsync), gl_cxt(cxt) { }
+    uint32_t getHandle() const { return reinterpret_cast<uint64_t>(sync); }
+    EGLSyncKHR sync;
+    void* gl_cxt;
+};
+
+// The FenceSyncInfo class maps handles to FenceSync object pointers.
+// When eglClientWaitSyncKHR is called, we look up the
+// FenceSync object corresponding to the input handle,
+// and check whether it is already signaled by checking
+// for its presence in the map.
+//
+// If it is already signaled, the object is not present in the map,
+// and we can save the cost of calling out to the host OpenGL driver.
+// Otherwise, we will call glClientWaitSync.
+class FenceSyncInfo {
+public:
+    FenceSyncInfo() = default;
+
+    void addSync(FenceSync* fencesync);
+    void removeSync(uint64_t handle);
+
+    void setSignaled(uint64_t handle);
+    FenceSync* findNonSignaledSync(uint64_t handle);
+
+    static FenceSyncInfo* get();
+private:
+    std::unordered_map<uint64_t, std::unique_ptr<FenceSync> > mSyncMap;
+    android::base::ReadWriteLock mRWLock;
+    DISALLOW_COPY_ASSIGN_AND_MOVE(FenceSyncInfo);
+};
