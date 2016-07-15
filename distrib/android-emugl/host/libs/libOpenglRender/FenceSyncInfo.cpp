4991b9ba51bd90ed5c90624aab0f0e76bc98b53c
Lingfeng Yang <lfy@google.com>
2016-07-15 14:35:41 -0700

    FenceSyncInfo class: track GLsync objects on host
    The purpose of FenceSyncInfo is to track the state
    of OpenGL sync objects for implementing
    EGL_KHR_fence_sync and EGL_ANDROID_native_fence_sync
    extensions.
    
    FenceSyncInfo:
    - Wraps host-side EGL sync objects, providing a
      handle that the guest can use
    - Lets us query a sync object for whether it has been
      signaled already, so we don't need to incur
      extra calls
    - Can live and one place and safely interact with
      multiple threads at once, so we can transfer information
      about sync objects across threads.
    
    This is part of a sequential, multi-CL change:
    
    external/qemu:
    
    https://android-review.googlesource.com/240119
    https://android-review.googlesource.com/239442
    https://android-review.googlesource.com/221593 <- needed
    https://android-review.googlesource.com/248563 <- this CL
    https://android-review.googlesource.com/248564 <- needs this
    https://android-review.googlesource.com/223032 <- needs this
    
    external/qemu-android:
    
    https://android-review.googlesource.com/240155
    https://android-review.googlesource.com/238790
    
    kernel/goldfish:
    
    https://android-review.googlesource.com/232631
    https://android-review.googlesource.com/238399
    
    Change-Id: I76be0fbe8de1e23f1fdc49de9b926f9348ce5d8e

---
+++ distrib/android-emugl/host/libs/libOpenglRender/FenceSyncInfo.cpp
@@ -0,0 +1,68 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "FenceSyncInfo.h"
+
+#include "android/base/Compiler.h"
+#include "android/base/memory/LazyInstance.h"
+#include "android/base/synchronization/Lock.h"
+
+#include "android/utils/assert.h"
+
+using android::base::AutoReadLock;
+using android::base::AutoWriteLock;
+using android::base::ReadWriteLock;
+
+void FenceSyncInfo::addSync(FenceSync* fencesync) {
+    AutoWriteLock lock(mRWLock);
+    if (!fencesync) {
+        SyncHandledInfo& info = mSyncMap[0];
+        info.signaled = true;
+        info.fence_sync.reset();
+    } else {
+        SyncHandledInfo& info = mSyncMap[fencesync->getHandle()];
+        info.signaled = false;
+        info.fence_sync.reset(fencesync);
+    }
+}
+
+void FenceSyncInfo::setSignaled(uint64_t handle) {
+    AutoWriteLock lock(mRWLock);
+    SyncHandledInfo& info = mSyncMap[handle];
+    info.signaled = true;
+}
+
+FenceSync* FenceSyncInfo::findNonSignaledSync(uint64_t handle) {
+    AutoReadLock lock(mRWLock);
+    auto it = mSyncMap.find(handle);
+
+    if (it == mSyncMap.end() ||
+        it->second.signaled) {
+        // The sync has either been signaled
+        // or wasn't there at all.
+        // This function is to return
+        // non-signaled syncs only.
+        return nullptr;
+    }
+
+    return it->second.fence_sync.get();
+}
+
+::android::base::LazyInstance<FenceSyncInfo> sFenceSyncInfo = LAZY_INSTANCE_INIT;
+
+FenceSyncInfo* FenceSyncInfo::get() {
+    return sFenceSyncInfo.ptr();
+}
