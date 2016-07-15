caf78b01101ca5bb4ae2adeee78b00a664f80db9
Lingfeng Yang <lfy@google.com>
2016-07-15 08:25:11 -0700

    AndroidPipe et al: Generalize main-loop IO
    Virtual devices such as AndroidPipe and later, Goldfish sync
     or even virtual audio devices, need to be holding VM lock
    when raising IRQ.
    
    This CL makes it easier to add more of the same
    functionality, through the DeviceContextRunner class.
    
    To use DeviceContextRunner, one needs to introduce:
    
    1. A type (usu. struct) representing type of data needed
       when raising IRQs.
    2. The "device operation", which actually raises IRQs and does
       device-specific stuff.
    
    An example with AndroidPipe is given.
    
    Note: On Antutu benchmark, there seemed not to be any
    performance degradation when using a vector of pipe wakes
    instead of a map of OR'ed pipe wakes.
    
    Note: Also took out the TestVmLock class for use in
    both unit tests for VmLock and for DeviceContextRunner.
    
    Change-Id: I14bba0daf6718bfc82b91f8cd79d9560eda61f14

---
+++ android/emulation/testing/TestVmLock.h
@@ -0,0 +1,43 @@
+// Copyright 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/emulation/VmLock.h"
+
+namespace android {
+
+class TestVmLock : public VmLock {
+public:
+    TestVmLock() : mOldVmLock(VmLock::set(this)) {}
+
+    ~TestVmLock() { release(); }
+
+    void release() {
+        if (mOldVmLock) {
+            VmLock::set(mOldVmLock);
+            mOldVmLock = nullptr;
+        }
+    }
+
+    virtual void lock() override { mLockCount++; }
+    virtual void unlock() override { mUnlockCount++; }
+    virtual bool isLockedBySelf() const override {
+        return mLockCount > mUnlockCount;
+    }
+
+    int mLockCount = 0;
+    int mUnlockCount = 0;
+    VmLock* mOldVmLock = nullptr;
+};
+
+} // namespace android
