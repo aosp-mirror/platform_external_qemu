a4855aacd0c4da8ae5e6bf056564075fde35c8f1
David 'Digit' Turner <digit@google.com>
2016-07-20 19:16:30 +0200

    AdbGuestPipe: new class to handle ADB guest pipe connections.
    This patch introduces a new AndroidPipe derived class used to
    model ADB guest pipe connections, and interact with an
    AdbHostListener at runtime.
    
    + unit-tests.
    
    Change-Id: Iee4af18c986ead61b5104601c177a26163f19ac2

--- Makefile.android-emu.mk
+++ Makefile.android-emu.mk
@@ -221,6 +221,7 @@ LOCAL_SRC_FILES := \
     android/crashreport/CrashReporter_common.cpp \
     android/crashreport/CrashReporter_$(BUILD_TARGET_OS).cpp \
     android/curl-support.c \
+    android/emulation/AdbGuestPipe.cpp \
     android/emulation/AdbHostListener.cpp \
     android/emulation/AndroidPipe.cpp \
     android/emulation/android_pipe_host.cpp \
@@ -458,6 +459,7 @@ LOCAL_SRC_FILES := \
   android/base/Version_unittest.cpp \
   android/cmdline-option_unittest.cpp \
   android/console_auth_unittest.cpp \
+  android/emulation/AdbGuestPipe_unittest.cpp \
   android/emulation/AdbHostListener_unittest.cpp \
   android/emulation/android_pipe_pingpong_unittest.cpp \
   android/emulation/android_pipe_zero_unittest.cpp \
