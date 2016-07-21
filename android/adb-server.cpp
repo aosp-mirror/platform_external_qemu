c262033720222328a1a75a4e807eeee8b704270d
David 'Digit' Turner <digit@google.com>
2016-07-21 13:23:20 +0200

    Remove old adb-qemud / adb-server code.
    And replace it with our new AdbHostListener / AdbGuestPipe
    classes. However, due to a bug / bandaid in the way the
    ADB communication mode is computed for API levels >= 16,
    this code is not used yet.
    
    See next patch for the actual switch and performance
    numbers.
    
    Change-Id: I886edd0bee03ed61b7131382b8067441b9946acf

---
+++ android/adb-server.cpp
@@ -0,0 +1,94 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/adb-server.h"
+
+#include "android/base/files/StdioStream.h"
+#include "android/base/files/Stream.h"
+#include "android/base/memory/LazyInstance.h"
+#include "android/base/system/System.h"
+#include "android/emulation/AdbDebugPipe.h"
+#include "android/emulation/AdbGuestPipe.h"
+#include "android/emulation/AdbHostListener.h"
+#include "android/emulation/AndroidPipe.h"
+#include "android/utils/debug.h"
+
+#include <memory>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace {
+
+using android::emulation::AdbGuestPipe;
+using android::emulation::AdbHostListener;
+using android::AndroidPipe;
+
+// Global variables used here.
+struct Globals {
+    Globals() : hostListener(getClientPort()) {}
+
+    void registerServices() {
+        // Register adb pipe service.
+        pipeService = new AdbGuestPipe::Service(&hostListener);
+        AndroidPipe::Service::add(pipeService);
+
+        // Register adb-debug pipe service.
+        android::base::Stream* debugStream = nullptr;
+        if (VERBOSE_CHECK(adb)) {
+            debugStream = new android::base::StdioStream(stderr);
+        }
+        android::AndroidPipe::Service::add(
+                new android::emulation::AdbDebugPipe::Service(debugStream));
+        AndroidPipe::Service::add(pipeService);
+    }
+
+    static int getClientPort() {
+        int result = 5037;
+        auto env = android::base::System::get()->envGet(
+                "ANDROID_ADB_SERVER_PORT");
+        if (!env.empty()) {
+            result = static_cast<int>(strtol(env.c_str(), NULL, 10));
+        }
+        return result;
+    }
+
+    AdbHostListener hostListener;
+    AdbGuestPipe::Service* pipeService = nullptr;
+};
+
+android::base::LazyInstance<Globals> sGlobals = LAZY_INSTANCE_INIT;
+
+}  // namespace
+
+void android_adb_server_notify(int port) {
+    auto globals = sGlobals.ptr();
+    globals->hostListener.reset(port);
+    globals->hostListener.notifyServer();
+}
+
+int android_adb_server_init(int port) {
+    auto globals = sGlobals.ptr();
+    if (!globals->hostListener.reset(port)) {
+        return -1;
+    }
+    return 0;
+}
+
+void android_adb_server_undo_init(void) {
+    sGlobals->hostListener.reset(-1);
+}
+
+void android_adb_service_init(void) {
+    // Register adb pipe service.
+    sGlobals->registerServices();
+}
