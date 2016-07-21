1dbfa3de25b89664183f398d316a4f6ca2cbb6a5
Yahan Zhou <yahan@google.com>
2016-07-21 15:59:43 -0700

    Refactor GL name manager
    Refactor GL name manager with smart pointer. The smart pointer is mainly
    useful for texture handles, which can be shared across different context
    using egl image.
    
    Change-Id: I52636ec1ec2f2f8904f3d6d88c37b64fbe479908

---
+++ distrib/android-emugl/host/libs/Translator/GLcommon/NamedObject.cpp
@@ -0,0 +1,83 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "GLcommon/NamedObject.h"
+
+#include "emugl/common/mutex.h"
+#include "GLcommon/GLEScontext.h"
+#include "GLcommon/ObjectNameSpace.h"
+
+GenNameInfo::GenNameInfo(const GenNameInfo& genNameInfo)
+                        : m_type(genNameInfo.m_type)
+                        , m_extraInfo(genNameInfo.m_extraInfo) {}
+
+GenNameInfo::GenNameInfo(NamedObjectType type) : m_type(type) {
+    assert(type != SHADER);
+}
+
+GenNameInfo::GenNameInfo(GLenum shaderType)
+                        : m_type(SHADER) {
+    m_extraInfo.shaderType = shaderType;
+}
+
+NamedObject::NamedObject(GenNameInfo genNameInfo,
+                         GlobalNameSpace *globalNameSpace) {
+    // We need a global mutex from globalNameSpace
+    // And we can't change the mutex to a static variable because it would be
+    // passed around different shared libraries.
+    m_globalNameSpace = globalNameSpace;
+    emugl::Mutex::AutoLock _lock(m_globalNameSpace->m_lock);
+
+    switch (genNameInfo.m_type) {
+    case VERTEXBUFFER:
+        GLEScontext::dispatcher().glGenBuffers(1,&m_globalName);
+        m_glDelete = [](GLuint lname)
+                { GLEScontext::dispatcher().glDeleteBuffers(1, &lname);};
+        break;
+    case TEXTURE:
+        GLEScontext::dispatcher().glGenTextures(1,&m_globalName);
+        m_glDelete = [](GLuint lname)
+                { GLEScontext::dispatcher().glDeleteTextures(1, &lname);};
+        break;
+    case RENDERBUFFER:
+        GLEScontext::dispatcher().glGenRenderbuffersEXT(1, &m_globalName);
+        m_glDelete = [](GLuint lname)
+                { GLEScontext::dispatcher().glDeleteRenderbuffersEXT(1, &lname);};
+        break;
+    case FRAMEBUFFER:
+        GLEScontext::dispatcher().glGenFramebuffersEXT(1,&m_globalName);
+        m_glDelete = [](GLuint lname)
+                { GLEScontext::dispatcher().glDeleteFramebuffersEXT(1, &lname);};
+        break;
+    case SHADER:
+        m_globalName = GLEScontext::dispatcher().glCreateShader(
+                                            genNameInfo.m_extraInfo.shaderType);
+        m_glDelete = GLEScontext::dispatcher().glDeleteShader;
+        break;
+    case PROGRAM:
+        m_globalName = GLEScontext::dispatcher().glCreateProgram();
+        m_glDelete = GLEScontext::dispatcher().glDeleteProgram;
+        break;
+    default:
+        m_globalName = 0;
+    }
+}
+
+NamedObject::~NamedObject() {
+    emugl::Mutex::AutoLock _lock(m_globalNameSpace->m_lock);
+    m_glDelete(m_globalName);
+}
+
