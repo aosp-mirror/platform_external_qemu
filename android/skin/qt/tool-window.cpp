c53104e71e060894dc45204bdd70ad4b0a5c367b
Evan Birenbaum <birenbaum@google.com>
2016-07-25 15:17:00 -0700

    [UI] Enable the virtual sensors keyboard shortcut.
    Change-Id: I0e6ed3179d027280249aa9d2fb1da013add63561
    (cherry picked from commit 01f7986f0c02ce24a5934b30a98210ce5e45b187)

--- android/skin/qt/tool-window.cpp
+++ android/skin/qt/tool-window.cpp
@@ -96,10 +96,7 @@ ToolWindow::ToolWindow(EmulatorQtWindow* window,
             "Ctrl+Shift+C SHOW_PANE_CELLULAR\n"
             "Ctrl+Shift+B SHOW_PANE_BATTERY\n"
             "Ctrl+Shift+P SHOW_PANE_PHONE\n"
-// TODO(grigoryj): re-enable this when the virtual sensors UI is complete.
-#if 0
-        "Ctrl+Shift+V SHOW_PANE_VIRTSENSORS\n"
-#endif
+            "Ctrl+Shift+V SHOW_PANE_VIRTSENSORS\n"
             "Ctrl+Shift+F SHOW_PANE_FINGER\n"
             "Ctrl+Shift+D SHOW_PANE_DPAD\n"
             "Ctrl+Shift+S SHOW_PANE_SETTINGS\n"
