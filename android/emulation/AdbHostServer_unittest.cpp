21c5538624bb1f917d9f4f1923169d7d06a20c9f
David 'Digit' Turner <digit@google.com>
2016-07-26 10:46:37 +0200

    AdbHostListener: new class to handle ADB Server connections.
    + Add the AdbTypes.h header defining the interfaces between
      the two main components used to handle ADB host <-> guest
      connections. Separating them makes it easier to unit-test
      each implementation.
    
    + unit-tests
    
    Change-Id: Ic3e86133924e60b897683222f128893b6189acd2

--- android/emulation/AdbHostServer_unittest.cpp
+++ android/emulation/AdbHostServer_unittest.cpp
@@ -15,6 +15,7 @@
 #include "android/base/threads/Thread.h"
 #include "android/base/sockets/ScopedSocket.h"
 #include "android/base/sockets/SocketUtils.h"
+#include "android/base/testing/TestInputBufferSocketServerThread.h"
 #include "android/base/testing/TestSystem.h"
 
 #include <gtest/gtest.h>
@@ -28,82 +29,10 @@ using android::base::ScopedSocket;
 using android::base::StringView;
 using android::base::TestSystem;
 
-namespace {
-
-// Simple server thread that receives data and stores it in a buffer.
-// Usage is the following:
-//
-// 1) Create instance, passing a port to listen to the constructor.
-//    The default constructor will bind to a random port, which you
-//    can retrieve with the port() method. Check that the port could be
-//    be bound with the valid() method.
-//
-// 2) Start the thread, it will store all the data it receives into
-//    an internal buffer. The thread only stops when the connection
-//    is closed from the remote end.
-//
-// 3) Join the thread, the result is the size of the buffer in bytes, or
-//    -1 if a connection could not be accepted.
-//
-// 4) Use view() to retrieve a view of the content.
-//
-class AdbServerThread : public android::base::Thread {
-public:
-    // Create new thread instance, try to bound to specific TCP |port|,
-    // a value of 0 let the system choose a free IPv4 port, which can
-    // later be retrieved with port().
-    AdbServerThread(int port = 0)
-        : Thread(), mSocket(android::base::socketTcp4LoopbackServer(port)) {}
-
-    // Returns true if port could be bound.
-    bool valid() const { return mSocket.valid(); }
-
-    // Return bound server port.
-    int port() const { return android::base::socketGetPort(mSocket.get()); }
-
-    // Return buffer content as a StringView.
-    StringView view() const { return mString; }
-
-    // Main function simply receives everything and stores it in a string.
-    virtual intptr_t main() override {
-        // Wait for a single connection.
-        int fd = android::base::socketAcceptAny(mSocket.get());
-        if (fd < 0) {
-            LOG(ERROR) << "Could not accept one connection!";
-            return -1;
-        }
-
-        // Now read data from the connection until it closes.
-        size_t size = 0;
-        for (;;) {
-            size_t capacity = mString.size();
-            if (size >= capacity) {
-                mString.resize(1024 + capacity * 2);
-                capacity = mString.size();
-            }
-            size_t avail = capacity - size;
-            ssize_t len = android::base::socketRecv(fd, &mString[size], avail);
-            if (len <= 0) {
-                break;
-            }
-            size += len;
-        }
-        android::base::socketClose(fd);
-
-        mString.resize(size);
-        return static_cast<intptr_t>(size);
-    }
-
-private:
-    ScopedSocket mSocket;
-    std::string mString;
-};
-
-}  // namespace
 
 TEST(AdbHostServer, notify) {
     // Bind to random port to listen to
-    AdbServerThread serverThread;
+    android::base::TestInputBufferSocketServerThread serverThread;
     ASSERT_TRUE(serverThread.valid());
 
     const int emulatorPort = 7648;  // Don't use default (5554) here.
