7cf7d14cd2339f54a64f10574aca09c7155d61f4
bohu <bohu@google.com>
2016-09-07 07:41:10 -0700

    Enable userdata encryption
    The system side can use different device specific rc files and
    fstab files based on whether kernel option 'qemu.encrypt' is 1 or not.
    For old emulator, it will not set 'qemu.encrypt' and falls back to
    unencrypted userdata; for new emulator, it will check whether the
    system image has this feature (advancedFeature.ini) and enable it
    conditionally.
    
    Bug: 30897919
    Change-Id: I86c86749bada7c568c671b1dacb62d627aff5a46

--- android/avd/info.c
+++ android/avd/info.c
@@ -1142,6 +1142,12 @@ char*  avdInfo_getSdCardPath( const AvdInfo* i )
     return _avdInfo_getContentFilePath(i, imageName);
 }
 
+char* avdInfo_getEncryptionKeyImagePath(const AvdInfo* i )
+{
+    const char* imageName = _imageFileNames[ AVD_IMAGE_ENCRYPTIONKEY ];
+    return _avdInfo_getContentFilePath(i, imageName);
+}
+
 char*
 avdInfo_getSnapStoragePath( const AvdInfo* i )
 {
