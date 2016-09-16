5ed58045e00395a9af6066904c8ae71a1140d0e8
Tao Wu <lepton@google.com>
2016-09-15 17:41:01 -0700

    If env doesn't support IPv4, don't try it.
    This fixes the bug that emulator doesn't listen on adb port in pure IPv6
    env.
    
    Bug: http://b/26177732
    Change-Id: I5c525094c0342114b17f8fd59affe5142487ace7
    Signed-off-by: Tao Wu <lepton@google.com>

--- android/emulation/AdbHostListener.cpp
+++ android/emulation/AdbHostListener.cpp
@@ -17,12 +17,24 @@
 #include "android/base/sockets/ScopedSocket.h"
 #include "android/emulation/AdbHostServer.h"
 #include "android/emulation/AdbTypes.h"
+#include "android/utils/sockets.h"
 
 #include <memory>
 #include <vector>
 
 namespace android {
 namespace emulation {
+namespace {
+
+static bool detect_env_ipv4() {
+    int fd = socket_create_inet(SOCKET_STREAM);
+    if (fd < 0 && errno == EAFNOSUPPORT)
+        return false;
+    socket_close(fd);
+    return true;
+}
+
+}  // namespace
 
 using android::base::AsyncSocketServer;
 using android::base::ScopedSocket;
@@ -32,10 +44,14 @@ bool AdbHostListener::reset(int adbPort) {
         mServer.reset();
     } else if (!mServer || adbPort != mServer->port()) {
         CHECK(adbPort < 65536);
+        AsyncSocketServer::LoopbackMode mode = AsyncSocketServer::kIPv4AndOptionalIPv6;
+        if (!detect_env_ipv4()) {
+            mode = AsyncSocketServer::kIPv6;
+        }
         mServer = AsyncSocketServer::createTcpLoopbackServer(
                 adbPort,
                 [this](int port) { return this->onHostServerConnection(port); },
-                AsyncSocketServer::kIPv4AndOptionalIPv6,
+                mode,
                 android::base::ThreadLooper::get());
         if (!mServer) {
             // This can happen when the emulator is probing for a free port
