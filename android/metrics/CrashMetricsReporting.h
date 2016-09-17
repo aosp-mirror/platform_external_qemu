c30900d91facac84be3a95bd2a603371810368fc
Yurii Zubrytskyi <zyy@google.com>
2016-09-17 15:33:47 -0700

    [metrics] A function to report metrics for crashed emulators
    This function takes a list of crashed emulator sessions and
    writes corresponding metrics messages
    
    Change-Id: Ib159fef25af3022adbdb39507171570349b39ac0

---
+++ android/metrics/CrashMetricsReporting.h
@@ -0,0 +1,28 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/metrics/MetricsReporter.h"
+#include "android/metrics/MetricsWriter.h"
+
+namespace android {
+namespace metrics {
+
+//
+// This function sends a bunch of metrics messages to report crashed sessions
+// passed in |sessions| parameter using |reporter|.
+//
+void reportCrashMetrics(MetricsReporter& reporter,
+                        const MetricsWriter::AbandonedSessions& sessions);
+
+}  // namespace metrics
+}  // namespace android
