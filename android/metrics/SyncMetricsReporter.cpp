c81e4cc6c82dbdeaaee92c5e9c64084a3295eb26
Yurii Zubrytskyi <zyy@google.com>
2016-09-16 18:45:08 -0700

    [metrics] Actual implementations of MetricsReporter
    Change-Id: I5499f4205fd9fc2fa1d6fbef655630e160ea0630

---
+++ android/metrics/SyncMetricsReporter.cpp
@@ -0,0 +1,35 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/metrics/SyncMetricsReporter.h"
+
+#include "android/metrics/MetricsLogging.h"
+#include "android/metrics/proto/studio_stats.pb.h"
+
+namespace android {
+namespace metrics {
+
+SyncMetricsReporter::SyncMetricsReporter(MetricsWriter::Ptr writer)
+    : MetricsReporter(true, std::move(writer), {}, {}, {}) {
+    D("created");
+}
+
+void SyncMetricsReporter::reportConditional(ConditionalCallback callback) {
+    if (callback) {
+        D("executing a reporting callback");
+        android_studio::AndroidStudioEvent event;
+        callback(&event);
+        sendToWriter(&event);
+    }
+}
+
+}  // namespace metrics
+}  // namespace android
