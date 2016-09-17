8dd34dc638178adbe134f109a6992304557d6f52
Yurii Zubrytskyi <zyy@google.com>
2016-09-16 18:45:08 -0700

    [metrics] Actual implementations of MetricsReporter
    Change-Id: I5499f4205fd9fc2fa1d6fbef655630e160ea0630

---
+++ android/metrics/AsyncMetricsReporter.cpp
@@ -0,0 +1,60 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/metrics/AsyncMetricsReporter.h"
+
+#include "android/metrics/MetricsLogging.h"
+#include "android/metrics/proto/studio_stats.pb.h"
+
+namespace android {
+namespace metrics {
+
+AsyncMetricsReporter::AsyncMetricsReporter(
+        MetricsWriter::Ptr writer,
+        base::StringView emulatorVersion,
+        base::StringView emulatorFullVersion,
+        base::StringView qemuVersion)
+    : MetricsReporter(true, std::move(writer),
+                      emulatorVersion, emulatorFullVersion, qemuVersion),
+      mWorkerThread([this]() { worker(); }) {
+    D("created");
+    mWorkerThread.start();
+}
+
+AsyncMetricsReporter::~AsyncMetricsReporter() {
+    // Send an empty callback as a stop request.
+    D("sending a stop request to worker");
+    mCallbackQueue.send({});
+    mWorkerThread.wait();
+}
+
+void AsyncMetricsReporter::reportConditional(ConditionalCallback callback) {
+    if (callback) {
+        D("sending a callback for reporting to worker");
+        mCallbackQueue.send(std::move(callback));
+    }
+}
+
+void AsyncMetricsReporter::worker() {
+    while (auto cb = mCallbackQueue.receive()) {
+        if (!*cb) {
+            D("received a stop request");
+            break;
+        }
+        android_studio::AndroidStudioEvent event;
+        if ((*cb)(&event)) {
+            sendToWriter(&event);
+        }
+    }
+}
+
+}  // namespace metrics
+}  // namespace android
