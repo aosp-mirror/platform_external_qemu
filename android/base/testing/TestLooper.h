0f436d6aea10210a440ee33d56b050e0cf4002a0
Yurii Zubrytskyi <zyy@google.com>
2016-09-21 19:00:02 -0700

    [base] Split out GenLooper and create a TestLooper
    Current GetLooper code doesn't allow one to inspect its
    internals during testing, and GenLooper is really not
    supposed to be used anywhere else.
    That's why this CL moves it out into a separate file and
    adds a subclass, TestLooper, with a needed set of operations.
    
    + Use System class to get current time instead of a direct
      syscall in the looper. This allows UTs to override it
    
    Change-Id: I88c56c25d4efcf2e517a1eb89a74fe9e3d731125

---
+++ android/base/testing/TestLooper.h
@@ -0,0 +1,62 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/base/async/GenLooper.h"
+
+namespace android {
+namespace base {
+
+// A TestLooper is a Looper implementation that allows its user to inspect all
+// internals.
+//
+class TestLooper : public GenLooper {
+public:
+    using GenLooper::GenLooper;
+
+    // Add some functions to uncover the internal state
+    const TimerSet& timers() const;
+    const TimerList& activeTimers() const;
+    const TimerList& pendingTimers() const;
+
+    const FdWatchSet& fdWatches() const;
+    const FdWatchList& pendingFdWatches() const;
+
+    bool exitRequested() const;
+};
+
+const GenLooper::TimerSet& TestLooper::timers() const {
+    return mTimers;
+}
+
+const GenLooper::TimerList& TestLooper::activeTimers() const {
+    return mActiveTimers;
+}
+
+const GenLooper::TimerList& TestLooper::pendingTimers() const {
+    return mPendingTimers;
+}
+
+const GenLooper::FdWatchSet& TestLooper::fdWatches() const {
+    return mFdWatches;
+}
+
+const GenLooper::FdWatchList& TestLooper::pendingFdWatches() const {
+    return mPendingFdWatches;
+}
+
+bool TestLooper::exitRequested() const {
+    return mForcedExit;
+}
+
+}  // namespace base
+}  // namespace android
