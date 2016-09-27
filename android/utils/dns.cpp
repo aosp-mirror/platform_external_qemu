a895e7d61023687204fe0dacc48c6bd003571380
David 'Digit' Turner <digit@google.com>
2016-09-27 17:49:47 +0200

    android/base/network/Dns.h: new helper class.
    This introduces a new helper class used to resolve hostnames
    into IpAddress list instances. This patch also provides a
    TestDnsResolver class that can be used to mock the system
    resolver during unit-testing.
    
    Change-Id: I68dac0fff70897db14c58edf2c9eab2d89d9cb79

--- android/utils/dns.cpp
+++ android/utils/dns.cpp
@@ -12,95 +12,42 @@
 
 #include "dns.h"
 
-#include "android/utils/debug.h"
-#include "android/utils/ipaddr.h"
-#include "android/utils/sockets.h"
+#include "android/base/network/Dns.h"
+#include "android/base/network/IpAddress.h"
 
-#ifdef _WIN32
-#  include "android/base/sockets/Winsock.h"
-#  include <iphlpapi.h>
-#  include <vector>
-#else
-#  include <arpa/inet.h>
-#  include <fstream>
-#endif
+#include "android/utils/debug.h"
 
 #include <algorithm>
 #include <string>
 
-#ifdef _WIN32
+using android::base::Dns;
+using android::base::IpAddress;
+
 int android_dns_get_system_servers(uint32_t* buffer, size_t bufferSize) {
-    std::vector<char> fixedInfoBuffer(sizeof(FIXED_INFO));
-    FIXED_INFO* fixedInfo = reinterpret_cast<FIXED_INFO*>(&fixedInfoBuffer[0]);
-    ULONG bufLen = fixedInfoBuffer.size();
-
-    if (ERROR_BUFFER_OVERFLOW == GetNetworkParams(fixedInfo, &bufLen)) {
-        fixedInfoBuffer.resize(bufLen);
-        fixedInfo = reinterpret_cast<FIXED_INFO*>(&fixedInfoBuffer[0]);
-    }
-
-    if (GetNetworkParams(fixedInfo, &bufLen) != ERROR_SUCCESS) {
-        derror("Failed to get network parameters, cannot retrieve DNS servers");
+    Dns::AddressList list = Dns::getSystemServerList();
+    if (list.empty()) {
+        derror("Failed to retrieve DNS servers list from the system");
         return kAndroidDnsErrorBadServer;
     }
 
-    size_t dnsAddrCount = 0;
-    for (IP_ADDR_STRING* ipAddr = &fixedInfo->DnsServerList;
-            ipAddr && dnsAddrCount < bufferSize;
-            ipAddr = ipAddr->Next) {
-        uint32_t ip;
-        if (inet_strtoip(ipAddr->IpAddress.String, &ip) == 0) {
-            buffer[dnsAddrCount++] = ip;
+    size_t n = 0;
+    for (const auto& addr : list) {
+        if (!addr.isIpv4()) {
+            continue;
         }
-    }
-
-    if (dnsAddrCount == 0)
-        return kAndroidDnsErrorBadServer;
-
-    return dnsAddrCount;
-}
-
-#else
-
-int android_dns_get_system_servers(uint32_t* buffer, size_t bufferSize)
-{
-#ifdef CONFIG_DARWIN
-    /* on Darwin /etc/resolv.conf is a symlink to /private/var/run/resolv.conf
-     * in some siutations, the symlink can be destroyed and the system will not
-     * re-create it. Darwin-aware applications will continue to run, but "legacy"
-     * Unix ones will not.
-     */
-    std::ifstream fin("/private/var/run/resolv.conf");
-    if (!fin.good()) {
-        fin.open("/etc/resolv.conf");  /* desperate attempt to sanity */
-    }
-#else
-    std::ifstream fin("/etc/resolv.conf");
-#endif
-    if (!fin.good()) {
-        derror("Failed to open /etc/resolv.conf, cannot retrieve DNS servers");
-        return kAndroidDnsErrorBadServer;
-    }
-
-    std::string line;
-    size_t dnsAddrCount = 0;
-    while (dnsAddrCount < bufferSize && std::getline(fin, line)) {
-        char nameserver[257];
-        if (sscanf(line.c_str(), "nameserver%*[ \t]%256s", nameserver) == 1) {
-            uint32_t ip;
-            if (inet_strtoip(nameserver, &ip) == 0) {
-                buffer[dnsAddrCount++] = ip;
-            }
+        if (n >= bufferSize) {
+            break;
         }
+        buffer[n++] = addr.ipv4();
     }
 
-    if (dnsAddrCount == 0)
+    if (n == 0) {
+        derror("There are no IPv4 DNS servers used in this system");
         return kAndroidDnsErrorBadServer;
+    }
 
-    return dnsAddrCount;
+    return static_cast<int>(n);
 }
-
-#endif
 
 int android_dns_parse_servers(const char* input,
                               uint32_t* buffer,
@@ -109,31 +56,29 @@ int android_dns_parse_servers(const char* input,
 
     std::replace(servers.begin(), servers.end(), ',', '\0');
 
-    size_t dnsAddrCount = 0;
+    size_t count = 0;
     for (size_t pos = 0; pos < servers.size();) {
-        SockAddress addr;
         const char* server = &servers[pos];
-        if (sock_address_init_resolve(&addr, server, 53, 0) < 0) {
+        IpAddress addr(server);
+        if (!addr.valid()) {
             return kAndroidDnsErrorBadServer;
         }
+        if (!addr.isIpv4()) {
+            continue;
+        }
 
-        if (dnsAddrCount >= bufferSize) {
+        if (count >= bufferSize) {
             return kAndroidDnsErrorTooManyServers;
         }
 
-        int ip = sock_address_get_ip(&addr);
-        if (ip == -1) {
-            return kAndroidDnsErrorBadServer;
-        }
-
-        buffer[dnsAddrCount++] = static_cast<uint32_t>(ip);
+        buffer[count++] = addr.ipv4();
         pos = servers.find('\0', pos);
         if (pos == std::string::npos) {
             break;
         }
         ++pos; // Skip the actual null terminator
     }
-    return dnsAddrCount;
+    return count;
 }
 
 int android_dns_get_servers(const char* dnsServerOption,
