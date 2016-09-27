fef2bd806d054e58c5d7c2702ef59b955d3a6a3f
David 'Digit' Turner <digit@google.com>
2016-09-27 17:49:47 +0200

    android/base/network/Dns.h: new helper class.
    This introduces a new helper class used to resolve hostnames
    into IpAddress list instances. This patch also provides a
    TestDnsResolver class that can be used to mock the system
    resolver during unit-testing.
    
    Change-Id: I68dac0fff70897db14c58edf2c9eab2d89d9cb79

---
+++ android/utils/dns_unittest.cpp
@@ -0,0 +1,36 @@
+/* Copyright 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android/utils/dns.h"
+
+#include "android/base/Log.h"
+
+#include <gtest/gtest.h>
+#include <string>
+
+static std::string ipToString(uint32_t ip) {
+    char temp[32];
+    snprintf(temp, sizeof(temp), "%d.%d.%d.%d", (ip >> 24) & 255,
+             (ip >> 16) & 255, (ip >> 8) & 255, ip & 255);
+    return std::string(temp);
+}
+
+TEST(android_dns_get_system_servers, DumpList) {
+    const size_t kMaxServers = ANDROID_MAX_DNS_SERVERS;
+    uint32_t ips[kMaxServers];
+    int count = android_dns_get_system_servers(ips, kMaxServers);
+    EXPECT_GT(count, 0);
+    EXPECT_LT(count, kMaxServers);
+    for (int n = 0; n < count; ++n) {
+        LOG(INFO) << "DNS nameserver " << ipToString(ips[n]);
+    }
+}
