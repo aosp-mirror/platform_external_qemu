56337acda19754bdd40038f117738d12ee865985
Jim Kaye <jameskaye@google.com>
2016-10-14 14:45:47 -0700

    Support 3 new screen densities for API 25
    The new densities are 260, 300, and 340 DPI.
    
    Add some unit tests, too.
    
    BUG: http://b/32172842
    
    Change-Id: I49d6f587f3f400048cea9dca1bd48664191e065c

--- android/hw-lcd.c
+++ android/hw-lcd.c
@@ -14,11 +14,8 @@
 #include <math.h>
 #include <stdio.h>
 
-void
-hwLcd_setBootProperty(int density)
-{
-    char  temp[8];
-
+int
+hwLcd_mapDensity(int density) {
     /* Map density to one of our bucket values.
        The TV density is a bit particular (and not actually a bucket
        value) so we do only exact match on it.
@@ -28,12 +25,18 @@ hwLcd_setBootProperty(int density)
             density = LCD_DENSITY_LDPI;
         else if (density < (LCD_DENSITY_MDPI + LCD_DENSITY_HDPI)/2)
             density = LCD_DENSITY_MDPI;
-        else if (density < (LCD_DENSITY_HDPI + LCD_DENSITY_280DPI)/2)
+        else if (density < (LCD_DENSITY_HDPI + LCD_DENSITY_260DPI)/2)
             density = LCD_DENSITY_HDPI;
-        else if (density < (LCD_DENSITY_280DPI + LCD_DENSITY_XHDPI)/2)
+        else if (density < (LCD_DENSITY_260DPI + LCD_DENSITY_280DPI)/2)
+            density = LCD_DENSITY_260DPI;
+        else if (density < (LCD_DENSITY_280DPI + LCD_DENSITY_300DPI)/2)
             density = LCD_DENSITY_280DPI;
-        else if (density < (LCD_DENSITY_XHDPI + LCD_DENSITY_360DPI)/2)
+        else if (density < (LCD_DENSITY_300DPI + LCD_DENSITY_XHDPI)/2)
+            density = LCD_DENSITY_300DPI;
+        else if (density < (LCD_DENSITY_XHDPI + LCD_DENSITY_340DPI)/2)
             density = LCD_DENSITY_XHDPI;
+        else if (density < (LCD_DENSITY_340DPI + LCD_DENSITY_360DPI)/2)
+            density = LCD_DENSITY_340DPI;
         else if (density < (LCD_DENSITY_360DPI + LCD_DENSITY_400DPI)/2)
             density = LCD_DENSITY_360DPI;
         else if (density < (LCD_DENSITY_400DPI + LCD_DENSITY_420DPI) / 2)
@@ -47,8 +50,20 @@ hwLcd_setBootProperty(int density)
         else
             density = LCD_DENSITY_XXXHDPI;
     }
+    return density;
+}
 
-    snprintf(temp, sizeof temp, "%d", density);
+
+void
+hwLcd_setBootProperty(int density)
+{
+    char  temp[8];
+    int mapped_density;
+
+    mapped_density = hwLcd_mapDensity(density);
+
+
+    snprintf(temp, sizeof temp, "%d", mapped_density);
     boot_property_add("qemu.sf.lcd_density", temp);
 }
 
---
+++ android/hw-lcd_unittest.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include <gtest/gtest.h>
+
+#include "android/hw-lcd.h"
+
+namespace lcd_density {
+
+TEST(Hw_lcd, MapDensity) {
+
+    EXPECT_EQ(LCD_DENSITY_TVDPI,   hwLcd_mapDensity(LCD_DENSITY_TVDPI));
+
+    EXPECT_EQ(LCD_DENSITY_LDPI,    hwLcd_mapDensity(110));
+    EXPECT_EQ(LCD_DENSITY_260DPI,  hwLcd_mapDensity(266));
+    EXPECT_EQ(LCD_DENSITY_280DPI,  hwLcd_mapDensity(272));
+    EXPECT_EQ(LCD_DENSITY_560DPI,  hwLcd_mapDensity(590));
+    EXPECT_EQ(LCD_DENSITY_XXXHDPI, hwLcd_mapDensity(610));
+    EXPECT_EQ(LCD_DENSITY_XXXHDPI, hwLcd_mapDensity(999));
+}
+
+TEST(Hw_lcd, GetScreenSize) {
+
+    // Made-up device (3.4 inches)
+    EXPECT_EQ(LCD_SIZE_SMALL,  hwLcd_getScreenSize( 240,  480, LCD_DENSITY_MDPI));
+
+    // Nexus 5 (4.95 inches)
+    EXPECT_EQ(LCD_SIZE_NORMAL, hwLcd_getScreenSize(1080, 1920, LCD_DENSITY_XXHDPI));
+
+    // Nexus 5x (5.2 inches)
+    EXPECT_EQ(LCD_SIZE_LARGE,  hwLcd_getScreenSize(1080, 1920, LCD_DENSITY_420DPI));
+
+    // Nexus 9 (8.86 inches)
+    EXPECT_EQ(LCD_SIZE_XLARGE, hwLcd_getScreenSize(2048, 1536, LCD_DENSITY_XHDPI));
+}
+
+}  // namespace density
