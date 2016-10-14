33f9911a1273cd1ac4efb3874b7a78fb95f597c7
Weilun Du <wdu@google.com>
2016-10-14 12:15:38 -0700

    Add support for android timezone and android localtime
    Previously, emulator doesn't differentiate the default timezone/localtime
    at host OS from the timezone at the AVD. We should give users the ability
    to change the networked time provided by the emulator to AVD.
    
    At the start of emulator, on linux or mac, use zdump or date command to
    retrieve timezone information. On Windows, use registry key. To return
    the "android" localtime, we compare the provided timestamp with the timezone
    information and decide what would be the correct timezone offset.
    
    Change-Id: I8727ade86ced12d08fdadc40d7e90a9643ab2c9f

--- android/utils/timezone.h
+++ android/utils/timezone.h
@@ -13,11 +13,13 @@
 #pragma once
 
 #include "android/utils/compiler.h"
+#include <time.h>
 
 ANDROID_BEGIN_HEADER
 
-/* try to set the default host timezone, returns 0 on success, or -1 if
- * 'tzname' is not in zoneinfo format (e.g. Area/Location)
+/* Try to set the default android OS timezone. This operation will affect the
+ * emulated networked time in virtual modem
+ * returns 0 on success, or -1 if 'tzname' is not in zoneinfo format (e.g. Area/Location)
  */
 extern int  timezone_set( const char*  tzname );
 
@@ -31,5 +33,17 @@ extern int  timezone_set( const char*  tzname );
  * if TZ is defined to something like "CET" or "PST", this will return the name "Unknown/Unknown"
  */
 extern char*  bufprint_zoneinfo_timezone( char*  buffer, char*  end );
+
+/* Return the timezone offset including day light saving in seconds with respect to
+ * UTC in guest android OS
+ * + positive if the timezone is east of GMT
+ * - negative if the timezone is west of GMT
+ */
+extern long android_tzoffset_in_seconds(time_t* time);
+
+/* Return local time based on android timezone NOT host OS.
+ * the caller must dereference the returned parameter struct tm* before use.
+ */
+extern struct tm* android_localtime(time_t* time);
 
 ANDROID_END_HEADER
