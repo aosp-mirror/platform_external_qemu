c80f09b7bd755a7edc60d05ae95e6804261c1496
Lingfeng Yang <lfy@google.com>
2016-10-19 08:26:53 -0700

    [video] Do YV12 conversion on the host with a shader
    Doing YV12 conversion in the guest makes gralloc unlock
    block for a very long time, which chokes MediaCodec.
    
    Doing YV12 conversion in shader, 1080p60 is achievable
    for short bursts, since the conversion is faster by
    over 10x (<3ms instead of ~30ms to convert a 1080p
    YV12 frame to RGB888).
    
    Still, at 1080p60, there are various stalls/hiccups...
    
    Change-Id: I031c4fd7b1260e89d9bc4398602ea1baea6f5e98

--- distrib/android-emugl/host/libs/libOpenglRender/RenderControl.cpp
+++ distrib/android-emugl/host/libs/libOpenglRender/RenderControl.cpp
@@ -387,7 +387,21 @@ static uint32_t rcCreateColorBuffer(uint32_t width,
         return 0;
     }
 
-    return fb->createColorBuffer(width, height, internalFormat);
+    return fb->createColorBuffer(width, height, internalFormat,
+                                 FRAMEWORK_FORMAT_GL_COMPATIBLE);
+}
+
+static uint32_t rcCreateColorBufferDMA(uint32_t width,
+                                       uint32_t height, GLenum internalFormat,
+                                       int frameworkFormat)
+{
+    FrameBuffer *fb = FrameBuffer::getFB();
+    if (!fb) {
+        return 0;
+    }
+
+    return fb->createColorBuffer(width, height, internalFormat,
+                                 (FrameworkFormat)frameworkFormat);
 }
 
 static int rcOpenColorBuffer2(uint32_t colorbuffer)
@@ -549,6 +563,7 @@ static int rcUpdateColorBuffer(uint32_t colorBuffer,
                                GLenum format, GLenum type, void* pixels)
 {
     FrameBuffer *fb = FrameBuffer::getFB();
+
     if (!fb) {
         GRSYNC_DPRINT("unlock gralloc cb lock");
         sGrallocSync->unlockColorBufferPrepare();
@@ -556,19 +571,37 @@ static int rcUpdateColorBuffer(uint32_t colorBuffer,
     }
 
     // obtain pixels from guest DMA transfer
-    fb->updateColorBuffer(colorBuffer, x, y, width, height, format, type, pixels);
+    fb->updateColorBuffer(colorBuffer, x, y, width, height, format, type,
+                          FRAMEWORK_FORMAT_GL_COMPATIBLE, pixels);
 
     GRSYNC_DPRINT("unlock gralloc cb lock");
     sGrallocSync->unlockColorBufferPrepare();
+
     return 0;
 }
 
 static int rcUpdateColorBufferDMA(uint32_t colorBuffer,
                                   GLint x, GLint y,
                                   GLint width, GLint height,
-                                  GLenum format, GLenum type)
+                                  GLenum format, GLenum type,
+                                  int frameworkFormat)
 {
-    return rcUpdateColorBuffer(colorBuffer, x, y, width, height, format, type, (void*)emuglDma);
+    FrameBuffer *fb = FrameBuffer::getFB();
+
+    if (!fb) {
+        GRSYNC_DPRINT("unlock gralloc cb lock");
+        sGrallocSync->unlockColorBufferPrepare();
+        return -1;
+    }
+
+    fb->updateColorBuffer(colorBuffer, x, y, width, height,
+                          format, type, (FrameworkFormat)frameworkFormat,
+                          (void*)emuglDma);
+
+    GRSYNC_DPRINT("unlock gralloc cb lock");
+    sGrallocSync->unlockColorBufferPrepare();
+
+    return 0;
 }
 
 static uint32_t rcCreateClientImage(uint32_t context, EGLenum target, GLuint buffer)
@@ -728,7 +761,6 @@ void initRenderControlContext(renderControl_decoder_context_t *dec)
     dec->rcColorBufferCacheFlush = rcColorBufferCacheFlush;
     dec->rcReadColorBuffer = rcReadColorBuffer;
     dec->rcUpdateColorBuffer = rcUpdateColorBuffer;
-    dec->rcUpdateColorBufferDMA = rcUpdateColorBufferDMA;
     dec->rcOpenColorBuffer2 = rcOpenColorBuffer2;
     dec->rcCreateClientImage = rcCreateClientImage;
     dec->rcDestroyClientImage = rcDestroyClientImage;
@@ -738,4 +770,6 @@ void initRenderControlContext(renderControl_decoder_context_t *dec)
     dec->rcFlushWindowColorBufferAsync = rcFlushWindowColorBufferAsync;
     dec->rcDestroySyncKHR = rcDestroySyncKHR;
     dec->rcSetPuid = rcSetPuid;
+    dec->rcUpdateColorBufferDMA = rcUpdateColorBufferDMA;
+    dec->rcCreateColorBufferDMA = rcCreateColorBufferDMA;
 }
