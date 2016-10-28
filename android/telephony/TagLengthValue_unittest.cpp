4c9daa403963fce1a7447008b945cefdb9345689
Bjoern Johansson <bjoernj@google.com>
2016-10-27 17:01:06 -0700

    Add support for SIM logical channel AT commands
    This change adds support for the AT commands CCHO, CCHC, and CGLA which
    are used to communicate with the SIM card using logical channels. This
    feature is used by the framework to look for certificate hashes that are
    stored on the SIM card. Applications that are signed with a matching
    certificate receive special privileges called Carrier Privileges. In
    addition to supporting the feature this change also adds the certificate
    hash used by the CTS test application that verifies the presence of this
    feature.
    
    BUG: 31808032
    Change-Id: I06cb3e59677623c28289dff29aca086bef30f0d5

---
+++ android/telephony/TagLengthValue_unittest.cpp
@@ -0,0 +1,162 @@
+/* Copyright (C) 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android/telephony/TagLengthValue.h"
+
+#include <gtest/gtest.h>
+
+namespace android {
+
+TEST(TagLengthValue, ConstructDeviceAppIdRefDo) {
+    DeviceAppIdRefDo deviceAppIdRefDo("C0FFEE");
+
+    // Ensure the correct tag is used and that the length and data are correct
+    ASSERT_STREQ(/*tag*/ "C1" /*length*/ "03" /*payload*/ "C0FFEE",
+                 deviceAppIdRefDo.c_str());
+}
+
+TEST(TagLengthValue, ConstructRefDo) {
+    RefDo refDo(AidRefDo(), DeviceAppIdRefDo("C0FFEE"));
+
+    // Note that the AID-REF-DO does not end up in the payload because we
+    // construct an empty object.
+    ASSERT_STREQ(/*tag*/ "E1" /*length*/ "05" /*payload*/ "C103C0FFEE",
+                 refDo.c_str());
+}
+
+TEST(TagLengthValue, ConstructApduArDo) {
+    ApduArDo globalDenyRule(ApduArDo::Allow::Never);
+    ASSERT_STREQ(/*tag*/ "D0" /*length*/ "01" /*payload*/ "00",
+                 globalDenyRule.c_str());
+
+    ApduArDo globalAllowRule(ApduArDo::Allow::Always);
+    ASSERT_STREQ(/*tag*/ "D0" /*length*/ "01" /*payload*/ "01",
+                 globalAllowRule.c_str());
+
+    // These rules are actually supposed to have a better format than this
+    // but this will work for testing.
+    ApduArDo specificRule( {"C0FFEE", "BEEF"} );
+    ASSERT_STREQ(/*tag*/ "D0" /*length*/ "05" /*payload*/ "C0FFEEBEEF",
+                 specificRule.c_str());
+}
+
+TEST(TagLengthValue, ConstructNfcArDo) {
+    NfcArDo never(NfcArDo::Allow::Never);
+    ASSERT_STREQ(/*tag*/ "D1" /*length*/ "01" /*payload*/ "00", never.c_str());
+
+    NfcArDo always(NfcArDo::Allow::Always);
+    ASSERT_STREQ(/*tag*/ "D1" /*length*/ "01" /*payload*/ "01", always.c_str());
+}
+
+TEST(TagLengthValue, ConstructPermArDo) {
+    PermArDo permArDo("C0FFEE");
+
+    ASSERT_STREQ(/*tag*/ "DB" /*length*/ "03" /*payload*/ "C0FFEE",
+                 permArDo.c_str());
+}
+
+TEST(TagLengthValue, ConstructArDo) {
+    const ApduArDo apduArDo( {"C0FFEE", "BEEF"} );
+    const NfcArDo nfcArDo(NfcArDo::Allow::Always);
+    const PermArDo permArDo("DECADE");
+
+    ArDo arDoWithApdu(apduArDo);
+    ASSERT_STREQ(/*tag*/ "E3" /*length*/ "07" /*payload*/ "D005C0FFEEBEEF",
+                 arDoWithApdu.c_str());
+
+    ArDo arDoWithNfc(nfcArDo);
+    ASSERT_STREQ(/*tag*/ "E3" /*length*/ "03" /*payload*/ "D10101",
+                 arDoWithNfc.c_str());
+
+    ArDo arDoWithPerm(permArDo);
+    ASSERT_STREQ(/*tag*/ "E3" /*length*/ "05" /*payload*/ "DB03DECADE",
+                 arDoWithPerm.c_str());
+
+    ArDo arDoWithApduAndNfc(apduArDo, nfcArDo);
+    ASSERT_STREQ(/*tag*/ "E3" /*length*/ "0a"
+                 /*payload*/ "D005C0FFEEBEEFD10101",
+                 arDoWithApduAndNfc.c_str());
+}
+
+TEST(TagLengthValue, ConstructRefArDo) {
+    RefDo refDo(AidRefDo(), DeviceAppIdRefDo("C0FFEE"));
+    ArDo arDo(NfcArDo(NfcArDo::Allow::Always));
+
+    RefArDo refArDo(refDo, arDo);
+    ASSERT_STREQ(/*tag*/ "E2" /*length*/ "0c"
+                 /*payload*/ "E105C103C0FFEEE303D10101", refArDo.c_str());
+}
+
+TEST(TagLengthValue, ConstructAllRefArDo) {
+    // Create an ALL-REF-AR-DO with two REF-AR-DOs in it
+    AllRefArDo allRefArDo {
+        RefArDo {
+            RefDo {
+                AidRefDo(),
+                DeviceAppIdRefDo { "C0FFEE" }
+            },
+            ArDo {
+                ApduArDo { ApduArDo::Allow::Never }
+            }
+        },
+        RefArDo {
+            RefDo {
+                AidRefDo { },
+                DeviceAppIdRefDo { "BEEF" }
+            },
+            ArDo {
+                ApduArDo { ApduArDo::Allow::Always },
+                NfcArDo { NfcArDo::Allow::Never }
+            }
+        }
+    };
+
+    ASSERT_STREQ(/*tag*/ "FF40" /*length*/ "1e"
+                 /*1st REF-AR-DO*/ "E20cE105C103C0FFEEE303D00100"
+                 /*2nd REF-AR-DO*/ "E20eE104C102BEEFE306D00101D10100",
+                 allRefArDo.c_str());
+}
+
+TEST(TagLengthValue, TestLongLenths) {
+    // Create a string that's longer than 128 bytes, this will force the length
+    // part of the resulting string to span two bytes, see implementation for
+    // details on why this is. Lengths are multiplied by two because each byte
+    // is represented by two characters.
+    std::string twoByteString(2 * 0xa0, 'x');
+    DeviceAppIdRefDo twoByteTlv(twoByteString);
+    ASSERT_EQ(twoByteTlv.c_str(), strstr(twoByteTlv.c_str(), "C181a0x"));
+
+    // Longer than 255 bytes requires three byte length
+    std::string threeByteString(2 * 0x123, 'x');
+    DeviceAppIdRefDo threeByteTlv(threeByteString);
+    ASSERT_EQ(threeByteTlv.c_str(), strstr(threeByteTlv.c_str(), "C1820123x"));
+
+    // Longer than 65535 bytes requires four byte length
+    std::string fourByteString(2 * 0x10312, 'x');
+    DeviceAppIdRefDo fourByteTlv(fourByteString);
+    ASSERT_EQ(fourByteTlv.c_str(), strstr(fourByteTlv.c_str(), "C183010312x"));
+
+    /*
+     This test is disabled because of excessive memory usage and slow
+     performance. At the time of writing it works but since it's unlikely
+     we will ever be returning data larger than 16 MB skipping this should
+     be OK
+    // Longer than 16 * 65535 requires five byte length
+    std::string fiveByteString(2 * 0x1001002, 'x');
+    DeviceAppIdRefDo fiveByteTlv(fiveByteString);
+    ASSERT_EQ(fiveByteTlv.c_str(),
+              strstr(fiveByteTlv.c_str(), "C18401001002x"));
+    */
+}
+
+}  // namespace android
+
---
+++ android/telephony/modem_unittest.cpp
@@ -0,0 +1,184 @@
+/* Copyright (C) 2016 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android/telephony/modem.h"
+#include "android/base/ArraySize.h"
+
+#include <gtest/gtest.h>
+
+#include <stdio.h>
+#include <string.h>
+
+static const int kBasePort = 42;
+
+class ModemTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        mModem = amodem_create(kBasePort,
+                               &ModemTest::onUnsolicitedCommand,
+                               this);
+    }
+
+    void TearDown() override {
+        if (mModem) {
+            amodem_destroy(mModem);
+            mModem = nullptr;
+        }
+    }
+
+    static void onUnsolicitedCommand(void* opaque, const char* message) {
+    }
+
+    int openLogicalChannel(const char* name) {
+        // Create and send command
+        char command[1024];
+        snprintf(command, sizeof(command), "AT+CCHO=%s", name);
+        command[sizeof(command) - 1] = '\0';
+        const char* reply = amodem_send(mModem, command);
+        if (reply == nullptr) {
+            return -1;
+        }
+
+        // Parse the reply
+        int channel;
+        char ok[4] = { 0 };
+        char dummy;
+        if (sscanf(reply, "%d%3c%c", &channel, ok, &dummy) != 2) {
+            return -2;
+        }
+        if (strcmp(ok, "\rOK") != 0) {
+            return -3;
+        }
+        return channel;
+    }
+
+    bool closeLogicalChannel(int channel) {
+        char command[128];
+        snprintf(command, sizeof(command), "AT+CCHC=%d", channel);
+        command[sizeof(command) - 1] = '\0';
+        const char* reply = amodem_send(mModem, command);
+        return strcmp("+CCHC\rOK", reply) == 0;
+    }
+
+    AModem mModem;
+};
+
+TEST_F(ModemTest, OpenLogicalChannel) {
+    // Open a lgoical channel (CCHO operation) with some random name
+    int firstChannel = openLogicalChannel("F6238748383");
+
+    // Channels must start at 1 or above
+    ASSERT_GE(firstChannel, 1);
+
+    int secondChannel = openLogicalChannel("F6238748383");
+    ASSERT_GE(secondChannel, 1);
+
+    // Make sure we got a different channel
+    ASSERT_NE(firstChannel, secondChannel);
+
+    closeLogicalChannel(firstChannel);
+    closeLogicalChannel(secondChannel);
+}
+
+TEST_F(ModemTest, CloseLogicalChannel) {
+    int channel = openLogicalChannel("F6238748383");
+    ASSERT_GE(channel, 1);
+
+    // Should not be able to close another channel
+    ASSERT_FALSE(closeLogicalChannel(channel + 1));
+
+    // Should be able to close channel but only once
+    ASSERT_TRUE(closeLogicalChannel(channel));
+    ASSERT_FALSE(closeLogicalChannel(channel));
+}
+
+static const char* kValidReply = "+CGLA: 144,0,FF4026E224E116C11461ed377e85d386a8dfee6b864bd85b0bfaa5af81E30aDB080000000000000000\rOK";
+static const char* kCmeError = "+CME ERROR: ";
+
+TEST_F(ModemTest, TransmitLogicalChannel) {
+    static const struct {
+        const char* ChannelName;
+        const char* CommandFormat;
+        const char* ExpectedReply;
+        bool FullMatch;
+    } kData[] = {
+        // No open channel
+        { nullptr, "AT+CGLA=1,10,80CAFF4000", kCmeError, false },
+        // Open channel, correct format, correct reply, lower 4 bits in class
+        // shouldn't matter
+        { "A00000015141434C00", "AT+CGLA=%d,10,80CAFF4000", kValidReply, true },
+        { "A00000015141434C00", "AT+CGLA=%d,10,8FCAFF4000", kValidReply, true },
+        { "A00000015141434C00", "AT+CGLA=%d,10,87CAFF4000", kValidReply, true },
+        { "A00000015141434C00", "AT+CGLA=%d,10,83CAFF4000", kValidReply, true },
+        // Correct data but channel name with no matching data
+        { "ABCDEF", "AT+CGLA=%d,10,80CAFF4000", kCmeError, false },
+        // Mismatching length and command size, various combinations
+        { "F6238748383", "AT+CGLA=%d,9,80CAFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=%d,11,80CAFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=%d,10,80CAFF40", kCmeError, false },
+        // Too short, must be at least 10 characters to be an APDU command
+        { "F6238748383", "AT+CGLA=%d,8,80CAFF40", kCmeError, false },
+        // Open a channel but use an incorrect channel number
+        { "F6238748383", "AT+CGLA=10000,10,80CAFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=0,10,80CAFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=-1,10,80CAFF4000", kCmeError, false },
+        // Attempt to use an incorrect class or instruction (first two bytes)
+        { "F6238748383", "AT+CGLA=%d,10,90CAFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=%d,10,80CBFF4000", kCmeError, false },
+        { "F6238748383", "AT+CGLA=%d,10,A0DBFF4000", kCmeError, false },
+    };
+    static const size_t kNumTests = android::base::arraySize(kData);
+
+    for (size_t i = 0; i < kNumTests; ++i) {
+        int channel = -1;
+        if (kData[i].ChannelName) {
+            channel = openLogicalChannel(kData[i].ChannelName);
+            ASSERT_GE(channel, 1);
+        }
+        char command[1024];
+        if (strstr(kData[i].CommandFormat, "%d") != nullptr) {
+            // Insert channel number if there's a format argument in there
+            snprintf(command, sizeof(command), kData[i].CommandFormat, channel);
+        } else {
+            snprintf(command, sizeof(command), "%s", kData[i].CommandFormat);
+        }
+        command[sizeof(command) - 1] = '\0';
+
+        const char* reply = amodem_send(mModem, command);
+        ASSERT_NE(nullptr, reply);
+        if (kData[i].FullMatch) {
+            ASSERT_STREQ(kData[i].ExpectedReply, reply);
+        } else {
+            ASSERT_EQ(reply, strstr(reply, kData[i].ExpectedReply));
+        }
+        closeLogicalChannel(channel);
+    }
+
+    // Also test that transmission fails on a closed channel
+    int channel = openLogicalChannel("A00000015141434C00");
+    ASSERT_GE(channel, 1);
+
+    // First transmit something successfully, then repeat the exact same thing
+    // but with the channel closed
+    for (int i = 0; i < 2; ++i) {
+        char command[1024];
+        snprintf(command, sizeof(command), "AT+CGLA=%d,10,80CAFF4000", channel);
+        command[sizeof(command) - 1] = '\0';
+        const char* reply = amodem_send(mModem, command);
+        if (i == 0) {
+            ASSERT_STREQ(kValidReply, reply);
+            closeLogicalChannel(channel);
+        } else {
+            ASSERT_EQ(reply, strstr(reply, kCmeError));
+        }
+    }
+}
