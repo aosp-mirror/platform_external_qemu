9dee7b9be3128a7e06e198a0c0d42f813de627bc
Yurii Zubrytskyi <zyy@google.com>
2016-11-08 10:34:03 -0800

    [emulib] Fix clipboard pipe crash on guest restart
    There's a way to restart guest programmatically. In this case it
    closes all open pipes to create them back after coming online.
    
    Clipboard pipe code wasn't expecting this, keeping a single pipe
    object alive and asserting there's no other calls to create one.
    On restart this assert failed in debug mode, and we leaked the
    previous object in release.
    Another issue is that there was a race between
    setGuestClipboardContents() call and re-creating a pipe.
    
    This CL addresses the issues:
     - Delete the previous pipe object in onGuestClose()
     - Protect from concurrent calls into the pipe with shared_ptr<>
    
     + Format the code with clang-format
     + use LazyInstance<> to get rid of dynamic initializers.
    
    Change-Id: Id7cb90153d0b2c5a0452ed435c38e6e5f245347d
    (cherry picked from commit fede8f71d3cc7837f0506387d3217ec980fc4b08)

--- android/emulation/ClipboardPipe.cpp
+++ android/emulation/ClipboardPipe.cpp
@@ -11,17 +11,37 @@
 
 #include "android/emulation/ClipboardPipe.h"
 
+#include "android/base/memory/LazyInstance.h"
 #include "android/clipboard-pipe.h"
+
+#include <cassert>
 
 namespace android {
 namespace emulation {
 
-ClipboardPipe* ClipboardPipe::Service::mPipeInstance = nullptr;
-ClipboardPipe::GuestClipboardCallback ClipboardPipe::mGuestClipboardCallback =
-    [](const uint8_t*, size_t){};
+static constexpr auto emptyCallback = [](const uint8_t*, size_t) {};
 
-ClipboardPipe::ClipboardPipe(void* hwPipe, Service* svc) :
-        AndroidPipe(hwPipe, svc) {}
+// A storage for the current clipboard pipe instance data.
+// Shared pointer is required as there are potential races between clipboard
+// data changing from the host and guest's request to close the pipe. We need
+// to keep the object alive if we've started some operation.
+struct ClipboardPipeInstance {
+    android::base::Lock pipeLock;
+    ClipboardPipe::Ptr pipe;
+
+    ClipboardPipe::GuestClipboardCallback guestClipboardCallback =
+            emptyCallback;
+};
+
+static android::base::LazyInstance<ClipboardPipeInstance> sInstance = {};
+
+ClipboardPipe::ClipboardPipe(void* hwPipe, Service* svc)
+    : AndroidPipe(hwPipe, svc) {}
+
+void ClipboardPipe::onGuestClose() {
+    ClipboardPipe::Service::closePipe();
+}
+
 unsigned ClipboardPipe::onGuestPoll() const {
     unsigned result = PIPE_POLL_OUT;
     if (mHostClipboardHasNewData) {
@@ -37,10 +57,9 @@ int ClipboardPipe::processOperation(OperationType opType,
                                     bool* opComplete) {
     // This indicates the number of bytes needed to be read from or written
     // to the pipe.
-    uint32_t requiredBytes =
-        state->sizeProcessed ?
-            state->requiredBytes :
-            sizeof(state->requiredBytes);
+    uint32_t requiredBytes = state->sizeProcessed
+                                     ? state->requiredBytes
+                                     : sizeof(state->requiredBytes);
 
     // This will point to the buffer to be read from.
     const uint8_t* sourceBuffer = nullptr;
@@ -66,24 +85,23 @@ int ClipboardPipe::processOperation(OperationType opType,
         // buffer is state.buffer (or state.requiredBytes, if we haven't
         // sent the length of the buffer yet).
         sourceBuffer =
-            state->sizeProcessed ?
-                &state->buffer[0] :
-                reinterpret_cast<uint8_t*>(&(state->requiredBytes));
+                state->sizeProcessed
+                        ? state->buffer.data()
+                        : reinterpret_cast<uint8_t*>(&(state->requiredBytes));
     } else if (opType == OperationType::ReadFromGuestClipboard) {
         // If we're reading from the guest clipboard, the target buffer
         // is the state's buffer (or state.requiredBytes, if we haven't
         // read the length of the buffer yet).
         targetBuffer =
-            state->sizeProcessed ?
-                &state->buffer[0] :
-                reinterpret_cast<uint8_t*>(&(state->requiredBytes));
+                state->sizeProcessed
+                        ? state->buffer.data()
+                        : reinterpret_cast<uint8_t*>(&(state->requiredBytes));
     }
 
     while (pipeBuffer != endPipeBuffer) {
         // Decide how many bytes need to be read/written during the current
         // iteration.
-        int bytesToProcess =
-            std::min(
+        int bytesToProcess = std::min(
                 requiredBytes - state->processedBytes,
                 static_cast<uint32_t>(pipeBuffer->size - pipeBufferOffset));
 
@@ -91,19 +109,14 @@ int ClipboardPipe::processOperation(OperationType opType,
             // const_cast is ok here since the contents of AndroidPipeBuffer
             // passed into the function were intended to be changed.
             targetBuffer =
-                const_cast<uint8_t*>(pipeBuffer->data) + pipeBufferOffset;
-            memcpy(
-                targetBuffer,
-                sourceBuffer + state->processedBytes,
-                bytesToProcess);
+                    const_cast<uint8_t*>(pipeBuffer->data) + pipeBufferOffset;
+            memcpy(targetBuffer, sourceBuffer + state->processedBytes,
+                   bytesToProcess);
         } else if (opType == OperationType::ReadFromGuestClipboard) {
-            sourceBuffer =
-                static_cast<const uint8_t*>(pipeBuffer->data) +
-                    pipeBufferOffset;
-            memcpy(
-                targetBuffer + state->processedBytes,
-                sourceBuffer,
-                bytesToProcess);
+            sourceBuffer = static_cast<const uint8_t*>(pipeBuffer->data) +
+                           pipeBufferOffset;
+            memcpy(targetBuffer + state->processedBytes, sourceBuffer,
+                   bytesToProcess);
         }
         state->processedBytes += bytesToProcess;
         totalBytesProcessed += bytesToProcess;
@@ -125,11 +138,11 @@ int ClipboardPipe::processOperation(OperationType opType,
                 state->buffer.resize(state->requiredBytes);
 
                 // Make sure next iterations write to state's buffer.
-                targetBuffer = &state->buffer[0];
+                targetBuffer = state->buffer.data();
             } else if (opType == OperationType::WriteToGuestClipboard) {
                 // If we're writing to the guest clipboard, just
                 // make sure next iterations read from state's buffer.
-                sourceBuffer = &state->buffer[0];
+                sourceBuffer = state->buffer.data();
             }
         }
     }
@@ -144,10 +157,8 @@ int ClipboardPipe::onGuestRecv(AndroidPipeBuffer* buffers, int numBuffers) {
     if (mHostClipboardHasNewData) {
         bool opComplete = false;
         result = processOperation(OperationType::WriteToGuestClipboard,
-                                  &mGuestClipboardWriteState,
-                                  buffers,
-                                  numBuffers,
-                                  &opComplete);
+                                  &mGuestClipboardWriteState, buffers,
+                                  numBuffers, &opComplete);
         if (opComplete) {
             mHostClipboardHasNewData = false;
         }
@@ -155,19 +166,16 @@ int ClipboardPipe::onGuestRecv(AndroidPipeBuffer* buffers, int numBuffers) {
     return result;
 }
 
-int ClipboardPipe::onGuestSend(
-        const AndroidPipeBuffer* buffers,
-        int numBuffers) {
+int ClipboardPipe::onGuestSend(const AndroidPipeBuffer* buffers,
+                               int numBuffers) {
     bool opComplete = false;
     int result = processOperation(OperationType::ReadFromGuestClipboard,
-                                  &mGuestClipboardReadState,
-                                  buffers,
-                                  numBuffers,
-                                  &opComplete);
+                                  &mGuestClipboardReadState, buffers,
+                                  numBuffers, &opComplete);
     if (opComplete) {
-        mGuestClipboardCallback(
-            &mGuestClipboardReadState.buffer[0],
-            mGuestClipboardReadState.processedBytes);
+        sInstance->guestClipboardCallback(
+                mGuestClipboardReadState.buffer.data(),
+                mGuestClipboardReadState.processedBytes);
         mGuestClipboardReadState.sizeProcessed = false;
         mGuestClipboardReadState.requiredBytes = 0;
         mGuestClipboardReadState.processedBytes = 0;
@@ -181,12 +189,11 @@ void registerClipboardPipeService() {
 
 void ClipboardPipe::setGuestClipboardCallback(
         ClipboardPipe::GuestClipboardCallback cb) {
-    mGuestClipboardCallback = cb ? cb : [](const uint8_t*, size_t){};
+    sInstance->guestClipboardCallback = cb ? cb : emptyCallback;
 }
 
 void ClipboardPipe::setGuestClipboardContents(const uint8_t* buf, size_t len) {
-    mGuestClipboardWriteState.buffer.resize(len);
-    memcpy(&mGuestClipboardWriteState.buffer[0], buf, len);
+    mGuestClipboardWriteState.buffer.assign(buf, buf + len);
     mGuestClipboardWriteState.sizeProcessed = false;
     mGuestClipboardWriteState.requiredBytes = len;
     mGuestClipboardWriteState.processedBytes = 0;
@@ -196,8 +203,32 @@ void ClipboardPipe::setGuestClipboardContents(const uint8_t* buf, size_t len) {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+ClipboardPipe::Service::Service() : AndroidPipe::Service("clipboard") {}
+
+AndroidPipe* ClipboardPipe::Service::create(void* hwPipe, const char* args) {
+    const auto pipe = new ClipboardPipe(hwPipe, this);
+    {
+        android::base::AutoLock lock(sInstance->pipeLock);
+        assert(!sInstance->pipe);
+        sInstance->pipe.reset(pipe);
+    }
+    return pipe;
 }
+
+ClipboardPipe::Ptr ClipboardPipe::Service::getPipe() {
+    android::base::AutoLock lock(sInstance->pipeLock);
+    return sInstance->pipe;
 }
+
+void ClipboardPipe::Service::closePipe() {
+    android::base::AutoLock lock(sInstance->pipeLock);
+    sInstance->pipe.reset();
+}
+
+}  // namespace emulation
+}  // namespace android
 
 void android_init_clipboard_pipe(void) {
     android::emulation::registerClipboardPipeService();
