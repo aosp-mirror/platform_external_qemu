85510387f9a7628b3f79b72376095817d76efd5f
Yurii Zubrytskyi <zyy@google.com>
2016-11-10 12:48:51 -0800

    [base] Add a sscanf()-like functions to parse floats with C locale
    We've got several places where we use regular sscanf/sprintf and
    end up with a wrong decimal point character - because of non-US
    locale on the user machine.
    To mitigate the issues this file defines locale-independent
    functions for formatted input. They always parse floats as if
    we had default "C" locale set, with '.' as the decimal point
    character.
    
    + Create a helper function to set up a locale with comma as
      decimal point separator
    + Add default ctor to the FuncDelete<> template to allow
      empty scoped pointer construction
    + Got rid of a clang warning in FunctorThread_unittest.cpp
    
    Change-Id: I50e6af0473e635abd0a8bc3bf6d51e05dccf8e6e

---
+++ android/base/StringParse_unittest.cpp
@@ -0,0 +1,71 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/base/StringParse.h"
+
+#include "android/base/threads/FunctorThread.h"
+#include "android/base/testing/Utils.h"
+#include <gtest/gtest.h>
+
+namespace android {
+namespace base {
+
+static void testScanf() {
+    static const char comma[] = "1,3";
+    static const char dot[] = "1.3";
+    static const char format[] = "%f%n";
+    float val = 0;
+    int n = 0;
+
+    // Now make sure we parse floating point strings as expected.
+    EXPECT_EQ(1, sscanf(dot, format, &val, &n));
+    EXPECT_FLOAT_EQ(1, val);
+    EXPECT_EQ(1, n);
+    EXPECT_EQ(1, sscanf(comma, format, &val, &n));
+    EXPECT_FLOAT_EQ(1.3, val);
+    EXPECT_EQ(3, n);
+
+    // C-Locale parsing should be able to parse strings with C locale
+    EXPECT_EQ(1, SscanfWithCLocale(dot, format, &val, &n));
+    EXPECT_FLOAT_EQ(1.3, val);
+    EXPECT_EQ(3, n);
+
+    // And the regular parsing still works as it used to.
+    EXPECT_EQ(1, sscanf(dot, format, &val, &n));
+    EXPECT_FLOAT_EQ(1, val);
+    EXPECT_EQ(1, n);
+    EXPECT_EQ(1, sscanf(comma, format, &val, &n));
+    EXPECT_FLOAT_EQ(1.3, val);
+    EXPECT_EQ(3, n);
+}
+
+TEST(StringParse, SscanfWithCLocale) {
+    auto scopedCommaLocale = setScopedCommaLocale();
+    testScanf();
+}
+
+TEST(StringParse, SscanfWithCLocaleThreads) {
+    auto scopedCommaLocale = setScopedCommaLocale();
+
+    std::vector<std::unique_ptr<FunctorThread>> threads;
+    for (int i = 0; i < 20; ++i) {
+        threads.emplace_back(new FunctorThread(&testScanf));
+    }
+    for (auto& t : threads) {
+        ASSERT_TRUE(t->start());
+    }
+    for (auto& t : threads) {
+        ASSERT_TRUE(t->wait());
+    }
+}
+
+}  // namespace base
+}  // namespace android
