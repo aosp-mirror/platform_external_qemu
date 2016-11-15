aosp-mirror/platform_external_qemu

commit d2b2fa7251b43d45db8c84547cc1f9dd26178222

[gl] Optimize the slow GL operations
- Return raw ObjectData pointer where possible instead of copy
  of a shared_ptr<> from ShareGroup::getObjectData()
  This function is called A LOT, and shared_ptr<> copy is two
  interlocked operations (copy + destroy).
    ~1% in Antunu

- Use a spinlock instead of mutex for
  ShareGroup::getObjectData(). This function's mutex is always
  uncontended, so spinlock is always going to be a single atomic
  exchange, instead of a function call into pthread_mutex_lock.
    ~1% in Antutu

- Don't search for the max index in glDrawElements if it's not
  needed.
    ~2% in Antutu

Change-Id: Ia0ae78e42ddc6bc10b872a9ad10f9b615c33df51

distrib/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
@@ -43,33 +43,43 @@ GLESv2Context::~GLESv2Context()
 
 void GLESv2Context::setAttribute0value(float x, float y, float z, float w)
 {
+    m_attribute0valueChanged |=
+            x != m_attribute0value[0] || y != m_attribute0value[1] ||
+            z != m_attribute0value[2] || w != m_attribute0value[3];
     m_attribute0value[0] = x;
     m_attribute0value[1] = y;
     m_attribute0value[2] = z;
     m_attribute0value[3] = w;
 }
 
-void GLESv2Context::validateAtt0PreDraw(unsigned int count)
+bool GLESv2Context::needAtt0PreDrawValidation()
 {
     m_att0NeedsDisable = false;
 
-    if(count == 0)
-        return;
-
     int enabled = 0;
     s_glDispatch.glGetVertexAttribiv(0, GL_VERTEX_ATTRIB_ARRAY_ENABLED, &enabled);
-    if(enabled)
-        return;
+    return enabled == 0;
+}
 
-    if(count > m_att0ArrayLength)
-    {
-        delete [] m_att0Array; 
-        m_att0Array = new GLfloat[4*count];
-        m_att0ArrayLength = count;
+void GLESv2Context::validateAtt0PreDraw(unsigned int count)
+{
+    if (count == 0) {
+        return;
     }
 
-    for(unsigned int i=0; i<count; i++)
-        memcpy(m_att0Array+i*4, m_attribute0value, 4*sizeof(GLfloat));
+    if (count > m_att0ArrayLength) {
+        delete [] m_att0Array; 
+        unsigned newLen = 4 * std::max(count, 2 * m_att0ArrayLength);
+        m_att0Array = new GLfloat[newLen];
+        m_att0ArrayLength = newLen;
+        m_attribute0valueChanged = true;
+    }
+    if (m_attribute0valueChanged) {
+        for(unsigned int i = 0; i<m_att0ArrayLength; i++) {
+            memcpy(m_att0Array+i*4, m_attribute0value, sizeof(m_attribute0value));
+        }
+        m_attribute0valueChanged = false;
+    }
 
     s_glDispatch.glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, m_att0Array);
     s_glDispatch.glEnableVertexAttribArray(0);
@@ -79,20 +89,20 @@ void GLESv2Context::validateAtt0PreDraw(unsigned int count)
 
 void GLESv2Context::validateAtt0PostDraw(void)
 {
-    if(m_att0NeedsDisable)
+    if (m_att0NeedsDisable) {
         s_glDispatch.glDisableVertexAttribArray(0);
-
-    m_att0NeedsDisable = false;
+        m_att0NeedsDisable = false;
+    }
 }
 
 void GLESv2Context::setupArraysPointers(GLESConversionArrays& cArrs,GLint first,GLsizei count,GLenum type,const GLvoid* indices,bool direct) {
     ArraysMap::iterator it;
 
     //going over all clients arrays Pointers
-    for ( it=m_map.begin() ; it != m_map.end(); it++ ) {
+    for ( it=m_map.begin() ; it != m_map.end(); ++it) {
         GLenum array_id   = (*it).first;
         GLESpointer* p = (*it).second;
-        if(!isArrEnabled(array_id)) continue;
+        if(!p->isEnable()) continue;
 
         unsigned int size = p->getSize();
 

distrib/android-emugl/host/libs/Translator/EGL/EglImp.cpp
@@ -1065,11 +1065,11 @@ EGLAPI EGLImageKHR EGLAPIENTRY eglCreateImageKHR(EGLDisplay display, EGLContext 
 
         ImagePtr img( new EglImage() );
         if (img.get() != NULL) {
-            ObjectDataPtr objData = sg->getObjectData(
+            auto objData = sg->getObjectData(
                     NamedObjectType::TEXTURE, SafeUIntFromPointer(buffer));
-            if (!objData.get()) return EGL_NO_IMAGE_KHR;
+            if (!objData) return EGL_NO_IMAGE_KHR;
 
-            TextureData *texData = (TextureData *)objData.get();
+            TextureData *texData = (TextureData *)objData;
             if(!texData->width || !texData->height) return EGL_NO_IMAGE_KHR;
             img->width = texData->width;
             img->height = texData->height;

distrib/android-emugl/host/libs/Translator/EGL/EglOsApi_darwin.cpp
@@ -195,7 +195,7 @@ public:
         int i = 0;
         for (NativeFormatList::iterator it = s_nativeFormats.begin();
                 it != s_nativeFormats.end();
-                it++) {
+                ++it) {
             pixelFormatToConfig(i++,
                                 renderableType,
                                 *it,

distrib/android-emugl/host/libs/Translator/GLES_CM/GLEScmContext.cpp
@@ -107,11 +107,11 @@ void GLEScmContext::setupArraysPointers(GLESConversionArrays& cArrs,GLint first,
     m_pointsIndex = -1;
 
     //going over all clients arrays Pointers
-    for ( it=m_map.begin() ; it != m_map.end(); it++ ) {
+    for ( it=m_map.begin() ; it != m_map.end(); ++it) {
 
         GLenum array_id   = (*it).first;
         GLESpointer* p = (*it).second;
-        if(!isArrEnabled(array_id)) continue;
+        if(!p->isEnable()) continue;
         if(array_id == GL_TEXTURE_COORD_ARRAY) continue; //handling textures later
         setupArrayPointerHelper(cArrs,first,count,type,indices,direct,array_id,p);
     }
@@ -131,7 +131,7 @@ void GLEScmContext::setupArraysPointers(GLESConversionArrays& cArrs,GLint first,
 
         GLenum array_id   = GL_TEXTURE_COORD_ARRAY;
         GLESpointer* p = m_map[array_id];
-        if(!isArrEnabled(array_id)) continue;
+        if(!p->isEnable()) continue;
         setupArrayPointerHelper(cArrs,first,count,type,indices,direct,array_id,p);
     }
 

distrib/android-emugl/host/libs/Translator/GLES_CM/GLEScmImp.cpp
@@ -194,14 +194,14 @@ static TextureData* getTextureData(ObjectLocalName tex){
     }
 
     TextureData *texData = NULL;
-    ObjectDataPtr objData =
+    auto objData =
             ctx->shareGroup()->getObjectData(NamedObjectType::TEXTURE, tex);
-    if(!objData.get()){
+    if(!objData){
         texData = new TextureData();
         ctx->shareGroup()->setObjectData(NamedObjectType::TEXTURE, tex,
                                          ObjectDataPtr(texData));
     } else {
-        texData = (TextureData*)objData.get();
+        texData = (TextureData*)objData;
     }
     return texData;
 }
@@ -216,9 +216,9 @@ GL_API GLboolean GL_APIENTRY glIsBuffer(GLuint buffer) {
     GET_CTX_RET(GL_FALSE)
 
     if(buffer && ctx->shareGroup().get()) {
-        ObjectDataPtr objData = ctx->shareGroup()->getObjectData(
+        auto objData = ctx->shareGroup()->getObjectData(
                 NamedObjectType::VERTEXBUFFER, buffer);
-        return objData.get() ? ((GLESbuffer*)objData.get())->wasBinded()
+        return objData ? ((GLESbuffer*)objData)->wasBinded()
                              : GL_FALSE;
     }
     return GL_FALSE;
@@ -312,8 +312,7 @@ GL_API void GL_APIENTRY  glBindBuffer( GLenum target, GLuint buffer) {
     if (buffer) {
         GLESbuffer* vbo =
                 (GLESbuffer*)ctx->shareGroup()
-                        ->getObjectData(NamedObjectType::VERTEXBUFFER, buffer)
-                        .get();
+                        ->getObjectData(NamedObjectType::VERTEXBUFFER, buffer);
         vbo->setBinded();
     }
 }
@@ -1718,9 +1717,9 @@ GL_API void GL_APIENTRY glEGLImageTargetRenderbufferStorageOES(GLenum target, GL
     // raise INVALID_OPERATIOn if no renderbuffer is bounded
     GLuint rb = ctx->getRenderbufferBinding();
     SET_ERROR_IF(rb == 0,GL_INVALID_OPERATION);
-    ObjectDataPtr objData =
+    auto objData =
             ctx->shareGroup()->getObjectData(NamedObjectType::RENDERBUFFER, rb);
-    RenderbufferData *rbData = (RenderbufferData *)objData.get();
+    RenderbufferData *rbData = (RenderbufferData *)objData;
     SET_ERROR_IF(!rbData,GL_INVALID_OPERATION);
 
     //
@@ -1849,9 +1848,8 @@ GL_API void GLAPIENTRY glRenderbufferStorageOES(GLenum target, GLenum internalfo
     // raise INVALID_OPERATIOn if no renderbuffer is bounded
     GLuint rb = ctx->getRenderbufferBinding();
     SET_ERROR_IF(rb == 0,GL_INVALID_OPERATION);
-    ObjectDataPtr objData =
-            ctx->shareGroup()->getObjectData(NamedObjectType::RENDERBUFFER, rb);
-    RenderbufferData *rbData = (RenderbufferData *)objData.get();
+    auto objData = ctx->shareGroup()->getObjectData(NamedObjectType::RENDERBUFFER, rb);
+    RenderbufferData *rbData = (RenderbufferData *)objData;
     SET_ERROR_IF(!rbData,GL_INVALID_OPERATION);
 
     //
@@ -1874,9 +1872,9 @@ GL_API void GLAPIENTRY glGetRenderbufferParameterivOES(GLenum target, GLenum pna
     //
     GLuint rb = ctx->getRenderbufferBinding();
     if (rb) {
-        ObjectDataPtr objData = ctx->shareGroup()->getObjectData(
+        auto objData = ctx->shareGroup()->getObjectData(
                 NamedObjectType::RENDERBUFFER, rb);
-        RenderbufferData *rbData = (RenderbufferData *)objData.get();
+        RenderbufferData *rbData = (RenderbufferData *)objData;
         if (rbData && rbData->eglImageGlobalTexObject) {
             GLenum texPname;
             switch(pname) {
@@ -2013,10 +2011,10 @@ GL_API void GLAPIENTRY glFramebufferTexture2DOES(GLenum target, GLenum attachmen
 
     // Update the the current framebuffer object attachment state
     GLuint fbName = ctx->getFramebufferBinding();
-    ObjectDataPtr fbObj = ctx->shareGroup()->getObjectData(
+    auto fbObj = ctx->shareGroup()->getObjectData(
             NamedObjectType::FRAMEBUFFER, fbName);
-    if (fbObj.get() != NULL) {
-        FramebufferData *fbData = (FramebufferData *)fbObj.get();
+    if (fbObj) {
+        FramebufferData *fbData = (FramebufferData *)fbObj;
         fbData->setAttachment(attachment, textarget, 
                               texture, ObjectDataPtr());
     }
@@ -2042,11 +2040,10 @@ GL_API void GLAPIENTRY glFramebufferRenderbufferOES(GLenum target, GLenum attach
                                        renderbuffer);
             obj = ObjectDataPtr(new RenderbufferData());
             ctx->shareGroup()->setObjectData(
-                    NamedObjectType::RENDERBUFFER, renderbuffer,
-                    ObjectDataPtr(new RenderbufferData()));
+                    NamedObjectType::RENDERBUFFER, renderbuffer, obj);
         }
         else {
-            obj = ctx->shareGroup()->getObjectData(
+            obj = ctx->shareGroup()->getObjectDataPtr(
                     NamedObjectType::RENDERBUFFER, renderbuffer);
         }
         globalBufferName = ctx->shareGroup()->getGlobalName(
@@ -2055,10 +2052,10 @@ GL_API void GLAPIENTRY glFramebufferRenderbufferOES(GLenum target, GLenum attach
 
     // Update the the current framebuffer object attachment state
     GLuint fbName = ctx->getFramebufferBinding();
-    ObjectDataPtr fbObj = ctx->shareGroup()->getObjectData(
+    auto fbObj = ctx->shareGroup()->getObjectData(
             NamedObjectType::FRAMEBUFFER, fbName);
-    if (fbObj.get() != NULL) {
-        FramebufferData *fbData = (FramebufferData *)fbObj.get();
+    if (fbObj) {
+        FramebufferData *fbData = (FramebufferData *)fbObj;
         fbData->setAttachment(attachment, renderbuffertarget, renderbuffer, obj);
     }
 
@@ -2092,10 +2089,10 @@ GL_API void GLAPIENTRY glGetFramebufferAttachmentParameterivOES(GLenum target, G
     //
     GLuint fbName = ctx->getFramebufferBinding();
     if (fbName) {
-        ObjectDataPtr fbObj = ctx->shareGroup()->getObjectData(
+        auto fbObj = ctx->shareGroup()->getObjectData(
                 NamedObjectType::FRAMEBUFFER, fbName);
-        if (fbObj.get() != NULL) {
-            FramebufferData *fbData = (FramebufferData *)fbObj.get();
+        if (fbObj) {
+            FramebufferData *fbData = (FramebufferData *)fbObj;
             GLenum target;
             GLuint name = fbData->getAttachment(attachment, &target, NULL);
             if (pname == GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE_OES) {
@@ -2338,10 +2335,10 @@ void glDrawTexOES (T x, T y, T z, T width, T height) {
             unsigned int texname = ctx->getBindedTexture(GL_TEXTURE0+i,GL_TEXTURE_2D);
             ObjectLocalName tex = TextureLocalName(GL_TEXTURE_2D,texname);
             ctx->dispatcher().glClientActiveTexture(GL_TEXTURE0+i);
-            ObjectDataPtr objData = ctx->shareGroup()->getObjectData(
+            auto objData = ctx->shareGroup()->getObjectData(
                     NamedObjectType::TEXTURE, tex);
-            if (objData.get()) {
-                texData = (TextureData*)objData.get();
+            if (objData) {
+                texData = (TextureData*)objData;
                 //calculate texels
                 texels[i][0] = (float)(texData->crop_rect[0])/(float)(texData->width);
                 texels[i][1] = (float)(texData->crop_rect[1])/(float)(texData->height);

distrib/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.h
@@ -34,9 +34,10 @@ public:
     // current value, while OpenGL is not very clear about this, which results
     // in each implementation doing something different.
     void setAttribute0value(float x, float y, float z, float w);
+    bool needAtt0PreDrawValidation();
     void validateAtt0PreDraw(unsigned int count);
     void validateAtt0PostDraw(void);
-    const float* getAtt0(void) {return m_attribute0value;}
+    const float* getAtt0(void) const {return m_attribute0value;}
 
 protected:
     bool needConvert(GLESConversionArrays& fArrs,GLint first,GLsizei count,GLenum type,const GLvoid* indices,bool direct,GLESpointer* p,GLenum array_id);
@@ -45,6 +46,7 @@ private:
     void initExtensionString();
 
     float m_attribute0value[4] = {};
+    bool m_attribute0valueChanged = true;
     GLfloat* m_att0Array = nullptr;
     unsigned int m_att0ArrayLength = 0;
     bool m_att0NeedsDisable = false;

distrib/android-emugl/host/libs/Translator/GLcommon/GLEScontext.cpp
@@ -33,7 +33,7 @@ static void convertByteDirectLoop(const char* dataIn,unsigned int strideIn,void*
 static void convertByteIndirectLoop(const char* dataIn,unsigned int strideIn,void* dataOut,GLsizei count,GLenum indices_type,const GLvoid* indices,unsigned int strideOut,int attribSize);
 
 GLESConversionArrays::~GLESConversionArrays() {
-    for(auto it = m_arrays.begin(); it != m_arrays.end();it++) {
+    for(auto it = m_arrays.begin(); it != m_arrays.end(); ++it) {
         if((*it).second.allocated){
             if((*it).second.type == GL_FLOAT){
                 GLfloat* p = (GLfloat *)((*it).second.data);
@@ -241,7 +241,7 @@ void GLEScontext::setActiveTexture(GLenum tex) {
 }
 
 GLEScontext::~GLEScontext() {
-    for(ArraysMap::iterator it = m_map.begin(); it != m_map.end();it++) {
+    for(ArraysMap::iterator it = m_map.begin(); it != m_map.end(); ++it) {
         GLESpointer* p = (*it).second;
         if(p) {
             delete p;
@@ -258,8 +258,7 @@ const GLvoid* GLEScontext::setPointer(GLenum arrType,GLint size,GLenum type,GLsi
         GLESbuffer* vbo = static_cast<GLESbuffer*>(
                 m_shareGroup
                         ->getObjectData(NamedObjectType::VERTEXBUFFER,
-                                        bufferName)
-                        .get());
+                                        bufferName));
         m_map[arrType]->setBuffer(size,type,stride,vbo,bufferName,offset,normalize);
         return  static_cast<const unsigned char*>(vbo->getData()) +  offset;
     }
@@ -511,8 +510,7 @@ GLvoid* GLEScontext::getBindedBuffer(GLenum target) {
 
     GLESbuffer* vbo = static_cast<GLESbuffer*>(
             m_shareGroup
-                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName)
-                    .get());
+                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName));
     return vbo->getData();
 }
 
@@ -520,8 +518,7 @@ void GLEScontext::getBufferSize(GLenum target,GLint* param) {
     GLuint bufferName = getBuffer(target);
     GLESbuffer* vbo = static_cast<GLESbuffer*>(
             m_shareGroup
-                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName)
-                    .get());
+                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName));
     *param = vbo->getSize();
 }
 
@@ -529,8 +526,7 @@ void GLEScontext::getBufferUsage(GLenum target,GLint* param) {
     GLuint bufferName = getBuffer(target);
     GLESbuffer* vbo = static_cast<GLESbuffer*>(
             m_shareGroup
-                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName)
-                    .get());
+                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName));
     *param = vbo->getUsage();
 }
 
@@ -539,8 +535,7 @@ bool GLEScontext::setBufferData(GLenum target,GLsizeiptr size,const GLvoid* data
     if(!bufferName) return false;
     GLESbuffer* vbo = static_cast<GLESbuffer*>(
             m_shareGroup
-                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName)
-                    .get());
+                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName));
     return vbo->setBuffer(size,usage,data);
 }
 
@@ -550,8 +545,7 @@ bool GLEScontext::setBufferSubData(GLenum target,GLintptr offset,GLsizeiptr size
     if(!bufferName) return false;
     GLESbuffer* vbo = static_cast<GLESbuffer*>(
             m_shareGroup
-                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName)
-                    .get());
+                    ->getObjectData(NamedObjectType::VERTEXBUFFER, bufferName));
     return vbo->setSubBuffer(offset,size,data);
 }
 
@@ -857,12 +851,12 @@ void GLEScontext::drawValidate(void)
     if(m_framebuffer == 0)
         return;
 
-    ObjectDataPtr fbObj = m_shareGroup->getObjectData(
+    auto fbObj = m_shareGroup->getObjectData(
             NamedObjectType::FRAMEBUFFER, m_framebuffer);
-    if (fbObj.get() == NULL)
+    if (!fbObj)
         return;
 
-    FramebufferData *fbData = (FramebufferData *)fbObj.get();
+    FramebufferData *fbData = (FramebufferData *)fbObj;
 
     fbData->validate(this);
 }

commit 13aeeb4fdda28d42e05c117a8987502ef1da6851

[crash] Fix wrong array size calculation in GLES2 context
The issue was introduced in commit d2b2fa7251b43d45db8c84547cc1f9dd26178222
and caused CTS failures.

Change-Id: I0f3cff96ed9fe3d568c3293b368b2d396008a181

distrib/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
@@ -68,9 +68,9 @@ void GLESv2Context::validateAtt0PreDraw(unsigned int count)
     }
 
     if (count > m_att0ArrayLength) {
-        delete [] m_att0Array; 
-        unsigned newLen = 4 * std::max(count, 2 * m_att0ArrayLength);
-        m_att0Array = new GLfloat[newLen];
+        delete [] m_att0Array;
+        const unsigned newLen = std::max(count, 2 * m_att0ArrayLength);
+        m_att0Array = new GLfloat[4 * newLen];
         m_att0ArrayLength = newLen;
         m_attribute0valueChanged = true;
     }