bad1e7050ff605d09789c1b1b05eeb9037172847
Tao Wu <lepton@google.com>
2016-11-16 09:49:20 -0800

    Keep original initial accelerate sensor values.
    Historically, emulator booting with some special accelerate sensor
    values. Some tests depends on it. In http://r.android.com//276152/
    We remove some check in setPhonePositionFromSliders which reveals
    an bug that setRange will overwrite the special rotation we just
    setup. Move the special rotation setup after setRange calls to fix
    it.
    
    Bug: 32922607
    
    Change-Id: I084ebc6462da4936e3edb1b062d53a1f6de7ab18
    Signed-off-by: Tao Wu <lepton@google.com>
    (cherry picked from commit be2c09b4ef1360afdba891f962bbe604da0d5605)

--- android/skin/qt/extended-pages/virtual-sensors-page.cpp
+++ android/skin/qt/extended-pages/virtual-sensors-page.cpp
@@ -29,14 +29,7 @@ VirtualSensorsPage::VirtualSensorsPage(QWidget* parent) :
     mUi(new Ui::VirtualSensorsPage()),
     mSensorsAgent(nullptr)
 {
-    // Historically, the AVD starts up with the screen mostly
-    // vertical, but tilted back 4.75 degrees. Retain that
-    // initial orientation.
-    static const QQuaternion initialQuat =
-            QQuaternion::fromEulerAngles(-4.75, 0.00, 0.00);
-
     mUi->setupUi(this);
-    mUi->accelWidget->setRotation(initialQuat);
     mUi->temperatureSensorValueWidget->setRange(-273.1, 100.0);
     mUi->temperatureSensorValueWidget->setValue(25.0);
     mUi->lightSensorValueWidget->setRange(0, 40000.0);
@@ -114,6 +107,15 @@ VirtualSensorsPage::VirtualSensorsPage(QWidget* parent) :
                                    Accelerometer3DWidget::MaxX);
     mUi->positionYSlider->setRange(Accelerometer3DWidget::MinY,
                                    Accelerometer3DWidget::MaxY);
+    // Historically, the AVD starts up with the screen mostly
+    // vertical, but tilted back 4.75 degrees. Retain that
+    // initial orientation.
+    // We need to do this after we call setRange since setRange will trigger
+    // on_*Slider_valueChanged which just trigger setRotation from default
+    // value of Sliders.
+    static const QQuaternion initialQuat =
+            QQuaternion::fromEulerAngles(-4.75, 0.00, 0.00);
+    mUi->accelWidget->setRotation(initialQuat);
     onPhoneRotationChanged();
 
     using android::metrics::PeriodicReporter;
