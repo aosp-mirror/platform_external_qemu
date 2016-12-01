f4170dde6d82b885f9804a3f72170c7609249327
Yurii Zubrytskyi <zyy@google.com>
2016-12-01 10:35:31 -0800

    [apigen] Allow limited use of android/base in emugen
    CL allows one to use header-only parts of android/base in
    the emugen sources
    
    Change-Id: I5c1690359f839291692b981575141e3952c5ff9e
    (cherry picked from commit cbebc8ccc6a64909f8279f5873561717c583eb32)

--- android/android-emugl/common.mk
+++ android/android-emugl/common.mk
@@ -42,7 +42,7 @@ emugl-begin-module = \
     $(eval include $(CLEAR_VARS)) \
     $(eval LOCAL_MODULE := $1) \
     $(eval LOCAL_MODULE_CLASS := $(patsubst HOST_%,%,$(patsubst %EXECUTABLE,%EXECUTABLES,$(patsubst %LIBRARY,%LIBRARIES,$2)))) \
-    $(eval LOCAL_C_INCLUDES += $(if $3,,$(ANDROID_EMU_BASE_INCLUDES)) $(EMUGL_COMMON_INCLUDES)) \
+    $(eval LOCAL_C_INCLUDES += $(ANDROID_EMU_BASE_INCLUDES) $(EMUGL_COMMON_INCLUDES)) \
     $(eval LOCAL_CFLAGS += $(EMUGL_COMMON_CFLAGS)) \
     $(eval LOCAL_STATIC_LIBRARIES += $(if $3,,$(ANDROID_EMU_BASE_STATIC_LIBRARIES))) \
     $(eval LOCAL_LDLIBS += $(if $3,,$(ANDROID_EMU_BASE_LDLIBS)) $(CXX_STD_LIB)) \
