118506dff6e0f2fc678d2ba1ec752a7f3b7a4970
Lingfeng Yang <lfy@google.com>
2016-12-07 15:43:36 -0800

    [WIP][HACK] Use nonzero buffers on host OpenGL
    +detect if a nonzero buffer is bound during glDrawArrays/Elements.
    If so, call them directly without copying over host array
    portions.
    
    linux z840: 3dmark ice storm unlimited: 71k->81k
    
    Change-Id: I7a603196eddfd6631d0aa3269f5cb6fdc9d0f740

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -245,12 +245,19 @@ GL_APICALL void  GL_APIENTRY glBindBuffer(GLenum target, GLuint buffer){
         ctx->shareGroup()->setObjectData(NamedObjectType::VERTEXBUFFER, buffer,
                                          ObjectDataPtr(new GLESbuffer()));
     }
+
     ctx->bindBuffer(target,buffer);
     if (buffer) {
         GLESbuffer* vbo =
                 (GLESbuffer*)ctx->shareGroup()
                         ->getObjectData(NamedObjectType::VERTEXBUFFER, buffer);
         vbo->setBinded();
+        const GLuint globalBufferName = ctx->shareGroup()->getGlobalName(NamedObjectType::VERTEXBUFFER, buffer);
+        ctx->dispatcher().glBindBuffer(target, globalBufferName);
+    }
+
+    if (!buffer) {
+        ctx->dispatcher().glBindBuffer(target, buffer);
     }
 }
 
@@ -373,6 +380,7 @@ GL_APICALL void  GL_APIENTRY glBlendFuncSeparate(GLenum srcRGB, GLenum dstRGB, G
 
 GL_APICALL void  GL_APIENTRY glBufferData(GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage){
     GET_CTX_V2();
+
     SET_ERROR_IF(!GLESv2Validate::bufferTarget(
                      target,
                      ctx->getMajorVersion(),
@@ -381,6 +389,7 @@ GL_APICALL void  GL_APIENTRY glBufferData(GLenum target, GLsizeiptr size, const 
     SET_ERROR_IF(!ctx->isBindedBuffer(target),GL_INVALID_OPERATION);
     SET_ERROR_IF(!GLESv2Validate::bufferUsage(usage),GL_INVALID_ENUM);
     ctx->setBufferData(target,size,data,usage);
+    ctx->dispatcher().glBufferData(target, size, data, usage);
 }
 
 GL_APICALL void  GL_APIENTRY glBufferSubData(GLenum target, GLintptr offset, GLsizeiptr size, const GLvoid* data){
@@ -392,6 +401,7 @@ GL_APICALL void  GL_APIENTRY glBufferSubData(GLenum target, GLintptr offset, GLs
                  GL_INVALID_ENUM);
     SET_ERROR_IF(!ctx->isBindedBuffer(target),GL_INVALID_OPERATION);
     SET_ERROR_IF(!ctx->setBufferSubData(target,offset,size,data),GL_INVALID_VALUE);
+    ctx->dispatcher().glBufferSubData(target, offset, size, data);
 }
 
 
@@ -747,6 +757,11 @@ GL_APICALL void  GL_APIENTRY glDrawArrays(GLenum mode, GLint first, GLsizei coun
 
     ctx->drawValidate();
 
+    if (ctx->isBindedBuffer(GL_ARRAY_BUFFER)) {
+        ctx->dispatcher().glDrawArrays(mode,first,count);
+        return;
+    }
+
     GLESConversionArrays tmpArrs;
     ctx->setupArraysPointers(tmpArrs,first,count,0,NULL,true);
 
@@ -779,7 +794,10 @@ GL_APICALL void  GL_APIENTRY glDrawElements(GLenum mode, GLsizei count, GLenum t
     ctx->drawValidate();
 
     const GLvoid* indices = elementsIndices;
-    if(ctx->isBindedBuffer(GL_ELEMENT_ARRAY_BUFFER)) { // if vbo is binded take the indices from the vbo
+    if(ctx->isBindedBuffer(GL_ELEMENT_ARRAY_BUFFER)) { // if vbo is binded use the offset path
+        ctx->dispatcher().glDrawElements(mode,count,type,indices);
+        return;
+
         const unsigned char* buf = static_cast<unsigned char *>(ctx->getBindedBuffer(GL_ELEMENT_ARRAY_BUFFER));
         indices = buf + SafeUIntFromPointer(elementsIndices);
     }
@@ -2417,7 +2435,11 @@ GL_APICALL void  GL_APIENTRY glVertexAttribPointer(GLuint indx, GLint size, GLen
     GET_CTX();
     SET_ERROR_IF((!GLESv2Validate::arrayIndex(ctx,indx)),GL_INVALID_VALUE);
     if (type == GL_HALF_FLOAT_OES) type = GL_HALF_FLOAT;
+
     ctx->setPointer(indx,size,type,stride,ptr,normalized);
+    if (ctx->isBindedBuffer(GL_ARRAY_BUFFER)) {
+        ctx->dispatcher().glVertexAttribPointer(indx, size, type, normalized, stride, ptr);
+    }
 }
 
 GL_APICALL void  GL_APIENTRY glViewport(GLint x, GLint y, GLsizei width, GLsizei height){
