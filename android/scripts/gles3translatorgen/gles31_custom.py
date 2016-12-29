762db289d6ea5fe6d28632696dbb51653e87d229
Lingfeng Yang <lfy@google.com>
2016-12-29 15:35:02 -0800

    [WIP][GLESv3.1] separate vertex attrib format/binding/divisor
    Change-Id: Ia2d515e3e8ab3031a99c43858d1a574f579c5614

--- android/scripts/gles3translatorgen/gles31_custom.py
+++ android/scripts/gles3translatorgen/gles31_custom.py
@@ -1,4 +1,24 @@
 custom_preprocesses = {
+"glBindVertexBuffer" : """
+    ctx->bindIndexedBuffer(0, bindingindex, buffer, offset, 0, stride);
+""",
+
+"glVertexAttribBinding" : """
+    ctx->setVertexAttribBindingIndex(attribindex, bindingindex);
+""",
+
+"glVertexAttribFormat" : """
+    ctx->setVertexAttribFormat(attribindex, size, type, normalized, relativeoffset, false);
+""",
+
+"glVertexAttribIFormat" : """
+    ctx->setVertexAttribFormat(attribindex, size, type, GL_FALSE, relativeoffset, true);
+""",
+
+"glVertexBindingDivisor" : """
+    ctx->setVertexAttribDivisor(bindingindex, divisor);
+""",
+
 }
 
 custom_postprocesses = {
