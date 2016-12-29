f2c0b5a76d7478169a44b5fdd59970add8c61a2d
Lingfeng Yang <lfy@google.com>
2016-12-29 15:35:48 -0800

    [WIP][GLESv3.1] Multisampled textures
    Change-Id: Ie6613c042b636d5f259ca7ac1a1db03e8373f1c8

--- android/scripts/gles3translatorgen/gles31_custom.py
+++ android/scripts/gles3translatorgen/gles31_custom.py
@@ -19,6 +19,13 @@ custom_preprocesses = {
     ctx->setVertexAttribDivisor(bindingindex, divisor);
 """,
 
+"glTexStorage2DMultisample" : """
+    GLint err = GL_NO_ERROR;
+    GLenum format, type;
+    GLESv2Validate::getCompatibleFormatTypeForInternalFormat(internalformat, &format, &type);
+    sPrepareTexImage2D(target, 0, (GLint)internalformat, width, height, 0, format, type, NULL, &type, (GLint*)&internalformat, &err);
+    SET_ERROR_IF(err != GL_NO_ERROR, err);
+""",
 }
 
 custom_postprocesses = {
