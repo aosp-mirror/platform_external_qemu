cfe57de7496f51da37cdec9cf1a83e94448365ce
Yurii Zubrytskyi <zyy@google.com>
2017-01-04 16:30:24 -0800

    [TEST] testing if treehugger can build this
    Change-Id: Ibd17d941acc39d8a553a87ccc83a18be6bc81fc2

--- android/android-emu/android/base/testing/Utils.h
+++ android/android-emu/android/base/testing/Utils.h
@@ -33,6 +33,9 @@ inline auto setScopedCommaLocale() ->
     static constexpr char commaLocaleName[] = "fr_FR.UTF-8";
 #endif
 
+    system("locale -a");
+    system("cat /var/lib/locales/supported.d/local");
+
     // Set up a locale with comma as a decimal mark.
     auto oldLocale = setlocale(LC_ALL, nullptr);
     EXPECT_NE(nullptr, oldLocale);
