ccc8c839c2dc7e25f40089531bb45c3a52dd90fd
Lingfeng Yang <lfy@google.com>
2017-01-09 11:33:09 -0800

    [GLESv3] Update validation for ES 3.x
    - Remove ctx->drawValidate() call in glCheckFramebufferStatus as it
    was causing dEQP-GLES3 tests to fail some situations involving
    multisample resolve with separate draw/read buffers. The call to
    glCheckFramebufferStatus would trigger ctx->drawValidate() and
    corrupt the FBO state, causing the test to fail. On the other hand,
    if the dEQP test did not issue glCheckFramebufferStatus, all would be
    fine :)
    - Allow NPOT mipmaps if client version > 3.
    - Make low/med/highp types all with 32 bits precision, since the precision
    limits are indeed hit and checked in some shader tests.
    - Allow transpose for glUniformMatrix*.
    - Some validation has been removed from the host, left to the guest.
    
    Change-Id: I77bec820b6d597a75c07aa2ca375cf25bb959d65

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Validate.h
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Validate.h
@@ -19,29 +19,43 @@
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
 #include <GLcommon/GLESvalidate.h>
 
-struct GLESv2Validate:public GLESvalidate{
-static bool blendEquationMode(GLenum mode);
-static bool blendSrc(GLenum s);
-static bool blendDst(GLenum d);
-static bool textureParams(GLenum param);
-static bool hintTargetMode(GLenum target,GLenum mode);
-static bool capability(GLenum cap);
-static bool pixelStoreParam(GLenum param);
-static bool readPixelFrmt(GLenum format);
-static bool shaderType(GLenum type);
-static bool precisionType(GLenum type);
-static bool arrayIndex(GLEScontext * ctx,GLuint index);
-static bool pixelType(GLEScontext * ctx,GLenum type);
-static bool pixelFrmt(GLEScontext* ctx,GLenum format);
-static bool pixelItnlFrmt(GLEScontext* ctx, GLenum internalformat);
-static bool pixelSizedFrmt(GLEScontext* ctx, GLenum internalformat,
-                           GLenum format, GLenum type);
-static bool attribName(const GLchar* name);
-static bool attribIndex(int index);
-static bool programParam(GLenum pname);
-static bool textureIsCubeMap(GLenum target);
+struct GLESv2Validate : public GLESvalidate {
+
+    static bool renderbufferParam(GLEScontext* ctx, GLenum param);
+    static bool framebufferTarget(GLEScontext* ctx, GLenum target);
+    static bool framebufferAttachment(GLEScontext* ctx, GLenum attachment);
+    static bool bufferTarget(GLEScontext* ctx, GLenum target);
+    static bool bufferUsage(GLEScontext* ctx, GLenum usage);
+    static bool bufferParam(GLEScontext* ctx, GLenum pname);
+    static bool blendEquationMode(GLEScontext* ctx, GLenum mode);
+    static bool blendSrc(GLenum s);
+    static bool blendDst(GLenum d);
+    static bool textureTarget(GLEScontext* ctx, GLenum param);
+    static bool textureParams(GLEScontext* ctx, GLenum param);
+    static bool hintTargetMode(GLenum target,GLenum mode);
+    static bool capability(GLenum cap);
+    static bool pixelStoreParam(GLEScontext* ctx, GLenum param);
+    static bool readPixelFrmt(GLenum format);
+    static bool shaderType(GLEScontext* ctx, GLenum type);
+    static bool precisionType(GLenum type);
+    static bool arrayIndex(GLEScontext * ctx,GLuint index);
+    static bool pixelType(GLEScontext * ctx,GLenum type);
+    static bool pixelFrmt(GLEScontext* ctx,GLenum format);
+    static bool pixelItnlFrmt(GLEScontext* ctx, GLenum internalformat);
+    static bool pixelSizedFrmt(GLEScontext* ctx, GLenum internalformat,
+            GLenum format, GLenum type);
+    static bool isCompressedFormat(GLenum format);
+    static void getCompatibleFormatTypeForInternalFormat(GLenum internalformat, GLenum* format_out, GLenum* type_out);
+    static bool attribName(const GLchar* name);
+    static bool attribIndex(int index, int max);
+    static bool programParam(GLEScontext* ctx, GLenum pname);
+    static bool textureIsCubeMap(GLenum target);
+
+    static int sizeOfType(GLenum type);
 };
 
 #endif
