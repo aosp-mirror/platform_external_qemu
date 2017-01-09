f3fecae8ff45313bc9fa1d764b1f65e7c0c835da
Lingfeng Yang <lfy@google.com>
2017-01-09 10:21:48 -0800

    [GLESv3] Changes to buffer state
    - Add indexed buffers for:
        - transform feedback
        - uniform
        - atomic counter
        - shader storage
        - indexed vertex attribs
    - add "isInt" to state for glVertexAttribIPointer and friends
    - Switch from a single attrib state per context to several, per VAO
    - For 3.1, separate format from buffer binding state (for these and related calls):
        - glVertexAttribFormat
        - glBindVertexBuffer
    
    Also:
    
    - Don't convert GL_FIXED anymore (Needs testing with core profile / ARB_ES2_compatibility)
    
    Change-Id: I5cc9533dd564f61f97d11aada2f00488f5c280f8

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
@@ -49,9 +49,8 @@ void GLESv2Context::init(GlLibrary* glLib) {
     if(!m_initialized) {
         s_glDispatch.dispatchFuncs(GLES_2_0, glLib);
         GLEScontext::init(glLib);
-        for(int i=0; i < s_glSupport.maxVertexAttribs;i++){
-            m_map[i] = new GLESpointer();
-        }
+        addVertexArrayObject(0);
+        setVertexArrayObject(0);
         setAttribute0value(0.0, 0.0, 0.0, 1.0);
 
         buildStrings((const char*)dispatcher().glGetString(GL_VENDOR),
@@ -130,35 +129,51 @@ void GLESv2Context::setupArraysPointers(GLESConversionArrays& cArrs,GLint first,
     ArraysMap::iterator it;
 
     //going over all clients arrays Pointers
-    for ( it=m_map.begin() ; it != m_map.end(); ++it) {
-        GLenum array_id   = (*it).first;
+    for ( it=m_currVaoState.begin() ; it != m_currVaoState.end(); ++it) {
+        GLenum array_id = (*it).first;
         GLESpointer* p = (*it).second;
-        if(!p->isEnable()) continue;
+        if (!p->isEnable()) continue;
 
-        unsigned int size = p->getSize();
-
-        if(needConvert(cArrs,first,count,type,indices,direct,p,array_id)){
-            //conversion has occured
-            ArrayData currentArr = cArrs.getCurrentArray();
-            setupArr(currentArr.data,array_id,currentArr.type,size,currentArr.stride, p->getNormalized());
-            ++cArrs;
-        } else {
-            setupArr(p->getData(),array_id,p->getType(),
-                     size,p->getStride(), p->getNormalized());
-        }
+        setupArr(p->getData(),
+                 array_id,
+                 p->getType(),
+                 p->getSize(),
+                 p->getStride(),
+                 p->getNormalized(),
+                 -1,
+                 p->isIntPointer());
     }
 }
 
 //setting client side arr
-void GLESv2Context::setupArr(const GLvoid* arr,GLenum arrayType,GLenum dataType,GLint size,GLsizei stride,GLboolean normalized, int index){
-     if(arr == NULL) return;
-     s_glDispatch.glVertexAttribPointer(arrayType,size,dataType,normalized,stride,arr);
+void GLESv2Context::setupArr(const GLvoid* arr,GLenum arrayType,GLenum dataType,GLint size,GLsizei stride,GLboolean normalized, int index, bool isInt){
+    // is not really a client side arr.
+    if (arr == NULL) return;
+
+    if (isInt)
+        s_glDispatch.glVertexAttribIPointer(arrayType, size, dataType, stride, arr);
+    else
+        s_glDispatch.glVertexAttribPointer(arrayType, size, dataType, normalized, stride, arr);
 }
+
+void GLESv2Context::setVertexAttribDivisor(GLuint bindingindex, GLuint divisor) {
+    m_currVaoState.bufferBindings()[bindingindex].divisor = divisor;
+}
+
+void GLESv2Context::setVertexAttribBindingIndex(GLuint attribindex, GLuint bindingindex) {
+    m_currVaoState[attribindex]->setBindingIndex(bindingindex);
+}
+
+void GLESv2Context::setVertexAttribFormat(GLuint attribindex, GLint size, GLenum type, GLboolean normalized, GLuint reloffset, bool isInt) {
+    m_currVaoState[attribindex]->setFormat(size, type, normalized == GL_TRUE ? true : false, reloffset, isInt);
+}
+
 
 bool GLESv2Context::needConvert(GLESConversionArrays& cArrs,GLint first,GLsizei count,GLenum type,const GLvoid* indices,bool direct,GLESpointer* p,GLenum array_id) {
 
     bool usingVBO = p->isVBO();
     GLenum arrType = p->getType();
+
     /*
      conversion is not necessary in the following cases:
       (*) array type is not fixed
