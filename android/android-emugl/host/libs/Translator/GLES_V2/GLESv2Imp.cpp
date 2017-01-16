6b25f7b1735d551a779c024ff7fba2badcb22b9b
Joshua Lang <joshualang@google.com>
2017-01-15 16:30:30 -0800

    [WIP] Hack around glBindTexture on defaultTexture (0)
    Change-Id: I183b1cd08b8806a5d875f142e0392efeb59a412c

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -433,6 +433,7 @@ GL_APICALL void  GL_APIENTRY glBindTexture(GLenum target, GLuint texture){
     //for handling default texture (0)
     ObjectLocalName localTexName = TextureLocalName(target,texture);
     GLuint globalTextureName = localTexName;
+    if (globalTextureName) {
     if(ctx->shareGroup().get()){
         globalTextureName = ctx->shareGroup()->getGlobalName(
                 NamedObjectType::TEXTURE, localTexName);
@@ -453,6 +454,7 @@ GL_APICALL void  GL_APIENTRY glBindTexture(GLenum target, GLuint texture){
         }
         SET_ERROR_IF(ctx->GLTextureTargetToLocal(texData->target) != ctx->GLTextureTargetToLocal(target), GL_INVALID_OPERATION);
         texData->wasBound = true;
+    }
     }
 
     ctx->setBindedTexture(target,texture);
