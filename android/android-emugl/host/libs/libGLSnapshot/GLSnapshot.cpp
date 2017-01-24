8b18b24d1116c07a913d87a25211b8c2ab823bfb
Lingfeng Yang <lfy@google.com>
2017-01-24 08:32:51 -0800

    [WIP] GL state snapshot
    Change-Id: Idf2eb9822d62f9308275ff2df8d12f4402e085c3

---
+++ android/android-emugl/host/libs/libGLSnapshot/GLSnapshot.cpp
@@ -0,0 +1,88 @@
+#include "GLSnapshot.h"
+
+#include <GLES2/gl2.h>
+#include <GLES3/gl31.h>
+
+#include <stdio.h>
+
+#define DEBUG 0
+
+#if DEBUG
+
+#define D(fmt,...) do { \
+    fprintf(stderr, "%s:%s:%d: " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+} while(0) \
+
+#else
+#define D(...)
+#endif
+
+namespace GLSnapshot {
+
+GLSnapshotState::GLSnapshotState(const GLESv2Dispatch* gl) : mGL(gl) {
+    D("init snapshot state");
+}
+
+void GLSnapshotState::getGlobalStateEnum(GLenum name, int size) {
+    D("save 0x%x", name);
+    std::vector<GLenum>& store = mGlobals[name].enums;
+    store.resize(size);
+    mGL->glGetIntegerv(name, (GLint*)&store[0]);
+}
+
+void GLSnapshotState::getGlobalStateByte(GLenum name, int size) {
+    D("save 0x%x", name);
+    std::vector<unsigned char>& store = mGlobals[name].bytes;
+    store.resize(size);
+    mGL->glGetBooleanv(name, (GLboolean*)&store[0]);
+}
+
+void GLSnapshotState::getGlobalStateInt(GLenum name, int size) {
+    D("save 0x%x", name);
+    std::vector<uint32_t>& store = mGlobals[name].ints;
+    store.resize(size);
+    mGL->glGetIntegerv(name, (GLint*)&store[0]);
+}
+
+void GLSnapshotState::getGlobalStateFloat(GLenum name, int size) {
+    D("save 0x%x", name);
+    std::vector<float>& store = mGlobals[name].floats;
+    store.resize(size);
+    mGL->glGetFloatv(name, (GLfloat*)&store[0]);
+}
+
+void GLSnapshotState::getGlobalStateInt64(GLenum name, int size) {
+    D("save 0x%x", name);
+    std::vector<uint64_t>& store = mGlobals[name].int64s;
+    store.resize(size);
+    mGL->glGetInteger64v(name, (GLint64*)&store[0]);
+}
+
+void GLSnapshotState::getGlobalStateEnable(GLenum name) {
+    D("save 0x%x", name);
+    mEnables[name] = mGL->glIsEnabled(name) == GL_TRUE;
+}
+
+void GLSnapshotState::save() {
+    getGlobalStateEnable(GL_DEPTH_TEST);
+
+    getGlobalStateFloat(GL_COLOR_CLEAR_VALUE, 4);
+    getGlobalStateInt(GL_ACTIVE_TEXTURE, 1);
+}
+
+void GLSnapshotState::restore() {
+    for (const auto& it : mEnables) {
+        if (it.second) {
+            mGL->glEnable(it.first);
+        } else {
+            mGL->glDisable(it.first);
+        }
+    }
+
+    std::vector<float> clearColor = mGlobals[GL_COLOR_CLEAR_VALUE].floats;
+    mGL->glClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
+
+    mGL->glActiveTexture(mGlobals[GL_ACTIVE_TEXTURE].ints[0]);
+}
+
+} // namespace GLSnapshot
---
+++ android/android-emugl/host/libs/libGLSnapshot/GLSnapshot.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "GLESv2Dispatch.h"
+
+#include <map>
+#include <vector>
+
+#include <inttypes.h>
+
+#include <GLES2/gl2.h>
+
+namespace GLSnapshot {
+
+struct GLValue {
+    std::vector<GLenum> enums;
+    std::vector<unsigned char> bytes;
+    std::vector<uint16_t> shorts;
+    std::vector<uint32_t> ints;
+    std::vector<float> floats;
+    std::vector<uint64_t> int64s;
+};
+
+typedef std::map<GLenum, GLValue> GlobalStateMap;
+typedef std::map<GLenum, bool> GlobalEnables;
+
+class GLSnapshotState {
+public:
+    GLSnapshotState(const GLESv2Dispatch* gl);
+    void save();
+    void restore();
+private:
+    void getGlobalStateEnum(GLenum name, int size);
+    void getGlobalStateByte(GLenum name, int size);
+    void getGlobalStateInt(GLenum name, int size);
+    void getGlobalStateFloat(GLenum name, int size);
+    void getGlobalStateInt64(GLenum name, int size);
+
+    void getGlobalStateEnable(GLenum name);
+
+    const GLESv2Dispatch* mGL;
+    GlobalStateMap mGlobals;
+    GlobalEnables mEnables;
+};
+
+} // namespace GLSnapshot
