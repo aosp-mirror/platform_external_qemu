b49d18427f9a01b24436316e01d76f46111ea1ad
Joshua Duong <joshuaduong@google.com>
2017-01-27 12:04:17 -0800

    [UI] Layout for Google Play tab (hidden).
    Change-Id: I110920eb6cc14ed49a32d0f37083292f1db28f2b

---
+++ android/android-emu/android/skin/qt/extended-pages/google-play-page.h
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#pragma once
+
+#include "ui_google-play-page.h"
+#include <QWidget>
+#include <memory>
+
+class GooglePlayPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GooglePlayPage(QWidget *parent = 0);
+    ~GooglePlayPage();
+
+private slots:
+    void on_goog_updateServicesButton_clicked();
+
+private:
+    std::unique_ptr<Ui::GooglePlayPage> mUi;
+};
