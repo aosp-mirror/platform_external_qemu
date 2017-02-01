cd86f38ab2adc4e4033dbf549df9c69e611f4b03
Yurii Zubrytskyi <zyy@google.com>
2017-01-31 17:29:16 -0800

    Make BufferQueue able to save/load from snapshots
    1. Add shapshot mode to BufferQueue: when it is on the queue
      accepts all write requests, even if it needs to grow beyond
      the original max capacity. This way writes never block in
      snapshot mode, so we can make sure all writers will unblock
      and reach the point where they check for snapshot.
      On the other hand, reads always return error - so no new
      operation can start in snapshot mode.
    
    2. To implement #1, the internal buffer is now based on
      std::vector<> instead of a raw array.
    
    Change-Id: Icfe5d6a29a381f5367fe9592602458fb680acaf3

--- android/android-emugl/host/libs/libOpenglRender/BufferQueue.h
+++ android/android-emugl/host/libs/libOpenglRender/BufferQueue.h
@@ -15,11 +15,16 @@
 
 #include "OpenglRender/RenderChannel.h"
 #include "android/base/Compiler.h"
+#include "android/base/files/Stream.h"
+#include "android/base/files/StreamSerializing.h"
 #include "android/base/synchronization/Lock.h"
 #include "android/base/synchronization/MessageChannel.h"
 
-#include <memory>
+#include <iterator>
+#include <vector>
+#include <utility>
 
+#include <assert.h>
 #include <stddef.h>
 
 namespace emugl {
@@ -43,38 +48,50 @@ public:
     // Constructor. |capacity| is the maximum number of Buffer instances in
     // the queue, and |lock| is a reference to an external lock provided by
     // the caller.
-    BufferQueue(size_t capacity, android::base::Lock& lock)
-        : mCapacity(capacity), mBuffers(new Buffer[capacity]), mLock(lock) {}
-
-    // Return true iff the queue has been closed. Remaining items can
-    // be read from its, but it won't be possible to add new items to it.
-    // bool closed() const { return mClosed; }
+    BufferQueue(int capacity, android::base::Lock& lock)
+        : mBuffers(capacity), mLock(lock) {}
 
     // Return true iff one can send a buffer to the queue, i.e. if it
-    // is not full.
-    bool canPushLocked() const { return !mClosed && (mCount < mCapacity); }
+    // is not full or it would grow anyway.
+    bool canPushLocked() const { return !mClosed && mCount < (int)mBuffers.size(); }
 
     // Return true iff one can receive a buffer from the queue, i.e. if
     // it is not empty.
-    bool canPopLocked() const { return mCount > 0U; }
+    bool canPopLocked() const { return mCount > 0; }
 
     // Return true iff the queue is closed.
     bool isClosedLocked() const { return mClosed; }
+
+    // Changes the operation mode to snapshot or back. In snapshot mode
+    // BufferQueue accepts all write requests and accumulates the data, but
+    // returns error on all reads.
+    void setSnapshotModeLocked(bool on) {
+        mSnapshotMode = on;
+        if (on && !mClosed) {
+            wakeAllWaiters();
+        }
+    }
 
     // Try to send a buffer to the queue. On success, return IoResult::Ok
     // and moves |buffer| to the queue. On failure, return
     // IoResult::TryAgain if the queue was full, or IoResult::Error
     // if it was closed.
+    // Note: in snapshot mode it never returns TryAgain, but grows the max
+    //   queue size instead.
     IoResult tryPushLocked(Buffer&& buffer) {
         if (mClosed) {
             return IoResult::Error;
         }
-        if (mCount >= mCapacity) {
-            return IoResult::TryAgain;
+        if (mCount >= (int)mBuffers.size()) {
+            if (mSnapshotMode) {
+                grow();
+            } else {
+                return IoResult::TryAgain;
+            }
         }
-        size_t pos = mPos + mCount;
-        if (pos >= mCapacity) {
-            pos -= mCapacity;
+        int pos = mPos + mCount;
+        if (pos >= (int)mBuffers.size()) {
+            pos -= mBuffers.size();
         }
         mBuffers[pos] = std::move(buffer);
         if (mCount++ == 0) {
@@ -87,7 +104,7 @@ public:
     // move |buffer| into the queue and return IoResult::Ok. On failure,
     // return IoResult::Error meaning the queue was closed.
     IoResult pushLocked(Buffer&& buffer) {
-        while (mCount == mCapacity) {
+        while (mCount == (int)mBuffers.size() && !mSnapshotMode) {
             if (mClosed) {
                 return IoResult::Error;
             }
@@ -98,19 +115,20 @@ public:
 
     // Try to read a buffer from the queue. On success, moves item into
     // |*buffer| and return IoResult::Ok. On failure, return IoResult::Error
-    // if the queue is empty and closed, and IoResult::TryAgain if it is
-    // empty but not close.
+    // if the queue is empty and closed or in snapshot mode, and
+    // IoResult::TryAgain if it is empty but not closed.
     IoResult tryPopLocked(Buffer* buffer) {
         if (mCount == 0) {
-            return mClosed ? IoResult::Error : IoResult::TryAgain;
+            return (mClosed || mSnapshotMode) ? IoResult::Error
+                                              : IoResult::TryAgain;
         }
         *buffer = std::move(mBuffers[mPos]);
-        size_t pos = mPos + 1;
-        if (pos >= mCapacity) {
-            pos -= mCapacity;
+        int pos = mPos + 1;
+        if (pos >= (int)mBuffers.size()) {
+            pos -= mBuffers.size();
         }
         mPos = pos;
-        if (mCount-- == mCapacity) {
+        if (mCount-- == (int)mBuffers.size()) {
             mCanPush.signal();
         }
         return IoResult::Ok;
@@ -118,9 +136,10 @@ public:
 
     // Pop a buffer from the queue. This is a blocking call. On success,
     // move item into |*buffer| and return IoResult::Ok. On failure,
-    // return IoResult::Error to indicate the queue was closed.
+    // return IoResult::Error to indicate the queue was closed or is in
+    // snapshot mode.
     IoResult popLocked(Buffer* buffer) {
-        while (mCount == 0) {
+        while (mCount == 0 && !mSnapshotMode) {
             if (mClosed) {
                 // Closed queue is empty.
                 return IoResult::Error;
@@ -136,9 +155,59 @@ public:
     // IoResult::Error once the queue becomes empty).
     void closeLocked() {
         mClosed = true;
+        wakeAllWaiters();
+    }
 
-        // Wake any potential waiters.
-        if (mCount == mCapacity) {
+    // Save to a snapshot file
+    void onSaveLocked(android::base::Stream* stream) {
+        stream->putByte(mClosed);
+        if (!mClosed) {
+            stream->putBe32(mCount);
+            for (int i = 0; i < mCount; i++) {
+                android::base::saveBuffer(
+                        stream, mBuffers[(i + mPos) % mBuffers.size()]);
+            }
+        }
+    }
+
+    bool onLoadLocked(android::base::Stream* stream) {
+        mClosed = stream->getByte();
+        if (!mClosed) {
+            mCount = stream->getBe32();
+            if ((int)mBuffers.size() < mCount) {
+                mBuffers.resize(mCount);
+            }
+            mPos = 0;
+            for (int i = 0; i < mCount; i++) {
+                if (!android::base::loadBuffer(stream, &mBuffers[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+private:
+    void grow() {
+        assert(mCount == (int)mBuffers.size());
+        std::vector<Buffer> newBuffers;
+        newBuffers.reserve(mBuffers.size() * 2);
+        newBuffers.insert(newBuffers.end(),
+                          std::make_move_iterator(mBuffers.begin() + mPos),
+                          std::make_move_iterator(
+                                  mBuffers.begin() +
+                                  std::min<int>(mPos + mCount, mBuffers.size())));
+        newBuffers.insert(
+                newBuffers.end(), std::make_move_iterator(mBuffers.begin()),
+                std::make_move_iterator(mBuffers.begin() +
+                                        (mPos + mCount) % mBuffers.size()));
+        mBuffers = std::move(newBuffers);
+        mBuffers.resize(mBuffers.capacity());
+        mPos = 0;
+    }
+
+    void wakeAllWaiters() {
+        if (mCount == (int)mBuffers.size()) {
             mCanPush.broadcast();
         }
         if (mCount == 0) {
@@ -147,17 +216,17 @@ public:
     }
 
 private:
-    size_t mCapacity = 0;
-    size_t mPos = 0;
-    size_t mCount = 0;
+    int mPos = 0;
+    int mCount = 0;
     bool mClosed = false;
-    std::unique_ptr<Buffer[]> mBuffers;
+    bool mSnapshotMode = false;
+    std::vector<Buffer> mBuffers;
 
     Lock& mLock;
     ConditionVariable mCanPush;
     ConditionVariable mCanPop;
 
-    // This will force the same for SyncBufferQueue and RenderChannelImpl
+    // This will force the same for RenderChannelImpl
     DISALLOW_COPY_ASSIGN_AND_MOVE(BufferQueue);
 };
 
