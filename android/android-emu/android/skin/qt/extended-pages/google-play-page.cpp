24903aa1de9a0c25d5420a807f8c919fa413ace1
Joshua Duong <joshuaduong@google.com>
2017-02-02 14:10:19 -0800

    [UI] Implement Play Store and Play Services Button.
    Change-Id: Ibbd6034f82c8ddb854a08a88f5516c6aba60307e

--- android/android-emu/android/skin/qt/extended-pages/google-play-page.cpp
+++ android/android-emu/android/skin/qt/extended-pages/google-play-page.cpp
@@ -11,18 +11,79 @@
 
 #include "android/skin/qt/extended-pages/google-play-page.h"
 
-GooglePlayPage::GooglePlayPage(QWidget *parent) :
-    QWidget(parent),
-    mUi(new Ui::GooglePlayPage)
-{
+#include "android/skin/qt/error-dialog.h"
+
+using android::emulation::GooglePlayServices;
+
+GooglePlayPage::GooglePlayPage(QWidget* parent)
+    : QWidget(parent),
+      mGooglePlayServices(nullptr),
+      mUi(new Ui::GooglePlayPage) {
     mUi->setupUi(this);
 }
 
-GooglePlayPage::~GooglePlayPage() {
+GooglePlayPage::~GooglePlayPage() {}
+
+void GooglePlayPage::setAdbInterface(android::emulation::AdbInterface* adb) {
+    mGooglePlayServices.setAdbInterface(adb);
+}
+
+void GooglePlayPage::playStoreSettings() {
+    mGooglePlayServices.showPlayStoreSettings(
+            [this](GooglePlayServices::Result result) {
+                GooglePlayPage::playStoreSettingsDone(result);
+            });
+}
+
+void GooglePlayPage::playStoreSettingsDone(GooglePlayServices::Result result) {
+    QString msg;
+    switch (result) {
+        case GooglePlayServices::Result::kSuccess:
+            return;
+
+        case GooglePlayServices::Result::kOperationInProgress:
+            msg =
+                    tr("Still waiting for play store to open.<br/>"
+                       "Please try again later.");
+            break;
+        default:
+            msg =
+                    tr("There was an unknown error while opening the "
+                       "play store.");
+    }
+    showErrorDialog(msg, tr("Play Store Settings"));
+}
+
+void GooglePlayPage::playServicesPage() {
+    mGooglePlayServices.showPlayServicesPage(
+            [this](GooglePlayServices::Result result) {
+                GooglePlayPage::playServicesPageDone(result);
+            });
+}
+
+void GooglePlayPage::playServicesPageDone(GooglePlayServices::Result result) {
+    QString msg;
+    switch (result) {
+        case GooglePlayServices::Result::kSuccess:
+            return;
+
+        case GooglePlayServices::Result::kOperationInProgress:
+            msg =
+                    tr("Still waiting for play services page to open.<br/>"
+                       "Please try again later.");
+            break;
+        default:
+            msg =
+                    tr("There was an unknown error while opening the "
+                       "play services page.");
+    }
+    showErrorDialog(msg, tr("Play Services Page"));
 }
 
 void GooglePlayPage::on_goog_updateServicesButton_clicked() {
+    playServicesPage();
 }
 
 void GooglePlayPage::on_goog_updateStoreButton_clicked() {
+    playStoreSettings();
 }
