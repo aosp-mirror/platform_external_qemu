e9422647f49d545167b8f590e2255e84880e7c83
Joshua Duong <joshuaduong@google.com>
2017-02-07 21:36:35 -0800

    [UI] Conditional Google Play tab.
    Only show this tab for Google APIs >= 24.
    
    Change-Id: I5fa0760388ef2dc57c38bc96d5743c6f3149fa4e

--- android/android-emu/android/skin/qt/extended-window.cpp
+++ android/android-emu/android/skin/qt/extended-window.cpp
@@ -59,8 +59,6 @@ ExtendedWindow::ExtendedWindow(
     mExtendedUi->setupUi(this);
     mExtendedUi->helpPage->initialize(shortcuts);
     mExtendedUi->dpadPage->setEmulatorWindow(mEmulatorWindow);
-    mExtendedUi->googlePlayPage->initialize(
-            mEmulatorWindow->getAdbInterface());
     mExtendedUi->rotaryInputPage->setEmulatorWindow(mEmulatorWindow);
     mExtendedUi->settingsPage->setAdbInterface(
             mEmulatorWindow->getAdbInterface());
@@ -119,11 +117,21 @@ ExtendedWindow::ExtendedWindow(
     mSidebarButtons.addButton(mExtendedUi->recordScreenButton);
     mSidebarButtons.addButton(mExtendedUi->settingsButton);
     mSidebarButtons.addButton(mExtendedUi->helpButton);
-    // TODO(joshuaduong): This tab should only be used when running
-    // Google API images with the play store. This condition will
-    // rely on two factors: the minimum API level we support play
-    // store for and whether it is a Google API image.
-    mSidebarButtons.addButton(mExtendedUi->googlePlayButton);
+
+    // For now, we are only shipping play store images with
+    // >= API 24 Google images.
+    const int MIN_GOOGLE_PLAY_API = 24;
+    const int DEFAULT_API = 1000; // returned if getApiLevel() fails
+    int apiLevel = avdInfo_getApiLevel(android_avdInfo);
+    if (apiLevel >= MIN_GOOGLE_PLAY_API
+        && apiLevel != DEFAULT_API
+        && avdInfo_isGoogleApis(android_avdInfo)) {
+        mSidebarButtons.addButton(mExtendedUi->googlePlayButton);
+        mExtendedUi->googlePlayPage->initialize(
+                mEmulatorWindow->getAdbInterface());
+    } else {
+        mExtendedUi->googlePlayButton->hide();
+    }
 
 #ifdef __APPLE__
     for (QWidget* w : findChildren<QWidget*>()) {
