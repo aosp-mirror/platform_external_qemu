3ee0d6b098cac7e023d7c984c698ff9cede1269a
Joshua Duong <joshuaduong@google.com>
2017-02-13 11:57:05 -0800

    [UI] Add text for version labels in unsuccessful queries.
    Change-Id: Id50f7e66204511b477b1103f0161e36915fce334

--- android/android-emu/android/skin/qt/extended-pages/google-play-page.cpp
+++ android/android-emu/android/skin/qt/extended-pages/google-play-page.cpp
@@ -166,6 +166,8 @@ void GooglePlayPage::playVersionDone(GooglePlayServices::Result result,
         case PlayApps::PlayServices:
             textEdit = mUi->goog_playServicesVersionBox;
             break;
+        default:
+            return;
     }
 
     switch (result) {
@@ -174,16 +176,19 @@ void GooglePlayPage::playVersionDone(GooglePlayServices::Result result,
             return;
 
         case GooglePlayServices::Result::AppNotInstalled:
+            textEdit->setPlainText(tr("Not Installed"));
             msg = tr("It doesn't look like you have %1 "
                      "installed.")
                           .arg(getPlayAppDescription(app));
             break;
         case GooglePlayServices::Result::OperationInProgress:
+            textEdit->setPlainText(tr("Loading..."));
             msg = tr("Still waiting for %1 version.<br/>"
                      "Please try again later.")
                           .arg(getPlayAppDescription(app));
             break;
         default:
+            textEdit->setPlainText(tr("Unknown Error"));
             msg = tr("There was an unknown error while getting %1"
                      " version.")
                           .arg(getPlayAppDescription(app));
