4144d4437636d95b8923eb4f2283678a7beda991
Huihong Luo <huisinro@google.com>
2017-02-28 11:20:42 -0800

    Start to add virtio based goldfish for opengl. Use this command line options to run: objs/emulator -engine qemu2 -gpu host -skip-adb-auth -skin 1280x800 -qemu -device virtio-goldfish-pipe-pci
    This is initial checkin, opengl processing is not
    hooked yet.
    
    Change-Id: Ifa36318571e7fa73b14f2c4bfe7918f7d647021a

---
+++ include/standard-headers/linux/virtio_goldfish_pipe.h
@@ -0,0 +1,8 @@
+#ifndef _LINUX_VIRTIO_GOLDFISH_PIPE_H
+#define _LINUX_VIRTIO_GOLDFISH_PIPE_H
+/* This header is BSD licensed so anyone can use the definitions to implement
+ * compatible drivers/servers. */
+#include "standard-headers/linux/virtio_ids.h"
+#include "standard-headers/linux/virtio_config.h"
+
+#endif /* _LINUX_VIRTIO_GOLDFISH_PIPE_H */
