cd620fab36899a44034244e937c1dbaae992b485
Yao Chen <yaochen@google.com>
2017-02-28 11:31:40 -0800

    [WIP] Add Car sensor data emulation
    Change-Id: Ib40b4bd66370d6e70b93891d7463b94b601d2969

---
+++ android/android-emu/android/skin/qt/extended-pages/car-data-emulation/car-sensor-data.cpp
@@ -0,0 +1,319 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#include "android/skin/qt/extended-pages/car-data-emulation/car-sensor-data.h"
+
+#include "android/skin/qt/qt-settings.h"
+#include "android/utils/debug.h"
+#include "ui_car-sensor-data.h"
+#include "vehicle_constants.h"
+
+#include <QSettings>
+
+#define D(...) VERBOSE_PRINT(car, __VA_ARGS__)
+
+using std::function;
+using std::string;
+
+using emulator::EmulatorMessage;
+using emulator::MsgType;
+using emulator::Status;
+using emulator::VehicleProperty;
+using emulator::VehiclePropValue;
+using emulator::VehicleGear;
+using emulator::VehicleDrivingStatus;
+using emulator::VehicleIgnitionState;
+
+CarSensorData::CarSensorData(QWidget* parent)
+    : QWidget(parent), mUi(new Ui::CarSensorData) {
+    mUi->setupUi(this);
+}
+
+static EmulatorMessage getSetPropMsg() {
+    EmulatorMessage emulatorMsg;
+    emulatorMsg.set_msg_type(MsgType::SET_PROPERTY_CMD);
+    emulatorMsg.set_status(Status::RESULT_OK);
+    return emulatorMsg;
+}
+
+void CarSensorData::sendGearChangeMsg(int gear, string gearName) {
+    if (sendEmulatorMsg == nullptr) {
+        return;
+    }
+
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::CURRENT_GEAR));
+    value->add_int32_values(gear);
+    string log = "Gear changed to " + gearName;
+    sendEmulatorMsg(emulatorMsg, log);
+}
+
+void CarSensorData::sendDrivingStatusChangeMsg(int status, string statusName) {
+    if (sendEmulatorMsg == nullptr) {
+        return;
+    }
+
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::DRIVING_STATUS));
+    value->add_int32_values(status);
+    string log = "Driving status: " + statusName;
+    sendEmulatorMsg(emulatorMsg, log);
+}
+
+void CarSensorData::collectDrivingStatusAndReport() {
+    if (sendEmulatorMsg == nullptr) {
+        return;
+    }
+    string statusMsg;
+    int drivingStatus = 0;
+    if (mUi->checkBox_no_video->isChecked()) {
+        drivingStatus |= static_cast<int32_t>(VehicleDrivingStatus::NO_VIDEO);
+        statusMsg += "NO_VIDEO ";
+    }
+
+    if (mUi->checkBox_no_keyboard->isChecked()) {
+        drivingStatus |=
+                static_cast<int32_t>(VehicleDrivingStatus::NO_KEYBOARD_INPUT);
+        statusMsg += "NO_KEYBOARD_INPUT ";
+    }
+
+    if (mUi->checkBox_limit_msg_len->isChecked()) {
+        drivingStatus |=
+                static_cast<int32_t>(VehicleDrivingStatus::LIMIT_MESSAGE_LEN);
+        statusMsg += "LIMIT_MESSAGE_LEN ";
+    }
+
+    if (mUi->checkBox_no_config->isChecked()) {
+        drivingStatus |= static_cast<int32_t>(VehicleDrivingStatus::NO_CONFIG);
+        statusMsg += "NO_CONFIG ";
+    }
+
+    if (mUi->checkBox_no_voice->isChecked()) {
+        drivingStatus |=
+                static_cast<int32_t>(VehicleDrivingStatus::NO_VOICE_INPUT);
+        statusMsg += "NO_VOICE_INPUT ";
+    }
+
+    if (drivingStatus != 0) {
+        mUi->checkBox_unrestricted->setChecked(false);
+    }
+
+    sendDrivingStatusChangeMsg(drivingStatus, statusMsg);
+}
+
+void CarSensorData::sendIgnitionChangeMsg(int ignition, string ignitionName) {
+    if (sendEmulatorMsg == nullptr) {
+        return;
+    }
+
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::IGNITION_STATE));
+    value->add_int32_values(ignition);
+    string log = "Ignition state: " + ignitionName;
+    sendEmulatorMsg(emulatorMsg, log);
+}
+
+void CarSensorData::on_car_speedSlider_valueChanged(int speed) {
+    mUi->car_speedLabel->setText(QString::number(speed) + " mph");
+    if (sendEmulatorMsg != nullptr) {
+        EmulatorMessage emulatorMsg = getSetPropMsg();
+        VehiclePropValue* value = emulatorMsg.add_value();
+        value->set_prop(
+                static_cast<int32_t>(VehicleProperty::PERF_VEHICLE_SPEED));
+        value->add_float_values(speed);
+        string log = "Speed changed to " + std::to_string(speed);
+        sendEmulatorMsg(emulatorMsg, log);
+    }
+}
+
+void CarSensorData::setSendEmulatorMsg(
+        const function<void(const EmulatorMessage, const string log)>& func) {
+    sendEmulatorMsg = func;
+}
+
+void CarSensorData::on_radioButton_D_toggled() {
+    if (mUi->radioButton_D->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_DRIVE), "D");
+    }
+}
+
+void CarSensorData::on_radioButton_R_toggled() {
+    if (mUi->radioButton_R->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_REVERSE), "R");
+    }
+}
+
+void CarSensorData::on_radioButton_P_toggled() {
+    if (mUi->radioButton_P->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_PARK), "P");
+    }
+}
+
+void CarSensorData::on_radioButton_N_toggled() {
+    if (mUi->radioButton_N->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_NEUTRAL), "N");
+    }
+}
+
+void CarSensorData::on_radioButton_D1_toggled() {
+    if (mUi->radioButton_D1->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_1), "D1");
+    }
+}
+
+void CarSensorData::on_radioButton_D2_toggled() {
+    if (mUi->radioButton_D2->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_2), "D2");
+    }
+}
+
+void CarSensorData::on_radioButton_D3_toggled() {
+    if (mUi->radioButton_D3->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_3), "D3");
+    }
+}
+
+void CarSensorData::on_radioButton_D4_toggled() {
+    if (mUi->radioButton_D4->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_4), "D4");
+    }
+}
+
+void CarSensorData::on_radioButton_D5_toggled() {
+    if (mUi->radioButton_D5->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_5), "D5");
+    }
+}
+
+void CarSensorData::on_radioButton_D6_toggled() {
+    if (mUi->radioButton_D6->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_6), "D6");
+    }
+}
+
+void CarSensorData::on_radioButton_D7_toggled() {
+    if (mUi->radioButton_D7->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_7), "D7");
+    }
+}
+
+void CarSensorData::on_radioButton_D8_toggled() {
+    if (mUi->radioButton_D8->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_8), "D8");
+    }
+}
+
+void CarSensorData::on_radioButton_D9_toggled() {
+    if (mUi->radioButton_D9->isChecked()) {
+        sendGearChangeMsg(static_cast<int32_t>(VehicleGear::GEAR_9), "D9");
+    }
+}
+
+void CarSensorData::on_checkBox_night_toggled() {
+    bool night = mUi->checkBox_night->isChecked();
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::NIGHT_MODE));
+    value->add_int32_values(night ? 1 : 0);
+    string log = "Night mode: " + std::to_string(night);
+    sendEmulatorMsg(emulatorMsg, log);
+}
+
+void CarSensorData::on_radioButton_lock_toggled() {
+    if (mUi->radioButton_lock->isChecked()) {
+        sendIgnitionChangeMsg(static_cast<int32_t>(VehicleIgnitionState::LOCK),
+                              "LOCK");
+    }
+}
+
+void CarSensorData::on_radioButton_off_toggled() {
+    if (mUi->radioButton_off->isChecked()) {
+        sendIgnitionChangeMsg(static_cast<int32_t>(VehicleIgnitionState::OFF),
+                              "OFF");
+    }
+}
+
+void CarSensorData::on_radioButton_start_toggled() {
+    if (mUi->radioButton_start->isChecked()) {
+        sendIgnitionChangeMsg(static_cast<int32_t>(VehicleIgnitionState::START),
+                              "START");
+    }
+}
+
+void CarSensorData::on_radioButton_acc_toggled() {
+    if (mUi->radioButton_acc->isChecked()) {
+        sendIgnitionChangeMsg(static_cast<int32_t>(VehicleIgnitionState::ACC),
+                              "ACC");
+    }
+}
+
+void CarSensorData::on_radioButton_on_toggled() {
+    if (mUi->radioButton_on->isChecked()) {
+        sendIgnitionChangeMsg(static_cast<int32_t>(VehicleIgnitionState::ON),
+                              "ON");
+    }
+}
+
+void CarSensorData::on_checkBox_park_toggled() {
+    bool parkBrakeOn = mUi->checkBox_park->isChecked();
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::PARKING_BRAKE_ON));
+    value->add_int32_values(parkBrakeOn ? 1 : 0);
+    string log = "Park brake: " + std::to_string(parkBrakeOn);
+    sendEmulatorMsg(emulatorMsg, log);
+}
+
+void CarSensorData::on_checkBox_unrestricted_toggled() {
+    // unrestricted status and restricted status are mutually exclusive.
+    if (mUi->checkBox_unrestricted->isChecked()) {
+        mUi->checkBox_no_video->setChecked(false);
+        mUi->checkBox_no_keyboard->setChecked(false);
+        mUi->checkBox_limit_msg_len->setChecked(false);
+        mUi->checkBox_no_config->setChecked(false);
+        mUi->checkBox_no_voice->setChecked(false);
+        sendDrivingStatusChangeMsg(
+                static_cast<int32_t>(VehicleDrivingStatus::UNRESTRICTED),
+                "UNRESTRICTED");
+    }
+}
+
+void CarSensorData::on_checkBox_no_video_toggled() {
+    collectDrivingStatusAndReport();
+}
+
+void CarSensorData::on_checkBox_no_keyboard_toggled() {
+    collectDrivingStatusAndReport();
+}
+
+void CarSensorData::on_checkBox_limit_msg_len_toggled() {
+    collectDrivingStatusAndReport();
+}
+
+void CarSensorData::on_checkBox_no_config_toggled() {
+    collectDrivingStatusAndReport();
+}
+
+void CarSensorData::on_checkBox_no_voice_toggled() {
+    collectDrivingStatusAndReport();
+}
+
+void CarSensorData::on_checkBox_fuel_low_toggled() {
+    bool fuelLow = mUi->checkBox_fuel_low->isChecked();
+    EmulatorMessage emulatorMsg = getSetPropMsg();
+    VehiclePropValue* value = emulatorMsg.add_value();
+    value->set_prop(static_cast<int32_t>(VehicleProperty::FUEL_LEVEL_LOW));
+    value->add_int32_values(fuelLow ? 1 : 0);
+    string log = "Fuel low: " + std::to_string(fuelLow);
+    sendEmulatorMsg(emulatorMsg, log);
+}
