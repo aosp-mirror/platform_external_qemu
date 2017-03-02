d45dbf1dcb2f43ef5a6909b1696768a36759e034
Yurii Zubrytskyi <zyy@google.com>
2017-03-01 16:10:43 -0800

    [lz4] Add lz4 library to package-release.sh
    Change-Id: Ida6ae3612add0e5e930a70d606beb263e8e44776

--- android/scripts/package-release.sh
+++ android/scripts/package-release.sh
@@ -656,7 +656,8 @@ build_darwin_binaries_on () {
             "$DARWIN_KERNEL_PREBUILTS"/x86/pc-bios
 
     for LIB in qemu-android-deps curl common/e2fsprogs common/libxml2 \
-            common/breakpad qt common/ANGLE common/x264 common/ffmpeg; do
+            common/breakpad qt common/ANGLE common/x264 common/ffmpeg \
+            common/lz4; do
         if [ ! -d "$AOSP_BUILD_PREBUILTS/$LIB/darwin-x86_64" ]; then
             panic "Missing $LIB Darwin prebuilts!"
         fi
