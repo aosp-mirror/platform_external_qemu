ccf6b900dea2f6a4319b0e8a628dbcc82109b858
Lingfeng Yang <lfy@google.com>
2017-03-06 17:06:03 -0800

    [snapshot] FenceSync
    Every once in a while, we can take a snapshot
    while rcDestroySyncKHR / rcClientWaitSyncKHR
    with stale FenceSync handles are about to come in.
    
    This accounts for that case using 2 unordered_set's,
    the set of fences currently active vs the old set of
    fences.
    
    If guest queried in the old set, it will just quit
    or return condition_satisfied.
    
    There are two unordered_set's to avoid collisions.
    
    Change-Id: I00d418694eb522e88b4b65eea9f0365e464b7a92

--- android/android-emugl/host/libs/libOpenglRender/SyncThread.cpp
+++ android/android-emugl/host/libs/libOpenglRender/SyncThread.cpp
@@ -170,7 +170,13 @@ void SyncThread::doSyncContextInit() {
 void SyncThread::doSyncWait(SyncThreadCmd* cmd) {
     DPRINT("enter");
 
-    assert(cmd->fenceSync);
+    FenceSync* fenceSync =
+        FenceSync::getFromHandle((uint64_t)(uintptr_t)cmd->fenceSync);
+
+    if (!fenceSync) {
+        emugl_sync_timeline_inc(cmd->timeline, kTimelineInterval);
+        return;
+    }
 
     EGLint wait_result = 0x0;
 
