0150c7e8c49c2d8e65c3e396c95aa726e9936cad
Weilun Du <wdu@google.com>
2017-03-09 18:08:28 -0800

    [UI] Load Adb Logcat and Generate Bugreport in Background
    Change-Id: I4ba194e9ba1cdbb7688120bcdb6be3ba8253574b
    Signed-off-by: Weilun Du <wdu@google.com>

--- android/android-emu/android/skin/qt/extended-pages/bug-report-window.h
+++ android/android-emu/android/skin/qt/extended-pages/bug-report-window.h
@@ -11,25 +11,38 @@
 
 #pragma once
 
-#include "ui_bug-report-window.h"
+#include "android/emulation/control/AdbBugReportServices.h"
 #include "android/emulation/control/ScreenCapturer.h"
+#include "ui_bug-report-window.h"
 
 #include <QFrame>
+#include <atomic>
 #include <memory>
 
 class BugReportWindow : public QFrame {
     Q_OBJECT
 
 public:
-    explicit BugReportWindow(android::emulation::ScreenCapturer* screenCapturer,
+    explicit BugReportWindow(android::emulation::AdbInterface* adb,
+                             android::emulation::ScreenCapturer* screenCapturer,
                              QWidget* parent = 0);
     void showEvent(QShowEvent* event);
 
 private:
+    void prepareBugReportInBackground();
+    void loadAdbLogcat();
     void loadScreenshotImage();
     void loadScreenshotImageDone(
             android::emulation::ScreenCapturer::Result result,
             android::base::StringView filePath);
-    std::unique_ptr<Ui::BugReportWindow> mUi;
+
+    android::emulation::AdbInterface* mAdb;
+    android::emulation::AdbBugReportServices mBugReportServices;
     android::emulation::ScreenCapturer* mScreenCapturer;
+    std::unique_ptr<Ui::BugReportWindow> mUi;
+    std::atomic_bool mBugReportSucceed;
+    std::atomic_bool mScreenshotSucceed;
+    std::string mBugReportSaveLocation;
+    android::base::StringView mAdbBugreportFilePath;
+    android::base::StringView mScreenshotFilePath;
 };
