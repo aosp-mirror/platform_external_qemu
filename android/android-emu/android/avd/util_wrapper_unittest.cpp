401020baccdab473077da97c9a3adc3ba90a7d65
Erwin Jansen <jansene@google.com>
2017-03-15 14:41:17 -0700

    Expand getSdkRoot to absolute path.
    - During the launch of qemu we set the ANDROID_EMULATOR_LAUNCHER_DIR
      environment variable. This can contain a relative path. Which can
      result in the SdkRootDirectory returning a relative path.
    - Added a unit test exposing the issue
    
    Bug: 35923040
    Test: AvdUtilWrapper.emulator_path_getSdkRoot
    Change-Id: If42509bd01ed8038144860ad2140116aa1c73819

---
+++ android/android-emu/android/avd/util_wrapper_unittest.cpp
@@ -0,0 +1,38 @@
+// Copyright 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/avd/util.h"
+#include "android/utils/path.h"
+#include "android/base/testing/TestSystem.h"
+
+#include <gtest/gtest.h>
+
+using namespace android::base;
+
+TEST(AvdUtilWrapper, emulator_path_getSdkRoot) {
+  TestSystem sys("", 32, "/myhome");
+  ASSERT_TRUE(sys.getTempRoot()->makeSubDir("/tmp"));
+  ASSERT_TRUE(sys.getTempRoot()->makeSubDir("/tmp/Sdk"));
+  ASSERT_TRUE(sys.getTempRoot()->makeSubDir("/tmp/Sdk/platform-tools"));
+  ASSERT_TRUE(sys.getTempRoot()->makeSubDir("/tmp/Sdk/platforms"));
+  ASSERT_TRUE(sys.getTempRoot()->makeSubDir("/tmp/Sdk/tools"));
+  ASSERT_TRUE(sys.pathIsDir("/tmp/Sdk"));
+
+  sys.setLauncherDirectory(".");
+  sys.setCurrentDirectoryForTesting("/tmp/Sdk/tools");
+
+  char* sdk = path_getSdkRoot();
+  EXPECT_TRUE(sdk != nullptr);
+  EXPECT_TRUE(path_is_absolute(sdk));
+  free(sdk);
+}
+
+
