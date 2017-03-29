acecb692f6483bbe5f3f2957591fa6b22e0c2c99
Lingfeng Yang <lfy@google.com>
2017-03-29 07:58:43 -0700

    [GLESv3] If on play store image, limit max version to ES 3.0
    Change-Id: I7f726dc339841ba39472321c7aacd2794436e3bd

--- android/android-emugl/host/libs/libOpenglRender/GLESVersionDetector.cpp
+++ android/android-emugl/host/libs/libOpenglRender/GLESVersionDetector.cpp
@@ -60,6 +60,11 @@ GLESDispatchMaxVersion calcMaxVersionFromDispatch() {
             break;
     }
 
+    // TODO: Modify when CTS compliant for OpenGL ES 3.1
+    if (res >= GLES_DISPATCH_MAX_VERSION_3_1 &&
+        emugl_feature_is_enabled(android::featurecontrol::PlayStoreImage))
+        res = GLES_DISPATCH_MAX_VERSION_3_0;
+
     emugl::setGlesVersion(maj, min);
 
     return res;
