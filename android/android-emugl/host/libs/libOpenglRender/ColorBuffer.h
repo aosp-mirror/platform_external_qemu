d774aa4ad4bbee194b30b3d3faa15ac63501b53a
Lingfeng Yang <lfy@google.com>
2017-03-31 16:57:05 -0700

    [WIP] Separate thread for flushing/posting
    - glcopytexsubimage2d on blit thread by taking
    advantage of FBO emulation
    - put open/close cb and create sync on blit thread as well,
    to fix OOO frames
    Change-Id: I0c6ee7435c5825cd3535eb9dd8efe4a083d5208e

--- android/android-emugl/host/libs/libOpenglRender/ColorBuffer.h
+++ android/android-emugl/host/libs/libOpenglRender/ColorBuffer.h
@@ -191,6 +191,11 @@ public:
     // Return true on success, false on failure (e.g. no current context).
     bool blitFromCurrentReadBuffer();
 
+    // The other part where we are in the helper context.
+    static void bindHelperContext();
+    static void unbindHelperContext();
+    bool blitFromCurrentReadBuffer2();
+
     // Read the content of the whole ColorBuffer as 32-bit RGBA pixels.
     // |img| must be a buffer large enough (i.e. width * height * 4).
     void readback(unsigned char* img);
@@ -206,6 +211,11 @@ private:
     ColorBuffer(EGLDisplay display, HandleType hndl, Helper* helper);
 
 private:
+    EGLint m_readRbo = 0;
+    EGLint m_drawRbo = 0;
+    EGLint m_readFbo = 0;
+    EGLint m_drawFbo = 0;
+
     GLuint m_tex = 0;
     GLuint m_blitTex = 0;
     EGLImageKHR m_eglImage = nullptr;
@@ -222,6 +232,7 @@ private:
     std::unique_ptr<YUVConverter> m_yuv_converter;
     HandleType mHndl;
 };
+
 
 typedef emugl::SmartPtr<ColorBuffer> ColorBufferPtr;
 #endif
