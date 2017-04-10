b887384951dd4f387a756abf257f943c3d3eb617
Huihong Luo <huisinro@google.com>
2017-04-10 14:59:28 -0700

    Add external display support using ffmpeg to decode the H.264 frames and then use SDL to display them
    Change-Id: I1dc3738738d44a9fe45f81a727c1a0768cfe3b49

---
+++ android/android-emu/android/external-display/SDLRenderer.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <pthread.h>
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+struct AVFrame;
+
+namespace android {
+namespace display {
+
+class DisplayWindow;
+
+class SDLRenderer {
+public:
+    static DisplayWindow* recreateSDLWindow(int width, int height);
+    static void draw_frame(DisplayWindow* window, AVFrame* frame);
+    static void resize_sdl_window(DisplayWindow* window, int width, int height);
+    static void delete_sdl_window(DisplayWindow* window);
+    static void draw_frame_internal(DisplayWindow* pWin, AVFrame* frame);
+};
+
+void* sdl_thread_func(void* arg);
+
+}  // namespace display
+}  // namespace android
