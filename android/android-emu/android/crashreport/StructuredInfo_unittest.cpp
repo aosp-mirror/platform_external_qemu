c55072a5dd479ede32d91898ffc61cbe7c5fb588
Lingfeng Yang <lfy@google.com>
2017-04-13 12:46:11 -0700

    [WIP] structured crash report info
    Change-Id: Ied866ebb7e267d63ab89d2b4add56534573c025d

---
+++ android/android-emu/android/crashreport/StructuredInfo_unittest.cpp
@@ -0,0 +1,41 @@
+// Copyright 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "android/crashreport/StructuredInfo.h"
+
+using namespace android::base;
+using namespace android::crashreport;
+using android::HostHwInfo;
+
+static GpuInfo intelgpu("0x8086", "", "", "", "", "");
+static GpuInfo nvidiagpu("0x10de", "", "", "", "", "");
+
+TEST(StructuredInfo, basic) {
+    StructuredInfo si;
+
+    GpuInfoList testgpus;
+    testgpus.infos.push_back(intelgpu);
+    testgpus.infos.push_back(nvidiagpu);
+
+    HostHwInfo::Info testinfo = {
+        "KVMKVMKVM",
+        0, 1, 64, 0xf,
+        "Linux",
+        &testgpus,
+    };
+
+    si.addHostHwInfo(testinfo);
+}
