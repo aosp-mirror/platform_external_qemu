de676c43127f9209b62b4b346f83b13401263b77
Lingfeng Yang <lfy@google.com>
2017-04-18 15:56:36 -0700

    Fix yet another leak in emulated FBOs
    This looks like a NVIDIA driver bug.
    
    It's not enough to bind the global context when deleting
    rbos from emulated fbo, apparently.
    
    This CL delays surface destruction until another makeCurrent
    call, so we are sure we have a "good" gl context.
    
    It also helps to reset the renderbuffer storage to 0,0 when
    deleting.
    
    +setwindowcolorbuffer also sets opened so can quickly close
    +avoid eglmakecurrent in egldestroysurface
    
    Test: android.opengl.cts.WrapperTest#testThreadCleanup doesn't have out
    of control RAM usage.
    
    Change-Id: Ia5335bcd1d09e104b00ec47c036cf30a7509bd2f

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -925,6 +925,7 @@ bool FrameBuffer::setWindowSurfaceColorBuffer(HandleType p_surface,
     }
 
     (*w).second.first->setColorBuffer((*c).second.cb);
+    (*c).second.opened = true;
     if (w->second.second) {
         closeColorBufferLocked(w->second.second);
     }
