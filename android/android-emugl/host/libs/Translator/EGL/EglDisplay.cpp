c5f82df045daebb286143089106de20a41bb7c3d
Lingfeng Yang <lfy@google.com>
2017-04-19 10:41:52 -0700

    Allow sharing between ES 2 and 3 contexts
    Why not? sooner or later this could be a showstopper
    
    Change-Id: I64286169faa10a5a7ca0e777d6e46aaf2494939a

--- android/android-emugl/host/libs/Translator/EGL/EglDisplay.cpp
+++ android/android-emugl/host/libs/Translator/EGL/EglDisplay.cpp
@@ -30,8 +30,8 @@ EglDisplay::EglDisplay(EGLNativeDisplayType dpy,
 {
     m_manager[GLES_1_1] = new ObjectNameManager(&m_globalNameSpace);
     m_manager[GLES_2_0] = new ObjectNameManager(&m_globalNameSpace);
-    m_manager[GLES_3_0] = new ObjectNameManager(&m_globalNameSpace);
-    m_manager[GLES_3_1] = new ObjectNameManager(&m_globalNameSpace);
+    m_manager[GLES_3_0] = m_manager[GLES_2_0];
+    m_manager[GLES_3_1] = m_manager[GLES_2_0];
 };
 
 EglDisplay::~EglDisplay() {
