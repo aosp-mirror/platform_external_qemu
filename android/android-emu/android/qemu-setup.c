62b05847ace89d1aaec660a8594aaf1447e0083a
Yahan Zhou <yahan@google.com>
2017-04-24 16:50:58 -0700

    Keystore qemu pipe service
    CTS requires a hardware backed keystore implementation. So we implement
    a keystore service here to serve as the virtual keystore hardware.
    
    CtsKeystoreTestCases failure 1 -> 0
    
    BUG: 37640731
    
    Change-Id: Ied95ecbddf9219370ddabea6815b376ddf90901a

--- android/android-emu/android/qemu-setup.c
+++ android/android-emu/android/qemu-setup.c
@@ -24,6 +24,7 @@
 #include "android/globals.h"
 #include "android/hw-fingerprint.h"
 #include "android/hw-sensors.h"
+#include "android/keymaster/Keymaster_wrapper.h"
 #include "android/car.h"
 #include "android/logcat-pipe.h"
 #include "android/opengles-pipe.h"
@@ -310,6 +311,7 @@ bool android_emulation_setup(const AndroidConsoleAgents* agents, bool isQemu2) {
     android_init_opengles_pipe();
     android_init_clipboard_pipe();
     android_init_logcat_pipe();
+    android_init_keymaster_pipe();
 
     if (android_op_port && android_op_ports) {
         derror("options -port and -ports cannot be used together.");
