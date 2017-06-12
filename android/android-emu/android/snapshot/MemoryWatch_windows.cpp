bbc524c3d86b6104ca452ff1477aa77f4d42c428
Yurii Zubrytskyi <zyy@google.com>
2017-06-12 09:33:48 -0700

    [WIP] Lazy snapshot load - windows prototype
    It doesn't work yet as it needs HAXM changes to support
    protection violation in the guest
    
    Change-Id: I6f312a80d459a5090409b9ef5b18c9d7e59922b1

--- android/android-emu/android/snapshot/MemoryWatch_windows.cpp
+++ android/android-emu/android/snapshot/MemoryWatch_windows.cpp
@@ -11,10 +11,38 @@
 
 #include "android/snapshot/MemoryWatch.h"
 
+#include "android/base/threads/FunctorThread.h"
+
+#include <windows.h>
+
+#include <cassert>
+#include <utility>
+
 namespace android {
 namespace snapshot {
 
-class MemoryAccessWatch::Impl {};
+static MemoryAccessWatch* sWatch = nullptr;
+
+class MemoryAccessWatch::Impl {
+public:
+    // TODO: implement the idle loader
+    // base::FunctorThread mOnIdleThread;
+    void* mExceptionHandler = nullptr;
+
+    MemoryAccessWatch::AccessCallback mAccessCallback;
+    MemoryAccessWatch::IdleCallback mIdleCallback;
+
+    static LONG WINAPI handleException(EXCEPTION_POINTERS* exceptionInfo) {
+        if (exceptionInfo->ExceptionRecord->ExceptionCode !=
+            EXCEPTION_ACCESS_VIOLATION) {
+            return (LONG)EXCEPTION_CONTINUE_SEARCH;
+        }
+        auto ptr =
+                (void*)exceptionInfo->ExceptionRecord->ExceptionInformation[1];
+        sWatch->mImpl->mAccessCallback(ptr);
+        return (LONG)EXCEPTION_CONTINUE_EXECUTION;
+    }
+};
 
 bool MemoryAccessWatch::isSupported() {
     return false;
@@ -22,22 +50,38 @@ bool MemoryAccessWatch::isSupported() {
 
 MemoryAccessWatch::MemoryAccessWatch(AccessCallback&& accessCallback,
                                      IdleCallback&& idleCallback)
-    : mImpl(new Impl()) {}
+    : mImpl(new Impl()) {
+    mImpl->mAccessCallback = std::move(accessCallback);
+    mImpl->mIdleCallback = std::move(idleCallback);
+    sWatch = this;
+    mImpl->mExceptionHandler =
+            ::AddVectoredExceptionHandler(true, &Impl::handleException);
+}
 
-MemoryAccessWatch::~MemoryAccessWatch() {}
+MemoryAccessWatch::~MemoryAccessWatch() {
+    ::RemoveVectoredExceptionHandler(mImpl->mExceptionHandler);
+    assert(sWatch == this);
+    sWatch = nullptr;
+}
 
 bool MemoryAccessWatch::valid() const {
-    return false;
+    return mImpl->mExceptionHandler != nullptr;
 }
 
 bool MemoryAccessWatch::registerMemoryRange(void* start, size_t length) {
-    return false;
+    DWORD oldProtect;
+    return ::VirtualProtect(start, length, PAGE_NOACCESS, &oldProtect) != 0;
 }
 
 void MemoryAccessWatch::doneRegistering() {}
 
 bool MemoryAccessWatch::fillPage(void* ptr, size_t length, const void* data) {
-    return false;
+    DWORD oldProtect;
+    if (!::VirtualProtect(ptr, length, PAGE_EXECUTE_READWRITE, &oldProtect)) {
+        return false;
+    }
+    memcpy(ptr, data, length);
+    return true;
 }
 
 }  // namespace snapshot
