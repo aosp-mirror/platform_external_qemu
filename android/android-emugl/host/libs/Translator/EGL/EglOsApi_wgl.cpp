4256813bd1cebeb5b15ddf07344bf217b03e359d
Yurii Zubrytskyi <zyy@google.com>
2017-06-19 10:37:48 -0700

    Initialize the in-out struct when calling WinAPI for pixel format
    Change-Id: Ic32291e6074c90a4cce617adfd3b7282f5fe3d86

--- android/android-emugl/host/libs/Translator/EGL/EglOsApi_wgl.cpp
+++ android/android-emugl/host/libs/Translator/EGL/EglOsApi_wgl.cpp
@@ -672,7 +672,26 @@ bool initPixelFormat(HDC dc, const WglExtensionsDispatch* dispatch) {
         return dispatch->wglChoosePixelFormatARB(
                 dc, &i0, &f0, 1, &iPixelFormat, &numpf);
     } else {
-        PIXELFORMATDESCRIPTOR  pfd;
+        PIXELFORMATDESCRIPTOR pfd = {
+            sizeof(PIXELFORMATDESCRIPTOR),  //  size of this pfd
+            1,                     // version number
+            PFD_DRAW_TO_WINDOW |   // support window
+            PFD_SUPPORT_OPENGL |   // support OpenGL
+            PFD_DOUBLEBUFFER,      // double buffered
+            PFD_TYPE_RGBA,         // RGBA type
+            32,                    // 32-bit color depth
+            0, 0, 0, 0, 0, 0,      // color bits ignored
+            0,                     // no alpha buffer
+            0,                     // shift bit ignored
+            0,                     // no accumulation buffer
+            0, 0, 0, 0,            // accum bits ignored
+            24,                    // 24-bit z-buffer
+            0,                     // no stencil buffer
+            0,                     // no auxiliary buffer
+            PFD_MAIN_PLANE,        // main layer
+            0,                     // reserved
+            0, 0, 0                // layer masks ignored
+        };
         return dispatch->ChoosePixelFormat(dc, &pfd);
     }
 }
