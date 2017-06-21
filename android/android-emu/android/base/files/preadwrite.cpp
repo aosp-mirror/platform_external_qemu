c0fe99daa1bb9a14226abe6011adef699082c255
Yurii Zubrytskyi <zyy@google.com>
2017-06-20 22:32:12 -0700

    [base] Add pwrite() wrapper similar to pread()
    Change-Id: I80fc9aa478f620a60aaf5f28f09f6d2ee858cb85

--- android/android-emu/android/base/files/preadwrite.cpp
+++ android/android-emu/android/base/files/preadwrite.cpp
@@ -9,7 +9,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-#include "android/base/files/pread.h"
+#include "android/base/files/preadwrite.h"
 
 #ifndef _WIN32
 #error Only build this file for Win32, other platforms have native pread() call
@@ -39,18 +39,44 @@ int64_t pread(int fd, void* buf, size_t count, int64_t offset) {
     DWORD countRead = 0;
     if (!::ReadFile(handle, buf, count, &countRead, &params)) {
         switch (::GetLastError()) {
-        case ERROR_IO_PENDING:
-            errno = EAGAIN;
-            return -1;
-        default:
-            errno = EINVAL;
-            return -1;
+            case ERROR_IO_PENDING:
+                errno = EAGAIN;
+                return -1;
+            default:
+                errno = EINVAL;
+                return -1;
         }
     }
     return static_cast<int64_t>(countRead);
 }
 
+int64_t pwrite(int fd, const void* buf, size_t count, int64_t offset) {
+    if (fd < 0) {
+        errno = EINVAL;
+        return -1;
+    }
+    auto handle = (HANDLE)_get_osfhandle(fd);
+    if (handle == INVALID_HANDLE_VALUE) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    OVERLAPPED params = {};
+    params.Offset = static_cast<DWORD>(offset);
+    params.OffsetHigh = static_cast<DWORD>(offset >> 32);
+    DWORD countWritten = 0;
+    if (!::WriteFile(handle, buf, count, &countWritten, &params)) {
+        switch (::GetLastError()) {
+            case ERROR_IO_PENDING:
+                errno = EAGAIN;
+                return -1;
+            default:
+                errno = EINVAL;
+                return -1;
+        }
+    }
+    return static_cast<int64_t>(countWritten);
+}
+
 }  // namespace base
 }  // namespace android
-
-
