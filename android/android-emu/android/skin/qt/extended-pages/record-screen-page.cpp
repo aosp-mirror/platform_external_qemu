a877b213f38e4b5a2f248d4e6bd3ed6ee4e8614a
Joshua Duong <joshuaduong@google.com>
2017-06-22 15:17:41 -0700

    [UI] Hook backend recording code to UI.
    Change-Id: Ifd0180e047e4c36d86c64dd61d253dde88ef212a

--- android/android-emu/android/skin/qt/extended-pages/record-screen-page.cpp
+++ android/android-emu/android/skin/qt/extended-pages/record-screen-page.cpp
@@ -11,87 +11,177 @@
 
 #include "android/skin/qt/extended-pages/record-screen-page.h"
 
+#include "android/globals.h"
+#include "android/base/files/PathUtils.h"
+#include "android/emulation/control/record_screen_agent.h"
+#include "android/skin/qt/extended-pages/common.h"
+#include "android/skin/qt/error-dialog.h"
+#include "android/skin/qt/qt-settings.h"
+
+#include <QDesktopServices>
+#include <QFileDialog>
+#include <QSettings>
 //#include "android/emulation/control/record-screen_agent.h"
 
-RecordScreenPage::RecordScreenPage(QWidget *parent) :
+using android::base::PathUtils;
+
+// static
+const char RecordScreenPage::kTmpMediaName[] = "tmp.webm";
+
+RecordScreenPage::RecordScreenPage(QWidget* parent) :
     QWidget(parent),
-    mUi(new Ui::RecordScreenPage)
-{
+    mUi(new Ui::RecordScreenPage) {
     mUi->setupUi(this);
     setRecordState(RecordState::Ready);
 
-    QObject::connect(&mTimer, &QTimer::timeout, this, &RecordScreenPage::updateElapsedTime);
+    mTmpFilePath = PathUtils::join(avdInfo_getContentPath(android_avdInfo),
+                              &kTmpMediaName[0]);
+    QObject::connect(&mTimer, &QTimer::timeout, this,
+                     &RecordScreenPage::updateElapsedTime);
 }
 
-RecordScreenPage::~RecordScreenPage() {
+RecordScreenPage::~RecordScreenPage() {}
+
+void RecordScreenPage::setRecordScreenAgent(const QAndroidRecordScreenAgent* agent) {
+    mRecordScreenAgent = agent;
 }
 
 void RecordScreenPage::setRecordState(RecordState newState) {
-	mState = newState;
+    mState = newState;
 
-	switch(mState) {
-	case RecordState::Ready:
-		mUi->rec_recordOverlayWidget->show();
-		mUi->rec_timeElapsedWidget->hide();
-		mUi->rec_playStopButton->hide();
-		mUi->rec_formatSwitch->hide();
-		mUi->rec_saveButton->hide();
-		mUi->rec_timeResLabel->hide();
-		mUi->rec_recordButton->setText(QString("START RECORDING"));
-		break;
-	case RecordState::Recording:
-		mUi->rec_recordOverlayWidget->show();
-		mUi->rec_timeElapsedLabel->setText("0s Recording...");
-		mUi->rec_timeElapsedWidget->show();
-		mUi->rec_playStopButton->hide();
-		mUi->rec_formatSwitch->hide();
-		mUi->rec_saveButton->hide();
-		mUi->rec_timeResLabel->hide();
-		mUi->rec_recordButton->setText(QString("STOP RECORDING"));
+    switch (mState) {
+        case RecordState::Ready:
+            mUi->rec_recordOverlayWidget->show();
+            mUi->rec_timeElapsedWidget->hide();
+            mUi->rec_playStopButton->hide();
+            mUi->rec_formatSwitch->hide();
+            mUi->rec_saveButton->hide();
+            mUi->rec_timeResLabel->hide();
+            mUi->rec_recordButton->setText(QString("START RECORDING"));
+            break;
+        case RecordState::Recording:
+            mUi->rec_recordOverlayWidget->show();
+            mUi->rec_timeElapsedLabel->setText("0s Recording...");
+            mUi->rec_timeElapsedWidget->show();
+            mUi->rec_playStopButton->hide();
+            mUi->rec_formatSwitch->hide();
+            mUi->rec_saveButton->hide();
+            mUi->rec_timeResLabel->hide();
+            mUi->rec_recordButton->setText(QString("STOP RECORDING"));
 
-		// Update every second
-		mSec = 0;
-		//connect(mTimer, SIGNAL(timeout()), this, SLOT(updateElapsedTime()));
-		mTimer.start(1000);
-		break;
-	case RecordState::Stopped:
-		mTimer.stop();
-                // TODO: Need to only show this when hovering over the video
-                // widget, which we don't have yet.
-		//mUi->rec_recordOverlayWidget->hide();
-                mUi->rec_recordOverlayWidget->show();
-		mUi->rec_timeElapsedWidget->hide();
-		mUi->rec_playStopButton->show();
-		mUi->rec_formatSwitch->show();
-		mUi->rec_saveButton->show();
-		mUi->rec_timeResLabel->show();
-		mUi->rec_recordButton->setText(QString("RECORD AGAIN"));
-		break;
-	default:;
-	}
+            // Update every second
+            mSec = 0;
+            // connect(mTimer, SIGNAL(timeout()), this,
+            // SLOT(updateElapsedTime()));
+            mTimer.start(1000);
+            break;
+        case RecordState::Stopped:
+            mTimer.stop();
+            // TODO: Need to only show this when hovering over the video
+            // widget, which we don't have yet.
+            mUi->rec_recordOverlayWidget->show();
+            mUi->rec_timeElapsedWidget->hide();
+            mUi->rec_playStopButton->show();
+            mUi->rec_formatSwitch->show();
+            mUi->rec_saveButton->show();
+            mUi->rec_timeResLabel->setText(
+                    QString("%1s / %2 x %3")
+                        .arg(mSec)
+                        .arg(android_hw->hw_lcd_width)
+                        .arg(android_hw->hw_lcd_height));
+            mUi->rec_timeResLabel->show();
+            mUi->rec_recordButton->setText(QString("RECORD AGAIN"));
+            break;
+        default:;
+    }
 }
 
 void RecordScreenPage::updateElapsedTime() {
-	QString qs = QString("%1s Recording...").arg(++mSec);
-	mUi->rec_timeElapsedLabel->setText(qs);
-	mTimer.start(1000);
+    QString qs = QString("%1s Recording...").arg(++mSec);
+    mUi->rec_timeElapsedLabel->setText(qs);
+    mTimer.start(1000);
+}
+
+void RecordScreenPage::on_rec_playStopButton_clicked() {
+    QString url = QString("file://%1").arg(mTmpFilePath.c_str());
+    QDesktopServices::openUrl(QUrl(url));
 }
 
 void RecordScreenPage::on_rec_recordButton_clicked() {
-	RecordState newState = RecordState::Ready;
+    RecordState newState = RecordState::Ready;
 
-	switch(mState) {
-	case RecordState::Ready:
-		newState = RecordState::Recording;
-		break;
-	case RecordState::Recording:
-		newState = RecordState::Stopped;
-		break;
-	case RecordState::Stopped:
-		newState = RecordState::Recording;
-		break;
-	default:;
-	}
+    if (!mRecordScreenAgent) {
+        // agent not ready yet
+        return;
+    }
 
-	setRecordState(newState);
+    switch (mState) {
+        case RecordState::Ready:
+            // startRecording() will determine which codec to use based on the
+            // file extension.
+            if (mRecordScreenAgent->startRecording(mTmpFilePath.c_str())) {
+                newState = RecordState::Recording;
+            } else {
+                // User probably started recording from command-line.
+                // TODO: show MessageBox saying "already recording."
+            }
+            break;
+        case RecordState::Recording:
+            // TODO: Make this call async because the recorder may take a while
+            // to encode all the frames. We'll need something to poll to let us
+            // know when the encoder is done.
+            mRecordScreenAgent->stopRecording();
+            newState = RecordState::Stopped;
+            break;
+        case RecordState::Stopped: // we can combine this state into readystate
+            if (mRecordScreenAgent->startRecording(mTmpFilePath.c_str())) {
+                newState = RecordState::Recording;
+            } else {
+                // User probably started recording from command-line.
+                // TODO: show MessageBox saying "already recording."
+            }
+            newState = RecordState::Recording;
+            break;
+        default:;
+    }
+
+    setRecordState(newState);
+}
+
+void RecordScreenPage::on_rec_saveButton_clicked() {
+    QSettings settings;
+
+    QString ext = mUi->rec_formatSwitch->currentText();
+    QString savePath =
+        QDir::toNativeSeparators(getRecordingSaveDirectory());
+    QString recordingName = QFileDialog::getSaveFileName(
+            this, tr("Save Recording"),
+            savePath + QString("/untitled.%1").arg(ext),
+            tr("Multimedia (*.%1)").arg(ext));
+
+    if ( recordingName.isEmpty() ) return; // Operation was canceled
+
+    QFileInfo fileInfo(recordingName);
+    QString dirName = fileInfo.absolutePath();
+
+    dirName = QDir::toNativeSeparators(dirName);
+
+    if ( !directoryIsWritable(dirName) ) {
+        QString errStr = tr("The path is not writable:<br>")
+                         + dirName;
+        showErrorDialog(errStr, tr("Save Recording"));
+        return;
+    }
+
+    settings.setValue(Ui::Settings::SCREENREC_SAVE_PATH, dirName);
+
+    // move temp recording file
+    // TODO: Copy the file to the save location since the user may want to save
+    // in multiple formats. Since the initial encoding is webm, we need to do a
+    // conversion if the user selects something else.
+    if (rename(mTmpFilePath.c_str(), recordingName.toStdString().c_str()) != 0) {
+        QString errStr = tr("Unknown error while saving<br>")
+                         + recordingName;
+        showErrorDialog(errStr, tr("Save Recording"));
+    }
 }
