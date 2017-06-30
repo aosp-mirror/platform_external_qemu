174020d7b8757f88187de73ce20509be30824ec3
Lingfeng Yang <lfy@google.com>
2017-06-30 12:06:47 -0700

    [mac][startup] Don't launch processes to obtain gpu info
    TODO
    
    Linux
    Windows
    
    Change-Id: Id98486713bb5753c9a740ba22cd4f795d1ea0746

--- android/android-emu/Makefile.android-emu.mk
+++ android/android-emu/Makefile.android-emu.mk
@@ -370,6 +370,7 @@ LOCAL_SRC_FILES := \
     android/opengl/GLProcessPipe.cpp \
     android/opengl/gpuinfo.cpp \
     android/opengl/logger.cpp \
+    android/opengl/NativeGpuInfo_$(BUILD_TARGET_OS).cpp \
     android/opengl/OpenglEsPipe.cpp \
     android/opengles.cpp \
     android/openssl-support.cpp \
@@ -455,6 +456,7 @@ endif
 ifeq ($(BUILD_TARGET_OS),darwin)
     ANDROID_EMU_BASE_LDLIBS += -Wl,-framework,AppKit
     ANDROID_EMU_BASE_LDLIBS += -Wl,-framework,Accelerate
+    ANDROID_EMU_BASE_LDLIBS += -Wl,-framework,IOKit
 endif
 
 ANDROID_EMU_STATIC_LIBRARIES := \
---
+++ android/android-emu/android/opengl/NativeGpuInfo_windows.cpp
@@ -0,0 +1,5 @@
+#include "android/opengl/NativeGpuInfo.h"
+
+void getGpuInfoListNative(GpuInfoList* out) {
+    // TODO
+}
