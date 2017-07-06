cfe05ba5acfd9f3c32db3bc65d6955e78945656e
Yahan Zhou <yahan@google.com>
2017-07-06 15:32:33 -0700

    [ANGLE Snapshot] Wire up EGL to EGL window system
    This CL introduces EglOsApi_egl, which wires up OS window system calls
    on top of anther EGL library.
    
    EGL2EGL is windows only for now. It will switch between OS WGL and ANGLE
    EGL (later swiftshader also) based on the flag passed by eglUseOsEglApi.
    
    Change-Id: I2a9e5ee1a04815d65908020e31b7c93e865f11e8

--- android/android-emugl/host/libs/Translator/EGL/EglGlobalInfo.cpp
+++ android/android-emugl/host/libs/Translator/EGL/EglGlobalInfo.cpp
@@ -27,8 +27,22 @@ namespace {
 
 // Use a LazyInstance to ensure thread-safe initialization.
 emugl::LazyInstance<EglGlobalInfo> sSingleton = LAZY_INSTANCE_INIT;
+bool sEgl2Egl = false;
 
 }  // namespace
+
+void EglGlobalInfo::setEgl2Egl(EGLBoolean enable) {
+    if (sSingleton.hasInstance()) {
+        fprintf(stderr, "WARNING: setting EGL engin type"
+            " after it is created, takes no effect.\n");
+        return;
+    }
+    sEgl2Egl = enable;
+}
+
+bool EglGlobalInfo::isEgl2Egl() {
+    return sEgl2Egl;
+}
 
 // static
 EglGlobalInfo* EglGlobalInfo::getInstance() {
@@ -36,8 +50,12 @@ EglGlobalInfo* EglGlobalInfo::getInstance() {
 }
 
 EglGlobalInfo::EglGlobalInfo() {
-    // TODO(digit): Choose alternate engine based on env. variable?
-    m_engine = EglOS::Engine::getHostInstance();
+    printf("init engine %d\n", sEgl2Egl);
+    if (sEgl2Egl) {
+        m_engine = EglOS::getEgl2EglHostInstance();
+    } else {
+        m_engine = EglOS::Engine::getHostInstance();
+    }
     m_display = m_engine->getDefaultDisplay();
 }
 
