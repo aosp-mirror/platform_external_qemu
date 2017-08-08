81d8a585c04dd736a53a4e75440614f0176ef1d0
Jim Kaye <jameskaye@google.com>
2017-08-08 14:30:52 -0700

    [UI] Remove the toggle for SIM presence
    There are a lot of SIM parameters that we want to control. It is
    not good to have one small part of this control without the rest.
    
    Retain the "-no-sim" command-line option to allow automated testing.
    
    (The toggle was added by I4b4f0abd8b5e2b99c03e8c97cdaeec1ef555eb73)
    
    Bug: 62137708
    
    Test: Verified that the toggle is missing. Verified that the
          command-line option still works.
    
    Change-Id: I422d708529fcab9419d19d3075804d7255d6c47c

--- android/android-emu/android/skin/qt/extended-pages/cellular-page.h
+++ android/android-emu/android/skin/qt/extended-pages/cellular-page.h
@@ -31,15 +31,11 @@ private slots:
     void on_cell_standardBox_currentIndexChanged(int index);
     void on_cell_voiceStatusBox_currentIndexChanged(int index);
     void on_cell_signalStatusBox_currentIndexChanged(int index);
-    void on_cell_simBox_clicked(bool isPresent);
 
     // TODO: Implement Network delay setting
     // http://developer.android.com/tools/devices/emulator.html#netdelay
 
 private:
-    static bool simSetting(); // The UI setting for SIM presence
-
     std::unique_ptr<Ui::CellularPage> mUi;
     const QAndroidCellularAgent* mCellularAgent = nullptr;
-    bool  mSimChangeInfoWasShown = false;     // True after the info box has been shown
 };
