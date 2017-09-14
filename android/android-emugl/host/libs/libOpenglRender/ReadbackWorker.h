a218f0859624350df6db59450b2d5ab839e084f2
Lingfeng Yang <lfy@google.com>
2017-09-14 11:39:11 -0700

    [record] Async readback
    bug: 65640600
    
    - Add api in Renderer / GpuFrameBridge for async readback:
      - bool asyncReadbackSupported() - self explanatory
      - ReadPixelsFunc getReadPixelsFunc() - get a callback to
        retrieve the latest pixels with memcpy. Unlike the post
        callback, this doesn't need to be called in the
        emugl thread.
    - Make it so that in async mode with recording, the post
      callback only notifies that there is a new frame,
      and a separate getRecordFrameAsync is used to retrieve
      actual pixels.
    - ReadbackWorker class that performs glReadPixels and
      glMapBufferRange buffers in mailbox style:
      - FrameBuffer::post triggers glReadPixels with PBO so
        that we exit post() more quickly (~14ms -> ~140us)
      - 3 buffers required:
      - maintain double buffering in glReadPixels to have low
        latency reading GPU frame to a GPU buffer
      - don't simultaneously glMapBufferRange and glReadPixels
      - Always glMapBufferRange the buffer that is not getting
        glReadPixels to avoid a sync point on glMapBufferRange
    - Try to be zero copy as possible:
      - dont memcpy on every post callback
      - memcpy only to the recording buffer
    - PBO only works on Mac with GL_UNSIGNED_INT_8_8_8_8_REV btw.
    
    + finish() method to wait on items of WorkerThread to finish
    + Stopwatch macros for more convenient time printouts
    + re-enable screen recording UI
    + encode at 24 fps (up from 20)
    
    Numbers:
    
    Antutu Marooned: 72 (no recording) -> 61 fps (with recording)
    Antutu Garden: 58 (no recording) -> 51 fps (with recording)
    Kube OpenGL ES 1 demo: 60 (no recording) -> 60 (with recording)
    
    Change-Id: I2ef2f2e7491ef165efd7618048ac099612e2955c

---
+++ android/android-emugl/host/libs/libOpenglRender/ReadbackWorker.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "android/base/synchronization/Lock.h"
+#include "android/base/synchronization/ConditionVariable.h"
+
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+#include <atomic>
+#include <memory>
+#include <vector>
+
+class ColorBuffer;
+class FrameBuffer;
+class RenderThreadInfo;
+
+class ReadbackWorker {
+public:
+    // Constructor and setLastBuffer() on a single GL thread only.
+    ReadbackWorker(uint32_t width, uint32_t height);
+    ~ReadbackWorker();
+
+    // GL initialization (must be on one thread)
+    void initGL();
+
+    void doNextReadback(ColorBuffer* cb, void* fbImage);
+
+    // Function that copies the input buffer's first
+    // |bytes| to |callback| that takes the resulting buffer data.
+    void setLastBuffer(uint32_t index);
+
+    void getPixels(void* out, uint32_t bytes);
+private:
+    EGLContext mContext;
+    EGLSurface mSurf;
+    RenderThreadInfo* mTLS;
+    FrameBuffer* mFb;
+
+    uint32_t mReadPixelsIndexEven = 0;
+    uint32_t mReadPixelsIndexOdd = 1;
+    uint32_t mMapCopyIndex = 1;
+    std::atomic<bool> mIsCopying { false };
+
+    uint32_t mBufferSize = 0;
+    std::vector<GLuint> mBuffers = {};
+    uint32_t m_readbackCount = 0;
+};
