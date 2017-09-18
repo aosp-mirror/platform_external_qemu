f386aa6edba961223488b4947bd5e72a5eea8fe2
Huihong Luo <huisinro@google.com>
2017-09-18 14:26:03 -0700

    Add audio output engine, required by video player
    Change-Id: I418cc058f1897a9e154906eef87977ed6daf6b01

---
+++ android/android-emu/android/emulation/AudioOutputEngine.h
@@ -0,0 +1,92 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/base/Compiler.h"
+#include "android/base/Log.h"
+
+namespace android {
+namespace emulation {
+
+// the audio formats supported by QEMU 1 and 2
+enum AudioFormat {
+    AUDIO_FMT_U8,
+    AUDIO_FMT_S8,
+    AUDIO_FMT_U16,
+    AUDIO_FMT_S16,
+    AUDIO_FMT_U32,
+    AUDIO_FMT_S32
+};
+
+typedef void (*audio_callback_fn)(void *opaque, int avail);
+
+// A generic engine for audio output
+
+// AudioOutputEngine provides a generic inteface for audio output.
+// The specific vm engine must implements a subclass of AudioOutputEngine.
+//
+// Usage:
+//       At vm starts up, register this enigne by calling the AudioOutputEngine::set() static method
+//       bool qemu_android_emulation_early_setup() {
+//          ...
+//          AudioOutputEngine::set(new YouAudioOutputEngine());
+//          ...
+//       }
+//
+class AudioOutputEngine {
+protected:
+     AudioOutputEngine() = default;
+     virtual ~AudioOutputEngine() = default;
+
+public:
+     // Open the audio output, subclass should implement this method
+     // params:
+     //     name - an arbitrary name for the audio output
+     //     format - audio format, one of the AudioFormat enums, AUDIO_FMT_S16 is the most common format
+     //     freq - audio sampling rate, usually 44100 or 48000
+     //     nchannels - number of channels, 2 for stero audio
+     //     callback - audio output callback, write() call should be made inside this callback
+     //     callback_opaque - arbitrary parameter for user to pass custom data reference
+     // return:
+     //     0 if successful
+     //     < 0  if failed
+     virtual int open(const char *name, AudioFormat format, int freq, int nchannels, audio_callback_fn callback, void *callback_opaque) = 0;
+
+     // render the buffer, this function should be called from inside the audio audio_callback_fn function
+     virtual int write(void *pcm_buf, int size) = 0;
+
+     // close the audio output, subclass should implement this method
+     virtual void close() = 0;
+
+     // set the audio capture engine
+     // The set() needs to be called at the point where no races are possible
+     // for example, during the period where vm setups
+     static void set(AudioOutputEngine* engine) {
+         mInstance = engine;
+     }
+
+     static AudioOutputEngine* get() {
+         CHECK(mInstance);
+         return mInstance;
+     }
+
+private:
+    static AudioOutputEngine *mInstance;
+
+    DISALLOW_COPY_AND_ASSIGN(AudioOutputEngine);
+};
+
+}  // namespace emulation
+}  // namespace android
