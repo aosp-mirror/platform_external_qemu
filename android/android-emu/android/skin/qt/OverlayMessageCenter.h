770439368ec2463aaf096986eb4f6f42a4c96f21
Yurii Zubrytskyi <zyy@google.com>
2017-09-21 16:53:09 -0700

    [ui] Add a "message center" class for overlay messages
      OverlayMessageCenter shows string messages on top of the
    emulator main window. One can use the following function:
    
    EmulatorContainer::messageCenter()::addMessage(text, icon, timeout)
    
    Messages appear on the bottom of the emulator, and disappear
    after the specified timeout (or if there's too many messages at
    once). User can dismiss them manually as well.
    
    Change-Id: I616eafc12b3068e70116c826e46c6b56bd1f636a
    TODO: on Windows animation causes noticable flickering

---
+++ android/android-emu/android/skin/qt/OverlayMessageCenter.h
@@ -0,0 +1,56 @@
+// Copyright 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <QString>
+#include <QWidget>
+
+namespace Ui {
+
+enum class OverlayMessageIcon { None, Info, Warning, Error };
+
+class OverlayMessageCenter : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit OverlayMessageCenter(QWidget* parent = nullptr);
+
+    void adjustSize();
+
+    using Icon = OverlayMessageIcon;
+
+    static constexpr int kDefaultTimeoutMs = 0;
+    static constexpr int kInfiniteTimeout = -1;
+
+signals:
+    void addMessage(QString message,
+                    Icon icon,
+                    int timeoutMs = kDefaultTimeoutMs);
+    void resized();
+
+private slots:
+    void slot_addMessage(QString message, Icon icon, int timeoutMs);
+
+private:
+    void showEvent(QShowEvent*) override;
+    //void resizeEvent(QResizeEvent*) override;
+
+    void setOnTop(bool onTop);
+    QIcon icon(Icon type);
+    void dismissMessage(QWidget* messageWidget);
+
+    bool mOnTop = false;
+};
+
+}  // namespace Ui
+
+Q_DECLARE_METATYPE(Ui::OverlayMessageIcon);
