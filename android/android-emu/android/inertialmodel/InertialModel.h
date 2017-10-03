6e47ff34796ca31c12e798896558b74821c02b13
Tim Psiaki <tpsiaki@google.com>
2017-10-02 18:28:37 -0700

    Refactor motion sensors into InertialModel component.
    This changes pulls the tracking of master position/orientation and
    calculation of related sensor data into a self-contained and unit-testable
    inertial motion model this is in preparation for updating the sensor
    calculations to better simulate physical movement.
    
    Change-Id: I04496d0439fda76b47e155c6cedf9f7984ddb306

---
+++ android/android-emu/android/inertialmodel/InertialModel.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include <array>
+
+struct QAndroidSensorsAgent;
+class InertialModel {
+public:
+    InertialModel();
+
+    void setTargetPosition(glm::vec3 position);
+    void setTargetRotation(glm::quat rotation);
+    void setMagneticValue(float north, float east, float vertical);
+
+    glm::vec3 getPosition() const;
+    glm::vec3 getAcceleration() const;
+    glm::vec3 getMagneticVector() const;
+    glm::quat getRotation() const;
+    glm::vec3 getGyroscope() const;
+
+    void setSensorsAgent(const QAndroidSensorsAgent* agent);
+private:
+    void updateSensorValues();
+    void updateAccelerations();
+
+    void recalcRotationUpdateInterval();
+
+    glm::vec3 mLinearAcceleration;
+    glm::vec3 mPreviousPosition;
+    glm::vec3 mCurrentPosition;
+    glm::quat mPreviousRotation;
+    glm::quat mCurrentRotation;
+
+    glm::vec3 mAcceleration;
+    glm::vec3 mMagneticVector;
+    glm::vec3 mGyroscope;
+
+    const QAndroidSensorsAgent* mSensorsAgent;
+
+    uint64_t mUpdateIntervalMs = 0;
+    uint64_t mLastRotationUpdateMs = 0;
+    static const uint64_t ROTATION_UPDATE_RESET_TIME_MS = 100;
+    static const size_t ROTATION_UPDATE_TIME_WINDOW_SIZE = 8;
+    std::array<uint64_t, ROTATION_UPDATE_TIME_WINDOW_SIZE>
+        mLastUpdateIntervals = {};
+    size_t mRotationUpdateTimeWindowElt = 0;
+};
+
---
+++ android/android-emu/android/inertialmodel/InertialModel_unittest.cpp
@@ -0,0 +1,127 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/inertialmodel/InertialModel.h"
+
+#include "android/base/testing/TestSystem.h"
+#include "android/base/testing/TestTempDir.h"
+
+#include <gtest/gtest.h>
+
+#include <assert.h>
+
+using android::base::TestSystem;
+using android::base::System;
+
+TEST(InertialModel, DefaultPosition) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    mTestSystem.setUnixTime(1);
+    InertialModel inertialModel;
+    EXPECT_EQ(glm::vec3(0.f, 0.f, 0.f), inertialModel.getPosition());
+    EXPECT_EQ(glm::quat(), inertialModel.getRotation());
+}
+
+TEST(InertialModel, ConvergeToPosition) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    mTestSystem.setUnixTime(0);
+    InertialModel inertialModel;
+    glm::vec3 targetPosition(2.0f, 3.0f, 4.0f);
+    inertialModel.setTargetPosition(targetPosition);
+    // After 1 second, check that we are close to the target position.
+    mTestSystem.setUnixTime(1);
+    glm::vec3 currentPosition(inertialModel.getPosition());
+    EXPECT_NEAR(targetPosition.x, currentPosition.x, 0.01f);
+    EXPECT_NEAR(targetPosition.y, currentPosition.y, 0.01f);
+    EXPECT_NEAR(targetPosition.z, currentPosition.z, 0.01f);
+}
+
+TEST(InertialModel, AtRestGyroscope) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    mTestSystem.setUnixTime(1);
+    InertialModel inertialModel;
+    EXPECT_EQ(glm::vec3(0.0f, 0.0f, 0.0f),
+                 inertialModel.getGyroscope());
+}
+
+typedef struct GravityTestCase_ {
+    glm::vec3 target_rotation;
+    glm::vec3 expected_acceleration;
+} GravityTestCase;
+
+const GravityTestCase gravityTestCases[] = {
+    {{0.0f, 0.0f, 0.0f}, {0.0f, 9.81f, 0.0f}},
+    {{90.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -9.81f}},
+    {{-90.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 9.81f}},
+    {{0.0f, 90.0f, 0.0f}, {0.0f, 9.81f, 0.0f}},
+    {{0.0f, 0.0f, 90.0f}, {9.81f, 0.0f, 0.0f}},
+    {{0.0f, 0.0f, -90.0f}, {-9.81f, 0.0f, 0.0f}},
+    {{0.0f, 0.0f, 180.0f}, {0.0f, -9.81f, 0.0f}},
+};
+
+TEST(InertialModel, GravityAcceleration) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    for (const auto& testCase : gravityTestCases) {
+        mTestSystem.setUnixTime(1);
+        InertialModel inertialModel;
+        inertialModel.setTargetRotation(
+                glm::quat(testCase.target_rotation *
+                          (static_cast<float>(M_PI) / 180.0f)));
+        inertialModel.setTargetPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+        mTestSystem.setUnixTime(2);
+        inertialModel.setTargetPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+        EXPECT_EQ(glm::vec3(0.f, 0.f, 0.f), inertialModel.getPosition());
+        glm::vec3 acceleration(inertialModel.getAcceleration());
+        EXPECT_NEAR(testCase.expected_acceleration.x,
+                    acceleration.x, 0.01f);
+        EXPECT_NEAR(testCase.expected_acceleration.y,
+                    acceleration.y, 0.01f);
+        EXPECT_NEAR(testCase.expected_acceleration.z,
+                    acceleration.z, 0.01f);
+    }
+}
+
+TEST(InertialModel, ZeroAcceleration) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    mTestSystem.setUnixTime(1);
+    InertialModel inertialModel;
+    glm::vec3 targetPosition(2.0f, 3.0f, 4.0f);
+    // at 1 second we move the target to (2, 3, 4)
+    inertialModel.setTargetPosition(targetPosition);
+    mTestSystem.setUnixTime(2);
+    // at 2 seconds the target is still at (2, 3, 4);
+    inertialModel.setTargetPosition(targetPosition);
+    // the acceleration is expected to be close to zero at this point.
+    glm::vec3 currentPosition(inertialModel.getPosition());
+    EXPECT_NEAR(targetPosition.x, currentPosition.x, 0.01f);
+    EXPECT_NEAR(targetPosition.y, currentPosition.y, 0.01f);
+    EXPECT_NEAR(targetPosition.z, currentPosition.z, 0.01f);
+}
+
+TEST(InertialModel, ConvergeToRotation) {
+    TestSystem mTestSystem("/", System::kProgramBitness);
+    mTestSystem.setLiveUnixTime(false);
+    mTestSystem.setUnixTime(0);
+    InertialModel inertialModel;
+    glm::quat targetRotation(glm::vec3(90.f, 90.f, 90.f));
+    inertialModel.setTargetRotation(targetRotation);
+    // After 1 second, check that we are close to the target position.
+    mTestSystem.setUnixTime(1);
+    glm::quat currentRotation(inertialModel.getRotation());
+    EXPECT_NEAR(targetRotation.w, currentRotation.w, 0.01f);
+    EXPECT_NEAR(targetRotation.x, currentRotation.x, 0.01f);
+    EXPECT_NEAR(targetRotation.y, currentRotation.y, 0.01f);
+    EXPECT_NEAR(targetRotation.z, currentRotation.z, 0.01f);
+}
