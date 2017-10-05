8146f0f1daa7d6860834dd348756a91febd32f56
Miodrag Dinic <miodrag.dinic@mips.com>
2017-10-05 10:25:58 +0200

    mips: ranchu: Fix Goldfish PIC initialization and IRQ routing
    Goldfish PIC is connected to the CPU IRQ 2 line and should be acting
    as a cascaded interrupt controller, and before this change it was not
    following this kind of connection topology and a workaround was needed
    in the kernel driver to route CPU IRQs 0..7 through this device to
    overcome this discrapency. This was not the proper way of functioning
    nor it was following the virtual platform design.
    
    Additionally, the Goldfish PIC initial design for masking and
    unmasking interrupts was using IRQ number instead of proper IRQ mask
    as input values. Modifying Goldfish PIC to use IRQ mask as input
    values is bringing it closer to how common interrupt controllers work
    and permits using the IRQ generic chip API in the kernel for the
    driver, which significantly improves portability.
    
    GFPIC_REG_IRQ_PENDING register when read, will now return the 32-bit
    IRQ mask of the pending interrupts, which is how most interrupts
    controllers work. This change will actually optimize the IRQ handling,
    because the chained irq handler in the kernel can process all pending
    IRQs (if there are more than one) in one go.
    
    Goldfish PIC device implementation was cleaned up:
    
      - checkpatch.pl errors/warnings cleanup
      - removed all global variables
      - renamed all variables and functions from goldfish_int to
        goldfish_pic
      - removed fiq (fast interrupt request) dead code.
      - GOLDFISH_PIC_SAVE_VERSION set to 2
    
    Device tree was updated to include the proper layout of the Interrupt
    controller nodes and its topological connections. This required adding
    information about a CPU interrupt controller which is a parent to the
    Goldfish PIC and is supposed to handle interrupts between 0..7 using
    separate driver in the kernel.
    
    For the UHI capable kernel, the final DT layout of the interrupt
    controller nodes looks as follows:
    
        goldfish_pic@1f000000 {
            interrupts = <0x2>;
            interrupt-parent = <0x8000>;
            compatible = "google,goldfish-pic", "generic,goldfish-pic";
            phandle = <0x8001>;
            #interrupt-cells = <0x1>;
            interrupt-controller;
            reg = <0x1f000000 0x0 0x1000>;
        };
    
        cpuintc {
            compatible = "mti,cpu-interrupt-controller";
            phandle = <0x8000>;
            #interrupt-cells = <0x1>;
            interrupt-controller;
        };
    
    Since now we have two controllers defined, each device needs to
    have additional "interrupt-parent" property set to the appropriate
    controller (Goldfish PIC).
    
    Change-Id: I33f35172e95728a2b05df9fac60f0279c724d0c2
    Signed-off-by: Miodrag Dinic <miodrag.dinic@mips.com>
    Signed-off-by: Goran Ferenc <goran.ferenc@mips.com>
    Signed-off-by: Aleksandar Markovic <aleksandar.markovic@mips.com>

--- hw/intc/goldfish_pic.c
+++ hw/intc/goldfish_pic.c
@@ -12,49 +12,46 @@
 
 #include "qemu/osdep.h"
 #include "cpu.h"
-#include "exec/ram_addr.h"
-#include "migration/qemu-file.h"
-#include "sysemu/char.h"
 #include "exec/address-spaces.h"
-
-#include "migration/qemu-file.h"
+#include "exec/ram_addr.h"
+#include "hw/hw.h"
 #include "hw/intc/goldfish_pic.h"
 #include "hw/irq.h"
-#include "hw/hw.h"
 #include "hw/sysbus.h"
-
-static qemu_irq* goldfish_pic = NULL;
-static qemu_irq pic_parent_irq = NULL;
-static qemu_irq pic_parent_fiq = NULL;
+#include "migration/qemu-file.h"
+#include "migration/qemu-file.h"
+#include "sysemu/char.h"
 
 enum {
-    INTERRUPT_STATUS        = 0x00, // number of pending interrupts
-    INTERRUPT_NUMBER        = 0x04,
-    INTERRUPT_DISABLE_ALL   = 0x08,
-    INTERRUPT_DISABLE       = 0x0c,
-    INTERRUPT_ENABLE        = 0x10
+    /* Number of pending interrupts         */
+    GFPIC_REG_IRQ_STATUS        = 0x00,
+    /* Highest priority pending irq number  */
+    GFPIC_REG_IRQ_PENDING       = 0x04,
+    /* Disable all irq lines                */
+    GFPIC_REG_IRQ_DISABLE_ALL   = 0x08,
+    /* Mask specific interrupt lines        */
+    GFPIC_REG_IRQ_DISABLE       = 0x0c,
+    /* Unmask specific interrupt lines      */
+    GFPIC_REG_IRQ_ENABLE        = 0x10
 };
 
-struct goldfish_int_state {
+struct goldfish_pic_state {
     SysBusDevice parent;
-
     MemoryRegion iomem;
-    qemu_irq irq;
 
     uint32_t level;
     uint32_t pending_count;
     uint32_t irq_enabled;
-    uint32_t fiq_enabled;
     qemu_irq parent_irq;
-    qemu_irq parent_fiq;
 };
 
-#define  GOLDFISH_INT_SAVE_VERSION  1
+#define  GOLDFISH_PIC_SAVE_VERSION  2
 
 #define TYPE_GOLDFISH_PIC "goldfish_pic"
-#define GOLDFISH_PIC(obj) OBJECT_CHECK(struct goldfish_int_state, (obj), TYPE_GOLDFISH_PIC)
+#define GOLDFISH_PIC(obj) \
+OBJECT_CHECK(struct goldfish_pic_state, (obj), TYPE_GOLDFISH_PIC)
 
-static void goldfish_int_update(struct goldfish_int_state *s)
+static void goldfish_pic_update(struct goldfish_pic_state *s)
 {
     uint32_t flags;
     bool locked = false;
@@ -68,173 +65,170 @@ static void goldfish_int_update(struct goldfish_int_state *s)
     flags = (s->level & s->irq_enabled);
     qemu_set_irq(s->parent_irq, flags != 0);
 
-    flags = (s->level & s->fiq_enabled);
-    qemu_set_irq(s->parent_fiq, flags != 0);
-
     if (locked) {
         qemu_mutex_unlock_iothread();
     }
 }
 
-static int  goldfish_int_load(void* opaque, int  version_id)
+static int goldfish_pic_load(void *opaque, int  version_id)
 {
-    struct goldfish_int_state*  s = opaque;
+    struct goldfish_pic_state *s = opaque;
 
-    if (version_id != GOLDFISH_INT_SAVE_VERSION)
+    if (version_id != GOLDFISH_PIC_SAVE_VERSION) {
         return -1;
+    }
 
-    goldfish_int_update(s);
+    goldfish_pic_update(s);
     return 0;
 }
 
 static const VMStateDescription goldfish_pic_vmsd = {
     .name = "goldfish_pic",
-    .version_id = GOLDFISH_INT_SAVE_VERSION,
-    .minimum_version_id = GOLDFISH_INT_SAVE_VERSION,
-    .minimum_version_id_old = GOLDFISH_INT_SAVE_VERSION,
-    .post_load = &goldfish_int_load,
+    .version_id = GOLDFISH_PIC_SAVE_VERSION,
+    .minimum_version_id = GOLDFISH_PIC_SAVE_VERSION,
+    .minimum_version_id_old = GOLDFISH_PIC_SAVE_VERSION,
+    .post_load = &goldfish_pic_load,
     .fields = (VMStateField[]) {
-        VMSTATE_UINT32(level, struct goldfish_int_state),
-        VMSTATE_UINT32(pending_count, struct goldfish_int_state),
-        VMSTATE_UINT32(irq_enabled, struct goldfish_int_state),
-        VMSTATE_UINT32(fiq_enabled, struct goldfish_int_state),
+        VMSTATE_UINT32(level, struct goldfish_pic_state),
+        VMSTATE_UINT32(pending_count, struct goldfish_pic_state),
+        VMSTATE_UINT32(irq_enabled, struct goldfish_pic_state),
         VMSTATE_END_OF_LIST()
     }
 };
 
-static void goldfish_int_set_irq(void *opaque, int irq, int level)
+static void goldfish_pic_set_irq(void *opaque, int irq, int level)
 {
-    struct goldfish_int_state *s = (struct goldfish_int_state *)opaque;
+    struct goldfish_pic_state *s = (struct goldfish_pic_state *)opaque;
     uint32_t mask = (1U << irq);
 
-    if(level) {
-        if(!(s->level & mask)) {
-            if(s->irq_enabled & mask)
+    if (level) {
+        if (!(s->level & mask)) {
+            if (s->irq_enabled & mask) {
                 s->pending_count++;
+            }
             s->level |= mask;
         }
-    }
-    else {
-        if(s->level & mask) {
-            if(s->irq_enabled & mask)
+    } else {
+        if (s->level & mask) {
+            if (s->irq_enabled & mask) {
                 s->pending_count--;
+            }
             s->level &= ~mask;
         }
     }
-    goldfish_int_update(s);
+    goldfish_pic_update(s);
 }
 
-static uint64_t goldfish_int_read(void *opaque, hwaddr offset, unsigned size)
+static uint64_t goldfish_pic_read(void *opaque, hwaddr offset, unsigned size)
 {
-    struct goldfish_int_state *s = (struct goldfish_int_state *)opaque;
+    struct goldfish_pic_state *s = (struct goldfish_pic_state *)opaque;
 
     switch (offset) {
-    case INTERRUPT_STATUS: /* IRQ_STATUS */ {
+    case GFPIC_REG_IRQ_STATUS:
+        /* return number of pending interrupts */
         return s->pending_count;
-    }
-    case INTERRUPT_NUMBER: {
-        int i;
-        uint32_t pending = s->level & s->irq_enabled;
-        for(i = 0; i < 32; i++) {
-            if(pending & (1U << i)) {
-                return i;
-            }
-        }
-        return 0;
-    }
+
+    case GFPIC_REG_IRQ_PENDING:
+        /* return pending interrupts mask */
+        return s->level & s->irq_enabled;
+
     default:
         cpu_abort(current_cpu,
-                  "goldfish_int_read: Bad offset %" HWADDR_PRIx "\n",
+                  "goldfish_pic_read: Bad offset %" HWADDR_PRIx "\n",
                   offset);
         return 0;
     }
 }
 
-static void goldfish_int_write(void *opaque, hwaddr offset, uint64_t value, unsigned size)
+static void goldfish_pic_write(void *opaque, hwaddr offset,
+                               uint64_t value, unsigned size)
 {
-    struct goldfish_int_state *s = (struct goldfish_int_state *)opaque;
+    struct goldfish_pic_state *s = (struct goldfish_pic_state *)opaque;
 
-    uint32_t mask = (1U << value);
+    uint32_t mask = value;
 
     switch (offset) {
-        case INTERRUPT_DISABLE_ALL:
-            s->pending_count = 0;
-            s->level = 0;
-            s->irq_enabled = 0;
-            break;
+    case GFPIC_REG_IRQ_DISABLE_ALL:
+        s->pending_count = 0;
+        s->level = 0;
+        s->irq_enabled = 0;
+        break;
 
-        case INTERRUPT_DISABLE:
-            if(s->irq_enabled & mask) {
-                if(s->level & mask)
-                    s->pending_count--;
-                s->irq_enabled &= ~mask;
+    case GFPIC_REG_IRQ_DISABLE:
+        if (s->irq_enabled & mask) {
+            if (s->level & mask) {
+                s->pending_count--;
             }
-            break;
-        case INTERRUPT_ENABLE:
-            if(!(s->irq_enabled & mask)) {
-                s->irq_enabled |= mask;
-                if(s->level & mask)
-                    s->pending_count++;
+            s->irq_enabled &= ~mask;
+        }
+        break;
+
+    case GFPIC_REG_IRQ_ENABLE:
+        if (!(s->irq_enabled & mask)) {
+            s->irq_enabled |= mask;
+            if (s->level & mask) {
+                s->pending_count++;
             }
-            break;
+        }
+        break;
 
     default:
         cpu_abort(current_cpu,
-                  "goldfish_int_write: Bad offset %" HWADDR_PRIx "\n",
+                  "goldfish_pic_write: Bad offset %" HWADDR_PRIx "\n",
                   offset);
         return;
     }
-    goldfish_int_update(s);
+    goldfish_pic_update(s);
 }
 
-static const MemoryRegionOps mips_qemu_int_ops = {
-    .read = goldfish_int_read,
-    .write = goldfish_int_write,
+static const MemoryRegionOps goldfish_pic_ops = {
+    .read = goldfish_pic_read,
+    .write = goldfish_pic_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
-qemu_irq*  goldfish_interrupt_init(uint32_t base, qemu_irq parent_irq, qemu_irq parent_fiq)
+qemu_irq *goldfish_pic_init(uint32_t base, qemu_irq parent_irq)
 {
-    pic_parent_irq = parent_irq;
-    pic_parent_fiq = parent_fiq;
-    sysbus_create_simple("goldfish_pic", base, NULL);
+    struct goldfish_pic_state *s;
+    DeviceState *dev;
 
-    return goldfish_pic;
+    dev = sysbus_create_simple(TYPE_GOLDFISH_PIC, base, NULL);
+
+    s = GOLDFISH_PIC(dev);
+    s->parent_irq = parent_irq;
+
+    return qemu_allocate_irqs(goldfish_pic_set_irq, s, GFD_MAX_IRQ);
 }
 
-static void goldfish_int_realize(DeviceState *dev, Error **errp)
+static void goldfish_pic_realize(DeviceState *dev, Error **errp)
 {
     SysBusDevice *sbdev = SYS_BUS_DEVICE(dev);
-    struct goldfish_int_state *s = GOLDFISH_PIC(dev);
+    struct goldfish_pic_state *s = GOLDFISH_PIC(dev);
 
-    goldfish_pic = qemu_allocate_irqs(goldfish_int_set_irq, s, GFD_MAX_IRQ);
-    s->parent_irq = pic_parent_irq;
-    s->parent_fiq = pic_parent_fiq;
-
-    memory_region_init_io(&s->iomem, OBJECT(s), &mips_qemu_int_ops, s,
-            "goldfish_pic", 0x1000);
+    memory_region_init_io(&s->iomem, OBJECT(s), &goldfish_pic_ops, s,
+            TYPE_GOLDFISH_PIC, 0x1000);
     sysbus_init_mmio(sbdev, &s->iomem);
 }
 
-static void goldfish_int_class_init(ObjectClass *klass, void *data)
+static void goldfish_pic_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
-    dc->realize = goldfish_int_realize;
+    dc->realize = goldfish_pic_realize;
     dc->vmsd = &goldfish_pic_vmsd;
     dc->desc = "goldfish PIC";
 }
 
-static const TypeInfo goldfish_int_info = {
+static const TypeInfo goldfish_pic_info = {
     .name          = TYPE_GOLDFISH_PIC,
     .parent        = TYPE_SYS_BUS_DEVICE,
-    .instance_size = sizeof(struct goldfish_int_state),
-    .class_init    = goldfish_int_class_init,
+    .instance_size = sizeof(struct goldfish_pic_state),
+    .class_init    = goldfish_pic_class_init,
 };
 
-static void goldfish_int_register(void)
+static void goldfish_pic_register(void)
 {
-    type_register_static(&goldfish_int_info);
+    type_register_static(&goldfish_pic_info);
 }
 
-type_init(goldfish_int_register);
+type_init(goldfish_pic_register);
