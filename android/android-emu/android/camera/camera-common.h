52723dcc033cbc93bd25ec257421b7dd3553cfd0
Jeff McGlynn <jwmcglynn@google.com>
2017-10-19 13:22:42 -0700

    Allocate a I420 staging frame buffer and add it to ClientFrame
    * Create a calculate_framebuffer_size helper in camera-format-converters,
    and add alignment and bits-per-pixel information to support it.
    * Update camera-service to use that instead of its own helper.
    
    Change-Id: I8e68cd08e950159f73400abf94e41af75037c0c6

--- android/android-emu/android/camera/camera-common.h
+++ android/android-emu/android/camera/camera-common.h
@@ -122,6 +122,13 @@ typedef struct ClientFrame {
      * framebuffers_count. Note that the caller must make sure that the buffers
      * are large enough to contain the entire frame captured from the device. */
     ClientFrameBuffer* framebuffers;
+
+    /* Staging buffer, used internally by convert_frame. To avoid allocating
+     * every frame, convert_frame uses |staging_framebuffer| internally to store
+     * intermediate results. */
+    uint8_t* staging_framebuffer;
+    /* Size of the staging_framebuffer, in bytes. */
+    size_t staging_framebuffer_size;
 } ClientFrame;
 
 /* Describes frame dimensions.
