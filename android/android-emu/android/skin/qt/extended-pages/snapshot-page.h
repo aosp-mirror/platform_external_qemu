3b633c2ca57610a038282e9c7ea8ad2852ae4ffe
Jim Kaye <jameskaye@google.com>
2017-10-23 12:49:53 -0700

    [UI] Add UI for snapshot management
    Add a sub-page on the extended controls to let the user
    manage snapshot images.
    
    The page lists the snapshots available for the AVD and
    shows the screenshot of each one. The user can edit the
    name of a snapshot and add arbitrary descriptive text.
    
    The user can take new snapshots, load snapshots, and
    delete existing snapshots.
    
    Fixes: 73307190
    
    Test: Manually tested take/load/edit/delete on Windows, Mac, Linux
    
    Change-Id: I147cf045c65f69188f01ef37d73495cadd2d79f0

---
+++ android/android-emu/android/skin/qt/extended-pages/snapshot-page.h
@@ -0,0 +1,121 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#pragma once
+
+#include "ui_snapshot-page.h"
+
+#include "android/snapshot/proto/snapshot.pb.h"
+
+#include <QDateTime>
+#include <QSemaphore>
+#include <QString>
+#include <QWidget>
+#include <memory>
+
+class SnapshotPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SnapshotPage(QWidget* parent = 0);
+
+public slots:
+    void slot_snapshotSaveCompleted(int status, QString name);
+    void slot_snapshotDeleteCompleted();
+
+signals:
+    void saveCompleted(int status, QString name);
+    void deleteCompleted();
+
+private slots:
+    void on_snapshotDisplay_itemSelectionChanged();
+    void on_deleteSnapshot_clicked();
+    void on_editSnapshot_clicked();
+    void on_loadSnapshot_clicked();
+    void on_takeSnapshotButton_clicked();
+
+private:
+
+    // A class for items in the QTreeWidget. These items
+    // have a QDateTime associated with them.
+    class WidgetSnapshotItem : public QTreeWidgetItem {
+        public:
+            // Top-level item
+            WidgetSnapshotItem(QString      fileName,
+                               QString      logicalName,
+                               QDateTime    dateTime) :
+                QTreeWidgetItem((QTreeWidget*)0),
+                mFileName(fileName),
+                mDateTime(dateTime)
+            {
+                setText(COLUMN_NAME, logicalName);
+            }
+
+            // Subordinate item
+            WidgetSnapshotItem(QTreeWidgetItem* parentItem,
+                               QString          fileName,
+                               QString          logicalName,
+                               QDateTime        dateTime) :
+                QTreeWidgetItem(parentItem),
+                mFileName(fileName),
+                mDateTime(dateTime)
+            {
+                mParentName = parentItem->data(COLUMN_NAME, Qt::DisplayRole).toString();
+                setText(COLUMN_NAME, logicalName);
+            }
+
+            QString fileName() const {
+                return mFileName;
+            }
+
+            QString parentName() const {
+                return mParentName;
+            }
+
+            QDateTime dateTime() const {
+                return mDateTime;
+            }
+
+        private:
+            QString   mFileName;
+            QString   mParentName;
+            QDateTime mDateTime;
+    };
+
+    static const int COLUMN_NAME = 0;
+
+    void    showEvent(QShowEvent* ee) override;
+
+    void    populateSnapshotDisplay();
+    void    populateSnapshotDisplay_flat();
+    qint64  recursiveSize(QString base, QString fileName);
+    void    recursiveCopy(QString sourceDirPath,
+                          QString destinationDirPath);
+
+    QString formattedSize(qint64 theSize);
+    QString getDescription(QString fileName);
+
+    void    deleteSnapshot(const WidgetSnapshotItem* theItem);
+    void    editSnapshot(const WidgetSnapshotItem* theItem);
+    void    showPreviewImage(QString snapshotPath, QString snapshotName);
+    void    writeLogicalNameToProtobuf(QString fileName, QString logicalName);
+    void    writeParentToProtobuf(QString fileName, QString parentName);
+    void    writeLogicalNameAndParentToProtobuf(QString fileName, QString logicalName, QString parentName);
+
+    const WidgetSnapshotItem* getSelectedSnapshot();
+
+    emulator_snapshot::Snapshot* loadProtobuf(QString fileName);
+    void                         writeProtobuf(QString fileName, emulator_snapshot::Snapshot* protobuf);
+
+    std::unique_ptr<Ui::SnapshotPage> mUi;
+
+    QGraphicsScene mPreviewScene;     // Used to render the preview
+};
