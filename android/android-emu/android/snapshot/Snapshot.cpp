76866c26602286cd438cbb77aa3f44301779a591
Lingfeng Yang <lfy@google.com>
2017-11-17 11:46:25 -0800

    Bump emulator to 27.0.1 and snapshot version to 10
    Change-Id: Ic5c1f8afc778638fbb803f91fc1cdf4b075574d3

--- android/android-emu/android/snapshot/Snapshot.cpp
+++ android/android-emu/android/snapshot/Snapshot.cpp
@@ -251,7 +251,7 @@ struct {
          avdInfo_getEncryptionKeyImagePath},
 };
 
-static constexpr int kVersion = 9;
+static constexpr int kVersion = 10;
 
 base::StringView Snapshot::dataDir(const char* name) {
     return getSnapshotDir(name);
