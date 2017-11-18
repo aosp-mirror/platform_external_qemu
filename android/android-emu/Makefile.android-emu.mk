49bd7e876bdec09db8baa1eaf48473e826e1c90d
Jeff McGlynn <jwmcglynn@google.com>
2017-11-17 17:13:03 -0800

    Split VirtualSceneRenderer, create VirtualScene entry point
    Splitting VirtualSceneRenderer, and moving the interface to the header
    file so that the renderer can be used to host shared functionality
    in the virtual scene.
    
    Change-Id: Iac8f9dd2f25ef726d419ae4ca718490b12914516

--- android/android-emu/Makefile.android-emu.mk
+++ android/android-emu/Makefile.android-emu.mk
@@ -472,6 +472,7 @@ LOCAL_SRC_FILES := \
     android/utils/socket_drainer.cpp \
     android/utils/sockets.c \
     android/utils/looper.cpp \
+    android/virtualscene/VirtualScene.cpp \
     android/virtualscene/VirtualSceneCamera.cpp \
     android/virtualscene/VirtualSceneRenderer.cpp \
     android/virtualscene/VirtualSceneTexture.cpp \
