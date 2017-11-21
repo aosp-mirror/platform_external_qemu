a1773c08c875a00db96cd1a2153ec44d3a9045e3
Yahan Zhou <yahan@google.com>
2017-11-20 18:34:52 -0800

    Use screenshot from renderer directly
    We now have a screenshot code path which directly queries screenshots
    from renderer. Let's use it instead of the adb code path because it gets
    rid of the dependency of adb and is supposed to be faster.
    
    Change-Id: I222be60434f5f1f45294b254b1d943d8c596d3b5

--- android/android-emu/android/emulation/control/ScreenCapturer_unittest.cpp
+++ android/android-emu/android/emulation/control/ScreenCapturer_unittest.cpp
@@ -14,33 +14,20 @@
 
 #include "android/emulation/control/ScreenCapturer.h"
 
-#include "android/base/testing/TestLooper.h"
 #include "android/base/Compiler.h"
 #include "android/base/system/System.h"
 #include "android/base/testing/TestSystem.h"
-#include "android/emulation/control/TestAdbInterface.h"
-#include "android/utils/path.h"
+#include "OpenglRender/RenderChannel.h"
+#include "OpenglRender/Renderer.h"
 
 #include <gtest/gtest.h>
-
-#include <algorithm>
-#include <atomic>
-#include <memory>
 #include <string>
-#include <vector>
 
-using android::base::StringView;
 using android::base::System;
-using android::emulation::ScreenCapturer;
-using android::emulation::TestAdbInterface;
-using std::string;
-using std::vector;
+using android::emulation::captureScreenshot;
 
 class ScreenCapturerTest : public ::testing::Test {
 public:
-    // Convenience typedef.
-    using Result = ScreenCapturer::Result;
-
     ScreenCapturerTest()
         : mTestSystem(PATH_SEP "progdir",
                       System::kProgramBitness,
@@ -48,131 +35,93 @@ public:
                       PATH_SEP "appdir") {}
 
     void SetUp() override {
-        mInvalidShellCommand = false;
-        mHaveResult = false;
-        mTestSystem.setShellCommand(&ScreenCapturerTest::shellCmdHandler,
-                                    this);
-        mLooper = new android::base::TestLooper();
-        mAdb.reset(new TestAdbInterface(mLooper, "adb"));
-        mCapturer.reset(new ScreenCapturer(mAdb.get()));
         mTestSystem.getTempRoot()->makeSubDir(PATH_SEP "ScreencapOut");
+        mScreenshotPath = mTestSystem.getTempRoot()->makeSubPath(
+                PATH_SEP "ScreencapOut");
     }
-
-    void TearDown() override {
-        // Ensure that no hanging thread makes the object undead at the end of
-        // the test.
-        mCapturer.reset();
-        mAdb.reset();
-        delete mLooper;
-    }
-
-    void looperAdvance() {
-        auto now = mTestSystem.getHighResTimeUs();
-        now += 1000;
-        mLooper->runOneIterationWithDeadlineMs(now / 1000);
-        mTestSystem.setUnixTimeUs(now);
-    }
-
-    static bool shellCmdHandler(void* opaque,
-                                const vector<string>& commandLine,
-                                System::Duration timeoutMs,
-                                System::ProcessExitCode* outExitCode,
-                                System::Pid* outChildPid,
-                                const string& outputFile) {
-        return static_cast<ScreenCapturerTest*>(opaque)
-                ->shellCmdHandlerImpl(commandLine, timeoutMs,
-                                      outExitCode, outChildPid,
-                                      outputFile);
-    }
-
-    bool shellCmdHandlerImpl(const vector<string>& commandLine,
-                             System::Duration,
-                             System::ProcessExitCode* outExitCode,
-                             System::Pid*,
-                             const string&) {
-        bool isScreencap =
-            std::find(commandLine.begin(), commandLine.end(), "screencap") != commandLine.end();
-        bool isPull =
-            std::find(commandLine.begin(), commandLine.end(), "pull") != commandLine.end();
-        // Always play nice with the exit code.
-        if (outExitCode) {
-            *outExitCode = 0;
-        }
-        if (isScreencap && !isPull) {
-            return mCaptureMustSucceed;
-        } else if (isPull && !isScreencap) {
-            return mPullMustSucceed;
-        } else {
-            mInvalidShellCommand = true;
-            return false;
-        }
-    }
-
-    void resultSaver(Result result) {
-        mResult = result;
-        mHaveResult = true;
-    }
-
 protected:
-    android::base::TestSystem mTestSystem;
-    android::base::TestLooper* mLooper;
-    std::unique_ptr<TestAdbInterface> mAdb;
-    std::unique_ptr<ScreenCapturer> mCapturer;
-    bool mCaptureMustSucceed;
-    bool mPullMustSucceed;
-    bool mInvalidShellCommand;
-    // Result from the ScreenCapturer callback is saved here.
-    Result mResult = Result::kUnknownError;
-    std::atomic_bool mHaveResult;
-
     DISALLOW_COPY_AND_ASSIGN(ScreenCapturerTest);
+    android::base::TestSystem mTestSystem;
+    std::string mScreenshotPath = {};
 };
 
-TEST_F(ScreenCapturerTest, screenshotCaptureFailure) {
-    mCaptureMustSucceed = false;
-    mPullMustSucceed = false;
-    mCapturer->capture(PATH_SEP "ScreencapOut",
-                       [this](ScreenCapturer::Result result,
-                              StringView filePath) { resultSaver(result); });
-    while (!mHaveResult) {
-        looperAdvance();
+class MockRenderer : public emugl::Renderer {
+public:
+    MockRenderer(bool hasValidScreenshot)
+        : mHasValidScreenshot(hasValidScreenshot) { }
+    emugl::RenderChannelPtr createRenderChannel(
+            android::base::Stream* loadStream = nullptr) {
+        return nullptr;
     }
-    EXPECT_FALSE(mInvalidShellCommand);
-    EXPECT_EQ(Result::kCaptureFailed, mResult);
+    HardwareStrings getHardwareStrings() {
+        return {};
+    }
+    void setPostCallback(OnPostCallback onPost, void* context) {
+        return;
+    }
+    bool asyncReadbackSupported() {
+        return false;
+    }
+    ReadPixelsCallback getReadPixelsCallback() {
+        return nullptr;
+    }
+    bool showOpenGLSubwindow(FBNativeWindowType window,
+                                     int wx,
+                                     int wy,
+                                     int ww,
+                                     int wh,
+                                     int fbw,
+                                     int fbh,
+                                     float dpr,
+                                     float zRot,
+                                     bool deleteExisting) {
+        return false;
+    }
+    bool destroyOpenGLSubwindow() {
+        return false;
+    }
+    void setOpenGLDisplayRotation(float zRot) { }
+    void setOpenGLDisplayTranslation(float px, float py) { }
+    void repaintOpenGLDisplay() { }
+    void cleanupProcGLObjects(uint64_t puid) { }
+    void stop(bool wait) { }
+    void pauseAllPreSave() { }
+    void resumeAll() { }
+    void save(android::base::Stream* stream,
+            const android::snapshot::ITextureSaverPtr& textureSaver) { }
+    bool load(android::base::Stream* stream,
+            const android::snapshot::ITextureLoaderPtr& textureLoader) {
+        return false;
+    }
+    void fillGLESUsages(android_studio::EmulatorGLESUsages*) { }
+    void getScreenshot(unsigned int nChannels, unsigned int* width,
+        unsigned int* height, std::vector<unsigned char>& pixels) {
+        if (mHasValidScreenshot) {
+            *width = kWidth;
+            *height = kHeight;
+            pixels.assign(*width * *height * nChannels, 0);
+        } else {
+            *width = 0;
+            *height = 0;
+            pixels.resize(0);
+        }
+    }
+private:
+    bool mHasValidScreenshot = false;
+    static const int kWidth = 1080;
+    static const int kHeight = 1920;
+};
+
+TEST_F(ScreenCapturerTest, badRenderer) {
+    EXPECT_FALSE(captureScreenshot(nullptr, mScreenshotPath.c_str()));
 }
 
-TEST_F(ScreenCapturerTest, adbPullFailure) {
-    mCaptureMustSucceed = true;
-    mPullMustSucceed = false;
-    mCapturer->capture(PATH_SEP "ScreencapOut",
-                       [this](ScreenCapturer::Result result,
-                              StringView filePath) { resultSaver(result); });
-    while (!mHaveResult) {
-        looperAdvance();
-    }
-    EXPECT_FALSE(mInvalidShellCommand);
-    EXPECT_EQ(Result::kPullFailed, mResult);
+TEST_F(ScreenCapturerTest, rendererCaptureFailure) {
+    MockRenderer renderer(false);
+    EXPECT_FALSE(captureScreenshot(&renderer, mScreenshotPath.c_str()));
 }
 
 TEST_F(ScreenCapturerTest, success) {
-    mCaptureMustSucceed = true;
-    mPullMustSucceed = true;
-    mCapturer->capture(PATH_SEP "ScreencapOut",
-                       [this](ScreenCapturer::Result result,
-                              StringView filePath) { resultSaver(result); });
-    while (!mHaveResult) {
-        looperAdvance();
-    }
-    EXPECT_FALSE(mInvalidShellCommand);
-    EXPECT_EQ(Result::kSuccess, mResult);
-}
-
-TEST_F(ScreenCapturerTest, synchronousFailures) {
-    mCapturer->capture(PATH_SEP "ScreencapOut",
-                       [this](ScreenCapturer::Result result,
-                              StringView filePath) { resultSaver(result); });
-    mCapturer->capture(PATH_SEP "ScreencapOut",
-                       [this](ScreenCapturer::Result result,
-                              StringView filePath) { resultSaver(result); });
-    EXPECT_EQ(Result::kOperationInProgress, mResult);
+    MockRenderer renderer(true);
+    EXPECT_TRUE(captureScreenshot(&renderer, mScreenshotPath.c_str()));
 }
