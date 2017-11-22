d95d6eefb93c02078d0f4872ecdf1290e6c8937a
Lingfeng Yang <lfy@google.com>
2017-11-22 12:59:11 -0800

    [snapshot][haxm] Avoid hang on save with high RAM settings
    Set it artifically lower. Some values of it cause a hang on
    snapshot save.
    
    Change-Id: I0134d31ae7fcce87f2ca908238765209cea5a7ff

--- vl.c
+++ vl.c
@@ -3259,6 +3259,9 @@ static bool set_memory_options(uint64_t *ram_slots, ram_addr_t *maxram_size,
     }
 
     const int requested_meg = ram_size / (1024 * 1024);
+    // TODO: Fix HAXM so that snapshot save doesn't hang
+    // with values of RAM above 4093.
+#define HAX_SNAPSHOT_MAX_RAM 4093 * 1024 * 1024
 
 #ifdef CONFIG_HAX
     if (hax_enabled()) {
@@ -3269,6 +3272,9 @@ static bool set_memory_options(uint64_t *ram_slots, ram_addr_t *maxram_size,
         }
         if (ram_size > hax_max_ram) {
             ram_size = hax_max_ram;
+        }
+        if (ram_size > HAX_SNAPSHOT_MAX_RAM) {
+            ram_size = HAX_SNAPSHOT_MAX_RAM;
         }
     }
 #endif
