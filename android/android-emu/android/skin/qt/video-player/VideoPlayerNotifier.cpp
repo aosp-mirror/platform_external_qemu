0e6938e8ccebb5b38f0b5d3509da82c867dd9789
Huihong Luo <huisinro@google.com>
2017-11-27 15:02:40 -0800

    Add an ffmpeg based video player to play the recorded screen movies This is a very robust player, although simple Change-Id: I6956ba6aa5b19b9e478b211cc6f4775b6c9f7b13

---
+++ android/android-emu/android/skin/qt/video-player/VideoPlayerNotifier.cpp
@@ -0,0 +1,67 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Copyright (c) 2003 Fabrice Bellard
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "android/skin/qt/video-player/VideoPlayerNotifier.h"
+#include "android/skin/qt/video-player/VideoPlayer.h"
+#include "android/skin/qt/video-player/VideoPlayerWidget.h"
+
+namespace android {
+namespace videoplayer {
+
+// Qt related functions for the video player
+// to notifier updates to the caller and a timer for the player
+// to refresh video displays
+
+// initialize a Qt timer, must be called from Qt UI thread
+void VideoPlayerNotifier::initTimer() {
+    mTimer.setSingleShot(true);
+    QObject::connect(&mTimer, &QTimer::timeout, this,
+                     &VideoPlayerNotifier::videoRefreshTimer);
+}
+
+// start the Qt timer, must be called from Qt UI thread
+void VideoPlayerNotifier::startTimer(int delayMs) {
+    mTimer.start(delayMs);
+}
+
+// stop a Qt timer, must be called from Qt UI thread
+void VideoPlayerNotifier::stopTimer() {
+    mTimer.stop();
+}
+
+void VideoPlayerNotifier::videoRefreshTimer() {
+    if (mPlayer) {
+        mPlayer->videoRefresh();
+    }
+}
+
+}  // namespace videoplayer
+}  // namespace android
