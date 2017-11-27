a82e632f2403702c08bfd41c5978ad96c89bdb4c
Tim Psiaki <tpsiaki@google.com>
2017-11-27 15:06:50 -0800

    Plumb frame time through to camera.
    Add sensor service support for calculating and saving a guest time offset and
    sending frame and sensor times using this offset when supported by the guest image.
    
    Change-Id: I6d08d5927bfb871ad0dba99656c49cdd2088d703

--- android/android-emu/android/camera/camera-capture-linux.c
+++ android/android-emu/android/camera/camera-capture-linux.c
@@ -982,7 +982,8 @@ int camera_device_read_frame(CameraDevice* ccd,
                              float r_scale,
                              float g_scale,
                              float b_scale,
-                             float exp_comp) {
+                             float exp_comp,
+                             int64_t* frame_timestamp) {
     LinuxCameraDevice* cd;
 
     /* Sanity checks. */
