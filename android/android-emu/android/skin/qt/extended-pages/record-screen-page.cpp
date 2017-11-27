b6bf6ceaa034c27cb675cc30f0a020c6af404b15
Huihong Luo <huisinro@google.com>
2017-11-27 15:02:40 -0800

    Add a video player to play the recorded screen movies
    Change-Id: I6956ba6aa5b19b9e478b211cc6f4775b6c9f7b13

--- android/android-emu/android/skin/qt/extended-pages/record-screen-page.cpp
+++ android/android-emu/android/skin/qt/extended-pages/record-screen-page.cpp
@@ -10,6 +10,8 @@
 // GNU General Public License for more details.
 
 #include "android/skin/qt/extended-pages/record-screen-page.h"
+#include "android/skin/qt/video-player/VideoPlayerWidget.h"
+#include "android/skin/qt/video-player/VideoPlayer.h"
 
 #include "android/skin/qt/extended-pages/record-screen-page-tasks.h"
 #include "android/base/files/PathUtils.h"
@@ -41,6 +43,8 @@ RecordScreenPage::RecordScreenPage(QWidget* parent)
                                    &kTmpMediaName[0]);
     QObject::connect(&mTimer, &QTimer::timeout, this,
                      &RecordScreenPage::updateElapsedTime);
+
+    mUi->rec_playerOverlayWidget->hide();
 }
 
 RecordScreenPage::~RecordScreenPage() {}
@@ -52,9 +56,11 @@ void RecordScreenPage::setRecordScreenAgent(
 
 void RecordScreenPage::setRecordState(RecordState newState) {
     mState = newState;
+    mUi->rec_playerOverlayWidget->hide();
     switch (mState) {
         case RecordState::Ready:
             mUi->rec_recordOverlayWidget->show();
+            mUi->rec_playerOverlayWidget->hide();
             mUi->rec_timeElapsedWidget->hide();
             mUi->rec_playStopButton->hide();
             mUi->rec_formatSwitch->hide();
@@ -98,10 +104,19 @@ void RecordScreenPage::setRecordState(RecordState newState) {
             mUi->rec_formatSwitch->setCurrentIndex(0);
             break;
         }
+        case RecordState::Playing:
+            mUi->rec_playerOverlayWidget->show();
+            // Change the icon on the play/stop button.
+            mUi->rec_playStopButton->show();
+            mUi->rec_playStopButton->setIcon(getIconForCurrentTheme("stop"));
+            mUi->rec_playStopButton->setProperty("themeIconName", "stop");
+            mUi->rec_saveButton->hide();
+            break;
         case RecordState::Stopped:
             // TODO: Need to only show this when hovering over the video
             // widget, which we don't have yet.
             mUi->rec_recordOverlayWidget->show();
+            mUi->rec_playerOverlayWidget->hide();
             mUi->rec_timeElapsedWidget->hide();
             mUi->rec_playStopButton->show();
             mUi->rec_formatSwitch->show();
@@ -117,6 +132,8 @@ void RecordScreenPage::setRecordState(RecordState newState) {
             mUi->rec_playStopButton->setEnabled(true);
             mUi->rec_formatSwitch->setEnabled(true);
             mUi->rec_saveButton->setEnabled(true);
+            mUi->rec_playStopButton->setIcon(getIconForCurrentTheme("play_arrow"));
+            mUi->rec_playStopButton->setProperty("themeIconName", "play_arrow");
             break;
         case RecordState::Converting:
         {
@@ -147,8 +164,26 @@ void RecordScreenPage::updateElapsedTime() {
 }
 
 void RecordScreenPage::on_rec_playStopButton_clicked() {
-    QString url = QString("file://%1").arg(mTmpFilePath.c_str());
-    QDesktopServices::openUrl(QUrl(url));
+    if (mState == RecordState::Stopped) {
+        mVideoWidget.reset(new android::videoplayer::VideoPlayerWidget(this));
+        mVideoWidget->setVisible(false);
+        mUi->rec_playerOverlayLayout->addWidget(mVideoWidget.get());
+
+        mVideoPlayer.reset(new android::videoplayer::VideoPlayer(mVideoWidget.get(), mTmpFilePath));
+        connect(mVideoPlayer.get(), SIGNAL(updateWidget()), this, SLOT(updateVideoView()));
+        connect(mVideoPlayer.get(), SIGNAL(videoFinished()), this, SLOT(videoPlayingFinished()));
+
+        mVideoWidget->setVisible(true);
+        mVideoPlayer->scheduleRefresh(20);
+        mVideoPlayer->start();
+        setRecordState(RecordState::Playing);
+    } else if (mState == RecordState::Playing) {
+        mVideoPlayer->stop();
+        mUi->rec_playStopButton->setIcon(getIconForCurrentTheme("play_arrow"));
+        mUi->rec_playStopButton->setProperty("themeIconName", "play_arrow");
+        // stop call will cause videoPlayingFinished() method to be called
+        // where we update the button state
+    }
 }
 
 void RecordScreenPage::on_rec_recordButton_clicked() {
@@ -315,4 +350,12 @@ void ConvertingTask::run() {
                                             mEndFilename.c_str(),
                                             64 * 1024);
     emit(finished(!rc));
+}
+
+void RecordScreenPage::updateVideoView() {
+    mVideoWidget->repaint();
+}
+
+void RecordScreenPage::videoPlayingFinished() {
+    setRecordState(RecordState::Stopped);
 }
