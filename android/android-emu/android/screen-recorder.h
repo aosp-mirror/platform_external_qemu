755197fdf29a8d5ff4955badb6d544a66c82196f
Joshua Duong <joshuaduong@google.com>
2017-11-27 17:43:56 -0800

    [record] Add ability to configure screen recording options.
    Change-Id: I86cbd5c77183b489cbd14c9857da3325837bf55a

--- android/android-emu/android/screen-recorder.h
+++ android/android-emu/android/screen-recorder.h
@@ -18,15 +18,41 @@
 #include "android/utils/looper.h"
 
 ANDROID_BEGIN_HEADER
+// This callback will be called in the following scenarios:
+//
+//    1) The encoding has been stopped
+//    2) The encoding is finished
+//    3) An error has occurred while encoding was trying to finish.
+//
+// When screen_recorder_stop() is called, this callback will get called, with
+// success set to 0. There is some time elapsed when we want to stop recording
+// and when the encoding is actually finished, so we'll get a second call to the
+// callback once the encoding is finished, with success set to 1. If any errors
+// occur while stopping the recording, success will be set to -1.
+typedef void (*RecordingStoppedCallback)(void* opaque, int success);
+
+typedef struct RecordingInfo {
+    const char* filename;
+    int width;
+    int height;
+    int bitrate;
+    int time_limit;
+    RecordingStoppedCallback cb;
+    void* opaque;
+} RecordingInfo;
+
 // Initializes internal global structure. Call this before doing any recording
 // operations. |w| and |h| are the FrameBuffer width and height.
-extern void screen_recorder_init(bool isGuestMode, int w, int h);
+extern void screen_recorder_init(int isGuestMode, int w, int h);
 // Starts recording the screen. When stopped, the file will be saved as
-// |filename|. Returns true if recorder started recording, false if it failed.
-extern bool screen_recorder_start(const char* filename);
-// Stop recording. After calling this function, the file will be complete and at
-// |filename|.
-extern void screen_recorder_stop(void);
+// |info->filename|. Returns true if recorder started recording, false if it failed.
+extern int screen_recorder_start(const RecordingInfo* info);
+// Stop recording. After calling this function, the encoder will stop processing
+// frames. The encoder still needs to process any remaining frames it has, so
+// calling this does not mean that the encoder has finished and |filename| is
+// ready. Attach a RecordingStoppedCallback to get an update when the encoder
+// has finished.
+extern void screen_recorder_stop_async(void);
 ANDROID_END_HEADER
 
 
