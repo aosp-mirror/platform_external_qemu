499e18f0837fb0fc0123352274e8a2b7ce057f49
Lingfeng Yang <lfy@google.com>
2017-12-12 12:01:42 -0800

    [4/5][snapshot][generic] Error handling
    bug: 70566429
    
    Now the interface functions
    
    androidSnapshot_save
    androidSnapshot_load
    
    (meant for generic snapshots)
    
    check whether it is a good idea to save/load,
    and report the results to metrics.
    
    + Refactor the snapshot protobuf's filling into
      a common function, fillSnapshotMetrics
    
    Change-Id: Ia4e3fb247a6799eec5c768c602ea5a9e427acd5d

--- android/android-emu/android/snapshot/interface.cpp
+++ android/android-emu/android/snapshot/interface.cpp
@@ -28,31 +28,7 @@ AndroidSnapshotStatus androidSnapshot_prepareForLoading(const char* name) {
 }
 
 AndroidSnapshotStatus androidSnapshot_load(const char* name) {
-    auto res = Snapshotter::get().load(false /* is not Quickboot */, name);
-    if (res != OperationStatus::Ok) {
-        // Check if the error is about something done as just a check or
-        // we've started actually loading the VM data
-        if (auto failureReason =
-                    Snapshotter::get().loader().snapshot().failureReason()) {
-            if (*failureReason != FailureReason::Empty &&
-                *failureReason < FailureReason::ValidationErrorLimit) {
-                dwarning(
-                        "Snapshot '%s' can not be loaded (%d), state is "
-                        "unchanged.",
-                        name, int(*failureReason));
-                return SNAPSHOT_STATUS_ERROR_NOT_CHANGED;
-            } else {
-                derror("Snapshot '%s' can not be loaded (%d), emulator "
-                       "stopped.",
-                       name, int(*failureReason));
-            }
-        } else {
-            derror("Snapshot '%s' can not be loaded (reason not set), emulator "
-                   "stopped.",
-                   name);
-        }
-    }
-    return AndroidSnapshotStatus(res);
+    return AndroidSnapshotStatus(Snapshotter::get().loadGeneric(name));
 }
 
 const char* androidSnapshot_loadedSnapshotFile() {
@@ -64,7 +40,7 @@ AndroidSnapshotStatus androidSnapshot_prepareForSaving(const char* name) {
 }
 
 AndroidSnapshotStatus androidSnapshot_save(const char* name) {
-    return AndroidSnapshotStatus(Snapshotter::get().save(name));
+    return AndroidSnapshotStatus(Snapshotter::get().saveGeneric(name));
 }
 
 void androidSnapshot_delete(const char* name) {
