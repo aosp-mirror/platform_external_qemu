e893d31b6205cd5b94831d99da621eddee9c3513
Tim Psiaki <tpsiaki@google.com>
2017-12-21 12:00:15 -0800

    Don't let emulator change orientation while WASD/Mouse engaged.
    The virtual sensors page has a slot that gets called when WASD is engaged and
    disables orientation checks during this time, and until sensors stabilize after
    WASD control is finished.  This delay is necessary because it takes a non-zero amount of time for the phone to stop when Alt is released and we don't want to
    flip the orientation when it is decelerating to a stop.
    
    Change-Id: I1f9a6fc8a0e3f7c170a930d996e1404db0af47ed

--- android/android-emu/android/skin/qt/virtualscene-control-window.h
+++ android/android-emu/android/skin/qt/virtualscene-control-window.h
@@ -57,6 +57,9 @@ public:
 
     void setActive(bool active);
     bool isActive();
+    
+signals:
+    void virtualSceneControlsEngaged(bool engaged);
 
 private slots:
     void slot_mousePoller();
