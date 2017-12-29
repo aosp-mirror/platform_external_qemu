a940ddc5d7b6377d84010f29cef257680114cf4b
Lingfeng Yang <lfy@google.com>
2017-12-28 17:46:46 -0800

    slap a timeout on ipv6 "loopback" connections
    bug: 70986667
    
    Change-Id: I834edc78f1c1df1a83e79d21ed5b723041d8123c

--- android/android-emu/android/base/sockets/SocketUtils.cpp
+++ android/android-emu/android/base/sockets/SocketUtils.cpp
@@ -16,6 +16,8 @@
 #include "android/base/files/Fd.h"
 #include "android/base/sockets/ScopedSocket.h"
 #include "android/base/sockets/SocketErrors.h"
+#include "android/base/system/System.h"
+#include "android/utils/system.h"
 
 #ifdef _WIN32
 #include "android/base/sockets/Winsock.h"
@@ -554,6 +556,7 @@ static int socketCreateTcpFor(int domain) {
     socketSetOption(s, SOL_SOCKET, SO_NOSIGPIPE, 1);
 #endif
     fdSetCloexec(s);
+
     return s;
 }
 
@@ -597,11 +600,70 @@ static int socketTcpLoopbackClientFor(int port, int domain) {
     SockAddressStorage addr;
     addr.initLoopbackFor(port, domain);
 
-    if (::connect(s.get(), &addr.generic, addr.size()) < 0) {
-        return -1;
-    }
+    // Get all our select()-related friends together---
+    // |connres| the result of connect(),
+    // a timeval to hold the timeout |tv|,
+    // and a fd_set |my_set| to specify our socket fd.
+    int connres;
+    struct timeval tv;
+    fd_set my_set;
 
-    return s.release();
+    memset(&tv, 0, sizeof(timeval));
+    FD_ZERO(&my_set);
+
+    // Allow an entire 100ms to connect to "loopback" address :thinkingface:
+    tv.tv_usec = 1000 * 100;
+    int fd = s.get();
+    FD_SET(fd, &my_set);
+
+    // The initial connection needs to be nonblocking since simple configs like
+    // firewalls can make connect() hang. The initial connect() is in a VCPU
+    // thread and we don't really want to hang the entire emulator for usecases
+    // like connecting to adb, so make this socket non-blocking at first.
+    socketSetNonBlocking(fd);
+
+    BEGIN_NOSIGALRM
+    connres = ::connect(s.get(), &addr.generic, addr.size());
+    END_NOSIGALRM
+
+    if (connres < 0) {
+        int selectRes = HANDLE_EINTR(::select(fd + 1, 0, &my_set, 0, &tv));
+        // EINPROGRESS macro is screwed up, just define it ourselves here
+        int inprogress = 115;
+        if (selectRes > 0 && errno != inprogress) {
+            if (domain == AF_INET) {
+                fprintf(stderr, "%s: ipv4 loopback connection success. res %d errno %d\n", __func__, selectRes, errno);
+            } else {
+                fprintf(stderr, "%s: ipv6 loopback connection success, res %d errno %d\n", __func__, selectRes, errno);
+            }
+            socketSetBlocking(fd);
+            return s.release();
+        } else {
+            fprintf(stderr, "%s: inprogress %d\n", __func__, inprogress);
+            if (errno == inprogress) {
+                if (domain == AF_INET) {
+                    fprintf(stderr, "%s: ipv4 loopback connection already in progress, bail %d %d\n", __func__, selectRes, errno);
+                } else {
+                    fprintf(stderr, "%s: ipv6 loopback connection already in progress, bail %d %d\n", __func__, selectRes, errno);
+                }
+            } else {
+                if (domain == AF_INET) {
+                    fprintf(stderr, "%s: ipv4 loopback connection err %d %d\n", __func__, selectRes, errno);
+                } else {
+                    fprintf(stderr, "%s: ipv6 loopback connection err %d %d\n", __func__, selectRes, errno);
+                }
+            }
+            return -1;
+        }
+    } else {
+                if (domain == AF_INET) {
+                    fprintf(stderr, "%s: ipv4 loopback connection success (sync path)\n", __func__);
+                } else {
+                    fprintf(stderr, "%s: ipv6 loopback connection success (sync path)\n", __func__);
+                }
+            socketSetBlocking(fd);
+        return s.release();
+    }
 }
 
 int socketTcp4LoopbackClient(int port) {
