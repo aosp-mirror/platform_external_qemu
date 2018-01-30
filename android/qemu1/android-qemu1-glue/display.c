40e1980584568bc60f2f7b7429ef3fe0cb7b3ae0
Joshua Duong <joshuaduong@google.com>
2018-01-29 16:00:44 -0800

    [record] Add support for no-window recording in gpu guest mode.
    BUG: b/68774265
    Change-Id: Ib8568c3fe35f768ef2d4138bf664ecfdbf6c970d

--- android/qemu1/android-qemu1-glue/display.c
+++ android/qemu1/android-qemu1-glue/display.c
@@ -91,6 +91,12 @@ android_display_refresh(DisplayState *ds)
     }
 }
 
+void android_display_init_no_window(QFrameBuffer* qf) {
+    // Only need to attach the check and invalidate callbacks in no-window mode
+    // to request for a refresh of the framebuffer.
+    qframebuffer_set_producer(qf, NULL, android_display_producer_check,
+                              android_display_producer_invalidate, NULL);
+}
 
 void android_display_init(DisplayState* ds, QFrameBuffer* qf)
 {
