d64509ba30238c052f0a6ab932e36726acfac7df
Erwin Jansen <jansene@google.com>
2018-02-07 19:38:50 -0800

    Use libc++ instead of libstdc++ on linux.
    - We now use libc++ with clang vs. libstdc++ with gcc4.8
    - Fixes compiler issues
    - Fixes build scripts to use libc.so, set rpath in executables
    - Removes libstdc++.so copying
    - Deprecrates -use-system-libs
    - Fix reporting bug in rebuild.sh
    - Remove 32 bit support on windows, cross compilers currently expect
      same bitness on host as as target. Clang only produces 64 bit,
      breaking the 32 bit build.
    
    Test: Unit tests green, can launch
    Test: Runs on Xenial
    Bug: b/37075150
    Change-Id: Id37a98c36d7e9c0d26d737286e3a2f9e46269cf6

--- android/scripts/build-protobuf.sh
+++ android/scripts/build-protobuf.sh
@@ -85,6 +85,15 @@ for SYSTEM in $LOCAL_HOST_SYSTEMS; do
             chmod a+x configure install-sh
         ) || panic "Could not set 'configure' and 'install-sh' as executables"
 
+        case $SYSTEM in
+            linux*)
+                # The build script will not allows us to use the $ sign without
+                # so we will fix-up the rpath later
+                export CXXFLAGS='-Wl,-rpath=TO_BE_REPLACED/../lib'
+                ;;
+            *) ;;
+        esac
+
         builder_build_autotools_package protobuf \
                 --disable-shared \
                 --without-zlib \
@@ -93,6 +102,15 @@ for SYSTEM in $LOCAL_HOST_SYSTEMS; do
         copy_directory \
                 "$(builder_install_prefix)" \
                 "$INSTALL_DIR/$SYSTEM"
+
+        case $SYSTEM in
+            linux*)
+                # We need to fixup the rpath to make sure we can find libc++.so
+                cp "$(aosp_clang_libcplusplus)" "$INSTALL_DIR/$SYSTEM/lib"
+                chrpath  -r \$ORIGIN/../lib  "$INSTALL_DIR/$SYSTEM/bin/protoc"
+                ;;
+            *) ;;
+        esac
 
     ) || panic "[$SYSTEM] Could not build protobuf!"
 
