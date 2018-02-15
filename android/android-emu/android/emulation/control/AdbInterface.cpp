16224eb6ebf1af7ea6fc177d9fdf3d83d2efe6a8
Erwin Jansen <jansene@google.com>
2018-02-14 16:49:39 -0800

    Select adb executable based upon protocol version if available
    When ADB is launched it first makes sure that the server it talks to has the same
    protocol version as the client. If not the existing server will be
    terminated and a new one will be started.
    
    Since the emulator launches adb it is possible that an existing server
    gets terminated due to a protocol version mismatch. This can cause
    "fights" where the emulator tries to launch adb on a regular basis and
    another process does the same (with a different protocol version). This
    causes frequent ADB restarts.
    
    We now try to detect if an adb server is running, and if so we will
    try to find an executable that has a matching protocol version.
    
    Test: Launched under various ADB configurations.
    Bug: b/72288706
    Change-Id: Ibb8514c8d055263f709ba62a5007473bef064ea6

--- android/android-emu/android/emulation/control/AdbInterface.cpp
+++ android/android-emu/android/emulation/control/AdbInterface.cpp
@@ -14,19 +14,28 @@
 
 #include "android/emulation/control/AdbInterface.h"
 
+#include "android/base/ArraySize.h"
 #include "android/base/StringView.h"
 #include "android/base/Uuid.h"
 #include "android/base/files/PathUtils.h"
+#include "android/base/files/ScopedFd.h"
+#include "android/base/Log.h"
+#include "android/base/memory/ScopedPtr.h"
+#include "android/base/sockets/ScopedSocket.h"
+#include "android/base/sockets/SocketWaiter.h"
 #include "android/base/system/System.h"
+#include "android/base/misc/FileUtils.h"
 #include "android/emulation/ComponentVersion.h"
 #include "android/emulation/ConfigDirs.h"
 #include "android/utils/debug.h"
 #include "android/utils/path.h"
+#include "android/utils/sockets.h"
 
 #include <cstdio>
 #include <fstream>
 #include <string>
 #include <utility>
+
 
 using android::base::AutoLock;
 using android::base::Looper;
@@ -36,6 +45,10 @@ using android::base::RunOptions;
 using android::base::System;
 using android::base::Uuid;
 using android::base::Version;
+using android::base::ScopedPtr;
+using android::base::ScopedCPtr;
+using android::base::ScopedSocket;
+using android::base::SocketWaiter;
 
 namespace android {
 namespace emulation {
@@ -68,6 +81,8 @@ public:
                                 bool want_output = true) final;
 
 private:
+    void configureAdbPaths();
+
     Looper* mLooper;
     std::string mAutoAdbPath;
     std::string mCustomAdbPath;
@@ -77,6 +92,44 @@ private:
 
 std::unique_ptr<AdbInterface> AdbInterface::create(Looper* looper) {
     return std::unique_ptr<AdbInterface>{new AdbInterfaceImpl(looper)};
+}
+
+static const int kAdbQueryTimeoutMs = 200;
+static const int kAdbDefaultPort = 5037;
+
+// Gets the reported adb protocol version from the given executable
+// This is the last digit in the adb version string.
+static int getAdbVersion(const std::string& adb_path) {
+    const std::vector<std::string> adbVersion = { adb_path, "--version" };
+    int major, minor, protocol = 0;
+    std::string tmp_dir = System::get()->getTempDir();
+
+    // Get temporary file path
+    constexpr base::StringView temp_filename_pattern = "adbversion_XXXXXX";
+    std::string temp_file_path =
+            PathUtils::join(tmp_dir, temp_filename_pattern);
+
+    auto tmpfd = android::base::ScopedFd(mkstemp((char*)temp_file_path.data()));
+    if (!tmpfd.valid()) {
+        return protocol;
+    }
+    temp_file_path.resize(strlen(temp_file_path.c_str()));
+    auto tmpFileDeleter = base::makeCustomScopedPtr(
+            &temp_file_path,
+            [](const std::string* name) { remove(name->c_str()); });
+
+    // Execute the command to get GPU info.
+    if (!System::get()->runCommand(adbVersion,
+                                   RunOptions::WaitForCompletion |
+                                           RunOptions::TerminateOnTimeout |
+                                           RunOptions::DumpOutputToFile,
+                                   kAdbQueryTimeoutMs, nullptr, nullptr,
+                                   temp_file_path)) {
+        return 0;
+    }
+    std::string read = android::readFileIntoString(temp_file_path).valueOr({});
+    sscanf(read.c_str(),"Android Debug Bridge version %d.%d.%d", &major, &minor, &protocol);
+    return protocol;
 }
 
 // Helper function, checks if the version of adb in the given SDK is
@@ -108,8 +161,81 @@ static bool checkAdbVersion(const std::string& sdk_root_directory,
     }
 }
 
-AdbInterfaceImpl::AdbInterfaceImpl(Looper* looper)
-    : mLooper(looper) {
+// Attempts to read a response from the given socket fd.
+static std::string readResponse(int fd) {
+    ScopedPtr<SocketWaiter> waiter(SocketWaiter::create());
+    waiter->update(fd, SocketWaiter::kEventRead);
+    char buffer[512];
+    if (waiter->wait(kAdbQueryTimeoutMs)) {
+        socket_recv(fd, (void*) buffer, base::arraySize(buffer));
+        std::string msg(buffer);
+        LOG(VERBOSE) << "readResponse [" << msg << "]";
+        return msg;
+    }
+
+    return "";
+}
+
+// Best effort to figure out if an adb server is running, and if so which
+// protocol version it is using.
+static int getAdbServerVersion() {
+    std::string server_host = System::get()->envGet("ANDROID_ADB_SERVER_ADDRESS");
+    std::string server_port = System::get()->envGet("ANDROID_ADB_SERVER_PORT");
+    // Protocol = 4 bytes, indicating length of message (0x000c = 12 chars)
+    // "host:version".size() == 12
+    std::string version_info = "000chost:version";
+    int running = 0;
+    int port = kAdbDefaultPort;
+
+    // First we figure out where we think we can find ADB.
+    sscanf(server_port.c_str(), "%d", &port);
+    if (server_host.empty()) {
+        server_host = "localhost";
+    }
+
+    // Okay. let's see if we can connect to this adb server.
+    ScopedSocket fd = socket_network_client(server_host.c_str(), port, SOCKET_STREAM);
+    if (!fd.valid()) {
+        LOG(VERBOSE) << "Unable to connect to " << server_host << ":" << port;
+        return running;
+    }
+
+    // And ask it for its protocol version.
+    if (socket_send(fd.get(), version_info.c_str(), version_info.size()) != version_info.size()) {
+        return running;
+    }
+
+    // The server should respond with OKAY, after which it will send the
+    // version.
+    if (readResponse(fd.get()).compare("OKAY") == 0) {
+        std::string version_result = readResponse(fd.get());
+        // Protocol format is: 4 bytes indicating size of message in bytes.
+        // Actual message.. We are expecting for bytes with the protocol
+        // version.
+        sscanf(version_result.c_str(), "0004%04x", &running);
+    }
+
+    LOG(VERBOSE) << "Extracted version: " << running;
+
+    return running;
+}
+
+#ifdef _WIN32
+static const char kAdbExe[] = "adb.exe";
+#else
+static const char kAdbExe[] = "adb";
+#endif
+
+// We consider adb shipped with 23.1.0 or later to be modern enough
+// to not notify people to upgrade adb. 23.1.0 ships with adb protocol 32
+static const int kMinAdbProtocol = 32;
+
+// Configures the adb paths
+void AdbInterfaceImpl::configureAdbPaths() {
+    // First see if we can get the running protocol version of adb
+    int protocol = getAdbServerVersion();
+    std::vector<std::pair<std::string, std::string>> options;
+
     // First try finding ADB by the environment variable.
     auto sdk_root_by_env = android::ConfigDirs::getSdkRootDirectoryByEnv();
     if (!sdk_root_by_env.empty()) {
@@ -118,31 +244,62 @@ AdbInterfaceImpl::AdbInterfaceImpl(Looper* looper)
         // also fails, we'll warn the user about this one.
         auto adb_path = PathUtils::join(sdk_root_by_env, "platform-tools",
                                         PathUtils::toExecutableName("adb"));
-        if (checkAdbVersion(sdk_root_by_env, adb_path)) {
-            mAutoAdbPath = adb_path;
-            mAdbVersionCurrent = true;
-            return;
-        }
+        options.push_back(std::make_pair(sdk_root_by_env, adb_path));
     }
 
-    // If the first path was non-existent or a bad version, try to infer the
-    // path based on the emulator executable location.
+    // Try to infer the path based on the emulator executable location.
     auto sdk_root_by_path = android::ConfigDirs::getSdkRootDirectoryByPath();
     if (sdk_root_by_path != sdk_root_by_env && !sdk_root_by_path.empty()) {
         auto adb_path = PathUtils::join(sdk_root_by_path, "platform-tools",
                                         PathUtils::toExecutableName("adb"));
-        if (checkAdbVersion(sdk_root_by_path, adb_path)) {
-            mAutoAdbPath = adb_path;
+        options.push_back(std::make_pair(sdk_root_by_path, adb_path));
+    }
+
+    // Well, maybe there's an adb executable on the path..
+    ScopedCPtr<char> adb_exe(path_search_exec(kAdbExe));
+    if (adb_exe && System::get()->pathCanExec(adb_exe.get())) {
+        options.push_back(std::make_pair("", std::string(adb_exe.get())));
+    }
+
+    // We now have several adb choices, we prefer the one that has the same
+    // protocol version as the running one (if any). If we
+    // don't pick the same executable as the running version it will be
+    // terminated which can cause all sorts of havoc.
+    if (protocol) {
+        for (auto option : options) {
+            auto adb = option.second;
+            if (getAdbVersion(adb) == protocol) {
+                // Matching protocol versions! Nice, we definitely want to use this
+                // one as we will not be restarting adb.
+                LOG(INFO) << "Found matching protocol: " << protocol <<  " adb:" << adb;
+                mAutoAdbPath = adb;
+                mAdbVersionCurrent =  protocol >= kMinAdbProtocol;
+                return;
+            }
+        }
+    }
+
+    // Okay, we were unable to extract the protocol version from the running adb
+    // or we have no matching executable.. Let's just use our old preference
+    // ordering, we will use the first adb that has a proper version.
+    for (auto option : options) {
+        auto root = option.first;
+        mAutoAdbPath = option.second;
+
+        if (checkAdbVersion(root, mAutoAdbPath)) {
             mAdbVersionCurrent = true;
             return;
         }
     }
 
-    // TODO(zyy): check if there's an adb binary on %PATH% and use that as a
-    //  last line of defense.
+    // Oh, oh. We either have no valid adb executable, or an
+    // outdated/unverifiable one.
+    return;
+}
 
-    // If no ADB has been found at this point, an error message will warn the
-    // user and direct them to the custom adb path setting.
+AdbInterfaceImpl::AdbInterfaceImpl(Looper* looper)
+    : mLooper(looper) {
+    configureAdbPaths();
 }
 
 AdbCommandPtr AdbInterfaceImpl::runAdbCommand(
