55a79fe02fe4a39dc1993ca1b3eed1f0e5d555be
Joshua Duong <joshuaduong@google.com>
2018-02-21 15:39:39 -0800

    [record] Refactor ffmpeg-muxer to use C++ classes.
    In addition to refactoring, below is a high-level summary of other
    changes:
    
    - ffmpeg recorder code now has a start() and stop() call after you add
      av tracks. The start is to allow the recorder to control the av
      producers.
    - ffmpeg recorder is now void of any code for codec configuration and
      emulator code for producing audio. Now we can interchange the audio
      and video producers to make unit-testing much easier (see Producer.h)
      and also swap out the audio and video codecs (see CodecHelper.h). The
      codec configurations we use for the emulator, VP9 for the video and
      Vorbis for the audio, are now separate from the recording code.
    - Separated the video producing code from the screen recording code.
    - The VideoProducer code used to allocate and delete frames on each new
      post. Now we use two MessageChannels and pass around pre-allocated
      frames to avoid the time for allocation and deletion.
    - Audio and video containers now use the same struct (Frame.h).
    - The guest video producer previously used a double buffer setup. It now
      uses a triple buffer setup similar to ReadbackWorker in order to
      reduce the time the post call is blocked while producer is memcpying
      the new data. The code used to be in screen-recorder.cpp but is now in
      its own class (GuestReadbackWorker.h).
    - The GIF converter code has moved into its own class (GifConverter.h).
    
    Change-Id: I08898698c84efd2f9fa021e0bbde53a6352ee48a

---
+++ android/android-emu/android/recording/codecs/CodecHelper.h
@@ -0,0 +1,79 @@
+// Copyright 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+///
+// CodecHelper.h
+//
+// Used to help configure codecs in FfmpegRecorder. Derive from this class and
+// pass it to the recorder for any codecs you would like to use. See
+// VP9CodecHelper.h for an example.
+//
+
+#pragma once
+
+#include <cassert>
+
+extern "C" {
+#include "libavformat/avformat.h"
+}
+
+namespace android {
+namespace recording {
+
+// audio/video parameters to configure the codec. Feel free to add more if you
+// need to.
+struct CodecParams {
+    uint32_t width = 0;
+    uint32_t height = 0;
+    uint32_t bitrate = 0;
+    uint32_t sample_rate = 0;
+    uint8_t fps = 0;
+    uint8_t intra_spacing = 0;
+
+    CodecParams(CodecParams&& params) = default;
+    CodecParams& operator=(CodecParams&& params) = default;
+};
+
+template <class T>
+class CodecHelper {
+public:
+    virtual ~CodecHelper() {}
+
+    // Configures the encoder and opens the codec context. Returns true if
+    // successful, false otherwise.
+    virtual bool configAndOpenEncoder(const AVFormatContext* oc,
+                                      AVStream* stream) const = 0;
+    // Configures and initializes either the SwsContext or SwrContext
+    virtual bool initSwxContext(const AVCodecContext* c, T* swxCxt) const = 0;
+
+    // Configures and initializes the resampling context
+    // Returns the codec id.
+    AVCodecID getCodecId() const { return mCodecId; }
+    // Returns the codec.
+    const AVCodec* getCodec() const { return mCodec; }
+    // Returns the codec parameters
+    const CodecParams* getCodecParams() const { return &mParams; }
+
+protected:
+    explicit CodecHelper(AVCodecID id, CodecParams&& params)
+        : mCodecId(id), mParams(std::move(params)) {
+        mCodec = avcodec_find_encoder(mCodecId);
+        assert(mCodec);
+    }
+
+protected:
+    AVCodecID mCodecId = AV_CODEC_ID_NONE;
+    CodecParams mParams;
+    AVCodec* mCodec = nullptr;
+};
+
+}  // namespace recording
+}  // namespace android
