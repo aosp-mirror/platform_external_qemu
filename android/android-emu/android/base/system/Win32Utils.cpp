7ef3fa9130cae645ea771d0c99e7592ab782277f
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2018-02-23 11:46:15 +0200

    AndroidEmu: WHPX support
    This change allows the Windows Hypervisor Platform (WHPX)
    accelerator to be used, when available.
    
    Change-Id: I6f0cc3f389d1303ce342cb2606c7bc7c19ac4128
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- android/android-emu/android/base/system/Win32Utils.cpp
+++ android/android-emu/android/base/system/Win32Utils.cpp
@@ -84,5 +84,30 @@ std::string Win32Utils::getErrorString(DWORD error_code) {
     return result;
 }
 
+// static
+Optional<_OSVERSIONINFOEXW> Win32Utils::getWindowsVersion() {
+    HMODULE hLib;
+    OSVERSIONINFOEXW ver;
+    ver.dwOSVersionInfoSize = sizeof(ver);
+
+    typedef DWORD (WINAPI *RtlGetVersion_t) (OSVERSIONINFOEXW*);
+
+    hLib = LoadLibraryW(L"Ntdll.dll");
+    if (!hLib) {
+        return {};
+    }
+
+    auto f_RtlGetVersion =
+            (RtlGetVersion_t)GetProcAddress(hLib, "RtlGetVersion");
+    if (!f_RtlGetVersion || f_RtlGetVersion(&ver)) {
+        // RtlGetVersion returns non-zero values in case of errors.
+        ver = {};
+    }
+
+    FreeLibrary(hLib);
+    return ver;
+}
+
+
 }  // namespace base
 }  // namespace android
