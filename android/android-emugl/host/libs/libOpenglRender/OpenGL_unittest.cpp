a1c1625071f97115aaa1b6f2d05831732cb8c55c
Lingfeng Yang <lfy@google.com>
2018-03-30 08:12:27 -0700

    [gl] Make OpenGL tests run on Mac/Linux by default
    TODO: Some Windows path
    
    Tried the mingw build, seems it skips those anyway.
    
    bug: 77325935
    
    Change-Id: I9500d87763f45037e6f37d6e339c2631e39c8616

--- android/android-emugl/host/libs/libOpenglRender/OpenGL_unittest.cpp
+++ android/android-emugl/host/libs/libOpenglRender/OpenGL_unittest.cpp
@@ -13,8 +13,10 @@
 // limitations under the License.
 
 #include "android/base/Log.h"
+#include "android/base/files/PathUtils.h"
 #include "android/base/synchronization/Lock.h"
 #include "android/base/synchronization/MessageChannel.h"
+#include "android/base/system/System.h"
 #include "android/base/memory/LazyInstance.h"
 #include "android/base/threads/Thread.h"
 
@@ -28,143 +30,64 @@
 #include <GLES2/gl2.h>
 #include <GLES3/gl31.h>
 
-#define AUTO_RUN_GL_UNITTESTS 0
-
-#if AUTO_RUN_GL_UNITTESTS
-#define GLTEST(x, y) TEST(x, y)
-#else
-#define GLTEST(x, y) TEST(x, DISABLED_##y)
-#endif
-
 namespace emugl {
 
+using android::base::LazyInstance;
 using android::base::Lock;
 using android::base::AutoLock;
+using android::base::PathUtils;
+using android::base::System;
 using namespace gltest;
 
-GLTEST(OpenGL, Dispatch) {
-    const EGLDispatch* egl = LazyLoadedEGLDispatch::get();
-    const GLESv2Dispatch* gl = LazyLoadedGLESv2Dispatch::get();
-    EXPECT_TRUE(egl != nullptr);
-    EXPECT_TRUE(gl != nullptr);
-}
+class GLTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        const EGLDispatch* egl = LazyLoadedEGLDispatch::get();
+        const GLESv2Dispatch* gl = LazyLoadedGLESv2Dispatch::get();
+        EXPECT_TRUE(egl != nullptr);
+        EXPECT_TRUE(gl != nullptr);
 
-GLTEST(OpenGL, CreateContext) {
-    const EGLDispatch* egl = LazyLoadedEGLDispatch::get();
-    const GLESv2Dispatch* gl = LazyLoadedGLESv2Dispatch::get();
-    EXPECT_TRUE(egl != nullptr);
-    EXPECT_TRUE(gl != nullptr);
-
-    EGLDisplay dpy = getDisplay();
-    EGLConfig config = createConfig(dpy, 8, 8, 8, 8, 24, 8, 0);
-    EGLSurface surf = pbufferSurface(dpy, config, 512, 512);
-    EGLContext context = createContext(dpy, config, 2, 0);
-
-    destroyContext(dpy, context);
-    destroySurface(dpy, surf);
-    destroyDisplay(dpy);
-}
-
-class SnapshotContext : public GLESv2Decoder{
-public:
-    SnapshotContext() : mSnap(LazyLoadedGLESv2Dispatch::get()) {
-        mWidth = 512;
-        mHeight = 512;
-        mMajorVersion = 2;
-        mMinorVersion = 0;
-
-        this->m_snapshot = &mSnap;
-        initGL(gles2_dispatch_get_proc_func, NULL);
-        EXPECT_TRUE(this->glGenBuffers != nullptr);
-
-        mEGL = LazyLoadedEGLDispatch::get();
-        EXPECT_TRUE(mEGL != nullptr);
-        mDpy = getDisplay();
-        mConfig = createConfig(mDpy, 8, 8, 8, 8, 24, 8, 0);
-        mSurf = pbufferSurface(mDpy, mConfig, mWidth, mHeight);
-        mContext = createContext(mDpy, mConfig, mMajorVersion, mMinorVersion);
-
-        mEGL->eglMakeCurrent(mDpy, mSurf, mSurf, mContext);
+        m_display = getDisplay();
+        m_config = createConfig(m_display, 8, 8, 8, 8, 24, 8, 0);
+        m_surface = pbufferSurface(m_display, m_config, 32, 32);
+        m_context = createContext(m_display, m_config, 3, 0);
+        egl->eglMakeCurrent(m_display, m_surface, m_surface, m_context);
     }
 
-    void doSnapshot() {
-        mSnap.save();
-        mEGL->eglMakeCurrent(mDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        destroyContext(mDpy, mContext);
-        destroySurface(mDpy, mSurf);
-        mSurf = pbufferSurface(mDpy, mConfig, mWidth, mHeight);
-        mContext = createContext(mDpy, mConfig, mMajorVersion, mMinorVersion);
-        mEGL->eglMakeCurrent(mDpy, mSurf, mSurf, mContext);
-        mSnap.restore();
+    virtual void TearDown() {
+        const EGLDispatch* egl = LazyLoadedEGLDispatch::get();
+        egl->eglMakeCurrent(m_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+        destroyContext(m_display, m_context);
+        destroySurface(m_display, m_surface);
+        destroyDisplay(m_display);
     }
 
-    ~SnapshotContext() {
-        mEGL->eglMakeCurrent(mDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        destroyContext(mDpy, mContext);
-        destroySurface(mDpy, mSurf);
-        destroyDisplay(mDpy);
-    }
-
-private:
-    int mWidth;
-    int mHeight;
-    int mMajorVersion;
-    int mMinorVersion;
-    const EGLDispatch* mEGL;
-
-    GLSnapshot::GLSnapshotState mSnap;
-    EGLDisplay mDpy;
-    EGLConfig mConfig;
-    EGLSurface mSurf;
-    EGLContext mContext;
+    EGLDisplay m_display;
+    EGLConfig m_config;
+    EGLSurface m_surface;
+    EGLContext m_context;
 };
 
-GLTEST(OpenGL, Snapshot_ClearColor) {
-    SnapshotContext gl;
+class SnapshotTest : public GLTest {
+public:
+    void doSnapshot() {
+        // TODO: use Translator snapshot instead of libGLSnapshot
+        // const EGLDispatch* egl = LazyLoadedEGLDispatch::get();
+        // mSnap.save();
+        // mEGL->eglMakeCurrent(m_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+        // destroyContext(m_display, m_context);
+        // destroySurface(m_display, m_surface);
+        // m_surface = pbufferSurface(m_display, m_config, mWidth, mHeight);
+        // m_context = createContext(m_display, m_config, mMajorVersion, mMinorVersion);
+        // mEGL->eglMakeCurrent(m_display, m_surface, m_surface, m_context);
+        // mSnap.restore();
+    }
+};
 
-    gl.glClearColor(0.0, 1.0, 0.0, 1.0);
-
-    gl.doSnapshot();
-
-    GLfloat restoredClearColor[4];
-
-    gl.glGetFloatv(GL_COLOR_CLEAR_VALUE, restoredClearColor);
-
-    EXPECT_TRUE(restoredClearColor[0] == 0.0);
-    EXPECT_TRUE(restoredClearColor[1] == 1.0);
-    EXPECT_TRUE(restoredClearColor[2] == 0.0);
-    EXPECT_TRUE(restoredClearColor[3] == 1.0);
+TEST_F(GLTest, InitDestroy) {
 }
 
-GLTEST(OpenGL, Snapshot_ActiveTexture) {
-    SnapshotContext gl;
-
-    gl.glActiveTexture(GL_TEXTURE4);
-
-    gl.doSnapshot();
-
-    GLint restoredActiveTexture;
-    gl.glGetIntegerv(GL_ACTIVE_TEXTURE, &restoredActiveTexture);
-
-    EXPECT_TRUE(restoredActiveTexture == GL_TEXTURE4);
-}
-
-GLTEST(OpenGL, Snapshot_DepthTest) {
-    SnapshotContext gl;
-
-    gl.glEnable(GL_DEPTH_TEST);
-    gl.doSnapshot();
-
-    EXPECT_TRUE(gl.glIsEnabled(GL_DEPTH_TEST) == GL_TRUE);
-}
-
-GLTEST(OpenGL, Snapshot_CreateShader0) {
-    SnapshotContext gl;
-
-    GLuint shader = gl.glCreateShader_dec(&gl, GL_VERTEX_SHADER);
-    gl.doSnapshot();
-
-    EXPECT_TRUE(gl.glIsShader(shader) == GL_TRUE);
+TEST_F(SnapshotTest, InitDestroy) {
 }
 
 }  // namespace emugl
