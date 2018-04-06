8a073fe35a1482c3efdd05a38ae0c17522d1244e
Jeff McGlynn <jwmcglynn@google.com>
2018-04-05 20:22:24 -0700

    Add asynchronous texture loading in virtual scene
    Texture decoding is very slow, and takes as much as 1500ms on my machine
    for large PNGs.  Before this change, this was done on the render thread
    which runs within the camera service's qemud callback, which blocks all
    emulator UI while it is running.  So when the virtual scene was loading
    or posters were loaded, the emulator UI appeared frozen.
    
    To mitigate this, add a Renderer::loadTextureAsync API which returns
    returns a placeholder transparent texture, but then asynchronously
    performs the loading in another thread.
    
    After the load is complete, the loaded texture buffers are dispatched
    back to the render thread so that we can replace the OpenGL texture
    storage with the actual data.
    
    If the texture is used before it is finished loading, it will be
    transparent.  After the texture loads, the actual data will pop in
    place, which is a good compromise for now, and could be improved in the
    future by doing a fade-in or something along those lines.
    
    To make this work, the following related changes were made:
    * Change Renderer Mesh/Material/Texture resources so that they operate
    independently of SceneObjects, and have to be explicitly released
    instead of using the Renderer::releaseObjectResources method.
    * Change SceneObject into a virtual base class, and move .obj loading
    functionality into MeshSceneObject.  Then update the virtual destructor
    to clean up resources.  The split and virtual is not strictly required,
    but is a good practice when we’re performing resource cleanup in the
    destructor.
    * Change PosterSceneObject so that we can update the texture each frame
    when new textures are loaded instead of having to destroy and recreate
    a new PosterSceneObject.
    
    Bug: 75015959
    Change-Id: I821ee79e22bd81ff8998ed16f93f35fd25a33314

--- android/android-emu/android/virtualscene/SceneObject.cpp
+++ android/android-emu/android/virtualscene/SceneObject.cpp
@@ -19,136 +19,23 @@
 #include "android/base/files/PathUtils.h"
 #include "android/base/system/System.h"
 #include "android/utils/debug.h"
-#include "android/virtualscene/Renderer.h"
-
-#include <tiny_obj_loader.h>
-
-#include <unordered_map>
 
 #define E(...) derror(__VA_ARGS__)
 #define W(...) dwarning(__VA_ARGS__)
 #define D(...) VERBOSE_PRINT(virtualscene, __VA_ARGS__)
 #define D_ACTIVE VERBOSE_CHECK(virtualscene)
 
-using android::base::PathUtils;
-using android::base::System;
-
 namespace android {
 namespace virtualscene {
 
-SceneObject::SceneObject() = default;
-SceneObject::~SceneObject() = default;
+SceneObject::SceneObject(Renderer& renderer) : mRenderer(renderer) {}
 
-std::unique_ptr<SceneObject> SceneObject::loadFromObj(Renderer& renderer,
-                                                      const char* filename) {
-    const std::string resourcesDir =
-            PathUtils::addTrailingDirSeparator(PathUtils::join(
-                    System::get()->getLauncherDirectory(), "resources"));
-    const std::string filePath = PathUtils::join(
-            System::get()->getLauncherDirectory(), "resources", filename);
-
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
-
-    std::string err;
-    const bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err,
-                                      filePath.c_str(), resourcesDir.c_str());
-    if (!ret) {
-        E("%s: Error loading obj %s: %s", __FUNCTION__, filename,
-          err.empty() ? "<no message>" : err.c_str());
-        return nullptr;
-    } else if (!err.empty()) {
-        W("%s: Warnings loading obj %s: %s", __FUNCTION__, filename,
-          err.c_str());
+SceneObject::~SceneObject() {
+    for (Renderable& renderable : mRenderables) {
+        mRenderer.releaseMaterial(renderable.material);
+        mRenderer.releaseMesh(renderable.mesh);
+        mRenderer.releaseTexture(renderable.texture);
     }
-
-    std::unique_ptr<SceneObject> result(new SceneObject());
-
-    const size_t vertexCount = attrib.vertices.size() / 3;
-    const size_t texcoordCount = attrib.texcoords.size() / 2;
-
-    for (const tinyobj::shape_t& shape : shapes) {
-        const tinyobj::mesh_t& mesh = shape.mesh;
-
-        std::vector<VertexPositionUV> vertices;
-        std::unordered_map<VertexPositionUV, GLuint, VertexPositionUVHash>
-                existingVertexToIndex;
-        std::vector<GLuint> indices;
-        Texture texture;
-
-        bool useCheckerboardMaterial = false;
-
-        if (!mesh.material_ids.empty()) {
-            const int material_id = mesh.material_ids[0];
-            if (material_id >= 0 && material_id < materials.size()) {
-                if (strstr(materials[material_id].diffuse_texname.c_str(),
-                           "TV")) {
-                    useCheckerboardMaterial = true;
-                } else {
-                    texture = renderer.loadTexture(
-                            result.get(),
-                            materials[material_id].diffuse_texname.c_str());
-                }
-            }
-        }
-
-        for (size_t i = 0; i < mesh.indices.size(); i++) {
-            tinyobj::index_t index = mesh.indices[i];
-            VertexPositionUV vertex;
-
-            if (index.vertex_index < 0 || index.vertex_index >= vertexCount) {
-                E("%s: Error parsing %s, invalid vertex index %d, expected "
-                  "less than %d",
-                  __FUNCTION__, filename, index.vertex_index, vertexCount);
-                return nullptr;
-            }
-
-            vertex.pos = glm::vec3(attrib.vertices[3 * index.vertex_index],
-                                   attrib.vertices[3 * index.vertex_index + 1],
-                                   attrib.vertices[3 * index.vertex_index + 2]);
-
-            if (index.texcoord_index >= 0) {
-                if (index.texcoord_index >= texcoordCount) {
-                    E("%s: Error parsing %s, invalid vertex index %d, expected "
-                      "less than %d",
-                      __FUNCTION__, filename, index.texcoord_index,
-                      texcoordCount);
-                    return nullptr;
-                }
-
-                vertex.uv = glm::vec2(
-                        attrib.texcoords[2 * index.texcoord_index],
-                        attrib.texcoords[2 * index.texcoord_index + 1]);
-            }
-
-            auto existingEntry = existingVertexToIndex.find(vertex);
-            if (existingEntry != existingVertexToIndex.end()) {
-                indices.push_back(existingEntry->second);
-            } else {
-                vertices.push_back(vertex);
-
-                const GLuint index = vertices.size() - 1;
-                indices.push_back(index);
-
-                existingVertexToIndex[vertex] = index;
-            }
-        }
-
-        D("%s: Creating mesh with %d vertices, %d indices", __FUNCTION__,
-          vertices.size(), indices.size());
-
-        Renderable renderable;
-        renderable.material = useCheckerboardMaterial ?
-                renderer.createMaterialCheckerboard(result.get()) :
-                renderer.createMaterialTextured();
-        renderable.mesh = renderer.createMesh(result.get(), vertices, indices);
-        renderable.texture = texture;
-
-        result.get()->mRenderables.emplace_back(std::move(renderable));
-    }
-
-    return result;
 }
 
 void SceneObject::setTransform(const glm::mat4& transform) {
@@ -161,6 +48,10 @@ glm::mat4 SceneObject::getTransform() const {
 
 const std::vector<Renderable>& SceneObject::getRenderables() const {
     return mRenderables;
+}
+
+bool SceneObject::isVisible() const {
+    return mVisible;
 }
 
 }  // namespace virtualscene
