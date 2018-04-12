a7ad869cecbae0850efa9c82b609611be0c7a9f8
Jeff McGlynn <jwmcglynn@google.com>
2018-04-11 19:41:04 -0700

    Update the posters UI design
    Replace the temporary posters UI with a fancy new UI from the design
    team.  This new design attempts to follow material design guidelines as
    close as possible within the emulator's current UI framework.
    
    * Rename image-well to poster-image-well and move the functionality to
      resize a poster within the widget.
    * Display an overlay when a file is dragged on top of the control.
    * Display different UI when there is no image selected.
    * Add a remove button to quickly remove an image from a poster location.
    * Introduce "Material*" font sizes that match the material design spec.
    * And quite a bit more, basically the entire camera page has changed.
    
    While this change modifies editable slider widget, I verified that other
    pages that use it render it identically.
    
    Bug: 77697835
    Change-Id: I9ea6c7a9de3bb353b0b2ab521fc3656876999e6d

--- android/android-emu/android/skin/qt/editable-slider-widget.cpp
+++ android/android-emu/android/skin/qt/editable-slider-widget.cpp
@@ -23,6 +23,7 @@ EditableSliderWidget::EditableSliderWidget(QWidget *parent) :
     mEditBoxLayout.addWidget(&mLineEdit);
     mEditBoxLayout.addStretch();
     mMainLayout.setSpacing(0);
+    mMainLayout.setContentsMargins(0, 0, 0, 0);
     mMainLayout.addLayout(&mAnnotatedSliderLayout);
     mMainLayout.addSpacing(16);
     mMainLayout.addLayout(&mEditBoxLayout);
