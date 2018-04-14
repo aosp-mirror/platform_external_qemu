9fdd5df9909a182a218bb5f7eddde6a5d88a61a3
Jeff McGlynn <jwmcglynn@google.com>
2018-04-14 00:08:05 -0700

    Update poster size to be specified in meters
    Split .posters parsing out of Scene.cpp, and plumb the poster min and
    max size out of the VirtualSceneManager.  Previously, posters were
    loaded by Scene.cpp and SceneObjects were created in the Scene
    constructor.  Now, the .posters file is loaded by VirtualSceneManager
    via the parsePostersFileHelper (which has been split out into its own
    file.
    
    In addition to storing poster sizes and filenames, the
    VirtualSceneManager Settings now stores all poster locations that were
    loaded from the file.  enumeratePosters now enumerates over all poster
    locations, and not just ones with a non-default filename/size, so that
    the poster min/max size can be propagated to the UI for all posters.
    
    When the Scene is created, VirtualSceneManager calls
    Scene::createPosterLocation to instantiate each poster location.
    
    Change the UI to specify the poster size in meters instead of a scale
    value, and update EditableSliderWidget in order to specify the step count and display precision so that values between 0-2 are easily editable.
    
    Bug: 77822313
    Change-Id: Ic14b9453ea17a3da03706512af3a9625b99ffc6c
    (cherry picked from commit 36f2d9ad15388475f7b37718f7bd39c7c7f29b16)

--- android/android-emu/android/skin/qt/editable-slider-widget.cpp
+++ android/android-emu/android/skin/qt/editable-slider-widget.cpp
@@ -2,15 +2,15 @@
 
 #include <QEvent>
 #include <QStyle>
+#include <QtMath>
 
-EditableSliderWidget::EditableSliderWidget(QWidget *parent) :
-    QWidget(parent),
-    mMinValueLabel(this),
-    mMaxValueLabel(this),
-    mSlider(Qt::Horizontal, this),
-    mLineEdit(this),
-    mLineEditValidator(&mLineEdit) {
-
+EditableSliderWidget::EditableSliderWidget(QWidget* parent)
+    : QWidget(parent),
+      mMinValueLabel(this),
+      mMaxValueLabel(this),
+      mSlider(Qt::Horizontal, this),
+      mLineEdit(this),
+      mLineEditValidator(&mLineEdit) {
     // Arrange the child widgets in a layout.
     mSliderLabelsLayout.setSpacing(0);
     mSliderLabelsLayout.addWidget(&mMinValueLabel);
@@ -30,9 +30,9 @@ EditableSliderWidget::EditableSliderWidget(QWidget *parent) :
     setLayout(&mMainLayout);
 
     // Set initial values.
-    setMinimum(static_cast<int>(mSlider.minimum()/10.0));
-    setMaximum(static_cast<int>(mSlider.maximum()/10.0));
-    setValue(static_cast<int>(mSlider.value()/10.0));
+    setMinimum(static_cast<int>(mSlider.minimum() / mSteps));
+    setMaximum(static_cast<int>(mSlider.maximum() / mSteps));
+    setValue(static_cast<int>(mSlider.value() / mSteps));
 
     // Do some adjustments to child widgets.
     mSlider.setFocusPolicy(Qt::ClickFocus);
@@ -57,6 +57,17 @@ EditableSliderWidget::EditableSliderWidget(QWidget *parent) :
     connect(&mLineEdit, SIGNAL(editingFinished()), this, SLOT(lineEditValueChanged()));
 }
 
+void EditableSliderWidget::setSteps(double steps) {
+    mPrecision = qCeil(qLn(steps) / M_LN10);
+    mSteps = steps;
+
+    // The slider's value is a multiple of the steps, so changing the steps
+    // the values has effectively changed.  Reset the range and value to rescale
+    // the sliders to the new step value.
+    setRange(mMinimum, mMaximum, false);
+    setValue(mValue, false);
+}
+
 void EditableSliderWidget::setValue(double value, bool emit_signal) {
     // Clip the value to the allowed range.
     mValue =
@@ -65,13 +76,13 @@ void EditableSliderWidget::setValue(double value, bool emit_signal) {
     // Update the values displayed by the slider and the edit box.
     {
         QSignalBlocker blocker(mSlider);
-        mSlider.setValue(static_cast<int>(mValue * 10.0));
+        mSlider.setValue(static_cast<int>(mValue * mSteps));
     }
 
     if (mLineEditFocused) {
         mValueChangeIgnored = true;
     } else {
-        mLineEdit.setText(QString("%1").arg(mValue, 0, 'f', 1, '0'));
+        mLineEdit.setText(QString("%1").arg(mValue, 0, 'f', mPrecision, '0'));
     }
     if (emit_signal) {
         emit valueChanged(mValue);
@@ -87,7 +98,7 @@ void EditableSliderWidget::setMinimum(double minimum, bool emit_signal) {
     setValue(mValue,
              emit_signal);  // Force the current value into the new bounds.
     mMinValueLabel.setText(QString::number(mMinimum));
-    mSlider.setMinimum(static_cast<int>(mMinimum * 10.0));
+    mSlider.setMinimum(static_cast<int>(mMinimum * mSteps));
 }
 
 void EditableSliderWidget::setMaximum(double maximum, bool emit_signal) {
@@ -96,11 +107,11 @@ void EditableSliderWidget::setMaximum(double maximum, bool emit_signal) {
     setValue(mValue,
              emit_signal);  // Force the current value into the new bounds.
     mMaxValueLabel.setText(QString::number(mMaximum));
-    mSlider.setMaximum(static_cast<int>(mMaximum * 10.0));
+    mSlider.setMaximum(static_cast<int>(mMaximum * mSteps));
 }
 
 void EditableSliderWidget::sliderValueChanged(int new_value) {
-    setValue(static_cast<double>(new_value) / 10.0);
+    setValue(static_cast<double>(new_value) / mSteps);
 }
 
 void EditableSliderWidget::lineEditValueChanged() {
