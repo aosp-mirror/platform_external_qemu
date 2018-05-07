e6b5a03fbc2a5b15e91da555be90f46cdfdf0780
Lingfeng Yang <lfy@google.com>
2018-05-07 09:40:29 -0700

    [snapshot] Start on unit tests for RamSaver
    Just add a basic test to start with.
    
    Note that the file size on disk might be bigger than our golden.
    
    bug: 79347848
    Change-Id: I7f1eaf6254a363e7de96a1d1ff5b36208f14ff27

---
+++ android/android-emu/android/snapshot/RamSaver_unittest.cpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/snapshot/RamSaver.h"
+
+#include "android/base/testing/TestTempDir.h"
+#include "android/base/misc/FileUtils.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <vector>
+
+using android::base::TestTempDir;
+using android::snapshot::RamSaver;
+using android::snapshot::RamBlock;
+
+// Needed because Quickboot statically depends on Qt and there is a link
+// failure if the function is not defined.
+bool userSettingIsDontSaveSnapshot() {
+    return false;
+}
+
+class RamSaverTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        fprintf(stderr, "RamSaverTest::%s\n", __func__);
+        mTempDir.reset(new TestTempDir("ramsavertest"));
+    }
+
+    void TearDown() override {
+        fprintf(stderr, "RamSaverTest::%s\n", __func__);
+        mTempDir.reset();
+    }
+
+    std::unique_ptr<TestTempDir> mTempDir;
+};
+
+// Save 10 pages that are all zero with no compression.
+TEST_F(RamSaverTest, Simple) {
+    std::string ramSaverTestPath =
+        mTempDir->makeSubPath("ram.bin");
+
+    std::vector<uint8_t> testRam(10 * 4096, 0);
+
+    RamBlock block = {
+        "ramSaverTestBlock",
+        0x0,
+        testRam.data(),
+        (int64_t)testRam.size(),
+        4096,
+    };
+
+    RamSaver s(ramSaverTestPath.c_str(), RamSaver::Flags::None, nullptr, true);
+
+    s.registerBlock(block);
+
+    s.savePage(0x0, 0x0000, 0x1000);
+    s.savePage(0x0, 0x1000, 0x1000);
+    s.savePage(0x0, 0x2000, 0x1000);
+    s.savePage(0x0, 0x3000, 0x1000);
+    s.savePage(0x0, 0x4000, 0x1000);
+
+    s.join();
+
+    auto fileContents = android::readFileIntoString(ramSaverTestPath);
+
+    std::vector<uint8_t> golden = {
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x8,
+        0x0, 0x0, 0x0, 0x2,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0xa,
+        0x11, 0x72, 0x61, 0x6d,
+        0x53, 0x61, 0x76, 0x65,
+        0x72, 0x54, 0x65, 0x73,
+        0x74, 0x42, 0x6c, 0x6f,
+        0x63, 0x6b, 0x0, 0x0,
+        0x0, 0xa, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+    };
+
+    EXPECT_LE(golden.size(), fileContents->size());
+    for (size_t i = 0; i < golden.size(); i++) {
+        EXPECT_EQ(golden.at(i), fileContents->at(i));
+    }
+}
