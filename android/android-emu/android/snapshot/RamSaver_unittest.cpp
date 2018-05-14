25546aec6a201eaf777f13f2c2ab4febd9c1c90f
Lingfeng Yang <lfy@google.com>
2018-05-14 10:22:44 -0700

    [snapshot] Test for RAM save and load together
    bug: 79387848
    
    Change-Id: I2776988b2cad22dff7c17e2c012a7ebff559804e

--- android/android-emu/android/snapshot/RamSaver_unittest.cpp
+++ android/android-emu/android/snapshot/RamSaver_unittest.cpp
@@ -9,12 +9,11 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-#include "android/snapshot/RamSaver.h"
-
 #include "android/base/AlignedBuf.h"
 #include "android/base/StringView.h"
 #include "android/base/misc/FileUtils.h"
 #include "android/base/testing/TestTempDir.h"
+#include "android/snapshot/RamSnapshotTesting.h"
 
 #include <gtest/gtest.h>
 
@@ -25,64 +24,15 @@
 using android::AlignedBuf;
 using android::base::StringView;
 using android::base::TestTempDir;
-using android::snapshot::RamBlock;
-using android::snapshot::RamSaver;
 
-static constexpr int kPageSize = 4096;
-using TestRam = AlignedBuf<uint8_t, kPageSize>;
-
-// Needed because Quickboot statically depends on Qt and there is a link
-// failure if the function is not defined.
-bool userSettingIsDontSaveSnapshot() {
-    return false;
-}
+namespace android {
+namespace snapshot {
 
 class RamSaverTest : public ::testing::Test {
 protected:
     void SetUp() override { mTempDir.reset(new TestTempDir("ramsavertest")); }
 
     void TearDown() override { mTempDir.reset(); }
-
-    void saveRamSnapshot(uint8_t* buffer,
-                         size_t size,
-                         RamSaver::Flags flags,
-                         StringView filename) {
-        const RamBlock block = {
-                "ramSaverTestBlock", 0x0, buffer, (int64_t)size, kPageSize,
-        };
-
-        RamSaver s(filename.c_str(), flags, nullptr, true);
-
-        s.registerBlock(block);
-        s.savePage(0x0, 0x0000, kPageSize);
-        s.join();
-    }
-
-    TestRam randomRam(size_t numPages, float zeroPageChance) {
-        std::default_random_engine generator;
-        // Use a consistent seed value to avoid flakes
-        generator.seed(0);
-
-        // Distributions for the random patterns and zero pages
-        std::uniform_int_distribution<char> patternDistribution(0, 255);
-        std::bernoulli_distribution zeroPageDistribution(zeroPageChance);
-
-        TestRam res(numPages * kPageSize);
-
-        uint8_t* ram = res.data();
-
-        for (size_t i = 0; i < numPages; ++i) {
-            uint8_t* currentPage = ram + i * kPageSize;
-            if (zeroPageDistribution(generator)) {
-                memset(currentPage, 0x0, kPageSize);
-            } else {
-                char pattern = patternDistribution(generator);
-                memset(ram + i * kPageSize, pattern, kPageSize);
-            }
-        }
-
-        return res;
-    }
 
     std::unique_ptr<TestTempDir> mTempDir;
 };
@@ -106,12 +56,19 @@ TEST_F(RamSaverTest, Simple) {
     const int numPages = 10;
 
     // Create aligned buf
-    TestRam testRam(numPages * kPageSize);
+    TestRamBuffer testRam(numPages * kTestingPageSize);
     uint8_t* ramAligned = testRam.data();
-    memset(ramAligned, 0, numPages * kPageSize);
+    memset(ramAligned, 0, numPages * kTestingPageSize);
 
-    saveRamSnapshot(ramAligned, testRam.size(), RamSaver::Flags::None,
-                    ramSaverTestPath);
+    saveRamSingleBlock(RamSaver::Flags::None,
+                       {
+                               "ramSaverTestBlock",
+                               0x0,
+                               testRam.data(),
+                               (int64_t)testRam.size(),
+                               kTestingPageSize,
+                       },
+                       ramSaverTestPath);
 
     const std::vector<uint8_t> golden = {
             0x0,  0x0,  0x0,  0x0,  0x0,  0x0,  0x0,  0x8,  0x0,  0x0,
@@ -130,15 +87,21 @@ TEST_F(RamSaverTest, SimpleNonzero) {
     std::string ramSaverTestPath = mTempDir->makeSubPath("ram.bin");
 
     const int numPages = 1;
-    const int kPageSize = 4096;
 
     // Create aligned buf
-    TestRam testRam(numPages * kPageSize);
+    TestRamBuffer testRam(numPages * kTestingPageSize);
     uint8_t* ramAligned = testRam.data();
-    memset(ramAligned, 0xff, numPages * kPageSize);
+    memset(ramAligned, 0xff, numPages * kTestingPageSize);
 
-    saveRamSnapshot(ramAligned, testRam.size(), RamSaver::Flags::Compress,
-                    ramSaverTestPath);
+    saveRamSingleBlock(RamSaver::Flags::Compress,
+                       {
+                               "ramSaverTestBlock",
+                               0x0,
+                               testRam.data(),
+                               (int64_t)testRam.size(),
+                               kTestingPageSize,
+                       },
+                       ramSaverTestPath);
 
     const std::vector<uint8_t> golden = {
             0x0,  0x0,  0x0,  0x0,  0x0,  0x0,  0x0,  0x22, 0x1f, 0xff,
@@ -159,12 +122,23 @@ TEST_F(RamSaverTest, SimpleNonzero) {
 // Save 100 pages with compression, some zero, some nonzero.
 // Does not actually check contents; just performs saving.
 TEST_F(RamSaverTest, BasicRandom) {
-    const int numPages = 100;
-    const float zeroPageChance = 0.5;
     std::string ramSaverTestPath = mTempDir->makeSubPath("random-ram-100.bin");
 
-    auto testRam = randomRam(numPages, zeroPageChance);
+    const int numPages = 100;
+    const float zeroPageChance = 0.5;
 
-    saveRamSnapshot(testRam.data(), testRam.size(), RamSaver::Flags::Compress,
-                    ramSaverTestPath);
+    auto testRam = generateRandomRam(numPages, zeroPageChance);
+
+    saveRamSingleBlock(RamSaver::Flags::Compress,
+                       {
+                               "ramSaverTestBlock",
+                               0x0,
+                               testRam.data(),
+                               (int64_t)testRam.size(),
+                               kTestingPageSize,
+                       },
+                       ramSaverTestPath);
 }
+
+}  // namespace snapshot
+}  // namespace android
