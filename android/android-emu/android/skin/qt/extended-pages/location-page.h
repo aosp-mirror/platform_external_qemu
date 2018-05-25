23c189beed4e90944f3ea8936465809456412ccf
Joshua Duong <joshuaduong@google.com>
2018-05-25 07:28:05 -0700

    [DO NOT MERGE] Trying out QtWebEngine.
    Has double rendering issues. (fixed with qt 5.11.0)
    
    This is a simple map that just moves the marker to where ever you
    click.
    
    Configured the web sockets so now we can get data from the webpage
    to the C++ code and vice-versa.
    
    Change-Id: Ic1516aea65b4e648d2ec21bdd86fd5d3357bd4d7

--- android/android-emu/android/skin/qt/extended-pages/location-page.h
+++ android/android-emu/android/skin/qt/extended-pages/location-page.h
@@ -15,13 +15,17 @@
 #include "android/base/threads/FunctorThread.h"
 #include "android/gps/GpsFix.h"
 #include "android/metrics/PeriodicReporter.h"
+#include "android/skin/qt/websockets/websocketclientwrapper.h"
+#include "android/skin/qt/websockets/websockettransport.h"
 #include <QTimer>
 #include <QThread>
+#include <QWebChannel>
+#include <QWebEngineView>
+#include <QWebSocketServer>
 #include <QWidget>
 #include <memory>
 
 struct QAndroidLocationAgent;
-class GeoDataLoaderThread;
 class LocationPage : public QWidget
 {
     Q_OBJECT
@@ -32,120 +36,25 @@ public:
 
     static void setLocationAgent(const QAndroidLocationAgent* agent);
 
-    bool isLoadingGeoData() const { return mNowLoadingGeoData; }
-    void requestStopLoadingGeoData() { mGpsNextPopulateIndex = mGpsFixesArray.size(); }
+    Q_INVOKABLE void sendLocation(const QString& lat, const QString& lng);
 
-    static void writeDeviceLocationToSettings(double lat,
-                                              double lon,
-                                              double alt);
 signals:
-    void locationUpdateRequired(double latitude, double longitude, double altitude);
-    void populateNextGeoDataChunk();
+	// a way to send location updates to the js code
+	void locationChanged(QString lat, QString lng);
 
 private slots:
-    void on_loc_GpxKmlButton_clicked();
-    void on_loc_pathTable_cellChanged(int row, int col);
-    void on_loc_playStopButton_clicked();
-    void on_loc_modeSwitch_currentIndexChanged(int index);
-    void on_loc_sendPointButton_clicked();
-    void on_loc_longitudeInput_valueChanged(double);
-    void on_loc_latitudeInput_valueChanged(double);
-    void on_loc_altitudeInput_editingFinished();
-    void on_loc_playbackSpeed_currentIndexChanged(int index);
-
-    // Called when the thread that loads and parses
-    // geo data from file is initiated.
-    void geoDataThreadStarted();
-
-    // Called when the geo data loding thread is finished.
-    // Calls finishGeoDataLoading.
-    // Do not change QString to QString& - these arguments are passed
-    // between threads.
-    void geoDataThreadFinished(QString file_name, bool ok, QString error);
-
-    // Takes the loaded geodata and populates UI table chunk-by-chunk, allowing
-    // UI to process other events while loading.
-    void populateTableByChunks();
-
-    // Same as above, but only works for the initial invokation of the geo data
-    // loader thread (the one that occurs when the widget is first constructed)
-    void startupGeoDataThreadFinished(QString file_name, bool ok, QString error);
-
-    void updateDisplayedLocation(double lat, double lon, double alt);
-
-    void locationPlaybackStart();
-    void locationPlaybackStop();
-    void timeout();
+    void on_singlePoint_importGpxButton_clicked();
+    void on_singlePoint_setLocationButton_clicked();
 
 private:
-    void finishGeoDataLoading(
-        const QString& file_name,
-        bool ok,
-        const QString& error_message,
-        bool ignore_error);
-
-    void updateControlsAfterLoading();
-
-    void writeLocationPlaybackFilePathToSettings(const QString& file);
-    QString getLocationPlaybackFilePathFromSettings();
-
-    void writeLocationPlaybackSpeedToSettings(int speed);
-    int getLocationPlaybackSpeedFromSettings();
-
-    static void getDeviceLocationFromSettings(double* pOutLatitude,
-                                              double* pOutLongitude,
-                                              double* pOutAltitude);
-
-    static void getDeviceLocation(double* pOutLatitude,
-                                  double* pOutLongitude,
-                                  double* pOutAltitude);
-    static void sendLocationToDevice();
-    static bool validateCell(QTableWidget* table,
-                             int row,
-                             int col,
-                             QString* outErrorMessage);
-
     static const QAndroidLocationAgent* sLocationAgent;
 
+    // Last point sent to the emulator from the map
+    QString mLastLat = "-122.084";
+    QString mLastLng = "37.422";;
+
+	std::unique_ptr<QWebSocketServer> mServer;
+	std::unique_ptr<WebSocketClientWrapper> mClientWrapper;
+	std::unique_ptr<QWebChannel> mWebChannel;
     std::unique_ptr<Ui::LocationPage> mUi;
-    QDoubleValidator mAltitudeValidator;
-    GpsFixArray          mGpsFixesArray;
-    int                  mGpsNextPopulateIndex = 0;
-    GeoDataLoaderThread* mGeoDataLoader;
-    QTimer mTimer;
-    bool mNowPlaying = false;
-    bool mNowLoadingGeoData = false;
-    bool mLocationUsed = false;
-    int mRowToSend;
-    android::metrics::PeriodicReporter::TaskToken mMetricsReportingToken;
-    android::base::FunctorThread mUpdateThread;
-    android::base::ConditionVariable mUpdateThreadCv;
-    android::base::Lock mUpdateThreadLock;
-    bool mShouldCloseUpdateThread = false;
-};
-
-class GeoDataLoaderThread : public QThread {
-Q_OBJECT
-public:
-    // Loads geo data from a gpx or kml file specified
-    // by file_name into the GpsFixArray pointed to
-    // by fixes
-    void loadGeoDataFromFile(const QString& file_name, GpsFixArray* fixes);
-
-    static GeoDataLoaderThread* newInstance(
-            const QObject* handler,
-            const char* started_slot,
-            const char* finished_slot);
-
-signals:
-    void loadingFinished(QString file_name, bool ok, QString error);
-
-protected:
-    // Reimplemented to load the file into the given fixes array.
-    void run() override;
-
-private:
-    GeoDataLoaderThread() = default;
-    QString mFileName;
-    GpsFixArray* mFixes = nullptr;
 };
