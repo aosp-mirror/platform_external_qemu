237393d30a730182b20ce8ca8f5f1e7f66fb7ba5
Haitao Shan <hshan@google.com>
2018-06-08 11:01:39 -0700

    Revert "Allow emulator with WindowsHypervisorPlatform to run on AMD."
    This reverts commit bb21c0e51c4362dd17ec182594020d2b1be72a2e.

--- android/android-emu/android/emulation/CpuAccelerator.cpp
+++ android/android-emu/android/emulation/CpuAccelerator.cpp
@@ -138,7 +138,10 @@ GlobalState gGlobals = {false,  false,  CPU_ACCELERATOR_NONE,
 #include <WinHvEmulation.h>
 
 static bool isOkToTryWHPX() {
-    return featurecontrol::isEnabled(featurecontrol::WindowsHypervisorPlatform);
+    char cpu_vendor[16];
+    android_get_x86_cpuid_vendor_id(cpu_vendor, sizeof(cpu_vendor));
+    return android_get_x86_cpuid_vendor_id_type(cpu_vendor) == VENDOR_ID_INTEL &&
+           featurecontrol::isEnabled(featurecontrol::WindowsHypervisorPlatform);
 }
 
 AndroidCpuAcceleration ProbeWHPX(std::string* status) {
