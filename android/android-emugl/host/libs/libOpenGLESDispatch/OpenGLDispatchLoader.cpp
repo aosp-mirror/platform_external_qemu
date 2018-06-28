37f9cfadbd6eceacad1ff5682df3690f169359b2
Lingfeng Yang <lfy@google.com>
2018-06-28 11:23:52 -0700

    WIP: goldfish-opengl build
    Change-Id: I999ae9c46c3f47506b63a03b71fd2d6e2f2de0e9

--- android/android-emugl/host/libs/libOpenGLESDispatch/OpenGLDispatchLoader.cpp
+++ android/android-emugl/host/libs/libOpenGLESDispatch/OpenGLDispatchLoader.cpp
@@ -13,10 +13,15 @@
 // limitations under the License.
 
 #include "emugl/common/OpenGLDispatchLoader.h"
+#include "emugl/common/shared_library.h"
 
+#include "android/base/containers/Lookup.h"
 #include "android/base/memory/LazyInstance.h"
 
 #include "OpenGLESDispatch/DispatchTables.h"
+
+#include <string>
+#include <unordered_map>
 
 GLESv1Dispatch s_gles1;
 GLESv2Dispatch s_gles2;
@@ -28,6 +33,96 @@ using android::base::LazyInstance;
 LazyInstance<LazyLoadedGLESv1Dispatch> sGLESv1Dispatch = LAZY_INSTANCE_INIT;
 LazyInstance<LazyLoadedGLESv2Dispatch> sGLESv2Dispatch = LAZY_INSTANCE_INIT;
 LazyInstance<LazyLoadedEGLDispatch> sEGLDispatch = LAZY_INSTANCE_INIT;
+
+// Class that holds all dispatches we wanted to get so far.
+class DispatchStore {
+public:
+    DispatchStore() = default;
+
+    ~DispatchStore() {
+        for (auto it : mEGLDispatches) {
+            delete it.second;
+        }
+        for (auto it : mGLESv1Dispatches) {
+            delete it.second;
+        }
+        for (auto it : mGLESv2Dispatches) {
+            delete it.second;
+        }
+    }
+
+    EGLDispatch* loadEGL(const char* libPath, emugl::SharedLibrary** outLibrary) {
+
+        if (auto table = android::base::findOrDefault(mEGLDispatches,
+                                                      libPath,
+                                                      nullptr)) {
+            return table;
+        }
+
+        EGLDispatch dispatch;
+        if (init_egl_dispatch_from(libPath, &dispatch, outLibrary)) {
+            EGLDispatch* allocatedDispatch = new EGLDispatch;
+            mEGLDispatches[libPath] = allocatedDispatch;
+            mEGLLibs[libPath] = *outLibrary;
+            memcpy(allocatedDispatch, &dispatch, sizeof(EGLDispatch));
+            return allocatedDispatch;
+        } else {
+            mEGLDispatches[libPath] = nullptr;
+            return nullptr;
+        }
+    }
+
+    GLESv1Dispatch* loadGLESv1(const char* libPath, EGLDispatch* egl_dispatch, emugl::SharedLibrary** outLibrary) {
+        if (auto table = android::base::findOrDefault(mGLESv1Dispatches,
+                                                    libPath,
+                                                    nullptr)) {
+            return table;
+        }
+
+        GLESv1Dispatch dispatch;
+        if (gles1_dispatch_init_from(libPath, egl_dispatch, &dispatch, outLibrary)) {
+            GLESv1Dispatch* allocatedDispatch = new GLESv1Dispatch;
+            mGLESv1Dispatches[libPath] = allocatedDispatch;
+            mGLESv1Libs[libPath] = *outLibrary;
+            memcpy(allocatedDispatch, &dispatch, sizeof(GLESv1Dispatch));
+            return allocatedDispatch;
+        } else {
+            mGLESv1Dispatches[libPath] = nullptr;
+            return nullptr;
+        }
+    }
+
+    GLESv2Dispatch* loadGLESv2(const char* libPath, EGLDispatch* egl_dispatch, emugl::SharedLibrary** outLibrary) {
+        if (auto table = android::base::findOrDefault(mGLESv2Dispatches,
+                                                    libPath,
+                                                    nullptr)) {
+            return table;
+        }
+
+        GLESv2Dispatch dispatch;
+        if (gles2_dispatch_init_from(libPath, egl_dispatch, &dispatch, outLibrary)) {
+            GLESv2Dispatch* allocatedDispatch = new GLESv2Dispatch;
+            mGLESv2Dispatches[libPath] = allocatedDispatch;
+            mGLESv2Libs[libPath] = *outLibrary;
+            memcpy(allocatedDispatch, &dispatch, sizeof(GLESv2Dispatch));
+            return allocatedDispatch;
+        } else {
+            mGLESv2Dispatches[libPath] = nullptr;
+            return nullptr;
+        }
+    }
+
+private:
+    std::unordered_map<std::string, emugl::SharedLibrary*> mGLESv1Libs;
+    std::unordered_map<std::string, emugl::SharedLibrary*> mGLESv2Libs;
+    std::unordered_map<std::string, emugl::SharedLibrary*> mEGLLibs;
+
+    std::unordered_map<std::string, GLESv1Dispatch*> mGLESv1Dispatches;
+    std::unordered_map<std::string, GLESv2Dispatch*> mGLESv2Dispatches;
+    std::unordered_map<std::string, EGLDispatch*> mEGLDispatches;
+};
+
+static LazyInstance<DispatchStore> sDispatchStore = LAZY_INSTANCE_INIT;
 
 // static
 const GLESv1Dispatch* LazyLoadedGLESv1Dispatch::get() {
@@ -67,6 +162,37 @@ const EGLDispatch* LazyLoadedEGLDispatch::get() {
     } else {
         return nullptr;
     }
+}
+
+// static
+const DispatchTables* LazyLoadedEGLDispatch::getLibrariesFrom(
+        const char* eglPath,
+        const char* glesv1Path,
+        const char* glesv2Path) {
+
+    emugl::SharedLibrary* eglLib;
+    emugl::SharedLibrary* gles1Lib;
+    emugl::SharedLibrary* gles2Lib;
+    EGLDispatch* egl = sDispatchStore->loadEGL(eglPath, &eglLib);
+    GLESv1Dispatch* gles1 = sDispatchStore->loadGLESv1(glesv1Path, egl, &gles1Lib);
+    GLESv2Dispatch* gles2 = sDispatchStore->loadGLESv2(glesv2Path, egl, &gles2Lib);
+
+    if (!egl || !gles1 || !gles2) {
+        fprintf(stderr, "%s: failed to load EGL/GLESv1/GLESv2: EGL %p GLESv1 %p GLESv2 %p\n",
+                __func__, egl, gles1, gles2);
+        return nullptr;
+    }
+
+    DispatchTables* table = new DispatchTables;
+    memcpy(&table->egl, egl, sizeof(EGLDispatch));
+    memcpy(&table->gles1, gles1, sizeof(GLESv1Dispatch));
+    memcpy(&table->gles2, gles2, sizeof(GLESv2Dispatch));
+
+    table->eglLib = eglLib;
+    table->gles1Lib = gles1Lib;
+    table->gles2Lib = gles2Lib;
+
+    return table;
 }
 
 LazyLoadedEGLDispatch::LazyLoadedEGLDispatch() { mValid = init_egl_dispatch(); }
