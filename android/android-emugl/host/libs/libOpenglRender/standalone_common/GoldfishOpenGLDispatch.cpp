317c22b8ddf5512e3b97292a537a4100a3a79d1e
Lingfeng Yang <lfy@google.com>
2018-06-28 11:23:52 -0700

    WIP: goldfish-opengl build
    Change-Id: I999ae9c46c3f47506b63a03b71fd2d6e2f2de0e9

---
+++ android/android-emugl/host/libs/libOpenglRender/standalone_common/GoldfishOpenGLDispatch.cpp
@@ -0,0 +1,71 @@
+#include "GoldfishOpenGLDispatch.h"
+
+#include "android/base/memory/LazyInstance.h"
+#include "android/emulation/hostpipe/HostGoldfishPipe.h"
+#include "android/opengles.h"
+#include "android/opengles-pipe.h"
+
+#include "emugl/common/OpenGLDispatchLoader.h"
+#include "emugl/common/shared_library.h"
+
+#include <functional>
+
+#include <string.h>
+
+using android::base::LazyInstance;
+
+namespace emugl {
+
+class GoldfishOpenGLStore {
+public:
+    GoldfishOpenGLStore() {
+        const DispatchTables* tables =
+            LazyLoadedEGLDispatch::getLibrariesFrom(
+                "libEGL_emulation.dylib",
+                "libGLESv1_CM_emulation.dylib",
+                "libGLESv2_emulation.dylib");
+        memcpy(&mTables, tables, sizeof(DispatchTables));
+        delete tables;
+
+        emugl::SharedLibrary* eglLib = (emugl::SharedLibrary*)mTables.eglLib;
+
+        mRegisterFunc = (registerFunc_t)(eglLib->findSymbol("registerCustomGLESLibraries"));
+        
+        HostPipeDispatch dispatch = make_host_pipe_dispatch();
+
+        int gles_major_version = 0;
+        int gles_minor_version = 0;
+        android_initOpenglesEmulation();
+        android_startOpenglesRenderer(
+                256,
+                256,
+                0 /* AVD_PHONE */,
+                28,
+                &gles_major_version,
+                &gles_minor_version);
+
+        if (mRegisterFunc) {
+            android_init_opengles_pipe();
+            mRegisterFunc(mTables.gles1Lib, mTables.gles2Lib, &dispatch);
+        }
+    }
+
+    const DispatchTables* tables() const {
+        return &mTables;
+    }
+
+private:
+
+    typedef void (*registerFunc_t)(void*,void*,void*);
+
+    registerFunc_t mRegisterFunc;
+    DispatchTables mTables;
+};
+
+static LazyInstance<GoldfishOpenGLStore> sGoldfishOpenGLStore = LAZY_INSTANCE_INIT;
+
+const DispatchTables* loadGoldfishOpenGL() {
+    return sGoldfishOpenGLStore->tables();
+}
+
+} // namespace emugl
