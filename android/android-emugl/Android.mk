7a2fb643556bfc52cf703147ca873c1252b158b3
Lingfeng Yang <lfy@google.com>
2018-06-28 11:23:52 -0700

    WIP: goldfish-opengl build
    Change-Id: I999ae9c46c3f47506b63a03b71fd2d6e2f2de0e9

--- android/android-emugl/Android.mk
+++ android/android-emugl/Android.mk
@@ -94,4 +94,7 @@ include $(EMUGL_PATH)/host/libs/libOpenglRender/Android.mk
 include $(EMUGL_PATH)/host/libs/libGLES12Translator/Android.mk
 include $(EMUGL_PATH)/host/libs/libGLSnapshot/Android.mk
 
+# Guest encoder (if applicable)
+include $(EMUGL_PATH)/guest/Android.mk
+
 LOCAL_PATH := $(EMUGL_OLD_LOCAL_PATH)
