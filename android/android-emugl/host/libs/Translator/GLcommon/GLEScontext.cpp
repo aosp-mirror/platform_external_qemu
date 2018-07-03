43bfedfbf0844ad1de095a39baf099409bc468c7
Jeff McGlynn <jwmcglynn@google.com>
2018-07-03 14:50:34 -0700

    Fix Sceneform dynamic resolution scaling on the emulator
    Sceneform's rendering pipeline includes a post-process step, which ends
    with a glBlitFramebuffer call to scale a scaled viewport back to the
    full framebuffer resolution.
    
    After doing this call, GL_READ_FRAMEBUFFER is set to an intermediate
    framebuffer, and GL_DRAW_FRAMEBUFFER is 0, at which point the swap
    occurs and the buffers are presented to the screen.
    
    On device, it appears that if a non-zero framebuffer is bound at the
    time of swap, framebuffer 0 is still presented to the screen.  On
    emulator, however, the current read framebuffer is presented instead.
    This results in the rendered view, with a size determined by the scaled
    viewport, rendered at the bottom left of the screen instead of it being
    scaled back up to fit the screen.
    
    Change the emulator so that it always swaps framebuffer 0 instead of the
    currently bound read framebuffer.
    
    Bug: 110996998
    Change-Id: Iedd50d2bb8a9788d20b85ba851837d0e483f9747

--- android/android-emugl/host/libs/Translator/GLcommon/GLEScontext.cpp
+++ android/android-emugl/host/libs/Translator/GLcommon/GLEScontext.cpp
@@ -2603,6 +2603,8 @@ bool GLEScontext::setupImageBlitForTexture(uint32_t width,
         gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     }
 
+    gl.glBindFramebuffer(GL_READ_FRAMEBUFFER, m_defaultReadFBO);
+
     if (m_blitState.samples > 0) {
         GLint rWidth = width;
         GLint rHeight = height;
