6c07911422c3e58abff7ed54c9c8f7f2155f774f
Ben Zhang <benzene@google.com>
2018-07-11 15:52:35 -0700

    [test] GL snapshot: Renderbuffer object tests
    Adds tests for snapshot's preservation of Renderbuffer object states.
    
    Modifies RenderbufferData to better respect how drivers treat the
    default storage format component sizes:
    
    - Changes RenderbufferData's default internalformat to RGBA4, which
      respects the defaults described in the GLES2 and GLES3 specs.
    
    - Changes RenderbufferData's hostInternalFormat default to GL_NONE,
      which indicates whether the renderbuffer object's storage has been
      initialized. If it hasn't, we can skip glRenderbufferStorage in when
      restoring from a snapshot.
    
    Bug: 110379559
    Test: builds, runs on Linux
    Change-Id: Icd7cf674385cd7bdc2882db9e4d457d863d1ef6d

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -2227,7 +2227,15 @@ GL_APICALL void  GL_APIENTRY glGetRenderbufferParameteriv(GLenum target, GLenum 
     }
 
     ctx->dispatcher().glGetRenderbufferParameteriv(target,pname,params);
+    fprintf(stderr,
+            "%s %s %d called dispatcher get 0x%x, returned %d (0x%x) \n",
+            __FILE__, __func__, __LINE__, pname, params[0], params[0]);
+
+    // An uninitialized renderbuffer storage may give a format of GL_RGBA on
+    // some drivers; GLES2 default is GL_RGBA4.
     if (pname == GL_RENDERBUFFER_INTERNAL_FORMAT && *params == GL_RGBA) {
+        fprintf(stderr, "%s %s %d inside, setting GL_RGBA4 \n", __FILE__,
+                __func__, __LINE__);
         *params = GL_RGBA4;
     }
 }
@@ -2962,6 +2970,8 @@ static GLenum sPrepareRenderbufferStorage(GLenum internalformat, GLsizei width,
         }
     }
 
+    fprintf(stderr, "%s %s %d internalformat is 0x%x \n", __FILE__, __func__,
+            __LINE__, internalformat);
 
     // Get current bounded renderbuffer
     // raise INVALID_OPERATIOn if no renderbuffer is bounded
@@ -2972,7 +2982,11 @@ static GLenum sPrepareRenderbufferStorage(GLenum internalformat, GLsizei width,
     if (!rbData) { *err = GL_INVALID_OPERATION; return GL_NONE; }
 
     rbData->internalformat = internalformat;
+    fprintf(stderr, "%s %s %d internalformat is 0x%x \n", __FILE__, __func__,
+            __LINE__, internalformat);
     rbData->hostInternalFormat = internal;
+    fprintf(stderr, "%s %s %d hostinternalformat is 0x%x \n", __FILE__,
+            __func__, __LINE__, internal);
     rbData->width = width;
     rbData->height = height;
     rbData->samples = samples;
@@ -2995,6 +3009,8 @@ GL_APICALL void  GL_APIENTRY glRenderbufferStorage(GLenum target, GLenum interna
     internalformat = sPrepareRenderbufferStorage(internalformat, width, height, 0, &err);
     SET_ERROR_IF(err != GL_NO_ERROR, err);
     ctx->dispatcher().glRenderbufferStorage(target,internalformat,width,height);
+    fprintf(stderr, "%s %s %d calling with internalformat 0x%x\n", __FILE__,
+            __func__, __LINE__, internalformat);
 }
 
 GL_APICALL void  GL_APIENTRY glSampleCoverage(GLclampf value, GLboolean invert){
