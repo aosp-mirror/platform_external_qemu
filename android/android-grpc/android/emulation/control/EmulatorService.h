73bff23dbc767db6988181088dc11c8875067783
Erwin Jansen <jansene@google.com>
2018-07-13 12:20:47 -0700

    Add gRPC service to control the emulator
    gRPC (https://grpc.io/) is a modern open source high performance RPC
    framework that can run in any environment. It can efficiently connect
    services in and across data centers with pluggable support for load
    balancing, tracing, health checking and authentication.
    
    We add a service definition that allows third party developers to
    directly control the emulator by using the gRPC binding. The grpc
    service can be launched by addig the -grpc <port> flag.
    
    Currently it has the following limitations:
    
    - Only available on X64 platforms
    - No authentication
    - No TLS
    
    This will be used by:
    
    - The webrtc module, which will need to control the emulator to forward
    mouse/key and control events.
    - Testing, the rpc bindings will enable easy testing scenarios.
    
    A sample is added that demonstrates how to interact with the emulator
    through an http bridge. See
    android/android-grpc/docs/grpc-samples/GRPC.MD for details on how to use
    it.
    
    It makes some small changes to the compiler toolchain, and boring ssl to
    make sure we can properly compile the depdendencies.
    
    Change-Id: I1c79895d716c819ab719a1a42cd026bf3a1415b7

---
+++ android/android-grpc/android/emulation/control/EmulatorService.h
@@ -0,0 +1,53 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include "android/console.h"
+
+namespace grpc {
+    class ServerCredentials;
+}
+
+namespace android {
+namespace emulation {
+namespace control {
+class EmulatorControllerService {
+public:
+    class Builder;
+    virtual ~EmulatorControllerService() {}
+    virtual void stop() = 0;
+};
+
+
+class EmulatorControllerService::Builder {
+public:
+    Builder();
+    Builder& withConsoleAgents(const AndroidConsoleAgents* const consoleAgents);
+    Builder& withCredentials(
+            std::shared_ptr<grpc::ServerCredentials> credentials);
+    Builder& withPort(int port);
+
+    // Returns the fully configured and running service, or nullptr if
+    // construction failed.
+    std::unique_ptr<EmulatorControllerService> build();
+
+private:
+    const AndroidConsoleAgents* mAgents;
+    int mPort{5556};
+    std::shared_ptr<grpc::ServerCredentials> mCredentials;
+};
+
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
