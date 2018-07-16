84eca959efe534692380db93a25d98dc432da468
Ben Zhang <benzene@google.com>
2018-07-16 16:26:37 -0700

    [gl][test] GL snapshot basic texture units/objects
    Adds tests for snapshot's preservation of GL texture units and texture
    objects. So far, these only cover state as described in the GLES2 spec
    state tables. More tests should follow.
    
    TextureData's globalName is put behind a get/setter.
    SaveableTexture is reset on the first bind to ensure it has the correct
    globalName.
    
    This fixes texture parameters not being restored to the correct texture
    objects in some cases when SaveableTexture's globalName was never set.
    
    Bug: 110379553
    Test: builds, tests pass on Linux with/without host GPU
    Change-Id: I62bb0a84f81ebd3fa8a3e0e6dfda4ff1d7084e4c

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -546,8 +546,11 @@ GL_APICALL void  GL_APIENTRY glBindTexture(GLenum target, GLuint texture){
             fprintf(stderr, "%s: Set invalid operation!\n", __func__);
         }
         SET_ERROR_IF(ctx->GLTextureTargetToLocal(texData->target) != ctx->GLTextureTargetToLocal(target), GL_INVALID_OPERATION);
+        texData->setGlobalName(globalTextureName);
+        if (!texData->wasBound) {
+            texData->resetSaveableTexture();
+        }
         texData->wasBound = true;
-        texData->globalName = globalTextureName;
     }
 
     ctx->setBindedTexture(target,texture);
@@ -804,7 +807,7 @@ void s_glInitTexImage2D(GLenum target, GLint level, GLint internalformat,
                 ctx->dispatcher().glBindTexture(GL_TEXTURE_2D,
                         globalTextureName);
                 texData->sourceEGLImage = 0;
-                texData->globalName = globalTextureName;
+                texData->setGlobalName(globalTextureName);
             }
             texData->resetSaveableTexture();
         }
@@ -2508,9 +2511,9 @@ GL_APICALL void  GL_APIENTRY glGetTexParameterfv(GLenum target, GLenum pname, GL
     TextureData* texData = getTextureTargetData(target);
     if (sShouldEmulateSwizzles(texData, target, pname)) {
         *params = (GLfloat)(texData->getSwizzle(pname));
-    } else {
-        ctx->dispatcher().glGetTexParameterfv(target,pname,params);
+        return;
     }
+    ctx->dispatcher().glGetTexParameterfv(target, pname, params);
 }
 
 GL_APICALL void  GL_APIENTRY glGetTexParameteriv(GLenum target, GLenum pname, GLint* params){
@@ -2522,9 +2525,9 @@ GL_APICALL void  GL_APIENTRY glGetTexParameteriv(GLenum target, GLenum pname, GL
     TextureData* texData = getTextureTargetData(target);
     if (sShouldEmulateSwizzles(texData, target, pname)) {
         *params = texData->getSwizzle(pname);
-    } else {
-        ctx->dispatcher().glGetTexParameteriv(target,pname,params);
+        return;
     }
+    ctx->dispatcher().glGetTexParameteriv(target, pname, params);
 }
 
 GL_APICALL void  GL_APIENTRY glGetUniformfv(GLuint program, GLint location, GLfloat* params){
@@ -3667,7 +3670,7 @@ GL_APICALL void GL_APIENTRY glEGLImageTargetTexture2DOES(GLenum target, GLeglIma
             texData->type = img->type;
             texData->texStorageLevels = img->texStorageLevels;
             texData->sourceEGLImage = imagehndl;
-            texData->globalName = img->globalTexObj->getGlobalName();
+            texData->setGlobalName(img->globalTexObj->getGlobalName());
             texData->setSaveableTexture(
                     SaveableTexturePtr(img->saveableTexture));
             if (img->sync) {
