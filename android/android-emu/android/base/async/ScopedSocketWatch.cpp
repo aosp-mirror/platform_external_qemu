485e5486be9e14c7e08b3aef67b6aa3af1672c9b
Yahan Zhou <yahan@google.com>
2018-07-19 16:21:52 -0700

    [WIP] Snapshot adb pipe
    Change-Id: I0ff15bc2cde68165437f1dd668bf3b6a7225d22c

--- android/android-emu/android/base/async/ScopedSocketWatch.cpp
+++ android/android-emu/android/base/async/ScopedSocketWatch.cpp
@@ -13,11 +13,26 @@
 
 #include "android/base/sockets/SocketUtils.h"
 
+#include <execinfo.h>
+#include <stdio.h>
+
+void printCallStack() {
+    void* callstack[128];
+    int i, frames = backtrace(callstack, 128);
+    char** strs = backtrace_symbols(callstack, frames);
+    for (i = 0; i < frames; ++i) {
+        fprintf(stderr, "%s\n", strs[i]);
+    }
+    free(strs);
+}
+
+
 void android::base::SocketWatchDeleter::operator()(
         android::base::Looper::FdWatch* watch) const {
     int fd = watch->fd();
     if (fd >= 0) {
-        socketClose(watch->fd());
+        //socketClose(watch->fd());
     }
+    printCallStack();
     delete watch;
 }
