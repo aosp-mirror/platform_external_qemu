3c84ea584088cc6f9281e53373bc3121e30e31da
Lingfeng Yang <lfy@google.com>
2018-07-20 15:20:43 +0000

    Revert "Detect hangs for overflowing goldfish events."
    This reverts commit e6afe2395e96e5e2d3153358962941dc9b3247b0.
    
    Reason for revert: bug 111680683
    
    Change-Id: I07505b812f3897db7c2b63900ff3b488670c5681

--- android/android-emu/android/cmdline-options.h
+++ android/android-emu/android/cmdline-options.h
@@ -196,7 +196,6 @@ OPT_FLAG(skip_adb_auth, "Skip adb authentication dialogue")
 
 OPT_FLAG(metrics_to_console, "Enable usage metrics and print the messages to stdout")
 OPT_PARAM(metrics_to_file, "<file>", "Enable usage metrics and write the messages into specified file")
-OPT_FLAG(detect_image_hang, "Enable the detection of system image hangs.")
 
 OPT_LIST(feature, "<name|-name>", "Force-enable or disable (-name) the features")
 
