125be4d2417790e9c119059a643baedad66a4cef
Ben Zhang <benzene@google.com>
2018-07-26 11:26:14 -0700

    [test] fix OpenGLTestContext missing gtest header
    OpenGLTestContext declares a class based on ::testing::Test, but didn't
    include the gtest header. Fixing this allows the order of headers to
    more closely follow our style guide.
    
    gtest.h contains definitions for None and Bool that conflict with some
    that result from including OpenGLDispatchLoader.h.
    
    Test: builds, tests pass on Linux
    Change-Id: I3a171527fe999ae7a36d567adaf61c3d3190772a

--- android/android-emugl/host/libs/libOpenglRender/tests/OpenGLTestContext.cpp
+++ android/android-emugl/host/libs/libOpenglRender/tests/OpenGLTestContext.cpp
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gtest/gtest.h>
+#include "OpenGLTestContext.h"
 
 #include "android/utils/system.h"
-#include "OpenGLTestContext.h"
 
 #include "Standalone.h"
 
