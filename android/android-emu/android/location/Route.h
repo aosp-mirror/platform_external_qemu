4d9c3df5d6c5bfc3d182ec8fd064c271e2f9616c
Jim Kaye <jameskaye@google.com>
2018-07-26 11:51:52 -0700

    Create protobuf for GPS points and routes
    Create protobuf definitions to hold metadata associated
    with saved GPS locations and routes. The "point" protobuf
    contains the location in addition to metadata. The "route"
    protobuf contains only metadata--the actual route is kept
    in a separate JSON or XML file.
    
    Bug: 79420176
    Test: Added location/Point_unittest and /Route_unittest
    Change-Id: I751927306ed6d35390e499e64ea6e9c2f36bf0b3

---
+++ android/android-emu/android/location/Route.h
@@ -0,0 +1,40 @@
+// Copyright 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/base/StringView.h"
+#include "android/location/proto/route.pb.h"
+
+namespace android {
+namespace location {
+
+class Route final {
+public:
+    Route(const char* name);
+
+    base::StringView name() const { return mName; }
+    const emulator_location::RouteMetadata* getProtoInfo();
+
+private:
+
+    bool isValid = false;
+    std::string mName;
+    emulator_location::RouteMetadata mRoutePb;
+    uint64_t mSize = 0; // Size of the protobuf file
+};
+
+inline bool operator==(const Route& l, const Route& r) {
+    return l.name() == r.name();
+}
+
+}  // namespace location
+}  // namespace android
