5fa08c7a571a8a1fc5ce3184936764832195edc0
Lingfeng Yang <lfy@google.com>
2018-08-06 12:13:12 -0700

    [snapshot] File-backed RAM mechanism in AndroidEmu
    This CL contains the mechanisms in AndroidEmu snapshot library
    to deal with file-backed RAM; a way to tell Snapshotter
    that the RAM is file backed, along with mechanisms to skip most
    of the RAM save/load steps if file-backed RAM is active.
    
    Skip RAM load if:
    
    - RAM is file-backed
    
    Skip RAM save on exit if:
    
    - RAM is file-backed and mapped as shared
    
    For generic RAM saves/loads, there is no ram image for them so
    RAM save/load will use the previous mechanism.
    
    This code should not execute yet. The following additional changes
    are required:
    
    1. Cross-platform mmap-alloc.c, in qemu 2.12
    2. A change in vl.c that, upon seeing a mem_path specified,
    connects it up to androidSnapshot interface
    3. Feature flag protecting enablement of (2)
    
    Test: No change to snapshot / Quickboot behavior
    
    + Add a feature flag
    
    Change-Id: I6b522413aa016e62da75b1b8eaff8a16c4a96f88

--- android/android-emu/android/snapshot/Snapshotter.cpp
+++ android/android-emu/android/snapshot/Snapshotter.cpp
@@ -284,7 +284,7 @@ void Snapshotter::prepareLoaderForSaving(const char* name) {
         mLoader->status() != OperationStatus::Ok) {
         mLoader.reset();
     } else {
-        mLoader->synchronize(mIsOnExit);
+        mLoader->synchronize(mIsOnExit && (mRamFile.empty() || !mRamFileShared));
     }
 }
 
@@ -621,7 +621,9 @@ OperationStatus Snapshotter::prepareForSaving(const char* name) {
             name, (mLoader && mLoader->status() != OperationStatus::Error)
                           ? &mLoader->ramLoader()
                           : nullptr,
-            mIsOnExit));
+            mIsOnExit,
+            mRamFile,
+            mRamFileShared));
     mVmOperations.vmStart();
     mSaver->prepare();
     return mSaver->status();
@@ -639,6 +641,11 @@ OperationStatus Snapshotter::save(bool isOnExit, const char* name) {
     mVmOperations.snapshotSave(name, this, nullptr);
     mLastSaveDuration.emplace(sw.elapsedUs() / 1000);
     return mSaver->status();
+}
+
+void Snapshotter::setRamFile(const char* path, bool shared) {
+    mRamFile = path;
+    mRamFileShared = shared;
 }
 
 void Snapshotter::cancelSave() {
@@ -751,7 +758,9 @@ bool Snapshotter::onStartSaving(const char* name) {
                 name, (mLoader && mLoader->status() != OperationStatus::Error)
                               ? &mLoader->ramLoader()
                               : nullptr,
-                mIsOnExit));
+                mIsOnExit,
+                mRamFile,
+                mRamFileShared));
     }
     if (mSaver->status() == OperationStatus::Error) {
         onSavingComplete(name, -1);
