c2f7d3a71394205c2a6eea6582d0fbec7d7bfe1a
Jeff McGlynn <jwmcglynn@google.com>
2018-08-07 13:02:36 -0700

    Update to use the latest version of googletest
    Change-Id: I36f7141857307d94310826751d1ea67f2f5b4d13

--- android/third_party/googletest/Android.mk
+++ android/third_party/googletest/Android.mk
@@ -1,29 +1,39 @@
 ###########################################################
 ###########################################################
 ###
-###  GTest libraries.
+###  Googletest libraries.
 ###
-###  GoogleTest is used to build the emulator's Android-specific
+###  Googletest is used to build the emulator's Android-specific
 ###  unit tests. The sources are located under
 ###  $ANDROID/extern/gtest but because we need to build both
 ###  32-bit and 64-bit host libraries, don't reuse the
 ###  Android.mk there, define a module here instead.
 
-EMULATOR_GTEST_SOURCES_DIR ?= $(LOCAL_PATH)/../gtest
+EMULATOR_GTEST_SOURCES_DIR ?= $(LOCAL_PATH)/../googletest
 EMULATOR_GTEST_SOURCES_DIR := $(EMULATOR_GTEST_SOURCES_DIR)
 ifeq (,$(strip $(wildcard $(EMULATOR_GTEST_SOURCES_DIR))))
     $(error Cannot find GoogleTest sources directory: $(EMULATOR_GTEST_SOURCES_DIR))
 endif
 
-EMULATOR_GTEST_INCLUDES := $(EMULATOR_GTEST_SOURCES_DIR)/include
-EMULATOR_GTEST_SOURCES := src/gtest-all.cc src/gtest_main.cc
+EMULATOR_GTEST_INCLUDES := \
+    $(EMULATOR_GTEST_SOURCES_DIR)/googletest/include \
+    $(EMULATOR_GTEST_SOURCES_DIR)/googlemock/include
+
+EMULATOR_GTEST_SOURCES := \
+    googletest/src/gtest-all.cc \
+    googlemock/src/gmock-all.cc \
+    googlemock/src/gmock_main.cc
+
 
 old_LOCAL_PATH := $(LOCAL_PATH)
 
 LOCAL_PATH := $(EMULATOR_GTEST_SOURCES_DIR)
 
 $(call start-emulator-library, emulator-libgtest)
-LOCAL_C_INCLUDES += $(EMULATOR_GTEST_INCLUDES)
+LOCAL_C_INCLUDES += \
+    $(EMULATOR_GTEST_SOURCES_DIR)/googletest \
+    $(EMULATOR_GTEST_SOURCES_DIR)/googlemock \
+    $(EMULATOR_GTEST_INCLUDES)
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_CFLAGS += -O0 -Wno-unused-variable
 LOCAL_SRC_FILES := $(EMULATOR_GTEST_SOURCES)
