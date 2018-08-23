33189b58f06b446247ec665eedde141f41fe9dee
Yahan Zhou <yahan@google.com>
2018-08-23 21:30:02 +0000

    Revert "Revert "Allow switching block devices on snapshot load""
    This reverts commit 7d13971181269f6f3a2f16943d38426bbbe176fb.
    
    Reason for revert: fix coming in following up CLs
    
    Change-Id: Iffb58f9a67935108a75248fb62ad5f3e76c8f034

---
+++ android-qemu2-glue/drive-share.h
@@ -0,0 +1,26 @@
+// Copyright 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/utils/compiler.h"
+
+ANDROID_BEGIN_HEADER
+
+#include "qemu/osdep.h"
+#include "sysemu/blockdev.h"
+
+extern int android_drive_share_init(bool wipe_data,
+                                    bool read_only,
+                                    const char* snapshot_name,
+                                    BlockInterfaceType blockDefaultType);
+
+ANDROID_END_HEADER
