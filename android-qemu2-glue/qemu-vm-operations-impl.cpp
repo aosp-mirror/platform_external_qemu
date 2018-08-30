f7ce1394f87784e2737cba6f8b55158a101c2e27
Yahan Zhou <yahan@google.com>
2018-08-29 17:35:46 -0700

    Wire up file-backed ram with snapshot test runner
    This CL switches file-backed RAM between share and private mode for
    snapshot test runner. Currently it is only supported with the snapshot
    name "default_boot", thus only 1 snapshot can be supported at a time.
    
    It also change the behavior of qemu_snapshot_remap such that if it does
    not have a backing default_boot previously, then it would do a save.
    
    Change-Id: I2dd0803b67784a541357ffd765a56910259aa360

--- android-qemu2-glue/qemu-vm-operations-impl.cpp
+++ android-qemu2-glue/qemu-vm-operations-impl.cpp
@@ -240,11 +240,13 @@ static bool qemu_snapshot_remap(bool shared,
     // (going from auto saving to continuing to auto-save).
     auto currentRamFileStatus = androidSnapshot_getRamFileInfo();
 
-    if (currentRamFileStatus == SNAPSHOT_RAM_FILE_NONE) {
-        return true;
-    }
-
-    if (android::snapshot::Snapshotter::get().loadedSnapshotFile() != "default_boot") {
+    // If the backing file is not "default_boot" then we cannot use file-backed
+    // ram.
+    // If it doesn't have a backing file then we will create a default_boot
+    // snapshot.
+    if (android::snapshot::Snapshotter::get().loadedSnapshotFile() != "" &&
+        android::snapshot::Snapshotter::get().loadedSnapshotFile() !=
+                "default_boot") {
         return true;
     }
 
@@ -254,7 +256,7 @@ static bool qemu_snapshot_remap(bool shared,
 
     bool wasVmRunning = runstate_is_running() != 0;
 
-    if (currentRamFileStatus == SNAPSHOT_RAM_FILE_SHARED && !shared) {
+    if (currentRamFileStatus != SNAPSHOT_RAM_FILE_PRIVATE && !shared) {
         vm_stop(RUN_STATE_SAVE_VM);
         android::snapshot::Snapshotter::get().setRemapping(true);
         qemu_savevm("default_boot", MessageCallback(opaque, nullptr, errConsumer));
