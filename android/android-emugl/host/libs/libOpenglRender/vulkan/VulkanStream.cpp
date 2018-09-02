c5c095cf5f9d650f430ee1ec248f9321bdeb7e80
Lingfeng Yang <lfy@google.com>
2018-09-01 20:51:05 -0700

    [vulkan] Support for unmarshaling strings
    bug: 111137294
    
    Strings and string arrays are now automatically allocated in the
    received Vulkan structs.
    
    The VulkanStream now has a Pool object for performing these allocations,
    which will tend to be small. Functions are added to VulkanStream to
    allocate space for these strings and populate them in place.
    
    Test: Unit test
    
    Change-Id: Ie74f8e9f520da895005551873d428bfd43d5d3b1

--- android/android-emugl/host/libs/libOpenglRender/vulkan/VulkanStream.cpp
+++ android/android-emugl/host/libs/libOpenglRender/vulkan/VulkanStream.cpp
@@ -14,10 +14,59 @@
 
 #include "VulkanStream.h"
 
+#include "android/base/files/StreamSerializing.h"
+
 namespace goldfish_vk {
 
 VulkanStream::VulkanStream() {
 }
+
+void VulkanStream::loadStringInPlace(char** forOutput) {
+    size_t len = this->getBe32();
+
+    *forOutput = mPool.allocArray<char>(len + 1);
+
+    memset(*forOutput, 0x0, len + 1);
+
+    if (len > 0) {
+        this->read(*forOutput, len);
+    }
+}
+
+void VulkanStream::loadStringArrayInPlace(char*** forOutput) {
+    size_t count = this->getBe32();
+
+    if (!count) { *forOutput = nullptr; return; }
+
+    *forOutput = mPool.allocArray<char*>(count);
+
+    char** stringsForOutput = *forOutput;
+
+    for (size_t i = 0; i < count; i++) {
+        loadStringInPlace(stringsForOutput + i);
+    }
+}
+
+// void VulkanStream::loadAndDupString(char** forOutput) {
+//     std::string s = getString();
+// 
+//     *forOutput = mPool.alloc(s.size() + 1);
+//     memset(*forOutput, 0x0, s.size() + 1);
+//     memcpy(*forOutput, s.data(), s.size());
+// }
+// 
+// void VulkanStream::loadAndDupStringArray(char*** forOutput) {
+//     std::vector<std::string> s = loadStringArray(this);
+// 
+// 
+// 
+//     *forOutput = mPool.alloc(s.size() + 1);
+//     memset(*forOutput, 0x0, s.size() + 1);
+// 
+//     memcpy(*forOutput, s.data(), s.size());
+// }
+
+
 
 // TODO: Vulkan-specific common implementations
 
