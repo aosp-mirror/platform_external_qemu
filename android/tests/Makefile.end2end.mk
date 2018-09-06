dc11ba5bc48987b49a819f8c3d6b2f60722161bb
Lingfeng Yang <lfy@google.com>
2018-09-05 22:58:23 -0700

    [test] Start on a test that starts the emulator
    bug: 114158027
    
    Just -accel-check for now.
    
    Change-Id: I797b8af06668ac76c7a44c491e1a92dc179d60fa

---
+++ android/tests/Makefile.end2end.mk
@@ -0,0 +1,12 @@
+emulator-end2end-test = \
+    $(eval $(call start-emulator-program, $1_end2endtest)) \
+	$(eval LOCAL_C_INCLUDES += $(ANDROID_EMU_INCLUDES) $(EMULATOR_GTEST_INCLUDES)) \
+    $(eval LOCAL_STATIC_LIBRARIES += android-emu-base emulator-libgtest) \
+	$(eval LOCAL_SRC_FILES := android/tests/end2end/$1.cpp) \
+	$(eval $(call end-emulator-program)) \
+
+ifeq ($(BUILD_TARGET_SUFFIX),64)
+
+$(call emulator-end2end-test,HelloEmulator)
+
+endif
