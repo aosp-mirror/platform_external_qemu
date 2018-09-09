df218c83f5d6ce29d7f45f3d2e5b2f9270db3339
Lingfeng Yang <lfy@google.com>
2018-09-09 11:12:05 -0700

    [win][filelock] close handles in contended case
    Fixes: 73999036
    
    If the lock cannot be acquired, we open the file for reading and check
    the pid, but don't close it.  This can prevent the lock from getting
    successfully unlocked later on.
    
    Change-Id: I37a2ede07a48aadaaae68706ed159d9090793140

--- android/android-emu/android/utils/filelock.cpp
+++ android/android-emu/android/utils/filelock.cpp
@@ -12,6 +12,7 @@
 
 #include "android/utils/filelock.h"
 
+#include "android/base/files/ScopedFileHandle.h"
 #include "android/base/system/System.h"
 #include "android/utils/eintr_wrapper.h"
 #include "android/utils/lock.h"
@@ -38,6 +39,7 @@
 #  include <signal.h>
 #endif
 
+using android::base::ScopedFileHandle;
 using android::base::System;
 
 // Set to 1 to enable debug traces here.
@@ -189,7 +191,7 @@ static int filelock_lock(FileLock* lock, int timeout) {
             (lastErr == ERROR_ACCESS_DENIED ||
              lastErr == ERROR_SHARING_VIOLATION)) {
 
-            HANDLE getpidHandle =
+            ScopedFileHandle getpidHandle(
                 ::CreateFileW(
                     unicodeName.c_str(),
                     GENERIC_READ, // open only for reading
@@ -199,25 +201,27 @@ static int filelock_lock(FileLock* lock, int timeout) {
                     nullptr,               // default security
                     OPEN_EXISTING,         // existing file only
                     FILE_ATTRIBUTE_NORMAL, // normal file
-                    nullptr);              // no template file
+                    nullptr));              // no template file
 
-            // Read the pid of the locking process.
-            char buf[12] = {};
-            DWORD bytesRead;
-            if (::ReadFile(getpidHandle, buf, 12, &bytesRead, nullptr)) {
-                DWORD lockingPid;
-                if (sscanf(buf, "%lu", &lockingPid) == 1) {
-                    // Try waiting for the specified timeout for
-                    // the locking process to exit. If that doesn't work, bail.
-                    System::get()->waitForProcessExit(lockingPid,
-                            sleep_duration_ms);
-                    slept = true;
+            if (getpidHandle.valid()) {
+                // Read the pid of the locking process.
+                char buf[12] = {};
+                DWORD bytesRead;
+                if (::ReadFile(getpidHandle.get(), buf, 12, &bytesRead, nullptr)) {
+                    DWORD lockingPid;
+                    if (sscanf(buf, "%lu", &lockingPid) == 1) {
+                        // Try waiting for the specified timeout for
+                        // the locking process to exit. If that doesn't work, bail.
+                        System::get()->waitForProcessExit(lockingPid,
+                                sleep_duration_ms);
+                        slept = true;
+                    }
                 }
+                // Sometimes a previous file gets readonly attribute even when
+                // its parent process has exited; that prevents us from opening
+                // it for writing.
+                SetFileAttributesW(unicodeName.c_str(), FILE_ATTRIBUTE_NORMAL);
             }
-            // Sometimes a previous file gets readonly attribute even when
-            // its parent process has exited; that prevents us from opening
-            // it for writing.
-            SetFileAttributesW(unicodeName.c_str(), FILE_ATTRIBUTE_NORMAL);
         }
         if (lockHandle != INVALID_HANDLE_VALUE) {
             createFileResult = true;
