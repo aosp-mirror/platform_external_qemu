d2db4a395ac6ad78bd2b2f95eed1f7c3167a9051
Yahan Zhou <yahan@google.com>
2018-09-11 17:25:12 -0700

    [snapshot][gl] Re-add snapshot operation callback to Renderer
    bug: 115828136
    bug: 115884608
    
    - Rework of
    https://android-review.googlesource.com/c/platform/external/qemu/+/753833
    but only considering the reliability / renderer suspend part
    - Operation callback to Renderer helps RenderThread/Window suppress
    activity while a snapshot load is in progress, which helps reliability.
    - Other parts such as background load enabling / disabling not included,
    for future CLs.
    
    Change-Id: Ib3b285d19bc6dbc13046a1da8772f6cf40e45bad

--- android/android-emugl/host/libs/Translator/include/GLcommon/GLBackgroundLoader.h
+++ android/android-emugl/host/libs/Translator/include/GLcommon/GLBackgroundLoader.h
@@ -33,6 +33,10 @@ public:
         m_eglIface(eglIface),
         m_glesIface(glesIface),
         m_textureMap(textureMap) { }
+    ~GLBackgroundLoader() {
+        wait(nullptr);
+        m_textureMap.clear();
+    }
 
     intptr_t main();
 
