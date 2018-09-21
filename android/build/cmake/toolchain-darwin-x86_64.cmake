9f8a8b5218287f3d9400ad9eac85b7a6a649ab3a
Erwin Jansen <jansene@google.com>
2018-09-21 14:14:29 -0700

    Generate toolchain from within CMake
    - This will enable to us to transition aways from needing the configure
      script.
    - It generates the compiler and sets up the projects.
    - It sets the cmake tool incude path, so you can include what is needed
      easily
    - It will define: QEMU2_TOP_DIR and the proper O
    
    You can now simply do:
    
    cmake \
    -DCMAKE_TOOLCHAIN_FILE=./android/build/cmake-toolchain-<mytarget>.cmake
    make
    
    Test: android/build/cmake/test/run_cmake_tests.sh looks good.
    Change-Id: I24cb8a6eedecd3686e341333e86226faacd85be8

---
+++ android/build/cmake/toolchain-darwin-x86_64.cmake
@@ -0,0 +1,23 @@
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+get_filename_component(ADD_PATH "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
+list (APPEND CMAKE_MODULE_PATH "${ADD_PATH}")
+include(toolchain)
+
+# First we create the toolchain
+set(LOCAL_TARGET_TAG "darwin-x86_64")
+toolchain_generate("${LOCAL_TARGET_TAG}")
+
+# No magical includes or dependencies for darwin..
