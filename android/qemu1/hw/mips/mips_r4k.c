050d56e1a284486d22f0f3ee570dc13e13e2be1d
Lingfeng Yang <lfy@google.com>
2018-09-25 09:04:23 -0700

    Fix hang on exit in debug mac build
    Fixes: 116383800
    Fixes: 116609070
    
    qemu_system_shutdown_request requires 1 argument, while it is specified
    and called with 0 arguments on a regular basis. This makes the stack
    unhappy.
    
    + Make QEMU1 use the same interface
    + Add build tests to make sure it the androidemu version of the struct
    is the same.
    
    Change-Id: I97af136f0204bc5aaf4476d04976ec4591845b06

--- android/qemu1/hw/mips/mips_r4k.c
+++ android/qemu1/hw/mips/mips_r4k.c
@@ -48,7 +48,7 @@ static void mips_qemu_writel (void *opaque, hwaddr addr,
     if ((addr & 0xffff) == 0 && val == 42)
         qemu_system_reset_request ();
     else if ((addr & 0xffff) == 4 && val == 42)
-        qemu_system_shutdown_request ();
+        qemu_system_shutdown_request (SHUTDOWN_CAUSE_GUEST_SHUTDOWN);
 }
 
 static uint32_t mips_qemu_readl (void *opaque, hwaddr addr)
