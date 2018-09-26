19102db4b9ab4acf49f4e972f1c7fdab99b0f955
Weilun Du <wdu@google.com>
2018-09-26 14:35:29 -0700

    [GL] Use RefCount Pipe to Manage Color Buffer
    Host side reference counting of Color Buffer objects is triggered
    by functions in gralloc in guest side implementaion.
    However, there are situations where gralloc_free() can arrive
    before register_buffer() has arrived in gralloc.
    
    Previously, we patched this issue with other workaround such as a
    delayed list of closed color buffers. Altough, we have fixed the problem
    of color buffer being destroyed too early, the workaround has flaw such as
    memory leaks and it becomes harder and harder to maintain in both guest
    and host side.
    
    Another approach is to delegate the work of reference counting to guest
    kernel. To do so, in guest side, a pipe is created when the host color
    buffer is created. The pipe would be closed when all the guest side
    references to the RefCount file descriptor are closed. When the pipe is
    closed, on the hos side, color buffer will be immediately destroyed.
    This approach wins by simplicity. A guest side feature flag is added. If
    the feature flag is on, the code path to do reference counting for color
    buffer on host will not be executed.
    
    BUG: 80050618
    BUG: 110765634
    Change-Id: I6201b491c69a88b63d8f0c2d8e744dc7796ae3ba
    Signed-off-by: Weilun Du <wdu@google.com>

---
+++ android/android-emu/android/emulation/RefcountPipe.cpp
@@ -0,0 +1,102 @@
+// Copyright 2018 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/emulation/RefcountPipe.h"
+
+#include "android/refcount-pipe.h"
+#include "android/utils/debug.h"
+
+#include <fstream>
+#include <iostream>
+
+namespace android {
+namespace emulation {
+
+static OnLastColorBufferRef sOnLastColorBufferRef = nullptr;
+
+RefcountPipe::RefcountPipe(void* hwPipe, Service* svc, base::Stream* loadStream)
+    : AndroidPipe(hwPipe, svc) {
+    if (loadStream) {
+        mHandle = loadStream->getBe32();
+    }
+}
+
+void RefcountPipe::onGuestClose(PipeCloseReason reason) {
+    if (sOnLastColorBufferRef != nullptr)
+        sOnLastColorBufferRef(mHandle);
+    delete this;
+}
+
+unsigned RefcountPipe::onGuestPoll() const {
+    // Guest can always write
+    return PIPE_POLL_OUT;
+}
+
+int RefcountPipe::onGuestRecv(AndroidPipeBuffer* buffers, int numBuffers) {
+    // Guest is not supposed to read
+    return PIPE_ERROR_IO;
+}
+
+int RefcountPipe::onGuestSend(const AndroidPipeBuffer* buffers,
+                              int numBuffers) {
+    int result = 0;
+    char forRecv[4] = {};
+
+    while (numBuffers > 0) {
+        memcpy(forRecv, buffers->data, buffers->size);
+        result += static_cast<int>(buffers->size);
+        buffers++;
+        numBuffers--;
+    }
+
+    if (result == 4) {
+        memcpy(&mHandle, forRecv, 4);
+    }
+
+    return result;
+}
+
+void RefcountPipe::onSave(base::Stream* stream) {
+    stream->putBe32(mHandle);
+}
+
+void registerRefcountPipeService() {
+    android::AndroidPipe::Service::add(new RefcountPipe::Service());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+RefcountPipe::Service::Service() : AndroidPipe::Service("refcount") {}
+
+AndroidPipe* RefcountPipe::Service::create(void* hwPipe, const char* args) {
+    return new RefcountPipe(hwPipe, this);
+}
+
+AndroidPipe* RefcountPipe::Service::load(void* hwPipe,
+                                         const char* args,
+                                         android::base::Stream* stream) {
+    return new RefcountPipe(hwPipe, this, stream);
+}
+
+bool RefcountPipe::Service::canLoad() const {
+    return true;
+}
+
+void registerOnLastRefCallback(OnLastColorBufferRef func) {
+    sOnLastColorBufferRef = func;
+}
+
+}  // namespace emulation
+}  // namespace android
+
+void android_init_refcount_pipe(void) {
+    android::emulation::registerRefcountPipeService();
+}
