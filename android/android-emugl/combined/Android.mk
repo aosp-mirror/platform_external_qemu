2e7d7f53f5ee02de9968f8ebe1157c51ddef8673
Lingfeng Yang <lfy@google.com>
2018-09-27 16:29:27 -0700

    wip: combined test
    figure out rpath:
    
    - make android emu shared not in the objs/build/intermediates64 path
    - LOCAL_INSTALL_OPENGL makes stuff search locally for the right libs on
    Mac at least
    
    figure out how to import swiftshader correctly
    
    - more rpath stuff?
    
    Change-Id: I403f3ab319bd99700cf1d3a78492a225d3e183ef

---
+++ android/android-emugl/combined/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+$(call emugl-begin-executable,emugl_combined_unittests)
+
+$(call emugl-import,libemugl_gtest)
+
+$(call emugl-export,C_INCLUDES,$(EMUGL_PATH)/host/include)
+$(call emugl-export,C_INCLUDES,$(LOCAL_PATH))
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH) \
+    $(EMUGL_PATH)/host/include \
+    $(ANDROID_EMU_BASE_INCLUDES) \
+
+LOCAL_SRC_FILES += combined_unittest.cpp
+
+LOCAL_SHARED_LIBRARIES += \
+    android-emu-shared \
+    libOpenglSystemCommon \
+    libEGL_emulation \
+
+LOCAL_INSTALL_OPENGL := true
+
+$(call emugl-end-module)
