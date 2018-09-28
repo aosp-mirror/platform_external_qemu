bea6563574fd0ef8e8ec0068c42ff27be76f80fd
Matt Wachowski <mattwach@google.com>
2018-09-28 10:56:46 -0700

    Disable verified boot when -writable-system is set
    Hashes for verified boot are calculated at build time.  Changing the
    system image at runtime will invalidate these hashes (as per design).
    
    If someone is using -writable-system, they want to be able to modify
    the system image.  This CL disables verified boot for
    this special case.
    
    Bug: b/116826984
    Test: I corrupted VerifiedBootParams.textproto and asserted the emulator
    would not boot.  I then tried booting the emulator with -writable-system
    set and asserted that the boot was successful.
    
    Change-Id: Ib73467abc42a5b0dbe0303db7b1c14f38e080e37

--- android-qemu2-glue/main.cpp
+++ android-qemu2-glue/main.cpp
@@ -1438,10 +1438,13 @@ extern "C" int main(int argc, char** argv) {
         }
 
         // Get verified boot kernel parameters, if they exist.
+        // Disable if the system partition is writable.
         std::vector<std::string> verified_boot_params;
-        android::verifiedboot::getParametersFromFile(
-                avdInfo_getVerifiedBootParamsPath(avd),  // NULL here is OK
-                &verified_boot_params);
+        if (!android_op_writable_system) {
+          android::verifiedboot::getParametersFromFile(
+                  avdInfo_getVerifiedBootParamsPath(avd),  // NULL here is OK
+                  &verified_boot_params);
+        }
 
         ScopedCPtr<char> kernel_parameters(emulator_getKernelParameters(
                 opts, kTarget.androidArch, apiLevel, kTarget.ttyPrefix,
