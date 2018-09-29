2478c9aa43f02c9cd60e3c1b94276a5a72a04726
Lingfeng Yang <lfy@google.com>
2018-09-29 13:07:49 -0700

    [goldfish-opengl] libgui: BQ, ANW
    bug: 116983208
    
    This CL includes enough implementation of BufferQueue
    and ANativeWindow to be dangerous (work with dEQP).
    
    The following functionality is supported:
    
    - Basic queue/dequeue of buffers to/from BufferQueue's,
    ignore fence fds (those come later).
    - Creating ANativeWindow objects that only hold native buffer
    handles and don't do much else.
    - ANativeWindow.cpp from frameworks/native is copied directly
    to provide a plausible looking interface.
    
    The plan is to create those ANativeWindow's from the dEQP framework
    code. Fakes of SF/gralloc on the host side will then provide the
    ANativeWindowBuffer's, and will also take care of the eglSwapBuffers
    implementation. This can potentially also be hooked up to an existing
    HWC HAL implementation if available.
    
    Change-Id: I13f5039c9da1d3f032e750020e4f67737ebbd67e

---
+++ android/android-emugl/guest/androidImpl/AndroidWindow.h
@@ -0,0 +1,58 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "AndroidBufferQueue.h"
+
+#include <system/window.h>
+
+namespace aemu {
+
+class AndroidWindow : public ANativeWindow {
+public:
+    AndroidWindow(int width, int height);
+
+    ~AndroidWindow() = default;
+
+    static AndroidWindow* getSelf(ANativeWindow* window) {
+        return (AndroidWindow*)(window);
+    }
+
+    static const AndroidWindow* getSelfConst(const ANativeWindow* window) {
+        return (const AndroidWindow*)(window);
+    }
+
+    void setProducer(AndroidBufferQueue* fromProducer,
+                     AndroidBufferQueue* toProducer);
+    int dequeueBuffer(ANativeWindowBuffer** buffer, int* fenceFd);
+    int queueBuffer(ANativeWindowBuffer* buffer, int fenceFd);
+
+    int query(int what, int* value) const;
+
+    AndroidBufferQueue* fromProducer = nullptr;
+    AndroidBufferQueue* toProducer = nullptr;
+
+    int swapInterval = 1;
+    int width = 256;
+    int height = 256;
+};
+
+} // namespace aemu
+
+extern "C" {
+
+ANativeWindow* create_host_anativewindow(int width, int height);
+void destroy_host_anativewindow(ANativeWindow* window);
+
+} // extern "C"
