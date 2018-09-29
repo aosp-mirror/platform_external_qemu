52a184bf9c531bb6c5b314698a6d2eaa63c908bd
Lingfeng Yang <lfy@google.com>
2018-09-29 10:15:26 -0700

    [goldfish-opengl] Combined test with gralloc, GLES
    Fixes: 116981035
    
    - Open the gralloc HAL module as Android would do
    - Allocate color buffers
    - Query viewport in GLES
    
    Missing:
    
    - No eglSwapBuffers (no native window, buffer queue)
    
    Change-Id: I651838c1824b293b3cff55d2f893a8258d3cdbf2

--- android/android-emugl/guest/androidImpl/Log.cpp
+++ android/android-emugl/guest/androidImpl/Log.cpp
@@ -25,6 +25,7 @@ EXPORT int __android_log_print(int prio, const char* tag, const char* fmt, ...) 
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
     va_end(ap);
     return 0;
 }
