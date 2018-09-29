e387d46cce16db99dc34e279859d0c5c5899766b
Lingfeng Yang <lfy@google.com>
2018-09-29 10:15:26 -0700

    [goldfish-opengl] Combined test with gralloc, GLES
    Fixes: 116981035
    
    - Open the gralloc HAL module as Android would do
    - Allocate color buffers
    - Query viewport in GLES
    
    Missing:
    
    - No eglSwapBuffers (no native window, buffer queue)
    
    Change-Id: I651838c1824b293b3cff55d2f893a8258d3cdbf2

---
+++ android/android-emugl/guest/androidImpl/GrallocDispatch.cpp
@@ -0,0 +1,60 @@
+// Copyright 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "GrallocDispatch.h"
+#include "AndroidHostCommon.h"
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+
+extern "C" {
+
+EXPORT void load_gralloc_module(
+    const char* path,
+    struct gralloc_implementation* impl_out) {
+
+    memset(impl_out, 0x0, sizeof(struct gralloc_implementation));
+
+    impl_out->lib = dlopen(path, RTLD_NOW);
+
+    if (!impl_out->lib) {
+        fprintf(stderr, "%s: failed to load gralloc module from %s\n",
+                __func__, impl_out->lib);
+        return;
+    }
+
+    struct hw_module_t* hw_module = (struct hw_module_t*)dlsym(
+            impl_out->lib, HAL_MODULE_INFO_SYM_AS_STR);
+
+    framebuffer_open(hw_module, &impl_out->fb_dev);
+    gralloc_open(hw_module, &impl_out->alloc_dev);
+
+    impl_out->fb_module = reinterpret_cast<gralloc_module_t*>(
+        impl_out->fb_dev->common.module);
+    impl_out->alloc_module = reinterpret_cast<gralloc_module_t*>(
+        impl_out->alloc_dev->common.module);
+}
+
+EXPORT void unload_gralloc_module(
+    const struct gralloc_implementation* impl) {
+
+    if (!impl->lib) return;
+
+    gralloc_close(impl->alloc_dev);
+    framebuffer_close(impl->fb_dev);
+
+    dlclose(impl->lib);
+}
+
+} // extern "C"
