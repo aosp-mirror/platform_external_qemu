adf637ed4b0e976fc0a1e1174eec1ceb42420573
Lingfeng Yang <lfy@google.com>
2018-10-01 12:08:56 -0700

    [goldfish-opengl] Gralloc support
    bug: 116981035
    
    - Add GrallocDispatch class that makes it convenient to
    open the gralloc.ranchu.dylib using the standard Android
    HAL interfaces, and run the module methods.
    
    Change-Id: I862001848b74e7b31e970085ce66acc81b3279d0

--- android/android-emugl/guest/androidImpl/Ashmem.cpp
+++ android/android-emugl/guest/androidImpl/Ashmem.cpp
@@ -13,12 +13,19 @@
 // limitations under the License.
 #include <cutils/ashmem.h>
 
+#include "android/base/containers/Lookup.h"
 #include "android/base/memory/LazyInstance.h"
 #include "android/base/memory/SharedMemory.h"
-#include "AndroidHostCommon.h"
+#include "android/base/Uuid.h"
 
+#include "AndroidHostCommon.h"
+#include "Ashmem.h"
+
+#include <atomic>
+#include <sstream>
 #include <unordered_map>
 
+#include <errno.h>
 
 using android::base::LazyInstance;
 using android::base::SharedMemory;
@@ -28,21 +35,41 @@ public:
     SharedMemoryStore() = default;
 
     int create(const char* name, size_t size) {
-        SharedMemory mem(name, size);
-        mem.create(0755);
-        int fd = mem.getFd();
+        SharedMemory* mem = new SharedMemory(name, size);
+        mem->createNoMapping(0755);
+
+        int fd = mem->getFd();
         if (fd >= 0) {
-            mMemories.emplace(fd, std::move(mem));
+            mMemories[fd] = mem;
+        } else {
+            fprintf(stderr, "%s: SharedMemory creation failed! errno: %d\n",
+                    __func__, errno);
+            delete mem;
         }
         return fd;
     }
 
+    void clear() {
+        auto it = mMemories.begin();
+        for (; it != mMemories.end(); ++it) {
+            it->second->close();
+        }
+        mMemories.clear();
+    }
+
     void close(int fd) {
-        mMemories.erase(fd);
+        if (auto sharedMem = android::base::find(mMemories, fd)) {
+            (*sharedMem)->close();
+        }
+    }
+
+    int nextUniqueId() {
+        return std::atomic_fetch_add(&mNextId, 1);
     }
 
 private:
-    std::unordered_map<int, SharedMemory> mMemories;
+    std::atomic<int> mNextId = { 0 };
+    std::unordered_map<int, SharedMemory*> mMemories;
 };
 
 static LazyInstance<SharedMemoryStore> sStore = LAZY_INSTANCE_INIT;
@@ -54,8 +81,22 @@ EXPORT int ashmem_valid(int fd) {
     return true;
 }
 
+// ashmem_create_region seems to mean to create a new
+// subregion associated with |name|, not necessarily share
+// the region named |name|. So we implement it in terms of
+// the name + a number counting from 0.
 EXPORT int ashmem_create_region(const char *name, size_t size) {
-    return sStore->create(name, size);
+    std::stringstream ss;
+    ss << name;
+    ss << sStore->nextUniqueId();
+
+    std::string nameWithHash = ss.str();
+
+    return sStore->create(nameWithHash.c_str(), size);
+}
+
+EXPORT void ashmem_teardown() {
+    sStore->clear();
 }
 
 // TODO
