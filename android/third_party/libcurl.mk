e561653f9820b63dfa18516333b2fd2c041a9e50
Joshua Duong <joshuaduong@google.com>
2018-09-30 20:33:35 -0700

    [windows-msvc] Include windows-msvc prebuilts.
    Change-Id: I014e68cf6ba1d3ba9de49b884490973a20be1d7c

--- android/third_party/libcurl.mk
+++ android/third_party/libcurl.mk
@@ -6,17 +6,31 @@ LOCAL_PATH := $(call my-dir)
 
 LIBCURL_TOP_DIR := $(LIBCURL_PREBUILTS_DIR)/$(BUILD_TARGET_TAG)
 
-$(call define-emulator-prebuilt-library,\
-    emulator-libcurl,\
-    $(LIBCURL_TOP_DIR)/lib/libcurl.a)
+ifeq ($(BUILD_TARGET_OS),windows_msvc)
+    $(call define-emulator-prebuilt-library,\
+        emulator-libcurl,\
+        $(LIBCURL_TOP_DIR)/lib/libcurl_a.lib)
+    # ssleay32.lib ==> libssl.a on Windows
+    $(call define-emulator-prebuilt-library,\
+        emulator-libssl,\
+        $(LIBCURL_TOP_DIR)/lib/ssleay32.lib)
+    # libeay32.lib ==> libcrypto.a on Windows
+    $(call define-emulator-prebuilt-library,\
+        emulator-libcrypto,\
+        $(LIBCURL_TOP_DIR)/lib/libeay32.lib)
+else
+    $(call define-emulator-prebuilt-library,\
+        emulator-libcurl,\
+        $(LIBCURL_TOP_DIR)/lib/libcurl.a)
 
-$(call define-emulator-prebuilt-library,\
-    emulator-libssl,\
-    $(LIBCURL_TOP_DIR)/lib/libssl.a)
+    $(call define-emulator-prebuilt-library,\
+        emulator-libssl,\
+        $(LIBCURL_TOP_DIR)/lib/libssl.a)
 
-$(call define-emulator-prebuilt-library,\
-    emulator-libcrypto,\
-    $(LIBCURL_TOP_DIR)/lib/libcrypto.a)
+    $(call define-emulator-prebuilt-library,\
+        emulator-libcrypto,\
+        $(LIBCURL_TOP_DIR)/lib/libcrypto.a)
+endif
 
 LIBCURL_INCLUDES := $(LIBCURL_TOP_DIR)/include
 LIBCURL_CFLAGS += -DCURL_STATICLIB
@@ -26,11 +40,10 @@ LIBCURL_STATIC_LIBRARIES := \
     emulator-libcrypto \
     emulator-zlib
 
-ifeq ($(BUILD_TARGET_OS),windows)
-    # Believe it or not: libcurl depends on gdi32.dll on Windows!
-    LIBCURL_LDLIBS += -lcrypt32 -lgdi32
-else
-    LIBCURL_LDLIBS += -ldl
-endif
+# Believe it or not: libcurl depends on gdi32.dll on Windows!
+LIBCURL_LDLIBS += \
+        $(call if-target-any-windows, \
+        -lcrypt32 -lgdi32, \
+        -ldl)
 
 LOCAL_PATH := $(LIBCURL_OLD_LOCAL_PATH)
