5082ec2d2b864f38b03809ed60e5fe2b64bac1fb
Joshua Duong <joshuaduong@google.com>
2018-10-01 08:44:05 -0700

    [windows-msvc] Add files for windows-msvc build.
    Change-Id: Ic50176158913b98d7ec7bb28661783720a39878d

--- android/android-emugl/host/libs/libOpenglRender/Android.mk
+++ android/android-emugl/host/libs/libOpenglRender/Android.mk
@@ -50,7 +50,7 @@ ifeq ($(BUILD_TARGET_OS),darwin)
     host_common_LDLIBS += -Wl,-framework,AppKit
 endif
 
-ifeq ($(BUILD_TARGET_OS),windows)
+ifeq ($(BUILD_TARGET_OS_FLAVOR),windows)
     host_OS_SRCS = NativeSubWindow_win32.cpp
     host_common_LDLIBS += -lgdi32
 endif
@@ -89,17 +89,20 @@ standalone_common_SRC_FILES := \
     standalone_common/SearchPathsSetup.cpp \
     standalone_common/ShaderUtils.cpp \
 
-standalone_common_LDLIBS := -lm
 
 ifeq ($(BUILD_TARGET_OS),linux)
-    standalone_common_LDLIBS += -lX11 -lrt
+    standalone_common_LDLIBS += -lm -lX11 -lrt
 endif
 
 ifeq ($(BUILD_TARGET_OS),darwin)
-    standalone_common_LDLIBS += -Wl,-framework,AppKit
+    standalone_common_LDLIBS += -lm -Wl,-framework,AppKit
 endif
 
 ifeq ($(BUILD_TARGET_OS),windows)
+    standalone_common_LDLIBS += -lm
+endif
+
+ifeq ($(BUILD_TARGET_OS_FLAVOR),windows)
     standalone_common_LDLIBS += -lgdi32
 endif
 
