f9badf9361b2d67f04743f54649a8e52c356d6b0
Lingfeng Yang <lfy@google.com>
2018-10-04 19:25:22 -0700

    [goldfish-opengl] SurfaceFlinger and client composition
    bug: 117319168
    
    Change-Id: Icf1cf4564e283eeaaa8b18b6ff61aa1c90458051

--- android/android-emugl/combined/Android.mk
+++ android/android-emugl/combined/Android.mk
@@ -1,5 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
+combined_SHARED_LIBRARIES = \
+    android-emu-shared \
+    libcutils \
+    libgui \
+    libOpenglSystemCommon \
+    libEGL_emulation \
+    libGLESv2_emulation \
+
+$(call emugl-begin-shared-library,libaemugraphics)
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH) \
+    $(EMUGL_PATH)/guest \
+    $(EMUGL_PATH)/guest/androidImpl \
+    $(EMUGL_PATH)/host/include \
+    $(ANDROID_EMU_BASE_INCLUDES) \
+
+LOCAL_SRC_FILES += \
+    ClientComposer.cpp \
+
+$(call emugl-export,SHARED_LIBRARIES, $(combined_SHARED_LIBRARIES))
+
+$(call emugl-end-module)
+
 $(call emugl-begin-executable,emugl_combined_unittests)
 
 $(call emugl-import,libemugl_gtest)
@@ -14,21 +38,16 @@ LOCAL_C_INCLUDES += \
     $(EMUGL_PATH)/host/include \
     $(ANDROID_EMU_BASE_INCLUDES) \
 
+LOCAL_SRC_FILES += \
+    ClientComposer.cpp \
+    combined_unittest.cpp \
+
 ifeq ($(BUILD_TARGET_OS),linux)
 LOCAL_LDFLAGS += '-Wl,-rpath,$$ORIGIN/lib$(BUILD_TARGET_SUFFIX),-rpath,$$ORIGIN/lib$(BUILD_TARGET_SUFFIX)/gles_swiftshader'
 LOCAL_LDLIBS += -ldl
 endif
 
-LOCAL_SRC_FILES += combined_unittest.cpp
-
-LOCAL_SHARED_LIBRARIES += \
-    android-emu-shared \
-    libcutils \
-    libgui \
-    libOpenglSystemCommon \
-    libEGL_emulation \
-    libGLESv2_emulation \
-
 LOCAL_INSTALL_OPENGL := true
 
+$(call emugl-export,SHARED_LIBRARIES, $(combined_SHARED_LIBRARIES))
 $(call emugl-end-module)
