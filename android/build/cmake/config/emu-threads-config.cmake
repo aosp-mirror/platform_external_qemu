6d19d58b6a0426875297eea315fa736dfe6358fb
Erwin Jansen <jansene@google.com>
2018-10-09 18:36:18 -0700

    Use CMake build system
    This change make everything build under cmake. It activates the
    following:
    
    Working generators:
    
    - GNUMake (slow)
    - Ninja (fast!)
    - XCode (nice ide)
    
    You should be able to directly import the projects in:
    
    - VSCode
      - Install https://github.com/vector-of-bool/vscode-cmake-tools
      - Install cmake (preferably 3.7 or higher)
      - Install ninja (optional but A LOT faster)
      - Tested on Linux/Mac
      - All configuration files are checked in.
    - CLion
      - Make sure to explicitly set the toolchain of choice.
    
    Test: ./android/rebuild.sh
    Test: Emulator boots on all platforms
    
    The change contains a few set of fixes to enable compilation in Xcode.
    
    Change-Id: I6eb90dfc9c7aa102006269e2e55264086df0e3a6

---
+++ android/build/cmake/config/emu-threads-config.cmake
@@ -0,0 +1,19 @@
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# We now what kinds of threads we are using, and we've disabled finding of packages, so we just declare it explicitly
+
+add_library(Threads::Threads INTERFACE IMPORTED)
+set_property(TARGET Threads::Threads PROPERTY INTERFACE_COMPILE_OPTIONS "-pthread")
+set_property(TARGET Threads::Threads PROPERTY INTERFACE_LINK_LIBRARIES "-lpthread")
+set(CMAKE_USE_PTHREADS_INIT 1)
+set(Threads_FOUND TRUE)
+set(PACKAGE_EXPORT "Threads_FOUND;CMAKE_USE_PTHREADS_INIT")
