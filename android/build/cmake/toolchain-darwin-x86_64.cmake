22b5aef07ac029e2514b5c9eb79a96e964a2dba2
Erwin Jansen <jansene@google.com>
2018-10-09 18:36:18 -0700

    Use CMake build system
    This change make everything build under cmake. It activates the
    following:
    
    Working generators:
    
    - GNUMake (slow)
    - Ninja (fast!)
    - XCode (nice ide)
    
    You should be able to directly import the projects in:
    
    - VSCode
      - Install https://github.com/vector-of-bool/vscode-cmake-tools
      - Install cmake (preferably 3.7 or higher)
      - Install ninja (optional but A LOT faster)
      - Tested on Linux/Mac
      - All configuration files are checked in.
    - CLion
      - Make sure to explicitly set the toolchain of choice.
    
    Test: ./android/rebuild.sh
    Test: Emulator boots on all platforms
    
    The change contains a few set of fixes to enable compilation in Xcode.
    
    Change-Id: I6eb90dfc9c7aa102006269e2e55264086df0e3a6

--- android/build/cmake/toolchain-darwin-x86_64.cmake
+++ android/build/cmake/toolchain-darwin-x86_64.cmake
@@ -21,5 +21,13 @@ get_host_tag(ANDROID_HOST_TAG)
 set(ANDROID_TARGET_TAG "darwin-x86_64")
 set(ANDROID_TARGET_OS "darwin")
 set(ANDROID_TARGET_OS_FLAVOR "darwin")
+
+# Make sure we always set the rpath to include current dir and ./lib64. We need this to create self
+# contained executables that dynamically load dylibs.
+set(RUNTIME_OS_PROPERTIES "INSTALL_RPATH>=@loader_path;INSTALL_RPATH>=@loader_path/lib64;BUILD_WITH_INSTALL_RPATH=ON;INSTALL_RPATH_USE_LINK_PATH=ON")
 toolchain_generate("${ANDROID_TARGET_TAG}")
+
+# Always consider the source to be darwin.
+add_definitions(-D_DARWIN_C_SOURCE=1)
+
 # No magical includes or dependencies for darwin..
