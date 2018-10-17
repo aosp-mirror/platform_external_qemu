dca23b97cbaaa29181be611ee53028bc7d75d0d6
Joshua Duong <joshuaduong@google.com>
2018-10-17 15:58:40 -0700

    [windows-msvc] Fix cmake build errors.
    Change-Id: Iff2ab2e5348e5b2335bc6887bde51cc3c1a1ee98

--- android/android-emu/stubs/stubs.cpp
+++ android/android-emu/stubs/stubs.cpp
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Need to include this header in case the compiler decides to mangle the
+// symbol.
+#include "android/globals.h"
+
 int guest_data_partition_mounted = 0;
