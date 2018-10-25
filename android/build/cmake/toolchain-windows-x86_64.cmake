d937c0bc49e4494efc12a96507dbac29425ee619
Joshua Duong <joshuaduong@google.com>
2018-10-25 11:01:40 -0700

    Use host Qt generators.
    Windows-msvc build for Qt has windows exes for moc, rcc and uic, so
    when cross-compiling we need to use the host's.
    
    Change-Id: Idddfb7fff8750c082a346946b02ee90ebd02260f

--- android/build/cmake/toolchain-windows-x86_64.cmake
+++ android/build/cmake/toolchain-windows-x86_64.cmake
@@ -17,6 +17,7 @@ list (APPEND CMAKE_MODULE_PATH "${ADD_PATH}")
 include(toolchain)
 
 # First we create the toolchain
+set (ANDROID_HOST_TAG "linux-x86_64")
 set (ANDROID_TARGET_TAG "windows-x86_64")
 set (ANDROID_TARGET_OS "windows")
 set (ANDROID_TARGET_OS_FLAVOR "windows")
