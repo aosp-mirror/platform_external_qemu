a0efe359b9c45c69962a5c3ede4f08a28936a5a0
John <johnxiang@google.com>
2018-10-26 11:12:47 -0700

    Create widget to receive frames from android port
    When visible, the widget starts a thread and establishes a socket
    connection with a port on the Android side so it can read the
    broadcasted data and find the encoded video frames within it.
    
    Currently, the only thing it does with the incoming encoded frames is
    print their size to console just to verify they are actually being
    received. Otherwise, there is a temporary placeholder display on the
    actual tab.
    
    Test: Built with car target & started emulator
    Test: adb forward tcp:1234 tcp:5151
    Test: Manually verify the tab and console output.
    Change-Id: I66f2df327a8d0d6f3cceb6063ab42f98d0839969

---
+++ android/android-emu/android/skin/qt/extended-pages/instr-cluster-render/renderthread.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <QWidget>
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <memory>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <QImage>
+#include <QThread>
+
+class RenderThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    RenderThread(QWidget* parent);
+    ~RenderThread();
+
+signals:
+    void renderedImage(const QImage &image);
+
+private slots:
+
+public slots:
+    void wakeThread();
+    void exitThread();
+
+protected:
+    void run();
+
+private:
+    int readStream();
+    bool tryConnect();
+    bool isFrameStart(std::vector<char> encodedFrames, int ind);
+    int findNextFrameIndex(std::vector<char> encodedFrames, int startInd);
+    int getNextFrame();
+
+    bool exitRequested = true;
+
+    int sockfd;
+    struct sockaddr_in serverAddr;
+    bool isConnected = false;
+
+    QWidget* mParent;
+
+    std::vector<char> mEncodedData;
+    char* mNextFrame;
+};
