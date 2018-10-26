62db6fcb1a593103109d6192fec3af3d573e2956
John <johnxiang@google.com>
2018-10-26 11:12:47 -0700

    Create widget to receive frames from android port
    When visible, the widget starts a thread and establishes a socket
    connection with a port on the Android side so it can read the
    broadcasted data and find the encoded video frames within it.
    
    Currently, the only thing it does with the incoming encoded frames is
    print their size to console just to verify they are actually being
    received. Otherwise, there is a temporary placeholder display on the
    actual tab.
    
    Test: Built with car target & started emulator
    Test: adb forward tcp:1234 tcp:5151
    Test: Manually verify the tab and console output.
    Change-Id: I66f2df327a8d0d6f3cceb6063ab42f98d0839969

---
+++ android/android-emu/android/skin/qt/extended-pages/instr-cluster-render/renderwidget.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <QPixmap>
+#include <QWidget>
+#include "renderthread.h"
+#include <QImage>
+
+class RenderWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    RenderWidget(QWidget* parent = 0);
+
+signals:
+    void widgetIsVisible();
+    void widgetIsInvisible();
+
+protected:
+    void paintEvent(QPaintEvent* event);
+    void showEvent(QShowEvent* event);
+    void hideEvent(QHideEvent* event);
+
+private slots:
+    void updatePixmap(const QImage& image);
+
+private:
+    RenderThread* thread;
+    QPixmap pixmap;
+};
