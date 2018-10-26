02cf45050aa7a36c8272914b8ed77fc1e3ad58a3
John <johnxiang@google.com>
2018-10-26 11:12:47 -0700

    Create widget to receive frames from android port
    When visible, the widget starts a thread and establishes a socket
    connection with a port on the Android side so it can read the
    broadcasted data and find the encoded video frames within it.
    
    Currently, the only thing it does with the incoming encoded frames is
    print their size to console just to verify they are actually being
    received. Otherwise, there is a temporary placeholder display on the
    actual tab.
    
    Test: Built with car target & started emulator
    Test: adb forward tcp:1234 tcp:5151
    Test: Manually verify the tab and console output.
    Change-Id: I66f2df327a8d0d6f3cceb6063ab42f98d0839969

---
+++ android/android-emu/android/skin/qt/extended-pages/instr-cluster-render/renderwidget.cpp
@@ -0,0 +1,43 @@
+#include <QtGui>
+#include <math.h>
+#include "renderwidget.h"
+
+RenderWidget::RenderWidget(QWidget* parent)
+    : QWidget(parent) {
+    thread = new RenderThread(this);
+    qRegisterMetaType<QImage>("QImage");
+    connect(thread, SIGNAL(renderedImage(QImage)),
+            this, SLOT(updatePixmap(QImage)), Qt::BlockingQueuedConnection);
+    connect(this, SIGNAL(isVisible()), thread, SLOT(wakeThread()));
+    connect(this, SIGNAL(isInvisible()), thread, SLOT(exitThread()));
+}
+
+void RenderWidget::paintEvent(QPaintEvent* event) {
+    QPainter painter(this);
+    painter.fillRect(rect(), Qt::black);
+
+    if (pixmap.isNull()) {
+        painter.setPen(Qt::white);
+        painter.drawText(rect(), Qt::AlignCenter,
+                         tr("Failed to get image, possibly due to failed connection, bad frame, etc."));
+    } else {
+        painter.drawPixmap(rect(), pixmap);
+    }
+}
+
+void RenderWidget::showEvent(QShowEvent* event) {
+    emit isVisible();
+}
+
+void RenderWidget::hideEvent(QHideEvent* event) {
+    emit isInvisible();
+}
+
+void RenderWidget::updatePixmap(const QImage& image) {
+    pixmap.convertFromImage(image);
+    if (pixmap.isNull()) {
+        return;
+    }
+    pixmap = pixmap.scaled(size());
+    repaint();
+}
