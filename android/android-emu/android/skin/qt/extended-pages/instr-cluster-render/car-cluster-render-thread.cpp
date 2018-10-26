4fbb4a2b9ef24146fae38fb40f86f0cb777cb371
John <johnxiang@google.com>
2018-10-26 11:12:47 -0700

    Create widget to receive frames from android port
    When visible, the widget starts a thread and establishes a socket
    connection with a port on the Android side so it can read the
    broadcasted data and find the encoded video frames within it.
    
    Currently, the only thing it does with the incoming encoded frames is
    print their size to console just to verify they are actually being
    received. Otherwise, there is a temporary placeholder display on the
    actual tab.
    
    Test: Built with car target & started emulator
    Test: adb forward tcp:1234 tcp:5151
    Test: Manually verify the tab and console output.
    Change-Id: I66f2df327a8d0d6f3cceb6063ab42f98d0839969

---
+++ android/android-emu/android/skin/qt/extended-pages/instr-cluster-render/car-cluster-render-thread.cpp
@@ -0,0 +1,109 @@
+#include "android/skin/qt/extended-pages/instr-cluster-render/car-cluster-render-thread.h"
+#include <math.h>
+
+static constexpr char ADDR[] = "127.0.0.1";
+static constexpr int PORT = 1234;
+static constexpr int FRAME_WIDTH = 1280;
+static constexpr int FRAME_HEIGHT = 720;
+static constexpr int BUF_SIZE = 1024;
+
+CarClusterRenderThread::CarClusterRenderThread(QWidget* parent) {
+    uint32_t ip;
+    inet_strtoip(ADDR, &ip);
+    sock_address_init_inet(&serverAddr, ip, PORT);
+
+    mBuf = new char[BUF_SIZE];
+}
+
+
+CarClusterRenderThread::~CarClusterRenderThread() {
+    socket_close(mSocket);
+    delete[] mBuf;
+}
+
+void CarClusterRenderThread::startThread() {
+    exitRequested = false;
+    if (!isRunning()) {
+        this->start();
+    }
+}
+
+void CarClusterRenderThread::stopThread() {
+    exitRequested = true;
+}
+
+void CarClusterRenderThread::run() {
+    int frameSize;
+    mSocket = socket_create_inet(SOCKET_STREAM);
+    while (!exitRequested) {
+        //loops until we successfully connect
+        if (!(isConnected = tryConnect())) {
+            continue;
+        }
+
+        if ((frameSize = getNextFrame()) == -1) {
+            //close current connections, attempt to reconnect on next iteration
+            socket_close(mSocket);
+            isConnected = false;
+            mSocket = socket_create_inet(SOCKET_STREAM);
+            continue;
+        }
+
+        std::cout << "current frame size: " << frameSize << std::endl;
+
+        //TODO: decode frame to form image & render it
+        //just emitting a placeholder for now
+
+        emit renderedImage(QImage());
+        delete[] mNextFrame;
+    }
+    socket_close(mSocket);
+    isConnected = false;
+}
+
+//Returns the size of the packet read, or -1 on error, i.e. broken pipe
+int CarClusterRenderThread::readPacket() {
+    int n = socket_recv(mSocket, mBuf, BUF_SIZE);
+    if (n < 0) {
+        return -1;
+    }
+    mEncodedData.insert(mEncodedData.end(), mBuf, mBuf + n);
+    return n;
+}
+
+bool CarClusterRenderThread::tryConnect() {
+    return isConnected ||
+            socket_connect(mSocket, &serverAddr) == 0;
+}
+
+bool CarClusterRenderThread::isFrameStart(std::vector<char> encodedData, int ind) {
+    return (ind + 3) < encodedData.size() && encodedData[ind] == 0 &&
+            encodedData[ind + 1] == 0 && encodedData[ind + 2] == 0 && encodedData[ind + 3] == 1;
+}
+
+int CarClusterRenderThread::findNextFrameIndex(std::vector<char> encodedData, int startInd) {
+    for (int i = startInd; i < encodedData.size(); i++) {
+        if (isFrameStart(encodedData, i)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//Returns size of the next frame, or -1 on error, i.e. no frame
+//Contents of the next frame are stored in mNextFrame
+int CarClusterRenderThread::getNextFrame() {
+    int n;
+    int frameInd = findNextFrameIndex(mEncodedData, 4);
+    while (frameInd == -1) {
+        if ((n = readPacket()) == -1) {
+            return -1;
+        }
+        frameInd = findNextFrameIndex(mEncodedData,
+                                       std::max((unsigned long) 0, mEncodedData.size() - n - 3));
+    }
+    mNextFrame = new char[frameInd];
+    copy(mEncodedData.begin(), mEncodedData.begin() + frameInd, mNextFrame);
+    mEncodedData.erase(mEncodedData.begin(), mEncodedData.begin() + frameInd);
+    return frameInd;
+}
