a134a714859636b88c91f7ba9708605681071272
Erwin Jansen <jansene@google.com>
2018-11-01 13:51:18 -0700

    Fix envsetup to add ninja if it is not there
    Sourcing the script resulted in a wrong path, now it can be sourced
    from everywhere.
    
    Change-Id: I2e733cdf0db075a7c61beec5c2fcaba50e7e8ca5

--- android/envsetup.sh
+++ android/envsetup.sh
@@ -1,4 +1,10 @@
-# The posix way of getting the full path..
-PROGDIR="$( cd "$(dirname "$0")" ; pwd -P )"
-export ASAN_OPTIONS=`cat android/asan_overrides`
-export PATH=$PATH:$PROGDIR/third_party/chromium/depot_tools
+#!/bin/bash
+# We forcefully use bash to make sure we can find the proper location of this script
+# So we can add ninja to the path, and setup the ASAN_OPTIONS
+PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export ASAN_OPTIONS=$(cat ${PROGDIR}/asan_overrides)
+if [ $(which ninja) ] ; then
+  echo "Ninja is already on the path, no need to add it."
+else 
+  export PATH=$PATH:$PROGDIR/third_party/chromium/depot_tools
+fi
