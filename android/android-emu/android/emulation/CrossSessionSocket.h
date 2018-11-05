ab1ebf58cd7b9c70e697c745106210186718c4d6
Yahan Zhou <yahan@google.com>
2018-11-05 15:05:25 -0800

    [WIP] Refactor socket reconnection for adb after snapshot
    Change-Id: I1fb90c5b1b8fd9fcd756adb915177fcdfd7df872

---
+++ android/android-emu/android/emulation/CrossSessionSocket.h
@@ -0,0 +1,47 @@
+#include "android/base/StringView.h"
+#include "android/base/async/Looper.h"
+#include "android/base/async/ScopedSocketWatch.h"
+#include "android/base/sockets/ScopedSocket.h"
+
+#include <vector>
+
+namespace android {
+namespace emulation {
+class CrossSessionSocket {
+public:
+    CrossSessionSocket() = default;
+    CrossSessionSocket(CrossSessionSocket&& other);
+    CrossSessionSocket& operator=(CrossSessionSocket&& other);
+    CrossSessionSocket(android::base::Looper::FdWatch* fdWatcher);
+    CrossSessionSocket(android::base::Looper::FdWatch* fdWatcher,
+                            android::base::ScopedSocket&& socket);
+    void swapSocketAndClear(android::base::ScopedSocket* socket);
+    void reset();
+    android::base::Looper::FdWatch* fdWatcher();
+    const android::base::Looper::FdWatch* fdWatcher() const;
+    bool valid() const;
+    // Check if a socket should be recycled for snapshot (if yes recycle it)
+    static void recycleSocket(CrossSessionSocket&& socket);
+    static void registerForRecycle(int socket);
+    static android::base::ScopedSocket reclaimSocket(int fd);
+
+    enum class DrainBehavior {
+        Clear,
+        AppendToBuffer
+    };
+    void drainSocket(DrainBehavior drainBehavior);
+    bool hasStaleData() const;
+    size_t readStaleData(void* data, size_t dataSize);
+    size_t getReadableDataSize() const;
+    void onSave(android::base::Stream* stream);
+    void onLoad(android::base::Stream* stream);
+
+private:
+    std::unique_ptr<android::base::Looper::FdWatch> mFdWatcher;
+    android::base::ScopedSocket mSocket;
+    std::vector<uint8_t> mRecvBuffer;
+    int mRecvBufferBegin = 0;
+    int mRecvBufferEnd = 0;
+};
+}
+}
