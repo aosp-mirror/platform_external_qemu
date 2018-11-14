20e146ffb663314b6cfb3abd7f0d0d33491b2309
Lingfeng Yang <lfy@google.com>
2018-11-14 08:23:36 -0800

    [vulkan] VK_ANDROID_native_buffer host implementation
    bug: 111137294
    bug: 119426975
    
    This is a significant set of custom logic in order to implement
    VK_ANDROID_native_buffer.
    
    This takes a similar approach to chadversary@'s implementation:
    
    https://lists.freedesktop.org/archives/mesa-dev/2017-September/168351.html
    
    This CL implements most of the functionality of VK_ANDROID_native_buffer
    on the host side. The guest side is assumed to take care of waiting on
    the fence fd and closing it in vkAcquireImageANDROID.
    
    On vkQueueSignalReleaseImageANDROID, we wait for the semaphore to get
    signaled, but also put a nontrivial set of commands in there to copy to
    the associated staging buffer. That's because we're going to send it
    back out to the host's OpenGL texture.
    
    Change-Id: I32f053445a10ac4b4068e2555cfdbb549e3f8b79

---
+++ android/android-emugl/host/libs/libOpenglRender/vulkan/VkDecoderComponent.h
@@ -0,0 +1,50 @@
+// Copyright 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expresso or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "android/base/containers/Lookup.h"
+
+#include <unordered_map>
+
+// Class that defines per-Vulkan-object auxiliary data that is useful
+// for Vulkan emulation.
+// This is to organize our custom logic for Vulkan emulation in a tractable
+// manner, using Entity-Component-System style, where entities are Vulkan
+// handles, components are auxiliary data, and the system is
+// VkDecoderGlobalState.
+// Subclasses of VkDecoderComponent can then communicate with each other.
+template <class VulkanObject, class AuxiliaryData>
+class VkDecoderComponent {
+public:
+    VkDecoderComponent() = default;
+
+    void set(VulkanObject o, AuxiliaryData d) {
+        mData[o] = d;
+    }
+
+    void erase(VulkanObject o) {
+        mData.erase(o);
+    }
+
+    AuxiliaryData* get(VulkanObject o) {
+        return android::base::find(mData, o);
+    }
+
+    bool exists(VulkanObject o) const {
+        return mData.find(o) != mData.end();
+    }
+
+private:
+    std::unordered_map<VulkanObject, AuxiliaryData> mData;
+};
