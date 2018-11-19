1fbc1a28eb04b92f2f302888b4b90de2fa1c0b3f
Weilun Du <wdu@google.com>
2018-11-18 20:26:28 -0800

    [Memory Usage Test]
    1, Add GL object counter to faciliate GL object counting.
    2, Verify GL object counts before and after testcases in
    combined_unittest.
    BUG: 119495131
    
    Change-Id: Ic44c635b14f40abf77a9e9252a888ffc8a23c144
    Signed-off-by: Weilun Du <wdu@google.com>

---
+++ android/android-emugl/shared/emugl/common/gl_object_counter.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "emugl/common/gl_object_counter.h"
+
+emugl_counter_t emugl_object_count_inc;
+emugl_counter_t emugl_object_count_dec;
+
+void set_emugl_gl_object_counter(emugl_counter_struct counter) {
+    emugl_object_count_inc = counter.inc;
+    emugl_object_count_dec = counter.dec;
+}
