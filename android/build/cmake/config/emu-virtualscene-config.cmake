15a18eaf2c01942aaa1ce119805dc6edcfb24757
Erwin Jansen <jansene@google.com>
2018-12-05 22:01:50 -0800

    Enable building on Native Windows
    This contains two sets of changes:
    
    - Fixes to the build system
      Windows behaves slightly different in subtle ways. We update
      the build scripts to make sure the build can complete succesfully on
      windows
    - Fixes to unit tests
      A series of unit tests are failing on windows. We include a set of
      fixes for these failing tests.
    
    For more details on how to get started on windows read:
    
    android/docs/WINDOWS-DEV
    
    If you have a repo you can just run:
    
    C:\aosp\external\qemu> android/rebuild
    
    or if you like gradle:
    
    C:\aosp\> python tools/buildSrc/build_tools.py --help
    
    Change-Id: Ia46a91884ff241ebbd3a680a712e68eedd47153b

--- android/build/cmake/config/emu-virtualscene-config.cmake
+++ android/build/cmake/config/emu-virtualscene-config.cmake
@@ -1,6 +1,6 @@
 
 get_filename_component(PREBUILT_ROOT "${ANDROID_QEMU2_TOP_DIR}/../../prebuilts/android-emulator-build/common" ABSOLUTE)
-set(VIRTUAL_SCENE_DEPENDENCIES 
+set(VIRTUAL_SCENE_DEPENDENCIES
     ${PREBUILT_ROOT}/virtualscene/Toren1BD/Toren1BD.mtl>resources/Toren1BD.mtl;
     ${PREBUILT_ROOT}/virtualscene/Toren1BD/Toren1BD.obj>resources/Toren1BD.obj;
     ${PREBUILT_ROOT}/virtualscene/Toren1BD/Toren1BD.posters>resources/Toren1BD.posters;
