fbf528cbb460241dc21b749dfb7803a4408e1d54
Erwin Jansen <jansene@google.com>
2018-12-07 14:46:55 -0800

    Support for building on windows
    This generalizes part of the build scripts in python so we can
    start building and running the unit tests on windows.
    
    Change-Id: I2eddfe97feb3e01cdff55f425078c3adaa105f71

---
+++ android/build/python/aemu/process.py
@@ -0,0 +1,43 @@
+import os
+from Queue import Queue
+import subprocess
+from threading import Thread
+
+from absl import logging
+
+
+def _reader(pipe, queue):
+    try:
+        with pipe:
+            for line in iter(pipe.readline, b''):
+                queue.put((pipe, line[:-1]))
+    finally:
+        queue.put(None)
+
+
+def log_std_out(proc):
+    """Logs the output of the given process."""
+    q = Queue()
+    Thread(target=_reader, args=[proc.stdout, q]).start()
+    Thread(target=_reader, args=[proc.stderr, q]).start()
+    for _ in range(2):
+        for _, line in iter(q.get, None):
+            logging.info(line)
+
+
+def run(cmd, cwd=None):
+    if cwd:
+        cwd = os.path.abspath(cwd)
+
+    logging.info('Running: %s in %s', ' '.join(cmd), cwd)
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=cwd)
+
+    log_std_out(proc)
+    proc.wait()
+    if proc.returncode != 0:
+        raise Exception('Failed to run %s - %s' %
+                        (' '.join(cmd), proc.returncode))
