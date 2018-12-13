8c14861838f753e0fc663cbc3f2e8d726a96fab4
Lingfeng Yang <lfy@google.com>
2018-12-12 19:55:41 -0800

    [androidImpl] Support for more Surface queries/performs
    bug: 111137294
    bug: 119426975
    
    This is geared toward having a testing environment for
    VK_ANDROID_native_buffer support. The swapchain code in the
    framework does quite a lot more with ANW than EGL.
    
    Change-Id: I50152d4b4afc2f17932989e13b7b5be3e53c62cd

--- android/android-emugl/guest/androidImpl/AndroidWindow.cpp
+++ android/android-emugl/guest/androidImpl/AndroidWindow.cpp
@@ -15,11 +15,26 @@
 
 #include "AndroidHostCommon.h"
 
+#include "android/base/Backtrace.h"
+
+#include <hardware/gralloc.h>
 #include <errno.h>
 #include <stdio.h>
 
+#define AW_DEBUG 1
+
 #define E(fmt,...) \
-    fprintf(stderr, "%s: " fmt "\n", __func__, ##__VA_ARGS__);
+    fprintf(stderr, "%s: " fmt "\n @: %s\n", __func__, ##__VA_ARGS__, \
+            android::base::bt().c_str()); \
+
+#if AW_DEBUG
+#define D(fmt,...) \
+    fprintf(stderr, "%s: " fmt "\n @: %s\n", __func__, ##__VA_ARGS__, \
+            android::base::bt().c_str());  \
+
+#else
+#define D(fmt,...)
+#endif
 
 namespace aemu {
 
@@ -92,11 +107,95 @@ int AndroidWindow::query(int what, int* value) const {
         case NATIVE_WINDOW_HEIGHT:
             *value = height;
             break;
+        case NATIVE_WINDOW_FORMAT:
+            *value = HAL_PIXEL_FORMAT_RGBA_8888;
+            break;
+        case NATIVE_WINDOW_TRANSFORM_HINT:
+            *value = 0;
+            break;
+        case NATIVE_WINDOW_MAX_BUFFER_COUNT:
+            *value = 3;
+            break;
+        case NATIVE_WINDOW_MIN_UNDEQUEUED_BUFFERS:
+            *value = 1;
+            break;
+
+        case NATIVE_WINDOW_QUEUES_TO_WINDOW_COMPOSER:
+        case NATIVE_WINDOW_CONCRETE_TYPE:
+        case NATIVE_WINDOW_CONSUMER_RUNNING_BEHIND:
+        case NATIVE_WINDOW_BUFFER_AGE:
+        case NATIVE_WINDOW_LAST_DEQUEUE_DURATION:
+        case NATIVE_WINDOW_LAST_QUEUE_DURATION:
+        case NATIVE_WINDOW_FRAME_TIMESTAMPS_SUPPORTS_PRESENT:
+        case NATIVE_WINDOW_IS_VALID:
+        case NATIVE_WINDOW_DATASPACE:
         default:
-            E("Unknown query 0x%x, not implemented", what)
+            E("Unknown query 0x%x, not implemented.", what);
             return -EINVAL;
     }
     return 0;
+}
+
+int AndroidWindow::perform(int operation, va_list args)
+{
+    int res = 0;
+    switch (operation) {
+    case NATIVE_WINDOW_CONNECT:
+        // deprecated. must return NO_ERROR.
+        break;
+    case NATIVE_WINDOW_DISCONNECT:
+        // deprecated. must return NO_ERROR.
+        break;
+    case NATIVE_WINDOW_GET_CONSUMER_USAGE64: {
+        uint64_t* usage = va_arg(args, uint64_t*);
+        *usage =
+            GRALLOC_USAGE_HW_TEXTURE |
+            GRALLOC_USAGE_HW_RENDER;
+        break;
+    }
+    case NATIVE_WINDOW_API_CONNECT:
+    case NATIVE_WINDOW_API_DISCONNECT: {
+        break;
+    }
+    case NATIVE_WINDOW_GET_WIDE_COLOR_SUPPORT: {
+        bool* outSupport = va_arg(args, bool*);
+        *outSupport = false;
+        break;
+    }
+    case NATIVE_WINDOW_SET_USAGE:
+    case NATIVE_WINDOW_SET_CROP:
+    case NATIVE_WINDOW_SET_BUFFER_COUNT:
+    case NATIVE_WINDOW_SET_BUFFERS_GEOMETRY:
+    case NATIVE_WINDOW_SET_BUFFERS_TRANSFORM:
+    case NATIVE_WINDOW_SET_BUFFERS_STICKY_TRANSFORM:
+    case NATIVE_WINDOW_SET_BUFFERS_TIMESTAMP:
+    case NATIVE_WINDOW_SET_BUFFERS_DIMENSIONS:
+    case NATIVE_WINDOW_SET_BUFFERS_USER_DIMENSIONS:
+    case NATIVE_WINDOW_SET_BUFFERS_FORMAT:
+    case NATIVE_WINDOW_LOCK:
+    case NATIVE_WINDOW_UNLOCK_AND_POST:
+    case NATIVE_WINDOW_SET_SCALING_MODE:
+    case NATIVE_WINDOW_SET_SIDEBAND_STREAM:
+    case NATIVE_WINDOW_SET_BUFFERS_DATASPACE:
+    case NATIVE_WINDOW_SET_BUFFERS_SMPTE2086_METADATA:
+    case NATIVE_WINDOW_SET_BUFFERS_CTA861_3_METADATA:
+    // case NATIVE_WINDOW_SET_BUFFERS_HDR10_PLUS_METADATA:
+    case NATIVE_WINDOW_SET_SURFACE_DAMAGE:
+    case NATIVE_WINDOW_SET_SHARED_BUFFER_MODE:
+    case NATIVE_WINDOW_SET_AUTO_REFRESH:
+    case NATIVE_WINDOW_GET_REFRESH_CYCLE_DURATION:
+    case NATIVE_WINDOW_GET_NEXT_FRAME_ID:
+    case NATIVE_WINDOW_ENABLE_FRAME_TIMESTAMPS:
+    case NATIVE_WINDOW_GET_COMPOSITOR_TIMING:
+    case NATIVE_WINDOW_GET_FRAME_TIMESTAMPS:
+    case NATIVE_WINDOW_GET_HDR_SUPPORT:
+    case NATIVE_WINDOW_SET_USAGE64:
+
+    default:
+        E("Unknown perform 0x%x, not implemented.", operation);
+        break;
+    }
+    return res;
 }
 
 // Android native window implementation
@@ -127,8 +226,11 @@ static int hook_query(const struct ANativeWindow* window, int what, int* value) 
 }
 
 static int hook_perform(struct ANativeWindow* window, int operation, ... ) {
-    E("Not implemented");
-    return 0;
+    va_list args;
+    va_start(args, operation);
+    AndroidWindow* w = AndroidWindow::getSelf(window);
+    int result = w->perform(operation, args);
+    return result;
 }
 
 static int hook_cancelBuffer_DEPRECATED(struct ANativeWindow* window, struct ANativeWindowBuffer* buffer) {
@@ -152,11 +254,9 @@ static int hook_cancelBuffer(struct ANativeWindow* window, struct ANativeWindowB
 }
 
 static void hook_incRef(struct android_native_base_t* common) {
-    E("Not implemented");
 }
 
 static void hook_decRef(struct android_native_base_t* common) {
-    E("Not implemented");
 }
 
 } // namespace aemu
