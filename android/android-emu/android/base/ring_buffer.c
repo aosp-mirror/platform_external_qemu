7816bdaf17dcc2e68d67c4f2c879d26ad6228255
Lingfeng Yang <lfy@google.com>
2018-12-31 11:28:09 -0800

    [base] ring_buffer: lockless single producer/consumer queue
    For userspace emulator drivers
    
    Change-Id: I1dd5a6df9945f1f2f31b782913bc3800e8d50dd8

---
+++ android/android-emu/android/base/ring_buffer.c
@@ -0,0 +1,98 @@
+// Copyright 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/base/ring_buffer.h"
+
+#include <errno.h>
+#include <string.h>
+
+#define RING_BUFFER_MASK (RING_BUFFER_SIZE - 1)
+
+void ring_buffer_init(struct ring_buffer* r) {
+    r->write_pos = 0;
+    r->read_pos = 0;
+}
+
+bool ring_buffer_empty(const struct ring_buffer* r) {
+    uint32_t write_view;
+    __atomic_load(&r->write_pos, &write_view, __ATOMIC_SEQ_CST);
+    return r->read_pos == write_view;
+}
+
+static uint32_t get_ring_pos(uint32_t index) {
+    return index & RING_BUFFER_MASK;
+}
+
+bool ring_buffer_full_general(const struct ring_buffer* r, uint32_t bytes) {
+    uint32_t read_view;
+    __atomic_load(&r->read_pos, &read_view, __ATOMIC_SEQ_CST);
+    return get_ring_pos(r->write_pos) == get_ring_pos(read_view - bytes);
+}
+
+long ring_buffer_write_general(
+    struct ring_buffer* r, const void* data, uint32_t step_size, uint32_t steps) {
+    uint8_t* data_bytes = (uint8_t*)data;
+    uint32_t i;
+
+    for (i = 0; i < steps; ++i) {
+        if (ring_buffer_full_general(r, step_size)) {
+            errno = -EAGAIN;
+            return (long)i;
+        }
+
+        memcpy(
+            &r->buf[get_ring_pos(r->write_pos)],
+            data_bytes + i * step_size,
+            step_size);
+
+        __atomic_add_fetch(&r->write_pos, step_size, __ATOMIC_SEQ_CST);
+    }
+
+    errno = 0;
+    return (long)steps;
+}
+
+long ring_buffer_read_general(
+    struct ring_buffer* r, void* data, uint32_t step_size, uint32_t steps) {
+    uint8_t* data_bytes = (uint8_t*)data;
+    uint32_t i;
+
+    for (i = 0; i < steps; ++i) {
+        if (ring_buffer_empty(r)) {
+            errno = -EAGAIN;
+            return (long)i;
+        }
+
+        memcpy(
+            data_bytes + i * step_size,
+            &r->buf[get_ring_pos(r->read_pos)],
+            step_size);
+
+        __atomic_add_fetch(&r->read_pos, step_size, __ATOMIC_SEQ_CST);
+    }
+
+    errno = 0;
+    return (long)steps;
+}
+
+bool ring_buffer_full(const struct ring_buffer* r) {
+    return ring_buffer_full_general(r, 1);
+}
+
+long ring_buffer_write(struct ring_buffer* r, const void* data, uint32_t bytes) {
+    return ring_buffer_write_general(r, data, 1, bytes);
+}
+
+long ring_buffer_read(struct ring_buffer* r, void* data, uint32_t bytes) {
+    return ring_buffer_read_general(r, data, 1, bytes);
+}
---
+++ android/android-emu/android/base/ring_buffer_unittest.cpp
@@ -0,0 +1,233 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#include "android/base/ring_buffer.h"
+
+#include "android/base/threads/FunctorThread.h"
+
+#include <gtest/gtest.h>
+
+#include <random>
+
+#include <errno.h>
+
+namespace android {
+namespace base {
+
+TEST(ring_buffer, Init) {
+    ring_buffer r;
+    ring_buffer_init(&r);
+}
+
+static constexpr size_t kNumElts = 65536;
+
+// Tests that a large buffer can be produced and consumed,
+// in a single thread.
+TEST(ring_buffer, ProduceConsume) {
+    std::default_random_engine generator;
+    generator.seed(0);
+
+    std::vector<uint8_t> elements(kNumElts);
+
+    // int for toolchain compatibility
+    std::uniform_int_distribution<int>
+        eltDistribution(0, 255);
+
+    for (size_t i = 0; i < kNumElts; ++i) {
+        elements[i] =
+            static_cast<uint8_t>(
+                eltDistribution(generator));
+    }
+
+    std::vector<uint8_t> result(kNumElts);
+
+    ring_buffer r;
+    ring_buffer_init(&r);
+
+    size_t written = 0;
+    size_t read = 0;
+
+    int i = 0;
+    while (written < kNumElts) {
+        ++i;
+
+        // Safety factor; we do not expect the ring buffer
+        // implementation to be this hangy if used this way.
+        if (i > kNumElts * 10) {
+            FAIL() << "Error: too many iterations. Hanging?";
+            return;
+        }
+
+        uint32_t toWrite = kNumElts - written;
+        long writtenThisTime =
+            ring_buffer_write(&r, elements.data() + written, toWrite);
+        written += writtenThisTime;
+
+        if (writtenThisTime < toWrite) {
+            EXPECT_EQ(-EAGAIN, errno);
+        }
+
+        uint32_t toRead = kNumElts - read;
+        long readThisTime =
+            ring_buffer_read(&r, result.data() + read, toRead);
+        read += readThisTime;
+
+        if (readThisTime < toRead) {
+            EXPECT_EQ(-EAGAIN, errno);
+        }
+    }
+
+    EXPECT_EQ(elements, result);
+}
+
+// General function to pass to FunctorThread to read/write
+// data completely to/from a ring buffer.
+static void writeTest(ring_buffer* r, const uint8_t* data, size_t stepSize, size_t numSteps) {
+    size_t stepsWritten = 0;
+    size_t bytes = stepSize * numSteps;
+    int i = 0;
+    while (stepsWritten < numSteps) {
+        ++i;
+
+        // Safety factor; we do not expect the ring buffer
+        // implementation to be this hangy if used this way.
+        if (i > bytes * 10) {
+            FAIL() << "Error: too many iterations. Hanging?";
+            return;
+        }
+
+        uint32_t stepsRemaining = numSteps - stepsWritten;
+        long stepsWrittenThisTime =
+            ring_buffer_write_general(r,
+                data + stepSize * stepsWritten,
+                stepSize, stepsRemaining);
+        stepsWritten += stepsWrittenThisTime;
+
+        if (stepsWrittenThisTime < stepsRemaining) {
+            EXPECT_EQ(-EAGAIN, errno);
+        }
+    }
+}
+
+static void readTest(ring_buffer* r, uint8_t* data, size_t stepSize, size_t numSteps) {
+    size_t stepsRead = 0;
+    size_t bytes = stepSize * numSteps;
+    int i = 0;
+    while (stepsRead < numSteps) {
+        ++i;
+
+        // Safety factor; we do not expect the ring buffer
+        // implementation to be this hangy if used this way.
+        if (i > bytes * 10) {
+            FAIL() << "Error: too many iterations. Hanging?";
+            return;
+        }
+
+        uint32_t stepsRemaining = numSteps - stepsRead;
+        long stepsReadThisTime =
+            ring_buffer_read_general(r,
+                data + stepSize * stepsRead,
+                stepSize, stepsRemaining);
+        stepsRead += stepsReadThisTime;
+
+        if (stepsReadThisTime < stepsRemaining) {
+            EXPECT_EQ(-EAGAIN, errno);
+        }
+    }
+}
+
+// Tests transmission of a large buffer where
+// the producer is in one thread
+// while the consumer is in another thread.
+TEST(ring_buffer, ProduceConsumeMultiThread) {
+    std::default_random_engine generator;
+    generator.seed(0);
+
+    std::vector<uint8_t> elements(kNumElts);
+
+    // int for toolchain compatibility
+    std::uniform_int_distribution<int>
+        eltDistribution(0, 255);
+
+    for (size_t i = 0; i < kNumElts; ++i) {
+        elements[i] =
+            static_cast<uint8_t>(
+                eltDistribution(generator));
+    }
+
+    std::vector<uint8_t> result(kNumElts, 0);
+
+    ring_buffer r;
+    ring_buffer_init(&r);
+
+    FunctorThread producer([&r, &elements]() {
+        writeTest(&r, (uint8_t*)elements.data(), 1, kNumElts);
+    });
+
+    FunctorThread consumer([&r, &result]() {
+        readTest(&r, (uint8_t*)result.data(), 1, kNumElts);
+    });
+
+    producer.start();
+    consumer.start();
+
+    consumer.wait();
+
+    EXPECT_EQ(elements, result);
+}
+
+// Tests various step sizes of ring buffer transmission.
+TEST(ring_buffer, ProduceConsumeMultiThreadVaryingStepSize) {
+    std::default_random_engine generator;
+    generator.seed(0);
+
+    std::vector<uint8_t> elements(kNumElts);
+
+    // int for toolchain compatibility
+    std::uniform_int_distribution<int>
+        eltDistribution(0, 255);
+
+    for (size_t i = 0; i < kNumElts; ++i) {
+        elements[i] =
+            static_cast<uint8_t>(
+                eltDistribution(generator));
+    }
+
+    static constexpr size_t kStepSizesToTest[] = {
+        1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+    };
+
+    for (auto stepSize : kStepSizesToTest) {
+        size_t numSteps = kNumElts / stepSize;
+
+        std::vector<uint8_t> result(kNumElts, 0);
+
+        ring_buffer r;
+        ring_buffer_init(&r);
+
+        FunctorThread producer([&r, &elements, stepSize, numSteps]() {
+            writeTest(&r, (uint8_t*)elements.data(), stepSize, numSteps);
+        });
+
+        FunctorThread consumer([&r, &result, stepSize, numSteps]() {
+            readTest(&r, (uint8_t*)result.data(), stepSize, numSteps);
+        });
+
+        producer.start();
+        consumer.start();
+
+        consumer.wait();
+
+        EXPECT_EQ(elements, result);
+    }
+}
+
+} // namespace android
+} // namespace base
