5f91df87b478d57c3908c5fa32737900cde5dc72
Tiantian Bian <tianbian@google.com>
2019-01-17 11:13:34 -0800

    Extend OffWorldPipe to accept command for video injection
    Bug: 122529899
    
    This CL add video injection command support in the offworld service.
    Design doc: go/lens-emu-offworld
    
    - Define the proto for video injection.
    - Exposes a static method tryGetNextVideoInjectionCommand for video
    playback camera.
    - Use the offworld pipe to communicate with test running on guest os.
    
    Change-Id: I9984b871eefbcd719b01387df638908954ec417c

---
+++ android/android-emu/android/automation/VideoInjectionController.h
@@ -0,0 +1,72 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/automation/proto/video_injection.pb.h"
+#include "android/base/Result.h"
+#include "android/emulation/AndroidAsyncMessagePipe.h"
+
+// Forward declarations.
+namespace offworld {
+class Response;
+}  // namespace offworld
+
+namespace android {
+namespace automation {
+
+namespace pb = emulator_automation;
+
+enum class VideoInjectionError {
+    InvalidFilename,
+    FileOpenError,
+    InvalidCommand,
+    FileIsNotLoaded,
+    AnotherVideoIsPlaying,
+    VideoIsNotStarted,
+    InternalError,
+};
+
+using VideoInjectionResult = base::Result<void, VideoInjectionError>;
+std::ostream& operator<<(std::ostream& os, const VideoInjectionError& value);
+
+//
+// Receives automation events from other emulator subsystems so that they may
+// be recorded or streamed.
+//
+
+class VideoInjectionController {
+DISALLOW_COPY_AND_ASSIGN(VideoInjectionController);
+public:
+
+    virtual ~VideoInjectionController() = default;
+
+    virtual VideoInjectionResult addCommand(
+        android::AsyncMessagePipeHandle pipe,
+        android::base::StringView command,
+        uint32_t asyncId) = 0;
+
+    virtual std::unique_ptr<pb::VideoInjectionCommand> getNextCommand(
+        VideoInjectionResult previousResult) = 0;
+
+    static std::unique_ptr<VideoInjectionController> create(
+        std::function<void(android::AsyncMessagePipeHandle,
+                           const ::offworld::Response&)> sendMessageCallback);
+
+protected:
+    VideoInjectionController() = default;
+};
+
+}  // namespace automation
+}  // namespace android
