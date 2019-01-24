e36a82a7f8444e9ce49df2c97072e5ffff8a9e5e
Erwin Jansen <jansene@google.com>
2019-01-24 07:36:02 -0800

    Enable symbol processing and stripping on all platforms.
    In order to process symbols on MSVC we need to build a version of
    breakpad that can extract symbols from pdbs that are generated during
    the build.
    
    We now make breakpad part of the build so we can:
    
    - Always execute breakpad in the right windows environment. Breakpad
    relies on a specific set of dlls to be available on the windows machine.
    By building breakpad ourselves we are guaranteed that all the
    dependencies are met. (Default breakpad executables require VS 2013
    Express) - We now run all the breakpad unit tests.
    - We now process symbols (and strip so/dll/exe) during the cmake install phase.
      It works as follows:
        - Breakpad dumps the original binary
        - We use platform recommended way of stripping
        - We upload the symbols during install (note that this is slow!)
    - We only upload our symbols, no need to upload prebuilt modules.
    - We will be up-to-date with the latest chrome breakpad patches.
    - Move gtest to a separate folder, so breakpad can be built exclusively
    
    Bug: b/121210176
    Change-Id: I079a2ff80edd31add197e0c35071a2d16edbe83c

--- android/build/cmake/emu-windows-libs.cmake
+++ android/build/cmake/emu-windows-libs.cmake
@@ -14,11 +14,70 @@ function(android_find_windows_library NAME)
     message(STATUS "Discovering ${NAME} : ${${NAME}_LIBRARIES}")
     add_library(${NAME}::${NAME} INTERFACE IMPORTED GLOBAL)
     set_target_properties(${NAME}::${NAME} PROPERTIES INTERFACE_LINK_LIBRARIES ${${NAME}_LIBRARIES})
-    get_target_property(VAR ${NAME}::${NAME} INTERFACE_LINK_LIBRARIES)
   endif()
 endfunction()
 
-set(WINDOWS_LIBS "ws2_32;d3d9;ole32;psapi;iphlpapi;wldap32;shell32;user32;advapi32;mfuuid;winmm;shlwapi;gdi32;dxguid")
+set(WINDOWS_LIBS
+    ws2_32
+    d3d9
+    ole32
+    psapi
+    iphlpapi
+    wldap32
+    shell32
+    user32
+    advapi32
+    mfuuid
+    winmm
+    shlwapi
+    gdi32
+    dxguid
+    wininet
+    diaguids
+    dbghelp
+    imagehlp
+    mincore)
 foreach(LIB ${WINDOWS_LIBS})
   android_find_windows_library(${LIB})
 endforeach()
+
+if(WIN32 AND MSVC)
+  # Find the DIA SDK path, it will typically look something like this. C:\Program Files (x86)\Microsoft Visual
+  # Studio\2017\Enterprise\DIA SDK\include C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include\dia2.h
+  # Find the DIA SDK path, as we need to include it.
+  get_filename_component(VS_PATH32 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0;InstallDir]" ABSOLUTE
+                         CACHE)
+  get_filename_component(VS_PATH64
+                         "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\14.0;InstallDir]"
+                         ABSOLUTE CACHE)
+  # VS_PATH32 will be something like C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE
+
+  # Also look for in vs15 install.
+  set(PROGRAMFILES_X86 "ProgramFiles(x86)")
+  get_filename_component(VS15_C_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Community/Common7/IDE"
+                         ABSOLUTE CACHE)
+  get_filename_component(VS15_P_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Professional/Common7/IDE"
+                         ABSOLUTE CACHE)
+  get_filename_component(VS15_E_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Enterprise/Common7/IDE"
+                         ABSOLUTE CACHE)
+
+  # Find the DIA SDK path, it will typically look something like this. C:\Program Files (x86)\Microsoft Visual
+  # Studio\2017\Enterprise\DIA SDK\include C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include\dia2.h
+  find_path(DIASDK_INCLUDE_DIR # Set variable DIASDK_INCLUDE_DIR
+            dia2.h # Find a path with dia2.h
+            HINTS "${VS15_C_PATH32}/../../DIA SDK/include"
+            HINTS "${VS15_P_PATH32}/../../DIA SDK/include"
+            HINTS "${VS15_E_PATH32}/../../DIA SDK/include"
+            HINTS "${VS_PATH64}/../../DIA SDK/include"
+            HINTS "${VS_PATH32}/../../DIA SDK/include"
+            DOC "path to DIA SDK header files")
+  message(STATUS "Found DIA SDK Include: ${DIASDK_INCLUDE_DIR}")
+
+  find_library(DIASDK_GUIDS_LIBRARY NAMES diaguids.lib HINTS ${DIASDK_INCLUDE_DIR}/../lib/amd64)
+
+  # add_library(${NAME}::${NAME} INTERFACE IMPORTED GLOBAL)
+  set_target_properties(diaguids::diaguids
+                        PROPERTIES INTERFACE_LINK_LIBRARIES ${DIASDK_GUIDS_LIBRARY} INTERFACE_INCLUDE_DIRECTORIES
+                                   ${DIASDK_INCLUDE_DIR})
+
+endif()
