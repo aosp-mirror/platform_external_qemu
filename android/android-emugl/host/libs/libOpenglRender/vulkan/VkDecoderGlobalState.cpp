39bccb842d288411e3a8029603b60b6494bd491c
Lingfeng Yang <lfy@google.com>
2019-01-25 09:22:14 -0800

    [vulkan] Add external memory emulation state
    bug: 111137294
    bug: 122080810
    Change-Id: I424a3a75f6d92e263334bb2736c53322245a11bb

--- android/android-emugl/host/libs/libOpenglRender/vulkan/VkDecoderGlobalState.cpp
+++ android/android-emugl/host/libs/libOpenglRender/vulkan/VkDecoderGlobalState.cpp
@@ -48,7 +48,9 @@ kEmulatedExtensions[] = {
 
 class VkDecoderGlobalState::Impl {
 public:
-    Impl() : m_vk(emugl::vkDispatch()) { }
+    Impl() :
+        m_vk(emugl::vkDispatch()),
+        m_emu(createOrGetGlobalVkEmulation(m_vk)) { }
     ~Impl() = default;
 
     // A list of extensions that should not be passed to the host driver.
@@ -999,6 +1001,7 @@ private:
     }
 
     VulkanDispatch* m_vk;
+    VkEmulation* m_emu;
 
     Lock mLock;
 
