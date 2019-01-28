cb3732c4d80ecc1d47f5bffc3e26417f2b45f5a1
Joshua Duong <joshuaduong@google.com>
2019-01-28 15:35:37 -0800

    Remove prebuilt zlib. We build zlib from source now.
    Change-Id: Ib5e9f01cadf4e2624b180a61e07c4def127e4ba3

--- android/android-emu/android-emu.cmake
+++ android/android-emu/android-emu.cmake
@@ -336,7 +336,7 @@ target_link_libraries(android-emu
                               LibXml2::LibXml2
                               PNG::PNG
                               lz4
-                              ZLIB::ZLIB
+                              zlib
 )
 
 # Here are the windows library and link dependencies. They are public and will propagate onwards to others that depend
@@ -466,7 +466,7 @@ target_link_libraries(android-emu-shared
                               LibXml2::LibXml2
                               PNG::PNG
                               lz4
-                              ZLIB::ZLIB)
+                              zlib)
 
 # Here are the windows library and link dependencies. They are public and will propagate onwards to others that depend
 # on android-emu-shared
@@ -547,11 +547,9 @@ target_include_directories(android-mock-vm-operations
                                    ${LIBXML2_INCLUDE_DIRS}
                                    ${LIBPNG_INCLUDE_DIRS}
                                    ${LZ4_INCLUDE_DIRS}
-                                   ${ZLIB_INCLUDE_DIRS}
                                    ${PROTOBUF_INCLUDE_DIRS}
                                    ${LZ4_INCLUDE_DIRS}
                                    ${PNG_INCLUDE_DIRS}
-                                   ${ZLIB_INCLUDE_DIRS}
                                    ${DARWINN_INCLUDE_DIRS})
 target_link_libraries(android-mock-vm-operations PRIVATE gmock)
 
@@ -759,11 +757,9 @@ target_include_directories(android-emu_unittests
                                    ${LIBXML2_INCLUDE_DIRS}
                                    ${LIBPNG_INCLUDE_DIRS}
                                    ${LZ4_INCLUDE_DIRS}
-                                   ${ZLIB_INCLUDE_DIRS}
                                    ${PROTOBUF_INCLUDE_DIRS}
                                    ${LZ4_INCLUDE_DIRS}
                                    ${PNG_INCLUDE_DIRS}
-                                   ${ZLIB_INCLUDE_DIRS}
                                    ${DARWINN_INCLUDE_DIRS})
 
 target_compile_definitions(android-emu_unittests PRIVATE -DGTEST_HAS_RTTI=0)
