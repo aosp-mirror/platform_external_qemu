e844928c735e3a626a96295c203383599de97c49
Joshua Duong <joshuaduong@google.com>
2019-02-19 11:16:53 -0800

    Revert "[DO NOT MERGE] Switch linker to lld and objcopy to llvm-objcopy."
    This reverts commit b24e667d688adc89378269a3c56786108898dd4b.

--- android/scripts/unix/gen-android-sdk-toolchain.sh
+++ android/scripts/unix/gen-android-sdk-toolchain.sh
@@ -215,10 +215,6 @@ gen_wrapper_program ()
                 DST_PROG=llvm-ar
                 DST_PREFIX=$CLANG_BINDIR/
                 ;;
-            objcopy)
-                DST_PROG=llvm-objcopy
-                DST_PREFIX=$CLANG_BINDIR/
-                ;;
         esac
     fi
 
@@ -279,9 +275,9 @@ gen_dbg_splitter() {
 $EXTRA_ENV_SETUP
 target=\$(basename \$1)
 mkdir -p build/debug_info
-objcopy --only-keep-debug  "\$1" "build/debug_info/\$target.debug"
-objcopy --strip-unneeded  "\$1"
-objcopy --add-gnu-debuglink="build/debug_info/\$target.debug" "\$1"
+${DST_PREFIX}objcopy --only-keep-debug  "\$1" "build/debug_info/\$target.debug"
+${DST_PREFIX}objcopy --strip-unneeded  "\$1"
+${DST_PREFIX}objcopy --add-gnu-debuglink="build/debug_info/\$target.debug" "\$1"
 
 EOF
  chmod +x "$DST_FILE"
@@ -365,7 +361,7 @@ gen_wrapper_toolchain () {
     done
 
     for PROG in $PROGRAMS; do
-        gen_wrapper_program $PROG "$SRC_PREFIX" "$DST_PREFIX" "$DST_DIR" "$CLANG_BINDIR"
+        gen_wrapper_program $PROG "$SRC_PREFIX" "$DST_PREFIX" "$DST_DIR"
     done
 
     # Setup additional host specific things
@@ -463,7 +459,7 @@ prepare_build_for_linux_x86_64() {
     var_append GCC_LINK_FLAGS "-B$PREBUILT_TOOLCHAIN_DIR/lib/gcc/x86_64-linux/4.8.3/"
     var_append GCC_LINK_FLAGS "-L$PREBUILT_TOOLCHAIN_DIR/lib/gcc/x86_64-linux/4.8.3/"
     var_append GCC_LINK_FLAGS "-L$PREBUILT_TOOLCHAIN_DIR/x86_64-linux/lib64/"
-    var_append GCC_LINK_FLAGS "-fuse-ld=lld"
+    var_append GCC_LINK_FLAGS "-fuse-ld=${PREBUILT_TOOLCHAIN_DIR}/bin/x86_64-linux-ld"
     var_append GCC_LINK_FLAGS "-L${CLANG_BINDIR}/../lib64/clang/${CLANG_VERSION}/lib/linux/"
     var_append GCC_LINK_FLAGS "-L${CLANG_BINDIR}/../lib64/"
     var_append GCC_LINK_FLAGS "--sysroot=${SYSROOT}"
