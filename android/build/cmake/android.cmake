b65efefbe0cd02c70d3a48f6daea332ea6abc0bd
Lingfeng Yang <lfy@google.com>
2019-02-21 06:25:45 -0800

    wip: min-config build
    bug: 124524885
    
    Change-Id: I648072363436e70551879391b4f819c6dfeaab3a

--- android/build/cmake/android.cmake
+++ android/build/cmake/android.cmake
@@ -517,6 +517,48 @@ function(android_add_qemu_executable ANDROID_AARCH QEMU_AARCH CONFIG_AARCH STUBS
   android_install_exe(qemu-system-${ANDROID_AARCH} "./qemu/${ANDROID_TARGET_OS_FLAVOR}-x86_64")
 endfunction()
 
+function(android_add_qemu_min_config_executable ANDROID_AARCH QEMU_AARCH CONFIG_AARCH STUBS CPU)
+  # Workaround b/121393952, older cmake does not have proper object archives
+  if (NOT MSVC)
+    android_complete_archive(QEMU_COMPLETE_LIB "qemu2-common")
+  endif()
+  add_executable(qemu-system-${ANDROID_AARCH}-min
+                 android-qemu2-glue/main.cpp
+                 vl.c
+                 ${STUBS}
+                 ${qemu-system-${QEMU_AARCH}_sources}
+                 ${qemu-system-${QEMU_AARCH}_generated_sources})
+  target_include_directories(qemu-system-${ANDROID_AARCH}-min
+                             PRIVATE android-qemu2-glue/config/target-${CONFIG_AARCH} target/${CPU})
+  target_compile_definitions(qemu-system-${ANDROID_AARCH}-min
+                             PRIVATE
+                             -DNEED_CPU_H
+                             -DCONFIG_ANDROID
+                             -DCONFIG_MIN_QEMU
+                             -DANDROID_SDK_TOOLS_REVISION=${OPTION_SDK_TOOLS_REVISION}
+                             -DANDROID_SDK_TOOLS_BUILD_NUMBER=${OPTION_SDK_TOOLS_BUILD_NUMBER})
+  target_link_libraries(qemu-system-${ANDROID_AARCH}-min
+                        PRIVATE android-qemu-deps
+                                ${QEMU_COMPLETE_LIB}
+                                libqemu2-glue
+                                libqemu2-util
+                                android-emu
+                                android-qemu-deps)
+  if(MSVC)
+    # Workaround b/121393952, msvc linker does not have notion of whole-archive. so we need to use the general approach
+    # supported by newer cmake versions
+    target_link_libraries(qemu-system-${ANDROID_AARCH}-min PRIVATE $<TARGET_OBJECTS:qemu2-common>)
+    set_target_properties(qemu-system-${ANDROID_AARCH}-min PROPERTIES LINK_FLAGS "/FORCE:multiple /NODEFAULTLIB:LIBCMT")
+  endif()
+  # Make the common dependency explicit, as some generators might not detect it properly (Xcode)
+  add_dependencies(qemu-system-${ANDROID_AARCH}-min qemu2-common)
+  # XCode bin places this not where we want this...
+  set_target_properties(qemu-system-${ANDROID_AARCH}-min
+                        PROPERTIES RUNTIME_OUTPUT_DIRECTORY
+                                   "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/qemu/${ANDROID_TARGET_OS_FLAVOR}-x86_64")
+  android_install_exe(qemu-system-${ANDROID_AARCH} "./qemu/${ANDROID_TARGET_OS_FLAVOR}-x86_64")
+endfunction()
+
 # Copies a
 function(android_copy_shared_lib TGT SHARED_LIB NAME)
   android_copy_file(${TGT} $<TARGET_FILE:${SHARED_LIB}>
