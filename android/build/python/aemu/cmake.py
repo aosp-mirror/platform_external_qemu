a5823f47ebaa51aec625714f650aade886094aec
Erwin Jansen <jansene@google.com>
2019-02-26 12:54:10 -0800

    Remove duplicate flags
    All of the flags are implied by crash flag.
    
    crash prod -->  symbols available on prod server --> symbols generated
    crash staging-- > symbols availabe on staging --> symbols generated
    crash none -->  no uploading of symbols --> No need to generate symbols.
    
    Bug:b/126384972
    Change-Id: Ibee9ae42336b4ae4ea95dacff4cd5c7a5c224a6c

--- android/build/python/aemu/cmake.py
+++ android/build/python/aemu/cmake.py
@@ -33,8 +33,6 @@ from aemu.definitions import Generator, Crash, BuildConfig, SymbolUris, Toolchai
 from aemu.run_tests import run_tests
 
 FLAGS = flags.FLAGS
-flags.DEFINE_enum('symbol_dest', 'none', SymbolUris.values(),
-                  'Endpoint to send the symbols to.')
 flags.DEFINE_string('sdk_revision', None, 'The emulator sdk revision.')
 flags.DEFINE_string('sdk_build_number', None, 'The emulator sdk build number.')
 flags.DEFINE_enum('config', 'release', BuildConfig.values(),
@@ -61,7 +59,6 @@ flags.DEFINE_boolean(
     'clean', True, 'Clean the destination build directory before configuring. '
     'Setting this to false will attempt an incremental build. '
     'Note that this can introduce cmake caching issues.')
-flags.DEFINE_boolean('symbols', False, 'Strip binaries and generate symbols after build.')
 
 def configure():
     """Configures the cmake project."""
@@ -80,7 +77,7 @@ def configure():
     # Setup the right toolchain/compiler configuration.
     cmake_cmd += Toolchain.from_string(FLAGS.target).to_cmd()
     cmake_cmd += Crash.from_string(FLAGS.crash).to_cmd()
-    cmake_cmd += SymbolUris.from_string(FLAGS.symbol_dest).to_cmd()
+    cmake_cmd += SymbolUris.from_string(FLAGS.crash).to_cmd()
     cmake_cmd += BuildConfig.from_string(FLAGS.config).to_cmd()
     cmake_cmd += ['-DQTWEBENGINE=%s' % FLAGS.qtwebengine]
     if FLAGS.sdk_revision:
@@ -104,7 +101,7 @@ def get_build_cmd():
     '''Gets the command that will build all the sources.'''
     target = 'install'
     # Stripping is meaning less in windows world.
-    if FLAGS.symbols and platform.system().lower() != 'windows':
+    if FLAGS.crash != 'none' and platform.system().lower() != 'windows':
         target += '/strip'
     return [get_cmake(), '--build', FLAGS.out, '--target', target]
 
