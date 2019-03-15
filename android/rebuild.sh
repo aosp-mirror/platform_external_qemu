bf9c8cae395b53b8eace08cef59846d444469960
Erwin Jansen <jansene@google.com>
2019-03-15 16:40:23 -0700

    Use symbol server api V1
    This enables uploading of symbols using the new symbol server api. In
    order to use the new api you will need to:
    
    - Activate the symbol server api and obtain a key:
      "APIs & Services" -> "Library", then in the search box put "Crash
      Symbol Collector API" and hit search. You should see one just named
      "Crash Symbol Collector API" and another with "(Staging)" prefix.
      Choose whichever you want to use (in the below instructions we use the
      prod one) and click "Enable".
    - Next go to "APIs & Services" -> "Credentials" and click "Create
    credentials". Follow the directions, when you get back to the
    "Credentials" UI you will be able to copy the "Key" value for your new
    credentials. This is your API key.
    
    Make this key available in ~/.emulator_symbol_server_key
    
    Change-Id: Ica00bae15e96e4b7e88fc1f5213882513476cf79
    Bug: b/126949479

--- android/rebuild.sh
+++ android/rebuild.sh
@@ -37,7 +37,7 @@ install_deps() {
 
 exists python || panic "No python interpreter available, please install python!"
 install_deps absl absl-py
-install_deps urlfetch urlfetch
+install_deps requests requests
 install_deps enum enum34
 
 python $PROGDIR/build/python/cmake.py --noshowprefixforinfo  $*
