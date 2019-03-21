9eba902d62f265f7b59be68dc9de6d02b7318309
Erwin Jansen <jansene@google.com>
2019-03-21 15:58:04 -0700

    Fix msvc build break
    A set of openssl symbols are missing from the windows msvc prebuilts.
    For the msvc build we will rely on boringssl.
    
    Bug: b/129081460
    Change-Id: I5b476166a60baf99752c8741f80d96b39fd4a3ac

--- android/build/cmake/config/emu-openssl-config.cmake
+++ android/build/cmake/config/emu-openssl-config.cmake
@@ -9,29 +9,43 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-
 get_filename_component(PREBUILT_ROOT
-
-"${ANDROID_QEMU2_TOP_DIR}/../../prebuilts/android-emulator-build/curl/${ANDROID_TARGET_TAG}"
+                       "${ANDROID_QEMU2_TOP_DIR}/../../prebuilts/android-emulator-build/curl/${ANDROID_TARGET_TAG}"
                        ABSOLUTE)
-                       
+
 set(OPENSSL_INCLUDE_DIR "${PREBUILT_ROOT}/include")
 set(OPENSSL_INCLUDE_DIRS "${PREBUILT_ROOT}/include")
-set(OPENSSL_LIBRARIES "${PREBUILT_ROOT}/lib/libssl${CMAKE_STATIC_LIBRARY_SUFFIX};${PREBUILT_ROOT}/lib/libcrypto${CMAKE_STATIC_LIBRARY_SUFFIX}")
+set(
+  OPENSSL_LIBRARIES
+  "${PREBUILT_ROOT}/lib/libssl${CMAKE_STATIC_LIBRARY_SUFFIX};${PREBUILT_ROOT}/lib/libcrypto${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
 set(OPENSSL_FOUND TRUE)
 set(OPENSSL_VERSION "1.0.2j")
 
-android_add_prebuilt_library(OpenSSL SSL "${PREBUILT_ROOT}/lib/libssl" "${PREBUILT_ROOT}/include" "" "")
-
-if(ANDROID_TARGET_TAG MATCHES "windows.*" AND NOT TARGET OpenSSL::Crypto)
-   prebuilt(GLIB2)
-    add_library(OpenSSL::Crypto INTERFACE IMPORTED GLOBAL)
-        set_target_properties(OpenSSL::Crypto PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${OPENSSL_INCLUDE_DIRS}"
-        INTERFACE_LINK_LIBRARIES "${PREBUILT_ROOT}/lib/libcrypto${CMAKE_STATIC_LIBRARY_SUFFIX};ws2_32::ws2_32;GLIB2::GLIB2"
-    )
+if(ANDROID_TARGET_TAG MATCHES "windows.*" AND NOT TARGET crypto)
+  prebuilt(GLIB2)
+  add_library(crypto INTERFACE IMPORTED GLOBAL)
+  set_target_properties(
+    crypto
+    PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${OPENSSL_INCLUDE_DIRS}" INTERFACE_LINK_LIBRARIES
+               "${PREBUILT_ROOT}/lib/libcrypto${CMAKE_STATIC_LIBRARY_SUFFIX};ws2_32::ws2_32;GLIB2::GLIB2")
 else()
-  android_add_prebuilt_library(OpenSSL Crypto "${PREBUILT_ROOT}/lib/libcrypto" "${PREBUILT_ROOT}/include" "" "")
+  add_library(crypto STATIC IMPORTED GLOBAL)
+  set_target_properties(crypto
+                        PROPERTIES IMPORTED_LOCATION "${PREBUILT_ROOT}/lib/libcrypto${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                                   INTERFACE_INCLUDE_DIRECTORIES "${PREBUILT_ROOT}/include")
+
+endif()
+
+if(NOT TARGET ssl)
+  add_library(ssl STATIC IMPORTED GLOBAL)
+  set_target_properties(ssl
+                        PROPERTIES IMPORTED_LOCATION
+                                   "${PREBUILT_ROOT}/lib/libssl${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                                   INTERFACE_LINK_LIBRARIES
+                                   "crypto"
+                                   INTERFACE_INCLUDE_DIRECTORIES
+                                   "${PREBUILT_ROOT}/include")
 endif()
 
 set(PACKAGE_EXPORT "OPENSSL_INCLUDE_DIR;OPENSSL_INCLUDE_DIRS;OPENSSL_LIBRARIES;OPENSSL_FOUND;OPENSSL_VERSION")
