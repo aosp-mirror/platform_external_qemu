a0790d00615aad5d0c192e206c96809d70c0aea1
Calvin Huang <calhuang@google.com>
2019-03-20 20:16:37 -0700

    Show Stand alone Car Cluster window after connection is set, Add a thread to build car cluster service connection, remove the instrument cluster in Car data
    Test: Manual, start the emulator, go to car data -> instrument cluster
    Test: Car cluster will work after android completely start.
    Test: Close emulator, restart without -no-snapshot, go to car cluster,
    Test: It should work without extra steps
    Bug:129015752
    Change-Id: I858ca1425d3e696a2fa423c3871fba5faf35465a

--- android/android-emu/android-libui.cmake
+++ android/android-emu/android-libui.cmake
@@ -57,6 +57,7 @@ set(ANDROID_LIBUI_SRC_FILES
     android/skin/window.c
     android/skin/resource.c
     android/skin/ui.c
+    android/skin/qt/car-cluster-window.cpp
     android/skin/qt/event-qt.cpp
     android/skin/qt/init-qt.cpp
     android/skin/qt/surface-qt.cpp
@@ -81,6 +82,7 @@ set(ANDROID_LIBUI_SRC_FILES
     android/skin/qt/extended-pages/bug-report-page.cpp
     android/skin/qt/extended-pages/camera-page.cpp
     android/skin/qt/extended-pages/camera-virtualscene-subpage.cpp
+    android/skin/qt/extended-pages/car-cluster-connector/car-cluster-connector.cpp
     android/skin/qt/extended-pages/car-data-emulation/car-property-table.cpp
     android/skin/qt/extended-pages/car-data-emulation/car-property-utils.cpp
     android/skin/qt/extended-pages/car-data-emulation/car-sensor-data.cpp
