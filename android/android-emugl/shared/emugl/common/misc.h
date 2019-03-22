b111c0d9fedc04cf001d245353b1460d3b7f6b71
Weilun Du <wdu@google.com>
2019-03-21 17:41:02 -0700

    [Refactoring] GL Object Counter
    Instead of passing two function pointers into libOpenglLibrary, pass in a
    pointer to class. This approach enables libOpenglLibrary to invoke any
    public functions in GLObjectCounter class.
    
    BUG: 119495131
    
    Change-Id: I2b1f1710491b33dc8ba8d407e83aba713c99f033
    Signed-off-by: Weilun Du <wdu@google.com>

--- android/android-emugl/shared/emugl/common/misc.h
+++ android/android-emugl/shared/emugl/common/misc.h
@@ -27,10 +27,12 @@
 #endif
 
 namespace android {
+
 namespace base {
 
 class CpuUsage;
 class MemoryTracker;
+class GLObjectCounter;
 
 } // namespace base
 } // namespace android
@@ -52,6 +54,11 @@ namespace emugl {
     // Extension string query
     EMUGL_COMMON_API bool hasExtension(const char* extensionsStr,
                       const char* wantedExtension);
+
+    // GL object counter get/set
+    EMUGL_COMMON_API void setGLObjectCounter(
+            android::base::GLObjectCounter* counter);
+    EMUGL_COMMON_API android::base::GLObjectCounter* getGLObjectCounter();
 
     // CPU usage get/set.
     EMUGL_COMMON_API void setCpuUsage(android::base::CpuUsage* usage);
