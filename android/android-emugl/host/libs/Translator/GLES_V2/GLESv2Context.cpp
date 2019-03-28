e84a90b21752d046ceb4ef8112553a4efa2f03ed
Yahan Zhou <yahan@google.com>
2019-03-28 16:38:41 -0700

    Advertise BGRA format in GLES2 if supported
    BUG: 127995849
    
    Change-Id: I48702affc8817cfd16f7f86c695d3b468cc17c5b

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
@@ -737,6 +737,10 @@ void GLESv2Context::initExtensionString() {
         *s_glExtensions+="GL_OES_rgb8_rgba8 ";
     if (s_glSupport.ext_GL_EXT_color_buffer_float)
         *s_glExtensions+="GL_EXT_color_buffer_float ";
+    if (s_glSupport.GL_EXT_TEXTURE_FORMAT_BGRA8888) {
+        *s_glExtensions+="GL_EXT_texture_format_BGRA8888 GL_APPLE_texture_format_BGRA8888 ";
+    }
+
 }
 
 int GLESv2Context::getMaxTexUnits() {
