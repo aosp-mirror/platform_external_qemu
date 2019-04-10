def437f7912851def00b0815aab05c7629122670
Yahan Zhou <yahan@google.com>
2019-04-10 11:39:00 -0700

    [WIP] Use compute shader to descompress textures
    Change-Id: I45c4beee859401efde795a7f14b18cbb6b4068ad

---
+++ android/scripts/build-shaders.py
@@ -0,0 +1,32 @@
+import os
+import platform
+import shutil
+import tempfile
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+qemu_path = os.path.join(script_path, "..", "..")
+prebuilts_path = os.path.join(qemu_path, "..", "..", "prebuilts")
+
+if platform.system() == "Linux":
+    platform_name = "linux-x86_64"
+elif platform.system() == "Windows":
+    platform_name = "windows-x86_64"
+elif platform.system() == "Darwin":
+    platform_name = "darwin-x86_64"
+
+glslangValidator_path = os.path.join(prebuilts_path, "android-emulator-build", "common", "vulkan", platform_name, "glslangValidator")
+
+shader_source_path = os.path.join(qemu_path, "android", "android-emugl", "host", "libs", "libOpenglRender", "vulkan")
+shader_source_names = {"Etc2Decomp"}
+shader_sources = ["", ""]
+
+for shader_source_name in shader_source_names:
+    file_name = os.path.join(shader_source_path, shader_source_name + ".comp")
+    with open(file_name, 'r') as file:
+        shader_sources[1] = file.read()
+    tmp_file, tmp_file_name = tempfile.mkstemp(suffix=".comp")
+    os.write(tmp_file, "\n".join(shader_sources))
+    os.close(tmp_file)
+    os.system(" ".join([glslangValidator_path, "--spirv-val", "-V", tmp_file_name]))
+    shutil.move("comp.spv", os.path.join(shader_source_path, shader_source_name + ".spv"))
+    os.remove(tmp_file_name)
