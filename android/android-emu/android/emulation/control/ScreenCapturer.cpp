59d548b3ff91a614a5c327d793448cb50e2d90e4
Erwin Jansen <jansene@google.com>
2019-04-10 13:53:03 -0700

    Generalize screen capturing.
    We can now screen capture and convert at the same time. A new method
    takeScreenShot has been introduced that can take a screenshot in
    a desired format.
    
    This is mainly useful for future additions, where we would like the
    screenshot to be of a particular type.
    
    Contains a set of unit tests to validate that it works, and does not
    introduce new memory leaks.
    
    Change-Id: Icfa6a7a5b013158e9dfd9b2d903fa6a8a6722d82

--- android/android-emu/android/emulation/control/ScreenCapturer.cpp
+++ android/android-emu/android/emulation/control/ScreenCapturer.cpp
@@ -25,6 +25,7 @@
 #include "android/opengles.h"
 #include "android/utils/string.h"
 
+#include <png.h>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -41,47 +42,46 @@ bool captureScreenshot(android::base::StringView outputDirectoryPath,
                                  outputDirectoryPath, pOutputFilepath);
     } else {
         // renderer is nullptr in -gpu guest
-        return captureScreenshot(nullptr,
+        return captureScreenshot(
+                nullptr,
                 emulator_window_get()->uiEmuAgent->display->getFrameBuffer,
                 rotation, outputDirectoryPath, pOutputFilepath);
     }
 }
 
-bool captureScreenshot(emugl::Renderer* renderer,
-                       std::function<void(int* w, int* h, int* lineSize,
-                            int* bytesPerPixel, uint8_t** frameBufferData)>
-                            getFrameBuffer,
-                       SkinRotation rotation,
-                       android::base::StringView outputDirectoryPath,
-                       std::string* pOutputFilepath) {
-    if (!renderer && !getFrameBuffer) {
-        LOG(WARNING) << "Cannot take screenshots.\n";
-        return false;
-    }
+Image takeScreenshot(
+        ImageFormat desiredFormat,
+        SkinRotation rotation,
+        emugl::Renderer* renderer,
+        std::function<void(int* w,
+                           int* h,
+                           int* lineSize,
+                           int* bytesPerPixel,
+                           uint8_t** frameBufferData)> getFrameBuffer) {
     unsigned int nChannels = 4;
     unsigned int width;
     unsigned int height;
+    ImageFormat outputFormat = ImageFormat::RGBA8888;
     std::vector<unsigned char> pixelBuffer;
-    unsigned char* pixels = nullptr;
     if (renderer) {
+        LOG(INFO) << "Screenshot from renderer";
         renderer->getScreenshot(nChannels, &width, &height, pixelBuffer);
-        pixels = pixelBuffer.data();
     } else {
+        unsigned char* pixels = nullptr;
         int bpp = 4;
         int lineSize = 0;
-        getFrameBuffer((int*)&width, (int*)&height, &lineSize, &bpp,
-                &pixels);
+        getFrameBuffer((int*)&width, (int*)&height, &lineSize, &bpp, &pixels);
         if (bpp < 1 || bpp > 4) {
             // unknown pixel buffer format
-            return false;
+            return Image(0, 0, 0, ImageFormat::RGB888, {});
         }
         // -gpu guest usually gives us bpp=2
         // bpp=2 infers we are using rgb565
         // convert it to rgb888
         nChannels = bpp == 2 ? 3 : bpp;
+        outputFormat = ImageFormat::RGB888;
         // Need to handle padding if lineSize != width * nChannels
-        if ((lineSize != 0 && lineSize != width * nChannels) ||
-                bpp == 2) {
+        if ((lineSize != 0 && lineSize != width * nChannels) || bpp == 2) {
             pixelBuffer.resize(width * height * nChannels);
             unsigned char* src = pixels;
             unsigned char* dst = pixelBuffer.data();
@@ -92,9 +92,10 @@ bool captureScreenshot(emugl::Renderer* renderer,
                 } else {
                     // rgb565, need convertion
                     for (int w = 0; w < width; w++) {
-                        unsigned char r = src[w*2+1] >> 3;
-                        unsigned char g = (src[w*2+1] & 7) << 3 | src[w*2] >> 5;
-                        unsigned char b = src[w*2] & 63;
+                        unsigned char r = src[w * 2 + 1] >> 3;
+                        unsigned char g =
+                                (src[w * 2 + 1] & 7) << 3 | src[w * 2] >> 5;
+                        unsigned char b = src[w * 2] & 63;
                         r = r << 3 | r >> 2;
                         g = g << 2 | g >> 4;
                         b = b << 3 | b >> 2;
@@ -106,9 +107,53 @@ bool captureScreenshot(emugl::Renderer* renderer,
                 src += lineSize;
             }
             pixels = pixelBuffer.data();
+        } else {
+            // Just copy the pixels to our buffer.
+            pixelBuffer.insert(pixelBuffer.end(), &pixels[0], &pixels[width * height * nChannels]);
         }
     }
-    if (width == 0 || height == 0) {
+    // We only convert png at this time..
+    if (desiredFormat == ImageFormat::PNG) {
+            std::vector<uint8_t> pngData;
+            png_structp p = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL,
+                                                    NULL, NULL);
+            png_infop pi = png_create_info_struct(p);
+            png_set_write_fn(
+                    p, &pngData,
+                    [](png_structp png_ptr, png_bytep data, png_size_t length) {
+                        std::vector<uint8_t>* vec =
+                                reinterpret_cast<std::vector<uint8_t>*>(
+                                        png_get_io_ptr(png_ptr));
+                        vec->insert(vec->end(), &data[0], &data[length]);
+                    },
+                    [](png_structp png_ptr) {});
+            write_png_user_function(p, pi, nChannels, width,
+                                    height, rotation,
+                                    pixelBuffer.data());
+            png_destroy_write_struct(&p, &pi);
+            return Image((uint16_t)width, (uint16_t)height, nChannels, ImageFormat::PNG, pngData);
+    }
+    return Image((uint16_t)width, (uint16_t)height, nChannels, outputFormat, pixelBuffer);
+}
+
+bool captureScreenshot(
+        emugl::Renderer* renderer,
+        std::function<void(int* w,
+                           int* h,
+                           int* lineSize,
+                           int* bytesPerPixel,
+                           uint8_t** frameBufferData)> getFrameBuffer,
+        SkinRotation rotation,
+        android::base::StringView outputDirectoryPath,
+        std::string* pOutputFilepath) {
+    if (!renderer && !getFrameBuffer) {
+        LOG(WARNING) << "Cannot take screenshots.\n";
+        return false;
+    }
+
+    Image img = takeScreenshot(ImageFormat::RAW, rotation, renderer, getFrameBuffer);
+
+    if (img.getWidth() == 0 || img.getHeight() == 0) {
         return false;
     }
 
@@ -125,17 +170,18 @@ bool captureScreenshot(emugl::Renderer* renderer,
         observation.set_timestamp_us(
                 android::base::System::get()->getUnixTimeUs());
         Observation::Image* screen = observation.mutable_screen();
-        screen->set_width(width);
-        screen->set_height(height);
-        screen->set_num_channels(nChannels);
-        screen->set_data(pixels, pixelBuffer.size());
+        screen->set_width(img.getWidth());
+        screen->set_height(img.getHeight());
+        screen->set_num_channels(img.getChannels());
+        screen->set_data(img.getPixelBuf(), img.getPixelCount());
         observation.SerializeToOstream(&file);
         return true;
     }
 
     // the file name is ~25 characters
     char fileName[100];
-    int fileNameSize = snprintf(fileName, sizeof(fileName), "Screenshot_%lld.png",
+    int fileNameSize = snprintf(
+            fileName, sizeof(fileName), "Screenshot_%lld.png",
             (long long int)android::base::System::get()->getUnixTime());
     assert(fileNameSize < sizeof(fileName));
     (void)fileNameSize;
@@ -145,7 +191,8 @@ bool captureScreenshot(emugl::Renderer* renderer,
     if (pOutputFilepath) {
         *pOutputFilepath = outputFilePath;
     }
-    savepng(outputFilePath.c_str(), nChannels, width, height, rotation, pixels);
+    savepng(outputFilePath.c_str(), img.getChannels(), img.getWidth(),
+            img.getHeight(), rotation, img.getPixelBuf());
     return true;
 }
 
