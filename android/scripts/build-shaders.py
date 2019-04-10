bdb76947823bb9fb682e579fa192525f0f33d10f
Yahan Zhou <yahan@google.com>
2019-04-10 11:39:00 -0700

    [WIP] Use compute shader to descompress textures
    Change-Id: I45c4beee859401efde795a7f14b18cbb6b4068ad

---
+++ android/scripts/build-shaders.py
@@ -0,0 +1,44 @@
+import os
+import platform
+import shutil
+import tempfile
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+qemu_path = os.path.join(script_path, "..", "..")
+prebuilts_path = os.path.join(qemu_path, "..", "..", "prebuilts")
+
+if platform.system() == "Linux":
+    platform_name = "linux-x86_64"
+elif platform.system() == "Windows":
+    platform_name = "windows-x86_64"
+elif platform.system() == "Darwin":
+    platform_name = "darwin-x86_64"
+
+glslangValidator_path = os.path.join(prebuilts_path, "android-emulator-build", "common", "vulkan", platform_name, "glslangValidator")
+
+shader_source_path = os.path.join(qemu_path, "android", "android-emugl", "host", "libs", "libOpenglRender", "vulkan")
+shader_lib_name = "Etc2ShaderLib"
+shader_source_names = {"Etc2RGB8", "Etc2RGBA8", "EacR11Unorm", "EacR11Snorm", "EacRG11Unorm", "EacRG11Snorm"}
+
+shader_sources = ["", ""]
+file_name = os.path.join(shader_source_path, shader_lib_name + ".comp")
+with open(file_name, 'r') as file:
+    shader_sources[0] = file.read()
+
+for shader_source_name in shader_source_names:
+    file_name = os.path.join(shader_source_path, shader_source_name + ".comp")
+    with open(file_name, 'r') as file:
+        shader_sources[1] = file.read()
+    tmp_file, tmp_file_name = tempfile.mkstemp(suffix=".comp")
+    os.write(tmp_file, "\n".join(shader_sources))
+    os.close(tmp_file)
+    out_spv_path = os.path.join(shader_source_path, shader_source_name + ".spv")
+    if os.path.exists(out_spv_path):
+        os.remove(out_spv_path)
+    ret = os.system(" ".join([glslangValidator_path, "--spirv-val", "--target-env", "vulkan1.1", "-V", "-o", out_spv_path, tmp_file_name]))
+    if ret != 0:
+        print "compiling %s got return code %d" % (shader_source_name, ret)
+        print "intermediate source file: %s" % tmp_file_name
+    else:
+        print "compiled %s successfully" % shader_source_name
+        os.remove(tmp_file_name)
