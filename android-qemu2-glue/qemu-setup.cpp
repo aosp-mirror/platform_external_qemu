3042c902003516e47c86a8f76c6228100ae19db2
bohu <bohu@google.com>
2019-04-19 13:57:54 -0700

    image-hang-detection: add heartbeat detector
    This adds support for stateful hang detectors. If a hang detector needs
    to keep track of state it can now do so in its own class. This will make
    it slightly easier to test and easier to isolate code.
    
    It includes an adapter that will only call the underlying detector if
    a certain amout of time has passed, and a heart beat detector that
    detects if the guest is not stalled.
    
    The heart beat detector is scheduled to detect at most once a minute.
    A heartbeat is expected to come at every 30 seconds.
    
    Contains a series of unit tests to validate new behavior.
    
    Change-Id: Iab160286515d232e4e633bff4b38f53b627d6c06

--- android-qemu2-glue/qemu-setup.cpp
+++ android-qemu2-glue/qemu-setup.cpp
@@ -23,7 +23,9 @@
 #include "android/cmdline-option.h"
 #include "android/crashreport/CrashReporter.h"
 #include "android/crashreport/crash-handler.h"
+#include "android/crashreport/detectors/CrashDetectors.h"
 #include "android/emulation/address_space_device.h"
+#include "android/emulation/QemuMiscPipe.h"
 #include "android/snapshot/interface.h"
 
 #include "android/featurecontrol/FeatureControl.h"
@@ -233,11 +235,19 @@ bool qemu_android_emulation_setup() {
         android::crashreport::CrashReporter::get()
                 ->hangDetector()
                 .addPredicateCheck(
+                        new android::crashreport::TimedHangDetector(
+                                60 * 1000, new android::crashreport::HeartBeatDetector(
+                                                   get_guest_heart_beat_count,
+                                                   &guest_boot_completed)),
+                        "The guest is not sending heartbeat update, probably "
+                        "stalled.")
+                .addPredicateCheck(
                         [] {
                             return gQAndroidUserEventAgent->eventsDropped() >
-                                    kMaxEventsDropped;
+                                   kMaxEventsDropped;
                         },
-                        "The goldfish event queue is overflowing, the system is no longer responding.");
+                        "The goldfish event queue is overflowing, the system "
+                        "is no longer responding.");
     }
 #ifdef ANDROID_GRPC
     int grpc = -1;
