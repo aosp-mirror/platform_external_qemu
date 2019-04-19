7e69d2f9261b11ed21e61e9a525e525e99e9c317
bohu <bohu@google.com>
2019-04-19 15:59:18 -0700

    kvm: abort emualtor when hardware error happens
    Aborting is better than stalled.
    
    BUG: 130662334
    Change-Id: I4e4bdf4dc0754ef4615abbf3b8e73aa826729a8c

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -3272,6 +3272,7 @@ int kvm_arch_handle_exit(CPUState *cs, struct kvm_run *run)
                         "\n\n");
         }
         ret = -1;
+        abort();
         break;
     case KVM_EXIT_EXCEPTION:
         fprintf(stderr, "KVM: exception %d exit (error code 0x%x)\n",
