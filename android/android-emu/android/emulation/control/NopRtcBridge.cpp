306fc312eba7667d6f69690f069d5638b4c949fd
Erwin Jansen <jansene@google.com>
2019-04-22 11:09:31 -0700

    [WebRTC] Initial support for WebRTC
    This change consists of:
    
    - A goldfish-webrtc-videobridge executable. This is a process that is
    resposbile for:
       - Execute JSEP protocol
       - Do ICE/STUN discovery
       - Do actual stream encoding and negotiation with peer
    - A JavaScript WebRTC react component:
       - It executes the JSEP protocol
       - It displays the video stream
    - Exensions to gRPC to enable the JSEP messaging protocol.
    - A WebRtcBridge that is responsible for interacting with the video
    bridge process.
    
    Change-Id: I665f58caf9bee8c3a4bdacb274747782294be323

---
+++ android/android-emu/android/emulation/control/NopRtcBridge.cpp
@@ -0,0 +1,25 @@
+
+#include "android/emulation/control/RtcBridge.h"
+
+namespace android {
+namespace emulation {
+namespace control {
+
+bool NopRtcBridge::connect(std::string identity) {
+    return true;
+}
+
+void NopRtcBridge::disconnect(std::string identity) {}
+
+bool NopRtcBridge::acceptJsepMessage(std::string identity, std::string msg) {
+    return true;
+}
+bool NopRtcBridge::nextMessage(std::string identity, std::string* nextMessage, System::Duration blockAtMostMs) {
+    *nextMessage = std::move("{ \"bye\" : \"good times, but i am not real.\" }");
+    return true;
+}
+
+void NopRtcBridge::terminate() {}
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
