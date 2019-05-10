fb63cd42dd01593afa7bace71442eac87a34c4e4
huans <huans@google.com>
2019-05-10 16:15:24 -0700

    [MultiDisplay] Add MultiDisplay service pipe and agent for UI to control
    Bug: 128857705
    
    Test: verify creation/del/modify of the multi display
    Change-Id: I696620f1bec6afac61a080ec363fb98525266d1b

--- android/android-emugl/host/libs/libOpenglRender/RenderWindow.h
+++ android/android-emugl/host/libs/libOpenglRender/RenderWindow.h
@@ -128,6 +128,10 @@ public:
     // Receive a screen mask and pass it to TextureDraw
     void setScreenMask(int width, int height, const unsigned char* rgbaData);
 
+    void setMultiDisplay(uint32_t id, uint32_t x, uint32_t y, uint32_t w,
+                         uint32_t h, uint32_t dpi, bool add);
+    void setMultiDisplayColorBuffer(uint32_t id, uint32_t cb);
+
     // Force a repaint of the whole content into the sub-window.
     void repaint();
 
