e079b9a82d840c8ace1270a581bdf36796cf39bd
Yahan Zhou <yahan@google.com>
2019-05-10 16:28:13 -0700

    [WIP] ASTC support in vulkan
    Spec: https://www.khronos.org/registry/OpenGL/extensions/KHR/KHR_texture_compression_astc_hdr.txt
    
    Change-Id: I90208785f743fc5a7af54ffbda922c3a2ae1ec93

--- android/scripts/build-shaders.py
+++ android/scripts/build-shaders.py
@@ -2,6 +2,7 @@ import os
 import platform
 import shutil
 import tempfile
+from gen_astc_unquant_map import genAllTables
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 qemu_path = os.path.join(script_path, "..", "..")
@@ -18,8 +19,10 @@ glslangValidator_path = os.path.join(prebuilts_path, "android-emulator-build", "
 
 shader_source_path = os.path.join(qemu_path, "android", "android-emugl", "host", "libs", "libOpenglRender", "vulkan")
 shader_lib_name = "Etc2ShaderLib"
-shader_source_names = {"Etc2RGB8", "Etc2RGBA8", "EacR11Unorm", "EacR11Snorm", "EacRG11Unorm", "EacRG11Snorm"}
+shader_source_names = {"Etc2RGB8", "Etc2RGBA8", "EacR11Unorm", "EacR11Snorm", "EacRG11Unorm", "EacRG11Snorm", "Astc"}
 image_types = {"1DArray", "2DArray", "3D"}
+
+unquant_table_string = genAllTables()
 
 shader_sources = ["", ""]
 file_name = os.path.join(shader_source_path, shader_lib_name + ".comp")
@@ -32,7 +35,11 @@ for shader_source_name in shader_source_names:
         with open(file_name, 'r') as file:
             shader_sources[1] = file.read()
         tmp_file, tmp_file_name = tempfile.mkstemp(suffix=".comp")
-        os.write(tmp_file, "\n".join(shader_sources).replace("${type}", image_type))
+        if shader_source_name != "Astc":
+            combined_shader_source = "\n".join(shader_sources)
+        else:
+            combined_shader_source = shader_sources[1]
+        os.write(tmp_file, combined_shader_source.replace("${type}", image_type).replace("${UnquantTables}", unquant_table_string))
         os.close(tmp_file)
         out_spv_path = os.path.join(shader_source_path, shader_source_name + "_" + image_type + ".spv")
         if os.path.exists(out_spv_path):
@@ -40,7 +47,7 @@ for shader_source_name in shader_source_names:
         ret = os.system(" ".join([glslangValidator_path, "--spirv-val", "--target-env", "vulkan1.1", "-V", "-o", out_spv_path, tmp_file_name]))
         if ret != 0:
             print "Compiling %s got return code %d" % (out_spv_path, ret)
-            print "intermediate source file: %s" % tmp_file_name
+            print "Please look at intermediate source file for debug: %s" % tmp_file_name
         else:
             print "Compiled %s successfully" % out_spv_path
             os.remove(tmp_file_name)
