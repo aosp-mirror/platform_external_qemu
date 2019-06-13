8b86e855b0acedfc95e87b364bfc404bc8393320
Wei Zhao <weizhaowz@google.com>
2019-06-13 16:23:42 -0700

    fixed failed tests cases by elimiating complete status.
    Change-Id: Iee89e452b9c61ebc6e8a9e069ad6b634a249daf0

--- android/android-emu/android/videoinjection/VideoInjectionController.h
+++ android/android-emu/android/videoinjection/VideoInjectionController.h
@@ -92,6 +92,11 @@ public:
 
     // Called on pipe close, to cancel any pending operations.
     virtual void pipeClosed(android::AsyncMessagePipeHandle pipe) = 0;
+
+    // Send async response containing error message to java video injection
+    // controller.
+    virtual void sendErrorAsyncResponse(uint32_t async_id,
+                                        const VideoInjectionError& error) = 0;
 };
 
 }  // namespace videoinjection
