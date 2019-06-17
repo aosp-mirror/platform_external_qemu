0b85bccdfa675578eb1cd56b00f9b11232b6071e
Haipeng Wu <haipengwu@google.com>
2019-06-17 13:48:16 -0700

    Refactor the demuxing logic in VideoPlayer so that we could reuse it elsewhere and add additional testing.
    Design doc: go/ar-dataset-emu
    Bug: 132256776
    
    Change-Id: I635fcb9ed6ab2ce553a4d24efe07b6e37f8169f1

---
+++ android/android-emu/android/mp4/MP4Dataset.cpp
@@ -0,0 +1,92 @@
+#include "android/mp4/MP4Dataset.h"
+
+#include <string>
+
+#include "android/base/Log.h"
+#include "android/recording/AVScopedPtr.h"
+#include "android/recording/video/player/PacketQueue.h"
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+}
+
+using android::recording::AVScopedPtr;
+using android::recording::makeAVScopedPtr;
+
+namespace android {
+namespace mp4 {
+
+class Mp4DatasetImpl : public Mp4Dataset {
+public:
+    Mp4DatasetImpl(std::string filepath) : mDatasetFilepath(filepath) {}
+    virtual ~Mp4DatasetImpl() {};
+    int init();
+    int getAudioStreamIndex() { return mAudioStreamIdx; }
+    int getVideoStreamIndex() { return mVideoStreamIdx; }
+    int getSensorDataStreamIndex(AndroidSensor sensor) {
+        return mSensorStreamsIdx[sensor];
+    }
+    int getLocationDataStreamIndex() { return mLocationStreamIdx; }
+    AVFormatContext* getFormatContext() { return mFormatCtx.get(); }
+    void clearFormatContext() { mFormatCtx.reset(); }
+
+private:
+    std::string mDatasetFilepath;
+    AVScopedPtr<AVFormatContext> mFormatCtx;
+    int mAudioStreamIdx = -1;
+    int mVideoStreamIdx = -1;
+    int mSensorStreamsIdx[MAX_SENSORS] = {-1, -1, -1, -1, -1, -1,
+                                          -1, -1, -1, -1, -1};
+    int mLocationStreamIdx = -1;
+};
+
+std::unique_ptr<Mp4Dataset> Mp4Dataset::create(std::string filepath) {
+    std::unique_ptr<Mp4Dataset> dataset;
+    dataset.reset(new Mp4DatasetImpl(filepath));
+    return std::move(dataset);
+}
+
+// TODO(haipengwu): Add data stream indices setting logic whith proto
+// integration
+int Mp4DatasetImpl::init() {
+    const char* filename = mDatasetFilepath.c_str();
+
+    // Register all formats and codecs
+    av_register_all();
+    avformat_network_init();
+
+    AVFormatContext* inputCtx = nullptr;
+    if (avformat_open_input(&inputCtx, filename, NULL, NULL) != 0) {
+        LOG(ERROR) << ": Failed to open input context";
+        return -1;
+    }
+
+    mFormatCtx = makeAVScopedPtr(inputCtx);
+
+    if (avformat_find_stream_info(mFormatCtx.get(), NULL) < 0) {
+        LOG(ERROR) << ": Failed to find stream info";
+        return -1;
+    }
+
+    av_dump_format(mFormatCtx.get(), 0, filename, false);
+
+    for (int i = 0; i < mFormatCtx->nb_streams; i++) {
+        if (mFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
+            mAudioStreamIdx = i;
+        } else if (mFormatCtx->streams[i]->codec->codec_type ==
+                   AVMEDIA_TYPE_VIDEO) {
+            mVideoStreamIdx = i;
+        }
+    }
+
+    if (mVideoStreamIdx == -1) {
+        LOG(ERROR) << ": No video stream found";
+        return -1;
+    }
+
+    return 0;
+}
+
+}  // namespace mp4
+}  // namespace android
