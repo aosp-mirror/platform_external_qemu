9733d0fa3587cb29341de8ae99297c1558d35caf
Haipeng Wu <haipengwu@google.com>
2019-06-17 13:48:16 -0700

    Refactor the demuxing logic in VideoPlayer so that we could reuse it elsewhere and add additional testing.
    Design doc: go/ar-dataset-emu
    Bug: 132256776
    
    Change-Id: I635fcb9ed6ab2ce553a4d24efe07b6e37f8169f1

---
+++ android/android-emu/android/mp4/MP4Dataset.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "android/hw-sensors.h"
+#include "android/utils/compiler.h"
+
+extern "C" {
+#include "libavformat/avformat.h"
+}
+
+namespace android {
+namespace mp4 {
+
+// A wrapper around an MP4 dataset file that provides access to information
+// about this dataset.
+class Mp4Dataset {
+public:
+    virtual ~Mp4Dataset() = default;
+    static std::unique_ptr<Mp4Dataset> create(std::string filepath);
+    virtual int init();
+    virtual int getAudioStreamIndex();
+    virtual int getVideoStreamIndex();
+    virtual int getSensorDataStreamIndex(AndroidSensor sensor);
+    virtual int getLocationDataStreamIndex();
+    virtual AVFormatContext* getFormatContext();
+    virtual void clearFormatContext();
+
+protected:
+    Mp4Dataset() = default;
+};
+
+}  // namespace mp4
+}  // namespace android
