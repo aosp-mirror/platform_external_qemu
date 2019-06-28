501640305d8395fa2818cce6845089004b2422af
Lingfeng Yang <lfy@google.com>
2019-06-28 10:14:38 -0700

    [vulkan] Readback hint
    bug: 136266299
    
    calculate the expected number of bytes to readback, then hint
    to the underlying stream device.
    
    expected different behavior:
    
    fuchsia: the readback bytes hint serves as the parameter to
    pipe cmd code call() and/or does non blocking read
    
    android: read() back all the traffic in one call to the
    pipe at the end, instead of a ton of them (or adopt cmd code call()
    and non blocking read of fuchsia version)
    
    Change-Id: Ic2f8d063d90baedc61e34fe18a40dd24d9d4dfd2

--- android/android-emugl/host/libs/libOpenglRender/vulkan-registry/xml/cereal/encoder.py
+++ android/android-emugl/host/libs/libOpenglRender/vulkan-registry/xml/cereal/encoder.py
@@ -228,6 +228,16 @@ def emit_count_marshal(typeInfo, param, cgen):
     if not res:
         cgen.stmt("(void)%s" % param.paramName)
 
+def emit_count_unmarshal(typeInfo, param, cgen):
+    res = \
+        iterateVulkanType(
+            typeInfo, param,
+            VulkanMarshalingCodegen( \
+               cgen, COUNTING_STREAM, param.paramName,
+               API_PREFIX_UNMARSHAL, direction="read"))
+    if not res:
+        cgen.stmt("(void)%s" % param.paramName)
+
 def emit_marshal(typeInfo, param, cgen):
     forOutput = param.isHandleType() and ("out" in param.inout)
     if forOutput:
@@ -402,6 +412,19 @@ def emit_parameter_encode_do_parameter_write(typeInfo, api, cgen):
 
 def emit_parameter_encode_read(typeInfo, api, cgen):
     encodingParams = EncodingParameters(api)
+
+    cgen.stmt("AEMU_SCOPED_TRACE(\"%s readParams count\")" % api.name)
+
+    cgen.beginBlock()
+
+    for p in encodingParams.toRead:
+        emit_count_unmarshal(typeInfo, p, cgen)
+
+    cgen.stmt("size_t expectedReadback_%s = %s->bytesRead()" % (api.name, COUNTING_STREAM))
+    cgen.stmt("%s->rewind()" % COUNTING_STREAM)
+    cgen.stmt("%s->setExpectedReadback(expectedReadback_%s)" % (STREAM, api.name))
+
+    cgen.endBlock()
 
     cgen.stmt("AEMU_SCOPED_TRACE(\"%s readParams\")" % api.name)
 
