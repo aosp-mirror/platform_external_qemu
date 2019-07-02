4d20de1302ec62879dee9387b9f726c8f65cfa37
Lingfeng Yang <lfy@google.com>
2019-07-02 12:03:07 -0700

    [vulkan][gl] communicate pending readbacks, small vector opts
    1. Communicate pending readbacks
    
    It's common to read back shortly after writing a bunch; this
    optimization makes it so we spin less in onGuestRecv in the case where a
    bunch of data is written and we are just waiting for the host to run the
    command.
    
    With pending readbacks, it seems better to use them instead of
    spinning in onGuestRecv.
    
    2. Small vector opts
    
    When we successfully push or pop data in the RenderChannel, free() gets
    called in SmallVector's destructor. This can take a lot longer than
    copying. We convert to using copy constructors over move constructors.
    
    Test: glmark2 gles2 on ANGLE slightly faster, hit 710 once
    Test: % time in pipe I/O relative to vcpu run decreased from 23% to 7%
    in glmark2 gles2 on ANGLE
    
    Change-Id: I0358ccef6fdc991d7110d09e31f9a81f8f07448f

--- android/android-emugl/host/tools/emugen/tests/t.001/expected/decoder/foo_dec.cpp
+++ android/android-emugl/host/tools/emugen/tests/t.001/expected/decoder/foo_dec.cpp
@@ -65,12 +65,14 @@ size_t foo_decoder_context_t::decode(void *buf, size_t len, IOStream *stream, Ch
 			size_t totalTmpSize = sizeof(FooBoolean);
 			totalTmpSize += checksumSize;
 			unsigned char *tmpBuf = stream->alloc(totalTmpSize);
+			mOnBeginReadback();
 			DEBUG("foo(%p): fooIsBuffer(%p(%u) )\n", stream, (void*)(inptr_stuff.get()), size_stuff);
 			*(FooBoolean *)(&tmpBuf[0]) = 			this->fooIsBuffer((void*)(inptr_stuff.get()));
 			if (useChecksum) {
 				ChecksumCalculatorThreadInfo::writeChecksum(checksumCalc, &tmpBuf[0], totalTmpSize - checksumSize, &tmpBuf[totalTmpSize - checksumSize], checksumSize);
 			}
 			stream->flush();
+			mOnEndReadback();
 			SET_LASTCALL("fooIsBuffer");
 			break;
 		}
@@ -132,6 +134,7 @@ size_t foo_decoder_context_t::decode(void *buf, size_t len, IOStream *stream, Ch
 			size_t totalTmpSize = size_obj;
 			totalTmpSize += checksumSize;
 			unsigned char *tmpBuf = stream->alloc(totalTmpSize);
+			mOnBeginReadback();
 			OutputBuffer outptr_obj(&tmpBuf[0], size_obj);
 			void* forPacking_obj = nullptr;
 			 FooStruct tmp; forPacking_obj = (void*)tmp;
@@ -144,6 +147,7 @@ size_t foo_decoder_context_t::decode(void *buf, size_t len, IOStream *stream, Ch
 				ChecksumCalculatorThreadInfo::writeChecksum(checksumCalc, &tmpBuf[0], totalTmpSize - checksumSize, &tmpBuf[totalTmpSize - checksumSize], checksumSize);
 			}
 			stream->flush();
+			mOnEndReadback();
 			SET_LASTCALL("fooGetComplexStruct");
 			break;
 		}
@@ -157,6 +161,7 @@ size_t foo_decoder_context_t::decode(void *buf, size_t len, IOStream *stream, Ch
 			size_t totalTmpSize = size_count;
 			totalTmpSize += checksumSize;
 			unsigned char *tmpBuf = stream->alloc(totalTmpSize);
+			mOnBeginReadback();
 			OutputBuffer outptr_count(&tmpBuf[0], size_count);
 			memcpy(outptr_count.get(), inptr_count.get(), size_count);
 			DEBUG("foo(%p): fooInout(%p(%u) )\n", stream, (uint32_t*)(outptr_count.get()), size_count);
@@ -166,6 +171,7 @@ size_t foo_decoder_context_t::decode(void *buf, size_t len, IOStream *stream, Ch
 				ChecksumCalculatorThreadInfo::writeChecksum(checksumCalc, &tmpBuf[0], totalTmpSize - checksumSize, &tmpBuf[totalTmpSize - checksumSize], checksumSize);
 			}
 			stream->flush();
+			mOnEndReadback();
 			SET_LASTCALL("fooInout");
 			break;
 		}
