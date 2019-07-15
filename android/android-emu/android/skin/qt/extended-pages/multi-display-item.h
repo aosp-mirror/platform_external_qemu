ac1174a11e6237a3551fa8f4e92f4c2b9e1d5bc7
Huan Song <huans@google.com>
2019-07-15 16:14:07 -0700

    WIP: [MultiDisplay UI] create the multidisplay subpage base UI.
    Bug: 132825401
    
    Test: Manual
    Change-Id: I267f09647bf3c64daff3e0ed1a432e6958e6336b

---
+++ android/android-emu/android/skin/qt/extended-pages/multi-display-item.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "ui_multi-display-item.h"
+#include <QWidget>
+#include <memory>
+
+class MultiDisplayItem : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit MultiDisplayItem(int id, QWidget* parent = 0);
+private:
+    std::unique_ptr<Ui::MultiDisplayItem> mUi;
+    struct displayType  {
+        std::string name;
+        int width;
+        int height;
+        int dpi;
+    };
+    static std::vector<displayType> sDisplayTypes;
+    int mId;
+    void hideWidthHeightDpiBox(bool hide);
+
+private slots:
+    void onDisplayTypeChanged(int index);
+    void on_deleteDisplay_clicked();
+
+signals:
+    void deleteDisplay(int id);
+};
+
