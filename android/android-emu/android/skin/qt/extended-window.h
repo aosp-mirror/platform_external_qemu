75846cb7d6d9e0763b3e56315432f2cf10c1c35b
Huan Song <huans@google.com>
2019-07-15 16:14:07 -0700

    [MultiDisplay UI] create the multidisplay subpage base UI.
    Bug: 132825401
    
    Test: Manual
    Change-Id: I267f09647bf3c64daff3e0ed1a432e6958e6336b

--- android/android-emu/android/skin/qt/extended-window.h
+++ android/android-emu/android/skin/qt/extended-window.h
@@ -81,6 +81,7 @@ private slots:
     void on_bugreportButton_clicked();
     void on_cellularButton_clicked();
     void on_dpadButton_clicked();
+    void on_displaysButton_clicked();
     void on_fingerButton_clicked();
     void on_googlePlayButton_clicked();
     void on_helpButton_clicked();
