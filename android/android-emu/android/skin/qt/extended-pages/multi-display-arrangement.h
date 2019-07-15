60f18482d5d75d27ac03894a1b31e283797988c0
Huan Song <huans@google.com>
2019-07-15 16:14:07 -0700

    [MultiDisplay] create the multidisplay new UI.
    * Subpage UI dedicated for multidisplay.
    * Max display is 3.
    * Snapshot support.
    * Change function name setMultiDisplay to setUIMultiDisplay,
      better indicating function calls to the UI.
    * Sync the snapshot loading and config.ini replay by adding
      a dedicated lock.
    * Polish the code.
    
    Bug: 132825401
         139928669
    
    Test: Manual
    Change-Id: I267f09647bf3c64daff3e0ed1a432e6958e6336b

---
+++ android/android-emu/android/skin/qt/extended-pages/multi-display-arrangement.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <QWidget>
+#include <memory>
+#include <string>
+#include <utility>
+#include <unordered_map>
+
+class MultiDisplayArrangement : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit MultiDisplayArrangement(QWidget* parent = 0);
+    virtual void paintEvent(QPaintEvent* e) override;
+    void setLayout(std::unordered_map<uint32_t, std::pair<uint32_t, uint32_t>>& size,
+                   std::unordered_map<uint32_t, std::pair<uint32_t, uint32_t>>& offset,
+                   std::unordered_map<uint32_t, std::string>& name);
+    void setHighLightDisplay(int id);
+private:
+    struct rectangle {
+        uint32_t x;
+        uint32_t y;
+        uint32_t w;
+        uint32_t h;
+        std::string name;
+    };
+    std::unordered_map<uint32_t, struct rectangle> mLayout;
+    void getCombinedDisplaySize(int* w, int* h);
+    int mHighLightDisplayId = -1;
+};
