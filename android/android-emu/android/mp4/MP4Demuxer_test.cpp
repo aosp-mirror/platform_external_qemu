7b8efed27e035f4abecb46598309f9f54c37e212
Haipeng Wu <haipengwu@google.com>
2019-07-17 15:42:02 -0700

    Connect the data stream processing pipeline to the virtual sensor through Automation Controller. This allows us to play the sensor data in an MP4 file to emulator. Note that location data processing is not yet implemented.
    Design doc: go/ar-dataset-emu
    Bug: 132256776
    
    Change-Id: Ibbf05c3d22f8a986635191eddea10b7ae8e0b182

--- android/android-emu/android/mp4/MP4Demuxer_test.cpp
+++ android/android-emu/android/mp4/MP4Demuxer_test.cpp
@@ -17,6 +17,7 @@
 #include "android/base/files/PathUtils.h"
 #include "android/base/system/System.h"
 #include "android/base/threads/Thread.h"
+#include "android/offworld/proto/offworld.pb.h"
 #include "android/recording/video/player/VideoPlayer.h"
 #include "android/recording/video/player/VideoPlayerWaitInfo.h"
 
@@ -39,6 +40,7 @@ public:
     bool isRunning() const { return mRunning; }
     void videoRefresh() {}
     void scheduleRefresh(int delayMS) {}
+    void loadVideoFileWithData(const ::offworld::DatasetInfo& datasetInfo) {}
 
 private:
     bool mRunning = false;
@@ -60,7 +62,8 @@ protected:
 
         mMockedVideoPlayer.reset(new MockedVideoPlayer());
 
-        mDataset = Mp4Dataset::create(absDataPath, nullptr);
+        mDataset = Mp4Dataset::create(
+                absDataPath, ::offworld::DatasetInfo::default_instance());
         mDemuxer = Mp4Demuxer::create(mMockedVideoPlayer.get(), mDataset.get(),
                                       nullptr);
 
