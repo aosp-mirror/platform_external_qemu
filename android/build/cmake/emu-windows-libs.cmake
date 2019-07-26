b8164db48819eb5f4770c80139b841ed97f5cbc4
Erwin <jansene@google.com>
2019-07-26 21:26:09 +0000

    [Win] Support MSVC 2019
    - We now use 'vswhere' to discover all available MSVC installs
    - Contains fixes to compile against newer STL libraries
    - Updated documentation with to solve problems with vscode you might
      encounter
    
    Change-Id: If4032f6b69b1f0fa4ba3f37ea15379fa1b2c5811

--- android/build/cmake/emu-windows-libs.cmake
+++ android/build/cmake/emu-windows-libs.cmake
@@ -43,36 +43,16 @@ foreach(LIB ${WINDOWS_LIBS})
   android_find_windows_library(${LIB})
 endforeach()
 
-if(WIN32 AND MSVC)
-  # Find the DIA SDK path, it will typically look something like this. C:\Program Files (x86)\Microsoft Visual
-  # Studio\2017\Enterprise\DIA SDK\include C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include\dia2.h
-  # Find the DIA SDK path, as we need to include it.
-  get_filename_component(VS_PATH32 "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0;InstallDir]" ABSOLUTE
-                         CACHE)
-  get_filename_component(VS_PATH64
-                         "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\14.0;InstallDir]"
-                         ABSOLUTE CACHE)
-  # VS_PATH32 will be something like C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE
-
-  # Also look for in vs15 install.
-  set(PROGRAMFILES_X86 "ProgramFiles(x86)")
-  get_filename_component(VS15_C_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Community/Common7/IDE"
-                         ABSOLUTE CACHE)
-  get_filename_component(VS15_P_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Professional/Common7/IDE"
-                         ABSOLUTE CACHE)
-  get_filename_component(VS15_E_PATH32 "$ENV{${PROGRAMFILES_X86}}/Microsoft Visual Studio/2017/Enterprise/Common7/IDE"
-                         ABSOLUTE CACHE)
+if(WIN32 AND MSVC)  
+   # We expect all the visual studio variables to be set. The rebuild script will take care of this..
+   # If you are building from 
   get_filename_component(VCTOOLS_PATH "$ENV{VCToolsInstallDir}" ABSOLUTE CACHE)
+  get_filename_component(VSINSTALLDIR "$ENV{VSINSTALLDIR}" ABSOLUTE CACHE)
 
-  # Find the DIA SDK path, it will typically look something like this. C:\Program Files (x86)\Microsoft Visual
-  # Studio\2017\Enterprise\DIA SDK\include C:\Program Files (x86)\Microsoft Visual Studio 14.0\DIA SDK\include\dia2.h
+  # Find the DIA SDK path, it will typically look something like this.
   find_path(DIASDK_INCLUDE_DIR # Set variable DIASDK_INCLUDE_DIR
             dia2.h # Find a path with dia2.h
-            HINTS "${VS15_C_PATH32}/../../DIA SDK/include"
-            HINTS "${VS15_P_PATH32}/../../DIA SDK/include"
-            HINTS "${VS15_E_PATH32}/../../DIA SDK/include"
-            HINTS "${VS_PATH64}/../../DIA SDK/include"
-            HINTS "${VS_PATH32}/../../DIA SDK/include"
+            HINTS "${VSINSTALLDIR}/DIA SDK/include"
             DOC "path to DIA SDK header files")
   message(STATUS "Found DIA SDK Include: ${DIASDK_INCLUDE_DIR}")
 
@@ -87,15 +67,11 @@ if(WIN32 AND MSVC)
   #C:\Program Files (x86)\Microsoft Visual Studio\2017\xxx\VC\Tools\MSVC\14.16.27023\atlmfc\include
   find_path(ATL_INCLUDE_DIR # Set variable DIASDK_INCLUDE_DIR
             atlbase.h # Find a path with atlbase.h
-            HINTS "${VS15_C_PATH32}/../../VC/TOOLS/MSVC/14.16.27023/atlmfc/include"
-            HINTS "${VS15_P_PATH32}/../../VC/TOOLS/MSVC/14.16.27023/atlmfc/include"
-            HINTS "${VS15_E_PATH32}/../../VC/TOOLS/MSVC/14.16.27023/atlmfc/include"
             HINTS "${VCTOOLS_PATH}/atlmfc/include"
             DOC "path to ATL header files")
   message(STATUS "Found ATL Include: ${ATL_INCLUDE_DIR}")
 
   find_library(ATL_LIBRARY NAMES atls.lib HINTS ${ATL_INCLUDE_DIR}/../lib/x64)
-
   set_target_properties(atls::atls
                         PROPERTIES INTERFACE_LINK_LIBRARIES ${ATL_LIBRARY} INTERFACE_INCLUDE_DIRECTORIES
                                    ${ATL_INCLUDE_DIR})
