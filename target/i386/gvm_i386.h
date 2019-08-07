e2e09a4b381bef3c9ec0d54e58431c3f0d7714b0
Haitao Shan <hshan@google.com>
2019-08-07 09:22:04 -0700

    Remove KVM legacy code.
    Change-Id: I40e54f4d16cfaa3bf5d4beca343984258e698375

--- target/i386/gvm_i386.h
+++ target/i386/gvm_i386.h
@@ -37,30 +37,8 @@
 
 #endif  /* CONFIG_GVM */
 
-bool gvm_allows_irq0_override(void);
-bool gvm_has_smm(void);
 void gvm_synchronize_all_tsc(void);
 void gvm_arch_reset_vcpu(X86CPU *cs);
 void gvm_arch_do_init_vcpu(X86CPU *cs);
-
-int gvm_device_pci_assign(GVMState *s, PCIHostDeviceAddress *dev_addr,
-                          uint32_t flags, uint32_t *dev_id);
-int gvm_device_pci_deassign(GVMState *s, uint32_t dev_id);
-
-int gvm_device_intx_assign(GVMState *s, uint32_t dev_id,
-                           bool use_host_msi, uint32_t guest_irq);
-int gvm_device_intx_set_mask(GVMState *s, uint32_t dev_id, bool masked);
-int gvm_device_intx_deassign(GVMState *s, uint32_t dev_id, bool use_host_msi);
-
-int gvm_device_msi_assign(GVMState *s, uint32_t dev_id, int virq);
-int gvm_device_msi_deassign(GVMState *s, uint32_t dev_id);
-
-bool gvm_device_msix_supported(GVMState *s);
-int gvm_device_msix_init_vectors(GVMState *s, uint32_t dev_id,
-                                 uint32_t nr_vectors);
-int gvm_device_msix_set_vector(GVMState *s, uint32_t dev_id, uint32_t vector,
-                               int virq);
-int gvm_device_msix_assign(GVMState *s, uint32_t dev_id);
-int gvm_device_msix_deassign(GVMState *s, uint32_t dev_id);
 
 #endif /*QEMU_GVM_I386_H */
