8ca5e88dddd36f4ad86935cb490373bcd7424596
Haitao Shan <hshan@google.com>
2019-08-07 15:48:39 -0700

    Restruct code and add back log dirty.
    Change-Id: Ia98362d53fdfd9e47f2b4e7349c8125a0de84fe1

--- include/exec/ram_addr.h
+++ include/exec/ram_addr.h
@@ -300,7 +300,6 @@ static inline void cpu_physical_memory_set_dirty_range(ram_addr_t start,
     xen_hvm_modified_memory(start, length);
 }
 
-#if !defined(_WIN32)
 static inline void cpu_physical_memory_set_dirty_lebitmap(unsigned long *bitmap,
                                                           ram_addr_t start,
                                                           ram_addr_t pages)
@@ -374,7 +373,6 @@ static inline void cpu_physical_memory_set_dirty_lebitmap(unsigned long *bitmap,
         }
     }
 }
-#endif /* not _WIN32 */
 
 bool cpu_physical_memory_test_and_clear_dirty(ram_addr_t start,
                                               ram_addr_t length,
