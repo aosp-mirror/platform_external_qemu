a474348b92cba70e232521eaa25c40cc2c700382
Weilun Du <wdu@google.com>
2019-08-07 05:29:25 +0000

    Revert "[MultiDisplay] Hide rotation buttons in virtual sensor"
    This reverts commit 304c7fbee25615530311d7353c28b96bfb79d0d8.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I575ef0e5dfa9d28ed76d4025b10adbc02b71da6e

--- android/android-emu/android/skin/qt/extended-window.h
+++ android/android-emu/android/skin/qt/extended-window.h
@@ -67,7 +67,6 @@ public:
 
     void connectVirtualSceneWindow(
             VirtualSceneControlWindow* virtualSceneWindow);
-    void hideRotationButtons(bool hide);
 
 private slots:
     void switchFrameAlways(bool showFrame);
