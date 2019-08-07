50d62811a7e33c6f20b16fc28faf266309063820
Weilun Du <wdu@google.com>
2019-08-07 05:29:25 +0000

    Revert "[MultiDisplay] Hide rotation buttons in virtual sensor"
    This reverts commit 304c7fbee25615530311d7353c28b96bfb79d0d8.
    
    Reason for revert: Emulator will hang if multi display is configured
    in config.ini.
    
    BUG: 135756686
    Change-Id: I575ef0e5dfa9d28ed76d4025b10adbc02b71da6e

--- android/android-emu/android/skin/qt/tool-window.cpp
+++ android/android-emu/android/skin/qt/tool-window.cpp
@@ -1103,7 +1103,6 @@ void ToolWindow::hideRotationButton(bool hide) {
     } else {
         mToolsUi->prev_layout_button->setHidden(hide);
         mToolsUi->next_layout_button->setHidden(hide);
-        mExtendedWindow.get()->hideRotationButtons(hide);
     }
 }
 
