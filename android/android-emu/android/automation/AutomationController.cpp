a39477adb7b2878168f7c0bef7986c70042aecdf
Joseph Lee <josephhlee@google.com>
2019-08-15 09:59:10 -0700

    Add location support to Automation Controller
    Add new automation event for location data. This needs to be its own
    event message as the corresponding location libraries essentially emulate the
    entire GPS package rather than individual sensors separately.
    
    Change-Id: Ia93730f5a66c023defc675901606fd7a93c44ba4

--- android/android-emu/android/automation/AutomationController.cpp
+++ android/android-emu/android/automation/AutomationController.cpp
@@ -37,9 +37,15 @@
 #include "android/offworld/OffworldPipe.h"
 #include "android/physics/PhysicalModel.h"
 
+extern "C" {
+#include "android/gps.h"
+}
+
 using namespace android::base;
 
 static constexpr uint32_t kFileVersion = 2;
+
+namespace pb = emulator_automation;
 
 namespace {
 
@@ -52,9 +58,22 @@ static offworld::Response createAsyncResponse(uint32_t asyncId) {
     return response;
 }
 
-}  // namespace.
+void sendLocationOverride(const pb::LocationOverrideEvent& location_override) {
+    const int kNumMicrosInSecond = 1000000;
+    timeval timeVal = {};
+    timeVal.tv_sec = location_override.timestamp_in_us() / kNumMicrosInSecond;
+    timeVal.tv_usec = location_override.timestamp_in_us() % kNumMicrosInSecond;
 
-namespace pb = emulator_automation;
+    android_gps_send_location(
+        location_override.latitude(),
+        location_override.longitude(),
+        location_override.meters_elevation(),
+        location_override.speed_knots(),
+        location_override.heading_degrees(),
+        location_override.num_satellites(),
+        &timeVal);
+}
+}  // namespace.
 
 namespace android {
 namespace automation {
@@ -899,6 +918,9 @@ void AutomationControllerImpl::replayNextEvent(const AutoLock& proofOfLock) {
                                               event.sensor_override());
             break;
         }
+        case pb::RecordedEvent::StreamCase::kLocationOverride:
+            sendLocationOverride(event.location_override());
+            break;
         case pb::RecordedEvent::StreamCase::STREAM_NOT_SET:
             // Last event for files to simulate recordings.
             break;
