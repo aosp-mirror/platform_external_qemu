eab711f71e93a7fd465a4c23547bc6a885fe898d
Erwin Jansen <jansene@google.com>
2019-08-29 12:46:21 -0700

    [gRPC] Add support for metrics
    We add an interceptor that measures:
    
    - Estimate on how long a request took
    - Estimate on how many bytes where send/received when streaming
    - How many times it was called, and how many succeeded or failed.
    - Method names are hashed using CRC32. Details on collision rates
      can be found [here]
      (https://preshing.com/20110504/hash-collision-probabilities/). It is
      unlikely that we will see collisions.
    
    The metrics will be reported upon emulator exit for those who opted in.
    
    Test:  ./objs/emulator @M -grpc 5556 -verbose -metrics-to-console
    Change-Id: I98aba8936e73ed14ff789c718b7bd5fab4b48b24
    emulator: VERBOSE: MetricsInterceptor.cpp:82: Sending metric [/android.emulation.control.EmulatorController/sendKey]: call_id: 3192300597 requests: 220 failures: 0 rcv_bytes_estimate { } snd_bytes_estimate { } duration { bucket { target_percentile: 0 value: 79 count: 0 } bucket { target_percentile: 0.45 value: 162.66837951333522 count: 117 } bucket { target_percentile: 0.9 value: 193.00785251793121 count: 202 } bucket { target_percentile: 0.95 value: 230.82778321506788 count: 211 } bucket { target_percentile: 1 value: 462 count: 220 } }
    emulator: VERBOSE: MetricsInterceptor.cpp:82: Sending metric [/waterfall.Waterfall/Push]: call_id: 4142548695 requests: 40 failures: 0 rcv_bytes_estimate { bucket { target_percentile: 0 value: 134334257 count: 0 } bucket { target_percentile: 0.45 value: 134795982.47802413 count: 26 } bucket { target_percentile: 0.9 value: 136407051.37034822 count: 38 } bucket { target_percentile: 0.95 value: 136560433 count: 39 } bucket { target_percentile: 1 value: 136560433 count: 40 } } snd_bytes_estimate { } duration { bucket { target_percentile: 0 value: 1076554 count: 0 } bucket { target_percentile: 0.45 value: 1195258.2990576837 count: 27 } bucket { target_percentile: 0.9 value: 1323596.7172854089 count: 38 } bucket { target_percentile: 0.95 value: 1359528.5 count: 39 } bucket { target_percentile: 1 value: 1362353 count: 40 } }

---
+++ android/android-grpc/android/emulation/control/interceptor/MetricsInterceptor.cpp
@@ -0,0 +1,95 @@
+
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/emulation/control/interceptor/MetricsInterceptor.h"
+
+#include <zlib.h>
+
+#include "android/base/memory/LazyInstance.h"
+#include "android/metrics/MetricsReporter.h"
+#include "android/metrics/proto/studio_stats.pb.h"
+
+namespace android {
+namespace control {
+namespace interceptor {
+
+using android::base::AutoLock;
+using android::metrics::MetricsReporter;
+using namespace android_studio;
+
+static void reportMetrics(const InvocationRecord& loginfo) {
+    InvocationMetrics::get()->record(loginfo);
+}
+
+static base::LazyInstance<InvocationMetrics> sInvocationMetrics =
+        LAZY_INSTANCE_INIT;
+
+void InvocationMetrics::record(const InvocationRecord& invocation) {
+    AutoLock lock(mLock);
+    bool registerCallback = mMetrics.count(invocation.method) == 0;
+    MethodMetrics& metrics = mMetrics[invocation.method];
+
+    // To minimize data transfer we only collect send/receive for non unary
+    // requests.
+    if (invocation.type == ServerRpcInfo::Type::CLIENT_STREAMING ||
+        invocation.type == ServerRpcInfo::Type::BIDI_STREAMING) {
+        metrics.recvBytes.addSample(invocation.rcvBytes);
+    }
+    if (invocation.type == ServerRpcInfo::Type::SERVER_STREAMING ||
+        invocation.type == ServerRpcInfo::Type::BIDI_STREAMING) {
+        metrics.sndBytes.addSample(invocation.sndBytes);
+    }
+
+    metrics.duration.addSample(((double)invocation.duration) / 1000);
+    if (invocation.status.error_code() == 0) {
+        metrics.success++;
+    } else {
+        metrics.fail++;
+    }
+
+    if (registerCallback) {
+        reportMetric(invocation.method, metrics);
+    }
+}
+
+void InvocationMetrics::reportMetric(const std::string& name,
+                                     const MethodMetrics& metric) {
+    MetricsReporter::get().reportOnExit(
+            [name, &metric](android_studio::AndroidStudioEvent* event) {
+                uint32_t crc =
+                        crc32(0, reinterpret_cast<const uint8_t*>(name.data()),
+                              name.size());
+                auto grpc = event->mutable_emulator_details()->mutable_grpc();
+                grpc->set_call_id(crc);
+                grpc->set_requests(metric.success + metric.fail);
+                grpc->set_failures(metric.fail);
+                metric.recvBytes.fillMetricsEvent(
+                        grpc->mutable_rcv_bytes_estimate());
+                metric.sndBytes.fillMetricsEvent(
+                        grpc->mutable_snd_bytes_estimate());
+                metric.duration.fillMetricsEvent(grpc->mutable_duration());
+                LOG(VERBOSE) << "Sending metric [" << name
+                             << "]: " << grpc->ShortDebugString();
+            });
+}
+
+InvocationMetrics* InvocationMetrics::get() {
+    return sInvocationMetrics.ptr();
+}
+
+MetricsInterceptorFactory::MetricsInterceptorFactory()
+    : LoggingInterceptorFactory(reportMetrics) {}
+}  // namespace interceptor
+}  // namespace control
+}  // namespace android
