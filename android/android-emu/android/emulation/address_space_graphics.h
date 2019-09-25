3434820816ee809b9323a98a7be39b6b84c46662
Lingfeng Yang <lfy@google.com>
2019-09-25 08:51:20 -0700

    [gl] address space graphics device (device+tests only)
    bug: 140112486
    
    This CL introduces the address space graphics device
    (ASG), which allows the guest to send / recv graphics API data with
    fewer VM exits by leveraging shared memory ring buffers.
    
    There is no change in normal behavior with this CL; it is merely that
    the device is added, and tested with unit tests with a client/consumer
    that will behave like our intended opengl/renderthread streams.
    
    The basic interface of address space graphics decouples the code for
    rendering from the transport.
    
    Next step is to introduce the guest graphics / host renderThread sides
    of things.
    
    Documentation is in address_space_graphics_types.h
    
    Test: unit tests only
    
    + now less heap-after-free and leaks
    
    Change-Id: If611f835bdf45ec7ca0e6c691f17fce7fd1cce7b

---
+++ android/android-emu/android/emulation/address_space_graphics.h
@@ -0,0 +1,84 @@
+// Copyright 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "android/emulation/AddressSpaceService.h"
+
+#include "android/base/ring_buffer.h"
+#include "android/base/synchronization/MessageChannel.h"
+#include "android/base/threads/FunctorThread.h"
+#include "android/emulation/address_space_device.h"
+#include "android/emulation/address_space_graphics_types.h"
+
+#include <functional>
+#include <vector>
+
+namespace android {
+namespace emulation {
+namespace asg {
+
+struct Allocation {
+    char* buffer = nullptr;
+    size_t blockIndex = 0;
+    uint64_t offsetIntoPhys = 0;
+    uint64_t size = 0;
+};
+
+class AddressSpaceGraphicsContext : public AddressSpaceDeviceContext {
+public:
+    AddressSpaceGraphicsContext();
+    ~AddressSpaceGraphicsContext();
+
+    static void setConsumer(ConsumerInterface);
+    static void init(const address_space_device_control_ops *ops);
+    static void clear();
+
+    void perform(AddressSpaceDevicePingInfo *info) override;
+    AddressSpaceDeviceType getDeviceType() const override;
+
+    void save(base::Stream* stream) const override;
+    bool load(base::Stream* stream) override;
+
+private:
+    // For consumer communication
+    enum ConsumerCommand {
+        Wakeup = 0,
+        Sleep = 1,
+        Exit = 2,
+    };
+
+    // For ConsumerCallbacks
+    int onUnavailableRead();
+
+    // Data layout
+    uint32_t mVersion = 1;
+    Allocation mRingAllocation;
+    Allocation mBufferAllocation;
+    struct asg_context mHostContext = {};
+
+    // Consumer storage
+    ConsumerCallbacks mConsumerCallbacks;
+    ConsumerInterface mConsumerInterface;
+    void* mCurrentConsumer = 0;
+
+    // Communication with consumer
+    base::MessageChannel<ConsumerCommand, 4> mConsumerMessages;
+    uint32_t mExiting = 0;
+    // For onUnavailableRead
+    uint32_t mUnavailableReadCount = 0;
+};
+
+}  // namespace asg
+}  // namespace android
+}  // namespace emulation
