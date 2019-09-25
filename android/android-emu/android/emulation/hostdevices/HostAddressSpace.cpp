ef9ef753a4ae19b4fc07e38d8df060fa6429f4af
Lingfeng Yang <lfy@google.com>
2019-09-24 21:05:26 -0700

    [address-space] Allow host-owned allocations
    This lets us easily share host allocations to the guest
    without taking up a KVM slot for each instance.
    
    Change-Id: I09e085a26e3e3f231eafb9493ddad6c827609a15

--- android/android-emu/android/emulation/hostdevices/HostAddressSpace.cpp
+++ android/android-emu/android/emulation/hostdevices/HostAddressSpace.cpp
@@ -35,6 +35,18 @@ public:
     Impl() : mControlOps(get_address_space_device_control_ops()),
              mPhysicalOffsetAllocator(0, 16ULL * 1024ULL * 1048576ULL, 4096) { }
 
+    void clear() {
+        std::vector<uint32_t> handlesToClose;
+        for (auto it : mEntries) {
+            handlesToClose.push_back(it.first);
+        }
+        for (auto handle : handlesToClose) {
+            close(handle);
+        }
+        mSharedRegions.clear();
+        mPhysicalOffsetAllocator.freeAll();
+    }
+
     uint32_t open() {
         AutoLock lock(mLock);
         uint32_t handle = mControlOps->gen_handle();
@@ -58,20 +70,12 @@ public:
 
     uint64_t allocBlock(uint32_t handle, size_t size, uint64_t* physAddr) {
         AutoLock lock(mLock);
-        uint64_t off = (uint64_t)(uintptr_t)mPhysicalOffsetAllocator.alloc(size);
-        auto& entry = mEntries[handle];
-        auto& block = entry.blocks[off];
-        block.size = size;
-        (void)block;
-        *physAddr = mPciStart + off;
-        return off;
+        return allocBlockLocked(handle, size, physAddr);
     }
 
     void freeBlock(uint32_t handle, uint64_t off) {
         AutoLock lock(mLock);
-        auto& entry = mEntries[handle];
-        entry.blocks.erase(off);
-        mPhysicalOffsetAllocator.free((void*)(uintptr_t)off);
+        freeBlockLocked(handle, off);
     }
 
     void setHostAddr(uint32_t handle, size_t off, void* hva) {
@@ -91,8 +95,13 @@ public:
             for (auto &it2 : it.second.blocks) {
                 if (it2.first == off) {
                     it2.second.hva = hva;
-                    return;
                 }
+            }
+        }
+
+        for (auto &it : mSharedRegions) {
+            if (it.first == off) {
+                it.second.hva = hva;
             }
         }
     }
@@ -107,8 +116,13 @@ public:
             for (auto &it2 : it.second.blocks) {
                 if (it2.first == off) {
                     it2.second.hva = nullptr;
-                    return;
                 }
+            }
+        }
+
+        for (auto &it : mSharedRegions) {
+            if (it.first == off) {
+                it.second.hva = nullptr;
             }
         }
     }
@@ -132,7 +146,17 @@ public:
             }
         }
 
+        for (auto &it : mSharedRegions) {
+            if (it.first == off) {
+                return it.second.hva;
+            }
+        }
+
         return nullptr;
+    }
+
+    uint64_t offsetToPhysAddr(uint64_t offset) const {
+        return mPciStart + offset;
     }
 
     void ping(uint32_t handle, AddressSpaceDevicePingInfo* pingInfo) {
@@ -149,6 +173,49 @@ public:
         memcpy(pingInfo, entry.pingInfo, sizeof(AddressSpaceDevicePingInfo));
     }
 
+    int claimShared(uint32_t handle, uint64_t off) {
+        auto& entry = mEntries[handle];
+
+        if (mSharedRegions.find(off) == mSharedRegions.end()) {
+            fprintf(stderr, "%s: failed, no shared region at offset 0x%llx\n", __func__,
+                    (unsigned long long)off);
+            return -EINVAL;
+        }
+
+        auto& block = mSharedRegions[off];
+
+        if (entry.blocks.find(off) != entry.blocks.end()) {
+            fprintf(stderr, "%s: failed, entry already owns offset 0x%llx\n", __func__,
+                    (unsigned long long)off);
+            return -EINVAL;
+        }
+
+        auto& entryBlock = entry.blocks[off];
+        entryBlock.size = block.size;
+        return 0;
+    }
+
+    int unclaimShared(uint32_t handle, uint64_t off) {
+        auto& entry = mEntries[handle];
+
+        if (mSharedRegions.find(off) == mSharedRegions.end()) {
+            fprintf(stderr, "%s: failed, no shared region at offset 0x%llx\n", __func__,
+                    (unsigned long long)off);
+            return -EINVAL;
+        }
+
+        auto& block = mSharedRegions[off];
+
+        if (entry.blocks.find(off) == entry.blocks.end()) {
+            fprintf(stderr, "%s: failed, entry does not own offset 0x%llx\n", __func__,
+                    (unsigned long long)off);
+            return -EINVAL;
+        }
+
+        entry.blocks.erase(off);
+        return 0;
+    }
+
     void saveSnapshot(base::Stream* stream) {
         emulation::goldfish_address_space_memory_state_save(stream);
     }
@@ -157,7 +224,73 @@ public:
         emulation::goldfish_address_space_memory_state_load(stream);
     }
 
+    // Simulated host interface
+    int allocSharedHostRegion(uint64_t page_aligned_size, uint64_t* offset) {
+
+        if (!offset) return -EINVAL;
+
+        AutoLock lock(mLock);
+
+        return allocSharedHostRegionLocked(page_aligned_size, offset);
+    }
+
+    int allocSharedHostRegionLocked(uint64_t page_aligned_size, uint64_t* offset) {
+        if (!offset) return -EINVAL;
+
+        uint64_t off = (uint64_t)(uintptr_t)mPhysicalOffsetAllocator.alloc(page_aligned_size);
+        auto& block = mSharedRegions[off];
+        block.size = page_aligned_size;
+        (void)block;
+        *offset = off;
+        return 0;
+    }
+
+    int freeSharedHostRegion(uint64_t offset) {
+        AutoLock lock(mLock);
+        return freeSharedHostRegionLocked(offset);
+    }
+
+    int freeSharedHostRegionLocked(uint64_t offset) {
+        if (mSharedRegions.find(offset) == mSharedRegions.end()) {
+            fprintf(stderr, "%s: could not free shared region, offset 0x%llx is not a start\n", __func__,
+                    (unsigned long long)offset);
+            return -EINVAL;
+        }
+
+        mSharedRegions.erase(offset);
+        mPhysicalOffsetAllocator.free((void*)(uintptr_t)offset);
+
+        return 0;
+    }
+
+    uint64_t getPhysAddrStart() {
+        AutoLock lock(mLock);
+        return getPhysAddrStartLocked();
+    }
+
+    uint64_t getPhysAddrStartLocked() {
+        return mPciStart;
+    }
+
 private:
+    uint64_t allocBlockLocked(uint32_t handle, size_t size, uint64_t* physAddr) {
+        uint64_t off = (uint64_t)(uintptr_t)mPhysicalOffsetAllocator.alloc(size);
+        auto& entry = mEntries[handle];
+        auto& block = entry.blocks[off];
+        block.size = size;
+        (void)block;
+        *physAddr = mPciStart + off;
+        return off;
+    }
+
+    void freeBlockLocked(uint32_t handle, uint64_t off) {
+        auto& entry = mEntries[handle];
+        entry.blocks.erase(off);
+        mPhysicalOffsetAllocator.free((void*)(uintptr_t)off);
+    }
+
+    bool mInitialized = false;
+
     Lock mLock;
     address_space_device_control_ops* mControlOps = nullptr;
     uint64_t mPciStart = 0x0101010100000000;
@@ -175,6 +308,7 @@ private:
     };
 
     std::unordered_map<uint32_t, Entry> mEntries;
+    std::unordered_map<uint64_t, BlockMemory> mSharedRegions;
 };
 
 static android::base::LazyInstance<HostAddressSpaceDevice> sHostAddressSpace =
@@ -185,7 +319,9 @@ HostAddressSpaceDevice::HostAddressSpaceDevice() :
 
 // static
 HostAddressSpaceDevice* HostAddressSpaceDevice::get() {
-    return sHostAddressSpace.ptr();
+    auto res = sHostAddressSpace.ptr();
+    res->initialize();
+    return res;
 }
 
 uint32_t HostAddressSpaceDevice::open() {
@@ -220,8 +356,20 @@ void* HostAddressSpaceDevice::getHostAddr(uint64_t physAddr) {
     return mImpl->getHostAddr(physAddr);
 }
 
+uint64_t HostAddressSpaceDevice::offsetToPhysAddr(uint64_t offset) const {
+    return mImpl->offsetToPhysAddr(offset);
+}
+
 void HostAddressSpaceDevice::ping(uint32_t handle, AddressSpaceDevicePingInfo* pingInfo) {
     mImpl->ping(handle, pingInfo);
+}
+
+int HostAddressSpaceDevice::claimShared(uint32_t handle, uint64_t off) {
+    return mImpl->claimShared(handle, off);
+}
+
+int HostAddressSpaceDevice::unclaimShared(uint32_t handle, uint64_t off) {
+    return mImpl->unclaimShared(handle, off);
 }
 
 void HostAddressSpaceDevice::saveSnapshot(base::Stream* stream) {
@@ -230,6 +378,54 @@ void HostAddressSpaceDevice::saveSnapshot(base::Stream* stream) {
 
 void HostAddressSpaceDevice::loadSnapshot(base::Stream* stream) {
     mImpl->loadSnapshot(stream);
+}
+
+// static
+HostAddressSpaceDevice::Impl* HostAddressSpaceDevice::getImpl() {
+    return HostAddressSpaceDevice::get()->mImpl.get();
+}
+
+int HostAddressSpaceDevice::allocSharedHostRegion(uint64_t page_aligned_size, uint64_t* offset) {
+    return HostAddressSpaceDevice::getImpl()->allocSharedHostRegion(page_aligned_size, offset);
+}
+
+int HostAddressSpaceDevice::freeSharedHostRegion(uint64_t offset) {
+    return HostAddressSpaceDevice::getImpl()->freeSharedHostRegion(offset);
+}
+
+int HostAddressSpaceDevice::allocSharedHostRegionLocked(uint64_t page_aligned_size, uint64_t* offset) {
+    return HostAddressSpaceDevice::getImpl()->allocSharedHostRegionLocked(page_aligned_size, offset);
+}
+
+int HostAddressSpaceDevice::freeSharedHostRegionLocked(uint64_t offset) {
+    return HostAddressSpaceDevice::getImpl()->freeSharedHostRegionLocked( offset);
+}
+
+uint64_t HostAddressSpaceDevice::getPhysAddrStart() {
+    return HostAddressSpaceDevice::getImpl()->getPhysAddrStart();
+}
+
+uint64_t HostAddressSpaceDevice::getPhysAddrStartLocked() {
+    return HostAddressSpaceDevice::getImpl()->getPhysAddrStartLocked();
+}
+
+static const AddressSpaceHwFuncs sAddressSpaceHwFuncs = {
+    &HostAddressSpaceDevice::allocSharedHostRegion,
+    &HostAddressSpaceDevice::freeSharedHostRegion,
+    &HostAddressSpaceDevice::allocSharedHostRegionLocked,
+    &HostAddressSpaceDevice::freeSharedHostRegionLocked,
+    &HostAddressSpaceDevice::getPhysAddrStart,
+    &HostAddressSpaceDevice::getPhysAddrStartLocked,
+};
+
+void HostAddressSpaceDevice::initialize() {
+    if (mInitialized) return;
+    address_space_set_hw_funcs(&sAddressSpaceHwFuncs);
+    mInitialized = true;
+}
+
+void HostAddressSpaceDevice::clear() {
+    mImpl->clear();
 }
 
 } // namespace android
