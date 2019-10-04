c2a4ab97a92fd231ba96baeb841dfb3c93f5a697
Lingfeng Yang <lfy@google.com>
2019-10-03 17:21:23 -0700

    ring stream
    Change-Id: If2af7d17a2757c2d84021db7205140c10859e497

--- android/android-emu/android/emulation/address_space_graphics.h
+++ android/android-emu/android/emulation/address_space_graphics.h
@@ -20,6 +20,7 @@
 #include "android/base/threads/FunctorThread.h"
 #include "android/emulation/address_space_device.h"
 
+#include <functional>
 #include <vector>
 
 namespace android {
@@ -27,6 +28,30 @@ namespace emulation {
 
 class AddressSpaceGraphicsContext : public AddressSpaceDeviceContext {
 public:
+    // One control page,
+    // two rings per context (guest to host, host to guest),
+    // each of
+    // AddressSpaceGraphicsContext::kMaxContexts contexts
+    static const int kPageSize;
+    static const int kRingInfoSize;
+    static const int kRingSize;
+    static const int kMaxContexts;
+    static const int kContextAllocationSize;
+    static const int kBackingSize;
+    static const int kToHostRingInfoOffset;
+    static const int kFromHostRingInfoOffset;
+    static const int kToHostRingBufferOffset;
+    static const int kFromHostRingBufferOffset;
+
+    using OnUnavailableReadCallback =
+        std::function<int()>;
+    using ConsumerCreateCallback =
+        std::function<void* (struct ring_buffer_with_view,
+                             struct ring_buffer_with_view,
+                             OnUnavailableReadCallback)>;
+    using ConsumerDestroyCallback =
+        std::function<void(void*)>;
+
     enum Command {
         AllocOrGetOffset = 0,
         GetSize = 1,
@@ -34,8 +59,11 @@ public:
         NotifyAvailable = 3,
         Echo = 4,
         EchoAsync = 5,
-        EchoAsyncStop = 6,
-        EchoAsyncWithHangup = 7,
+        EchoAsyncStart = 6,
+        EchoAsyncStop = 7,
+        EchoAsyncWithHangup = 8,
+        CreateConsumer = 9,
+        DestroyConsumer = 10,
     };
 
     static void init(const address_space_device_control_ops *ops);
@@ -47,13 +75,13 @@ public:
     void perform(AddressSpaceDevicePingInfo *info) override;
 
     AddressSpaceDeviceType getDeviceType() const override;
+
+    void setConsumer(
+        ConsumerCreateCallback createFunc,
+        ConsumerDestroyCallback destroyFunc);
+
     void save(base::Stream* stream) const override;
     bool load(base::Stream* stream) override;
-
-    struct Ring {
-        ring_buffer* ring;
-        ring_buffer_view view;
-    };
 
 private:
     enum ThreadCommand {
@@ -72,12 +100,14 @@ private:
     ConsumerState consumeReadbackLoop(ConsumerState);
     void threadFunc();
 
+    int onUnavailableRead();
+
     char* mBuffer = 0;
     size_t mReadPos = 0;
     std::vector<char> mReadBuffer;
 
-    Ring mToHost;
-    Ring mFromHost;
+    struct ring_buffer_with_view mToHost;
+    struct ring_buffer_with_view mFromHost;
 
     uint32_t mRingVersion;
     base::FunctorThread mThread;
@@ -85,6 +115,13 @@ private:
     base::MessageChannel<ThreadCommand, 4> mThreadReturnMessages;
     ConsumerState* mConsumerStatePtr;
     uint32_t mExiting = 0;
+
+    // For general render threads
+    void* mCurrentConsumer = 0;
+    ConsumerCreateCallback mConsumerCreateFunc;
+    ConsumerDestroyCallback mConsumerDestroyFunc;
+    uint32_t mUnavailableReadCount = 0;
+
 };
 
 }  // namespace emulation
