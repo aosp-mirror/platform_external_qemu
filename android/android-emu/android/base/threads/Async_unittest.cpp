a96650f032bb7eb73f5156e87d99bfe7cc40822c
Lingfeng Yang <lfy@google.com>
2019-10-05 15:43:45 -0700

    adb: stop using the qt thread
    if an adb operation is taking a long time,
    then the entire emulator hangs.
    
    Change-Id: I254be042614a1547ea22b84fe553ba2f806999ba

--- android/android-emu/android/base/threads/Async_unittest.cpp
+++ android/android-emu/android/base/threads/Async_unittest.cpp
@@ -14,6 +14,8 @@
 
 #include "android/base/threads/Async.h"
 
+#include "android/base/async/Looper.h"
+
 #include <gtest/gtest.h>
 
 namespace android {
@@ -23,6 +25,27 @@ TEST(AsyncTest, Simple) {
     for (int i = 0; i < 10; i++) {
         async([] { });
     }
+}
+
+// Tests that we can create and destroy the async thread with looper.
+TEST(AsyncThreadWithLooper, Simple) {
+    AsyncThreadWithLooper thread;
+}
+
+// Tests that we can send a few simple tasks to the async thread with looper.
+TEST(AsyncThreadWithLooper, Tasks) {
+    AsyncThreadWithLooper thread;
+    auto looper = thread.getLooper();
+    auto timer1 = looper->createTimer(
+        [](void* opaque, Looper::Timer* timer) {
+            fprintf(stderr, "%s: timer1\n", __func__);
+        }, 0);
+    timer1->startAbsolute(0);
+    auto timer2 = looper->createTimer(
+        [](void* opaque, Looper::Timer* timer) {
+            fprintf(stderr, "%s: timer2\n", __func__);
+        }, 0);
+    timer2->startAbsolute(0);
 }
 
 }  // namespace base
