a08cf409279f5d8cea3041d02b0cc4a047c82cff
Erwin Jansen <jansene@google.com>
2019-10-07 16:12:03 -0700

    [gRPC] Add support for certificates
    The gRPC endpoint will now look for the following certificates:
    
    ~/.android/emulator-grpc.cer
    ~/.android/emulator-grpc.key
    
    These certificates will be installed if they are available, if they are
    not available the endpoint will be launched insecurely.
    
    An insecure endpoint will bind to 127.0.0.1 (loopback address), and will
    not be publicly visible.
    
    Clients that wish to connect will have to offer a valid .cer. The
    keystrokes example demonstrates how this will work.
    
    Follow the steps here:
    https://letsencrypt.org/docs/certificates-for-localhost/ to create your
    own self signed certificate for localhost.
    
    Change-Id: Ib68a58cba80d1c9be10f850dcfe6c3c92f80d662
    Bug: b/142281541
    Test: python -m sample.keystrokes succeeds after installing
    certificates.

--- android/android-grpc/docs/grpc-samples/python/sample/keystrokes.py
+++ android/android-grpc/docs/grpc-samples/python/sample/keystrokes.py
@@ -18,11 +18,38 @@ import time
 from . import emulator_controller_pb2
 from . import emulator_controller_pb2_grpc
 from google.protobuf import empty_pb2
+import os
 
 _EMPTY_ = empty_pb2.Empty()
 
-# Open a grpc channel
-channel = grpc.insecure_channel('localhost:5556')
+
+def getChannel():
+  """Opens up a grpc an (in)secure channel to localhost.
+
+  If a cert is available in ~/.android then
+  this expects that the emulator is using a self signed cert:
+  made as follows:
+
+  openssl req -x509 -out localhost.crt -keyout localhost.key \
+      -newkey rsa:2048 -nodes -sha256 \
+      -subj '/CN=localhost' -extensions EXT -config <( \
+     printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+  """
+
+  cert = os.path.join(os.path.expanduser("~"), '.android', 'emulator-grpc.cer')
+  if os.path.exists(cert):
+    with open(cert, 'rb') as f:
+      creds = grpc.ssl_channel_credentials(f.read())
+      channel = grpc.secure_channel('localhost:5556', creds)
+  else:
+    # Let's go insecure..
+    channel = grpc.insecure_channel('localhost:5556')
+
+
+
+  return channel
+
+channel = getChannel()
 
 # Create a client
 stub = emulator_controller_pb2_grpc.EmulatorControllerStub(channel)
