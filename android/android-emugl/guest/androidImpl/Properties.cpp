9478401c474ccc1b893dd414b260c937e75b1baa
Lingfeng Yang <lfy@google.com>
2019-10-12 16:42:02 -0700

    [android-guest-impl] Support setting system properties
    bug: 140112486
    
    This is useful for later when we want to select between
    different gltransport types getting coverage in unit tests.
    
    Change-Id: I8f935d47c1b1078edaab6745f4fe587fc18b0651

--- android/android-emugl/guest/androidImpl/Properties.cpp
+++ android/android-emugl/guest/androidImpl/Properties.cpp
@@ -15,27 +15,67 @@
 
 #include "AndroidHostCommon.h"
 
+#include "android/base/memory/LazyInstance.h"
+#include "android/base/synchronization/Lock.h"
+
+#include <unordered_map>
 #include <string.h>
+
+using android::base::AutoLock;
+using android::base::LazyInstance;
+using android::base::Lock;
+
+class AndroidSystemProperties {
+public:
+    AndroidSystemProperties() = default;
+    ~AndroidSystemProperties() = default;
+
+    Lock mutex;
+    std::unordered_map<std::string, std::string> props;
+};
+
+static LazyInstance<AndroidSystemProperties> sProps = LAZY_INSTANCE_INIT;
 
 extern "C" {
 
 EXPORT int property_get(const char* key, char* value, const char* default_value) {
-    const char ro_kernel_qemu_gles_prop[] = "1";
-    const char qemu_sf_lcd_density[] = "420";
-    int len = 0;
+    auto props = sProps.ptr();
 
-    if (!strcmp(key, "ro.kernel.qemu.gles")) {
-        len = strlen(ro_kernel_qemu_gles_prop);
-        memcpy(value, ro_kernel_qemu_gles_prop, len + 1);
-    } else if (!strcmp(key, "qemu.sf.lcd_density")) {
-        len = strlen(qemu_sf_lcd_density);
-        memcpy(value, qemu_sf_lcd_density, len + 1);
-    } else if (default_value) {
-        len = strlen(qemu_sf_lcd_density);
-        memcpy(value, default_value, len + 1);
+    AutoLock lock(props->mutex);
+
+    auto it = props->props.find(key);
+
+    if (it == props->props.end()) {
+
+        if (!default_value) return 0;
+
+        size_t totalStrlen = strlen(default_value);
+        size_t maxPropStrlen = PROPERTY_VALUE_MAX - 1;
+        size_t copyStrlen =
+            maxPropStrlen < totalStrlen ? maxPropStrlen : totalStrlen;
+        memcpy(value, default_value, copyStrlen);
+        value[copyStrlen] = '\0';
+        return copyStrlen;
     }
 
-    return len;
+    const char* storedValue = it->second.data();
+    size_t totalStrlen = it->second.size();
+    size_t maxPropStrlen = PROPERTY_VALUE_MAX - 1;
+    size_t copyStrlen =
+        maxPropStrlen < totalStrlen ? maxPropStrlen : totalStrlen;
+    memcpy(value, storedValue, copyStrlen);
+    value[copyStrlen] = '\0';
+
+    return (int)copyStrlen;
+}
+
+EXPORT int property_set(const char *key, const char *value) {
+    auto props = sProps.ptr();
+    AutoLock lock(props->mutex);
+
+    props->props[key] = value;
+
+    return 0;
 }
 
 } // extern "C"
