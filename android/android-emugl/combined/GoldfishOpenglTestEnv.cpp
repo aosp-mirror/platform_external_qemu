7e242ac6df78e5b6132c66287e5d08157fc645bf
Lingfeng Yang <lfy@google.com>
2019-10-23 08:48:56 -0700

    [gl] Add gl/rc tracing to guest
    + re-enable asg in combined unittests
    
    Change-Id: Ibd1d7cb82ea7779f0e0a80febdf353dd48937392

--- android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
+++ android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
@@ -85,7 +85,7 @@ static AndroidOptions sTestEnvCmdLineOptions;
 
 // static
 std::vector<const char*> GoldfishOpenglTestEnv::getTransportsToTest() {
-    return { "pipe", };
+    return { "asg", "pipe" };
 }
 
 GoldfishOpenglTestEnv::GoldfishOpenglTestEnv() {
