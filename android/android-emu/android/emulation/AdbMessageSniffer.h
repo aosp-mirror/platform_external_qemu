05e2dc798435e3c6219ba7eb8fa872d03ee4fe4c
yahan <yahan@google.com>
2019-10-24 15:12:59 -0700

    [WIP] Postmortem implementation in emulator
    Change-Id: I71052d1eaf0c1ad128d0ab6d52505a0bd61ede46

--- android/android-emu/android/emulation/AdbMessageSniffer.h
+++ android/android-emu/android/emulation/AdbMessageSniffer.h
@@ -38,10 +38,10 @@ struct amessage {
 struct apacket
 {
     amessage mesg;
-    uint8_t data[1024];
+    uint8_t data[1024 * 1024];
 };
 
-    AdbMessageSniffer(const char* name);
+    AdbMessageSniffer(const char* name, void* pipe);
     ~AdbMessageSniffer();
 
     void read(const AndroidPipeBuffer*, int numBuffers, int count);
@@ -53,6 +53,7 @@ private:
     uint8_t* mBuffer;
     uint8_t* mBufferP;
     const char* mName;
+    const void* mPipe;
     std::set<unsigned> mDummyShellArg0;
 
     void grow_buffer_if_needed(int count);
