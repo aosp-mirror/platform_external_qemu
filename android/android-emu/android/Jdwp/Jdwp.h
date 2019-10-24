5f7ab33fcafdf0d6f7721e17945bd74fa7efdecd
yahan <yahan@google.com>
2019-10-24 15:12:59 -0700

    [WIP] Postmortem implementation in emulator
    Change-Id: I71052d1eaf0c1ad128d0ab6d52505a0bd61ede46

---
+++ android/android-emu/android/Jdwp/Jdwp.h
@@ -0,0 +1,169 @@
+// Copyright 2019 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace android {
+namespace jdwp {
+
+template <typename _T>
+_T readValFromBuffer(const uint8_t* buffer, int bytes) {
+    _T val = 0;
+    for (int i = 0; i < bytes; i ++) {
+        val |= buffer[i] << ((bytes - i - 1) << 3);
+    }
+    return val;
+}
+
+inline uint32_t uint32FromBuffer(const uint8_t* buffer) {
+    return readValFromBuffer<uint32_t>(buffer, 4);
+}
+
+template <typename _T>
+uint8_t* writeValToBuffer(uint8_t* buffer, _T val) {
+    const size_t bytes = sizeof(val);
+    for (size_t i = 0; i < bytes; i++) {
+        buffer[0] = val >> ((bytes - i - 1) << 3) & 0xff;
+        buffer ++;
+    }
+    return buffer;
+}
+
+inline uint8_t* writeStrToBuffer(uint8_t* buffer, const char* str) {
+    uint32_t len = strlen(str);
+    buffer = writeValToBuffer(buffer, (uint32_t)len);
+    memcpy(buffer, str, len);
+    buffer += len;
+    return buffer;
+}
+
+struct JdwpCommandHeader {
+    uint32_t length;
+    uint32_t id;
+    uint8_t flags;
+    uint8_t command_set;
+    uint8_t command;
+    JdwpCommandHeader(const uint8_t* buffer) {
+        length = uint32FromBuffer(buffer);
+        id = uint32FromBuffer(buffer + 4);
+        flags = buffer[8];
+        command_set = buffer[9];
+        command = buffer[10];
+    }
+    JdwpCommandHeader() {}
+    void writeToBuffer(uint8_t* buffer) {
+        writeValToBuffer(buffer, length);
+        writeValToBuffer(buffer + 4, id);
+        buffer[8] = flags;
+        buffer[9] = command_set;
+        buffer[10] = command;
+    }
+};
+
+struct JdwpIdSize {
+    int32_t field_id_size;
+    int32_t	method_id_size;
+    int32_t	object_id_size;
+    int32_t	reference_typ_id_size;
+    int32_t	frame_id_size;
+    void parseFrom(const uint8_t* buffer) {
+        field_id_size = uint32FromBuffer(buffer);
+        method_id_size = uint32FromBuffer(buffer + 4);
+        object_id_size = uint32FromBuffer(buffer + 8);
+        reference_typ_id_size = uint32FromBuffer(buffer + 12);
+        frame_id_size = uint32FromBuffer(buffer + 16);
+    }
+};
+
+struct JdwpEventRequestSet {
+    struct JdwpClassMatch {
+        std::string clazz;
+    };
+    uint8_t event_kind;
+    uint8_t suspend_policy;
+    //std::vector<JdwpClassMatch> class_matches;
+    int writeToBuffer(uint8_t* buffer, bool break_on_uncaught_exception = false,
+        JdwpIdSize* id_size = nullptr) {
+        const uint8_t* buffer_head_ptr = buffer;
+        *(buffer++) = event_kind;
+        *(buffer++) = suspend_policy;
+        uint8_t* request_num_pst = buffer;
+        buffer += 4;
+        uint32_t request_num = 0;
+        uint32_t total_len = 6;
+        if (break_on_uncaught_exception) {
+            request_num ++;
+            // Case ExceptionOnly - if modKind is 8:
+            //      referenceTypeID	exceptionOrNull
+            //      boolean	caught
+            //      boolean	uncaught
+            *(buffer++) = 8;
+            memset(buffer, 0, id_size->reference_typ_id_size);
+            buffer += id_size->reference_typ_id_size;
+            *(buffer++) = 1;
+            *(buffer++) = 1;
+        }
+        //buffer = buffer + total_len;
+        //for (const auto& it : class_matches) {
+        //    total_len += it.clazz.length() + 1;
+        //    memcpy(buffer, it.clazz.c_str(), it.clazz.length() + 1);
+        //    buffer += it.clazz.length() + 1;
+        //}
+        writeValToBuffer(request_num_pst, request_num);
+        return buffer - buffer_head_ptr;
+    }
+};
+
+struct JdwpAllClasses {
+    struct JdwpClass {
+        uint8_t ref_type_tag;
+        uint64_t type_id; // Actual size is referenceTypeID
+        std::string signature;
+        int32_t status;
+    };
+    std::vector<JdwpClass> classes;
+    void parseFrom(const uint8_t* buffer, const JdwpIdSize& id_size) {
+        uint32_t class_num = uint32FromBuffer(buffer);
+        buffer += 4;
+        classes.resize(class_num);
+        for (auto& clazz : classes) {
+            clazz.ref_type_tag = *(buffer ++);
+            clazz.type_id = readValFromBuffer<uint64_t>(buffer,
+                id_size.reference_typ_id_size);
+            buffer += id_size.reference_typ_id_size;
+            size_t string_len = readValFromBuffer<size_t>(buffer,
+                4);
+            buffer += 4;
+            clazz.signature = std::string((char*)buffer, string_len);
+            buffer += string_len;
+            clazz.status = readValFromBuffer<int32_t>(buffer, 4);
+            buffer += 4;
+        }
+    }
+};
+
+/*struct JdwpCompositeCommand {
+    struct Event {
+        uint8_t event_kind;
+        int32_t request_id;
+
+    };
+    uint8_t suspend_policy;
+    int32_t event_id;
+    std::vector<>
+}*/
+
+}
+}
