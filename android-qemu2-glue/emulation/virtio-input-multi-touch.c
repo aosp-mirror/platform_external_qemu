34a84bf3d49c22edae70df5c20265df1081467ba
Weilun Du <wdu@google.com>
2019-10-30 14:18:59 -0700

    [virtio-input-multi-touch] Fixed bug where fold button doesn't work
    s_current_virtio_input could be null pointer if no touch events had
    occurred before fold button is clicked, thus causing a no-op.
    
    BUG: 143174688
    
    Change-Id: I6e9eda37fdd23bfa0677fb5a1d229856465dff6b
    Signed-off-by: Weilun Du <wdu@google.com>

--- android-qemu2-glue/emulation/virtio-input-multi-touch.c
+++ android-qemu2-glue/emulation/virtio-input-multi-touch.c
@@ -49,9 +49,9 @@
     OBJECT_CHECK(VirtIOInputMultiTouchPCI##num, (obj), \
                  TYPE_VIRTIO_INPUT_MULTI_TOUCH_PCI(num))
 
-static VirtIOInput* s_current_virtio_input = NULL;
-
 static VirtIOInput* s_virtio_input_multi_touch[VIRTIO_INPUT_MAX_NUM];
+
+static VirtIOInput* s_current_virtio_input = NULL;
 
 static void translate_mouse_event(int x, int y, int buttons_state) {
     int pressure = multitouch_is_touch_down(buttons_state) ? 0x81 : 0;
@@ -194,6 +194,7 @@ static struct virtio_input_config multi_touch_config[] = {
                     1,
                 },
         },
+
         {/* end of list */},
 };
 
@@ -321,7 +322,11 @@ int android_virtio_input_send(int type, int code, int value) {
         return 0;
     }
 
-    VirtIOInput* vinput = VIRTIO_INPUT(s_current_virtio_input);
+    VirtIOInput* vinput = NULL;
+    if (!s_current_virtio_input) {
+        s_current_virtio_input = s_virtio_input_multi_touch[0];
+    }
+    vinput = VIRTIO_INPUT(s_current_virtio_input);
     if (vinput == NULL) {
         return 1;
     }
