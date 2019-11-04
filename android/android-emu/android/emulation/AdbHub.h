1ffe1b4eb409be900fe8b8952d3b25b106874cec
yahan <yahan@google.com>
2019-11-04 10:18:48 -0800

    Adb hub and Jdwp proxy
    This CL implements Adb hub and Jdwp proxy for icebox project. The design
    doc is here: go/aemu-adb-proxy.
    
    For regular adb traffic, the behavior of adb guest pipe will not change.
    For adb traffic from the icebox port, adb guest pipe will initialize adb
    hub, and let it handle all traffic.
    
    Adb hub might own multiple adb proxy (which is an abstract class).
    Currently jdwp proxy is the only implementation for adb proxy, but it is
    designed to be able to extend to other kinds of traffics. Before
    snapshot, adb hub and jdwp proxy is doing nothing other than book
    keeping. After a snapshot load, the emulator will load up icebox
    connections and the corresponding adb hubs/proxies from the previous
    session. The first adb connection request after a snapshot load will be
    wired up with the existing adb pipe, and adb hub/proxies will handle
    handshakes and package header translations so that the guest will think
    it is using the previous connection, while the host thinks it is making
    a new connection.
    
    This would allow us to reconnect to a jdwp debugger without interruption
    after a snapshot load.
    
    Change-Id: Ie02503da970b0f6bcfda351bb9c79a6f1c37d369

---
+++ android/android-emu/android/emulation/AdbHub.h
@@ -0,0 +1,82 @@
+// Copyright 2019 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+#include "android/base/async/Looper.h"
+#include "android/base/files/Stream.h"
+#include "android/emulation/AdbProxy.h"
+#include "android/emulation/android_pipe_common.h"
+#include "android/emulation/apacket_utils.h"
+#include "android/jdwp/JdwpProxy.h"
+
+namespace android {
+namespace emulation {
+
+class AdbHub {
+public:
+    typedef std::function<void(int)> WakePipeFunc;
+    void onSave(android::base::Stream* stream);
+    void onLoad(android::base::Stream* stream);
+    int onGuestSendData(const AndroidPipeBuffer* buffers, int numBuffers);
+    int onGuestRecvData(AndroidPipeBuffer* buffers, int numBuffers);
+    // Only used on load
+    // Should be called when the pipe start proxying data
+    int getProxyCount();
+    android::base::Looper::FdWatch* mFdWatcher = nullptr;
+    void setWakePipeFunc(WakePipeFunc wakePipe);
+    unsigned onGuestPoll() const;
+    void onHostSocketEvent(int fd,
+                           unsigned events,
+                           std::function<void()> onSocketClose);
+    bool socketWantRead();
+    bool socketWantWrite();
+
+private:
+    int sendToHost(const AndroidPipeBuffer* buffers, int numBuffers);
+    int recvFromHost(AndroidPipeBuffer* buffers, int numBuffers);
+    void checkRemoveProxy(std::unordered_map<int, AdbProxy*>::iterator proxy);
+    AdbProxy* onNewConnection(const apacket& requestPacket,
+                              const apacket& replyPacket);
+    AdbProxy* tryReuseConnection(const apacket& packet);
+    void pushToSendQueue(apacket&& packet);
+    void pushToRecvQueue(apacket&& packet);
+    int readSocket(int fd);
+    int writeSocket(int fd);
+    std::unordered_map<int, apacket> mPendingConnections;
+    std::unordered_map<int, AdbProxy*> mProxies;
+    // Only used on load
+    std::queue<apacket> mSendToHostQueue;
+    apacket mCurrentGuestSendPacket;
+    size_t mCurrentGuestSendPacketPst = 0;
+    apacket mCurrentHostSendPacket;
+    int mCurrentHostSendPacketPst = -1;
+    std::queue<apacket> mRecvFromHostQueue;
+    bool mWantRecv = false;
+    apacket mCurrentGuestRecvPacket;
+    int mCurrentGuestRecvPacketPst = -1;
+
+    apacket mCurrentHostRecvPacket;
+    size_t mCurrentHostRecvPacketPst = 0;
+    std::unordered_map<int, std::unique_ptr<jdwp::JdwpProxy>> mJdwpProxies;
+    WakePipeFunc mWakePipe = nullptr;
+    emulation::apacket mCnxnPacket;
+    bool mShouldReconnect = false;
+};
+}  // namespace emulation
+}  // namespace android
