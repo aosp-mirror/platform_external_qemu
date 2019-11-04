6dad113fa9a619f7767f490a2fbfec77e0992d14
yahan <yahan@google.com>
2019-11-04 10:18:48 -0800

    [WIP] Jdwp proxy
    Change-Id: Ie02503da970b0f6bcfda351bb9c79a6f1c37d369

---
+++ android/android-emu/android/emulation/AdbProxy.h
@@ -0,0 +1,32 @@
+// Copyright 2019 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "android/emulation/AdbMessageSniffer.h"
+#include "android/emulation/apacket_utils.h"
+
+namespace android {
+namespace emulation {
+
+class AdbProxy {
+public:
+    virtual bool onRecvDataHeader(const android::emulation::AdbMessageSniffer::amessage* mesg) = 0;
+    virtual void onGuestRecvData(const android::emulation::AdbMessageSniffer::amessage* mesg, const uint8_t* data) = 0;
+    virtual bool needProxySendData(const android::emulation::AdbMessageSniffer::amessage* mesg) = 0;
+    virtual void onGuestSendData(const android::emulation::AdbMessageSniffer::amessage* mesg, const uint8_t* data) = 0;
+    virtual bool shouldClose() const = 0;
+    virtual int32_t guestId() const = 0;
+    virtual int32_t originHostId() const = 0;
+    virtual int32_t currentHostId() const = 0;
+};
+}
+}
