75e0c59041a71127246f32541d969357b4e103e3
Erwin Jansen <jansene@google.com>
2019-11-11 13:41:39 -0800

    Fix breakpad not detecting crashes.
    The previous version of the breakpad could get into a state where it
    was waiting for a message that never arrived. The crashing process
    uses a client that intercepts the crash and sends it to the server.
    It was possible that the client never received this message, and would
    be stuck waiting for an ack.
    
    This was addressed in a series of patches in the prebuilts of breakpad,
    which we have ported to prevent this behavior.
    
    As part of this change wcollect additional binaries
    in the debug_info, zip so we can use these files later for end-to-end
    tests.
    
    We include the lib,lib64 and testdata directory as we need to make sure
    we have all the required dependencies available.
    
    Change-Id: I1749c5cf79aae4aa5450410750aae5792e9558c8
    Test: Includes an e2e test validating we can crash and read symbols.

--- android/build/python/aemu/distribution.py
+++ android/build/python/aemu/distribution.py
@@ -14,18 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
+
+import os
+import re
+import shutil
+import zipfile
 
 from absl import logging
 
 from aemu.definitions import get_qemu_root
-
-import os
-import re
-import zipfile
-import shutil
 
 # These are the set of zip files that will be generated in the distribution directory.
 # You can add additional zip sets here if you need. A file will be added to the given zipfile
@@ -34,24 +32,30 @@ import shutil
 # For naming you can use the {key} pattern, which will replace the given {key} with the value.
 # Most of the absl FLAGS are available.
 zip_sets = {
-    'debug':  # Release type..
-    {
+    "debug": {  # Release type..
         # Look for *.gcda files under the build tree
-        'code-coverage-{target}.zip': ['{build_dir}', r'.*(gcda|gcno)'],
+        "code-coverage-{target}.zip": [("{build_dir}", r".*(gcda|gcno)", "/")],
         # Look for all files under {out}/distribution
-        'sdk-repo-{target}-emulator-full-debug-{sdk_build_number}.zip': ['{build_dir}/distribution', r'.*']
-    },
-
-    'release':
-    {
-        'sdk-repo-{target}-debug-emulator-{sdk_build_number}.zip': [
-            # Look for all files under {out}/build/debug_info/
-            '{build_dir}/build/debug_info', r'.*'
+        "sdk-repo-{target}-emulator-full-debug-{sdk_build_number}.zip": [
+            ("{build_dir}/distribution", r".*", '/')
         ],
-        'sdk-repo-{target}-grpc-samples-{sdk_build_number}.zip': ['{src_dir}/android/android-grpc/docs/grpc-samples/', r'.*'],
+    },
+    "release": {
+        "sdk-repo-{target}-debug-emulator-{sdk_build_number}.zip": [
+            # Look for all files under {out}/build/debug_info/
+            ("{build_dir}/build/debug_info", r".*", '/'),
+            ("{build_dir}/lib64", r".*", '/tests/lib64/'),
+            ("{build_dir}/lib", r".*", '/tests/lib/'),
+            ("{build_dir}/testdata", r".*", '/tests/testdata/'),
+        ],
+        "sdk-repo-{target}-grpc-samples-{sdk_build_number}.zip": [
+            ("{src_dir}/android/android-grpc/docs/grpc-samples/", r".*", '/')
+        ],
         # Look for all files under {out}/distribution
-        'sdk-repo-{target}-emulator-{sdk_build_number}.zip': ['{build_dir}/distribution', r'.*']
-    }
+        "sdk-repo-{target}-emulator-{sdk_build_number}.zip": [
+            ("{build_dir}/distribution", r".*", '/')
+        ],
+    },
 }
 
 
@@ -67,13 +71,12 @@ def recursive_glob(dir, regex):
 
 def _construct_notice_file(files, aosp):
     """Constructs a notice file for all the given individual files."""
-    notice = '===========================================================\n'
-    notice += 'Notices for file(s):\n'
-    notice += '\n'.join(files)
-    notice += '\n'
-    notice += '===========================================================\n'
-    with open(os.path.join(aosp, 'external', 'qemu', 'NOTICE'),
-              'r') as nfile:
+    notice = "===========================================================\n"
+    notice += "Notices for file(s):\n"
+    notice += "\n".join(files)
+    notice += "\n"
+    notice += "===========================================================\n"
+    with open(os.path.join(aosp, "external", "qemu", "NOTICE"), "r") as nfile:
         notice += nfile.read()
 
     return notice
@@ -85,22 +88,28 @@ def create_distribution(dist_dir, build_dir, data):
         os.makedirs(dist_dir)
 
     # Let's find the set of
-    zip_set = zip_sets[data['config']]
+    zip_set = zip_sets[data["config"]]
 
     # First we create the individual zip sets using the regular expressions.
     for zip_fname, params in zip_set.iteritems():
         zip_fname = os.path.join(dist_dir, zip_fname.format(**data))
-        logging.info('Creating %s', zip_fname)
+        logging.info("Creating %s", zip_fname)
 
         files = []
-        with zipfile.ZipFile(zip_fname, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zipf:
-            start_dir, regex = params
-            search_dir = os.path.normpath(start_dir.format(build_dir = build_dir, src_dir = get_qemu_root()))
-            for fname in recursive_glob(search_dir, regex.format(data)):
-                arcname = fname[len(search_dir):]
-                files.append(arcname)
-                zipf.write(fname, arcname)
+        with zipfile.ZipFile(
+            zip_fname, "w", zipfile.ZIP_DEFLATED, allowZip64=True
+        ) as zipf:
+            for param in params:
+                start_dir, regex, dest = param
+                search_dir = os.path.normpath(
+                    start_dir.format(build_dir=build_dir, src_dir=get_qemu_root())
+                )
+                for fname in recursive_glob(search_dir, regex.format(data)):
+                    arcname = os.path.relpath(fname[len(search_dir):], '/')
+                    arcname = os.path.join(dest, arcname)
+                    files.append(arcname)
+                    zipf.write(fname, arcname)
 
             # add a notice file.
-            notice = _construct_notice_file(files, data['aosp'])
-            zipf.writestr('emulator/NOTICE.txt', notice)
+            notice = _construct_notice_file(files, data["aosp"])
+            zipf.writestr("emulator/NOTICE.txt", notice)
