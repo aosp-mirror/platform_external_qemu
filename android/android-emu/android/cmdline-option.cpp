1441257f32ea36fb2d8ca77cd9c86acac4fe1676
Haitao Shan <hshan@google.com>
2019-11-22 11:52:08 -0800

    Drop netspeed and netdelay parameter for AMD.
    Studio adds these parameters when invoking emulator. This brings
    adb connection issue with quickboot. The root cause is yet to be
    found by network experts.
    
    Bug: 144788277
    Change-Id: I33d229fe9eafecc8bf7e596ae00b32e74e9e8b97

--- android/android-emu/android/cmdline-option.cpp
+++ android/android-emu/android/cmdline-option.cpp
@@ -3,6 +3,7 @@
 #include "android/utils/debug.h"
 #include "android/utils/misc.h"
 #include "android/utils/system.h"
+#include <android/utils/x86_cpuid.h>
 #include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -147,6 +148,20 @@ android_parse_options( int  *pargc, char**  *pargv, AndroidOptions*  opt )
 
                         if (oo->var_type == OPTION_IS_PARAM)
                         {
+#ifdef _WIN32
+                            /* Drop netspeed and netdelay parameter for AMD platform.
+                             * Studio will add these two parameters to its emulator commandline.
+                             * However, this brings adb connection issue for quickboot.
+                             * This is a temporary fix for bug:144788277
+                             */
+                            char vendor_id[16] = { 0 };
+                            android_get_x86_cpuid_vendor_id(vendor_id, 16);
+                            if (android_get_x86_cpuid_vendor_id_type(vendor_id) == VENDOR_ID_AMD)
+                                if (!strcmp(oo->name, "netspeed") || !strcmp(oo->name, "netdelay")) {
+                                    aread++;
+                                    break;
+                                }
+#endif
                             ((char**)field)[0] = strdup(*aread++);
                         }
                         else if (oo->var_type == OPTION_IS_LIST)
