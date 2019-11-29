683128658ff743d4fce6d3bde7ab04a64a3dc3c9
Erwin Jansen <jansene@google.com>
2019-11-28 23:33:58 -0800

    Add support for native adb from emulator.
    This adds an AdbConnection class that speaks the adb protocol. It
    connect directly to the adbd running inside the emulator.
    It will do a proper handshake and authorize itself.
    
    This is used by the AdbWaterfallService to execute the waterfall service
    commands directly against ADB. This enables users to run waterfall
    commands without installing the waterfall binary and works with every
    system image (Playstore/AOSP)
    
    The implementation relies on:
    
    - AsynSockets for async i/o
    - QueueStreamBuf for a synchronized streambuf
    - Icebox to provide signing of the handshake on newer system images.
    
    The implementation moves the async sockets from android-webrtc to a base
    module and android-emu.
    
    The waterfall forwarder can be configured with the -waterfall flag, we
    default to the previous version where we forward requests to waterfall
    inside the emulator.
    
    Note that we currently only implement Exec, which supports both shell
    protocol v2 and v1 (for older images).
    
    For example:
    
    ./objs/emulator @Q -grpc 5556 -waterfall adb
    
    Will use the adb forwarder to forward waterfall calls.
    
    Test: emulator @L -grpc 5556 -waterfal adb
    Test: adb_bin -s tcp:localhost:5556 shell ls -l
    
    Change-Id: I317293b1f6a3bb1a429c88b00d12ac557f4054dd

--- android/android-emu/android/snapshot/Icebox.cpp
+++ android/android-emu/android/snapshot/Icebox.cpp
@@ -14,8 +14,6 @@
 
 #include "android/snapshot/Icebox.h"
 
-#include <openssl/pem.h>
-#include <openssl/rsa.h>
 #include <stdio.h>
 #include <atomic>
 #include <memory>
@@ -30,6 +28,7 @@
 #include "android/base/system/System.h"
 #include "android/base/threads/Async.h"
 #include "android/emulation/AdbMessageSniffer.h"
+#include "android/emulation/control/AdbAuthentication.h"
 #include "android/emulation/control/vm_operations.h"
 #include "android/jdwp/Jdwp.h"
 #include "android/snapshot/interface.h"
@@ -110,69 +109,6 @@ static bool recvOkay(int s) {
         return false;
     }
     return true;
-}
-
-// Adb authentication
-#define TOKEN_SIZE 20
-
-static bool read_key(const char* file, std::vector<RSA*>& keys) {
-    FILE* fp = fopen(file, "r");
-    if (!fp) {
-        DD("Failed to open '%s': %s", file, strerror(errno));
-        return false;
-    }
-
-    RSA* key_rsa = RSA_new();
-
-    if (!PEM_read_RSAPrivateKey(fp, &key_rsa, NULL, NULL)) {
-        DD("Failed to read key");
-        fclose(fp);
-        RSA_free(key_rsa);
-        return false;
-    }
-
-    keys.push_back(key_rsa);
-    fclose(fp);
-    return true;
-}
-
-static bool sign_token(RSA* key_rsa,
-                       const char* token,
-                       int token_size,
-                       char* sig,
-                       int& len) {
-    if (token_size != TOKEN_SIZE) {
-        DD("Unexpected token size %d\n", token_size);
-    }
-
-    if (!RSA_sign(NID_sha1, (const unsigned char*)token, (size_t)token_size,
-                  (unsigned char*)sig, (unsigned int*)&len, key_rsa)) {
-        return false;
-    }
-
-    DD("successfully signed with siglen %d\n", (int)len);
-    return true;
-}
-
-static bool sign_auth_token(const char* token,
-                            int token_size,
-                            char* sig,
-                            int& siglen) {
-    // read key
-    using android::base::pj;
-    std::vector<RSA*> keys;
-    std::string key_path = pj(android::base::System::get()->getHomeDirectory(),
-                              ".android", "adbkey");
-    if (!read_key(key_path.c_str(), keys)) {
-        // TODO: test the windows code path
-        std::string key_path =
-                pj(android::base::System::get()->getAppDataDirectory(),
-                   ".android", "adbkey");
-        if (!read_key(key_path.c_str(), keys)) {
-            return false;
-        }
-    }
-    return sign_token(keys[0], token, token_size, sig, siglen);
 }
 
 namespace android {
@@ -257,7 +193,7 @@ bool track(int pid, const char* snapshot_name) {
             pack_send.mesg.arg1 = 0;
             pack_send.data.resize(sigLen);
             pack_send.mesg.magic = pack_send.mesg.command ^ 0xffffffff;
-            if (!sign_auth_token((const char*)pack_recv.data.data(),
+            if (!android::emulation::sign_auth_token((const char*)pack_recv.data.data(),
                                  pack_recv.mesg.data_length,
                                  (char*)pack_send.data.data(), sigLen)) {
                 fprintf(stderr, "Fail to authenticate adb\n");
