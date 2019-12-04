d120256afeb72c2a58e6cdd190c0737f2aaf4d42
Erwin Jansen <jansene@google.com>
2019-12-03 21:36:54 -0800

    Remove dependency on external adb executable.
    We can now use the internal adb connection to make all the adb calls.
    This removes any dependency on external adb executable and running adb
    deamon. The new behavior can be activated by passing in the direct-adb
    flag.
    
    Introduces the AsbShellStream abstraction for interacting with adb using
    the proper protocol version. Version 2 protocol is preferred as it
    separates stdout, stderr and provides an exit code.
    
    Adds a series of unittests to validate the behavior.
    
    Fixes some reconnect issues related to ADBD being restarted on occassion
    in Q, when connecting to early in the boot process.
    
    Test: emulator @Q -direct-adb -monitor-adb 2
    emulator: INFO: AdbConnection.cpp:566: Opening adb service: shell,v2,raw:settings put system screen_off_timeout 2147483647
    HOST==>GUEST:command: OPEN arg0: 2 arg1: 0 data length: 64
    HOST==>GUEST:shell,v2,raw:settings put system screen_off_timeout 2147483647
    
    Change-Id: I0ed4b4d6a768ab8f8aee7d28d2e68160a1f8f2ae

--- android/android-grpc/android/emulation/control/waterfall/AdbWaterfallService.cpp
+++ android/android-grpc/android/emulation/control/waterfall/AdbWaterfallService.cpp
@@ -19,11 +19,11 @@
 
 #include "android/base/Log.h"
 #include "android/emulation/control/AdbConnection.h"
+#include "android/emulation/control/AdbShellStream.h"
 #include "android/emulation/control/waterfall/WaterfallService.h"
 #include "grpcpp/server.h"
 #include "grpcpp/server_builder.h"
 #include "waterfall.grpc.pb.h"
-
 namespace google {
 namespace protobuf {
 class Empty;
@@ -48,9 +48,8 @@ namespace android {
 namespace emulation {
 namespace control {
 
-static std::string serviceString(const char* serviceName,
-                                 const ::waterfall::Cmd& cmd) {
-    std::string service = serviceName;
+static std::string shellStr(const ::waterfall::Cmd& cmd) {
+    std::string service = "";
     int argIdx = 1;
 
     // Waterfall likes to executes commands in a subshell..
@@ -70,57 +69,7 @@ static std::string serviceString(const char* serviceName,
 
 class WaterfallAdbImpl final : public waterfall::Waterfall::Service {
 public:
-    WaterfallAdbImpl() : mAdb(AdbConnection::connection()) {}
-
-    Status ExecShellV2(
-            ServerContext* context,
-            ::grpc::ServerReaderWriter<::waterfall::CmdProgress,
-                                       ::waterfall::CmdProgress>* stream,
-            const ::waterfall::Cmd& cmd) {
-        ::waterfall::CmdProgress incoming;
-        auto adbStream = mAdb->open(serviceString("shell,v2,raw:", cmd));
-        std::thread fromDestinationToOrigin([&stream, &adbStream, &context]() {
-            bool ok = !context->IsCancelled() && adbStream->good();
-            while (ok) {
-                ::waterfall::CmdProgress outgoing;
-                ShellHeader hdr;
-                if (adbStream->read((char*)&hdr, sizeof(hdr))) {
-                    std::vector<char> msg(hdr.length);
-                    adbStream->read(msg.data(), hdr.length);
-
-                    switch (hdr.id) {
-                        case ShellHeader::kIdStderr:
-                            outgoing.set_stderr(msg.data(), hdr.length);
-                            break;
-                        case ShellHeader::kIdStdout:
-                            outgoing.set_stdout(msg.data(), hdr.length);
-                            break;
-                        case ShellHeader::kIdExit:
-                            outgoing.set_exit_code(msg[0]);
-                            break;
-                        default:
-                            // Ignonring the others..
-                            LOG(WARNING) << "Don't know how to handle shell "
-                                            "header of type: "
-                                         << hdr.id;
-                    }
-                }
-                ok = !context->IsCancelled() && stream->Write(outgoing) &&
-                     adbStream->good();
-            }
-        });
-        while (adbStream->good() && cmd.pipein() && stream->Read(&incoming)) {
-            ShellHeader hdr{
-                    .id = ShellHeader::kIdStdin,
-                    .length = static_cast<uint32_t>(incoming.stdin().size())};
-            adbStream->write((char*)&hdr, sizeof(hdr));
-            adbStream->write(incoming.stdin().data(), hdr.length);
-        };
-
-        fromDestinationToOrigin.join();
-        adbStream->close();
-        return Status::OK;
-    }
+    WaterfallAdbImpl() = default;
 
     Status Exec(ServerContext* context,
                 ::grpc::ServerReaderWriter<::waterfall::CmdProgress,
@@ -129,65 +78,42 @@ public:
         ::waterfall::CmdProgress fst;
 
         ::waterfall::CmdProgress incoming;
-        if (mAdb->state() != AdbState::connected || !stream->Read(&incoming)) {
+        // We are willing to wait 200ms to establish a connection if none exists.
+        auto adb = AdbConnection::connection(2000);
+        if (adb->state() != AdbState::connected || !stream->Read(&incoming)) {
             // TODO(jansene): what error code?
             fst.set_exit_code(0xFFFF);
             return Status::OK;
         }
 
-        if (mAdb->connection()->hasFeature("shell_v2"))
-            return ExecShellV2(context, stream, incoming.cmd());
-
         auto cmd = incoming.cmd();
-        auto adbStream = mAdb->open(serviceString("shell:", cmd));
-        std::thread fromDestinationToOrigin([&stream, &adbStream, &context]() {
-            bool ok = !context->IsCancelled() && adbStream->good();
-            auto strmbuf = adbStream->rdbuf();
+        AdbShellStream shell(shellStr(cmd));
+        std::thread fromDestinationToOrigin([&shell, &stream, &context]() {
+            bool ok = !context->IsCancelled() && shell.good();
+            int exitcode = 0;
+            std::vector<char> sout;
+            std::vector<char> serr;
             while (ok) {
                 ::waterfall::CmdProgress outgoing;
-
-                // Max chunk size we are willing to collect before sending.
-                int chunksize = 4096;
-                char buf[chunksize];
-                char* end = buf;
-                std::string stdout;
-                // Slurp up available bytes, worst case we send a single byte..
-                // but this will be very responsive..
-                if (strmbuf->in_avail() == 0) {
-                    // Block and wait until we have 1 byte, (probably more, but
-                    // we'll get to those)
-                    if (adbStream->read(end, 1)) {
-                        end += adbStream->gcount();
-                        chunksize -= adbStream->gcount();
-                    }
-                }
-                while (strmbuf->in_avail() > 0 && chunksize > 0) {
-                    if (adbStream->read(end,
-                                        std::min<size_t>(strmbuf->in_avail(),
-                                                         chunksize))) {
-                        end += adbStream->gcount();
-                        chunksize -= adbStream->gcount();
-                    }
-                }
-
-                outgoing.set_stdout(buf, end - buf);
-                ok = !context->IsCancelled() && stream->Write(outgoing) &&
-                     adbStream->good();
+                sout.clear();
+                serr.clear();
+                shell.read(sout, serr, exitcode);
+                outgoing.set_stdout(sout.data(), sout.size());
+                outgoing.set_stderr(serr.data(), serr.size());
+                outgoing.set_exit_code(exitcode);
+                ok = stream->Write(outgoing) && !context->IsCancelled() &&
+                     shell.good();
             }
         });
 
-        while (adbStream->good() && cmd.pipein() && stream->Read(&incoming)) {
-            adbStream->write(incoming.stdin().data(), incoming.stdin().size());
+        while (cmd.pipein() && stream->Read(&incoming)) {
+            shell.write(incoming.stdin());
         };
 
         fromDestinationToOrigin.join();
-        adbStream->close();
         return Status::OK;
     }
-
-private:
-    AdbConnection* mAdb;
-};
+};  // namespace control
 
 waterfall::Waterfall::Service* getAdbWaterfallService() {
     return new WaterfallAdbImpl();
