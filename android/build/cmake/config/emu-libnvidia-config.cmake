976b00945a5fb722fbb20248a01319a030f06836
bohu <bohu@google.com>
2019-12-15 11:12:40 -0800

    hw-codec: use nvcodec 8.0
    Newer library disabled run time dynamic loading
    uses 8.0 for dynamic loading instead
    
    WIP
    
    Change-Id: Idfe9d1405105c86b8137a9e53e6e720c956fb9d6

--- android/build/cmake/config/emu-libnvidia-config.cmake
+++ android/build/cmake/config/emu-libnvidia-config.cmake
@@ -8,13 +8,15 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+#  PREBUILT_ROOT "${ANDROID_QEMU2_TOP_DIR}/../../prebuilts/android-emulator-build/common/libnvidia/${ANDROID_TARGET_TAG}"
 get_filename_component(
   PREBUILT_ROOT "${ANDROID_QEMU2_TOP_DIR}/../../prebuilts/android-emulator-build/common/libnvidia/${ANDROID_TARGET_TAG}"
   ABSOLUTE)
 if(ANDROID_TARGET_TAG MATCHES "linux.*")
   set(LIBNVIDIA_INCLUDE_DIR "${PREBUILT_ROOT}/include")
   set(LIBNVIDIA_INCLUDE_DIRS ${LIBNVIDIA_INCLUDE_DIR})
-  set(LIBNVIDIA_LIBRARIES -lcuda -lnvcuvid -L${PREBUILT_ROOT}/lib)
+  set(LIBNVIDIA_LIBRARIES )
+#set(LIBNVIDIA_LIBRARIES -lcuda -lnvcuvid -L${PREBUILT_ROOT}/lib)
 
   add_library(LIBNVIDIA::LIBNVIDIA INTERFACE IMPORTED GLOBAL)
   set_target_properties(LIBNVIDIA::LIBNVIDIA PROPERTIES
