af9ca09f15299b90a036136327ebba9dd72d4b6e
Erwin Jansen <jansene@google.com>
2019-12-23 14:40:19 -0800

    [WebRTC] Support for data channels
    We now add support for receving mouse, keyboard and touch events over
    a data channel. A data channel for each event stream will be established
    with the client.
    
    The client is expected to deliver protobufs in accordings to the
    emulator_controller.proto definition for the corresponindg sendXXX
    calls.
    
    Datachannels are a lot faster for event delivery.
    
    Change-Id: I208c06cd763e95b0fcee37198b7ea85cbb4abe59

--- android/android-webrtc/android-webrtc/android/emulation/control/WebRtcBridge.cpp
+++ android/android-webrtc/android-webrtc/android/emulation/control/WebRtcBridge.cpp
@@ -19,6 +19,9 @@
 #include "emulator/net/AsyncSocket.h"
 
 using emulator::net::AsyncSocket;
+#include "android/base/sockets/ScopedSocket.h"
+#include "android/base/sockets/SocketUtils.h"
+
 using nlohmann::json;
 
 namespace android {
@@ -28,15 +31,17 @@ namespace control {
 using namespace android::base;
 
 const std::string WebRtcBridge::kVideoBridgeExe = "goldfish-webrtc-bridge";
+std::string WebRtcBridge::BRIDGE_RECEIVER = "WebrtcVideoBridge";
 
 WebRtcBridge::WebRtcBridge(AsyncSocketAdapter* socket,
-                           const QAndroidRecordScreenAgent* const screenAgent,
+                           const AndroidConsoleAgents* const agents,
                            int fps,
                            int videoBridgePort,
                            std::string turncfg)
     : mProtocol(this),
       mTransport(&mProtocol, socket),
-      mScreenAgent(screenAgent),
+      mEventDispatcher(agents),
+      mScreenAgent(agents->record),
       mFps(fps),
       mVideoBridgePort(videoBridgePort),
       mTurnConfig(turncfg) {}
@@ -146,6 +151,7 @@ bool WebRtcBridge::acceptJsepMessage(std::string identity,
     json msg;
     msg["from"] = identity;
     msg["msg"] = message;
+    LOG(INFO) << "sending " << msg.dump(2);
     return mProtocol.write(&mTransport, msg);
 }
 
@@ -163,7 +169,6 @@ void WebRtcBridge::terminate() {
 }
 
 void WebRtcBridge::received(SocketTransport* from, json object) {
-    LOG(VERBOSE) << "Received from video bridge: " << object.dump(2);
     // It's either  a message, or goodbye..
     bool message = object.count("msg") && object.count("topic");
     bool bye = object.count("bye") && object.count("topic");
@@ -173,6 +178,12 @@ void WebRtcBridge::received(SocketTransport* from, json object) {
     }
 
     std::string dest = object["topic"];
+    if (dest == BRIDGE_RECEIVER) {
+        // Handle an event..
+        mEventDispatcher.dispatchEvent(object);
+        return;
+    }
+
     if (bye) {
         // We are saying good bye..
         AutoWriteLock lock(mMapLock);
@@ -334,6 +345,12 @@ bool WebRtcBridge::start() {
 RtcBridge* WebRtcBridge::create(int port,
                                 const AndroidConsoleAgents* const consoleAgents,
                                 std::string turncfg) {
+    if (port == 0) {
+        // Find a free port.
+        android::base::ScopedSocket s0(socketTcp4LoopbackServer(0));
+        port = android::base::socketGetPort(s0.get());
+    }
+
     std::string executable =
             System::get()->findBundledExecutable(kVideoBridgeExe);
     if (executable.empty()) {
@@ -343,7 +360,7 @@ RtcBridge* WebRtcBridge::create(int port,
 
     Looper* looper = android::base::ThreadLooper::get();
     AsyncSocket* socket = new AsyncSocket(looper, port);
-    return new WebRtcBridge(socket, consoleAgents->record, kMaxFPS, port, turncfg);
+    return new WebRtcBridge(socket, consoleAgents, kMaxFPS, port, turncfg);
 }
 }  // namespace control
 }  // namespace emulation
