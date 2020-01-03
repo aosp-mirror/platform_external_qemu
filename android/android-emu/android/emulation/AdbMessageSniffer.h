ee0af4cac4318f9f5a45f68a5cd18ac56ecce654
Erwin Jansen <jansene@google.com>
2020-01-03 11:10:44 -0800

    Fix memory issues in AdbMessageSniffer
    The AdbMessageSniffer is used to monitor messages that are coming from
    Adb. The message sniffer will basically ingest adb messages and print
    the to the console if needed.
    
    There are certain cases where it was possible that the message sniffer
    only ingested partial packets (see AdbGuestPipe.cpp:598) for example.
    
    This can result in a misaligned packet stream, which in turn can result
    in integer underflows, which would cause crashes when we would memcpy
    before a valid memory region.
    
    This adds a series of fuzzing unit tests that exposed this issue when
    running with the -gtest_repeat=-1 flag, and includes fixes so that
    underruns will no longer happen.
    
    Includes the following additional fixes:
    
    - No longer reserve 1MB, we resize as needed.
    - AdbMessageSniffer will not use any resources when logging is disabled.
    - Disable message sniffer as soon as we detect an out of sync stream.
    
    Change-Id: I78e94dd486265fdbb376317251962ec74dd01cf0
    Test: ./emulator @Q -monitor-adb 2 works.

--- android/android-emu/android/emulation/AdbMessageSniffer.h
+++ android/android-emu/android/emulation/AdbMessageSniffer.h
@@ -8,67 +8,38 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
 #pragma once
+#include <ostream>
 
-#include "android/base/Log.h"
-#include "android/emulation/AndroidPipe.h"
-
-#include <map>
-#include <vector>
-#include <set>
-
+class AndroidPipeBuffer;
 namespace android {
 namespace emulation {
 
-// AdbMessageSniffer wraps around the raw adb message struct
+// An AdbMessageSniffer can intercept the adb message stream and output
+// them to a std::ostream when a complete packet is received.
+// packets will be printed while the stream contains valid packets.
+// Should only consume resources if the logging level > 0
 class AdbMessageSniffer {
 public:
+    struct amessage {
+        unsigned command;     /* command identifier constant      */
+        unsigned arg0;        /* first argument                   */
+        unsigned arg1;        /* second argument                  */
+        unsigned data_length; /* length of payload (0 is allowed) */
+        unsigned data_check;  /* checksum of data payload         */
+        unsigned magic;       /* command ^ 0xffffffff             */
+    };
 
+    virtual ~AdbMessageSniffer() = default;
+    virtual void read(const AndroidPipeBuffer*, int numBuffers, int count) = 0;
 
-struct amessage {
-    unsigned command;       /* command identifier constant      */
-    unsigned arg0;          /* first argument                   */
-    unsigned arg1;          /* second argument                  */
-    unsigned data_length;   /* length of payload (0 is allowed) */
-    unsigned data_check;    /* checksum of data payload         */
-    unsigned magic;         /* command ^ 0xffffffff             */
-};
-
-struct apacket
-{
-    amessage mesg;
-    uint8_t data[1024];
-};
-
-    AdbMessageSniffer(const char* name);
-    ~AdbMessageSniffer();
-
-    void read(const AndroidPipeBuffer*, int numBuffers, int count);
-private:
-    apacket mPacket;
-    int mState;
-    uint8_t* mCurrPos;
-    std::vector<uint8_t> mBufferVec;
-    uint8_t* mBuffer;
-    uint8_t* mBufferP;
-    const char* mName;
-    std::set<unsigned> mDummyShellArg0;
-
-    void grow_buffer_if_needed(int count);
-    int getAllowedBytesToPrint(int bytes);
-    bool checkForDummyShellCommand();
-    int getPayloadSize();
-    void copyFromBuffer(int count);
-    int readPayload(int dataSize);
-    void printMessage();
-    void updateCtsHeartBeatCount();
-    void printPayload();
-    const char* getCommandName(unsigned code);
-    void startNewMessage();
-    int readHeader(int inputDataSize);
-    void readToBuffer(const AndroidPipeBuffer* buffers, int numBuffers, int inputDataSize);
-    void parseBuffer(int bufferLength);
+    // Creates a new sniffer.
+    // |name| The prefix printed for each log message.
+    // |level| The requested logging level, only level > 0 will use resources.
+    // |oStream| Stream to which to log the messages.
+    static AdbMessageSniffer* create(const char* name,
+                                     int level,
+                                     std::ostream* oStream);
 };
 
 }  // namespace emulation
