9bd0ddf9431b4d7cfa95beb2049ff1a530beda8e
Erwin Jansen <jansene@google.com>
2020-01-13 16:59:13 -0800

    [gRPC] Add support for streaming of images.
    We extend the screen shot method to:
    
    - Resize the current frame with the correct aspect ratio.
    - Rotate the image based upon the device state, if requesting display 0
    - Translate the pixel format to the desired format.
    
    We also add support for streaming screenshots as soon as they are
    available. Streaming is accomplished by using an EventWaiter that
    registers for frame notifications from QEMU.
    
    The EventWaiter can be used by other event producing endpoints as long
    as the same callback interface is observed.
    
    Includes a python3 sample to display the current state. This requires PIL.
    It has a weird bug where the image will not always update if there are no mouse events.
    
    Change-Id: I1b2408d187be4522a8aac679fb743fef8471a1ad
    Bug: b/149428088
    Test: ./objs/emulator @L &; python3 -m sample.screenshot

---
+++ android/android-grpc/android/emulation/control/utils/ScreenshotUtils.cpp
@@ -0,0 +1,117 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/emulation/control/utils/ScreenshotUtils.h"
+
+#include <assert.h>  // for assert
+
+#include <array>    // for array
+#include <utility>  // for pair
+
+#include "android/base/Log.h"  // for LogStream, LOG, LogMessage
+
+namespace android {
+namespace emulation {
+namespace control {
+
+using ImageFormatPair =
+        std::pair<ImageFormat_ImgFormat, android::emulation::ImageFormat>;
+using RotationPair = std::pair<Rotation_SkinRotation, SkinRotation>;
+
+const std::array<ImageFormatPair, 3> imgFormatMap{
+        {{ImageFormat::RGBA8888, android::emulation::ImageFormat::RGBA8888},
+         {ImageFormat::PNG, android::emulation::ImageFormat::PNG},
+         {ImageFormat::RGB888, android::emulation::ImageFormat::RGB888}}};
+
+const std::array<RotationPair, 4> rotationMap{
+        {{Rotation::PORTRAIT, SKIN_ROTATION_0},
+         {Rotation::LANDSCAPE, SKIN_ROTATION_90},
+         {Rotation::REVERSE_PORTRAIT, SKIN_ROTATION_180},
+         {Rotation::REVERSE_LANDSCAPE, SKIN_ROTATION_270}}};
+
+// Translates our external format to the intenal format. Note that the
+// internal format contains values that do not exist in the external enum.
+android::emulation::ImageFormat ScreenshotUtils::translate(
+        ImageFormat_ImgFormat x) {
+    for (auto [fst, snd] : imgFormatMap) {
+        if (fst == x)
+            return snd;
+    }
+    // Shouldn't happen.
+    LOG(ERROR) << "Unknown enum: " << (int)x << ", returning RGBA8888 format.";
+    return android::emulation::ImageFormat::RGBA8888;
+};
+
+// Translates our internal format to the external format. Note that the
+// internal format contains values that do not exist in the external enum.
+ImageFormat_ImgFormat ScreenshotUtils::translate(
+        android::emulation::ImageFormat x) {
+    for (auto [fst, snd] : imgFormatMap) {
+        if (snd == x)
+            return fst;
+    }
+    // Shouldn't happen.
+    LOG(ERROR) << "Unknown enum: " << (int)x << ", returning RGBA8888 format.";
+    return ImageFormat::RGBA8888;
+};
+
+static constexpr bool match(Rotation_SkinRotation x, SkinRotation y) {
+    return static_cast<int>(x) == static_cast<int>(y);
+}
+
+SkinRotation ScreenshotUtils::translate(Rotation_SkinRotation x) {
+    // Make sure nobody accidentally renumbers the expected enums.
+    static_assert(match(Rotation::PORTRAIT, SKIN_ROTATION_0));
+    static_assert(match(Rotation::LANDSCAPE, SKIN_ROTATION_90));
+    static_assert(match(Rotation::REVERSE_PORTRAIT, SKIN_ROTATION_180));
+    static_assert(match(Rotation::REVERSE_LANDSCAPE, SKIN_ROTATION_270));
+    assert(x <= static_cast<int>(SKIN_ROTATION_270));
+    return static_cast<SkinRotation>(x);
+};
+
+Rotation_SkinRotation ScreenshotUtils::coarseRotation(double zaxis) {
+    if (zaxis < -90) {
+        return Rotation::REVERSE_PORTRAIT;
+    } else if (zaxis < 0) {
+        return Rotation::LANDSCAPE;
+    } else if (zaxis < 90) {
+        return Rotation::PORTRAIT;
+    }
+    return Rotation::REVERSE_LANDSCAPE;  // z >= 90;
+}
+
+std::tuple<int, int> ScreenshotUtils::resizeKeepAspectRatio(
+        double width,
+        double height,
+        double desiredWidth,
+        double desiredHeight) {
+    double aspectRatio = width / height;
+    double newAspectRatio = desiredWidth / desiredHeight;
+    int newWidth, newHeight;
+    if (newAspectRatio > aspectRatio) {
+        // Wider than necessary; use the same height and compute the width
+        // from the desired aspect ratio.
+        newHeight = desiredHeight;
+        newWidth = (desiredHeight * aspectRatio);
+    } else {
+        // Taller than necessary; use the same width and compute the height
+        // from the desired aspect ratio
+        newWidth = desiredWidth;
+        newHeight = (desiredWidth / aspectRatio);
+    }
+    return std::make_tuple(newWidth, newHeight);
+}
+
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
