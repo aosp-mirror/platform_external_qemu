4823ed310b6b6132b430ff257eddb90a477cbb4f
Erwin Jansen <jansene@google.com>
2020-01-15 13:42:36 -0800

    [gRPC] Tool to investigate snapshots.
    - Add a tool to query/push/pull snapshots.
    - Make .tar.gz optional. GZip is very slow, with gzip disabled we get
      10x throughput. (10Mb/s, vs. 175Mb/s)
    
    This tool will help us investigate some issues we are seeing with
    Icebox.
    
    Change-Id: I81492510411e7c5be9019e7dd33aefd378bcfa8c

---
+++ android/android-grpc/docs/python/snaptool/snaptool/snapshot.py
@@ -0,0 +1,134 @@
+"""
+Contains a SnapshotService that can talk to the emulator snapshot service on the given grpc port.
+"""
+import logging
+import os
+
+import grpc
+from google.protobuf import empty_pb2
+
+from proto.snapshot_service_pb2_grpc import SnapshotServiceStub
+from proto.snapshot_service_pb2 import Snapshot
+from snaptool.channel_provider import getEmulatorChannel
+from tqdm import tqdm
+
+
+_EMPTY_ = empty_pb2.Empty()
+
+
+class SnapshotService(object):
+    """A SnapshotService can be used to manipulate snapshots in the emulator."""
+
+    def __init__(self, port, logger=logging.getLogger()):
+        """Connect to the emulator on the given port, and log on the given logger."""
+        self.channel = getEmulatorChannel(port)
+        self.stub = SnapshotServiceStub(self.channel)
+        self.logger = logger
+
+    def _exec_unary_grpc(self, method, req):
+        """Executes the given request on the service endpoint with the provided parameter,
+
+        Returns: True on success, or False otherwise.
+        """
+        self.logger.debug("Executing %s(%s)", method, req)
+        method_to_call = getattr(self.stub, method)
+        try:
+            msg = method_to_call(req)
+            self.logger.debug("Response %s", msg)
+            if not msg.success:
+                self.logger.error("Failed to %s snapshot: %s", method, msg.err)
+            return msg.success
+        except grpc._channel._Rendezvous as err:
+            self.logger.error("Low level grpc error: %s ", err)
+
+        return False
+
+    def pull(self, snap_id, dest, fmt=Snapshot.Format.TAR):
+        """Pulls a snapshot with the given id to the given destination dir as id.tar.gz."""
+        fn = snap_id + ".tar"
+        if fmt == Snapshot.Format.TARGZ:
+            fn += ".gz"
+        fname = os.path.join(dest, fn)
+        self.logger.debug("Pulling %s -> %s", snap_id, fname)
+        snap = self.info(snap_id)
+        total_size = sum([img.size for img in snap.details.images if img.present])
+
+        try:
+            it = self.stub.pullSnapshot(Snapshot(snapshot_id=snap_id, format=fmt))
+            with open(fname, "wb") as fn:
+                with tqdm(fn, total=total_size, unit="B", unit_scale=True) as t:
+                    for msg in it:
+                        if not msg.success:
+                            self.logger.error("Failed to pull snapshot: %s", msg.err)
+                            return False
+                        fn.write(msg.payload)
+                        t.update(len(msg.payload))
+        except grpc._channel._Rendezvous as err:
+            self.logger.error("Low level grpc error: %s ", err)
+        self.logger.debug("Response %s", msg)
+        return msg and msg.success, fname
+
+    def push(self, src):
+        """Pushes a snapshot from the given src (tar.gz) as 'snap_id'.tar.gz
+
+        The snap_id.tar.gz should have a snaphost that has the id: snap_id.
+        So foo.tar.gz should contain the snapshot named 'foo'.
+        """
+
+        def read_in_chunks(file_object, chunk_size=(256 * 1024)):
+            """Ye olde python chunk reader.
+            """
+            while True:
+                data = file_object.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+        def push_snap_iterator(fname):
+            """An iterator that returns:
+
+            1. A message containing only the id.
+            2. A stream of byte objects from the tar (.gz) file.
+            """
+            fmt = Snapshot.Format.TAR
+            if fname.endswith(".tar.gz"):
+                fmt = Snapshot.Format.TARGZ
+
+
+            total_size = os.path.getsize(fname)
+            snap_id = os.path.basename(fname).replace(".gz", "").replace(".tar", "")
+
+            with tqdm(total=total_size, unit="B", unit_scale=True) as t:
+                yield Snapshot(snapshot_id=snap_id, format=fmt)
+                with open(fname, "rb") as snap:
+                    for chunk in read_in_chunks(snap):
+                        t.update(len(chunk))
+                        yield Snapshot(payload=chunk)
+
+        return self._exec_unary_grpc("pushSnapshot", push_snap_iterator(src))
+
+    def info(self, snap_id):
+        """Lists all available snapshots."""
+        self.logger.debug("Retrieving snapshots")
+        response = self.stub.listSnapshots(_EMPTY_)
+        self.logger.debug("Response %s", response)
+        return next((f for f in response.snapshots if f.snapshot_id == snap_id), None,)
+
+    def lists(self):
+        """Lists all available snapshots."""
+        self.logger.debug("Retrieving snapshots")
+        response = self.stub.listSnapshots(_EMPTY_)
+        self.logger.debug("Response %s", response)
+        return [f.snapshot_id for f in response.snapshots]
+
+    def load(self, snap_id):
+        """Loads a snapshot inside the emulator."""
+        return self._exec_unary_grpc("loadSnapshot", Snapshot(snapshot_id=snap_id))
+
+    def save(self, snap_id):
+        """Saves a snapshot inside the emulator."""
+        return self._exec_unary_grpc("saveSnapshot", Snapshot(snapshot_id=snap_id))
+
+    def delete(self, snap_id):
+        """Deletes the given snapshot from the emulator."""
+        return self._exec_unary_grpc("deleteSnapshot", Snapshot(snapshot_id=snap_id))
---
+++ android/android-grpc/docs/python/snaptool/snaptool/tool.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 - The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import, division, print_function
+
+# Super hack for protobuf not setting paths properly..
+
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "proto"))
+)
+
+import click
+import time
+from snaptool.snapshot import SnapshotService
+from proto.snapshot_pb2 import Image
+from proto.snapshot_service_pb2 import Snapshot
+
+
+def epoch_fmt(epoch):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(epoch))
+
+
+def sizeof_fmt(num):
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+@click.group()
+@click.option(
+    "--grpc", default="localhost:8556", help="Address of the emulator gRPC endpoint."
+)
+@click.pass_context
+def cli(ctx, grpc):
+    ctx.obj = SnapshotService(grpc)
+    pass
+
+
+@cli.command()
+@click.pass_obj
+def list(snapshotService):
+    snaps = snapshotService.lists()
+    if snaps:
+        for snap in snaps:
+            print(snap)
+    else:
+        print("No snapshots available.")
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+@click.option("--all", "-a", default=False, is_flag=True, help="Print the raw data.")
+def info(snapshotService, name, all):
+    snap = snapshotService.info(name)
+    if not snap:
+        print("{} does not exist".format(name))
+
+    if all:
+        print(snap)
+    else:
+        print(
+            "{} : Created: {}".format(
+                snap.snapshot_id, epoch_fmt(snap.details.creation_time)
+            )
+        )
+        total_size = 0
+        for img in snap.details.images:
+            if img.present:
+                print(
+                    "  {:30}   {:>10}".format(
+                        Image.Type.Name(img.type), sizeof_fmt(img.size)
+                    )
+                )
+                total_size += img.size
+        print("  {:30}   {:>10}".format("Total:", sizeof_fmt(total_size)))
+        print("  {:10} {:>7}".format("Cpu cores:", snap.details.config.cpu_core_count))
+        print(
+            "  {:10} {:>7}".format(
+                "Memory:", sizeof_fmt(snap.details.config.ram_size_bytes)
+            )
+        )
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+@click.argument("dest", default=".")
+@click.option("--compress/--no-compress", default=False)
+def pull(snapshotService, name, dest, compress):
+    fmt = Snapshot.Format.TAR
+    if compress:
+        fmt = Snapshot.Format.TARGZ
+    msg, fname = snapshotService.pull(name, dest, fmt)
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("src")
+def push(snapshotService, src):
+    if not os.path.exists(src):
+        print("File {} does not exist!".format(src))
+        sys.exit(1)
+    snapshotService.push(src)
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+def load(snapshotService, name):
+    snapshotService.load(name)
+
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+def save(snapshotService, name):
+    snapshotService.save(name)
+
+@cli.command()
+@click.pass_obj
+@click.argument("name")
+def delete(snapshotService, name):
+    snapshotService.delete(name)
+
