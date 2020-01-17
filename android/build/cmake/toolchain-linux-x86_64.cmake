e4d6f6f53ae91a9ca61a93bf5432c6ab096bf5e3
Erwin <jansene@google.com>
2020-01-17 22:37:55 +0000

    Switch to nasm vs. yasm
    We have seen a series of random crashes of the yasm compiler under
    clang-10. Instead of tracking down every issue we are switching to nasm
    instead. Nasm has been adopted by the chrome team due to lack of
    development in yasm and has found a happy home there.
    
    Nasm has the same external interface.
    
    Change-Id: Iefa700ca1ccd73038467e6b99f86de656fc3523a

--- android/build/cmake/toolchain-linux-x86_64.cmake
+++ android/build/cmake/toolchain-linux-x86_64.cmake
@@ -52,5 +52,5 @@ set(CMAKE_FIND_ROOT_PATH  "${ANDROID_SYSROOT}")
 # Configure how we strip executables.
 set(CMAKE_STRIP_CMD "${CMAKE_STRIP} -s")
 
-# And the asm type if we are compiling with yasm
-set(ANDROID_YASM_TYPE elf64)
+# And the asm type if we are compiling with ASM
+set(ANDROID_ASM_TYPE elf64)
