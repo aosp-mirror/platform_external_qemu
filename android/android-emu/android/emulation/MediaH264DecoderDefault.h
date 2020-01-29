d99e185240e57bdbec93d7335f2b9e3dae573cdc
bohu <bohu@google.com>
2020-01-28 17:42:40 -0800

    codecs: refactor H264 decoder implementation
    Now, the MediaH264DecoderDefault acts as the manager
    of verious different implementation of MediaH264DecoderPlugin.
    
    BUG: 124388359
    
    Change-Id: I2c665bde62f61dbb1a14367ab5a9d0c28e9b30d1

--- android/android-emu/android/emulation/MediaH264DecoderDefault.h
+++ android/android-emu/android/emulation/MediaH264DecoderDefault.h
@@ -15,21 +15,8 @@
 #pragma once
 
 #include "android/emulation/GoldfishMediaDefs.h"
+#include "android/emulation/MediaH264DecoderPlugin.h"
 #include "android/emulation/MediaH264Decoder.h"
-
-extern "C" {
-#include <libavcodec/avcodec.h>                 // for AVCodecContext, AVPacket
-#include <libavformat/avio.h>                   // for avio_open, AVIO_FLAG_...
-#include <libavutil/avutil.h>                   // for AVMediaType, AVMEDIA_...
-#include <libavutil/dict.h>                     // for AVDictionary
-#include <libavutil/error.h>                    // for av_make_error_string
-#include <libavutil/frame.h>                    // for AVFrame, av_frame_alloc
-#include <libavutil/log.h>                      // for av_log_set_callback
-#include <libavutil/pixfmt.h>                   // for AVPixelFormat, AV_PIX...
-#include <libavutil/rational.h>                 // for AVRational
-#include <libavutil/samplefmt.h>                // for AVSampleFormat
-#include <libavutil/timestamp.h>
-}
 
 #include <stddef.h>
 #include <mutex>
@@ -38,7 +25,6 @@ extern "C" {
 namespace android {
 namespace emulation {
 
-class MediaH264DecoderDefaultImpl;
 
 class MediaH264DecoderDefault : public MediaH264Decoder {
 public:
@@ -52,82 +38,17 @@ private:
     std::mutex mIdLock{};
     std::mutex mMapLock{};
     uint64_t mId = 0;
-    std::unordered_map<uint64_t, MediaH264DecoderDefaultImpl*> mDecoders;
+    std::unordered_map<uint64_t, MediaH264DecoderPlugin*> mDecoders;
     uint64_t readId(void* ptr);  // read id from the address
     void removeDecoder(uint64_t id);
     uint64_t createId();
     void addDecoder(uint64_t key,
-                    MediaH264DecoderDefaultImpl* val);  // this just add
+                    MediaH264DecoderPlugin* val);  // this just add
     void updateDecoder(
             uint64_t key,
-            MediaH264DecoderDefaultImpl* val);  // this will overwrite
-    MediaH264DecoderDefaultImpl* getDecoder(uint64_t key);
+            MediaH264DecoderPlugin* val);  // this will overwrite
+    MediaH264DecoderPlugin* getDecoder(uint64_t key);
 };
-
-class MediaH264DecoderDefaultImpl {
-public:
-    MediaH264DecoderDefaultImpl();
-    ~MediaH264DecoderDefaultImpl();
-
-    using PixelFormat = MediaH264Decoder::PixelFormat;
-    using Err = MediaH264Decoder::Err;
-
-private:
-    void initH264Context(unsigned int width,
-                         unsigned int height,
-                         unsigned int outWidth,
-                         unsigned int outHeight,
-                         PixelFormat pixFmt);
-    void destroyH264Context();
-    void decodeFrame(void* ptr,
-                     const uint8_t* frame,
-                     size_t szBytes,
-                     uint64_t inputPts);
-    void flush(void* ptr);
-    void getImage(void* ptr);
-
-    friend MediaH264DecoderDefault;
-
-private:
-    // image props
-    bool mImageReady = false;
-    bool mIsInFlush = false;
-    bool mFrameFormatChanged = false;
-    static constexpr int kBPP = 2; // YUV420 is 2 bytes per pixel
-    unsigned int mOutputHeight = 0;
-    unsigned int mOutputWidth = 0;
-    unsigned int mColorPrimaries = 2;
-    unsigned int mColorRange = 0;
-    unsigned int mColorTransfer = 2;
-    unsigned int mColorSpace = 2;
-    uint64_t mOutputPts = 0;
-    uint64_t mInputPts = 0;
-    unsigned int mSurfaceHeight = 0;
-    unsigned int mBPP = 0;
-    unsigned int mSurfaceWidth = 0;
-    unsigned int mLumaHeight = 0;
-    unsigned int mChromaHeight = 0;
-    PixelFormat mOutPixFmt;
-    unsigned int mOutBufferSize = 0;
-
-    // right now, decoding command only passes the input address;
-    // and output address is only available in getImage().
-    // TODO: this should be set to the output address to avoid
-    // extra copying
-    uint8_t *mDecodedFrame = nullptr;
-
-
-
-    // ffmpeg stuff
-    AVCodec *mCodec = nullptr;;
-    AVCodecContext *mCodecCtx = nullptr;
-    AVFrame *mFrame = nullptr;
-    AVPacket mPacket;
-
-private:
-    void copyFrame();
-
-};  // MediaH264DecoderDefault
 
 }  // namespace emulation
 }  // namespace android
