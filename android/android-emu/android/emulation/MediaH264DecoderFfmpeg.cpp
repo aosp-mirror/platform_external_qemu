442fb38e77585cdf0fb2e19d5cb6b04c1e7fe54c
bohu <bohu@google.com>
2020-01-30 15:49:34 -0800

    codecs: Add nvcodec 8.0 H264 decoder plugin
    Newer library disabled run time dynamic loading
    uses 8.0 for dynamic loading instead
    This decoder is invoked by setting
    ANDROID_EMU_CODEC_USE_CUVID_DECODER=1
    
    The h264_cuvid decoder in ffmpeg is invoked by
    ANDROID_EMU_CODEC_USE_FFMPEG_CUVID_DECODER=1
    
    BUG: 124388359
    Test: Build
    Test: adb stagefright -S buck.mp6
    
    based on the following cl:
    go/oag/1189738
    
    Change-Id: I5b4fadd8e7455f1275ac1cee89ef8f224d6de03c

--- android/android-emu/android/emulation/MediaH264DecoderFfmpeg.cpp
+++ android/android-emu/android/emulation/MediaH264DecoderFfmpeg.cpp
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 #include "android/emulation/MediaH264DecoderFfmpeg.h"
-#include "android/emulation/H264NaluParser.h"
 #include "android/base/system/System.h"
+#include "android/emulation/H264NaluParser.h"
+#include "android/emulation/YuvConverter.h"
 
 #include <cstdint>
 #include <string>
@@ -33,62 +34,6 @@
 
 namespace android {
 namespace emulation {
-
-template<typename T>
-class YuvConverter {
-public:
-    YuvConverter(int nWidth, int nHeight) : nWidth(nWidth), nHeight(nHeight) {
-        pQuad = new T[nWidth * nHeight / 4];
-    }
-    ~YuvConverter() {
-        delete pQuad;
-    }
-    void PlanarToUVInterleaved(T *pFrame, int nPitch = 0) {
-        if (nPitch == 0) {
-            nPitch = nWidth;
-        }
-        T *puv = pFrame + nPitch * nHeight;
-        if (nPitch == nWidth) {
-            memcpy(pQuad, puv, nWidth * nHeight / 4 * sizeof(T));
-        } else {
-            for (int i = 0; i < nHeight / 2; i++) {
-                memcpy(pQuad + nWidth / 2 * i, puv + nPitch / 2 * i, nWidth / 2 * sizeof(T));
-            }
-        }
-        T *pv = puv + (nPitch / 2) * (nHeight / 2);
-        for (int y = 0; y < nHeight / 2; y++) {
-            for (int x = 0; x < nWidth / 2; x++) {
-                puv[y * nPitch + x * 2] = pQuad[y * nWidth / 2 + x];
-                puv[y * nPitch + x * 2 + 1] = pv[y * nPitch / 2 + x];
-            }
-        }
-    }
-    void UVInterleavedToPlanar(T *pFrame, int nPitch = 0) {
-        if (nPitch == 0) {
-            nPitch = nWidth;
-        }
-        T *puv = pFrame + nPitch * nHeight,
-            *pu = puv,
-            *pv = puv + nPitch * nHeight / 4;
-        for (int y = 0; y < nHeight / 2; y++) {
-            for (int x = 0; x < nWidth / 2; x++) {
-                pu[y * nPitch / 2 + x] = puv[y * nPitch + x * 2];
-                pQuad[y * nWidth / 2 + x] = puv[y * nPitch + x * 2 + 1];
-            }
-        }
-        if (nPitch == nWidth) {
-            memcpy(pv, pQuad, nWidth * nHeight / 4 * sizeof(T));
-        } else {
-            for (int i = 0; i < nHeight / 2; i++) {
-                memcpy(pv + nPitch / 2 * i, pQuad + nWidth / 2 * i, nWidth / 2 * sizeof(T));
-            }
-        }
-    }
-
-private:
-    T *pQuad;
-    int nWidth, nHeight;
-};
 
 MediaH264DecoderFfmpeg::~MediaH264DecoderFfmpeg() {
     H264_DPRINT("destroyed MediaH264DecoderFfmpeg %p", this);
@@ -141,7 +86,7 @@ void MediaH264DecoderFfmpeg::initH264Context(unsigned int width,
 
     mCodec = NULL;
     auto useCuvidEnv = android::base::System::getEnvironmentVariable(
-            "ANDROID_EMU_CODEC_USE_CUVID_DECODER");
+            "ANDROID_EMU_CODEC_USE_FFMPEG_CUVID_DECODER");
     if (useCuvidEnv != "") {
         mCodec = avcodec_find_decoder_by_name("h264_cuvid");
         if (mCodec) {
