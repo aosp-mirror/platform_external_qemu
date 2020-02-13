fba031635d89bb9e146b8dfa9a62336c90ce3332
Erwin Jansen <jansene@google.com>
2020-02-13 11:30:24 -0800

    Upgrade build scripts to Python3
    Python 2 is no longer officially supported. It is no longer distributed
    as part of visual studio for example. This makes getting started harder
    on windows.
    
    We now make sure that the build works with Python2 and Python3, and
    install required dependencies automatically on windows.
    
    Change-Id: I0fec2123a2bb349af80e1d9aff968f56d7f336dd
    See: go/python-upgrade and https://python3statement.org/

--- android/rebuild.sh
+++ android/rebuild.sh
@@ -6,19 +6,29 @@
 # assume that the device tree is in TOP
 #
 . $(dirname "$0")/scripts/unix/utils/common.shi
-PROGDIR=`dirname $0`
+PROGDIR=$(dirname $0)
 set -e
 export LANG=C
 export LC_ALL=C
 
+PYTHON=python
+
 exists() {
-  # Returns true if $1 is on the path.
-  command -v "$1" >/dev/null 2>&1
+    # Returns true if $1 is on the path.
+    command -v "$1" >/dev/null 2>&1
+}
+
+check_python_version() {
+    PYV=$($PYTHON -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)")
+    if [ "${PYV}" == "2" ]; then
+        warn "Python 2, has been deprecated! See: https://python3statement.org/ and go/python-upgrade please upgrade to python 3!"
+        install_deps enum enum34 # Needed compatibility layer for Python2..
+    fi
 }
 
 python_module_exist() {
     #  returns true if $1 can be imported using the default python interpreter
-    python -c "import $1" > /dev/null 2>&1
+    $PYTHON -c "import $1" >/dev/null 2>&1
 }
 
 install_deps() {
@@ -29,15 +39,19 @@ install_deps() {
     local package=$2
     if ! python_module_exist ${import}; then
         echo "Missing dependency ${package} attempting to install dependency in user packages."
-        python_module_exist pip || panic "No installer available (https://pip.pypa.io/en/stable/installing/)! Please run: curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py"
-        python -m pip install --user $package
+        python_module_exist pip || panic "No installer available (https://pip.pypa.io/en/stable/installing/)! Please run: curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && $PYTHON get-pip.py"
+        $PYTHON -m pip install --user $package
     fi
 }
 
+exists python3 && PYTHON=python3
+exists $PYTHON || panic "No python interpreter available, please install python 3!"
 
-exists python || panic "No python interpreter available, please install python!"
-install_deps absl absl-py
-install_deps requests requests
-install_deps enum enum34
+# Check for python 3, and install compatibility layers if needed.
+check_python_version
 
-python $PROGDIR/build/python/cmake.py --noshowprefixforinfo  $*
+# Install needed depencies..
+install_deps absl absl-py      # Used to parse parameters.
+install_deps requests requests # We make outgoing calls..
+
+$PYTHON $PROGDIR/build/python/cmake.py --noshowprefixforinfo $*
