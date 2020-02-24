967c24bf24da305c83a54bbcb857d4042d40171d
Huihong Luo <huisinro@google.com>
2020-02-24 10:02:12 -0800

    Add EDID generator
    so edid bytes can be generated, instead of hardcoded values.
    Guest side calls into host side to fetch edid values.
    The generator code is modified from QEMU upstream source.
    
    Bug: 150056162
    Change-Id: Ie60fcce5cc7ddc33fc2fd265e36bcd70201cee8e

---
+++ android/android-emugl/host/libs/libOpenglRender/EdidGenerator.h
@@ -0,0 +1,19 @@
+#ifndef EDID_H
+#define EDID_H
+
+typedef struct qemu_edid_info {
+    const char *vendor; /* http://www.uefi.org/pnp_id_list */
+    const char *name;
+    const char *serial;
+    uint32_t    dpi;
+    uint32_t    prefx;
+    uint32_t    prefy;
+    uint32_t    maxx;
+    uint32_t    maxy;
+} qemu_edid_info;
+
+void qemu_edid_generate(uint8_t *edid, size_t size,
+                        qemu_edid_info *info);
+size_t qemu_edid_size(uint8_t *edid);
+
+#endif /* EDID_H */
