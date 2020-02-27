7db3067630a16b67857c367d72fd5c35bd5d1255
Erwin Jansen <jansene@google.com>
2020-02-26 16:48:46 -0800

    [gRPC] Refactor and WebRTC improvements
    ** Breaking change for JS clients! **
    
    This contains a series of fixes and improvements for our gRPC services.
    Over time we have started to accumulate more and more services. With
    this change we refactor each service into an individual service point
    that is registered using the builder pattern.
    
    Part of the refactor extracts the RTC service from the emulator
    controllor. This is done for the following reasons:
    
    - We want to converge to the same set of proto files with acid.
    - WebRTC is now lazily configured. On the first handshake all proceses
    are initialized. This reduces the risk of dangling processes when the
    emulator does not gracefully terminate.
    - Use streaming v.s. dangling requests. The dangling requests can cause
    issues with graceful termination.
    - The JavaScript endpoints will have to use the new service endpoint and
    will require an upgrade.
    - ACID should start the emulator with the -share-vid flag, to guarantee
      that the shared memory region is active, and will receive frames.
    
    Fixes updating of licenses in some headers.
    
    Bug: b/150248788
    Change-Id: I1b3fa9a4d599fc7c338ff6977b46912c86115d21

--- android-qemu2-glue/qemu-grpc-agent-impl.cpp
+++ android-qemu2-glue/qemu-grpc-agent-impl.cpp
@@ -9,46 +9,16 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-#include <string>  // for string
-
-#include "android-qemu2-glue/qemu-control-impl.h"       // IWYU pragma: keep
-#include "android/cmdline-option.h"                     // for AndroidOptions
-#include "android/console.h"                            // for AndroidConsol...
-#include "android/emulation/control/EmulatorService.h"  // for EmulatorContr...
-#include "android/emulation/control/grpc_agent.h"       // for QGrpcAgent
-
-#ifdef ANDROID_WEBRTC
-#include "android/emulation/control/WebRtcBridge.h"  // for WebRtcBridge
-#endif
-
-#include "android/emulation/control/GrpcServices.h"  // for GrpcServices
-
-namespace android {
-namespace emulation {
-namespace control {
-class RtcBridge;
-}  // namespace control
-}  // namespace emulation
-}  // namespace android
+#include "android-qemu2-glue/qemu-control-impl.h"  // IWYU pragma: keep
+#include "android-qemu2-glue/qemu-setup.h"         // for qemu_setup_grpc
+#include "android/console.h"                       // for AndroidConsoleAgents
+#include "android/emulation/control/grpc_agent.h"  // for QGrpcAgent
 
 extern const AndroidConsoleAgents* getConsoleAgents();
+extern int qemu_setup_grpc(void);
 
 int start_grpc(int port, const char* turncfg) {
-    android::emulation::control::RtcBridge* rtc_bridge = nullptr;
-#ifdef ANDROID_WEBRTC
-    std::string turn = "";
-    if (android_cmdLineOptions->turncfg) {
-        turn = android_cmdLineOptions->turncfg;
-    }
-    rtc_bridge = android::emulation::control::WebRtcBridge::create(
-            0, getConsoleAgents(), turn);
-#else
-    rtc_bridge = new android::emulation::control::NopRtcBridge();
-#endif
-    auto service = android::emulation::control::GrpcServices::setup(
-            port, port + 1, "0.0.0.0", getConsoleAgents(), rtc_bridge,
-            "forward");
-    return service ? service->port() : -1;
+    return qemu_setup_grpc();
 }
 
 static const QGrpcAgent grpcAgent = {
