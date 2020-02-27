f12d67f649fb9221a8b39843d58d34d1cefa4a03
Erwin Jansen <jansene@google.com>
2020-02-26 16:48:46 -0800

    [gRPC] Refactor and WebRTC improvements
    ** Breaking change for JS clients! **
    
    This contains a series of fixes and improvements for our gRPC services.
    Over time we have started to accumulate more and more services. With
    this change we refactor each service into an individual service point
    that is registered using the builder pattern.
    
    Part of the refactor extracts the RTC service from the emulator
    controllor. This is done for the following reasons:
    
    - We want to converge to the same set of proto files with acid.
    - WebRTC is now lazily configured. On the first handshake all proceses
    are initialized. This reduces the risk of dangling processes when the
    emulator does not gracefully terminate.
    - Use streaming v.s. dangling requests. The dangling requests can cause
    issues with graceful termination.
    - The JavaScript endpoints will have to use the new service endpoint and
    will require an upgrade.
    - ACID should start the emulator with the -share-vid flag, to guarantee
      that the shared memory region is active, and will receive frames.
    - We mark one method a DEPRECATED, which is currently needed for
      fuchsia.
    
    Fixes updating of licenses in some headers.
    
    Bug: b/150248788
    Change-Id: I1b3fa9a4d599fc7c338ff6977b46912c86115d21

--- android/android-grpc/android/emulation/control/GrpcServices.cpp
+++ android/android-grpc/android/emulation/control/GrpcServices.cpp
@@ -1,58 +1,191 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "android/emulation/control/GrpcServices.h"
 
-#include "android/base/files/PathUtils.h"               // for PathUtils
-#include "android/emulation/ConfigDirs.h"               // for ConfigDirs
-#include "android/emulation/control/EmulatorService.h"  // for EmulatorContr...
-#include "android/emulation/control/RtcBridge.h"        // for RtcBridge
+#ifdef _MSC_VER
+#include "msvc-posix.h"
+#else
+#endif
+#include <assert.h>
+#include <grpcpp/grpcpp.h>
+#include <stdio.h>
 
-using android::emulation::control::EmulatorControllerService;
-using android::emulation::control::GrpcServices;
-using android::emulation::control::NopRtcBridge;
-using android::emulation::control::RtcBridge;
+#include <chrono>
+#include <fstream>
+#include <iterator>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
-const std::string GrpcServices::kCertFileName{"emulator-grpc.cer"};
-const std::string GrpcServices::kPrivateKeyFileName{"emulator-grpc.key"};
+#include "android/base/Log.h"
+#include "android/base/sockets/ScopedSocket.h"
+#include "android/base/sockets/SocketUtils.h"
+#include "android/base/system/System.h"
+#include "android/console.h"
+#include "android/emulation/control/RtcBridge.h"
+#include "android/emulation/control/interceptor/LoggingInterceptor.h"
+#include "android/emulation/control/interceptor/MetricsInterceptor.h"
+#include "grpcpp/server_builder_impl.h"
+#include "grpcpp/server_impl.h"
 
-std::unique_ptr<EmulatorControllerService> GrpcServices::g_controler_service =
-        nullptr;
-std::unique_ptr<RtcBridge> GrpcServices::g_rtc_bridge = nullptr;
+namespace android {
+namespace emulation {
+namespace control {
 
-EmulatorControllerService* GrpcServices::setup(
-        int startPort,
-        int endPort,
-        std::string address,
-        const AndroidConsoleAgents* const consoleAgents,
-        RtcBridge* rtcBridge,
-        const char* waterfall) {
-    // Return the active service
-    if (g_controler_service) {
-        return g_controler_service.get();
+using Builder = EmulatorControllerService::Builder;
+using namespace android::base;
+using namespace android::control::interceptor;
+using grpc::ServerBuilder;
+using grpc::Service;
+
+// This class owns all the created resources, and is repsonsible for stopping
+// and properly releasing resources.
+class EmulatorControllerServiceImpl : public EmulatorControllerService {
+public:
+    void stop() override {
+        auto deadline = std::chrono::system_clock::now() +
+                        std::chrono::milliseconds(500);
+        mServer->Shutdown(deadline);
     }
 
-    g_rtc_bridge.reset(rtcBridge);
-    g_rtc_bridge->start();
+    EmulatorControllerServiceImpl(
+            int port,
+            std::string cert,
+            std::vector<std::shared_ptr<Service>> services,
+            grpc::Server* server)
+        : mPort(port),
+          mCert(cert),
+          mRegisteredServices(services),
+          mServer(server) {}
 
-    auto builder =
-            EmulatorControllerService::Builder()
-                    .withConsoleAgents(consoleAgents)
-                    .withCertAndKey(
-                            base::PathUtils::join(
-                                    android::ConfigDirs::getUserDirectory(),
-                                    kCertFileName),
-                            base::PathUtils::join(
-                                    android::ConfigDirs::getUserDirectory(),
-                                    kPrivateKeyFileName))
-                    .withPortRange(startPort, endPort)
-                    .withAddress(address)
-                    .withWaterfall(waterfall)
-                    .withRtcBridge(g_rtc_bridge.get());
+    int port() override { return mPort; }
+    std::string publicCert() override { return mCert; }
 
-    g_controler_service = builder.build();
-    return g_controler_service ? g_controler_service.get() : nullptr;
+private:
+    std::unique_ptr<grpc::Server> mServer;
+    std::vector<std::shared_ptr<Service>> mRegisteredServices;
+    int mPort;
+    std::string mCert;
+};
+
+Builder::Builder() : mCredentials{grpc::InsecureServerCredentials()} {}
+
+Builder& Builder::withConsoleAgents(
+        const AndroidConsoleAgents* const consoleAgents) {
+    mAgents = consoleAgents;
+    return *this;
 }
 
-void GrpcServices::teardown() {
-    if (g_controler_service) {
-        g_controler_service->stop();
+int Builder::port() {
+    return mPort;
+}
+
+Builder& Builder::withService(Service* service) {
+    if (service != nullptr)
+        mServices.emplace_back(std::shared_ptr<Service>(service));
+    return *this;
+}
+
+Builder& Builder::withCertAndKey(std::string certfile,
+                                 std::string privateKeyFile) {
+    if (!System::get()->pathExists(certfile)) {
+        LOG(WARNING) << "Cannot find certfile: " << certfile
+                     << " security will be disabled.";
+        return *this;
     }
+
+    if (!!System::get()->pathExists(privateKeyFile)) {
+        LOG(WARNING) << "Cannot find private key file: " << privateKeyFile
+                     << " security will be disabled, the emulator might be "
+                        "publicly accessible!";
+        return *this;
+    }
+    mCertfile = certfile;
+
+    std::ifstream key_file(privateKeyFile);
+    std::string key((std::istreambuf_iterator<char>(key_file)),
+                    std::istreambuf_iterator<char>());
+
+    std::ifstream cert_file(certfile);
+    std::string cert((std::istreambuf_iterator<char>(cert_file)),
+                     std::istreambuf_iterator<char>());
+
+    grpc::SslServerCredentialsOptions::PemKeyCertPair keycert = {key, cert};
+    grpc::SslServerCredentialsOptions ssl_opts;
+    ssl_opts.pem_key_cert_pairs.push_back(keycert);
+    mCredentials = grpc::SslServerCredentials(ssl_opts);
+    return *this;
 }
+
+Builder& Builder::withAddress(std::string address) {
+    mBindAddress = address;
+    return *this;
+}
+
+Builder& Builder::withPortRange(int start, int end) {
+    assert(end > start);
+    int port = start;
+    bool found = false;
+    for (port = start; !found && port < end; port++) {
+        // Find a free port.
+        android::base::ScopedSocket s0(socketTcp4LoopbackServer(port));
+        if (s0.valid()) {
+            mPort = android::base::socketGetPort(s0.get());
+            found = true;
+        }
+    }
+
+    return *this;
+}  // namespace control
+
+std::unique_ptr<EmulatorControllerService> Builder::build() {
+    if (mAgents == nullptr || mPort == -1) {
+        // No agents, or no port was found.
+        LOG(INFO) << "No agents, or valid port was found";
+        return nullptr;
+    }
+
+    std::string server_address = mBindAddress + ":" + std::to_string(mPort);
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, mCredentials);
+    for (auto service : mServices) {
+        builder.RegisterService(service.get());
+    }
+
+    // Register logging & metrics interceptor.
+    std::vector<std::unique_ptr<
+            grpc::experimental::ServerInterceptorFactoryInterface>>
+            creators;
+    creators.emplace_back(std::make_unique<StdOutLoggingInterceptorFactory>());
+    creators.emplace_back(std::make_unique<MetricsInterceptorFactory>());
+    builder.experimental().SetInterceptorCreators(std::move(creators));
+
+    // Allow large messages, as raw screenshots can take up a significant amount
+    // of memory.
+    const int megaByte = 1024 * 1024;
+    builder.SetMaxSendMessageSize(16 * megaByte);
+    auto service = builder.BuildAndStart();
+    if (!service)
+        return nullptr;
+
+    fprintf(stderr, "Started GRPC server at %s\n", server_address.c_str());
+    return std::unique_ptr<EmulatorControllerService>(
+            new EmulatorControllerServiceImpl(
+                    mPort, mCertfile, std::move(mServices), service.release()));
+}
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
