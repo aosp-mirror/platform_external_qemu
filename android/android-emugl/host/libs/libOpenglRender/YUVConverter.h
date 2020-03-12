8f8146a0da49105349281402212e3f32762e21ea
bohu <bohu@google.com>
2020-03-12 15:46:36 -0700

    codecs: add yuv related virtio-gpu-ops
    1. create yuv textures
    2. destroy them
    3. copy decoded frame into YUV textures
    4. swap out the YUV textures of existing colorbuffer;
    and update the color buffer content afterwards
    
    BUG: 150378029
    Test: Build
    
    Change-Id: I760e149f693d2c6297f8b414cfaaa00184539b09

--- android/android-emugl/host/libs/libOpenglRender/YUVConverter.h
+++ android/android-emugl/host/libs/libOpenglRender/YUVConverter.h
@@ -21,6 +21,7 @@
 
 #include "FrameworkFormats.h"
 
+#include <stdint.h>
 // The purpose of YUVConverter is to use
 // OpenGL shaders to convert YUV images to RGB
 // images that can be displayed on screen.
@@ -52,6 +53,16 @@ public:
     // the host color buffer
     // (rcUpdateColorBuffer)
     void drawConvert(int x, int y, int width, int height, char* pixels);
+
+    void swapTextures(uint32_t type, uint32_t* textures);
+
+    // public so other classes can call
+    static void createYUVGLTex(GLenum texture_unit,
+                               GLsizei width,
+                               GLsizei height,
+                               GLuint* texName_out,
+                               bool uvInterleaved);
+
 private:
     // For dealing with n-pixel-aligned buffers
     void updateCutoffs(float width, float ywidth, float halfwidth, float cwidth);
