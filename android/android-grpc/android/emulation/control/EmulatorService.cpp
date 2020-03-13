4e0b148e8f7eaf6d4017430ae7193c39b13459fc
Lingfeng Yang <lfy@google.com>
2020-03-13 00:09:02 -0700

    embedded emulator: fix inconsistent rotation
    bug: 150822527
    bug: 150960153
    
    - Fix rotation translation to follow that of hw-sensors.cpp
    - Use skin coordinate system for screenshots, which is clockwise not
    counterclockwise
    
    Test: Rotation and input all work in all orientations
    
    Change-Id: Iad428685edd621b58a2d3287ba056c197a1cf86c

--- android/android-grpc/android/emulation/control/EmulatorService.cpp
+++ android/android-grpc/android/emulation/control/EmulatorService.cpp
@@ -370,12 +370,13 @@ public:
                      const MouseEvent* requestPtr,
                      ::google::protobuf::Empty* reply) override {
         MouseEvent request = *requestPtr;
-        auto agent = mAgents->user_event;
 
         android::base::ThreadLooper::runOnMainLooper(
-            [agent, request]() {
-            agent->sendMouseEvent(request.x(), request.y(), 0,
-                    request.buttons(), 0);
+            [this, request]() {
+            mAgents->user_event->sendMouseEvent(
+                request.x(),
+                request.y(), 0,
+                request.buttons(), 0);
         });
 
         return Status::OK;
@@ -492,11 +493,10 @@ public:
 
         // TODO(jansene): Not clear if the rotation impacts displays other
         // than 0.
-        if (request->display() == 0) {
-            mAgents->sensors->getPhysicalParameter(
-                    PHYSICAL_PARAMETER_ROTATION, &xaxis, &yaxis, &zaxis,
-                    PARAMETER_VALUE_TYPE_CURRENT);
-        }
+        mAgents->sensors->getPhysicalParameter(
+                PHYSICAL_PARAMETER_ROTATION, &xaxis, &yaxis, &zaxis,
+                PARAMETER_VALUE_TYPE_CURRENT);
+
         auto rotation = ScreenshotUtils::coarseRotation(zaxis);
 
         // Calculate the desired rotation and width we should use..
@@ -531,6 +531,15 @@ public:
         if (rotation == Rotation::LANDSCAPE ||
             rotation == Rotation::REVERSE_LANDSCAPE) {
             std::swap(newWidth, newHeight);
+        }
+
+        // Screenshot backend uses the _skin layout_'s orientation,
+        // which is clockwise instead of counterclockwise.
+        // This makes the 90/270 cases flipped.
+        if (desiredRotation == SKIN_ROTATION_270) {
+            desiredRotation = SKIN_ROTATION_90;
+        } else if (desiredRotation == SKIN_ROTATION_90) {
+            desiredRotation = SKIN_ROTATION_270;
         }
 
         // Screenshots can come from either the gl renderer, or the guest.
