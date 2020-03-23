db1c3f5848696b10aa3a12ed37a23c413adafa60
Lingfeng Yang <lfy@google.com>
2020-03-23 13:28:32 -0700

    Add asg to transports that are unit tested
    Change-Id: If03c742d1cba7ff7a50741632aedd8883ba32f66

--- android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
+++ android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
@@ -86,7 +86,7 @@ static AndroidOptions sTestEnvCmdLineOptions;
 
 // static
 std::vector<const char*> GoldfishOpenglTestEnv::getTransportsToTest() {
-    return { "pipe", };
+    return { "pipe", "asg" };
 }
 
 GoldfishOpenglTestEnv::GoldfishOpenglTestEnv() {
