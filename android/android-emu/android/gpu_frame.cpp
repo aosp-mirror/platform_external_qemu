b1fa77f7ddb302b6a22e311e49d3659bcbc03e2d
Erwin Jansen <jansene@google.com>
2020-04-02 17:11:57 +0200

    [gRPC] Reduce delay for callback registration
    The streamScreenShot endpoint can experience significant delays if
    multiple concurrent requests arrive, even when they get cancelled.
    
    The delays are due to a frame repost request upon (un)registration.
    This is an expensive call blocking call that leads to serialization of
    the registration requests.  We now will have only one active scheduled
    repost call at a time.
    
    The fix for this exposed a bug in the FrameReceiverState class.
    The EventWaiter class relies on never receiving callbacks upon
    unregistration. However events can still arrive as unregister was
    processed on a separate thread. This never lead to problems as these
    usually happened while we were waiting for frame repost to complete.
    
    The Framereceiver class is moved into a public class with a set of unit
    tests that  exposed the issue.
    
    Bug: b/151458862
    Change-Id: Ib8a9b65d72afbcb20d1a1bb3b43c579349a22a29

--- android/android-emu/android/gpu_frame.cpp
+++ android/android-emu/android/gpu_frame.cpp
@@ -14,13 +14,14 @@
 
 #include "android/gpu_frame.h"
 
-#include <atomic>                                         // for atomic, __a...
-#include <cstddef>                                        // for NULL
-#include <unordered_map>                                  // for unordered_map
-#include <utility>                                        // for pair, move
-#include <vector>                                         // for vector
+#include <atomic>         // for atomic, __a...
+#include <cstddef>        // for NULL
+#include <unordered_map>  // for unordered_map
+#include <utility>        // for pair, move
+#include <vector>         // for vector
 
 #include "android/base/Log.h"                             // for LogMessage
+#include "android/base/async/CallbackRegistry.h"
 #include "android/base/memory/LazyInstance.h"             // for LazyInstance
 #include "android/base/synchronization/Lock.h"            // for AutoLock, Lock
 #include "android/base/synchronization/MessageChannel.h"  // for MessageChannel
@@ -28,7 +29,18 @@
 #include "android/opengl/virtio_gpu_ops.h"                // for AndroidVirt...
 #include "android/opengles.h"                             // for android_set...
 
+
 using android::base::AutoLock;
+
+/* set >0 for very verbose debugging */
+#define DEBUG 1
+#ifndef DEBUG
+#define DD(...) (void)0
+#else
+#define DD(fmt, ...)                                                   \
+    fprintf(stderr, "gpu_frame: %s:%d| " fmt "\n", __func__, __LINE__, \
+            ##__VA_ARGS__)
+#endif
 
 namespace android {
 namespace base {
@@ -41,101 +53,19 @@ class Looper;
 #define GL_UNSIGNED_BYTE 0x1401
 
 using android::opengl::GpuFrameBridge;
+using android::base::CallbackRegistry;
 
 static GpuFrameBridge* sBridge = NULL;
 // We need some way to disable the post() if only the recording is using that
 // path and it is not in use because glReadPixels will slow down everything.
 static bool sIsGuestMode = false;
+static std::atomic_bool sRequestPost{false};
 
-class FrameReceiverState {
-public:
-    FrameReceiverState() = default;
-
-    void registerCallback(FrameAvailableCallback frameAvailable, void* opaque) {
-        ForwarderMessage msg = {
-                .cmd = FRAME_FORWARDER_ADD,
-                .data = opaque,
-                .callback = frameAvailable,
-        };
-
-        mMessages.send(msg);
-    }
-
-    void unregisterCallback(void* opaque) {
-        ForwarderMessage msg = {
-                .cmd = FRAME_FORWARDER_REMOVE,
-                .data = opaque,
-                .callback = {},
-        };
-
-        mMessages.send(msg);
-    }
-
-    void process() {
-        ForwarderMessage msg;
-
-        while (mMessages.tryReceive(&msg)) {
-            switch (msg.cmd) {
-                case FRAME_FORWARDER_ADD: {
-                    AutoLock lock(mLock);
-                    mStoredForwarders[msg.data] = std::move(msg.callback);
-                    break;
-                }
-                case FRAME_FORWARDER_REMOVE: {
-                    AutoLock lock(mLock);
-                    mStoredForwarders.erase(msg.data);
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-
-        struct RegisteredForwarder {
-            void* data;
-            FrameAvailableCallback callback;
-        };
-
-        std::vector<RegisteredForwarder> toRun;
-
-        AutoLock lock(mLock);
-        for (auto it : mStoredForwarders) {
-            RegisteredForwarder rf = {
-                    .data = it.first,
-                    .callback = it.second,
-            };
-            toRun.push_back(rf);
-        }
-        lock.unlock();
-
-        for (const auto& rf : toRun) {
-            rf.callback(rf.data);
-        }
-    }
-
-private:
-    enum FrameForwarderCommand {
-        FRAME_FORWARDER_ADD = 0,
-        FRAME_FORWARDER_REMOVE = 1,
-    };
-
-    struct ForwarderMessage {
-        FrameForwarderCommand cmd;
-        void* data;
-        FrameAvailableCallback callback;
-    };
-
-    android::base::MessageChannel<ForwarderMessage, 16> mMessages;
-
-    android::base::Lock mLock;  // protects mStoredForwarders
-    std::unordered_map<void*, FrameAvailableCallback> mStoredForwarders;
-};
-
-static android::base::LazyInstance<FrameReceiverState> sReceiverState =
+static android::base::LazyInstance<CallbackRegistry> sReceiverState =
         LAZY_INSTANCE_INIT;
 
 static void frameReceivedForwader(void* __ignored) {
-    sReceiverState->process();
+    sReceiverState->invokeCallbacks();
 }
 
 // Used to keep track of how many recorders we have active.
@@ -288,11 +218,19 @@ void gpu_register_shared_memory_callback(FrameAvailableCallback frameAvailable,
                                          void* opaque) {
     sReceiverState->registerCallback(frameAvailable, opaque);
     gpu_frame_set_record_mode(true);
-    android_getVirtioGpuOps()->repost();
+    bool expected = false;
+    if (sRequestPost.compare_exchange_strong(expected, true)) {
+        android_getVirtioGpuOps()->repost();
+        sRequestPost.store(false);
+    }
 }
 
 void gpu_unregister_shared_memory_callback(void* opaque) {
     sReceiverState->unregisterCallback(opaque);
     gpu_frame_set_record_mode(false);
-    android_getVirtioGpuOps()->repost();
+    bool expected = false;
+    if (sRequestPost.compare_exchange_strong(expected, true)) {
+        android_getVirtioGpuOps()->repost();
+        sRequestPost.store(false);
+    }
 }
