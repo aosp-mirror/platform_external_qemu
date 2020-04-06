3ac00ccf0685bd4da1b8e73a2c89a878333a4b77
bohu <bohu@google.com>
2020-04-05 20:34:26 -0700

    codecs: add vpx ping parser
    This will read the command parameters
    from vpx guest side decoder.
    
    BUG: 150378029
    Change-Id: Id82a8a0256214a947334a0dd53a1667080fe5e20

--- android/android-emu/android-emu.cmake
+++ android/android-emu/android-emu.cmake
@@ -93,6 +93,7 @@ set(android-emu-common
     android/emulation/address_space_host_media.cpp
     android/emulation/H264NaluParser.cpp
     android/emulation/VpxFrameParser.cpp
+    android/emulation/VpxPingInfoParser.cpp
     android/emulation/H264PingInfoParser.cpp
     android/emulation/HostmemIdMapping.cpp
     android/emulation/MediaVpxDecoder.cpp
