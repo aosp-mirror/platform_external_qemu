46018c603a33869f0d34d8919fae669f467faa6c
Erwin Jansen <jansene@google.com>
2020-04-06 17:55:15 +0200

    Reduce flakiness in windows callback test
    This removes all timing from (sleep/yield) from the unit tests, this in
    turn exposed a subtle bug where unregistered events could still get
    fired if multiple threads are processing events concurrently.
    
    Bug: b/153381599
    Change-Id: I688a72208a54af8cc6c39e47853a547bbbff426b

--- android/android-emu/android/base/async/CallbackRegistry_unittest.cpp
+++ android/android-emu/android/base/async/CallbackRegistry_unittest.cpp
@@ -14,111 +14,134 @@
 
 #include "android/base/async/CallbackRegistry.h"
 
-#include <gtest/gtest.h>
+#include <gtest/gtest.h>  // for Message, TestPartResult, EXP...
 
-#include <chrono>
-#include <thread>
+#include <chrono>  // for operator<, operator+, system...
+#include <string>  // for string
+#include <thread>  // for thread
+#include <vector>  // for vector
 
-#include "android/base/system/System.h"
+#include "android/base/system/System.h"  // for System
 
-static std::unordered_map<void*, int> sInvokeCnt{};
-
+using android::base::AutoLock;
 using android::base::CallbackRegistry;
+using android::base::ConditionVariable;
+using android::base::Lock;
 using android::base::System;
 using std::chrono::system_clock;
+using std::chrono::time_point;
+
+static std::atomic_int sInvokeCnt{0};
 
 void fake_cb(void* opaque) {
-    sInvokeCnt[opaque]++;
+    sInvokeCnt++;
+}
+
+void called_by_foo(void* opaque) {
+    // Copy into a string. Otherwise if you introduce concurrency failures
+    // You might see failures, even though the error claims that
+    // the string are equivalent.
+    std::string incoming = static_cast<char*>(opaque);
+    EXPECT_EQ(incoming, "foo");
+    sInvokeCnt++;
 }
 
 class CallbackRegistryTest : public ::testing::Test {
 protected:
-    void SetUp() override { sInvokeCnt.clear(); }
-    CallbackRegistry mReg;
-};
+    void SetUp() override {
+        sInvokeCnt = 0;
+        mDone = false;
+    }
 
-TEST_F(CallbackRegistryTest, add_increases_count) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
-    std::string foo = "foo";
-    mReg.registerCallback(fake_cb, &foo);
-    mReg.invokeCallbacks();
-    EXPECT_EQ(sInvokeCnt[&foo], 1);
-}
+    void invokeCallback() {
+        mReg.invokeCallbacks();
+        mCallbackLock.lock();
+        mCvCallback.signalAndUnlock(&mCallbackLock);
+    }
 
-TEST_F(CallbackRegistryTest, no_events_after_remove) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
-    std::string foo = "foo";
-    mReg.registerCallback(fake_cb, &foo);
-    mReg.unregisterCallback(&foo);
-    mReg.invokeCallbacks();
-    EXPECT_EQ(sInvokeCnt[&foo], 0);
-}
+    void waitUntilProcessed() {
+        AutoLock lock(mCallbackLock);
+        // We processed all outstanding requests if the
+        // queue is empty and we are not pushing through events.
+        mCvCallback.wait(&mCallbackLock, [=]() {
+            return mReg.available() == mReg.max_slots() && !mReg.processing();
+        });
+    }
 
-TEST_F(CallbackRegistryTest, no_increase_after_remove) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
-    std::string foo = "foo";
-    mReg.registerCallback(fake_cb, &foo);
-    mReg.invokeCallbacks();
-    mReg.unregisterCallback(&foo);
-    mReg.invokeCallbacks();
-    EXPECT_EQ(sInvokeCnt[&foo], 1);
-}
+    void invokeCallback(system_clock::time_point until) {
+        // Keep processing requests until the time is over and
+        // the other thread has not yet finished.
+        while (system_clock::now() < until || !mDone) {
+            invokeCallback();
+        }
+    }
 
-void called_by_foo(void* opaque) {
-    std::string* incoming = reinterpret_cast<std::string*>(opaque);
-    EXPECT_EQ(*incoming, "foo");
-}
-
-TEST_F(CallbackRegistryTest, no_calls_after_unregister) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
-    auto until = system_clock::now() + std::chrono::milliseconds(100);
-    std::thread invokeCallbacks([=]() {
-        while (system_clock::now() < until)
-            mReg.invokeCallbacks();
-    });
-    std::thread reg_unreg([=]() {
-        std::string foo = "foo";
+    void noCallAfterUnregister(system_clock::time_point until) {
+        char foo[4] = "foo";
+        int loop = 0;
         while (system_clock::now() < until) {
+            loop++;
             foo[2] = 'o';
+            waitUntilProcessed();
+            EXPECT_EQ(mReg.available(), mReg.max_slots());
             mReg.registerCallback(called_by_foo, &foo);
-            System::get()->yield();
+            // Make sure we actually got registered.
+            waitUntilProcessed();
             mReg.unregisterCallback(&foo);
-
             // Callback should never been called after unregister.
             // if it does, it might see fox instead of foo.
             foo[2] = 'x';
-            EXPECT_EQ("fox", foo);
+            EXPECT_STREQ("fox", foo);
         }
-    });
+        mDone = true;
+    }
 
+    CallbackRegistry mReg;
+    Lock mCallbackLock;
+    ConditionVariable mCvCallback;
+    bool mDone;
+};
+
+TEST_F(CallbackRegistryTest, add_increases_count) {
+    char foo[4] = "foo";
+    mReg.registerCallback(fake_cb, &foo);
+    mReg.invokeCallbacks();
+    EXPECT_EQ(sInvokeCnt.load(), 1);
+}
+
+TEST_F(CallbackRegistryTest, no_events_after_remove) {
+    char foo[4] = "foo";
+    mReg.registerCallback(fake_cb, &foo);
+    mReg.unregisterCallback(&foo);
+    mReg.invokeCallbacks();
+    EXPECT_EQ(sInvokeCnt.load(), 0);
+}
+
+TEST_F(CallbackRegistryTest, no_increase_after_remove) {
+    char foo[4] = "foo";
+    mReg.registerCallback(fake_cb, &foo);
+    mReg.invokeCallbacks();
+    mReg.unregisterCallback(&foo);
+    mReg.invokeCallbacks();
+    EXPECT_EQ(sInvokeCnt.load(), 1);
+}
+
+TEST_F(CallbackRegistryTest, no_calls_after_unregister) {
+    auto until = system_clock::now() + std::chrono::milliseconds(100);
+    std::thread invokeCallbacks([=]() { invokeCallback(until); });
+    std::thread reg_unreg([=]() { noCallAfterUnregister(until); });
     invokeCallbacks.join();
     reg_unreg.join();
 }
 
 TEST_F(CallbackRegistryTest, no_calls_after_unregister_with_many_processors) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
     auto until = system_clock::now() + std::chrono::milliseconds(100);
     std::vector<std::thread> processors;
 
-    processors.emplace_back([=]() {
-        while (system_clock::now() < until)
-            mReg.invokeCallbacks();
-    });
-
-    std::thread reg_unreg([=]() {
-        std::string foo = "foo";
-        while (system_clock::now() < until) {
-            foo[2] = 'o';
-            mReg.registerCallback(called_by_foo, &foo);
-            System::get()->yield();
-            mReg.unregisterCallback(&foo);
-
-            // Callback should never been called after unregister.
-            // if it does, it might see fox instead of foo.
-            foo[2] = 'x';
-            EXPECT_EQ("fox", foo);
-        }
-    });
+    for (int i = 0; i < 5; i++) {
+        processors.emplace_back([=]() { invokeCallback(until); });
+    }
+    std::thread reg_unreg([=]() { noCallAfterUnregister(until); });
 
     reg_unreg.join();
     for (auto& t : processors) {
@@ -127,23 +150,21 @@ TEST_F(CallbackRegistryTest, no_calls_after_unregister_with_many_processors) {
 }
 
 TEST_F(CallbackRegistryTest, safe_exit_when_processing_stops) {
-    EXPECT_EQ(sInvokeCnt.size(), 0);
     auto until = system_clock::now() + std::chrono::milliseconds(10);
     std::thread invokeCallbacks([=]() {
-        while (system_clock::now() < until)
-            mReg.invokeCallbacks();
+        mDone = true;
+        invokeCallback(until);
         mReg.close();
     });
 
-    auto reg_until = system_clock::now() + std::chrono::milliseconds(10);
+    auto reg_until = system_clock::now() + std::chrono::milliseconds(20);
     ASSERT_LT(until, reg_until);
 
     std::thread reg_unreg([=]() {
-        std::string foo = "foo";
+        std::string unused = "unused";
         while (system_clock::now() < reg_until) {
-            foo[2] = 'o';
-            mReg.registerCallback(called_by_foo, &foo);
-            mReg.unregisterCallback(&foo);
+            mReg.registerCallback(fake_cb, &unused);
+            mReg.unregisterCallback(&unused);
         }
     });
 
