0e1f85e619471af917e1f60a8265dc5dc7df0aa4
Huan Song <huans@google.com>
2020-04-09 21:26:24 +0000

    Revert^2 "consle screenshot by displayId"
    ff209ea644c279ffd1f1be87e39c5af34ae8d3a0
    
    Change-Id: I8c485e37fd54d90f1b71bb3c90d45afdc3cfb77d

--- android/android-emu/android/emulation/control/ScreenCapturer.cpp
+++ android/android-emu/android/emulation/control/ScreenCapturer.cpp
@@ -34,12 +34,13 @@ namespace android {
 namespace emulation {
 
 bool captureScreenshot(android::base::StringView outputDirectoryPath,
-                       std::string* pOutputFilepath) {
+                       std::string* pOutputFilepath,
+                       uint32_t displayId) {
     const auto& renderer = android_getOpenglesRenderer();
     SkinRotation rotation = gQAndroidEmulatorWindowAgent->getRotation();
     if (const auto renderer_ptr = renderer.get()) {
         return captureScreenshot(renderer_ptr, nullptr, rotation,
-                                 outputDirectoryPath, pOutputFilepath);
+                                 outputDirectoryPath, pOutputFilepath, displayId);
     } else {
         // renderer is nullptr in -gpu guest
         return captureScreenshot(
@@ -161,6 +162,7 @@ bool captureScreenshot(
     Image img = takeScreenshot(ImageFormat::RAW, rotation, renderer, getFrameBuffer, displayId);
 
     if (img.getWidth() == 0 || img.getHeight() == 0) {
+        LOG(ERROR) << "take screenshot failed";
         return false;
     }
 
@@ -204,6 +206,7 @@ bool captureScreenshot(
     if (pOutputFilepath) {
         *pOutputFilepath = outputFilePath;
     }
+
     // already rotated through rendering
     rotation = renderer ? SKIN_ROTATION_0 : rotation;
     savepng(outputFilePath.c_str(), img.getChannels(), img.getWidth(),
