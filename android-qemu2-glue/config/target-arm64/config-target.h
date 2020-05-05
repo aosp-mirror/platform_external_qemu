07253d360d2482da2ef86bca4baeaa285d00ccb9
bohu <bohu@google.com>
2020-05-05 15:58:00 -0700

    aarch64: enable kvm config for aarch64
    BUG: 152456395
    BUG: 146226835
    Change-Id: Ia60e81fde40dd0bbf088d73b17fe38c0e7cd960b

--- android-qemu2-glue/config/target-arm64/config-target.h
+++ android-qemu2-glue/config/target-arm64/config-target.h
@@ -2,7 +2,10 @@
 #define TARGET_AARCH64 1
 #define TARGET_NAME "aarch64"
 #define TARGET_ARM 1
+#define CONFIG_KVM 1
 #define CONFIG_SOFTMMU 1
 #define TARGET_SUPPORTS_MTTCG 1
 #define CONFIG_I386_DIS 1
 #define CONFIG_ARM_DIS 1
+#define CONFIG_ARM_A64_DIS 1
+
