12c261feaab111aa2de04f44c777003eaf21b168
Bo Hu <bohu@google.com>
2020-05-08 19:00:07 +0000

    Revert "Stop recording when display remove or size change."
    This reverts commit 5135387c2e6935919f3b2735d31f5580f51abbe5.
    
    Reason for revert: try
    
    Change-Id: Ie13937137ee097441fe75fd9f7d0f66d247547e9

--- android/android-emu/android/recording/screen-recorder.cpp
+++ android/android-emu/android/recording/screen-recorder.cpp
@@ -95,7 +95,7 @@ public:
     bool startRecording(bool async);
     bool stopRecording(bool async);
 
-    RecorderStates getRecorderState() const;
+    RecorderState getRecorderState() const;
 
 private:
     bool startRecordingWorker();
@@ -153,9 +153,9 @@ ScreenRecorder::ScreenRecorder(uint32_t fbWidth,
     // string because info->fileName is pointing to data that we don't own.
     D("RecordingInfo "
       "{\n\tfileName=[%s],\n\twidth=[%u],\n\theight=[%u],\n\tvideoBitrate=[%u],"
-      "\n\ttimeLimit=[%u],\n\tdisplay=[%u],\n\tcb=[%p],\n\topaque=[%p]\n}\n",
+      "\n\ttimeLimit=[%u],\n\tcb=[%p],\n\topaque=[%p]\n}\n",
       info->fileName, info->width, info->height, info->videoBitrate,
-      info->timeLimit, info->displayId, info->cb, info->opaque);
+      info->timeLimit, info->cb, info->opaque);
     mFilename = info->fileName;
     mInfo.fileName = mFilename.c_str();
 }
@@ -282,6 +282,7 @@ bool ScreenRecorder::stopRecordingWorker() {
         mFinished = true;
         mCond.signalAndUnlock(&lock);
     }
+
     bool has_frames = ffmpegRecorder->stop();
     ffmpegRecorder.reset();
 
@@ -326,22 +327,21 @@ bool ScreenRecorder::parseRecordingInfo(RecordingInfo& info) {
     return true;
 }
 
-RecorderStates ScreenRecorder::getRecorderState() const {
-    RecorderStates ret = {mRecorderState, mInfo.displayId};
-    return ret;
+RecorderState ScreenRecorder::getRecorderState() const {
+    return mRecorderState;
 }
 }  // namespace
 
 // C compatibility functions
-RecorderStates screen_recorder_state_get(void) {
+RecorderState screen_recorder_state_get(void) {
     auto& globals = *sGlobals;
-    RecorderStates ret = {RECORDER_STOPPED, 0};
 
     AutoLock lock(globals.lock);
     if (globals.recorder) {
-        ret = globals.recorder->getRecorderState();
+        return globals.recorder->getRecorderState();
+    } else {
+        return RECORDER_STOPPED;
     }
-    return ret;
 }
 
 static void screen_recorder_record_session(const char* cmdLineArgs) {
@@ -424,17 +424,11 @@ bool screen_recorder_start(const RecordingInfo* info, bool async) {
     AutoLock lock(globals.lock);
 
     // Check if the display is created. For guest mode, display 0 return true.
-    uint32_t w, h, cb = 0;
+    uint32_t w, h;
     if (globals.multiDisplayAgent->getMultiDisplay(info->displayId, nullptr, nullptr,
                                                    &w, &h, nullptr,
                                                    nullptr, nullptr) == false) {
         return false;
-    }
-    if (info->displayId != 0) {
-        globals.multiDisplayAgent->getDisplayColorBuffer(info->displayId, &cb);
-        if (cb == 0) {
-            return false;
-        }
     }
 
     globals.recorder.reset(new ScreenRecorder(w, h, info, globals.displayAgent));
@@ -447,10 +441,9 @@ bool screen_recorder_stop(bool async) {
     AutoLock lock(globals.lock);
 
     if (globals.recorder) {
-        globals.recorder->stopRecording(async);
-        globals.recorder.reset();
-        return true;
+        return globals.recorder->stopRecording(async);
     }
+
     return false;
 }
 
