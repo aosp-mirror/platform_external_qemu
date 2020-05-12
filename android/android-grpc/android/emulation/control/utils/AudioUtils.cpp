dd1b9e1df5051bf06086e5470182d670691339e7
Erwin Jansen <jansene@google.com>
2020-05-12 22:58:52 +0200

    [gRPC] Add support for streaming audio.
    This adds supports for streaming audio samples from the emulator.
    
    An example that records to a wav file is provided:
    
    python -m sample.recordAudio test.wav
    
    Packet: 1589317929095392 - samplingRate: 44100 channels: Stereo format: AUD_FMT_S16
    Packet: 1589317929142109 - samplingRate: 44100 channels: Stereo format: AUD_FMT_S16
    Packet: 1589317929188708 - samplingRate: 44100 channels: Stereo format: AUD_FMT_S16
    
    Change-Id: I9cd3081076a50cb0aad43d7ea436c7c533885d65
    Bug: b/155253463

---
+++ android/android-grpc/android/emulation/control/utils/AudioUtils.cpp
@@ -0,0 +1,47 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/emulation/control/utils/AudioUtils.h"
+
+#include "emulator_controller.pb.h"  // for AudioFormat, AudioFormat::AUD_FM...
+
+namespace android {
+namespace emulation {
+namespace control {
+
+android::recording::AudioFormat AudioUtils::getSampleFormat(
+        const AudioFormat& fmt) {
+    switch (fmt.format()) {
+        case AudioFormat::AUD_FMT_S16:
+            return android::recording::AudioFormat::AUD_FMT_S16;
+        case AudioFormat::AUD_FMT_U8:
+            return android::recording::AudioFormat::AUD_FMT_U8;
+        default:
+            return android::recording::AudioFormat::AUD_FMT_S16;
+    }
+}
+
+int AudioUtils::getChannels(const AudioFormat& fmt) {
+    switch (fmt.channels()) {
+        case AudioFormat::Mono:
+            return 1;
+        case AudioFormat::Stereo:
+            return 2;
+        default:
+            return 2;
+    }
+}
+
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
---
+++ android/android-grpc/android/emulation/control/utils/AudioUtils.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include "android/recording/Frame.h"  // for AudioFormat
+
+namespace android {
+namespace emulation {
+namespace control {
+class AudioFormat;
+
+// Contains a set of convenience methods to assist with streaming audio.
+class AudioUtils {
+public:
+    // Translates our external format to the internal format.
+    static android::recording::AudioFormat getSampleFormat(const AudioFormat& fmt);
+
+    // Extracts the number of channels from the enum for usage with qemu.
+    static int getChannels(const AudioFormat& fmt);
+};
+
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
