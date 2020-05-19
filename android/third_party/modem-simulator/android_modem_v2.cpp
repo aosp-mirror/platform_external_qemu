4d4fe852b248f2abfc82e5141b6224b06e6b160a
bohu <bohu@google.com>
2020-05-19 14:57:30 -0700

    modem-simulator: add third-party modem lib
    wip
    
    this lib comes from cuttlefish modem-simulator
    this director will implement some hooks to link
    against that simulator library
    
    Change-Id: I63bea953eba2dd5502ddadb4a580f8853243f74b

---
+++ android/third_party/modem-simulator/android_modem_v2.cpp
@@ -0,0 +1,33 @@
+/* Copyright (C) 2020 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#include "android_modem_v2.h"
+#include "android/globals.h"
+#include "modem_main.h"
+
+void amodem_receive_sms_vx( AModem  modem, SmsPDU  sms ) {
+    if (android_modem_version == 1) {
+        amodem_receive_sms(modem, sms);
+    } else if (android_modem_version == 2) {
+         cvd::send_sms_msg(std::string(amodem_sms_to_string(modem, sms)));
+
+    }
+}
+
+int amodem_add_inbound_call_vx(AModem modem, const char* args) {
+    if (android_modem_version == 1) {
+        return amodem_add_inbound_call(modem, args);
+    } else if (android_modem_version == 2) {
+        cvd::receive_inbound_call(std::string(args));
+    }
+    return 0;
+}
---
+++ android/third_party/modem-simulator/android_modem_v2.h
@@ -0,0 +1,25 @@
+/* Copyright (C) 2020 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+
+#pragma once
+
+#include "android/utils/compiler.h"
+#include "android/telephony/modem.h"
+#include "android/telephony/sms.h"
+
+ANDROID_BEGIN_HEADER
+
+extern void amodem_receive_sms_vx( AModem  modem, SmsPDU  sms );
+
+extern int amodem_add_inbound_call_vx( AModem  modem, const char* args);
+
+ANDROID_END_HEADER
