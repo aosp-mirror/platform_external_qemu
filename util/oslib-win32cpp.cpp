680c36d6f0fc31315d0d2532c5ba3eecfe8a3610
bohu <bohu@google.com>
2020-05-19 14:57:30 -0700

    modem-simulator: add third-party modem lib
    The modem simulator comes from cuttlefish, and
    the thrid_party/modem_simulator provides the hook
    to work with emulator.
    
    The cl enables both legacy modem and new modem
    simulator work together. Which one to use is
    decided by guest's feature flag: ModemSimulator:
    if it is on, use new one; otherwise, use the legacy one.
    
    Implementaiton Notes:
    1. modem simulator from cf uses vsoc to communicate with guest
    rild; in case of emulator (goldfish), this is
    replaced by a virtual serial port in the guest backed
    by chardev based on tcp port on host so it can work
    on windows.
    2. modem simulator also uses c++ 17, and all buildbots on emulator
    sides have been upgraded to fully support c++ 17 already.
    3. modem simulator relies on some android libraries
    and also some private libs such as SharedFd, they are
    either ported (e.g., SharedFd) or added from source
    (Json and TinyXml): due to the different interfaces,
    Json and Xml are not reusing the available libs on
    emulator, this might be further simplified by adding
    a translation layer later.
    
    BUG: 155797805
    
    Test:
    launch avd with image built with go/oag/1318141
    the radio should work on linux/mac using modem
    simulator instead of legacy modem.
    
    TODO: TEST on windows
    Problem: windows gpoll has 64 fds limitation and the
    vport appears to trigger that issue
    
    wip cl to use mt gpoll: go/oag/1437489
    
    mt gpoll has some bug, but fixable;
    there are still 20-30% slow down of boot time on windows
    with new mt gpoll when objects > 64;
    trying using the std::async which uses the default
    thread pools on windows
    
    Change-Id: I63bea953eba2dd5502ddadb4a580f8853243f74b

---
+++ util/oslib-win32cpp.cpp
@@ -0,0 +1,164 @@
+// this provide some functions written in cpp for windows
+// such as wait for more than 64 objects
+// work in progress
+// main idea:
+// using std::async to use the thread pool on windows
+// so to avoid creation of threads in g_poll (windows version)
+
+extern "C" {
+#include <windows.h>
+
+#include "qemu/osdep.h"
+}
+
+#include <stdio.h>
+
+#include <future>
+#include <vector>
+
+namespace {
+
+constexpr int num_fds_per_item = 60;
+
+struct WorkItem {
+  std::vector<GPollFD> gfds;
+  HANDLE stopSignal;
+  HANDLE doneSignal;
+  int timeout;
+};
+
+std::vector<WorkItem> divideWorkItems(GPollFD* fds, int nfds) {
+  int nwork = nfds / num_fds_per_item;
+  int remain = nfds % num_fds_per_item;
+  if (remain > 0) ++nwork;
+
+  std::vector<WorkItem> items(nwork);
+  for (int i = 0; i < nfds; ++i) {
+    int index = i / num_fds_per_item;
+    auto& item = items[index];
+    item.gfds.push_back(fds[i]);
+  }
+  return items;
+}
+
+int combineWork(std::vector<WorkItem>& items, GPollFD* gfds, int nfds) {
+  int numReady = 0;
+  for (int i = 0; i < nfds; ++i) {
+    int index = i / num_fds_per_item;
+    auto& item = items[index];
+    gfds[i] = item.gfds[i % num_fds_per_item];
+    if (gfds[i].revents != 0) {
+      ++numReady;
+    }
+  }
+
+  return numReady;
+}
+
+int one_wait_job(WorkItem* pitem) {
+  auto& item = *pitem;
+  constexpr int array_size = 64;
+  HANDLE handles[array_size];
+
+  assert(item.gfds.size() < array_size);
+  int nfds = 0;
+  handles[nfds++] = item.stopSignal;
+  for (auto& gfd : item.gfds) {
+    handles[nfds++] = reinterpret_cast<HANDLE>(gfd.fd);
+    gfd.revents = 0;
+  }
+
+  HANDLE* phandles = handles;
+  int ret = WaitForMultipleObjects(nfds, phandles, FALSE, item.timeout);
+  if (ret == WAIT_FAILED) {
+    SetEvent(item.doneSignal);
+    return -1;
+  }
+  if (ret == WAIT_TIMEOUT) {
+    SetEvent(item.doneSignal);
+    return 0;
+  }
+
+  int numReady = 0;
+  while (ret >= WAIT_OBJECT_0 && ret < WAIT_OBJECT_0 + nfds) {
+    const int offset = phandles - handles;
+    const int indexFired = ret - WAIT_OBJECT_0;
+    const int indexOriginal = indexFired + offset;
+    if (handles[indexOriginal] != item.stopSignal) {
+      auto& gfd = item.gfds[indexOriginal];
+      gfd.revents = gfd.events;
+      ++numReady;
+    }
+
+    // prepare for next check: skip the fired one (1) and the ones before it
+    // (indexFired)
+    const int num_handles_to_ignore = (indexFired + 1);
+    nfds -= num_handles_to_ignore;
+    phandles += num_handles_to_ignore;
+    constexpr int timeout = 0;  // no wait
+
+    ret = WaitForMultipleObjects(nfds, phandles, FALSE, timeout);
+  }
+
+  SetEvent(item.doneSignal);
+  return numReady;
+}
+
+int wait_async(std::vector<WorkItem>& items, int timeout) {
+  auto stopSignal = CreateEventW(NULL, TRUE, FALSE, NULL);
+  std::vector<std::future<int>> results(items.size());
+  std::vector<HANDLE> thread_done_signals(items.size());
+  for (int i = 0; i < items.size(); ++i) {
+    auto handle = CreateEventW(NULL, TRUE, FALSE, NULL);
+    thread_done_signals[i] = handle;
+    items[i].stopSignal = stopSignal;
+    items[i].doneSignal = handle;
+    items[i].timeout = timeout;
+    results[i] = std::async(std::launch::async, one_wait_job, &(items[i]));
+  }
+
+  // wait for any threads to signal done
+  int ret = WaitForMultipleObjects(thread_done_signals.size(),
+                                   thread_done_signals.data(), FALSE,
+                                   items[0].timeout);
+
+  // if there is any thread signaled, stop all the others
+  SetEvent(stopSignal);
+
+  // wait for all to signal
+  ret = WaitForMultipleObjects(thread_done_signals.size(),
+                               thread_done_signals.data(), TRUE, INFINITE);
+
+  // drain the futures
+  for (auto& r : results) {
+    r.get();
+  }
+
+  return 0;
+}
+
+int wait_driver(GPollFD* fds, int nfds, int timeout) {
+  auto items = divideWorkItems(fds, nfds);
+
+  wait_async(items, timeout);
+
+  int numReady = combineWork(items, fds, nfds);
+
+  return numReady;
+}
+
+}  // anonymous namespace
+
+extern "C" {
+// timeout: -1: wait infinite
+// 0: no wait, just check fd status
+// >=1: wait 1ms or more
+int win32_wait_for_objects(GPollFD* fds, int nfds, int timeout) {
+    if (timeout == -1) {
+        timeout = INFINITE;
+    }
+    int ret = wait_driver(fds, nfds, timeout);
+}
+
+}
+
