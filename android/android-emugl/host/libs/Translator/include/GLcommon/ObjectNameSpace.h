0f89624093e228efc06213f4641e54fd58395809
Lingfeng Yang <lfy@google.com>
2020-06-05 16:56:03 -0700

    gles: use HybridComponentManager for object access
    Change-Id: Ib1ed30678763dbd2785dc7fdbe650dfd7732f8c5

--- android/android-emugl/host/libs/Translator/include/GLcommon/ObjectNameSpace.h
+++ android/android-emugl/host/libs/Translator/include/GLcommon/ObjectNameSpace.h
@@ -29,10 +29,12 @@
 #include <unordered_map>
 #include <unordered_set>
 
-typedef std::unordered_map<ObjectLocalName, NamedObjectPtr> NamesMap;
-// typedef android::base::HybridComponentManager<10000, ObjectLocalName, NamedObjectPtr> NamesMap;
+// typedef std::unordered_map<ObjectLocalName, NamedObjectPtr> NamesMap;
+typedef android::base::HybridComponentManager<10000, ObjectLocalName, NamedObjectPtr> NamesMap;
 typedef std::unordered_map<ObjectLocalName, ObjectDataPtr> ObjectDataMap;
-typedef std::unordered_map<unsigned int, ObjectLocalName> GlobalToLocalNamesMap;
+
+// typedef std::unordered_map<unsigned int, ObjectLocalName> GlobalToLocalNamesMap;
+typedef android::base::HybridComponentManager<10000, unsigned int, ObjectLocalName> GlobalToLocalNamesMap;
 
 typedef android::base::HybridComponentManager<10000, ObjectLocalName, bool> BoundAtLeastOnceMap;
 
