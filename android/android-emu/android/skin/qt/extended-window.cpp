45165b0762a7096244ef05d2df21c0aef22e21e8
Erwin Jansen <jansene@google.com>
2020-06-18 11:36:25 -0700

    Add detailed extended controls UI events.
    We now record which extended control was activated. We no longer track
    if the extended control panel was activated, as this can be inferred
    from the following:
    
    A ui event with context EXTENDED_WINDOW_OPEN -> we opened extended
    control and the user selected another tab
    A location_v2 event -> The user interacted with the location UI (default
    selected tab)
    
    EmulatorUiEvent.element_id will contain which pane was activated.
    EmulatorUiEvent.value will contain how often it was invoked.
    
    Bug: b/159333203
    Test:
    ./objs/emulator @L -metrics-to-console
    { kind: EMULATOR_UI_EVENT studio_session_id: "" product_details { product: EMULATOR build: "30.0.19-" version: "30.0.19.0" } emulator_details { system_time: 14060 user_time: 10120 wall_time: 12837 core_version: "2.12.0" } emulator_ui_event { element_id: "PANE_5" type: UNKONWN_EMULATOR_UI_EVENT_TYPE context: EXTENDED_WINDOW_OPEN value: 2 } }
    event time 1592949819846 ms
    { kind: EMULATOR_UI_EVENT studio_session_id: "" product_details { product: EMULATOR build: "30.0.19-" version: "30.0.19.0" } emulator_details { system_time: 14060 user_time: 10120 wall_time: 12837 core_version: "2.12.0" } emulator_ui_event { element_id: "PANE_6" type: UNKONWN_EMULATOR_UI_EVENT_TYPE context: EXTENDED_WINDOW_OPEN value: 3 } }
    event time 1592949819846 ms
    
    Change-Id: I5aac8a2e46a5ae4ea45184aca8d051aba761b37b

--- android/android-emu/android/skin/qt/extended-window.cpp
+++ android/android-emu/android/skin/qt/extended-window.cpp
@@ -278,19 +278,23 @@ ExtendedWindow::~ExtendedWindow() {
 void ExtendedWindow::sendMetricsOnShutDown() {
     mExtendedUi->location_page->sendMetrics();
     mExtendedUi->multiDisplayPage->sendMetrics();
-    if (mExtendedWindowWasShown) {
-        android::metrics::MetricsReporter::get().report(
-                [](android_studio::AndroidStudioEvent* event) {
-                    // Send extended-window open metrics
-                    auto* metrics = event->mutable_emulator_ui_event();
-                    metrics->set_context(android_studio::EmulatorUiEvent::
-                                                 EXTENDED_WINDOW_OPEN);
-                    metrics->set_type(android_studio::EmulatorUiEvent::
-                                              UNKONWN_EMULATOR_UI_EVENT_TYPE);
-                    metrics->set_value(1);
-                    event->set_kind(android_studio::AndroidStudioEvent::
-                                            EMULATOR_UI_EVENT);
-                });
+    if (mPaneInvocationCount.size() > 0) {
+        for (const auto& invocation : mPaneInvocationCount) {
+            android::metrics::MetricsReporter::get().report(
+                    [=](android_studio::AndroidStudioEvent* event) {
+                        auto* metrics = event->mutable_emulator_ui_event();
+                        metrics->set_element_id(
+                                "PANE_" + std::to_string(invocation.first));
+                        metrics->set_context(android_studio::EmulatorUiEvent::
+                                                     EXTENDED_WINDOW_OPEN);
+                        metrics->set_type(
+                                android_studio::EmulatorUiEvent::
+                                        UNKONWN_EMULATOR_UI_EVENT_TYPE);
+                        metrics->set_value(invocation.second);
+                        event->set_kind(android_studio::AndroidStudioEvent::
+                                                EMULATOR_UI_EVENT);
+                    });
+        }
     }
 }
 
@@ -430,9 +434,13 @@ void ExtendedWindow::adjustTabs(ExtendedWindowPane thisIndex) {
     if (it == mPaneButtonMap.end()) {
         return;
     }
+
+    if (mExtendedWindowWasShown && mExtendedUi->stackedWidget->currentIndex() != thisIndex) {
+        mPaneInvocationCount[thisIndex]++;
+    }
     QPushButton* thisButton = it->second;
     thisButton->toggle();
-    thisButton->clearFocus(); // It looks better when not highlighted
+    thisButton->clearFocus();  // It looks better when not highlighted
     mExtendedUi->stackedWidget->setCurrentIndex(static_cast<int>(thisIndex));
 }
 
