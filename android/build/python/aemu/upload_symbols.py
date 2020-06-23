27cb7c15d749ec991cfcff6208cf8503d4ac10de
Erwin Jansen <jansene@google.com>
2020-06-23 11:16:45 -0700

    Make builds scripts compatible with Python3
    This contains a series of fixes and cleanup to make the scripts run
    under python3.
    
    Change-Id: I592dc2f9111223ff0b08ec82c809084d8f2cdeee

--- android/build/python/aemu/upload_symbols.py
+++ android/build/python/aemu/upload_symbols.py
@@ -13,53 +13,63 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import, division, print_function
+
 
 import json
 import os
-import sys
 import time
-import urllib
+import urllib.request, urllib.parse, urllib.error
+
+from absl import app, flags, logging
 
 import requests
-from absl import app, flags, logging
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('environment', 'prod',
-                    'Which symbol server endpoint to use, cemuan be staging or prod')
 flags.DEFINE_string(
-    'api_key', None, 'Which api key to use. By default will load contents of ~/.emulator_symbol_server_key')
-flags.DEFINE_string('symbol_file', None, 'The symbol file you wish to process')
+    "environment",
+    "prod",
+    "Which symbol server endpoint to use, cemuan be staging or prod",
+)
+flags.DEFINE_string(
+    "api_key",
+    None,
+    "Which api key to use. By default will load contents of ~/.emulator_symbol_server_key",
+)
+flags.DEFINE_string("symbol_file", None, "The symbol file you wish to process")
 flags.DEFINE_boolean(
-    "force", False, "Upload symbol file, even though it is already available.")
+    "force", False, "Upload symbol file, even though it is already available."
+)
 
-flags.register_validator('environment', lambda value: value.lower() == 'prod' or value.lower() ==
-                         'staging', message='--environment should be either prod or staging (case insensitive).')
-flags.mark_flag_as_required('symbol_file')
+flags.register_validator(
+    "environment",
+    lambda value: value.lower() == "prod" or value.lower() == "staging",
+    message="--environment should be either prod or staging (case insensitive).",
+)
+flags.mark_flag_as_required("symbol_file")
+
 
 class SymbolFileServer(object):
     """Class to verify and upload symbols to the crash symbol api v1."""
 
     # The api key should work for both staging and production.
-    API_KEY_FILE = os.path.join(os.path.expanduser(
-        '~'), '.emulator_symbol_server_key')
+    API_KEY_FILE = os.path.join(os.path.expanduser("~"), ".emulator_symbol_server_key")
     API_URL = {
-        'prod': 'https://prod-crashsymbolcollector-pa.googleapis.com/v1',
-        'staging': 'https://staging-crashsymbolcollector-pa.googleapis.com/v1'
+        "prod": "https://prod-crashsymbolcollector-pa.googleapis.com/v1",
+        "staging": "https://staging-crashsymbolcollector-pa.googleapis.com/v1",
     }
     STATUS_MSG = {
-        'OK': 'The symbol data was written to symbol storage.',
-        'DUPLICATE_DATA': 'The symbol data was identical to data already in storage, so no changes were made.'
+        "OK": "The symbol data was written to symbol storage.",
+        "DUPLICATE_DATA": "The symbol data was identical to data already in storage, so no changes were made.",
     }
 
     # Old school api, we use this if we have no api key..
     CLASSIC_API_URL = {
-        'staging': 'https://clients2.google.com/cr/staging_symbol',
-        'prod': 'https://clients2.google.com/cr/symbol'
+        "staging": "https://clients2.google.com/cr/staging_symbol",
+        "prod": "https://clients2.google.com/cr/symbol",
     }
-    CLASSIC_PRODUCT = 'AndroidEmulator'
+    CLASSIC_PRODUCT = "AndroidEmulator"
 
     # Timeout for individual web requests.  an exception is raised if the server has not issued a response for timeout
     # seconds (more precisely, if no bytes have been received on the underlying socket for timeout seconds).
@@ -68,47 +78,57 @@ class SymbolFileServer(object):
     # Number of times we will attempt to make the call, including waiting between calls.
     MAX_RETRY = 5
 
-    def __init__(self, environment='prod', api_key=None):
-        endpoint = SymbolFileServer.API_URL if api_key else SymbolFileServer.CLASSIC_API_URL
+    def __init__(self, environment="prod", api_key=None):
+        endpoint = (
+            SymbolFileServer.API_URL if api_key else SymbolFileServer.CLASSIC_API_URL
+        )
         self.api_url = endpoint[environment.lower()]
-        self.api_key = api_key or ''
+        self.api_key = api_key or ""
 
     def use_classic_api(self):
-        '''Returns true if we should use the classic api.'''
+        """Returns true if we should use the classic api."""
         return not self.api_key
 
     def _extract_symbol_info(self, symbol_file):
-        '''Extracts os, archictecture, debug id and debug file.
+        """Extracts os, archictecture, debug id and debug file.
 
         Raises an error if the first line of the file does not contain:
         MODULE {os} {arch} {id} {filename}
-        '''
-        with open(symbol_file, 'r') as f:
+        """
+        with open(symbol_file, "r") as f:
             info = f.readline().split()
 
-        if len(info) != 5 or info[0] != 'MODULE':
-            raise IOError('Corrupt symbol file: %s, %s' % (symbol_file, info))
+        if len(info) != 5 or info[0] != "MODULE":
+            raise IOError("Corrupt symbol file: %s, %s" % (symbol_file, info))
 
         _, os, arch, dbg_id, dbg_file = info
         return os, arch, dbg_id, dbg_file
 
     def _exec_request_with_retry(self, oper, url, **kwargs):
-        '''Makes a web request with default timeout, returning the response.
+        """Makes a web request with default timeout, returning the response.
 
            The request will be tried multiple times, with exponential backoff:
            sleep 1 * (2 ^ ({number of total retries} - 1))
-        '''
-        retries = Retry(total=SymbolFileServer.MAX_RETRY,
-                        backoff_factor=1, status_forcelist=[502, 503, 504])
+        """
+        retries = Retry(
+            total=SymbolFileServer.MAX_RETRY,
+            backoff_factor=1,
+            status_forcelist=[502, 503, 504],
+        )
         with requests.Session() as s:
-            s.mount('https://', HTTPAdapter(max_retries=retries))
-            s.mount('http://', HTTPAdapter(max_retries=retries))
+            s.mount("https://", HTTPAdapter(max_retries=retries))
+            s.mount("http://", HTTPAdapter(max_retries=retries))
 
             return s.request(
-                oper, url, params={'key': self.api_key}, timeout=SymbolFileServer.DEFAULT_TIMEOUT, **kwargs)
+                oper,
+                url,
+                params={"key": self.api_key},
+                timeout=SymbolFileServer.DEFAULT_TIMEOUT,
+                **kwargs
+            )
 
     def _exec_request(self, oper, url, **kwargs):
-        '''Makes a web request with default timeout, returning the json result.
+        """Makes a web request with default timeout, returning the json result.
 
            The api key will be added to the url request as a parameter.
 
@@ -117,15 +137,19 @@ class SymbolFileServer(object):
 
            Note: If you are using verbose logging it is entirely possible that the subsystem will
            write your api key to the logs!
-        '''
+        """
         resp = self._exec_request_with_retry(oper, url, **kwargs)
         if resp.status_code > 399:
             # Make sure we don't leak secret keys by accident.
             if not self.use_classic_api():
-                resp.url = resp.url.replace(
-                    urllib.quote(self.api_key), 'XX-HIDDEN-XX')
-            logging.error('Url: %s, Status: %s, response: "%s", in: %s',
-                          resp.url, resp.status_code, resp.text, resp.elapsed)
+                resp.url = resp.url.replace(urllib.parse.quote(self.api_key), "XX-HIDDEN-XX")
+            logging.error(
+                'Url: %s, Status: %s, response: "%s", in: %s',
+                resp.url,
+                resp.status_code,
+                resp.text,
+                resp.elapsed,
+            )
             resp.raise_for_status()
 
         if resp.content:
@@ -141,30 +165,28 @@ class SymbolFileServer(object):
             return False
 
         _, _, dbg_id, dbg_file = self._extract_symbol_info(symbol_file)
-        url = '{}/symbols/{}/{}:checkStatus'.format(
-            self.api_url, dbg_file, dbg_id)
-        status = self._exec_request('get', url)
-        return status['status'] == 'FOUND'
+        url = "{}/symbols/{}/{}:checkStatus".format(self.api_url, dbg_file, dbg_id)
+        status = self._exec_request("get", url)
+        return status["status"] == "FOUND"
 
     def _upload_with_classic_api(self, symbol_file):
         """Uploads the symbols to old api end point. This is very slow."""
         _, _, dbg_id, dbg_file = self._extract_symbol_info(symbol_file)
 
-        self._exec_request('post',
-                           self.api_url,
-                           data={
-                               'product': SymbolFileServer.CLASSIC_PRODUCT,
-                               'codeIdentifier': dbg_id,
-                               'debugIdentifier': dbg_id,
-                               'codeFile': dbg_file,
-                               'debugFile': dbg_file,
-                           },
-                           files={
-                               'symbolFile': open(symbol_file, 'rb')
-                           }
-                           )
+        self._exec_request(
+            "post",
+            self.api_url,
+            data={
+                "product": SymbolFileServer.CLASSIC_PRODUCT,
+                "codeIdentifier": dbg_id,
+                "debugIdentifier": dbg_id,
+                "codeFile": dbg_file,
+                "debugFile": dbg_file,
+            },
+            files={"symbolFile": open(symbol_file, "rb")},
+        )
         # We will get exceptions when this fails.
-        return 'OK'
+        return "OK"
 
     def upload(self, symbol_file):
         """Makes the symbol_file available on the server, returning the status result."""
@@ -172,29 +194,27 @@ class SymbolFileServer(object):
             return self._upload_with_classic_api(symbol_file)
 
         _, _, dbg_id, dbg_file = self._extract_symbol_info(symbol_file)
-        upload = self._exec_request('post',
-                                    '{}/uploads:create'.format(self.api_url))
-        self._exec_request('put',
-                           upload['uploadUrl'],
-                           data=open(symbol_file, 'rb'), stream=True)
+        upload = self._exec_request("post", "{}/uploads:create".format(self.api_url))
+        self._exec_request(
+            "put", upload["uploadUrl"], data=open(symbol_file, "rb"), stream=True
+        )
 
-        status = self._exec_request('post',
-                                    '{}/uploads/{}:complete'.format(
-                                        self.api_url, upload['uploadKey']),
-                                    data=json.dumps(
-                                        {'symbol_id': {'debug_file': dbg_file, 'debug_id': dbg_id}}))
+        status = self._exec_request(
+            "post",
+            "{}/uploads/{}:complete".format(self.api_url, upload["uploadKey"]),
+            data=json.dumps(
+                {"symbol_id": {"debug_file": dbg_file, "debug_id": dbg_id}}
+            ),
+        )
 
-        return status['result']
+        return status["result"]
 
 
 def main(args):
     # The lower level enging will log individual requests!
-    logging.debug(
-        "-----------------------------------------------------------")
-    logging.debug(
-        "- WARNING!! You are likely going to leak your api key    --")
-    logging.debug(
-        "-----------------------------------------------------------")
+    logging.debug("-----------------------------------------------------------")
+    logging.debug("- WARNING!! You are likely going to leak your api key    --")
+    logging.debug("-----------------------------------------------------------")
 
     api_key = FLAGS.api_key
     if not api_key:
@@ -203,14 +223,22 @@ def main(args):
                 api_key = f.read()
         except IOError as e:
             logging.error(
-                "Unable to read api key due to: %s, reverting to (slow) classic behavior", e)
+                "Unable to read api key due to: %s, reverting to (slow) classic behavior",
+                e,
+            )
 
     server = SymbolFileServer(FLAGS.environment, api_key)
     if FLAGS.force or not server.is_available(FLAGS.symbol_file):
         start = time.time()
         status = server.upload(FLAGS.symbol_file)
-        print("{} after {} seconds".format(SymbolFileServer.STATUS_MSG.get(
-            status, 'Undefined or unknown result {}'.format(status)), time.time() - start))
+        print(
+            "{} after {} seconds".format(
+                SymbolFileServer.STATUS_MSG.get(
+                    status, "Undefined or unknown result {}".format(status)
+                ),
+                time.time() - start,
+            )
+        )
     else:
         print("Symbol already available, skipping upload.")
 
@@ -219,5 +247,5 @@ def launch():
     app.run(main)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     launch()
