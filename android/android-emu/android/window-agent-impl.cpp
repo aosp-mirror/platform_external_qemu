da220455b42ed22d51c308b1def596707e61cbe4
Emilian Mihalache <emmihala@microsoft.com>
2020-07-06 22:34:05 +0300

    Implement toolbar posture selection
    Change-Id: Ib7c8b2f2864fe7d02cb4e3b8e1fd8c264b15adad

--- android/android-emu/android/window-agent-impl.cpp
+++ android/android-emu/android/window-agent-impl.cpp
@@ -116,6 +116,15 @@ static const QAndroidEmulatorWindowAgent sQAndroidEmulatorWindowAgent = {
                     }
                     return false;
                 },
+        .updateFoldablePostureIndicator =
+                [] {
+                    if (const auto win = EmulatorQtWindow::getInstance()) {
+                        QtUICommand cmd = QtUICommand::UPDATE_FOLDABLE_POSTURE_INDICATOR;
+                        win->runOnUiThread([win, cmd]() {
+                            win->toolWindow()->handleUICommand(cmd);
+                        });
+                    }
+                },
         .setUIDisplayRegion =
                 [](int x, int y, int w, int h) {
                     if (const auto win = EmulatorQtWindow::getInstance()) {
