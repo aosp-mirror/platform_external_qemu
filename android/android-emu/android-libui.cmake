366e25e5e81b8addc4a43af930ecab772c61df20
Erwin Jansen <jansene@google.com>
2020-07-20 12:06:57 -0700

    Separate windowds launcher from libui library
    Win32 defines two entry points for applications:
    
    - WinMain:  Used for GUI based applications.
    - main:     Used by console based applications.
    
    Qt under msvc will call main from it's winmain method to actually launch
    the application. With the main function declated in emulator-libui.
    
    This means that libui defines a main method, which can cause linker
    collision when linking against another library (gtest/gmock) that
    defines a main method.
    
    It seems that under certain cases this worked due to how cmake decided
    to link the library (using just object files). However this breaks an
    upcoming refactor where we are introducing injection functions.
    
    We now move the main fucntion to a seperate library that gets linked
    into qemu-system-xxx to proide an entry point for Qt, so we no longer
    rely on the "cmake" lucky break of how it linked certain libraries.
    
    Bug: b/161717806
    Test: emulator @P launches successfully.
    Change-Id: Ie25781639f139df5acac54f5d9f6507cb10693a8

--- android/android-emu/android-libui.cmake
+++ android/android-emu/android-libui.cmake
@@ -488,3 +488,15 @@ android_target_compile_options(
 target_link_libraries(
   emulator-libui-headless PRIVATE android-emu emulator-libyuv FFMPEG::FFMPEG
                                   zlib android-hw-config)
+
+if (WINDOWS_MSVC_X86_64) 
+  # Qt in windows will call main from win-main v.s. calling qt_main.
+  # we have to make a separate launch library to make sure that we 
+  # do not end up with duplicate main symbols when linking emulator-libui
+  # (it used to work do to a cmake linker quirk).  
+  android_add_library(
+    TARGET emulator-winqt-launcher
+    LICENSE Apache-2.0
+    SRC # cmake-format: sortable
+    android/skin/qt/windows-qt-launcher.cpp)
+endif()
