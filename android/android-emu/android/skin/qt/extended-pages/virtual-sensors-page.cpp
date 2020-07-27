94fd13b70e56858a8d8822a51dd1b7f237acdec9
Huan Song <huans@google.com>
2020-07-27 16:41:20 -0700

    Fix hinge sensor precision
    Bug: 162269760
    
    Test: run foldable avd, check the 3d folding rendering
    Change-Id: I35571435630a3b1948afbede5bb0576bf80f97f4

--- android/android-emu/android/skin/qt/extended-pages/virtual-sensors-page.cpp
+++ android/android-emu/android/skin/qt/extended-pages/virtual-sensors-page.cpp
@@ -168,21 +168,18 @@ void VirtualSensorsPage::setupHingeSensorUI() {
                 mUi->hinge2Slider->setRange(
                         foldableState.config.hingeParams[2].minDegrees,
                         foldableState.config.hingeParams[2].maxDegrees, false);
-                mUi->hinge2Slider->setSteps(200);
                 mUi->hinge2Label->setHidden(false);
                 mUi->hinge2Slider->setHidden(false);
             case 2:
                 mUi->hinge1Slider->setRange(
                         foldableState.config.hingeParams[1].minDegrees,
                         foldableState.config.hingeParams[1].maxDegrees, false);
-                mUi->hinge1Slider->setSteps(200);
                 mUi->hinge1Label->setHidden(false);
                 mUi->hinge1Slider->setHidden(false);
             case 1:
                 mUi->hinge0Slider->setRange(
                         foldableState.config.hingeParams[0].minDegrees,
                         foldableState.config.hingeParams[0].maxDegrees, false);
-                mUi->hinge0Slider->setSteps(200);
                 mUi->hinge0Label->setHidden(false);
                 mUi->hinge0Slider->setHidden(false);
             default:;
