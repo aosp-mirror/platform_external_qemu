04ccd29dcce4600e56dabeba58eebb3472d6e1db
Kaiyi Li <kaiyili@google.com>
2020-07-28 04:11:39 -0700

    WIP: gfxstream init unittest
    This test covers the basic usage of launching gfxstream.
    
    Includes a series of fixes to run the test properly on windows:
    
    - Forward the complete path properly in ctest.
    - Fix nullpointer exception on string comparison
    - Always use swiftshader.
    
    Change-Id: Ifa3d6581b54c8d026c76540576981532ec8c6f07

---
+++ android/android-emugl/combined/GfxStreamBackend.h
@@ -0,0 +1,31 @@
+extern "C" {
+#include "hw/misc/goldfish_pipe.h"
+#include "hw/virtio/virtio-goldfish-pipe.h"
+}  // extern "C"
+
+enum BackendFlags {
+    GFXSTREAM_BACKEND_FLAGS_NO_VK_BIT = 1 << 0,
+    GFXSTREAM_BACKEND_FLAGS_EGL2EGL_BIT = 1 << 1,
+};
+
+// based on VIRGL_RENDERER_USE* and friends
+enum RendererFlags {
+    GFXSTREAM_RENDERER_FLAGS_USE_EGL_BIT = 1 << 0,
+    GFXSTREAM_RENDERER_FLAGS_THREAD_SYNC = 1 << 1,
+    GFXSTREAM_RENDERER_FLAGS_USE_GLX_BIT = 1 << 2,
+    GFXSTREAM_RENDERER_FLAGS_USE_SURFACELESS_BIT = 1 << 3,
+    GFXSTREAM_RENDERER_FLAGS_USE_GLES_BIT = 1 << 4,
+    GFXSTREAM_RENDERER_FLAGS_NO_VK_BIT = 1 << 5, // for disabling vk
+
+    GFXSTREAM_RENDERER_FLAGS_NO_SYNCFD_BIT = 1 << 20, // for disabling syncfd
+};
+
+extern "C" VG_EXPORT void gfxstream_backend_init(
+    uint32_t display_width,
+    uint32_t display_height,
+    uint32_t display_type,
+    void* renderer_cookie,
+    int renderer_flags,
+    struct virgl_renderer_callbacks* virglrenderer_callbacks);
+
+extern "C" VG_EXPORT void gfxstream_backend_teardown(void);
