8c715964ce7140f3b90e0a062bbcc7527d59e2c9
bohu <bohu@google.com>
2020-08-07 15:43:42 -0700

    modem: enable setting forbidden plmns
    Our current implementation is buggy and it only
    returns empty forbidden plmns;
    
    Fixed it by setting the forbidden plmn length to 15 (0xf) bytes
    (previously we set to 64bytes (0x40) but returns fewer, making
    guest to use empty forbidden plmns list)
    
    BUG: 162744279
    
    Test:
    android.carrierapi.cts.CarrierApiTest#testVoiceMailNumber
    android.telephony.cts.TelephonyManagerTest#testGetForbiddenPlmns
    android.telephony.cts.TelephonyManagerTest#testSetForbiddenPlmns*
    
    Also fix an off by one bug in voicemail setting code;
    
    Change-Id: I1f630a856ad766c24df8b5abdb5d9f0f1951e567

--- android/android-emu/android/telephony/sim_card.c
+++ android/android-emu/android/telephony/sim_card.c
@@ -474,15 +474,29 @@ asimcard_io( ASimCard  sim, const char*  cmd )
            the 15's f is simply copied from my own phone; the value does not seem
            to matter that much; but the length has to be a multiple of 5's
          */
-        { "+CRSM=192,28539,0,0,15", "+CRSM: 144,0,000000406fc7040011a0aa01000000" },
-        { "+CRSM=176,28539,0,0,64", "+CRSM: 144,0,ffffffffffffffffffffffff" },
+        { "+CRSM=192,28539,0,0,15", "+CRSM: 144,0,0000000f6fc7040011a0aa01000000" },
+        { "+CRSM=176,28539,0,0,15", "+CRSM: 144,0,21635487F910FFFFFFFFFFFFFFFFFF" },
 
         { NULL, NULL }
     };
 
     assert( memcmp( cmd, "+CRSM=", 6 ) == 0 );
 
-#define SET_VOICE_MAIL_NUMBER_COMMAND "+CRSM=220,28615,1,4,32"
+#define SET_FPLMN_COMMAND "+CRSM=214,28539,0,0,15,"
+    static char s_fplmn[256] = "21635487F910FFFFFFFFFFFFFFFFFF";
+    if (0 == strncmp(cmd, SET_FPLMN_COMMAND, strlen(SET_FPLMN_COMMAND))) {
+        snprintf(s_fplmn, sizeof(s_fplmn), "%s", cmd + strlen(SET_FPLMN_COMMAND));
+        return "+CRSM: 144,0";
+    }
+
+#define GET_FPLMN_COMMAND "+CRSM=176,28539,0,0,15"
+    if (0 == strncmp(cmd, GET_FPLMN_COMMAND, strlen(GET_FPLMN_COMMAND))) {
+        snprintf( sim->out_buff, sizeof(sim->out_buff), "+CRSM: 144,0,%s", s_fplmn);
+        return sim->out_buff;
+    }
+
+
+#define SET_VOICE_MAIL_NUMBER_COMMAND "+CRSM=220,28615,1,4,32,"
 
     static char s_voice_mail_number[256] = "566f6963656d61696cffffffffffffffffff07915155125740f9ffffffffffff";
     if (0 == strncmp(cmd, SET_VOICE_MAIL_NUMBER_COMMAND, strlen(SET_VOICE_MAIL_NUMBER_COMMAND))) {
