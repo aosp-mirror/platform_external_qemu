26e277fa22e5bff284dad6a1d58cc907e28efdae
Lingfeng Yang <lfy@google.com>
2020-08-20 12:33:24 -0700

    Host/guest tracing prototype Bug: 168843698
    Change-Id: Ie071c408f9e9bf677584fb9f080da768f60a37ea

--- android/android-emu/android/base/Tracing.cpp
+++ android/android-emu/android/base/Tracing.cpp
@@ -14,228 +14,36 @@
 // limitations under the License.
 #include "android/base/Tracing.h"
 
-#include "android/base/system/System.h"
-#include "android/base/memory/LazyInstance.h"
-#include "android/base/threads/Thread.h"
-#include "android/base/threads/ThreadStore.h"
+#include "perfetto-tracing-only.h"
 
 #include <string>
 #include <vector>
 
+#include <fcntl.h>
+
 namespace android {
 namespace base {
 
-static constexpr size_t kDefaultIndentLevels = 8;
-static constexpr char kIndentString[] = "  ";
+const bool* tracingDisabledPtr = nullptr;
 
-class IndentPrinter {
-public:
-    IndentPrinter() {
-        ensureIndentLevel(kDefaultIndentLevels);
-    }
-
-    void begin(const char* name) {
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us;
-        float ms = ((float)us) / 1000.0f;
-        mNames[mIndentLevel] = name;
-
-        printf("tid:0x%016llx:%s%s @ %f ms {\n",
-               (unsigned long long)getCurrentThreadId(),
-               mIndents[mIndentLevel].c_str(),
-               name,
-               ms);
-
-        ++mIndentLevel;
-
-        ensureIndentLevel(mIndentLevel);
-
-    }
-
-    void end() {
-        --mIndentLevel;
-
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us - mTimes[mIndentLevel];
-        float ms = (float)mTimes[mIndentLevel] / 1000.0f;
-
-        printf("tid:0x%016llx:%s} %s for %f ms\n",
-               (unsigned long long)getCurrentThreadId(),
-               mIndents[mIndentLevel].c_str(),
-               mNames[mIndentLevel].c_str(),
-               ms);
-    }
-
-private:
-    void ensureIndentLevel(size_t level) {
-        if (mIndents.size() < level) {
-            size_t prev_size = mIndents.size();
-            mIndents.resize(level * 2);
-            for (size_t i = prev_size;
-                 i < mIndents.size(); ++i) {
-                std::string s;
-                for (size_t j = 0; j < i; ++j) {
-                    s += kIndentString;
-                }
-                mIndents[i] = s;
-            }
-        }
-
-        if (mTimes.size() < level) {
-            mTimes.resize(level * 2, 0.0f);
-        }
-
-        if (mNames.size() < level) {
-            mNames.resize(level * 2);
-        }
-    }
-
-    size_t mIndentLevel = 0;
-    std::vector<std::string> mIndents;
-    std::vector<System::WallDuration> mTimes;
-    std::vector<std::string> mNames;
-};
-
-class ThresholdPrinter {
-public:
-    ThresholdPrinter() {
-        ensureIndentLevel(kDefaultIndentLevels);
-    }
-
-    void begin(const char* name, uint64_t thresholdUs) {
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us;
-        mThresholds[mIndentLevel] = thresholdUs;
-        float ms = ((float)us) / 1000.0f;
-        mNames[mIndentLevel] = name;
-
-        ++mIndentLevel;
-
-        ensureIndentLevel(mIndentLevel);
-    }
-
-    void end() {
-        --mIndentLevel;
-
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us - mTimes[mIndentLevel];
-
-        if (mTimes[mIndentLevel] > mThresholds[mIndentLevel]) {
-            float thresholdMs = mThresholds[mIndentLevel] / 1000.0f;
-            float ms = mTimes[mIndentLevel] / 1000.0f;
-            printf("tid:0x%016llx:%s [%s] over threshold %f ms: %f ms\n",
-                   (unsigned long long)getCurrentThreadId(),
-                   mIndents[mIndentLevel].c_str(),
-                   mNames[mIndentLevel].c_str(),
-                   thresholdMs, ms);
-        }
-    }
-
-private:
-    void ensureIndentLevel(size_t level) {
-        if (mIndents.size() < level) {
-            size_t prev_size = mIndents.size();
-            mIndents.resize(level * 2);
-            for (size_t i = prev_size;
-                 i < mIndents.size(); ++i) {
-                std::string s;
-                for (size_t j = 0; j < i; ++j) {
-                    s += kIndentString;
-                }
-                mIndents[i] = s;
-            }
-        }
-
-        if (mTimes.size() < level) {
-            mTimes.resize(level * 2, 0);
-        }
-
-        if (mThresholds.size() < level) {
-            mThresholds.resize(level * 2, 0);
-        }
-
-        if (mNames.size() < level) {
-            mNames.resize(level * 2);
-        }
-    }
-
-    size_t mIndentLevel = 0;
-    std::vector<std::string> mIndents;
-    std::vector<System::WallDuration> mTimes;
-    std::vector<System::WallDuration> mThresholds;
-    std::vector<std::string> mNames;
-};
-
-typedef ThreadStore<IndentPrinter> IndentPrinterStore;
-typedef ThreadStore<ThresholdPrinter> ThresholdPrinterStore;
-
-static LazyInstance<IndentPrinterStore> sIndentPrinterStore =
-    LAZY_INSTANCE_INIT;
-static LazyInstance<ThresholdPrinterStore> sThresholdPrinterStore =
-    LAZY_INSTANCE_INIT;
-
-static IndentPrinter* indentPrinter_getForThread() {
-    auto state = sIndentPrinterStore.ptr();
-
-    auto printer = state->get();
-    if (!printer) {
-        printer = new IndentPrinter;
-        state->set(printer);
-    }
-
-    return state->get();
+void initializeTracing() {
+    virtualdeviceperfetto::initialize(&tracingDisabledPtr);
 }
 
-static void indentTrace_begin(const char* name) {
-    indentPrinter_getForThread()->begin(name);
-}
-
-static void indentTrace_end() {
-    indentPrinter_getForThread()->end();
-}
-
-static ThresholdPrinter* thresholdPrinter_getForThread() {
-    auto state = sThresholdPrinterStore.ptr();
-
-    auto printer = state->get();
-    if (!printer) {
-        printer = new ThresholdPrinter;
-        state->set(printer);
+void enableTracing() {
+    if (virtualdeviceperfetto::queryTraceConfig().tracingDisabled) {
+        virtualdeviceperfetto::enableTracing();
     }
-
-    return state->get();
 }
 
-static void thresholdTrace_begin(const char* name, uint64_t thresholdUs) {
-    thresholdPrinter_getForThread()->begin(name, thresholdUs);
-}
-
-static void thresholdTrace_end() {
-    thresholdPrinter_getForThread()->end();
-}
-
-class TraceConfig {
-public:
-    TraceConfig() {
-        mEnabled =
-            System::getEnvironmentVariable(
-                "ANDROID_EMU_TRACING") == "1";
+void disableTracing() {
+    if (!virtualdeviceperfetto::queryTraceConfig().tracingDisabled) {
+        virtualdeviceperfetto::disableTracing();
     }
-
-    bool enabled() const { return mEnabled; }
-
-private:
-    bool mEnabled = false;
-};
-
-static TraceConfig sTraceConfig;
+}
 
 bool shouldEnableTracing() {
-    return sTraceConfig.enabled();
+    return !(virtualdeviceperfetto::queryTraceConfig().tracingDisabled);
 }
 
 #ifdef __cplusplus
@@ -246,59 +54,34 @@ bool shouldEnableTracing() {
 #   define CC_UNLIKELY( exp )  (__builtin_expect( !!(exp), 0 ))
 #endif
 
-void ScopedTrace::beginTraceImpl(const char* name) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_begin(name);
-    }
+__attribute__((always_inline)) void beginTrace(const char* name) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    virtualdeviceperfetto::beginTrace(name);
 }
 
-void ScopedTrace::endTraceImpl(const char*) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_end();
-    }
+__attribute__((always_inline)) void endTrace() {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    virtualdeviceperfetto::endTrace();
 }
 
-void beginTrace(const char* name) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_begin(name);
-    }
+__attribute__((always_inline)) void traceCounter(const char* name, int64_t value) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    virtualdeviceperfetto::traceCounter(name, value);
 }
 
-void endTrace() {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_end();
-    }
+ScopedTrace::ScopedTrace(const char* name) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    virtualdeviceperfetto::beginTrace(name);
 }
 
-void ScopedThresholdTrace::beginTraceImpl(const char* name, uint64_t thresholdUs) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_begin(name, thresholdUs);
-    }
+ScopedTrace::~ScopedTrace() {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    virtualdeviceperfetto::endTrace();
 }
 
-void ScopedThresholdTrace::endTraceImpl(const char*) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_end();
-    }
+void setGuestTime(uint64_t t) {
+    virtualdeviceperfetto::setGuestTime(t);
 }
-
-void beginThresholdTrace(const char* name, uint64_t thresholdUs) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_begin(name, thresholdUs);
-    }
-}
-
-void endThresholdTrace() {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_end();
-    }
-}
-
-// TODO(b/168843698): Connect this up to an underlying tracing library.
-void enableTracing() { }
-void disableTracing() { }
-void setGuestTime(uint64_t guestTime) { (void)guestTime; }
-void traceCounter(const char* tag, int64_t value) { (void)tag; (void)value; }
 
 } // namespace base
 } // namespace android
