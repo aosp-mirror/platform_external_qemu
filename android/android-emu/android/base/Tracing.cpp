697896636120ad26f7622e9ae2434ebf2b564d05
Lingfeng Yang <lfy@google.com>
2020-08-20 12:33:24 -0700

    Host/guest tracing prototype (all tracing-specific code, to be cleaned up)
    Bug: 168843698
    
    Change-Id: Ie071c408f9e9bf677584fb9f080da768f60a37ea

--- android/android-emu/android/base/Tracing.cpp
+++ android/android-emu/android/base/Tracing.cpp
@@ -14,228 +14,115 @@
 // limitations under the License.
 #include "android/base/Tracing.h"
 
-#include "android/base/system/System.h"
+#include "android/base/containers/BufferQueue.h"
+#include "android/base/containers/SmallVector.h"
+#include "android/base/synchronization/Lock.h"
 #include "android/base/memory/LazyInstance.h"
+#include "android/base/system/System.h"
+#include "android/base/threads/FunctorThread.h"
 #include "android/base/threads/Thread.h"
 #include "android/base/threads/ThreadStore.h"
+
+#include "perfetto-tracing-only.h"
 
 #include <string>
 #include <vector>
 
+#include <fcntl.h>
+
 namespace android {
 namespace base {
 
-static constexpr size_t kDefaultIndentLevels = 8;
-static constexpr char kIndentString[] = "  ";
+using TraceSinkBuffer = SmallFixedVector<uint8_t, 4096>;
 
-class IndentPrinter {
+class MultithreadedTraceSink {
 public:
-    IndentPrinter() {
-        ensureIndentLevel(kDefaultIndentLevels);
+    MultithreadedTraceSink(const char* filename) :
+        mFilename(filename),
+        mFileHandle(fopen(filename, "wb")),
+        mToSink(1024, mLock), mThread([this] { threadFunction(); }) {
+        mThread.start();
     }
 
-    void begin(const char* name) {
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us;
-        float ms = ((float)us) / 1000.0f;
-        mNames[mIndentLevel] = name;
-
-        printf("tid:0x%016llx:%s%s @ %f ms {\n",
-               (unsigned long long)getCurrentThreadId(),
-               mIndents[mIndentLevel].c_str(),
-               name,
-               ms);
-
-        ++mIndentLevel;
-
-        ensureIndentLevel(mIndentLevel);
-
+    ~MultithreadedTraceSink() {
+        mExiting = true;
+        mThread.wait();
+        fclose(mFileHandle);
     }
 
-    void end() {
-        --mIndentLevel;
-
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us - mTimes[mIndentLevel];
-        float ms = (float)mTimes[mIndentLevel] / 1000.0f;
-
-        printf("tid:0x%016llx:%s} %s for %f ms\n",
-               (unsigned long long)getCurrentThreadId(),
-               mIndents[mIndentLevel].c_str(),
-               mNames[mIndentLevel].c_str(),
-               ms);
+    void push(uint8_t* src, size_t bytes) {
+        TraceSinkBuffer res(src, src + bytes);
+        AutoLock lock(mLock);
+        mToSink.pushLocked(std::move(res));
     }
 
 private:
-    void ensureIndentLevel(size_t level) {
-        if (mIndents.size() < level) {
-            size_t prev_size = mIndents.size();
-            mIndents.resize(level * 2);
-            for (size_t i = prev_size;
-                 i < mIndents.size(); ++i) {
-                std::string s;
-                for (size_t j = 0; j < i; ++j) {
-                    s += kIndentString;
-                }
-                mIndents[i] = s;
+    void threadFunction() {
+        const uint64_t kBlockIntervalUs = 1000000;
+        TraceSinkBuffer currentBufferToWrite;
+
+        while (!mExiting || mToSink.canPopLocked()) {
+            auto currTime = System::get()->getUnixTimeUs();
+            AutoLock lock(mLock);
+            auto result = mToSink.popLockedBefore(&currentBufferToWrite, currTime + kBlockIntervalUs);
+            if (result == BufferQueueResult::Ok) {
+                fwrite(currentBufferToWrite.data(), currentBufferToWrite.size(), 1, mFileHandle);
             }
         }
-
-        if (mTimes.size() < level) {
-            mTimes.resize(level * 2, 0.0f);
-        }
-
-        if (mNames.size() < level) {
-            mNames.resize(level * 2);
-        }
     }
 
-    size_t mIndentLevel = 0;
-    std::vector<std::string> mIndents;
-    std::vector<System::WallDuration> mTimes;
-    std::vector<std::string> mNames;
+    mutable Lock mLock;
+    const char* mFilename;
+    FILE* mFileHandle;
+    BufferQueue<TraceSinkBuffer> mToSink;
+    bool mExiting = false;
+    FunctorThread mThread;
 };
 
-class ThresholdPrinter {
-public:
-    ThresholdPrinter() {
-        ensureIndentLevel(kDefaultIndentLevels);
-    }
+static MultithreadedTraceSink* sTraceSink = nullptr;
 
-    void begin(const char* name, uint64_t thresholdUs) {
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us;
-        mThresholds[mIndentLevel] = thresholdUs;
-        float ms = ((float)us) / 1000.0f;
-        mNames[mIndentLevel] = name;
-
-        ++mIndentLevel;
-
-        ensureIndentLevel(mIndentLevel);
-    }
-
-    void end() {
-        --mIndentLevel;
-
-        auto us = System::getSystemTimeUs();
-
-        mTimes[mIndentLevel] = us - mTimes[mIndentLevel];
-
-        if (mTimes[mIndentLevel] > mThresholds[mIndentLevel]) {
-            float thresholdMs = mThresholds[mIndentLevel] / 1000.0f;
-            float ms = mTimes[mIndentLevel] / 1000.0f;
-            printf("tid:0x%016llx:%s [%s] over threshold %f ms: %f ms\n",
-                   (unsigned long long)getCurrentThreadId(),
-                   mIndents[mIndentLevel].c_str(),
-                   mNames[mIndentLevel].c_str(),
-                   thresholdMs, ms);
-        }
-    }
-
-private:
-    void ensureIndentLevel(size_t level) {
-        if (mIndents.size() < level) {
-            size_t prev_size = mIndents.size();
-            mIndents.resize(level * 2);
-            for (size_t i = prev_size;
-                 i < mIndents.size(); ++i) {
-                std::string s;
-                for (size_t j = 0; j < i; ++j) {
-                    s += kIndentString;
-                }
-                mIndents[i] = s;
-            }
-        }
-
-        if (mTimes.size() < level) {
-            mTimes.resize(level * 2, 0);
-        }
-
-        if (mThresholds.size() < level) {
-            mThresholds.resize(level * 2, 0);
-        }
-
-        if (mNames.size() < level) {
-            mNames.resize(level * 2);
-        }
-    }
-
-    size_t mIndentLevel = 0;
-    std::vector<std::string> mIndents;
-    std::vector<System::WallDuration> mTimes;
-    std::vector<System::WallDuration> mThresholds;
-    std::vector<std::string> mNames;
+static perfetto::TraceWriterOperations sAndroidEmuTraceWriterOperations = {
+    .openSink = []() {
+        sTraceSink = new MultithreadedTraceSink(perfetto::queryTraceConfig().filename);
+    },
+    .closeSink = []() {
+        delete sTraceSink;
+    },
+    .writeToSink = [](uint8_t* src, size_t bytes) {
+        sTraceSink->push(src, bytes);
+    },
 };
 
-typedef ThreadStore<IndentPrinter> IndentPrinterStore;
-typedef ThreadStore<ThresholdPrinter> ThresholdPrinterStore;
+void setGuestTime(uint64_t t) {
+    perfetto::setTraceConfig([t](perfetto::TraceConfig& config) {
+        config.guestStartTime = t;
+        config.useGuestStartTime = true;
+    });
+}
 
-static LazyInstance<IndentPrinterStore> sIndentPrinterStore =
-    LAZY_INSTANCE_INIT;
-static LazyInstance<ThresholdPrinterStore> sThresholdPrinterStore =
-    LAZY_INSTANCE_INIT;
+const bool* tracingDisabledPtr = nullptr;
 
-static IndentPrinter* indentPrinter_getForThread() {
-    auto state = sIndentPrinterStore.ptr();
+void initializeTracing() {
+    perfetto::initialize(&tracingDisabledPtr);
+}
 
-    auto printer = state->get();
-    if (!printer) {
-        printer = new IndentPrinter;
-        state->set(printer);
+void enableTracing() {
+    if (perfetto::queryTraceConfig().tracingDisabled) {
+        perfetto::setTraceConfig([](perfetto::TraceConfig& config) {
+            config.ops = sAndroidEmuTraceWriterOperations;
+        });
+        perfetto::enableTracing();
     }
-
-    return state->get();
 }
 
-static void indentTrace_begin(const char* name) {
-    indentPrinter_getForThread()->begin(name);
-}
-
-static void indentTrace_end() {
-    indentPrinter_getForThread()->end();
-}
-
-static ThresholdPrinter* thresholdPrinter_getForThread() {
-    auto state = sThresholdPrinterStore.ptr();
-
-    auto printer = state->get();
-    if (!printer) {
-        printer = new ThresholdPrinter;
-        state->set(printer);
+void disableTracing() {
+    if (!perfetto::queryTraceConfig().tracingDisabled) {
+        perfetto::disableTracing();
     }
-
-    return state->get();
 }
-
-static void thresholdTrace_begin(const char* name, uint64_t thresholdUs) {
-    thresholdPrinter_getForThread()->begin(name, thresholdUs);
-}
-
-static void thresholdTrace_end() {
-    thresholdPrinter_getForThread()->end();
-}
-
-class TraceConfig {
-public:
-    TraceConfig() {
-        mEnabled =
-            System::getEnvironmentVariable(
-                "ANDROID_EMU_TRACING") == "1";
-    }
-
-    bool enabled() const { return mEnabled; }
-
-private:
-    bool mEnabled = false;
-};
-
-static TraceConfig sTraceConfig;
 
 bool shouldEnableTracing() {
-    return sTraceConfig.enabled();
+    return !(perfetto::queryTraceConfig().tracingDisabled);
 }
 
 #ifdef __cplusplus
@@ -246,59 +133,30 @@ bool shouldEnableTracing() {
 #   define CC_UNLIKELY( exp )  (__builtin_expect( !!(exp), 0 ))
 #endif
 
-void ScopedTrace::beginTraceImpl(const char* name) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_begin(name);
-    }
+__attribute__((always_inline)) void beginTrace(const char* name) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    perfetto::beginTrace(name);
 }
 
-void ScopedTrace::endTraceImpl(const char*) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_end();
-    }
+__attribute__((always_inline)) void endTrace() {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    perfetto::endTrace();
 }
 
-void beginTrace(const char* name) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_begin(name);
-    }
+__attribute__((always_inline)) void traceCounter(const char* name, int64_t value) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    perfetto::traceCounter(name, value);
 }
 
-void endTrace() {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        indentTrace_end();
-    }
+ScopedTrace::ScopedTrace(const char* name) {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    perfetto::beginTrace(name);
 }
 
-void ScopedThresholdTrace::beginTraceImpl(const char* name, uint64_t thresholdUs) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_begin(name, thresholdUs);
-    }
+ScopedTrace::~ScopedTrace() {
+    if (CC_LIKELY(*tracingDisabledPtr)) return;
+    perfetto::endTrace();
 }
-
-void ScopedThresholdTrace::endTraceImpl(const char*) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_end();
-    }
-}
-
-void beginThresholdTrace(const char* name, uint64_t thresholdUs) {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_begin(name, thresholdUs);
-    }
-}
-
-void endThresholdTrace() {
-    if (CC_UNLIKELY(shouldEnableTracing())) {
-        thresholdTrace_end();
-    }
-}
-
-// TODO(b/168843698): Connect this up to an underlying tracing library.
-void enableTracing() { }
-void disableTracing() { }
-void setGuestTime(uint64_t guestTime) { (void)guestTime; }
-void traceCounter(const char* tag, int64_t value) { (void)tag; (void)value; }
 
 } // namespace base
 } // namespace android
---
+++ android/third_party/perfetto-tracing-only-stub/perfetto-tracing-only.h
@@ -0,0 +1,50 @@
+// Copyright 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <functional>
+#include <cstdint>
+
+namespace perfetto {
+
+struct TraceWriterOperations {
+    std::function<void()> openSink;
+    std::function<void()> closeSink;
+    std::function<void(uint8_t*, size_t)> writeToSink;
+};
+
+struct TraceConfig {
+    bool initialized;
+    bool tracingDisabled;
+    uint32_t packetsWritten;
+    bool sequenceIdWritten;
+    uint32_t currentInterningId;
+    uint32_t currentThreadId;
+    const char* filename;
+    uint64_t guestStartTime;
+    uint64_t hostStartTime;
+    bool useGuestStartTime;
+    TraceWriterOperations ops;
+};
+
+void setTraceConfig(std::function<void(TraceConfig&)>);
+TraceConfig queryTraceConfig();
+void enableTracing();
+void disableTracing();
+
+void beginTrace(const char* eventName);
+void endTrace();
+void traceCounter(const char* name, int64_t value);
+
+void basic_test();
+
+} // namespace perfetto
