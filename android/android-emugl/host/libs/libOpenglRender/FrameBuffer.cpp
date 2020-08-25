c1223aab96d75e18a4805494012b3cc65ce1c51a
Yahan Zhou <yahan@google.com>
2020-08-25 13:56:43 -0700

    Add vulkan support to import YUV Android hardware buffer
    This commit is for the CTS test testMediaImportAndRendering. It does the
    following:
    (1) Allow Vulkan to import color buffer in YUV format;
    (2) Cache raw YUV data when copying to YUVConverter;
    (3) Allow reading raw YUV data from color buffer.
    
    With such, the test passes for both -gpu host and -gpu
    swiftshader_indirect.
    
    Bug: 134773396
    Test: android.graphics.cts.MediaVulkanGpuTest#testMediaImportAndRendering
    
    Change-Id: I328677a16b50ec85ce1a9f7dc786e807c0ee68d9

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -1943,7 +1943,8 @@ bool FrameBuffer::readColorBufferContents(
 }
 
 bool FrameBuffer::getColorBufferInfo(
-    HandleType p_colorbuffer, int* width, int* height, GLint* internalformat) {
+    HandleType p_colorbuffer, int* width, int* height, GLint* internalformat,
+    FrameworkFormat* frameworkFormat) {
 
     AutoLock mutex(m_lock);
 
@@ -1958,6 +1959,9 @@ bool FrameBuffer::getColorBufferInfo(
     *width = cb->getWidth();
     *height = cb->getHeight();
     *internalformat = cb->getInternalFormat();
+    if (frameworkFormat) {
+        *frameworkFormat = cb->getFrameworkFormat();
+    }
 
     return true;
 }
