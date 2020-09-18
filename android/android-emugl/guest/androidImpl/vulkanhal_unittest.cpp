3d7558fed2fd7847214de69c1fb8157b7f294c89
Lingfeng Yang <lfy@google.com>
2020-09-18 13:14:45 -0700

    vulkan: multithreading benchmark
    Change-Id: Id0650639bdf195550fb76b76d6c184af4355d5fe

--- android/android-emugl/guest/androidImpl/vulkanhal_unittest.cpp
+++ android/android-emugl/guest/androidImpl/vulkanhal_unittest.cpp
@@ -1573,7 +1573,6 @@ TEST_P(VulkanHalTest, ImmutableSamplersSuppressVkWriteDescriptorSetSampler) {
     vk->vkDestroySampler(mDevice, sampler, nullptr);
 }
 
-
 // Tests vkGetImageMemoryRequirements2
 TEST_P(VulkanHalTest, GetImageMemoryRequirements2) {
     VkImageCreateInfo testImageCi = {
@@ -1621,6 +1620,7 @@ TEST_P(VulkanHalTest, GetImageMemoryRequirements2) {
     vk->vkDestroyImage(mDevice, testImage, nullptr);
 }
 
+// Tests unclean process exit along with much vk commands still in the stream.
 TEST_P(VulkanHalTest, ProcessCleanup) {
     static constexpr uint32_t kNumTrials = 10;
     static constexpr uint32_t kNumImagesPerTrial = 100;
@@ -1652,6 +1652,101 @@ TEST_P(VulkanHalTest, ProcessCleanup) {
         restartProcessPipeAndHostConnection();
         setupVulkan();
     }
+}
+
+// Multithreaded benchmark
+TEST_P(VulkanHalTest, Multithreaded) {
+    constexpr uint32_t kThreadCount = 6;
+    VkDescriptorPool pool;
+    VkDescriptorSetLayout setLayout;
+    std::vector<VkDescriptorSet> sets(kThreadCount);
+
+    // Setup descriptor sets
+    EXPECT_EQ(VK_SUCCESS, allocateTestDescriptorSets(
+        kThreadCount, kThreadCount,
+        VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+        VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT,
+        &pool, &setLayout, sets.data()));
+
+    // Setup command buffers
+    VkCommandPoolCreateInfo poolCi = {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO, 0, 0, mGraphicsQueueFamily,
+    };
+
+    VkCommandPool commandPool;
+    vk->vkCreateCommandPool(mDevice, &poolCi, nullptr, &commandPool);
+
+    VkCommandBuffer cbs[kThreadCount];
+    VkCommandBufferAllocateInfo cbAi = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO, 0,
+        commandPool, VK_COMMAND_BUFFER_LEVEL_PRIMARY, kThreadCount,
+    };
+
+    vk->vkAllocateCommandBuffers(mDevice, &cbAi, cbs);
+
+    std::vector<FunctorThread*> threads;
+
+    constexpr uint32_t kRecordsPerThread = 500000;
+    constexpr uint32_t kTotalRecords = kThreadCount * kRecordsPerThread;
+
+    for (uint32_t i = 0; i < kThreadCount; ++i) {
+        FunctorThread* thread = new FunctorThread([this, cbs, i]() {
+            VkCommandBufferBeginInfo beginInfo = {
+                VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO, 0,
+                VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, 0,
+            };
+
+            for (uint32_t j = 0; j < kRecordsPerThread; ++j) {
+                vk->vkBeginCommandBuffer(cbs[i], &beginInfo);
+                vk->vkEndCommandBuffer(cbs[i]);
+            }
+
+            VkSubmitInfo si = {
+                VK_STRUCTURE_TYPE_SUBMIT_INFO, 0,
+                0, 0,
+                0,
+                1, &cbs[i],
+                0, 0,
+            };
+
+            vk->vkQueueSubmit(mQueue, 1, &si, 0);
+            return 0;
+        });
+        threads.push_back(thread);
+    }
+
+    auto cpuTimeStart = System::cpuTime();
+
+    for (uint32_t i = 0; i < kThreadCount; ++i) {
+        threads[i]->start();
+    }
+
+    for (uint32_t i = 0; i < kThreadCount; ++i) {
+        threads[i]->wait();
+        delete threads[i];
+    }
+
+    vk->vkDeviceWaitIdle(mDevice);
+
+    auto cpuTime = System::cpuTime() - cpuTimeStart;
+
+    uint64_t duration_us = cpuTime.wall_time_us;
+    uint64_t duration_cpu_us = cpuTime.usageUs();
+
+    float ms = duration_us / 1000.0f;
+    float sec = duration_us / 1000000.0f;
+
+    float submitHz = (float)kTotalRecords / sec;
+
+    printf("Record %u times in %f ms. Rate: %f Hz per thread: %f Hz\n", kTotalRecords, ms, submitHz, (float)submitHz / (float)kThreadCount);
+
+    vk->vkFreeCommandBuffers(mDevice, commandPool, 1, cbs);
+    vk->vkDestroyCommandPool(mDevice, commandPool, nullptr);
+
+    EXPECT_EQ(VK_SUCCESS, vk->vkFreeDescriptorSets(
+        mDevice, pool, kThreadCount, sets.data()));
+
+    vk->vkDestroyDescriptorPool(mDevice, pool, nullptr);
 }
 
 INSTANTIATE_TEST_SUITE_P(
