b2b6c4532490d08edf63e3be438deb58851aaee8
Lingfeng Yang <lfy@google.com>
2020-09-18 13:14:45 -0700

    vulkan: multithreading benchmark
    Bug: 171605370
    
    Change-Id: Id0650639bdf195550fb76b76d6c184af4355d5fe

--- android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
+++ android/android-emugl/combined/GoldfishOpenglTestEnv.cpp
@@ -87,7 +87,7 @@ static AndroidOptions sTestEnvCmdLineOptions;
 // static
 std::vector<const char*> GoldfishOpenglTestEnv::getTransportsToTest() {
     return {
-            "pipe",
+            "asg",
     };
 }
 
