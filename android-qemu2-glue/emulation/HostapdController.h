1270f605328a3ea2c14fb0478f6cb40c5deb9312
Weilun Du <wdu@google.com>
2020-09-21 11:50:24 -0700

    [Virtio Wifi] Refactor hostapd startup process
    Refactor hostapd startup/turndown process into a class and add logging
    level. No functional change in this CL.
    
    BUG: 148080973
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: I1474536e6c2780f9474f98b433c1f2ae97bdebae

---
+++ android-qemu2-glue/emulation/HostapdController.h
@@ -0,0 +1,47 @@
+/* Copyright 2020 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+#pragma once
+
+#include "android/base/Compiler.h"
+#include "android/base/sockets/ScopedSocket.h"
+#include "android/base/synchronization/Event.h"
+
+#include <memory>
+#include <string>
+
+namespace android {
+namespace qemu2 {
+class HostapdController {
+public:
+    static HostapdController* getInstance();
+    bool initAndRun(bool verbose);
+    void setDriverSocket(android::base::ScopedSocket sock);
+    void terminate(bool wait);
+
+private:
+    enum class DebugLevel {
+        MSG_EXCESSIVE,
+        MSG_MSGDUMP,
+        MSG_DEBUG,
+        MSG_INFO,
+        MSG_WARNING,
+        MSG_ERROR,
+        MSG_DISABLE,
+    };
+    HostapdController();
+    std::string mTemplateConf;
+    bool mRunning;
+    std::unique_ptr<android::base::Event> mEvent;
+    android::base::ScopedSocket mSock;
+};
+}  // namespace qemu2
+}  // namespace android
