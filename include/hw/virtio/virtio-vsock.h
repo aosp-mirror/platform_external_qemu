3605f300b99568a288b174353e5de5ef3f29bc19
Roman Kiryanov <rkir@google.com>
2020-09-25 14:45:17 -0700

    WIP: qemu virtio vsock
    Bug: 169261468
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: Ia79a7798ab0a6d3d992ca8f937250f585fa5ad1f

---
+++ include/hw/virtio/virtio-vsock.h
@@ -0,0 +1,37 @@
+/*
+ * Virt IO vsock virtio device
+ *
+ */
+
+#ifndef _QEMU_VIRTIO_VSOCK_H
+#define _QEMU_VIRTIO_VSOCK_H
+
+#include <stdint.h>
+#include <qemu/compiler.h>
+#include <qemu/osdep.h>
+#include <qemu/typedefs.h>
+#include <hw/virtio/virtio.h>
+#include <qapi/error.h>
+
+typedef struct VirtIOVSockTag {
+    VirtIODevice parent;
+    uint64_t guest_cid;
+    void *impl;  // see virtio_vsock_ctor/virtio_vsock_dctor
+
+    /* do not save/load below to the snapshot */
+    VirtQueue *host_to_guest_vq;
+    VirtQueue *guest_to_host_vq;
+    VirtQueue *host_to_guest_event_vq;
+} VirtIOVSock;
+
+void virtio_vsock_ctor(VirtIOVSock *s, Error **errp);
+void virtio_vsock_dctor(VirtIOVSock *s);
+
+void virtio_vsock_handle_host_to_guest(VirtIODevice *dev, VirtQueue *vq);
+void virtio_vsock_handle_guest_to_host(VirtIODevice *dev, VirtQueue *vq);
+void virtio_vsock_handle_event_to_guest(VirtIODevice *dev, VirtQueue *vq);
+
+#define TYPE_VIRTIO_VSOCK "virtio-vsock"
+#define VIRTIO_VSOCK(obj) OBJECT_CHECK(VirtIOVSock, (obj), TYPE_VIRTIO_VSOCK)
+
+#endif /* _QEMU_VIRTIO_VSOCK_H */
