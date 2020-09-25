c9092d388ea88496b3d3059c5000758aed56bfaa
Lingfeng Yang <lfy@google.com>
2020-09-25 15:58:03 -0700

    seqlock: read/write locks for short acquisition times
    The time to acquire a normal rwlock, even for read, is on the order of,
    or greater than, the time taken to read the shared data. This is often
    because a global variable about the number of readers needs to be
    updated.
    
    Seqlocks (sequence locks) (https://en.wikipedia.org/wiki/Seqlock)
    work via a double checked atomic field that writers increment before and
    after writing. Readers check the value before and after, and retry if
    the value has changed or is odd. This makes it so that readers are truly
    read only.
    
    Test: Unit test
    
    Change-Id: I7a8d29359a7302bce156200e473356155b28a3c0

--- android/android-emu-base/android/base/synchronization/Lock_unittest.cpp
+++ android/android-emu-base/android/base/synchronization/Lock_unittest.cpp
@@ -15,6 +15,7 @@
 #include "android/base/synchronization/Lock.h"
 
 #include "android/base/testing/TestThread.h"
+#include "android/base/threads/FunctorThread.h"
 
 #include <gtest/gtest.h>
 
@@ -104,6 +105,120 @@ TEST(Lock, AutoLockSynchronization) {
     }
 
     EXPECT_EQ(static_cast<int>(kNumThreads), p.counter);
+}
+
+// Seqlock tests
+static constexpr size_t kSeqLockTestDataArraySize = 200;
+
+struct SeqLockTestData {
+    // Make partial reads very obvious
+    int nums[kSeqLockTestDataArraySize];
+};
+
+static void setTestData(struct SeqLockTestData* data, int val) {
+    for (uint32_t i = 0; i < kSeqLockTestDataArraySize; ++i) {
+        data->nums[i] = val;
+    }
+}
+
+static bool checkTestData(const struct SeqLockTestData* data, int val) {
+    for (uint32_t i = 0; i < kSeqLockTestDataArraySize; ++i) {
+        if (val != data->nums[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool checkTestDataConsistency(const struct SeqLockTestData* data) {
+    int currVal = data->nums[0];
+    for (uint32_t i = 1; i < kSeqLockTestDataArraySize; ++i) {
+        if (currVal != data->nums[i]) {
+            fprintf(stderr, "%s: error: currVal %d vs %d %d\n", __func__, currVal, data->nums[i], i);
+            return false;
+        }
+    }
+    return true;
+}
+
+// Very basic single thread.
+TEST(SeqLock, BasicSingleThread) {
+    SeqLockTestData data;
+
+    SeqLock sl;
+
+    {
+        SeqLock::ScopedWrite write(&sl);
+        setTestData(&data, 1);
+    }
+
+    SeqLockTestData dataCopy;
+
+    AEMU_SEQLOCK_READ_WITH_RETRY(&sl,
+        memcpy(&dataCopy, &data, sizeof(data)));
+
+    EXPECT_TRUE(checkTestData(&dataCopy, 1));
+}
+
+struct SeqLockSharedState {
+    SeqLock* sl;
+    uint32_t* counter;
+    SeqLockTestData* data;
+};
+
+// SeqLock test with 1 writer and some number of readers.
+TEST(SeqLock, WriterReaders) {
+    uint32_t counter = 0;
+    SeqLockTestData data;
+    setTestData(&data, 0);
+    SeqLock sl;
+    SeqLockSharedState state;
+    state.sl = &sl;
+    state.data = &data;
+    state.counter = &counter;
+
+    static constexpr uint32_t kNumIters = 100000;
+    static constexpr uint32_t kReaderCount = 16;
+
+    FunctorThread writer([this, &state]() {
+        for (uint32_t i = 0; i < kNumIters; ++i) {
+            SeqLock::ScopedWrite write(state.sl);
+            *(state.counter) = *(state.counter) + 1;
+            setTestData(state.data, *(state.counter));
+        }
+    });
+
+    auto readerFunc = [this, &state]() {
+        SeqLockTestData readerCopy;
+        for (uint32_t i = 0; i < kNumIters; ++i) {
+            uint32_t seq = 0;
+            do {
+                seq = state.sl->beginRead();
+                memcpy(&readerCopy, state.data, sizeof(readerCopy));
+            } while(state.sl->shouldRetryRead(seq));
+            EXPECT_TRUE(checkTestDataConsistency(&readerCopy));
+        }
+    };
+
+    std::vector<FunctorThread*> readers;
+
+    for (uint32_t i = 0; i < kReaderCount; ++i) {
+        readers.push_back(new FunctorThread(readerFunc));
+    }
+
+    for (uint32_t i = 0; i < kReaderCount; ++i) {
+        readers[i]->start();
+    }
+
+    writer.start();
+    writer.wait();
+
+    for (uint32_t i = 0; i < kReaderCount; ++i) {
+        readers[i]->wait();
+        delete readers[i];
+    }
+
+    EXPECT_TRUE(checkTestData(&data, kNumIters));
 }
 
 }  // namespace base
