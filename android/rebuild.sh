daf8cdfbce1b22d4bb6484e1b85fd9898b0e3f75
Erwin Jansen <jansene@google.com>
2020-10-05 14:23:56 -0700

    Remove python dependencies.
    We now only make use of default python dependencies. The uploader script
    still makes use of the request library, which will be installed if it is
    missing.
    
    Bug: b/170128282
    Change-Id: I3411adc2f84e7a8d53e9e80f7b53065883fb910f

--- android/rebuild.sh
+++ android/rebuild.sh
@@ -16,28 +16,6 @@ exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-python_module_exist() {
-    #  returns true if $1 can be imported using the default python interpreter
-    python -c "import $1" > /dev/null 2>&1
-}
-
-install_deps() {
-    # Checks to see if the given import is availabe from the current python install
-    # if not, it will install the dependency using pip or try to use easy_install.
-    # If that fails, you will have to install
-    local import=$1
-    local package=$2
-    if ! python_module_exist ${import}; then
-        echo "Missing dependency ${package} attempting to install dependency in user packages."
-        python_module_exist pip || panic "No installer available (https://pip.pypa.io/en/stable/installing/)! Please run: curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py"
-        python -m pip install --user $package
-    fi
-}
-
-
 exists python || panic "No python interpreter available, please install python!"
-install_deps absl absl-py
-install_deps requests requests
-install_deps enum enum34
 
-python $PROGDIR/build/python/cmake.py --noshowprefixforinfo  $*
+python $PROGDIR/build/python/cmake.py   $*
