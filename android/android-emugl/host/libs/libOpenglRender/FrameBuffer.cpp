a2663b2ff8c24e95adaafdaec888e8f01b282756
bohu <bohu@google.com>
2020-10-07 21:51:54 -0700

    Framebuffer: avoid update color buffer with zero area
    This does happen on Qt branch, don't know why.
    
    BUG: 170042247
    
    Test:
    with hardware decoder into gpu texture mode:
    atest android.media.cts.ImageReaderDecoderTest#testOtherH264ImageReader
    
    Change-Id: Iec1155e7b66ac55d185b11c47b543310616ec7cf

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -1904,6 +1904,10 @@ bool FrameBuffer::updateColorBuffer(HandleType p_colorbuffer,
                                     GLenum format,
                                     GLenum type,
                                     void* pixels) {
+    if (width == 0 || height == 0) {
+        return false;
+    }
+
     AutoLock mutex(m_lock);
 
     ColorBufferMap::iterator c(m_colorbuffers.find(p_colorbuffer));
