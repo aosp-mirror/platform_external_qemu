217319f15d18e1877aae7ca1b938080605e37c55
Lingfeng Yang <lfy@google.com>
2020-10-13 19:52:13 -0700

    gfxstream: fwd fix after resetPipe retirement
    Change-Id: Ifda2cb242d3a3e09b846a18edce38bb6e2ef1590

--- android-qemu2-glue/emulation/virtio-goldfish-pipe.cpp
+++ android-qemu2-glue/emulation/virtio-goldfish-pipe.cpp
@@ -514,7 +514,6 @@ public:
     }
 
     void resetPipe(GoldfishHwPipe* hwPipe, GoldfishHostPipe* hostPipe) {
-        fprintf(stderr, "%s: call\n", __func__);
         VGPLOG("Want to reset hwpipe %p to hostpipe %p", hwPipe, hostPipe);
         VirglCtxId asCtxId = (VirglCtxId)(uintptr_t)hwPipe;
         auto it = mContexts.find(asCtxId);
@@ -1149,7 +1148,15 @@ public:
                     ((char*)entry.linear) + box->x + writtenBytes,
                     wantedBytes - writtenBytes,
                 };
+
+                // guest_send can now reallocate the pipe.
+                void* hostPipeBefore = hostPipe;
                 auto status = ops->guest_send(&hostPipe, &buf, 1);
+                if (hostPipe != hostPipeBefore) {
+                    resetPipe((GoldfishHwPipe*)(uintptr_t)(entry.ctxId), hostPipe);
+                    it = mResources.find(resId);
+                    entry = it->second;
+                }
 
                 if (status > 0) {
                     writtenBytes += status;
