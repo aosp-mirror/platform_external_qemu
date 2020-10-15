1c3fed90b43f383f216b920c08ffd5627e7bfcf2
Yilong Li <liyl@google.com>
2020-10-15 11:59:56 -0700

    darwin: Use MTLTexture to share ColorBuffer between Vulkan instances
    Previously we have used IOSurface to share Vulkan-backed ColorBuffers.
    This has a few drawbacks:
    
    * After creating IOSurfaces, the VkImage will be mapped to MTLTexture
      using the IOSurface, instead of using MTLBuffers from the
      VkDeviceMemory; so host memory mapping doesn't work well.
    
    * Host mapping of IOSurface requires lock and unlock operations
      which don't work well with guest mappings and can be a not
      particularly cheap operation.
    
    MoltenVK also has an API to export the internal MTLTexture of a
    VkImage (vkGetMTLSurfaceMVK) and import it into another image
    (vkSetMTLSurfaceMVK). This makes it possible to share host-visible
    images, and sharing MTL surfaces will be supported as a standard
    Vulkan extension in the future (see
    https://github.com/KhronosGroup/MoltenVK/issues/830 ).
    
    TEST=spinning-square-rs using
         MVK_CONFIG_FORCE_LOW_POWER_GPU=1 fx emu -N --host-gpu -e <emu-dir>/objs
    
    Change-Id: I118abe0917f3a37987f960bce38b14133925f944

--- android/android-emugl/host/libs/libOpenglRender/vulkan/VkCommonOperations.h
+++ android/android-emugl/host/libs/libOpenglRender/vulkan/VkCommonOperations.h
@@ -85,8 +85,9 @@ struct VkEmulation {
             getImageFormatProperties2Func = nullptr;
 
     bool instanceSupportsMoltenVK = false;
-    PFN_vkUseIOSurfaceMVK useIOSurfaceFunc = nullptr;
-    PFN_vkGetIOSurfaceMVK getIOSurfaceFunc = nullptr;
+    PFN_vkSetMTLTextureMVK setMTLTextureFunc = nullptr;
+    PFN_vkGetMTLTextureMVK getMTLTextureFunc = nullptr;
+    PFN_vkGetMTLBufferMVK getMTLBufferFunc = nullptr;
 
     // Queue, command pool, and command buffer
     // for running commands to sync stuff system-wide.
@@ -250,7 +251,7 @@ struct VkEmulation {
 
         VulkanMode vulkanMode = VulkanMode::Default;
 
-        IOSurfaceRef ioSurface = nullptr;
+        MTLTextureRef mtlTexture = nullptr;
     };
 
     struct BufferInfo {
@@ -267,7 +268,7 @@ struct VkEmulation {
 
         bool glExported = false;
         VulkanMode vulkanMode = VulkanMode::Default;
-        IOSurfaceRef ioSurface = nullptr;
+        MTLBufferRef mtlBuffer = nullptr;
     };
 
     // Track what is supported on whatever device was selected.
@@ -359,7 +360,7 @@ VkEmulation::ColorBufferInfo getColorBufferInfo(uint32_t colorBufferHandle);
 bool updateColorBufferFromVkImage(uint32_t colorBufferHandle);
 bool updateVkImageFromColorBuffer(uint32_t colorBufferHandle);
 VK_EXT_MEMORY_HANDLE getColorBufferExtMemoryHandle(uint32_t colorBufferHandle);
-IOSurfaceRef getColorBufferIOSurface(uint32_t colorBufferHandle);
+MTLTextureRef getColorBufferMTLTexture(uint32_t colorBufferHandle);
 bool setColorBufferVulkanMode(uint32_t colorBufferHandle, uint32_t vulkanMode);
 int32_t mapGpaToBufferHandle(uint32_t bufferHandle,
                              uint64_t gpa,
