d48eba105594a4d646b3e5a094c3dc3aa9a45554
Lingfeng Yang <lfy@google.com>
2020-10-19 13:06:46 -0700

    Fix color buffer lifetime with minigbm
    When using minigbm, don't try to refcount the color buffers
    separately on the host side. We can tell if minigbm is used if
    transfer 3d is called with proper arguments for creating color buffers.
    
    In this situation, we don't need the RefCountPipe either.
    
    Bug: 170897120
    
    Change-Id: I7a623ac762f010262f167be3008da6b6bbf15a6f

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -1362,13 +1362,15 @@ void FrameBuffer::drainWindowSurface() {
         const auto winIt = m_windows.find(winHandle);
         if (winIt != m_windows.end()) {
             if (const HandleType oldColorBufferHandle = winIt->second.second) {
-                if (m_refCountPipeEnabled) {
-                    if (decColorBufferRefCountLocked(oldColorBufferHandle)) {
-                        colorBuffersToCleanup.push_back(oldColorBufferHandle);
-                    }
-                } else {
-                    if (closeColorBufferLocked(oldColorBufferHandle)) {
-                        colorBuffersToCleanup.push_back(oldColorBufferHandle);
+                if (!m_guestManagedColorBufferLifetime) {
+                    if (m_refCountPipeEnabled) {
+                        if (decColorBufferRefCountLocked(oldColorBufferHandle)) {
+                            colorBuffersToCleanup.push_back(oldColorBufferHandle);
+                        }
+                    } else {
+                        if (closeColorBufferLocked(oldColorBufferHandle)) {
+                            colorBuffersToCleanup.push_back(oldColorBufferHandle);
+                        }
                     }
                 }
                 m_windows.erase(winIt);
@@ -1424,13 +1426,15 @@ std::vector<HandleType> FrameBuffer::DestroyWindowSurfaceLocked(HandleType p_sur
     const auto w = m_windows.find(p_surface);
     if (w != m_windows.end()) {
         ScopedBind bind(m_colorBufferHelper);
-        if (m_refCountPipeEnabled) {
-            if (decColorBufferRefCountLocked(w->second.second)) {
-                colorBuffersToCleanUp.push_back(w->second.second);
-            }
-        } else {
-            if (closeColorBufferLocked(w->second.second)) {
-                colorBuffersToCleanUp.push_back(w->second.second);
+        if (!m_guestManagedColorBufferLifetime) {
+            if (m_refCountPipeEnabled) {
+                if (decColorBufferRefCountLocked(w->second.second)) {
+                    colorBuffersToCleanUp.push_back(w->second.second);
+                }
+            } else {
+                if (closeColorBufferLocked(w->second.second)) {
+                    colorBuffersToCleanUp.push_back(w->second.second);
+                }
             }
         }
         m_windows.erase(w);
@@ -1468,7 +1472,7 @@ int FrameBuffer::openColorBuffer(HandleType p_colorbuffer) {
     c->second.refcount++;
     markOpened(&c->second);
 
-    uint64_t puid = tInfo->m_puid;
+    uint64_t puid = tInfo ? tInfo->m_puid : 0;
     if (puid) {
         m_procOwnedColorBuffers[puid].insert(p_colorbuffer);
     }
@@ -1478,15 +1482,16 @@ int FrameBuffer::openColorBuffer(HandleType p_colorbuffer) {
 void FrameBuffer::closeColorBuffer(HandleType p_colorbuffer) {
     // When guest feature flag RefCountPipe is on, no reference counting is
     // needed.
-    if (m_refCountPipeEnabled)
+    if (m_refCountPipeEnabled) {
         return;
+    }
 
     RenderThreadInfo* tInfo = RenderThreadInfo::get();
 
     std::vector<HandleType> toCleanup;
 
     AutoLock mutex(m_lock);
-    uint64_t puid = tInfo->m_puid;
+    uint64_t puid = tInfo ? tInfo->m_puid : 0;
     if (puid) {
         auto ite = m_procOwnedColorBuffers.find(puid);
         if (ite != m_procOwnedColorBuffers.end()) {
@@ -1527,14 +1532,16 @@ bool FrameBuffer::closeColorBufferLocked(HandleType p_colorbuffer,
                                          bool forced) {
     // When guest feature flag RefCountPipe is on, no reference counting is
     // needed.
-    if (m_refCountPipeEnabled)
+    if (m_refCountPipeEnabled) {
         return false;
+    }
 
     if (m_noDelayCloseColorBufferEnabled)
         forced = true;
 
     ColorBufferMap::iterator c(m_colorbuffers.find(p_colorbuffer));
     if (c == m_colorbuffers.end()) {
+        *(uint32_t*)(1234) = 5;
         // This is harmless: it is normal for guest system to issue
         // closeColorBuffer command when the color buffer is already
         // garbage collected on the host. (we dont have a mechanism
@@ -1651,13 +1658,15 @@ std::vector<HandleType> FrameBuffer::cleanupProcGLObjects_locked(uint64_t puid, 
             if (procIte != m_procOwnedWindowSurfaces.end()) {
                 for (auto whndl : procIte->second) {
                     auto w = m_windows.find(whndl);
-                    if (m_refCountPipeEnabled) {
-                        if (decColorBufferRefCountLocked(w->second.second)) {
-                            colorBuffersToCleanup.push_back(w->second.second);
-                        }
-                    } else {
-                        if (closeColorBufferLocked(w->second.second, forced)) {
-                            colorBuffersToCleanup.push_back(w->second.second);
+                    if (!m_guestManagedColorBufferLifetime) {
+                        if (m_refCountPipeEnabled) {
+                            if (decColorBufferRefCountLocked(w->second.second)) {
+                                colorBuffersToCleanup.push_back(w->second.second);
+                            }
+                        } else {
+                            if (closeColorBufferLocked(w->second.second, forced)) {
+                                colorBuffersToCleanup.push_back(w->second.second);
+                            }
                         }
                     }
                     m_windows.erase(w);
@@ -1670,14 +1679,16 @@ std::vector<HandleType> FrameBuffer::cleanupProcGLObjects_locked(uint64_t puid, 
         // the guest process, to give the correct reference count.
         // (Note that a color buffer can be shared across guest processes.)
         {
-            auto procIte = m_procOwnedColorBuffers.find(puid);
-            if (procIte != m_procOwnedColorBuffers.end()) {
-                for (auto cb : procIte->second) {
-                    if (closeColorBufferLocked(cb, forced)) {
-                        colorBuffersToCleanup.push_back(cb);
+            if (!m_guestManagedColorBufferLifetime) {
+                auto procIte = m_procOwnedColorBuffers.find(puid);
+                if (procIte != m_procOwnedColorBuffers.end()) {
+                    for (auto cb : procIte->second) {
+                        if (closeColorBufferLocked(cb, forced)) {
+                            colorBuffersToCleanup.push_back(cb);
+                        }
                     }
+                    m_procOwnedColorBuffers.erase(procIte);
                 }
-                m_procOwnedColorBuffers.erase(procIte);
             }
         }
 
@@ -1765,13 +1776,19 @@ bool FrameBuffer::setWindowSurfaceColorBuffer(HandleType p_surface,
     (*w).second.first->setColorBuffer((*c).second.cb);
     markOpened(&c->second);
     if (w->second.second) {
-        if (m_refCountPipeEnabled)
-            decColorBufferRefCountLocked(w->second.second);
-        else
-            closeColorBufferLocked(w->second.second);
+        if (!m_guestManagedColorBufferLifetime) {
+            if (m_refCountPipeEnabled) {
+                decColorBufferRefCountLocked(w->second.second);
+            } else {
+                closeColorBufferLocked(w->second.second);
+            }
+        }
     }
 
-    c->second.refcount++;
+    if (!m_guestManagedColorBufferLifetime) {
+        c->second.refcount++;
+    }
+
     (*w).second.second = p_colorbuffer;
 
     m_windowSurfaceToColorBuffer[p_surface] = p_colorbuffer;
@@ -3043,4 +3060,8 @@ void FrameBuffer::waitForGpuVulkan(uint64_t deviceHandle, uint64_t fenceHandle) 
 
     // Note: this will always signal right away.
     SyncThread::get()->triggerBlockedWaitNoTimeline(fenceSync);
+}
+
+void FrameBuffer::setGuestManagedColorBufferLifetime(bool guestManaged) {
+    m_guestManagedColorBufferLifetime = guestManaged;
 }
