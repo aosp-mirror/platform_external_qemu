9d9cacf7a6cf17df70bebd823ef6726ebef0bef7
Erwin Jansen <jansene@google.com>
2020-10-26 17:39:59 +0100

    [WebRTC] Stream images through gRPC with mmap
    This enables the goldfish video bridge to retrieve images using the gRPC
    endpoint and the MMAP side channel. Image start streaming as soon as the
    encoder is connected.
    
    This enables the goldfish bridge to become independent of the emulator.
    
    Note: This does not yet support rotation properly.
    
    Test: Can successfully stream video in webrtc.
    Change-Id: I95efc2966f0629fe488fce3c0fffacdf177044cf

--- android/android-emu/android-emu.cmake
+++ android/android-emu/android-emu.cmake
@@ -517,7 +517,7 @@ target_compile_definitions(
           "-DANDROID_SDK_TOOLS_BUILD_NUMBER=${OPTION_SDK_TOOLS_BUILD_NUMBER}")
 
 if(WEBRTC)
-  target_compile_definitions(android-emu PUBLIC -DANDROID_WEBRTC)
+  # target_compile_definitions(android-emu PUBLIC -DANDROID_WEBRTC)
 endif()
 
 if(OPTION_GFXSTREAM_BACKEND)
@@ -739,7 +739,7 @@ target_compile_definitions(
           "-DANDROID_SDK_TOOLS_REVISION=${OPTION_SDK_TOOLS_REVISION}"
           "-DANDROID_SDK_TOOLS_BUILD_NUMBER=${OPTION_SDK_TOOLS_BUILD_NUMBER}")
 if(WEBRTC)
-  target_compile_definitions(android-emu-shared PUBLIC -DANDROID_WEBRTC)
+   target_compile_definitions(android-emu-shared PUBLIC -DANDROID_WEBRTC)
 endif()
 
 target_compile_definitions(android-emu-shared PUBLIC -DAEMU_MIN=1)
