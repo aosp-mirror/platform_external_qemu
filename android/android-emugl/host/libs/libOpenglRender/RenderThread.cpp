bd56e60182ea352cee2f0b1ebaa1cc78764c0d7b
Lingfeng Yang <lfy@google.com>
2020-10-28 14:12:49 -0700

    When running with <= 4 system threads, only run one renderthread slice at a time
    If the user doesn't have hyperthreading or has an older machine with
    fewer cores, it can be advantageous to run only one renderthread slice
    at a time.
    
    Change-Id: Icbc640e5c92fcfa7ebf80d0b6c0c54eeddfa013f

--- android/android-emugl/host/libs/libOpenglRender/RenderThread.cpp
+++ android/android-emugl/host/libs/libOpenglRender/RenderThread.cpp
@@ -33,6 +33,7 @@
 #include "android/base/system/System.h"
 #include "android/base/Tracing.h"
 #include "android/base/files/StreamSerializing.h"
+#include "android/base/synchronization/MessageChannel.h"
 #include "android/utils/path.h"
 #include "android/utils/file_io.h"
 
@@ -43,6 +44,7 @@
 #include <assert.h>
 
 using android::base::AutoLock;
+using android::base::MessageChannel;
 
 namespace emugl {
 
@@ -71,10 +73,33 @@ static uint64_t currTimeUs(bool enable) {
 // Start with a smaller buffer to not waste memory on a low-used render threads.
 static constexpr int kStreamBufferSize = 128 * 1024;
 
+// Requires this many threads on the system available to run unlimited.
+static constexpr int kMinThreadsToRunUnlimited = 5;
+
+// A thread run limiter that limits render threads to run one slice at a time.
+class ThreadRunLimiter {
+public:
+    ThreadRunLimiter() {
+        mTokenChannel.send(0);
+    }
+    void acquire() {
+        int val;
+        mTokenChannel.receive(&val);
+    }
+    void release() {
+        mTokenChannel.send(0);
+    }
+private:
+    android::base::MessageChannel<int, 1> mTokenChannel;
+};
+
+static ThreadRunLimiter sThreadRunLimiter;
+
 RenderThread::RenderThread(RenderChannelImpl* channel,
                            android::base::Stream* loadStream)
     : emugl::Thread(android::base::ThreadFlags::MaskSignals, 2 * 1024 * 1024),
-      mChannel(channel) {
+      mChannel(channel),
+      mRunInLimitedMode(android::base::System::get()->getCpuCoreCount() < kMinThreadsToRunUnlimited) {
     if (loadStream) {
         const bool success = loadStream->getByte();
         if (success) {
@@ -85,6 +110,7 @@ RenderThread::RenderThread(RenderChannelImpl* channel,
             mFinished.store(true, std::memory_order_relaxed);
         }
     }
+    mRunInLimitedMode = true;
 }
 
 RenderThread::RenderThread(
@@ -391,7 +417,13 @@ intptr_t RenderThread::main() {
 
         auto progressStart = currTimeUs(benchmarkEnabled);
         bool progress;
+
+        if (mRunInLimitedMode) {
+            sThreadRunLimiter.acquire();
+        }
+
         do {
+
             progress = false;
 
             // try to process some of the command buffer using the GLESv1
@@ -463,6 +495,10 @@ intptr_t RenderThread::main() {
                 }
             }
         } while (progress);
+
+        if (mRunInLimitedMode) {
+            sThreadRunLimiter.release();
+        }
     }
 
     if (dumpFP) {
