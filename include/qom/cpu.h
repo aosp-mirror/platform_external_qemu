194e811ad7f4458c8e3b6e05448e66230d5a56bf
Lingfeng Yang <lfy@google.com>
2020-11-22 23:13:07 -0800

    darwin-aarch64: hvf: add a skeleton backend
    Remove the stub, at least.
    
    TODO
    
    - This runs a vcpu but it gets stuck
    - Add interrupts/events
    - Add enough insn emulator to service MMIO
    
    Change-Id: I5d50f7170528d3bdb8af2747472a4d31e4f8a1d3

--- include/qom/cpu.h
+++ include/qom/cpu.h
@@ -29,6 +29,8 @@
 #include "qemu/queue.h"
 #include "qemu/thread.h"
 
+#include <Hypervisor/Hypervisor.h>
+
 typedef int (*WriteCoreDumpFunction)(const void *buf, size_t size,
                                      void *opaque);
 
@@ -248,6 +250,7 @@ struct kvm_run;
 struct hvf_vcpu_caps;
 struct hvf_vcpu_state;
 struct hvf_x86_state;
+struct hvf_arm64_state;
 #endif
 
 struct hax_vcpu_state;
@@ -434,6 +437,10 @@ struct CPUState {
     // and x86 emulation state
     struct hvf_vcpu_caps* hvf_caps;
     struct hvf_x86_state* hvf_x86;
+    struct hvf_arm64_state* hvf_arm64;
+#ifdef __arm64__
+    hv_vcpu_exit_t* hvf_vcpu_exit_info;
+#endif
     union {
         uint32_t u32;
         icount_decr_u16 u16;
