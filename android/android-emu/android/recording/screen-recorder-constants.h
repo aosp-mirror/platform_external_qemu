f7710ee338a8632a139309a7724ffe8bd4a2ef19
bohu <bohu@google.com>
2021-01-07 17:43:55 -0800

    screenrecorder: extend maximum time limit to 30 minutes
    3 minutes seem too small for testing purpose, extend to 30
    
    Change-Id: I235d4040a163b4d05de5ba233c8d1b6eb2b17bfe

--- android/android-emu/android/recording/screen-recorder-constants.h
+++ android/android-emu/android/recording/screen-recorder-constants.h
@@ -16,7 +16,7 @@
 
 constexpr int kMinVideoBitrate = 100 * 1000;        // bps
 constexpr int kMaxVideoBitrate = 25 * 1000 * 1000;  // bps
-constexpr int kMaxTimeLimit = 3 * 60;               // seconds
+constexpr int kMaxTimeLimit = 30 * 60;              // seconds
 constexpr int kMaxFPS = 60;                         // fps
 
 // Spacing between intra frames
