c30036f235aa3b497742227030f875f7ea3f7986
Erwin Jansen <jansene@google.com>
2021-01-15 17:25:43 -0800

    [gRPC] Add support for multi display notifications.
    This add change support for multi display changes. The gRPC notification
    channel now adds an event for multi display changes.
    
    - Adds an EventNotificationSupport class to easily add event
    notification to classes.
    - Adds a unit test for the new class.
    - Adds property change support to Camera & Multidisplay class.
    - Update the stream notification method to listen for events from both
    the camera as well as multidisplay.
    - Disables the Arrangement UI when we are hiding the qt-window.
    
    Test: $ python -m sample.notifications
    event: DISPLAY_CONFIGURATIONS_CHANGED_UI
    event: DISPLAY_CONFIGURATIONS_CHANGED_UI
    
    Bug: b/176447902
    Change-Id: Ib007e70b2623347cd265ccd3cc5e6eb17073b88b

---
+++ android/android-emu-base/android/base/EventNotificationSupport_unittest.cpp
@@ -0,0 +1,64 @@
+// Copyright 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/base/EventNotificationSupport.h"
+
+#include <gtest/gtest.h>            // for Message, SuiteApiResolver, TestPa...
+
+#include "gtest/gtest_pred_impl.h"  // for Test, EXPECT_EQ, TEST
+
+namespace android {
+namespace base {
+
+class SimpleEvent : public EventNotificationSupport<SimpleEvent, int> {
+public:
+    void setValue(int val) {
+        mValue = val;
+        fireEvent(val);
+    }
+
+    int mValue{0};
+};
+
+TEST(EventNotificationSupport, willFireEvent) {
+    SimpleEvent evt;
+    int val = 0;
+    std::function<void(SimpleEvent*, const int)> listener =
+            [&val](auto src, int newVal) { val = newVal; };
+    evt.addListener(&listener);
+    evt.setValue(10);
+    EXPECT_EQ(10, val);
+}
+
+TEST(EventNotificationSupport, raiUnregisters) {
+    SimpleEvent evt;
+    int val = 0;
+    // No events are fired.
+    evt.setValue(10);
+    EXPECT_EQ(0, val);
+    {
+        // Should fire an event, as we have registed a listener
+        RaiiEventListener<SimpleEvent, int> listener(
+                &evt, [&val](auto src, int newVal) { val = newVal; });
+        evt.setValue(10);
+        EXPECT_EQ(10, val);
+    }
+
+    // Listener has been cleaned up, so no events are fired.
+    evt.setValue(100);
+    EXPECT_EQ(10, val);
+}
+
+}  // namespace base
+}  // namespace android
