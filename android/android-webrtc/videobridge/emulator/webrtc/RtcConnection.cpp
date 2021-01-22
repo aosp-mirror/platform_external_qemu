6bfb4512005b6f52823635c01af66149f97217f1
Erwin Jansen <jansene@google.com>
2021-01-21 20:44:31 -0800

    [WebRTC] Memory and perf optimizations
    This fixes a series of memory leaks and introduces some optimizations:
    
    - Plugs all memory leaks pointed out by ASAN
    - Improved participant cleanup, no more garbage collecting.
    - Improved stopping/exiting, no more crashes on exit due to signalling
      from incorrect thread.
    - There is a single video/audio source that can be used by multiple
    consumers.
    
    Change-Id: I07ff88d5b574353da2fc2962915c1c5acd5505a7

--- android/android-webrtc/videobridge/emulator/webrtc/RtcConnection.cpp
+++ android/android-webrtc/videobridge/emulator/webrtc/RtcConnection.cpp
@@ -63,20 +63,32 @@ RtcConnection::RtcConnection(EmulatorGrpcClient* client)
             ::webrtc::CreateBuiltinVideoEncoderFactory(),
             ::webrtc::CreateBuiltinVideoDecoderFactory(),
             nullptr /* audio_mixer */, nullptr /* audio_processing */);
-    ;
 
+    mLibrary =
+            std::make_unique<MediaSourceLibrary>(mClient, mConnectionFactory.get());
     mKeepAlive.start();
     mLooperThread = std::make_unique<std::thread>([this]() {
         mLooper.runWithDeadlineMs(Looper::Timeout::kDurationInfinite);
-        RTC_LOG(INFO) << "Completed looper";
     });
 }
+
 RtcConnection::~RtcConnection() {
+    // Stop our looper.
     mLooperActive = false;
     mLooper.forceQuit();
-    if (mLooperThread) {
-        mLooperThread->join();
-    }
+    mLooperThread->join();
+
+    // First we need to clean up the connection factory.
+    mConnectionFactory = nullptr;
+
+    // And then shut down the WebRTC threads.
+    mSignaling->Stop();
+    mWorker->Stop();
+    mNetwork->Stop();
+}
+
+void RtcConnection::scheduleTask(std::function<void()> callback) {
+      mSignaling->PostTask(RTC_FROM_HERE, callback);
 }
 
 }  // namespace webrtc
