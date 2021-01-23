a0489dd45a676a5815ce921fea79c6a75f77f07d
Lingfeng Yang <lfy@google.com>
2021-01-22 20:58:36 -0800

    [11/n] vulkan: introduce delayed_delete for handles
    Bug: 177241396
    
    We will be reordering vkCmd* forwards but not after their corresponding
    vkQueueSubmit. According to the spec and VK-CTS, the one API where we
    need to be careful about here is vkDestroyPipelineLayout, because the
    client is allowed to destroy pipeline layouts even if a command buffer
    referred to it, as long as the command buffer was not also in the
    recording state at the same time.
    
    To account for this reordering, we will push all
    vkDestoryPipelineLayout* calls into the space after vkCmd* reorderings
    and before vkQueueSubmits (and of course, when the device or instance is
    destroyed). This calls for a new API, delayed_delete_##type, which
    queues up the deletion function somewhere to be retired later at a more
    convenient time.
    
    Change-Id: I9920e37c805ad5752dc08890bbe664836e564ca9

--- android/android-emugl/host/libs/libOpenglRender/vulkan/VulkanHandleMapping.h
+++ android/android-emugl/host/libs/libOpenglRender/vulkan/VulkanHandleMapping.h
@@ -19,6 +19,8 @@
 #include "VulkanHandles.h"
 #include "VulkanDispatch.h"
 
+#include <functional>
+
 namespace goldfish_vk {
 
 class VkDecoderGlobalState;
@@ -62,6 +64,7 @@ GOLDFISH_VK_LIST_DISPATCHABLE_HANDLE_TYPES(DEFINE_BOXED_DISPATCHABLE_HANDLE_GLOB
 #define DEFINE_BOXED_NON_DISPATCHABLE_HANDLE_GLOBAL_API_DECL(type) \
     type new_boxed_non_dispatchable_##type(type underlying); \
     void delete_##type(type boxed); \
+    void delayed_delete_##type(type boxed, VkDevice device, std::function<void()> callback); \
     type unbox_##type(type boxed); \
     type unboxed_to_boxed_non_dispatchable_##type(type boxed); \
 
