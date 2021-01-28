46c8b7fa00f900e448a6cb089e594270181e2090
Kaiyi Li <kaiyili@google.com>
2021-01-28 10:57:23 -0800

    Native VK Swapchain: basic code for CompositorVk
    CompositorVk is the class to draw the frame onto a given VkImage. This
    change includes basic initialization code for CompositorVk to report
    required features and extensions, and a unit test fixture that creates
    the needed Vulkan instance, phsical device, logical device.
    
    Test: run CompositorVk_unittests
    
    Change-Id: I062e5b35287d90fcedeb14a6183f5c9cd1caff0b

---
+++ android/android-emugl/host/libs/libOpenglRender/CompositorVk.cpp
@@ -0,0 +1,45 @@
+#include "CompositorVk.h"
+
+#include "vulkan/vk_util.h"
+
+std::unique_ptr<CompositorVk> CompositorVk::create(
+    const goldfish_vk::VulkanDispatch &vk, VkDevice vkDevice) {
+    return std::unique_ptr<CompositorVk>(new CompositorVk(vk, vkDevice));
+}
+
+CompositorVk::CompositorVk(const goldfish_vk::VulkanDispatch &vk,
+                           VkDevice vkDevice)
+    : m_vk(vk), m_vkDevice(vkDevice) {}
+
+bool CompositorVk::validatePhysicalDeviceFeatures(
+    const VkPhysicalDeviceFeatures2 &features) {
+    auto descIndexingFeatures =
+        vk_find_struct<VkPhysicalDeviceDescriptorIndexingFeaturesEXT>(
+            &features);
+    if (descIndexingFeatures == nullptr) {
+        return false;
+    }
+    return descIndexingFeatures->descriptorBindingSampledImageUpdateAfterBind ==
+           VK_TRUE;
+}
+
+bool CompositorVk::validateQueueFamilyProperties(
+    const VkQueueFamilyProperties &properties) {
+    return properties.queueFlags & VK_QUEUE_GRAPHICS_BIT;
+}
+
+bool CompositorVk::enablePhysicalDeviceFeatures(
+    VkPhysicalDeviceFeatures2 &features) {
+    auto descIndexingFeatures =
+        vk_find_struct<VkPhysicalDeviceDescriptorIndexingFeaturesEXT>(
+            &features);
+    if (descIndexingFeatures == nullptr) {
+        return false;
+    }
+    descIndexingFeatures->descriptorBindingSampledImageUpdateAfterBind =
+        VK_TRUE;
+    return true;
+}
+std::vector<const char *> CompositorVk::getRequiredDeviceExtensions() {
+    return {VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME};
+}
