7019384c1c17d99f98e6f542ea037f9042c5f0c7
Kaiyi Li <kaiyili@google.com>
2021-02-01 07:57:28 -0800

    Native VK Swapchain: Basic compositor graphics pipeline initialization
    Add code to initialize a basic graphics pipeline for CompositorVk which
    includes a render pass with shaders. The render pass will draw an
    example triangle onto the render target.
    
    The Vulkan swapchain will provide multiple VKImages, so the CompositorVk
    allow to draw on multiple render targets and will generate one command
    buffer for each render target. When drawing, provide the index of the
    render target to retrieve the corresponding command buffer. Then submit
    the command buffer.
    
    To support future rcCompose, we may want to submit the same draw command
    buffer with different layers and composition parameters. To do this, we
    may want to change how the render pass transform the image layout and
    the VkAttachmentDescription::loadOp from VK_ATTACHMENT_LOAD_OP_CLEAR.
    
    Test: CompositorVk_unittests
    
    Change-Id: I5549f5d6334239c20d24c4de93d45e04c43d8b56

--- android/android-emugl/host/libs/libOpenglRender/CompositorVk.h
+++ android/android-emugl/host/libs/libOpenglRender/CompositorVk.h
@@ -2,6 +2,7 @@
 #define COMPOSITOR_VK_H
 
 #include <memory>
+#include <variant>
 #include <vector>
 
 #include "vulkan/cereal/common/goldfish_vk_dispatch.h"
@@ -9,7 +10,10 @@
 class CompositorVk {
    public:
     static std::unique_ptr<CompositorVk> create(
-        const goldfish_vk::VulkanDispatch &vk, VkDevice);
+        const goldfish_vk::VulkanDispatch &vk, VkDevice, VkFormat,
+        VkImageLayout initialLayout, VkImageLayout finalLayout, uint32_t width,
+        uint32_t height, const std::vector<VkImageView> &renderTargets,
+        VkCommandPool);
     static bool validatePhysicalDeviceFeatures(
         const VkPhysicalDeviceFeatures2 &features);
     static bool validateQueueFamilyProperties(
@@ -18,11 +22,29 @@ class CompositorVk {
         VkPhysicalDeviceFeatures2 &features);
     static std::vector<const char *> getRequiredDeviceExtensions();
 
+    ~CompositorVk();
+    VkCommandBuffer getCommandBuffer(uint32_t i) const;
+
    private:
-    explicit CompositorVk(const goldfish_vk::VulkanDispatch &, VkDevice);
+    explicit CompositorVk(const goldfish_vk::VulkanDispatch &, VkDevice,
+                          VkCommandPool);
+    void setUpGraphicsPipeline(uint32_t width, uint32_t height,
+                               VkFormat renderTargetFormat,
+                               VkImageLayout initialLayout,
+                               VkImageLayout finalLayout);
+    void setUpFramebuffers(const std::vector<VkImageView> &, uint32_t width,
+                           uint32_t height);
+    void setUpCommandBuffers(uint32_t width, uint32_t height);
 
     const goldfish_vk::VulkanDispatch &m_vk;
     const VkDevice m_vkDevice;
+    VkPipelineLayout m_vkPipelineLayout;
+    VkRenderPass m_vkRenderPass;
+    VkPipeline m_graphicsVkPipeline;
+    std::vector<VkCommandBuffer> m_vkCommandBuffers;
+    std::vector<VkFramebuffer> m_renderTargetVkFrameBuffers;
+
+    VkCommandPool m_vkCommandPool;
 };
 
 #endif /* COMPOSITOR_VK_H */
