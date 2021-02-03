ac3d9b5147bd1b0bd1b4e1de7da1f311bc4253ae
Lingfeng Yang <lfy@google.com>
2021-02-03 11:25:21 -0800

    gles: Advertise GL_OES_EGL_sync
    It's not enough to advertise EGL_KHR_fence_sync
    
    Change-Id: I676349272b2763a4750d9b3fc929b0fc2af1ccf3

--- android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
+++ android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Context.cpp
@@ -744,7 +744,7 @@ ProgramData* GLESv2Context::getUseProgram() {
 void GLESv2Context::initExtensionString() {
     if (s_glExtensionsInitialized) return;
 
-    *s_glExtensions = "GL_OES_EGL_image GL_OES_EGL_image_external GL_OES_depth24 GL_OES_depth32 GL_OES_element_index_uint "
+    *s_glExtensions = "GL_OES_EGL_sync GL_OES_EGL_image GL_OES_EGL_image_external GL_OES_depth24 GL_OES_depth32 GL_OES_element_index_uint "
                       "GL_OES_texture_float GL_OES_texture_float_linear "
                       "GL_OES_compressed_paletted_texture GL_OES_compressed_ETC1_RGB8_texture GL_OES_depth_texture ";
     if (s_glSupport.GL_ARB_HALF_FLOAT_PIXEL || s_glSupport.GL_NV_HALF_FLOAT)
