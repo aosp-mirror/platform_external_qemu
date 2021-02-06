f6652abfcc69f5edff23604e03a61cace5087a9e
Yilong Li <liyl@google.com>
2021-02-06 02:15:58 -0800

    vk1.2: [2] Update vulkan-registry to v1.2.162.
    This updates the vk.xml and all the scripts to v1.2.162 in Vulkan-Docs
    repository (https://github.com/KhronosGroup/Vulkan-Docs/).
    
    AEMU-specific changes are not merged yet; so this change doesn't
    work on its own.
    
    Bug: b/179480537
    Change-Id: I8ca606771ed8633ec37643e572f17b55fd6bcb15

--- android/android-emugl/host/libs/libOpenglRender/vulkan-registry/xml/Retired/realign.py
+++ android/android-emugl/host/libs/libOpenglRender/vulkan-registry/xml/Retired/realign.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 #
-# Copyright (c) 2013-2018 The Khronos Group Inc.
+# Copyright (c) 2013-2020 The Khronos Group Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 # Usage: realign [infile] > outfile
 # Used to realign XML tags in the Vulkan registry after it's operated on by
@@ -23,9 +13,9 @@ import copy, sys, string, re
 
 def realignXML(fp):
     patterns = [
-        [ '(^ *\<type .*)(category=[\'"]bitmask[\'"].*)', 58 ],
-        [ '(^ *\<enum [bv].*)(name=.*)',     28 ],
-        [ '(^ *\<enum [bv].*)(comment=.*)',  85 ]
+        [ r'(^ *\<type .*)(category=[\'"]bitmask[\'"].*)', 58 ],
+        [ r'(^ *\<enum [bv].*)(name=.*)',     28 ],
+        [ r'(^ *\<enum [bv].*)(comment=.*)',  85 ]
     ]
 
     # Assemble compiled expressions to match and alignment columns
