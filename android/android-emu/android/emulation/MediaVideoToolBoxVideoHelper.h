6cdfd127e7ae5e4d550c79ea096311458e7da3bb
bohu <bohu@google.com>
2021-03-04 10:06:14 -0800

    codecs: re-enable VideoToolBox decoder on OSX
    work in progress
    
    Change-Id: I91cc9ee16b6aba99d49f0e217acfd3deb261810e

---
+++ android/android-emu/android/emulation/MediaVideoToolBoxVideoHelper.h
@@ -0,0 +1,145 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/emulation/MediaSnapshotState.h"
+#include "android/emulation/MediaTexturePool.h"
+#include "android/emulation/MediaVideoHelper.h"
+#include "android/emulation/YuvConverter.h"
+
+// this is apple's video tool box header
+#include <VideoToolbox/VideoToolbox.h>
+
+#ifndef kVTVideoDecoderSpecification_RequireHardwareAcceleratedVideoDecoder
+#define kVTVideoDecoderSpecification_RequireHardwareAcceleratedVideoDecoder CFSTR("RequireHardwareAcceleratedVideoDecoder")
+#endif
+
+#include <cstdint>
+#include <list>
+#include <mutex>
+#include <string>
+#include <vector>
+
+extern "C" {
+
+
+}
+#include <stdio.h>
+#include <string.h>
+
+#include <stddef.h>
+
+namespace android {
+namespace emulation {
+
+class MediaVideoToolBoxVideoHelper : public MediaVideoHelper {
+public:
+    enum class FrameStorageMode {
+        USE_BYTE_BUFFER = 1,
+        USE_GPU_TEXTURE = 2,
+    };
+
+    enum class OutputTreatmentMode {
+        IGNORE_RESULT = 1,
+        SAVE_RESULT = 2,
+    };
+
+public:
+    MediaVideoToolBoxVideoHelper(OutputTreatmentMode outMode,
+                         FrameStorageMode fMode);
+    ~MediaVideoToolBoxVideoHelper() override;
+
+public:
+    // return true if success; false otherwise
+    bool init() override;
+    void decode(const uint8_t* frame,
+                size_t szBytes,
+                uint64_t inputPts) override;
+    void flush() override;
+    void deInit() override;
+
+    virtual int error() const override { return mErrorCode; }
+    virtual bool good() const override { return mIsGood; }
+    virtual bool fatal() const override { return false; }
+
+private:
+
+    // static
+    static void videoToolboxDecompressCallback(void* opaque,
+                                               void* sourceFrameRefCon,
+                                               OSStatus status,
+                                               VTDecodeInfoFlags flags,
+                                               CVImageBufferRef image_buffer,
+                                               CMTime pts,
+                                               CMTime duration);
+    static CFDictionaryRef createOutputBufferAttributes(int width,
+                                                        int height,
+                                                        OSType pix_fmt);
+    static CMSampleBufferRef createSampleBuffer(CMFormatDescriptionRef fmtDesc,
+                                                void* buffer,
+                                                size_t sz);
+private:
+    void copyFrame();
+
+    void recreateDecompressionSession();
+
+private:
+
+    uint64_t mNumInputFrame{0};
+    uint64_t mNumOutputFrame{0};
+    int mErrorCode = 0;
+    bool mIsGood = true;
+    unsigned int mHeight = 0;
+    bool mUseGpuTexture = false;
+    unsigned int mWidth = 0;
+    unsigned int mOutputHeight = 0;
+    unsigned int mOutputWidth = 0;
+    unsigned int mSurfaceHeight = 0;
+    unsigned int mBPP = 0;
+    unsigned int mSurfaceWidth = 0;
+    unsigned int mLumaWidth = 0;
+    unsigned int mLumaHeight = 0;
+    unsigned int mChromaHeight = 0;
+    unsigned int mOutBufferSize = 0;
+
+    uint64_t mOutputPts = 0;
+    bool mImageReady = false;
+
+    // used in vtb callback to saved decoded frame
+    std::vector<uint8_t> mSavedDecodedFrame;
+
+    unsigned int mColorPrimaries = 2;
+    unsigned int mColorRange = 0;
+    unsigned int mColorTransfer = 2;
+    unsigned int mColorSpace = 2;
+
+    std::mutex mFrameLock;
+
+    // videotoolbox stuff
+    // the fmt, this will be recreated each time
+    // we get a sps+pps frames
+    CMFormatDescriptionRef mCmFmtDesc = nullptr;
+    // The VideoToolbox decoder session: this could fail to
+    // create due to incompatible formats coming from android guest
+    VTDecompressionSessionRef mDecoderSession = nullptr;
+    // where the decoded frame is stored
+    CVImageBufferRef mDecodedFrame = nullptr;
+
+private:
+
+};  // MediaVideoToolBoxVideoHelper
+
+}  // namespace emulation
+}  // namespace android
