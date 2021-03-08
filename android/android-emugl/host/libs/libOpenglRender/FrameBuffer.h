e9bab0d23c110dcc8ccfd12886a91860c3d89b38
Sergey Prigogin <sprigogin@google.com>
2021-03-08 13:44:46 -0800

    Revert "Cleanup getScreenshot interface."
    This reverts commit 4ccae5550b3d96c616cd15998f2ea931954f8103.
    
    Change-Id: I90911c8fbef5e2d80ab38033e98284e74d5f095b

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.h
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.h
@@ -560,18 +560,26 @@ public:
     // Fill GLES usage protobuf
     void fillGLESUsages(android_studio::EmulatorGLESUsages*);
 
-    // Saves a screenshot of the previous frame.
+    // Save a screenshot of the previous frame.
     // nChannels should be 3 (RGB) or 4 (RGBA).
-    // You must provide a pre-allocated buffer of sufficient
+    // The provided vector will be resized to the required size.
+    //
+    // Note: swiftshader_indirect does not work with 3 channels
+    void getScreenshot(unsigned int nChannels, unsigned int* width,
+            unsigned int* height, std::vector<unsigned char>& pixels,
+            int displayId, int desiredWidth, int desiredHeight,
+            SkinRotation desiredRotation);
+
+    // Same as above, but you must provide a pre-allocated buffer of sufficient
     // size. Returns 0 on success. In the case of failure and if *cPixels != 0
     // you can call this function again with a buffer of size *cPixels. cPixels
     // should usually be at at least desiredWidth * desiredHeight * nChannels.
     //
-    // In practice the buffer should be > desiredWidth *
+    // Usually you first make a call with *cPixels == 0, to discover how big the
+    // buffer should be. In practice the buffer should be > desiredWidth *
     // desiredHeight * nChannels.
     //
     // Note: Do not call this function again if it fails and *cPixels == 0
-    //  swiftshader_indirect does not work with 3 channels
     int getScreenshot(unsigned int nChannels,
                       unsigned int* width,
                       unsigned int* height,
