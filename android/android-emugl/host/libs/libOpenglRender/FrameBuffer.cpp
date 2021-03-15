b45a4af73a3f7b60754f0e5ab5e1845146390fcf
Erwin Jansen <jansene@google.com>
2021-03-15 12:17:46 -0700

    Revert "Revert "Cleanup getScreenshot interface.""
    This reverts commit e3518b2d0197d69e04d04ff1ce161c81a03534af.
    
    Original commit message:
    
    This removes the std::vector based call from the screenshot api and
    updates the callers of the previous api to use the new array based one.
    
    bug: b/181591846
    Change-Id: I6bdaed57ddae9b32adfd19fb86082e2cb5d1f249

--- android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
+++ android/android-emugl/host/libs/libOpenglRender/FrameBuffer.cpp
@@ -2617,34 +2617,35 @@ static void loadProcOwnedCollection(Stream* stream, Collection* c) {
     });
 }
 
-
-int  FrameBuffer::getScreenshot(unsigned int nChannels, unsigned int* width,
-        unsigned int* height, uint8_t* pixels, size_t* cPixels, int displayId,
-        int desiredWidth, int desiredHeight, SkinRotation desiredRotation) {
+int FrameBuffer::getScreenshot(unsigned int nChannels,
+                               unsigned int* width,
+                               unsigned int* height,
+                               uint8_t* pixels,
+                               size_t* cPixels,
+                               int displayId,
+                               int desiredWidth,
+                               int desiredHeight,
+                               SkinRotation desiredRotation) {
     AutoLock mutex(m_lock);
     uint32_t w, h, cb;
-    if (!emugl::get_emugl_multi_display_operations().getMultiDisplay(displayId,
-                                                                     nullptr,
-                                                                     nullptr,
-                                                                     &w,
-                                                                     &h,
-                                                                     nullptr,
-                                                                     nullptr,
-                                                                     nullptr)) {
-        fprintf(stderr, "Screenshot of invalid display %d", displayId);
+    if (!emugl::get_emugl_multi_display_operations().getMultiDisplay(
+                displayId, nullptr, nullptr, &w, &h, nullptr, nullptr,
+                nullptr)) {
+        LOG(ERROR) << "Screenshot of invalid display: " << displayId;
         *width = 0;
         *height = 0;
         *cPixels = 0;
         return -1;
     }
     if (nChannels != 3 && nChannels != 4) {
-        fprintf(stderr, "Screenshot only support 3(RGB) or 4(RGBA) channels");
+        LOG(ERROR) << "Screenshot only support 3(RGB) or 4(RGBA) channels";
         *width = 0;
         *height = 0;
         *cPixels = 0;
         return -1;
     }
-    emugl::get_emugl_multi_display_operations().getDisplayColorBuffer(displayId, &cb);
+    emugl::get_emugl_multi_display_operations().getDisplayColorBuffer(displayId,
+                                                                      &cb);
     if (displayId == 0) {
         cb = m_lastPostedColorBuffer;
     }
@@ -2665,7 +2666,9 @@ int  FrameBuffer::getScreenshot(unsigned int nChannels, unsigned int* width,
         return -2;
     }
     *cPixels = needed;
-    if (desiredRotation == SKIN_ROTATION_90 || desiredRotation == SKIN_ROTATION_270) {
+
+    if (desiredRotation == SKIN_ROTATION_90 ||
+        desiredRotation == SKIN_ROTATION_270) {
         std::swap(*width, *height);
     }
 
@@ -2682,24 +2685,6 @@ int  FrameBuffer::getScreenshot(unsigned int nChannels, unsigned int* width,
 
     sendPostWorkerCmd(scrCmd);
     return 0;
-}
-
-
-void FrameBuffer::getScreenshot(unsigned int nChannels, unsigned int* width,
-        unsigned int* height, std::vector<unsigned char>& pixels, int displayId,
-        int desiredWidth, int desiredHeight, SkinRotation desiredRotation) {
-
-    size_t size = pixels.size();
-    if (getScreenshot(nChannels, width, height, pixels.data(), &size, displayId, desiredWidth, desiredHeight, desiredRotation) != 0)  {
-        if (size == 0) {
-            // Fatal error..
-            return;
-        }
-
-        // Buffer to small, increase the vector size.
-        pixels.resize(size);
-        getScreenshot(nChannels, width, height, pixels.data(), &size, displayId, desiredWidth, desiredHeight, desiredRotation);
-    }
 }
 
 void FrameBuffer::onLastColorBufferRef(uint32_t handle) {
