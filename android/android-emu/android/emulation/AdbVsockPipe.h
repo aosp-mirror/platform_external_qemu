f2992c3894ef1cd827f661b93e7f6246bfd2cb8b
Roman Kiryanov <rkir@google.com>
2021-03-14 17:44:47 -0700

    Do not call `~AdbVsockPipe` under `mPipesMtx`
    To destroy `AdbVsockPipe` the vsock must be
    closed which reqires aquiring the
    `Big Qemu Lock`. Snapshot saving/loading
    happens with the `Big Qemu Lock` aquired
    and then it aquires `mPipesMtx` to save the
    `AdbVsockPipe` state.
    
    If `~AdbVsockPipe` is called under `mPipesMtx`
    this could cause a deadlock.
    
    Bug: 182181736
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: Id012c50e5d64b152c8da21ac9532d77dfee06864

--- android/android-emu/android/emulation/AdbVsockPipe.h
+++ android/android-emu/android/emulation/AdbVsockPipe.h
@@ -90,16 +90,15 @@ public:
                  android::base::ScopedSocket socket,
                  AdbPortType portType);
 
-    ~AdbVsockPipe();
-
     struct Proxy {
         enum class EventBits {
             None = 0,
-            CloseSocket = 1u << 0,
-            WantWrite = 1u << 1,
-            DontWantWrite = 1u << 2,
-            WantRead = 1u << 3,
-            DontWantRead = 1u << 4,
+            HostClosed = 1u << 0,
+            GuestClosed = 1u << 1,
+            WantWrite = 1u << 2,
+            DontWantWrite = 1u << 3,
+            WantRead = 1u << 4,
+            DontWantRead = 1u << 5,
         };
 
         virtual ~Proxy() {}
