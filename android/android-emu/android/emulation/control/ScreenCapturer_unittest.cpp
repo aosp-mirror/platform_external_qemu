b78105f50bedf7fd3d1ec4df2ad59c78f4cd792c
Weilun Du <wdu@google.com>
2021-03-24 16:06:17 -0700

    [Embedded Emulator] Foldable Support
    When the screen is folded, getScreenshot() method will return an resized
    image based on the requested width and height while keeping the aspect
    ratio of the folded screen instead of the unfolded screen.
    
    The idea is the same as snipping a rectangle within the unfolded screen
    but we need to also consider rotation and reszing. There are 4 steps:
    
    1, Calculate the scaling factor. Get new width, height for the entire screen
    and compute the rectangle to snip.
    2, Apply rotation to image and transform xOffset and yOffset in the rectangle
    if necessary.
    3, Resize based on new width and height.
    4, Clip a rectangle from the resized screen.
    
    Test: Manual. "adb emu fold" and "adb emu rotate" must work properly.
    Bug: 171515275
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: I08bd318976e5060678eb184b93268f0297baaa02

--- android/android-emu/android/emulation/control/ScreenCapturer_unittest.cpp
+++ android/android-emu/android/emulation/control/ScreenCapturer_unittest.cpp
@@ -234,17 +234,29 @@ public:
     }
     void fillGLESUsages(android_studio::EmulatorGLESUsages*) { }
 
-    int getScreenshot(unsigned int nChannels, unsigned int* width,
-        unsigned int* height, uint8_t* pixels, size_t* cPixels, int displayId = 0,
-        int desiredWidth = 0, int desiredHeight = 0,
-        SkinRotation desiredRotation = SKIN_ROTATION_0) {
-            if (mHasValidScreenshot) {
-            if (desiredWidth == 0 && desiredHeight == 0) {
-                *width = kWidth;
-                *height = kHeight;
+    int getScreenshot(unsigned int nChannels,
+                      unsigned int* width,
+                      unsigned int* height,
+                      uint8_t* pixels,
+                      size_t* cPixels,
+                      int displayId = 0,
+                      int desiredWidth = 0,
+                      int desiredHeight = 0,
+                      SkinRotation desiredRotation = SKIN_ROTATION_0,
+                      SkinRect rect = {{0, 0}, {0, 0}}) {
+        if (mHasValidScreenshot) {
+            bool useSnipping = (rect.size.w != 0 && rect.size.h != 0);
+            if (useSnipping) {
+                *width = rect.size.w;
+                *height = rect.size.h;
             } else {
-                *width = desiredWidth;
-                *height = desiredHeight;
+                if (desiredWidth == 0 && desiredHeight == 0) {
+                    *width = kWidth;
+                    *height = kHeight;
+                } else {
+                    *width = desiredWidth;
+                    *height = desiredHeight;
+                }
             }
             size_t needed = *width * *height * nChannels;
             if (*cPixels < needed) {
