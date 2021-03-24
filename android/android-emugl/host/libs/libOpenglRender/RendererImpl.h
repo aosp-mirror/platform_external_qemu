5204788c322273be89d2a8f4de7f96669b646472
Weilun Du <wdu@google.com>
2021-03-24 16:06:17 -0700

    [Embedded Emulator] Foldable Support
    When the screen is folded, getScreenshot() method will return an resized
    image based on the requested width and height while keeping the aspect
    ratio of the folded screen instead of the unfolded screen.
    
    The idea is the same as snipping a rectangle within the unfolded screen
    but we need to also consider rotation and reszing. There are 4 steps:
    
    1, Calculate the scaling factor. Get new width, height for the entire
    screen and compute the rectangle to snip.
    2, Apply rotation to image and transform xOffset and yOffset in the
    rectangle if necessary.
    3, Resize based on new width and height.
    4, Clip a rectangle from the resized screen.
    
    Test: Manual. "adb emu fold" and "adb emu rotate" must work properly.
    Bug: 171515275
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: I08bd318976e5060678eb184b93268f0297baaa02
    (cherry picked from commit 0b0b34328292133fbd99ba08cef0346d31e08926)

--- android/android-emugl/host/libs/libOpenglRender/RendererImpl.h
+++ android/android-emugl/host/libs/libOpenglRender/RendererImpl.h
@@ -105,10 +105,16 @@ public:
     bool load(android::base::Stream* stream,
               const android::snapshot::ITextureLoaderPtr& textureLoader) final;
     void fillGLESUsages(android_studio::EmulatorGLESUsages*) final;
-    int getScreenshot(unsigned int nChannels, unsigned int* width,
-            unsigned int* height, uint8_t *pixels, size_t *cPixels,
-            int displayId, int desiredWidth, int desiredHeight,
-            SkinRotation desiredRotation) final;
+    int getScreenshot(unsigned int nChannels,
+                      unsigned int* width,
+                      unsigned int* height,
+                      uint8_t* pixels,
+                      size_t* cPixels,
+                      int displayId,
+                      int desiredWidth,
+                      int desiredHeight,
+                      SkinRotation desiredRotation,
+                      SkinRect rect) final;
 
     void snapshotOperationCallback(
             android::snapshot::Snapshotter::Operation op,
