7d8a41bd4ee45308f246fb5ab724a4772db058b9
Lingfeng Yang <lfy@google.com>
2021-03-31 01:34:49 +0000

    Revert "Replace the MessageChannel with one with dynamic size"
    This reverts commit d30f8f0c860dd4d53a2a8e212ad9d703b390edf8.
    
    Reason for revert: test revert for b/184087530
    
    Change-Id: I817a3a560a9f9b99ffd9e8175be1b167104dafd7

--- android/android-emu/android/emulation/AdbVsockPipe.cpp
+++ android/android-emu/android/emulation/AdbVsockPipe.cpp
@@ -219,7 +219,7 @@ AdbVsockPipe::Service::~Service() {
 
     stopPollGuestAdbdThread(kAdbdPollingThreadDisabled);
 
-    destroyPipe(nullptr);
+    mPipesToDestroy.stop();
     mDestroyPipesThread.join();
 }
 
@@ -249,36 +249,28 @@ void AdbVsockPipe::Service::onHostConnection(android::base::ScopedSocket&& socke
 void AdbVsockPipe::Service::destroyPipesThreadLoop() {
     while (true) {
         AdbVsockPipe *toDestroy;
-        {
-            std::unique_lock<std::mutex> guard(mPipesToDestroyMtx);
-            mPipesToDestroyCv.wait(guard, [this](){ return !mPipesToDestroy.empty(); });
-            toDestroy = mPipesToDestroy.front();
-            mPipesToDestroy.pop_front();
-        }
-        if (!toDestroy) {
+        if (mPipesToDestroy.receive(&toDestroy)) {
+            std::lock_guard<std::mutex> guard(mPipesMtx);
+
+            const auto end = std::remove_if(
+                mPipes.begin(), mPipes.end(),
+                [toDestroy](const std::unique_ptr<AdbVsockPipe> &pipe){
+                    return toDestroy == pipe.get();
+                });
+
+            mPipes.erase(end, mPipes.end());
+            if (mPipes.empty()) {
+                stopPollGuestAdbdThread(kAdbdPollingThreadIdle);
+                startPollGuestAdbdThread();
+            }
+        } else {
             break;
-        }
-
-        std::lock_guard<std::mutex> guard(mPipesMtx);
-
-        const auto end = std::remove_if(
-            mPipes.begin(), mPipes.end(),
-            [toDestroy](const std::unique_ptr<AdbVsockPipe> &pipe){
-                return toDestroy == pipe.get();
-            });
-
-        mPipes.erase(end, mPipes.end());
-        if (mPipes.empty()) {
-            stopPollGuestAdbdThread(kAdbdPollingThreadIdle);
-            startPollGuestAdbdThread();
         }
     }
 }
 
 void AdbVsockPipe::Service::destroyPipe(AdbVsockPipe *p) {
-    std::lock_guard<std::mutex> guard(mPipesToDestroyMtx);
-    mPipesToDestroy.push_back(p);
-    mPipesToDestroyCv.notify_one();
+    mPipesToDestroy.send(p);
 }
 
 void AdbVsockPipe::Service::pollGuestAdbdThreadLoop() {
