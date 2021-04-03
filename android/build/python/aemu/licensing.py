b391816cc67af41be2d34a4b57b6794b2455f684
Erwin Jansen <jansene@google.com>
2021-04-03 00:26:02 -0700

    Build webrtc from source.
    We now build WebRTC from source, and fix any compilation issues that
    arose due to the new import.
    
    Bug: 184770746
    Test: Increased test coverage from unit tests
    Test: Can connect to container with WebRTC enabled in browser.
    Change-Id: Ia21727363aacc65a67e6ad706023a192ba7d3f11

--- android/build/python/aemu/licensing.py
+++ android/build/python/aemu/licensing.py
@@ -192,6 +192,8 @@ class Licensing(object):
             and (" " not in target)
             and (not target.startswith("-"))
             and ("NOTFOUND" not in target)
+            and (not target.startswith('webrtc'))
+            and (not target.startswith('absl'))
             and (target not in Licensing.common_local_libs)
         )
 
