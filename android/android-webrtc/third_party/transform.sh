dbeeef31991dae34664a5e281eb90e2f2ed01b52
Erwin Jansen <jansene@google.com>
2021-04-03 00:26:02 -0700

    Build webrtc from source.
    We now compile WebRTC from source. The CMakeFiles have been auto
    generated by running:
    
    $ cd external/qemu/android/android-webrtc/third_party && ./transform.sh
    
    This generates the WebRTC build files based on the autogenerated WebRTC
    build found in google3. The build includes all the requirements under
    the third_party branch, and includes a large set of unit tests to
    validate the behavior.
    
    We generate all requirements for all targets. Linux arm does not yet
    build completely with gcc.
    
    Bug: 184770746
    Test: Increased test coverage from unit tests
    Test: Can connect to container with WebRTC enabled in browser.
    Change-Id: Ia21727363aacc65a67e6ad706023a192ba7d3f11

---
+++ android/android-webrtc/third_party/transform.sh
@@ -0,0 +1,27 @@
+# Convert build
+
+for platform in Darwin Darwin-aarch64 Linux-aarch64 Linux Windows; do
+    python ./import-webrtc.py \
+        --target webrtc_api_video_codecs_builtin_video_decoder_factory \
+        --target webrtc_api_video_codecs_builtin_video_encoder_factory \
+        --target webrtc_api_libjingle_peerconnection_api \
+        --target webrtc_pc_peerconnection \
+        --target webrtc_api_create_peerconnection_factory \
+        --target webrtc_api_audio_codecs_builtin_audio_decoder_factory \
+        --target webrtc_api_audio_codecs_builtin_audio_encoder_factory \
+        --target webrtc_common_audio_common_audio_unittests \
+        --target webrtc_common_video_common_video_unittests \
+        --target webrtc_media_rtc_media_unittests \
+        --target webrtc_modules_audio_coding_audio_decoder_unittests \
+        --target webrtc_pc_peerconnection_unittests \
+        --target webrtc_pc_rtc_pc_unittests \
+        --root ~/src/webrtc_g3 \
+        --platform $platform BUILD webrtc
+
+        # These tests require a large set of dependencies, but do not introduce new tests.
+        # --target webrtc__rtc_unittests \
+        # --target webrtc__video_engine_tests \
+        # --target webrtc__webrtc_perf_tests \
+        # --target webrtc__webrtc_nonparallel_tests \
+        # --target webrtc__voip_unittests \
+done
