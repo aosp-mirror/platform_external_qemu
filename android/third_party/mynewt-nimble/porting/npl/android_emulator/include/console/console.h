184146a166f4b78ca64d0fa34b92e4945d10006e
Erwin Jansen <jansene@google.com>
2021-04-28 15:49:13 -0700

    [Bluetooth] Add os abstraction layer for nibmle
    This adds an os abstraction layer that enables nimble to interact with
    rootcanal. It currently expects root canal to be available locally on
    port 6402.
    
    The project can be built independently from the emulator if desired.
    A vscode project is included to assist with development.
    
    It includes a set of examples that can be run against root canal.
    
    for example:
    
    ./objs/rootcanal
    ./objs/nimble_advertiser
    
    Note: We do not run the tests under asan, as we are currently missing
    a proper mechanism to cleanly shutdown the bluetooth stack, see:
    b/186763119
    
    Bug: 186680651
    Change-Id: Ic51d91a38bb41729d89cab0cfda64fa6f58f7868

--- android/third_party/mynewt-nimble/porting/npl/android_emulator/include/console/console.h
+++ android/third_party/mynewt-nimble/porting/npl/android_emulator/include/console/console.h
@@ -17,6 +17,19 @@
  * under the License.
  */
 
-// Most of the functions here have moved to os_sysinit.cc due to linker dependencies.
-// This is only left in the tree to resolve future merge conflicts.
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
 
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define console_printf(_fmt, ...) printf(_fmt, ##__VA_ARGS__)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONSOLE_H__ */
