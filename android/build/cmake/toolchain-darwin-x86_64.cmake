32224f279ceaf32c64573c67057001a9c04d29f8
Erwin Jansen <jansene@google.com>
2021-05-22 03:03:28 +0000

    Revert "Enable cross compilation to darwin arm64"
    This reverts commit 2888131c14578bb0423b6021add4e617e08cd931.
    
    Reason for revert: build break.
    
    Change-Id: I4ce932290f564084174ae47588e2e5b89831a76a

--- android/build/cmake/toolchain-darwin-x86_64.cmake
+++ android/build/cmake/toolchain-darwin-x86_64.cmake
@@ -29,20 +29,58 @@ set(RUNTIME_OS_PROPERTIES
 )
 
 if(NOT APPLE)
+  # We will use the osx cross compiler, and we expect it to be in /opt/osxcross/bin
+  set(OSXCROSS_TARGET_DIR /opt/osxcross/osxcross)
+
+  set(OSXCROSS_HOST x86_64-apple-darwin19)
   set(CMAKE_SYSTEM_NAME "Darwin")
-  toolchain_generate("darwin-x86_64")
+  string(REGEX REPLACE "-.*" "" CMAKE_SYSTEM_PROCESSOR "${OSXCROSS_HOST}")
+
+  # Detect sdk version
+  foreach(sdk_version 10.13 10.14 10.15)
+    if(EXISTS "${OSXCROSS_TARGET_DIR}/SDK/MacOSX${sdk_version}.sdk")
+        set(CMAKE_OSX_DEPLOYMENT_TARGET ${sdk_version})
+    endif()
+  endforeach()
+
+  # Detect cross compiler name, needed for linker config.
+  foreach(darwin_ver 16 17 18 19)
+    if(EXISTS "${OSXCROSS_TARGET_DIR}/bin/x86_64-apple-darwin${darwin_ver}-clang")
+       set(OSXCROSS_HOST x86_64-apple-darwin${darwin_ver})
+    endif()
+  endforeach()
+
+  message(STATUS "Using MacOS SDK ${CMAKE_OSX_DEPLOYMENT_TARGET}, and compiler host ${OSXCROSS_HOST}")
+  # specify the cross compiler
+  set(CMAKE_C_COMPILER "${OSXCROSS_TARGET_DIR}/bin/o64-clang")
+  set(CMAKE_CXX_COMPILER "${OSXCROSS_TARGET_DIR}/bin/o64-clang++")
+
+  # where is the target environment set(CMAKE_FIND_ROOT_PATH "${OSXCROSS_SDK}"
+  # "${OSXCROSS_TARGET_DIR}/macports/pkgs/opt/local")
+
   # search for programs in the build host directories
   set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
   # for libraries and headers in the target directories
   set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
   set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
   set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+  set(CMAKE_AR
+      "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-ar"
+      CACHE FILEPATH "ar")
+  set(CMAKE_RANLIB
+      "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-ranlib"
+      CACHE FILEPATH "ranlib")
+  set(CMAKE_INSTALL_NAME_TOOL
+      "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-install_name_tool"
+      CACHE FILEPATH "install_name_tool")
+  set(CMAKE_STRIP_CMD "${OSXCROSS_TARGET_DIR}/bin/${OSXCROSS_HOST}-strip")
+
   set(ENV{PKG_CONFIG_LIBDIR} "${OSXCROSS_TARGET_DIR}/macports/pkgs/opt/local/lib/pkgconfig")
   set(ENV{PKG_CONFIG_SYSROOT_DIR} "${OSXCROSS_TARGET_DIR}/macports/pkgs")
-
+  add_definitions(-DMACOSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
   set(CMAKE_INSTALL_DO_STRIP TRUE)
 else()
-
   toolchain_generate("darwin-x86_64")
   # Configure how we strip executables.
   set(CMAKE_STRIP_CMD "${CMAKE_STRIP} -S")
