f6016762c3f90f6eebeb9344d8f79629df8e0008
Lingfeng Yang <lfy@google.com>
2021-07-23 10:18:26 -0700

    (🍒) Remove extra RendererFlags def
    Original-Change-Id: I7d9c70ddc1451608e6504e69009ca1df69dca17b
    Change-Id: I3ff10359fbf57363359720ae98862b9b811d311d

--- android/android-emugl/combined/GfxStreamBackend.h
+++ android/android-emugl/combined/GfxStreamBackend.h
@@ -9,18 +9,6 @@ enum BackendFlags {
     GFXSTREAM_BACKEND_FLAGS_EGL2EGL_BIT = 1 << 1,
 };
 
-// based on VIRGL_RENDERER_USE* and friends
-enum RendererFlags {
-    GFXSTREAM_RENDERER_FLAGS_USE_EGL_BIT = 1 << 0,
-    GFXSTREAM_RENDERER_FLAGS_THREAD_SYNC = 1 << 1,
-    GFXSTREAM_RENDERER_FLAGS_USE_GLX_BIT = 1 << 2,
-    GFXSTREAM_RENDERER_FLAGS_USE_SURFACELESS_BIT = 1 << 3,
-    GFXSTREAM_RENDERER_FLAGS_USE_GLES_BIT = 1 << 4,
-    GFXSTREAM_RENDERER_FLAGS_NO_VK_BIT = 1 << 5, // for disabling vk
-
-    GFXSTREAM_RENDERER_FLAGS_NO_SYNCFD_BIT = 1 << 20, // for disabling syncfd
-};
-
 extern "C" VG_EXPORT void gfxstream_backend_init(
     uint32_t display_width,
     uint32_t display_height,
