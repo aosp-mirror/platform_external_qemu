3ced8aa14f2457e4edabe88a518bc0035968c262
Joe Komlodi <komlodi@google.com>
2021-10-05 12:02:31 -0700

    hw/ipmi: nuvoton-host-pcimbx: Add mailbox read/write functions
    These functions handle receiving commands from the BMC NPCM7xx device, as well
    as being able to format and send our own messages to the BMC device.
    
    Google-Bug-Id: 201426782
    Google-Rebase-Count: 6
    Google-Bug-Id: 262945337
    PiperOrigin-RevId: 401043178
    Signed-off-by: Joe Komlodi <komlodi@google.com>
    Change-Id: Ibe847775a4753cb1bed30aa80eed0d01e642c621

--- hw/ipmi/nuvoton_pci_ipmi_kcs.c
+++ hw/ipmi/nuvoton_pci_ipmi_kcs.c
@@ -19,13 +19,14 @@
 #include "qapi/error.h"
 #include "qom/object.h"
 #include "migration/vmstate.h"
+#include "chardev/char-fe.h"
 #include "hw/pci/pci.h"
 #include "hw/pci/pci_device.h"
 #include "hw/isa/isa.h"
 #include "hw/sysbus.h"
 #include "hw/ipmi/ipmi_kcs.h"
 #include "hw/irq.h"
-#include "hw/qdev-properties.h"
+#include "hw/qdev-properties-system.h"
 #include "hw/registerfields.h"
 #include "hw/pci/pci_regs.h"
 
@@ -39,6 +40,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(NuvotonPCIIPMIState, NUVOTON_PCI_IPMI)
 #define TYPE_NUVOTON_IPMI_KCS "nuvoton-ipmi-kcs"
 OBJECT_DECLARE_SIMPLE_TYPE(NuvotonIPMIKCSState, NUVOTON_IPMI_KCS)
 
+#define NUVOTON_PCI_MBX_BAR_SIZE  0x4000
 #define NUVOTON_PCI_MBX_REG_START 0x4000
 #define NUVOTON_PCI_MBX_MMIO_SIZE 0x8000
 
@@ -100,6 +102,24 @@ typedef enum {
     NUVOTON_HMBXCMD_RST  = 0x00,
 } NuvotonPCIIPMIMbxResets;
 
+typedef enum {
+    NUVOTON_PCI_IPMI_STATE_IDLE,
+    NUVOTON_PCI_IPMI_STATE_OFFSET,
+    NUVOTON_PCI_IPMI_STATE_SIZE,
+    NUVOTON_PCI_IPMI_STATE_DATA,
+} NuvotonPCIIPMIHostState;
+
+typedef enum {
+    NUVOTON_PCI_IPMI_CODE_OK           = 0x00,
+    NUVOTON_PCI_IPMI_CODE_OP_INVALID   = 0xa0,
+    NUVOTON_PCI_IPMI_CODE_SIZE_INVALID = 0xa1,
+} NuvotonPCIIPMICode;
+
+typedef enum {
+    NUVOTON_PCI_IPMI_OP_READ    = 0x01,
+    NUVOTON_PCI_IPMI_OP_WRITE   = 0x02,
+} NuvotonPCIIPMIOp;
+
 typedef struct NuvotonIPMIKCSState NuvotonIPMIKCSState;
 typedef struct NuvotonPCIIPMIState NuvotonPCIIPMIState;
 
@@ -108,6 +128,7 @@ struct NuvotonPCIIPMIState {
     NuvotonIPMIKCSState *ipmi_kcs_state;
 
     struct {
+        uint8_t  ram[NUVOTON_PCI_MBX_BAR_SIZE];
         uint32_t hmbxstat;
         uint32_t hmbxctl;
         uint32_t hmbxcmd;
@@ -115,6 +136,19 @@ struct NuvotonPCIIPMIState {
         qemu_irq irq_host[8];
         qemu_irq wire_bmc_cif[8];
         MemoryRegion iomem;
+
+        /* Variables for TX/RXing BMC data */
+        CharBackend chr;
+        NuvotonPCIIPMIHostState state;
+        uint8_t op;
+        hwaddr offset;
+        size_t size;
+        uint64_t data;
+        size_t rx_cnt;
+
+        hwaddr tx_addr;
+        size_t tx_size;
+        uint64_t tx_data;
     } pci_mbx;
 };
 
@@ -258,6 +292,25 @@ static void nuvoton_pcimbx_bmc_update_irq(NuvotonPCIIPMIState *s)
     }
 }
 
+static uint64_t nuvoton_pci_ipmi_mbx_ram_read(NuvotonPCIIPMIState *s,
+                                              hwaddr addr,
+                                              unsigned size)
+
+{
+    switch(size) {
+    case 1:
+        return *(uint8_t *)&s->pci_mbx.ram[addr];
+    case 2:
+        return *(uint16_t *)&s->pci_mbx.ram[addr];
+    case 4:
+        return *(uint32_t *)&s->pci_mbx.ram[addr];
+    case 8:
+        return *(uint64_t *)&s->pci_mbx.ram[addr];
+    default:
+        g_assert_not_reached();
+    }
+}
+
 static uint64_t nuvoton_pci_ipmi_mbx_reg_read(NuvotonPCIIPMIState *s,
                                               hwaddr addr)
 {
@@ -280,13 +333,15 @@ static uint64_t nuvoton_pci_ipmi_mbx_read(void *opaque, hwaddr addr,
                                           unsigned size)
 {
     NuvotonPCIIPMIState *s = NUVOTON_PCI_IPMI(opaque);
+    uint64_t data;
 
     if (addr < NUVOTON_PCI_MBX_REG_START) {
-        /* TODO(b/201426782): Handle reads to mailbox RAM region */
-        return 0;
+        data = nuvoton_pci_ipmi_mbx_ram_read(s, addr, size);
     } else {
-        return nuvoton_pci_ipmi_mbx_reg_read(s, addr);
+        data = nuvoton_pci_ipmi_mbx_reg_read(s, addr);
     }
+
+    return data;
 }
 
 static void nuvoton_pci_ipmi_hmbxstat_w(NuvotonPCIIPMIState *s, uint64_t val)
@@ -328,6 +383,79 @@ static void nuvoton_pci_ipmi_hmbxcmd_w(NuvotonPCIIPMIState *s, uint64_t val)
     nuvoton_pcimbx_bmc_update_irq(s);
 }
 
+static void nuvoton_pci_ipmi_mbx_tx_hdr(NuvotonPCIIPMIState *s)
+{
+    struct packet {
+        union {
+            struct {
+                uint8_t op;
+                uint64_t offset;
+                uint8_t size;
+            } __attribute__((packed));
+            uint8_t bytes[10];
+        };
+    } pkt = {
+        .op = NUVOTON_PCI_IPMI_OP_WRITE,
+        .offset = s->pci_mbx.tx_addr,
+        .size = s->pci_mbx.tx_size,
+    };
+
+    /*
+     * The BMC chardev can only handle 64-bits at a time, so we need at least
+     * 2 transactions to send the header.
+     */
+    qemu_chr_fe_write_all(&s->pci_mbx.chr, pkt.bytes, 5);
+    qemu_chr_fe_write_all(&s->pci_mbx.chr, &pkt.bytes[5], 5);
+}
+
+static void nuvoton_pci_ipmi_mbx_tx(NuvotonPCIIPMIState *s)
+{
+    /*
+     * BMC expects no more than 64-bits of data at a time, and our MMIO region
+     * also only accepts at most 64-bits at a time. Validate that sanity here.
+     */
+    assert(s->pci_mbx.size <= 8);
+
+    nuvoton_pci_ipmi_mbx_tx_hdr(s);
+    qemu_chr_fe_write(&s->pci_mbx.chr, (uint8_t *)&s->pci_mbx.tx_data,
+                              s->pci_mbx.tx_size);
+}
+
+static void nuvoton_pci_ipmi_mbx_ram_write(NuvotonPCIIPMIState *s,
+                                           hwaddr addr, uint64_t data,
+                                           unsigned size)
+
+{
+    switch(size) {
+    case 1:
+        s->pci_mbx.ram[addr] = (uint8_t)data;
+        break;
+    case 2:
+        *(uint16_t *)&s->pci_mbx.ram[addr] = (uint16_t)data;
+        break;
+    case 4:
+        *(uint32_t *)&s->pci_mbx.ram[addr] = (uint32_t)data;
+        break;
+    case 8:
+        *(uint64_t *)&s->pci_mbx.ram[addr] = data;
+        break;
+    default:
+        g_assert_not_reached();
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_write_and_tx(NuvotonPCIIPMIState *s,
+                                                     hwaddr addr, uint64_t data,
+                                                     unsigned size)
+{
+    s->pci_mbx.tx_addr = addr;
+    s->pci_mbx.tx_size = size;
+    s->pci_mbx.tx_data = data;
+
+    nuvoton_pci_ipmi_mbx_ram_write(s, addr, data, size);
+    nuvoton_pci_ipmi_mbx_tx(s);
+}
+
 static void nuvoton_pci_ipmi_mbx_reg_write(NuvotonPCIIPMIState *s, hwaddr addr,
                                            uint64_t val64)
 {
@@ -355,7 +483,7 @@ static void nuvoton_pci_ipmi_mbx_write(void *opaque, hwaddr addr,
     NuvotonPCIIPMIState *s = NUVOTON_PCI_IPMI(opaque);
 
     if (addr < NUVOTON_PCI_MBX_REG_START) {
-        /* TODO(b/201426782): Handle writes to mailbox RAM region */
+        nuvoton_pci_ipmi_mbx_write_and_tx(s, addr, val64, size);
     } else {
         nuvoton_pci_ipmi_mbx_reg_write(s, addr, val64);
     }
@@ -384,6 +512,157 @@ static const struct MemoryRegionOps nuvoton_pci_ipmi_mbx_ops = {
         .accepts = nuvoton_pci_ipmi_mbx_check_mem_op,
     },
 };
+
+static void nuvoton_pci_ipmi_mbx_send_response(NuvotonPCIIPMIState *s,
+                                               uint8_t code)
+{
+    qemu_chr_fe_write(&s->pci_mbx.chr, &code, 1);
+    if (code == NUVOTON_PCI_IPMI_CODE_OK &&
+        s->pci_mbx.op == NUVOTON_PCI_IPMI_OP_READ) {
+        qemu_chr_fe_write(&s->pci_mbx.chr, (uint8_t *)&s->pci_mbx.data,
+                          s->pci_mbx.size);
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_read(NuvotonPCIIPMIState *s)
+{
+    s->pci_mbx.data = nuvoton_pci_ipmi_mbx_ram_read(s, s->pci_mbx.offset,
+                                                    s->pci_mbx.size);
+    nuvoton_pci_ipmi_mbx_send_response(s, MEMTX_OK);
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_write(NuvotonPCIIPMIState *s)
+{
+    nuvoton_pci_ipmi_mbx_ram_write(s, s->pci_mbx.offset, s->pci_mbx.data,
+                                   s->pci_mbx.size);
+    nuvoton_pci_ipmi_mbx_send_response(s, MEMTX_OK);
+}
+
+static void nuvoton_pci_ipmi_mbx_decode_op(NuvotonPCIIPMIState *s,
+                                           NuvotonPCIIPMIOp op)
+{
+    switch (op) {
+    case NUVOTON_PCI_IPMI_OP_READ:
+    case NUVOTON_PCI_IPMI_OP_WRITE:
+        s->pci_mbx.op = op;
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_OFFSET;
+        s->pci_mbx.offset = 0;
+        s->pci_mbx.rx_cnt = 0;
+        break;
+
+    default:
+        qemu_log_mask(LOG_GUEST_ERROR, "Received unknown op: 0x%x\n", op);
+        nuvoton_pci_ipmi_mbx_send_response(s, NUVOTON_PCI_IPMI_CODE_OP_INVALID);
+        break;
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_idle(NuvotonPCIIPMIState *s,
+                                             uint8_t byte)
+{
+    /* We received an ACK from a write we did, do nothing */
+    if (byte == 0x00) {
+        return;
+    }
+
+    /* Otherwise it's an op */
+    nuvoton_pci_ipmi_mbx_decode_op(s, byte);
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_size(NuvotonPCIIPMIState *s,
+                                             uint8_t size)
+{
+    s->pci_mbx.size = size;
+
+    if (s->pci_mbx.size < 1 || s->pci_mbx.size > sizeof(s->pci_mbx.data)) {
+        nuvoton_pci_ipmi_mbx_send_response(s,
+                                           NUVOTON_PCI_IPMI_CODE_SIZE_INVALID);
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_IDLE;
+    } else if (s->pci_mbx.op == NUVOTON_PCI_IPMI_OP_READ) {
+        nuvoton_pci_ipmi_mbx_handle_read(s);
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_IDLE;
+    } else {
+        s->pci_mbx.rx_cnt = 0;
+        s->pci_mbx.data = 0;
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_DATA;
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_data(NuvotonPCIIPMIState *s,
+                                             uint8_t byte)
+{
+    s->pci_mbx.data += (uint64_t)byte <<
+                       (s->pci_mbx.rx_cnt * (sizeof(uint8_t) * 8));
+
+    s->pci_mbx.rx_cnt++;
+    if (s->pci_mbx.rx_cnt >= s->pci_mbx.size) {
+        nuvoton_pci_ipmi_mbx_handle_write(s);
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_IDLE;
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_handle_offset(NuvotonPCIIPMIState *s,
+                                               uint8_t offset)
+{
+    s->pci_mbx.offset += offset <<
+                         (s->pci_mbx.rx_cnt * (sizeof(uint8_t) * 8));
+    s->pci_mbx.rx_cnt++;
+    if (s->pci_mbx.rx_cnt >= sizeof(s->pci_mbx.size)) {
+        s->pci_mbx.state = NUVOTON_PCI_IPMI_STATE_SIZE;
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_receive_byte(NuvotonPCIIPMIState *s,
+                                              uint8_t byte)
+{
+    switch (s->pci_mbx.state) {
+    case NUVOTON_PCI_IPMI_STATE_IDLE:
+        nuvoton_pci_ipmi_mbx_handle_idle(s, byte);
+        break;
+    case NUVOTON_PCI_IPMI_STATE_OFFSET:
+        nuvoton_pci_ipmi_mbx_handle_offset(s, byte);
+        break;
+    case NUVOTON_PCI_IPMI_STATE_SIZE:
+        nuvoton_pci_ipmi_mbx_handle_size(s, byte);
+        break;
+    case NUVOTON_PCI_IPMI_STATE_DATA:
+        nuvoton_pci_ipmi_mbx_handle_data(s, byte);
+        break;
+    default:
+        g_assert_not_reached();
+    }
+}
+
+static void nuvoton_pci_ipmi_mbx_event(void *opaque, QEMUChrEvent evt)
+{
+    switch (evt) {
+    case CHR_EVENT_OPENED:
+    case CHR_EVENT_CLOSED:
+    case CHR_EVENT_BREAK:
+    case CHR_EVENT_MUX_IN:
+    case CHR_EVENT_MUX_OUT:
+        /* Ignore events */
+        break;
+    default:
+        g_assert_not_reached();
+    }
+}
+
+static int nuvoton_pci_ipmi_mbx_can_receive(void *opaque)
+{
+    return 1;
+}
+
+static void nuvoton_pci_ipmi_mbx_receive(void *opaque, const uint8_t *buf,
+                                         int size)
+{
+    size_t i;
+    NuvotonPCIIPMIState *s = NUVOTON_PCI_IPMI(opaque);
+
+    for (i = 0; i < size; i++) {
+        nuvoton_pci_ipmi_mbx_receive_byte(s, buf[i]);
+    }
+}
 
 static void nuvoton_pci_ipmi_reset(NuvotonPCIIPMIState *s)
 {
@@ -433,6 +712,11 @@ static void nuvoton_pci_ipmi_realize(PCIDevice *pd, Error **errp)
     pci_config_set_prog_interface(pd->config, 0x01);
     pci_config_set_interrupt_pin(pd->config, 0x01);
     pci_register_bar(pd, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->pci_mbx.iomem);
+
+    qemu_chr_fe_set_handlers(&s->pci_mbx.chr, nuvoton_pci_ipmi_mbx_can_receive,
+                             nuvoton_pci_ipmi_mbx_receive,
+                             nuvoton_pci_ipmi_mbx_event,
+                             NULL, OBJECT(s), NULL, true);
 }
 
 static void nuvoton_ipmi_kcs_realize(DeviceState *dev, Error **errp)
@@ -494,6 +778,7 @@ static const VMStateDescription vmstate_nuvoton_ipmi_kcs = {
 };
 
 static Property nuvoton_pci_ipmi_properties[] = {
+    DEFINE_PROP_CHR("bmc-chardev", NuvotonPCIIPMIState, pci_mbx.chr),
     DEFINE_PROP_LINK("ipmi-kcs", NuvotonPCIIPMIState, ipmi_kcs_state,
                      TYPE_NUVOTON_IPMI_KCS, NuvotonIPMIKCSState *),
     DEFINE_PROP_END_OF_LIST(),
