joyxu/vulkan-cereal

Always generate optional decoder logging
... that is hidden behind a define so that decoder logging can be
enabled without having to re-generate the decoders.

Bug: b/189133053
Test: enable logging on Cuttlefish
Change-Id: I519188704e77fbdb065adc1d7ef9f0f5f731a60f

host-common/logging.h
@@ -19,6 +19,13 @@
 #define GL_LOG(...) ((void)0)
 #endif
 
+//#define ENABLE_DECODER_LOG 1
+#if defined(ENABLE_DECODER_LOG)
+#define DECODER_DEBUG_LOG(...) GFXSTREAM_LOG(stderr, "I", __VA_ARGS__)
+#else
+#define DECODER_DEBUG_LOG(...) ((void)0)
+#endif
+
 #define ERR(...)                                 \
     do {                                         \
         GFXSTREAM_LOG(stderr, "E", __VA_ARGS__); \

aosp-mirror/platform_external_qemu

android/android-emugl/shared/emugl/common/logging.h
@@ -55,3 +55,10 @@ EMUGL_COMMON_API void set_emugl_cxt_logger(emugl_logger_t f);
 #else
 #define GL_LOG(...) 0
 #endif
+
+//#define ENABLE_DECODER_LOG 1
+#if defined(ENABLE_DECODER_LOG)
+#define DECODER_DEBUG_LOG(...) GL_LOG(stderr, "I", __VA_ARGS__)
+#else
+#define DECODER_DEBUG_LOG(...) ((void)0)
+#endif