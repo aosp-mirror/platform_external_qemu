0140767002794dc15f8e057c5d3d3c1d188133eb
Erwin Jansen <jansene@google.com>
2022-01-26 16:23:47 -0800

    Add support for JWT signing in python library
    The python gRPC library can now auto generate signed JWT tokens to
    connect to a local emulator.
    
    Includes documentation on how to use JWT tokens.
    
    Change-Id: If011f8e7d010af819fd382ea3007391eeeadbe24

--- android/android-grpc/python/setup.py
+++ android/android-grpc/python/setup.py
@@ -164,7 +164,7 @@ setup(
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
-    install_requires=["grpcio", "protobuf", "six"],  # Optional
+    install_requires=["grpcio", "protobuf", "six", "tink"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
