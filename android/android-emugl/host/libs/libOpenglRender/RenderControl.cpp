1d99bfb318deaf2013841b2cce4eb9e0399c7af7
Jason Macnak <natsu@google.com>
2022-09-14 10:18:26 -0700

    Add rcGetHostExtensions()
    ... to eventually prevent calling rcGetGlString() in non-GL
    specific contexts (which guest HostConnection calls by default).
    For now, this just defaults to rcGetGlString(GL_EXTENSIONS).
    Upcoming refactoring for b/233939967 will update this.
    
    Bug: b/233939967
    Test: presubmit
    Change-Id: I67ac25081b8318089839079e85875c5fff9d8163
    (cherry picked from commit f7d709515cccf67914f7a83a9d54840beb30cefb)
    Merged-In: I67ac25081b8318089839079e85875c5fff9d8163

--- android/android-emugl/host/libs/libOpenglRender/RenderControl.cpp
+++ android/android-emugl/host/libs/libOpenglRender/RenderControl.cpp
@@ -1643,6 +1643,11 @@ static void rcSetProcessMetadata(char* key, RenderControlByte* valuePtr, uint32_
     }
 }
 
+static int rcGetHostExtensionsString(uint32_t bufferSize, void* buffer) {
+    // TODO(b/233939967): split off host extensions from GL extensions.
+    return rcGetGLString(GL_EXTENSIONS, buffer, bufferSize);
+}
+
 void initRenderControlContext(renderControl_decoder_context_t *dec)
 {
     dec->rcGetRendererVersion = rcGetRendererVersion;
@@ -1714,4 +1719,5 @@ void initRenderControlContext(renderControl_decoder_context_t *dec)
     dec->rcGetFBDisplayConfigsParam = rcGetFBDisplayConfigsParam;
     dec->rcGetFBDisplayActiveConfig = rcGetFBDisplayActiveConfig;
     dec->rcSetProcessMetadata = rcSetProcessMetadata;
+    dec->rcGetHostExtensionsString = rcGetHostExtensionsString;
 }
