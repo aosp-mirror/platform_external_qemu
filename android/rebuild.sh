f103b80df69ef15fcbde8121a8f6f6dd5b291cec
Erwin Jansen <jansene@google.com>
2022-09-16 09:19:14 -0700

    Use the internal python toolchain
    This removes all the external dependencies and makes sure the build
    script can work with the internal Python3 toolchain in AOSP
    
    Bug: 245390993
    Change-Id: I8e046c1694731cfff1d680419d571a701a1f839d

--- android/rebuild.sh
+++ android/rebuild.sh
@@ -5,24 +5,51 @@
 #
 # assume that the device tree is in TOP
 #
-. $(dirname "$0")/scripts/unix/utils/common.shi
-PROGDIR=`dirname $0`
 set -e
 export LANG=C
 export LC_ALL=C
+PROGDIR=$(dirname $0)
+AOSP_DIR=$PROGDIR/../../..
 
-exists() {
-  # Returns true if $1 is on the path.
-  command -v "$1" >/dev/null 2>&1
+# Return the build machine's operating system tag.
+# Valid return values are:
+#    linux
+#    darwin
+#    freebsd
+#    windows   (really MSys)
+#    cygwin
+get_build_os () {
+    if [ -z "$_SHU_BUILD_OS" ]; then
+        _SHU_BUILD_OS=$(uname -s)
+        case $_SHU_BUILD_OS in
+            Darwin)
+                _SHU_BUILD_OS=darwin
+                ;;
+            FreeBSD)  # note: this is not tested
+                _SHU_BUILD_OS=freebsd
+                ;;
+            Linux)
+                # note that building  32-bit binaries on x86_64 is handled later
+                _SHU_BUILD_OS=linux
+                ;;
+            CYGWIN*|*_NT-*)
+                _SHU_BUILD_OS=windows
+                if [ "x$OSTYPE" = xcygwin ] ; then
+                    _SHU_BUILD_OS=cygwin
+                fi
+                ;;
+        esac
+    fi
+    echo "$_SHU_BUILD_OS"
 }
 
-PYTHON=python
+aosp_find_python() {
+    local AOSP_PREBUILTS_DIR=$AOSP_DIR/prebuilts
+    local OS_NAME=$(get_build_os)
+    local PYTHON=$AOSP_PREBUILTS_DIR/python/$OS_NAME-x86/bin/python3
+    $PYTHON --version >/dev/null || panic "Unable to get python version from $PYTHON"
+    printf "$PYTHON"
+}
 
-# Prefer python3 if it is available.
-if ! exists $PYTHON; then
-    PYTHON=python3 
-    echo "Using python3"
-    echo "This mmight not work with QEMU dependencies (b/229251286)!" 
-fi
-
+PYTHON=$(aosp_find_python)
 $PYTHON $PROGDIR/build/python/cmake.py --ccache auto $*
