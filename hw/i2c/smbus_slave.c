a0f34250179d5e28d894d0a0ba285ec83055e28c
Joe Komlodi <komlodi@google.com>
2022-10-11 16:55:19 +0000

    hw/i2c: smbus_slave: Reset state on reset
    If a reset comes while the SMBus device is not in its idle state, it's
    possible for it to get confused on valid transactions post-reset.
    
    Google-Bug-Id: 262983213
    Google-Rebase-Count: 4
    Change-Id: Ia36afb98d3b22887de9148155f081942683af983
    Signed-off-by: Joe Komlodi <komlodi@google.com>
    Original-Change-Id: http://go/team-change-id/Ifafc2b04331cba26a8f73ae2cae9e2324307d3d9
    Google-Bug-Id: 231153633

--- hw/i2c/smbus_slave.c
+++ hw/i2c/smbus_slave.c
@@ -205,10 +205,19 @@ static int smbus_i2c_send(I2CSlave *s, uint8_t data)
     return ret;
 }
 
+static void smbus_device_enter_reset(Object *obj, ResetType type)
+{
+    SMBusDevice *dev = SMBUS_DEVICE(obj);
+    dev->mode = SMBUS_IDLE;
+    dev->data_len = 0;
+}
+
 static void smbus_device_class_init(ObjectClass *klass, void *data)
 {
     I2CSlaveClass *sc = I2C_SLAVE_CLASS(klass);
+    ResettableClass *rc = RESETTABLE_CLASS(klass);
 
+    rc->phases.enter = smbus_device_enter_reset;
     sc->event = smbus_i2c_event;
     sc->recv = smbus_i2c_recv;
     sc->send = smbus_i2c_send;
