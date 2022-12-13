f8383153d1c30f77dc9f9faf3409c1bdb785cc86
Weilun Du <wdu@google.com>
2022-12-13 12:37:29 -0500

    [Bug Fix] Update modem simulator time when QT application state changes
    When emulator wakes up from sleep, the HandleSignalStrength callback is
    not necessarily invoked and this will prevent the emualtor time from being
    updated promptly. Therefore, we can force time update when QT application state
    changes from inactive to active.
    
    BUG: 257097404
    
    Change-Id: I15b968f270b2cd8bfd36635be0c7cdbbcab00425
    Signed-off-by: Weilun Du <wdu@google.com>

--- android/android-emu/android/skin/qt/emulator-qt-window.h
+++ android/android-emu/android/skin/qt/emulator-qt-window.h
@@ -38,6 +38,7 @@
 #include <QDragEnterEvent>
 #include <QDropEvent>
 #include <QFrame>
+#include <QGuiApplication>
 #include <QImage>
 #include <QImageReader>
 #include <QMessageBox>
@@ -401,7 +402,7 @@ public slots:
 
     void onScreenConfigChanged();
     void onScreenChanged(QScreen* newScreen);
-
+    void onApplicationStateChanged(Qt::ApplicationState state);
     bool event(QEvent* ev) override;  // Used to resume the MV on un-minimize
 
 public:
