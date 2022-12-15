80b254b84e38c1d3c69c9d61530d5a70317b6613
Haitao Shan <hshan@google.com>
2022-12-14 18:01:04 -0800

    Print error messages and exit when running with HAXM 7.8.0.
    BUG: 260749581
    Change-Id: I84775caec833e06b95e5ea580f6ee5c681a62b83
    (cherry picked from commit aae572df303f13a9e7208409f11edbc8f938999b)
    (cherry picked from commit 7c03f8e50593b848e934bd0878d57cff62469fa4)
    Merged-In: I84775caec833e06b95e5ea580f6ee5c681a62b83

--- android/emu/feature/src/android/emulation/CpuAccelerator.cpp
+++ android/emu/feature/src/android/emulation/CpuAccelerator.cpp
@@ -353,6 +353,8 @@ AndroidCpuAcceleration ProbeKVM(std::string* status) {
 
 #define HAXM_INSTALLER_VERSION_MINIMUM 0x06000001
 #define HAXM_INSTALLER_VERSION_MINIMUM_APPLE 0x6020001
+#define HAXM_INSTALLER_VERSION_RECOMMENDED 0x7060005
+#define HAXM_INSTALLER_VERSION_INCOMPATIBLE 0x7080000
 
 std::string cpuAcceleratorFormatVersion(int32_t version) {
     if (version < 0) {
@@ -592,6 +594,14 @@ AndroidCpuAcceleration ProbeHAX(std::string* status) {
                             HAXM_INSTALLER_VERSION_MINIMUM));
             return ANDROID_CPU_ACCELERATION_ACCEL_OBSOLETE;
         }
+
+        if (haxm_installer_version >= HAXM_INSTALLER_VERSION_INCOMPATIBLE) {
+            StringAppendFormat(
+                    status, "HAXM (version %s) is not compatible with the "
+		    "android emulator. Version 7.6.5 is recommended.",
+                    cpuAcceleratorFormatVersion(haxm_installer_version));
+            return ANDROID_CPU_ACCELERATION_ACCEL_OBSOLETE;
+        }
     }
 
     // 1) Try to find the HAX kernel module.
@@ -657,6 +667,9 @@ AndroidCpuAcceleration ProbeHAX(std::string* status) {
     StringAppendFormat(status, "HAXM version %s (%d) is installed and usable.",
                        cpuAcceleratorFormatVersion(haxm_installer_version),
                        hax_version.current_version);
+    if (haxm_installer_version > HAXM_INSTALLER_VERSION_RECOMMENDED)
+        StringAppendFormat(status, " Warning: HAXM version greater than 7.6.5 is not "
+                       "recommended. Some AVDs may fail to boot.");
     GlobalState* g = &gGlobals;
     ::snprintf(g->version, sizeof(g->version), "%s",
                cpuAcceleratorFormatVersion(haxm_installer_version).c_str());
