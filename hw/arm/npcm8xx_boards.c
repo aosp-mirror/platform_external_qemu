7a05b38c7e482128b0cc3c7de7478f3dc822a546
Hao Wu <wuhaotsh@google.com>
2023-01-17 11:44:23 -0800

    hw/arm: Use new flash type for NPCM845 EVB
    Currently NPCM845 EVB image uses a 64MB flash and this flash type
    is compatible with it.
    
    Tested that recent NPCM845 EVB image can boot up with this change.
    
    Google-Rebase-Count: 3
    Signed-off-by: Hao Wu <wuhaotsh@google.com>
    Change-Id: Ib710bf507e6ae18c4b91b1be300586bf78a0e91b

--- hw/arm/npcm8xx_boards.c
+++ hw/arm/npcm8xx_boards.c
@@ -231,7 +231,7 @@ static void npcm845_evb_init(MachineState *machine)
     qdev_realize(DEVICE(soc), NULL, &error_fatal);
 
     npcm8xx_load_bootrom(machine, soc);
-    npcm8xx_connect_flash(&soc->fiu[0], 0, "w25q256", drive_get(IF_MTD, 0, 0));
+    npcm8xx_connect_flash(&soc->fiu[0], 0, "mx66u51235f", drive_get(IF_MTD, 0, 0));
     npcm845_evb_i2c_init(soc);
     npcm845_evb_fan_init(NPCM8XX_MACHINE(machine), soc);
     sdhci_attach_drive(&soc->mmc.sdhci, 0);
