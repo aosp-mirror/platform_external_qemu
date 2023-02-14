ea39a384278c37186da5fdcb52cc6bc36d8fa553
Bo Hu <bohu@google.com>
2023-02-14 14:29:02 -0800

    downloadable snapshot: recreate firstboot.ini
    As it could get wiped out when copying downloaded snapshot.
    
    Bug: 249176844
    
    Change-Id: I75be478b12d430d5e10927a614b98f51d054b42c

--- android-qemu2-glue/main.cpp
+++ android-qemu2-glue/main.cpp
@@ -1431,7 +1431,6 @@ static SnapshotCompatibleType checkFirstbootIniCompatible(
 
     std::vector<std::string> keysOther{
             "Host.Os.Type",
-            "Host.Cpu.Model",
             "Host.Hypervisor.Name",
     };
 
@@ -2238,6 +2237,13 @@ extern "C" int main(int argc, char** argv) {
                         System::get()->findBundledExecutable("qemu-img");
                 std::string dataimageext4 = std::string(hw->hw_sdCard_path);
                 convertExt4ToQcow2(dataimageext4);
+            }
+
+            // firstboot.ini could be wiped out, create again
+            if (!path_exists(orgSrcFirstbootIniFileName.c_str())) {
+                android::avd::BugreportInfo bugInfo;
+                bugInfo.dumpFirstbootInfoForDownloadableSnapshot(
+                        orgSrcFirstbootIniFileName);
             }
 
             free(s_AvdFolder);
