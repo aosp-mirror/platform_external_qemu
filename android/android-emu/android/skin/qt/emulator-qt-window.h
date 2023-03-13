110050a098fcaba12c48844e614c66c8f429a3bc
Weilun Du <wdu@google.com>
2023-03-13 16:09:21 -0400

    Add an option to disable Pinch-to-Zoom feature
    This option is only available when emulator is running in standalone
    mode.
    
    BUG: 242496928
    Change-Id: Icbea0cf2d427bdbc1121c6672f4dffc8d1bac424
    Signed-off-by: Weilun Du <wdu@google.com>

--- android/android-emu/android/skin/qt/emulator-qt-window.h
+++ android/android-emu/android/skin/qt/emulator-qt-window.h
@@ -264,6 +264,7 @@ public:
     void setFrameAlways(bool showFrame);
     void setOnTop(bool onTop);
     void setIgnoreWheelEvent(bool ignore);
+    void setDisablePinchToZoom(bool disabled);
     void setPauseAvdWhenMinimized(bool ignore);
     void simulateKeyPress(int keyCode, int modifiers);
     void simulateScrollBarChanged(int x, int y);
@@ -570,6 +571,7 @@ private:
     OnDemandProgressDialog mPushDialog;
 
     bool mIgnoreWheelEvent = false;
+    bool mDisablePinchToZoom = false;
     bool mPauseAvdWhenMinimized = false;
     QTimer mWheelScrollTimer;
 #if QT_VERSION >= 0x060000
