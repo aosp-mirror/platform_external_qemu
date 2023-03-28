5c8304cb2b55a4b6568c5b670b3b37289d963062
Weilun Du <wdu@google.com>
2023-03-28 16:48:38 -0400

    [ASAN] Fix Double Free on Shutdown
    BUG: 275079481
    
    Change-Id: I7dd785db3dc0424a10542ce718d6ada5f7ff5800
    Signed-off-by: Weilun Du <wdu@google.com>

--- android/android-emu/android/emulation/H264NaluParser.cpp
+++ android/android-emu/android/emulation/H264NaluParser.cpp
@@ -37,44 +37,44 @@
 #define H264_WARN(fmt,...) H264_PRINT(YEL, fmt, ##__VA_ARGS__);
 #define H264_ERROR(fmt,...) H264_PRINT(RED, fmt, ##__VA_ARGS__);
 
+static const std::string kNaluTypesStrings[] = {
+        "0: Unspecified (non-VCL)",
+        "1: Coded slice of a non-IDR picture (VCL)",  // P frame
+        "2: Coded slice data partition A (VCL)",
+        "3: Coded slice data partition B (VCL)",
+        "4: Coded slice data partition C (VCL)",
+        "5: Coded slice of an IDR picture (VCL)",  // I frame
+        "6: Supplemental enhancement information (SEI) (non-VCL)",
+        "7: Sequence parameter set (non-VCL)",  // SPS parameter
+        "8: Picture parameter set (non-VCL)",   // PPS parameter
+        "9: Access unit delimiter (non-VCL)",
+        "10: End of sequence (non-VCL)",
+        "11: End of stream (non-VCL)",
+        "12: Filler data (non-VCL)",
+        "13: Sequence parameter set extension (non-VCL)",
+        "14: Prefix NAL unit (non-VCL)",
+        "15: Subset sequence parameter set (non-VCL)",
+        "16: Reserved (non-VCL)",
+        "17: Reserved (non-VCL)",
+        "18: Reserved (non-VCL)",
+        "19: Coded slice of an auxiliary coded picture without partitioning "
+        "(non-VCL)",
+        "20: Coded slice extension (non-VCL)",
+        "21: Coded slice extension for depth view components (non-VCL)",
+        "22: Reserved (non-VCL)",
+        "23: Reserved (non-VCL)",
+        "24: STAP-A Single-time aggregation packet (non-VCL)",
+        "25: STAP-B Single-time aggregation packet (non-VCL)",
+        "26: MTAP16 Multi-time aggregation packet (non-VCL)",
+        "27: MTAP24 Multi-time aggregation packet (non-VCL)",
+        "28: FU-A Fragmentation unit (non-VCL)",
+        "29: FU-B Fragmentation unit (non-VCL)",
+        "30: Unspecified (non-VCL)",
+        "31: Unspecified (non-VCL)",
+};
+
 namespace android {
 namespace emulation {
-
-const std::string H264NaluParser::kNaluTypesStrings[] =
-{
-    "0: Unspecified (non-VCL)",
-    "1: Coded slice of a non-IDR picture (VCL)",    // P frame
-    "2: Coded slice data partition A (VCL)",
-    "3: Coded slice data partition B (VCL)",
-    "4: Coded slice data partition C (VCL)",
-    "5: Coded slice of an IDR picture (VCL)",      // I frame
-    "6: Supplemental enhancement information (SEI) (non-VCL)",
-    "7: Sequence parameter set (non-VCL)",         // SPS parameter
-    "8: Picture parameter set (non-VCL)",          // PPS parameter
-    "9: Access unit delimiter (non-VCL)",
-    "10: End of sequence (non-VCL)",
-    "11: End of stream (non-VCL)",
-    "12: Filler data (non-VCL)",
-    "13: Sequence parameter set extension (non-VCL)",
-    "14: Prefix NAL unit (non-VCL)",
-    "15: Subset sequence parameter set (non-VCL)",
-    "16: Reserved (non-VCL)",
-    "17: Reserved (non-VCL)",
-    "18: Reserved (non-VCL)",
-    "19: Coded slice of an auxiliary coded picture without partitioning (non-VCL)",
-    "20: Coded slice extension (non-VCL)",
-    "21: Coded slice extension for depth view components (non-VCL)",
-    "22: Reserved (non-VCL)",
-    "23: Reserved (non-VCL)",
-    "24: STAP-A Single-time aggregation packet (non-VCL)",
-    "25: STAP-B Single-time aggregation packet (non-VCL)",
-    "26: MTAP16 Multi-time aggregation packet (non-VCL)",
-    "27: MTAP24 Multi-time aggregation packet (non-VCL)",
-    "28: FU-A Fragmentation unit (non-VCL)",
-    "29: FU-B Fragmentation unit (non-VCL)",
-    "30: Unspecified (non-VCL)",
-    "31: Unspecified (non-VCL)",
-};
 
 const std::string& H264NaluParser::naluTypeToString(H264NaluType n) {
     uint8_t idx = static_cast<uint8_t>(n);
