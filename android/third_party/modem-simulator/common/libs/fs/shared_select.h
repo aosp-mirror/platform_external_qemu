cf7cef591d230b876f2658d108ebc5adab2b5fc6
Bo Hu <bohu@google.com>
2023-08-31 08:45:08 -0700

    add comment to shared_select.h
    This is just to trigger a refresh, hopefully
    
    try 1
    
    Change-Id: I459e8152f64efb1a6d7604b3e22d9312cf55cfa7

--- android/third_party/modem-simulator/common/libs/fs/shared_select.h
+++ android/third_party/modem-simulator/common/libs/fs/shared_select.h
@@ -69,6 +69,8 @@ int Select(SharedFDSet* read_set,
            SharedFDSet* error_set,
            struct timeval* timeout);
 
+/* changed to ssize_t from int */
+
 ssize_t ReadExact(SharedFD fd, std::string* buffer);
 ssize_t WriteAll(SharedFD fd, const std::string& buffer);
 }  // namespace cuttlefish
