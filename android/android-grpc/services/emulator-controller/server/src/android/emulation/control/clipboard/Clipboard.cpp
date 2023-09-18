16000070ba305956578978efaab65a7a8e671453
Erwin Jansen <jansene@google.com>
2023-09-18 11:21:40 -0700

    gRPC: Make clipboard asynchronous
    This simplifies the clipboard implementation to become fully
    asynchronous and adds additional logging that can be enabled with the
    `-debug-keys` flag.
    
    This can be used to better understand clipboard behavior. Note that the
    emulator tries to keep the host and guest clipboards in sync in the QT
    ui. (i.e. if the guest sets the clipboard the QT ui will set the host
    clipboard)
    
    Example clipboard interaction:
    INFO    | ClipboardPipe update, host->guest: 'Hello there!'
    INFO    | Forwarding clipboard event: `text: "Hello there!"` from `:local:ipv4:127.0.0.1:61936` to gRPC listeners.
    INFO    | Registering listener with id: :local:ipv4:127.0.0.1:61938
    INFO    | Event from: android for :local:ipv4:127.0.0.1:61938 (text: "Hello there!")
    INFO    | ClipboardPipe update, host->guest: 'Hello world'
    INFO    | Forwarding clipboard event: `text: "Hello world"` from `:local:ipv4:127.0.0.1:61936` to gRPC listeners.
    INFO    | Event from: :local:ipv4:127.0.0.1:61936 for :local:ipv4:127.0.0.1:61938 (text: "Hello world")
    
    Note that it appears that the clipboard only gets updated once the
    toolwindow received focus.
    
    With this change listeners will immediately receive an update event when
    a call to setClipboard is being made. If a client sets the clipboard an
    event will be fired to all other channels than the channel used to set
    the clipboard.
    
    Bug: 230136107
    Change-Id: I61a62de7aac288f680adbeaacda8200616359705

--- android/android-grpc/services/emulator-controller/server/src/android/emulation/control/clipboard/Clipboard.cpp
+++ android/android-grpc/services/emulator-controller/server/src/android/emulation/control/clipboard/Clipboard.cpp
@@ -14,19 +14,74 @@
 // limitations under the License.
 #include "android/emulation/control/clipboard/Clipboard.h"
 
-#include "aemu/base/synchronization/Lock.h"  // for AutoLock, StaticLock
+#include <memory>
+#include <vector>
+
+#include "android/grpc/utils/SimpleAsyncGrpc.h"
+#include "android/utils/debug.h"
+#include "google/protobuf/util/message_differencer.h"
 
 namespace android {
 namespace emulation {
 namespace control {
 
+static std::string peerId(const ::grpc::ServerContextBase* context) {
+    std::string flat;
+    for (const auto& str : context->auth_context()->GetPeerIdentity()) {
+        flat += ":" + std::string(str.data(), str.size());
+    }
+
+    flat += ":" + context->peer();
+    return flat;
+}
+
+class ClipDataEventStreamWriter
+    : public BaseEventStreamWriter<ClipData, ClipboardEvent> {
+public:
+    ClipDataEventStreamWriter(EventChangeSupport<ClipboardEvent>* listener,
+                              ::grpc::CallbackServerContext* context)
+        : BaseEventStreamWriter<ClipData, ClipboardEvent>(listener),
+          mContext(context) {}
+    virtual ~ClipDataEventStreamWriter() = default;
+
+    void eventArrived(const ClipboardEvent event) override {
+        std::string dest = peerId(mContext);
+        const std::lock_guard<std::mutex> lock(mEventLock);
+        if (google::protobuf::util::MessageDifferencer::Equals(event.data,
+                                                               mLastEvent)) {
+            VERBOSE_INFO(keys, "%s is already aware of %s", dest.c_str(),
+                         event.data.ShortDebugString().c_str());
+            return;
+        }
+
+        mLastEvent = event.data;
+
+        if (dest != event.source) {
+            VERBOSE_INFO(keys, "Event from: %s for %s (%s)",
+                         event.source.c_str(), dest.c_str(),
+                         event.data.ShortDebugString().c_str());
+
+            SimpleServerWriter<ClipData>::Write(event.data);
+        } else {
+            VERBOSE_INFO(keys, "Dropping update from: %s for %s ",
+                         event.source.c_str(), dest.c_str());
+        }
+    }
+
+private:
+    ::grpc::ServerContextBase* mContext;
+
+    ClipData mLastEvent;
+    std::mutex mEventLock;
+};
+
 Clipboard* sClipboard = nullptr;
-android::base::StaticLock sLock;
+std::mutex sLock;
 
 Clipboard* Clipboard::getClipboard(const QAndroidClipboardAgent* clipAgent) {
     // We do not expect this method to be called frequently (usually only once
     // during emulator startup).
-    android::base::AutoLock autoLock(sLock);
+    std::lock_guard lock(sLock);
     if (!sClipboard) {
         sClipboard = new Clipboard(clipAgent);
     }
@@ -44,24 +99,52 @@ void Clipboard::guestClipboardCallback(void* context,
                                        const uint8_t* data,
                                        size_t size) {
     auto self = static_cast<Clipboard*>(context);
-    android::base::AutoLock lock(self->mContentsLock);
+    std::lock_guard lock(self->mContentsLock);
     self->mContents.assign((char*)data, size);
-    self->mEventWaiter.newEvent();
+
+    // Note that the source will never match anyone! (peerId always starts with
+    // a ':')
+    ClipboardEvent event{.source = "android"};
+    event.data.set_text(self->mContents);
+
+    VERBOSE_INFO(keys, "Forwarding clipboard event: `%s` to gRPC listeners.",
+                 event.data.ShortDebugString().c_str());
+    self->fireEvent(event);
 }
 
-void Clipboard::sendContents(const std::string& clipdata) {
-    android::base::AutoLock lock(mContentsLock);
+void Clipboard::sendContents(const std::string& clipdata,
+                             const ::grpc::ServerContextBase* from) {
+    std::lock_guard lock(mContentsLock);
     mContents.assign(clipdata);
     mClipAgent->setGuestClipboardContents((uint8_t*)(clipdata.c_str()),
                                           clipdata.size());
+
+    ClipboardEvent event{.source = peerId(from)};
+    event.data.set_text(clipdata);
+    VERBOSE_INFO(
+            keys,
+            "Forwarding clipboard event: `%s` from `%s` to gRPC listeners.",
+            event.data.ShortDebugString().c_str(), event.source.c_str());
+    fireEvent(event);
 }
 
 std::string Clipboard::getCurrentContents() {
     return mContents;
 }
 
-EventWaiter* Clipboard::eventWaiter() {
-    return &mEventWaiter;
+grpc::ServerWriteReactor<ClipData>* Clipboard::eventWriter(
+        ::grpc::CallbackServerContext* context) {
+    VERBOSE_INFO(keys, "Registering listener with id: %s",
+                 peerId(context).c_str());
+    // Note that the source will never match anyone! (peerId always starts with
+    // a ':')
+    ClipboardEvent event{.source = "android"};
+    event.data.set_text(getCurrentContents());
+
+    auto stream = new ClipDataEventStreamWriter(this, context);
+    stream->eventArrived(event);
+
+    return stream;
 }
 
 }  // namespace control
