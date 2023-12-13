374726011d8dd43ea4440e790b8e0644b951ad7c
Erwin Jansen <jansene@google.com>
2023-12-13 15:16:52 -0800

    Remove unused crash information
    This removes information we are not using and/or has a high chance of
    leaking private information.
    
    Change-Id: I537326e568e22af34c5d9870a44fb510e61220e5

--- android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/emulator-qt-window.cpp
+++ android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/emulator-qt-window.cpp
@@ -1703,7 +1703,6 @@ void EmulatorQtWindow::startThread(StartFunction f, int argc, char** argv) {
             arguments += argv[i];
             arguments += '\n';
         }
-        CrashReporter::get()->attachData("qemu-main-loop-args.txt", arguments);
 
         mMainLoopThread = new MainLoopThread(f, argc, argv);
         QObject::connect(mMainLoopThread, &QThread::finished, &mContainer,
