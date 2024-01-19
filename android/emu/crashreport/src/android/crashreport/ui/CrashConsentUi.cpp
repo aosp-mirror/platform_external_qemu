1c986e8ac6b3e11bd12b952fb56b397ded996ac3
Bo Hu <bohu@google.com>
2024-01-18 20:28:08 -0800

    crash reporter: remove the message box about crash id
    The id is readily available in the idea.log or on the
    console. There is no need to force developer to dismiss
    the message box.
    
    Bug: 319260175
    Change-Id: I89abbe85990fb2be1d2556e85bd5affb569ebd84

--- android/emu/crashreport/src/android/crashreport/ui/CrashConsentUi.cpp
+++ android/emu/crashreport/src/android/crashreport/ui/CrashConsentUi.cpp
@@ -112,6 +112,15 @@ public:
             return;
         }
 
+        //bug: 319260175
+        // emulator crash reporter somehow manages to keep poping up dialog
+        // about the crash id, which has shown to be more annoying than helpful.
+        // the crash id is in the idea.log anyway. let don't show it.
+        return;
+
+#if 0
+        // keeping this for posterity
+
         // Show a modal dialog with the report information.
         auto showFunc = [](void* data) {
             std::string* report_id = reinterpret_cast<std::string*>(data);
@@ -130,6 +139,7 @@ public:
 
         getConsoleAgents()->emu->runOnUiThread(
                 showFunc, new std::string(report.id), false);
+#endif
     }
 
 private:
