b13791e6c2a201ba1a9c0eafdd203071cccdc94a
Bo Hu <bohu@google.com>
2024-08-13 17:03:28 -0700

    foldable: support pixel 9 pro fold
    Bug: 357981279
    
    Change-Id: Ia1bb6ba49563b5fdbe45b9971ecc31e64687bd83

--- android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/emulator-qt-window.cpp
+++ android/android-ui/modules/aemu-ui-qt/src/android/skin/qt/emulator-qt-window.cpp
@@ -3778,7 +3778,7 @@ bool EmulatorQtWindow::getMonitorRect(uint32_t* width, uint32_t* height) {
 void EmulatorQtWindow::setFoldedSkin() {
     if (hasSkin()) {
         avdInfo_setCurrentSkin(getConsoleAgents()->settings->avdInfo(),
-                               "folded");
+                               "closed");
         ScreenMask::loadMask();
         EmulatorSkin::getInstance()->reset();
     }
@@ -3809,7 +3809,7 @@ void EmulatorQtWindow::setNoSkin() {
 void EmulatorQtWindow::restoreSkin() {
     if (android_foldable_is_pixel_fold() && hasSkin()) {
         avdInfo_setCurrentSkin(getConsoleAgents()->settings->avdInfo(),
-                               "unfolded");
+                               "default");
         EmulatorSkin::getInstance()->reset();
         ScreenMask::loadMask();
     }
